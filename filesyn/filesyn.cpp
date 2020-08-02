d7242e15170c2fd333503b14f8709b1dc3bb4261
玉斌 张 <957714080@qq.com>
2020-08-02 22:15:36 +0800

    添加cmake信息

--- filesyn/filesyn.cpp
+++ filesyn/filesyn.cpp
@@ -3,10 +3,11 @@
 
 #include "filesyn.h"
 #include "boost/filesystem.hpp"
+#include "fileSynConfig.h"
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << fileSyn_VERSION_MAJOR << "." << fileSyn_VERSION_MINOR << endl;
 	return 0;
 }
