2490eb182c5b75fa941d29181b6d77120776c842
玉斌 张 <957714080@qq.com>
2020-08-18 23:03:22 +0800

    我犯傻了,  将没有new出来的obj给了智能指针,  导致访问权限除了问题,  果然还是没主意

--- filesyn/filesyn.cpp
+++ filesyn/filesyn.cpp
@@ -1,12 +1,19 @@
 ﻿// filesyn.cpp: 定义应用程序的入口点。
 //
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
 
-#include <boost/filesystem.hpp>
-#include <boost/regex.hpp>
 #include "filesyn.h"
 #include "fileInfo.h"
 #include "setting.h"
 #include "fileSynConfig.h"
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+
+
+// debug_new.cpp
+// compile by using: cl /EHsc /W4 /D_DEBUG /MDd debug_new.cpp
 
 /// <summary>
 /// 主要同步类
@@ -33,7 +40,7 @@ void fileSyn::scan( )
 }
 
 
-void fileSyn::addFolderCompare(folderCompare &folderCom)
+void fileSyn::addFolderCompare(folderCompare & folderCom)
 {
 	boost::shared_ptr<folderCompare> ptr(&folderCom);
 	folder.push_back(ptr);
@@ -44,24 +51,24 @@ void fileSyn::clearFodlerCompare( )
 	folder.clear( );
 }
 
-inline void EnableMemLeakCheck( )
-{
-	//该语句在程序退出时自动调用 _CrtDumpMemoryLeaks(),用于多个退出出口的情况.
-	//如果只有一个退出位置，可以在程序退出之前调用 _CrtDumpMemoryLeaks()
-	_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
-}
+//inline void EnableMemLeakCheck( )
+//{
+//	//该语句在程序退出时自动调用 _CrtDumpMemoryLeaks(),用于多个退出出口的情况.
+//	//如果只有一个退出位置，可以在程序退出之前调用 _CrtDumpMemoryLeaks()
+//	_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+//}
 
 int main()
 {
-	EnableMemLeakCheck( );
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	std::cout << fileSyn_VERSION_MAJOR << "." << fileSyn_VERSION_MINOR << std::endl;
 	fileSyn f;
 	boost::filesystem::path p("F:\\USD\\plugin\\usd");
 	boost::filesystem::path p2("D:\\job");
-	folderCompareSys fcom;
-	fcom.setFolderCompare(p, p2);
-	f.addFolderCompare(fcom);
-	f.scan( );
-	//_CrtDumpMemoryLeaks( );
+	folderCompareSys* fcom = new folderCompareSys();
+	fcom->setFolderCompare(p, p2);
+	f.addFolderCompare(*fcom);
+	//f.scan( );
+	_CrtDumpMemoryLeaks( );
 	return 0;
 }
