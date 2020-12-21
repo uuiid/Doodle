91d0f30357ebc09e8d738189891c570302a4e4f6
texiao_zhang <texiao_zhang>
2020-12-21 10:22:38 +0800

    更改powershell脚本的映射

--- script/Cmd_tool/map_net_disk.ps1
+++ script/Cmd_tool/map_net_disk.ps1
@@ -3,15 +3,14 @@ $Net.RemoveNetworkDrive("X:")
 $Net.RemoveNetworkDrive("V:")
 $Net.RemoveNetworkDrive("U:")
 $Net.RemoveNetworkDrive("T:")
+$Net.RemoveNetworkDrive("Y:")
 
-
-$Net.MapNetworkDrive("Y:",'\\192.168.10.253\动画共享')
-$Net.MapNetworkDrive("Z:",'\\192.168.10.253\动画共享')
-$Net.MapNetworkDrive("W:",'\\192.168.10.253\Prism_projects')
-$Net.MapNetworkDrive("X:",'\\192.168.10.253\changanhuanjie')
-$Net.MapNetworkDrive("V:",'\\192.168.10.253\DuBuXiaoYao_3')
-$Net.MapNetworkDrive("U:",'\\192.168.10.253\WanYuFengShen')
-$Net.MapNetworkDrive("T:",'\\192.168.10.253\KuangShenMoZun')
+$Net.MapNetworkDrive("Y:", '\\192.168.10.250\public\动画共享')
+$Net.MapNetworkDrive("W:", '\\192.168.10.250\public\Prism_projects')
+$Net.MapNetworkDrive("X:", '\\192.168.10.250\public\changanhuanjie')
+$Net.MapNetworkDrive("V:", '\\192.168.10.250\public\DuBuXiaoYao_3')
+$Net.MapNetworkDrive("U:", '\\192.168.10.250\public\WanYuFengShen')
+$Net.MapNetworkDrive("T:", '\\192.168.10.250\public\KuangShenMoZun')
 
 $rename = new-object -ComObject Shell.Application
 $rename.NameSpace("X:\").Self.Name = "长安幻街"
@@ -19,3 +18,4 @@ $rename.NameSpace("V:\").Self.Name = "独步逍遥v3"
 $rename.NameSpace("U:\").Self.Name = "万域封神"
 $rename.NameSpace("T:\").Self.Name = "狂神魔尊"
 $rename.NameSpace("W:\").Self.Name = "程序开发"
+$rename.NameSpace("Y:\").Self.Name = "动画共享"
