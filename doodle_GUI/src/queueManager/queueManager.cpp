e6038d25e0222f7623d08fe9ab58ec103a665b61
texiao_zhang <texiao_zhang>
2020-12-30 18:36:55 +0800

    添加ue4影片渲染队列配置文件, 添加队列管理器

---
+++ doodle_GUI/src/queueManager/queueManager.cpp
@@ -0,0 +1,8 @@
+#include "queueManager.h"
+
+DOODLE_NAMESPACE_S
+
+queueManagerModel::queueManagerModel(/* args */) {
+}
+
+DOODLE_NAMESPACE_E
---
+++ doodle_GUI/src/queueManager/queueManager.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <core_global.h>
+#include <doodle_global.h>
+
+#include <QtCore/QAbstractListModel>
+
+DOODLE_NAMESPACE_S
+class queueManagerModel {
+ public:
+  queueManagerModel(/* args */);
+  ~queueManagerModel();
+
+ private:
+};
+
+DOODLE_NAMESPACE_E
