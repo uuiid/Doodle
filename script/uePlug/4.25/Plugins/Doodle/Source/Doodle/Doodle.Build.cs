fbb9e5cb5db0010c6af7eb7422130ab18b4ca803
texiao_zhang <957714080@qq.com>
2021-03-31 18:53:08 +0800

    添加ue插件中的自定义资产

--- script/uePlug/4.25/Plugins/Doodle/Source/Doodle/Doodle.Build.cs
+++ script/uePlug/4.25/Plugins/Doodle/Source/Doodle/Doodle.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 //using System.IO;
@@ -11,15 +11,15 @@ public class doodle : ModuleRules
 
         PublicIncludePaths.AddRange(
             new string[] {
-				// ... add public include paths required here ...
-			}
+                // ... add public include paths required here ...
+            }
             );
 
 
         PrivateIncludePaths.AddRange(
             new string[] {
-				// ... add other private include paths required here ...
-			}
+                // ... add other private include paths required here ...
+            }
             );
 
 
@@ -28,16 +28,27 @@ public class doodle : ModuleRules
             new string[]
             {
                 "Core",
-        "ContentBrowser",
-        "EditorScriptingUtilities",
-				//我的abc导入
-				// "GeometryCache",
-        // "AlembicLibrary",
-        // "AlembicLibrary",
-        // "Engine",//几何缓存的骨骼依赖
-				"UnrealEd" //创建actor工厂类需要
-				// ... add other public dependencies that you statically link with here ...
-			}
+                "ContentBrowser",
+                "EditorScriptingUtilities",
+                //我的abc导入
+                "GeometryCache",
+
+                "AlembicLibrary",
+                "Engine",//几何缓存的骨骼依赖
+                "UnrealEd", //创建actor工厂类需要
+                // ... add other public dependencies that you statically link with here ...
+                //abc的包装需要
+                "AlembicLib",
+                "MeshUtilities",
+                "MaterialUtilities",
+                "PropertyEditor",
+                "SlateCore",
+                "Slate",
+                "EditorStyle",
+                "Eigen",
+                "RenderCore",
+                "RHI"
+            }
             );
 
 
@@ -53,28 +64,28 @@ public class doodle : ModuleRules
                 "Slate",
                 "SlateCore",
 
-        "ContentBrowser",
-        "EditorScriptingUtilities",
+                "ContentBrowser",
+                "EditorScriptingUtilities",
                 "GeometryCache",  //复制材质依赖
-				"AssetTools",     //资产工具 导入依赖
+                "AssetTools",     //资产工具 导入依赖
 
-				"DesktopPlatform",//桌面依赖  打开文件对话框的依赖
-				"AlembicImporter",//abc导入依赖
-				"AlembicLibrary",
+                "DesktopPlatform",//桌面依赖  打开文件对话框的依赖
+                "AlembicImporter",//abc导入依赖
+                "AlembicLibrary",
                 "RenderCore",
 
                 "AIModule",//这个是ai控制使用的
-				"PlacementMode"//这个是创建面板用的
-				// ... add private dependencies that you statically link with here ...	
-			}
+                "PlacementMode"//这个是创建面板用的
+                // ... add private dependencies that you statically link with here ...	
+            }
             );
 
 
         DynamicallyLoadedModuleNames.AddRange(
             new string[]
             {
-				// ... add any modules that your module loads dynamically here ...
-			}
+                // ... add any modules that your module loads dynamically here ...
+            }
             );
     }
 }
---
+++ script/uePlug/4.25/Plugins/Doodle/Source/Doodle/Private/AbcWrap/DoodleCacheBonesComponent.cpp
@@ -0,0 +1,102 @@
+﻿#include "AbcWrap/DoodleCacheBonesComponent.h"
+
+#include "AbcWrap/DoodleGeometryCacheBones.h"
+
+#include "GeometryCacheComponent.h"
+#include "GeometryCache.h"
+
+UDoodleCacheBonesComponent::UDoodleCacheBonesComponent()
+{
+	//// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	//// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+
+	this->p_GeometryCache_bones = /*NewObject<UGeometryCache>(this);*/
+		this->CreateDefaultSubobject<UDoodleGeometryCacheBones>(FName{ UDoodleGeometryCacheBones::StaticClass()->GetName() });
+}
+
+bool UDoodleCacheBonesComponent::DoesSocketExist(FName InSocketName) const
+{
+	if (!p_GeometryCache_bones->p_GeometryCache_curve)
+		return false;
+	auto it = p_GeometryCache_bones->p_GeometryCache_curve->GetTranAnm().FindByPredicate([InSocketName](FTransformCurve& anm)->bool {
+		return anm.Name.DisplayName == InSocketName;
+	});
+	return it != nullptr;
+	//return false;
+}
+
+FTransform UDoodleCacheBonesComponent::GetSocketTransform(FName InSocketName, ERelativeTransformSpace TransformSpace) const
+{
+	if (!p_GeometryCache_bones->p_GeometryCache_curve)
+		return FTransform();
+
+	auto tran = GetComponentTransform();
+	if (InSocketName != NAME_None) {
+		auto socket = p_GeometryCache_bones->p_GeometryCache_curve->GetTranAnm().FindByPredicate([InSocketName](FTransformCurve& anm)->bool {
+			return anm.Name.DisplayName == InSocketName;
+		});
+		if (socket != nullptr) {
+			TArray<float> k_time{};
+			TArray<FTransform> k_tran{};
+
+			socket->GetKeys(k_time, k_tran);
+			const auto cuTime = this->p_GeometryCache_component->GetAnimationTime();
+			UE_LOG(LogTemp, Log, TEXT("currtime--> %f"), cuTime);
+
+			const auto k_t_index = k_time.IndexOfByPredicate([cuTime](float v) {
+				return v > cuTime;
+			});
+			if (k_t_index != INDEX_NONE) {
+				tran = tran * k_tran[k_t_index];
+			}
+			else {
+				if (k_tran.Num() > 0)
+					tran = tran * k_tran[0];
+			}
+
+		}
+	}
+	return tran;
+	//return {};
+}
+
+bool UDoodleCacheBonesComponent::HasAnySockets() const
+{
+	if (!p_GeometryCache_component || !p_GeometryCache_bones->p_GeometryCache_curve) return false;
+
+	UE_LOG(LogTemp, Log, TEXT("has Socket %s"), (p_GeometryCache_bones->p_GeometryCache_curve->GetTranAnm().Num() > 0) ? TEXT("true") : TEXT("fasle"));
+	return p_GeometryCache_bones->p_GeometryCache_curve->GetTranAnm().Num() > 0;
+	//return false;
+}
+
+void UDoodleCacheBonesComponent::QuerySupportedSockets(TArray<FComponentSocketDescription>& qutSocket) const
+{
+	if (!p_GeometryCache_component || !p_GeometryCache_bones->p_GeometryCache_curve) return;
+
+	for (auto i : p_GeometryCache_bones->p_GeometryCache_curve->GetTranAnm()) {
+		FComponentSocketDescription skInfo{ i.Name.DisplayName,EComponentSocketType::Socket };
+		qutSocket.Add(skInfo);
+	}
+}
+
+
+// Called when the game starts
+void UDoodleCacheBonesComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+}
+
+
+// Called every frame
+void UDoodleCacheBonesComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
