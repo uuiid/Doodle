91aea12c78b399ea65b81343f308aeee5a1cf115
texiao_zhang <957714080@qq.com>
2021-04-02 14:23:35 +0800

    添加新模块, 保证注册自定义类的地方

---
+++ script/uePlug/4.25/Plugins/Doodle/Source/DoodleEditor/Public/DoodleEditor.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FToolBarBuilder;
+class FMenuBuilder;
+
+class FdoodleEditorModule : public IModuleInterface {
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+
+
+};
