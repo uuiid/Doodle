b333536be4c8d21ca955a6162703dc0501aa8f1e
texiao_zhang <957714080@qq.com>
2021-04-14 13:15:45 +0800

    修复在没有插件加载的情况下生成ue 可使用项目,

--- Include/corelib/FileWarp/Ue4Project.h
+++ Include/corelib/FileWarp/Ue4Project.h
@@ -26,11 +26,29 @@ class CORE_API Ue4ProjectFile {
   std::string Description;
   std::vector<Ue4ProjectFilePulgins> Plugins;
 
-  NLOHMANN_DEFINE_TYPE_INTRUSIVE(Ue4ProjectFile, FileVersion,
-                                 EngineAssociation,
-                                 Category,
-                                 Description,
-                                 Plugins);
+  // NLOHMANN_DEFINE_TYPE_INTRUSIVE(Ue4ProjectFile, FileVersion,
+  //                                EngineAssociation,
+  //                                Category,
+  //                                Description,
+  //                                Plugins);
+  friend void to_json(nlohmann::json& nlohmann_json_j, const Ue4ProjectFile& nlohmann_json_t) {
+    nlohmann_json_j["FileVersion"]       = nlohmann_json_t.FileVersion;
+    nlohmann_json_j["EngineAssociation"] = nlohmann_json_t.EngineAssociation;
+    nlohmann_json_j["Category"]          = nlohmann_json_t.Category;
+    nlohmann_json_j["Description"]       = nlohmann_json_t.Description;
+    nlohmann_json_j["Plugins"]           = nlohmann_json_t.Plugins;
+  };
+  friend void from_json(const nlohmann::json& nlohmann_json_j, Ue4ProjectFile& nlohmann_json_t) {
+    nlohmann_json_j.at("FileVersion").get_to(nlohmann_json_t.FileVersion);
+    nlohmann_json_j.at("EngineAssociation").get_to(nlohmann_json_t.EngineAssociation);
+    nlohmann_json_j.at("Category").get_to(nlohmann_json_t.Category);
+    nlohmann_json_j.at("Description").get_to(nlohmann_json_t.Description);
+    try {
+      nlohmann_json_j.at("Plugins").get_to(nlohmann_json_t.Plugins);
+    } catch (const nlohmann::json::exception& error) {
+      ;
+    }
+  };
 };
 
 class CORE_API Ue4Project {
@@ -38,15 +56,15 @@ class CORE_API Ue4Project {
   FSys::path p_ue_Project_path;
   ProjectPtr p_project;
 
-  void addUe4ProjectPlugins(const std::vector<std::string> &str) const;
-  void runPythonScript(const std::string &python_str) const;
-  void runPythonScript(const FSys::path &python_file) const;
+  void addUe4ProjectPlugins(const std::vector<std::string>& str) const;
+  void runPythonScript(const std::string& python_str) const;
+  void runPythonScript(const FSys::path& python_file) const;
 
  public:
   const static std::string Content;
   const static std::string ContentShot;
   const static std::string UE4PATH;
   Ue4Project(FSys::path project_path);
-  void createShotFolder(const std::vector<ShotPtr> &inShotList);
+  void createShotFolder(const std::vector<ShotPtr>& inShotList);
 };
 }  // namespace doodle
