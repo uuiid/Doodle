7a312cb1a20124e8b2c3da3438bb95c083a96c2c
texiao_zhang <957714080@qq.com>
2021-04-14 19:57:02 +0800

    图标更改

--- src/doodle_GUI/source/mainWidght/mainWindows.cpp
+++ src/doodle_GUI/source/mainWidght/mainWindows.cpp
@@ -15,32 +15,41 @@ DOODLE_NAMESPACE_S
 
 mainWindows::mainWindows()
     : wxFrame(nullptr, wxID_ANY, {"doodle"}) {
+  SetIcon(wxICON(ID_DOODLE_ICON));
+
+  // auto icon = wxIcon{};
+  // icon.LoadFile("",wxBITMAP_TYPE_ICON_RESOURCE);
+
   SetMenuBar(new wxMenuBar{});
   CreateStatusBar(1);
   SetStatusText("doodle tools");
 
-  auto k_parent  = new wxPanel{this, wxID_ANY};
-  auto layout    = new wxBoxSizer{wxVERTICAL};
-  auto k_butten  = new wxButton{k_parent, wxID_ANY, "test"};
-  auto k_butten2 = new wxButton{k_parent, wxID_ANY, "test2"};
-  auto k_butten3 = new wxButton{k_parent, wxID_ANY, "test3"};
-  layout->Add(k_butten, /* wxGBPosition{0, 0}, wxGBSpan{1, 2}, */ wxSizerFlags{0}.Left().GetFlags());
-  layout->Add(k_butten2, /* wxGBPosition{1, 1}, wxGBSpan{1, 1}, */ wxSizerFlags{0}.Right().GetFlags());
-  layout->Add(k_butten3, /* wxGBPosition{1, 0}, wxGBSpan{1, 1}, */ wxSizerFlags{0}.Right().GetFlags());
+  auto k_parent           = new wxPanel{this, wxID_ANY};
+  auto layout             = new wxBoxSizer{wxVERTICAL};
+  auto k_exMaya_button    = new wxButton{k_parent, wxID_ANY, "从maya导出相机和文件"};
+  auto k_create_image     = new wxButton{k_parent, wxID_ANY, "从图片创建视频"};
+  auto k_create_dir_image = new wxButton{k_parent, wxID_ANY, "从多个文件夹创建视频"};
+  auto k_create_video     = new wxButton{k_parent, wxID_ANY, "连接视频"};
+  auto k_create_ue4File   = new wxButton{k_parent, wxID_ANY, "创建ue4关卡序列"};
+  layout->Add(k_exMaya_button, wxSizerFlags{0}.Left());
+  layout->Add(k_create_image, wxSizerFlags{0}.Left());
+  layout->Add(k_create_dir_image, wxSizerFlags{0}.Left());
+  layout->Add(k_create_video, wxSizerFlags{0}.Left());
+  layout->Add(k_create_ue4File, wxSizerFlags{0}.Left());
 
   k_parent->SetSizer(layout);
   layout->SetSizeHints(this);
 
-  k_butten->DragAcceptFiles(true);
+  k_exMaya_button->DragAcceptFiles(true);
 
-  k_butten->Bind(
+  k_exMaya_button->Bind(
       wxEVT_BUTTON,
       [this](wxCommandEvent& event) {
         auto k_ = wxMessageDialog{this, "ok", "ok"};
         k_.ShowModal();
         std::cout << "ok" << std::endl;
       });
-  k_butten->Bind(
+  k_exMaya_button->Bind(
       wxEVT_DROP_FILES,
       [this](wxDropFilesEvent& event) {
         if (event.GetNumberOfFiles() > 0) {
@@ -50,110 +59,6 @@ mainWindows::mainWindows()
           k_.ShowModal();
         }
       });
-
-  // Make a menubar
-  // wxMenu *file_menu = new wxMenu;
-
-  // file_menu->Append(LAYOUT_TEST_PROPORTIONS, "&Proportions demo...\tF1");
-  // file_menu->Append(LAYOUT_TEST_SIZER, "Test wx&FlexSizer...\tF2");
-  // file_menu->Append(LAYOUT_TEST_NB_SIZER, "Test &notebook sizers...\tF3");
-  // file_menu->Append(LAYOUT_TEST_GB_SIZER, "Test &gridbag sizer...\tF4");
-  // file_menu->Append(LAYOUT_TEST_SET_MINIMAL, "Test Set&ItemMinSize...\tF5");
-  // file_menu->Append(LAYOUT_TEST_NESTED, "Test nested sizer in a wxPanel...\tF6");
-  // file_menu->Append(LAYOUT_TEST_WRAP, "Test wrap sizers...\tF7");
-
-  // file_menu->AppendSeparator();
-  // file_menu->Append(LAYOUT_QUIT, "E&xit", "Quit program");
-
-  // wxMenu *help_menu = new wxMenu;
-  // help_menu->Append(LAYOUT_ABOUT, "&About", "About layout demo...");
-
-  // wxMenuBar *menu_bar = new wxMenuBar;
-
-  // menu_bar->Append(file_menu, "&File");
-  // menu_bar->Append(help_menu, "&Help");
-
-  // // Associate the menu bar with the frame
-  // SetMenuBar(menu_bar);
-
-  // CreateStatusBar(2);
-  // SetStatusText("wxWidgets layout demo");
-
-  // wxPanel *p = new wxPanel(this, wxID_ANY);
-
-  // we want to get a dialog that is stretchable because it
-  // has a text ctrl in the middle. at the bottom, we have
-  // two buttons which.
-
-  // wxBoxSizer *topsizer = new wxBoxSizer(wxVERTICAL);
-
-  // // 1) top: create wxStaticText with minimum size equal to its default size
-  // topsizer->Add(
-  //     new wxStaticText(p, wxID_ANY, "An explanation (wxALIGN_RIGHT)."),
-  //     wxSizerFlags().Align(wxALIGN_RIGHT).Border(wxALL & ~wxBOTTOM, 5));
-  // topsizer->Add(
-  //     new wxStaticText(p, wxID_ANY, "An explanation (wxALIGN_LEFT)."),
-  //     wxSizerFlags().Align(wxALIGN_LEFT).Border(wxALL & ~wxBOTTOM, 5));
-  // topsizer->Add(
-  //     new wxStaticText(p, wxID_ANY, "An explanation (wxALIGN_CENTRE_HORIZONTAL)."),
-  //     wxSizerFlags().Align(wxALIGN_CENTRE_HORIZONTAL).Border(wxALL & ~wxBOTTOM, 5));
-
-  // // 2) top: create wxTextCtrl with minimum size (100x60)
-  // topsizer->Add(
-  //     new wxTextCtrl(p, wxID_ANY, "My text (wxEXPAND).", wxDefaultPosition, wxSize(100, 60), wxTE_MULTILINE),
-  //     wxSizerFlags(1).Expand().Border(wxALL, 5));
-
-  // // 2.5) Gratuitous test of wxStaticBoxSizers
-  // wxBoxSizer *statsizer = new wxStaticBoxSizer(
-  //     new wxStaticBox(p, wxID_ANY, "A wxStaticBoxSizer"), wxVERTICAL);
-  // statsizer->Add(
-  //     new wxStaticText(p, wxID_ANY, "And some TEXT inside it"),
-  //     wxSizerFlags().Border(wxALL, 30));
-  // topsizer->Add(
-  //     statsizer,
-  //     wxSizerFlags(1).Expand().Border(wxALL, 10));
-
-  // 2.7) And a test of wxGridSizer
-  // wxGridSizer *gridsizer = new wxGridSizer(2, 5, 5);
-  // gridsizer->Add(new wxStaticText(p, wxID_ANY, "Label"),
-  //                wxSizerFlags().Align(wxALIGN_RIGHT | wxALIGN_CENTER_VERTICAL));
-  // gridsizer->Add(new wxTextCtrl(p, wxID_ANY, "Grid sizer demo"),
-  //                wxSizerFlags(1).Align(wxGROW | wxALIGN_CENTER_VERTICAL));
-  // gridsizer->Add(new wxStaticText(p, wxID_ANY, "Another label"),
-  //                wxSizerFlags().Align(wxALIGN_RIGHT | wxALIGN_CENTER_VERTICAL));
-  // gridsizer->Add(new wxTextCtrl(p, wxID_ANY, "More text"),
-  //                wxSizerFlags(1).Align(wxGROW | wxALIGN_CENTER_VERTICAL));
-  // gridsizer->Add(new wxStaticText(p, wxID_ANY, "Final label"),
-  //                wxSizerFlags().Align(wxALIGN_RIGHT | wxALIGN_CENTER_VERTICAL));
-  // gridsizer->Add(new wxTextCtrl(p, wxID_ANY, "And yet more text"),
-  //                wxSizerFlags().Align(wxGROW | wxALIGN_CENTER_VERTICAL));
-  // topsizer->Add(
-  //     gridsizer,
-  //     wxSizerFlags().Proportion(1).Expand().Border(wxALL, 10));
-
-  // #if wxUSE_STATLINE
-  //   // 3) middle: create wxStaticLine with minimum size (3x3)
-  //   topsizer->Add(
-  //       new wxStaticLine(p, wxID_ANY, wxDefaultPosition, wxSize(3, 3), wxHORIZONTAL),
-  //       wxSizerFlags().Expand());
-  // #endif  // wxUSE_STATLINE
-
-  // 4) bottom: create two centred wxButtons
-  // wxBoxSizer *button_box = new wxBoxSizer(wxHORIZONTAL);
-  // button_box->Add(
-  //     new wxButton(p, wxID_ANY, "Two buttons in a box"),
-  //     wxSizerFlags().Border(wxALL, 7));
-  // button_box->Add(
-  //     new wxButton(p, wxID_ANY, "(wxCENTER)"),
-  //     wxSizerFlags().Border(wxALL, 7));
-
-  // topsizer->Add(button_box, wxSizerFlags().Center());
-
-  // p->SetSizer(topsizer);
-
-  // don't allow frame to get smaller than what the sizers tell it and also set
-  // the initial size as calculated by the sizers
-  // topsizer->SetSizeHints(this);
 }
 
 Doodle::Doodle(){
