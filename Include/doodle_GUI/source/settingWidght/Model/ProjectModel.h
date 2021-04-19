fb8a9d133a96cdebbced5a0863c016928a0d7b46
texiao_zhang <957714080@qq.com>
2021-04-19 17:26:07 +0800

    整理文件

--- Include/doodle_GUI/source/settingWidght/Model/ProjectModel.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <doodle_GUI/doodle_global.h>
-
-namespace doodle {
-
-}  // namespace doodle
--- src/doodle_GUI/source/settingWidght/Model/ProjectModel.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-#include <doodle_GUI/source/SettingWidght/Model/ProjectModel.h>
-#include <boost/numeric/conversion/cast.hpp>
-
-namespace doodle {
-
-}  // namespace doodle
