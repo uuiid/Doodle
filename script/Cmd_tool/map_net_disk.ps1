eaeedc866c256e7e78db41ff8ee3a8cb5bcc2284
texiao_zhang <957714080@qq.com>
2021-04-23 09:14:54 +0800

    更新盘符映射脚本

--- script/Cmd_tool/map_net_disk.ps1
+++ script/Cmd_tool/map_net_disk.ps1
@@ -1,35 +1,43 @@
-﻿$Net = New-Object -ComObject WScript.Network
-$Net.RemoveNetworkDrive("X:")
-$Net.RemoveNetworkDrive("V:")
-$Net.RemoveNetworkDrive("W:")
-$Net.RemoveNetworkDrive("Y:")
-$Net.RemoveNetworkDrive("U:")
-$Net.RemoveNetworkDrive("T:")
-$Net.RemoveNetworkDrive("S:")
-$Net.RemoveNetworkDrive("R:")
-$Net.RemoveNetworkDrive("Q:")
-
-# Stop-Process -ProcessName explorer
-# Start-Process explorer
-$Net.MapNetworkDrive("X:", '\\192.168.10.250\public\changanhuanjie')
-$Net.MapNetworkDrive("Y:", '\\192.168.10.250\public\动画共享')
-$Net.MapNetworkDrive("W:", '\\192.168.10.250\public\Prism_projects')
-$Net.MapNetworkDrive("V:", '\\192.168.10.250\public\DuBuXiaoYao_3')
-$Net.MapNetworkDrive("U:", '\\192.168.10.250\public\WanYuFengShen')
-$Net.MapNetworkDrive("T:", '\\192.168.10.250\public\KuangShenMoZun')
-$Net.MapNetworkDrive("S:", '\\192.168.10.250\public\CangFeng')
-$Net.MapNetworkDrive("R:", '\\192.168.10.250\public\WanGuXieDi')
-$Net.MapNetworkDrive("Q:", '\\192.168.10.250\public\renjianzuideyi')
-
+﻿if ( (Test-Connection "192.168.10.250" -Count 1 -Delay 1 -Quiet) -and (Test-Path -Path "\\192.168.10.250\public\changanhuanjie") ) {
+  $Net_MAPS = @(
+    @("X:", "\\192.168.10.250\public\changanhuanjie", "长安幻街"),
+    @("V:", "\\192.168.10.250\public\DuBuXiaoYao_3", "独步逍遥v3"),
+    @("W:", "\\192.168.10.250\public\Prism_projects", "程序开发"),
+    @("Y:", "\\192.168.10.250\public\动画共享", "动画共享"),
+    @("U:", "\\192.168.10.250\public\WanYuFengShen", "万域封神"),
+    @("T:", "\\192.168.10.250\public\KuangShenMoZun", "狂神魔尊"),
+    @("S:", "\\192.168.10.250\public\CangFeng", "藏锋"),
+    @("R:", "\\192.168.10.250\public\WanGuXieDi", "万古邪帝"),
+    @("Q:", "\\192.168.10.250\public\renjianzuideyi", "人间最得意")
+  )
+}
+elseif ( (Test-Connection "192.168.10.218" -Count 1 -Delay 1 -Quiet) -and (Test-Path -Path "\\192.168.10.218\changanhuanjie")) {
+  $Net_MAPS = @(
+    @("X:", "\\192.168.10.218\changanhuanjie", "长安幻街"),
+    @("U:", "\\192.168.10.218\WanYuFengShen", "万域封神"),
+    @("T:", "\\192.168.10.218\KuangShenMoZun", "狂神魔尊"),
+    @("Z:", "\\192.168.10.57\netDir", "苗雨共享")
+  )
+}
+else {
+  exit
+}
+$Net = New-Object -ComObject WScript.Network
+$Net_List = $Net.EnumNetworkDrives();
 $rename = new-object -ComObject Shell.Application
-$rename.NameSpace("X:\").Self.Name = "长安幻街"
-$rename.NameSpace("V:\").Self.Name = "独步逍遥v3"
-$rename.NameSpace("U:\").Self.Name = "万域封神"
-$rename.NameSpace("T:\").Self.Name = "狂神魔尊"
-$rename.NameSpace("W:\").Self.Name = "程序开发"
-$rename.NameSpace("Y:\").Self.Name = "动画共享"
-$rename.NameSpace("S:\").Self.Name = "藏锋"
-$rename.NameSpace("R:\").Self.Name = "万古邪帝"
-$rename.NameSpace("Q:\").Self.Name = "人间最得意"
+
+foreach ($nets in $Net_MAPS) {
+  Write-Host($nets[0] + " 映射到: " + $nets[2])
+  for ($i = 0; $i -lt ($Net_List.Count()); ++$i) {
+    if ($Net_List[$i] -eq $nets[0]) {
+      $Net.RemoveNetworkDrive($nets[0])
+    }
+    ++$i
+  }
+
+  $Net.MapNetworkDrive($nets[0], $nets[1])
+  $rename.NameSpace($nets[0] + "\").Self.Name = $nets[2]
+}
+
 Stop-Process -ProcessName explorer
 Start-Process explorer
