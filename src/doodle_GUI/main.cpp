ead4b335ceb046ec279105cebd657904000e641c
uuiid <957714080@qq.com>
2021-05-20 15:32:57 +0800

    关闭内存泄露报告,这样可以在debug时快速关闭

--- src/doodle_GUI/main.cpp
+++ src/doodle_GUI/main.cpp
@@ -12,7 +12,7 @@ extern "C" int WINAPI WinMain(HINSTANCE hInstance,
                               HINSTANCE hPrevInstance,
                               wxCmdLineArgType WXUNUSED(lpCmdLine),
                               int nCmdShow) try {
-//  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF );
   //设置一下文件系统后端
   auto k_local = boost::locale::generator().generate("");
   boost::filesystem::path::imbue(k_local);
@@ -22,7 +22,7 @@ extern "C" int WINAPI WinMain(HINSTANCE hInstance,
   //初始化设置
   auto &set = doodle::coreSet::getSet();
   set.init();
-  auto result = wxEntry(hInstance, hPrevInstance, NULL, nCmdShow);
+  auto result = wxEntry(hInstance, hPrevInstance, nullptr, nCmdShow);
   boost::log::core::get()->remove_all_sinks();
 
   return result;
