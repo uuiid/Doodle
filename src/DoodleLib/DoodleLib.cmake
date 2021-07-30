c56be2bd62718c2122af947dc552dd6471dd2b65
uuiid <957714080@qq.com>
2021-07-30 18:47:02 +0800

    添加树容器

--- src/DoodleLib/DoodleLib.cmake
+++ src/DoodleLib/DoodleLib.cmake
@@ -123,7 +123,7 @@ set(
     core/Ue4Setting.cpp
     core/Util.cpp
     core/static_value.cpp
-        libWarp/nana_warp.cpp
+    libWarp/nana_warp.cpp
     rpc/RpcFileSystemClient.cpp
     rpc/RpcFileSystemServer.cpp
     rpc/RpcMetadaataServer.cpp
