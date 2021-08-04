0506b0241903f7814aea8ced09df535057130401
uuiid <957714080@qq.com>
2021-08-04 16:00:43 +0800

    添加工具栏导出maya fbx类

--- src/DoodleLib/Gui/action/actn_tool_box.cpp
+++ src/DoodleLib/Gui/action/actn_tool_box.cpp
@@ -3,6 +3,26 @@
 //
 
 #include "actn_tool_box.h"
-namespace doodle::toolbox{
 
+#include <threadPool/long_term.h>
+
+namespace doodle::toolbox {
+
+actn_export_maya::actn_export_maya() {
+  p_term = std::make_shared<long_term>();
+  p_name = "导出maya fbx 文件";
 }
+long_term_ptr actn_export_maya::run() {
+  p_date = sig_get_arg().value();
+
+  if (p_date.is_cancel) {
+    cancel("取消导出");
+    return p_term;
+  }
+  return p_term;
+}
+bool actn_export_maya::is_async() {
+  return true;
+}
+
+}  // namespace doodle::toolbox
--- src/DoodleLib/Gui/action/actn_tool_box.h
+++ src/DoodleLib/Gui/action/actn_tool_box.h
@@ -3,10 +3,19 @@
 //
 
 #pragma once
+#include <DoodleLib/DoodleLib_fwd.h>
 
+#include "action.h"
 
 namespace doodle::toolbox {
 
-class actn_export_maya {
+class DOODLELIB_API actn_export_maya
+    : public action_toolbox<action_arg::arg_path> {
+ public:
+  actn_export_maya();
+  virtual bool is_async() override;
+
+ protected:
+  virtual long_term_ptr run() override;
 };
 }  // namespace doodle::toolbox
