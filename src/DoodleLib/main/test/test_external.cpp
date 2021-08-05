8c16c1ee0b4b7f16c30778f68844dd5f06ba915c
uuiid <957714080@qq.com>
2021-08-05 13:33:39 +0800

    添加cmake时区数据库

--- src/DoodleLib/main/test/test_external.cpp
+++ src/DoodleLib/main/test/test_external.cpp
@@ -2,19 +2,10 @@
 // Created by TD on 2021/7/29.
 //
 #include <DoodleLib/DoodleLib.h>
-#include <st_tree.h>
 
 #include <catch.hpp>
 
 TEST_CASE("st tree", "[external]") {
-  st_tree::tree<std::string, st_tree::ordered<>> t{};
-  t.insert("a");
-  t.root().insert("s");
-  auto it = t.begin()->find("a");
-  if (it != t.root().end())
-    std::cout << it->data() << std::endl;
-
-  t.root().find("a");
 }
 TEST_CASE("my tree", "[tree]") {
   using namespace doodle;
@@ -53,7 +44,6 @@ TEST_CASE("my tree", "[tree]") {
 }
 TEST_CASE("date time", "[time]") {
   using namespace doodle;
-  date::set_install();
   date::current_zone();
 
 }
