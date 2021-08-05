7c4c9a738ac8438b98725bac7858cd9734a5eae7
uuiid <957714080@qq.com>
2021-08-05 15:02:26 +0800

    更改时区文件下载方式

---
+++ CMake/update_Time_Zone_Database.cmake
@@ -0,0 +1,31 @@
+include(CMakePrintHelpers)
+
+set(db_win ${ROOT_DIR}/src/DoodleLib/resource/tzdata/windowsZones.xml)
+set(tzdata ${ROOT_DIR}/src/DoodleLib/resource/tzdata/tzdata-latest.tar.gz)
+set(tzdata_dir ${ROOT_DIR}/src/DoodleLib/resource/tzdata/)
+
+cmake_print_variables(
+        ROOT_DIR
+        db_win
+        tzdata
+        tzdata_dir)
+
+
+if (EXISTS ${tzdata_dir})
+    file(REMOVE_RECURSE ${tzdata_dir})
+else ()
+    file(MAKE_DIRECTORY ${tzdata_dir})
+endif ()
+
+file(DOWNLOAD
+        https://raw.githubusercontent.com/unicode-org/cldr/master/common/supplemental/windowsZones.xml
+        ${db_win}
+        SHOW_PROGRESS)
+file(DOWNLOAD
+        https://www.iana.org/time-zones/repository/tzdata-latest.tar.gz
+        ${tzdata}
+        SHOW_PROGRESS)
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz ${tzdata}
+        WORKING_DIRECTORY ${tzdata_dir}
+        RESULT_VARIABLE rv)
+file(REMOVE ${tzdata})
