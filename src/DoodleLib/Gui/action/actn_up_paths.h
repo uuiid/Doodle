33f2e78e71a4a2416e6036bd8c2d45229d96ef54
uuiid <957714080@qq.com>
2021-08-13 18:01:18 +0800

    上传文件重复的条目算一条

--- src/DoodleLib/Gui/action/actn_up_paths.h
+++ src/DoodleLib/Gui/action/actn_up_paths.h
@@ -17,6 +17,12 @@ class actn_up_paths : public action_indirect<action_arg::arg_paths> {
   actn_up_paths();
   using arg_ = action_arg::arg_paths;
   bool is_async() override;
+  /**
+   * @brief 上传文件， 注意，在服务器中如果重复的文件是算一条
+   * @param in_data
+   * @param in_parent
+   * @return
+   */
   long_term_ptr run(const MetadataPtr& in_data, const MetadataPtr& in_parent) override;
 };
 
