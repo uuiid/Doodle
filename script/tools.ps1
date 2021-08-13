775563546edac65707d4355ac20d28bfb923c1ef
uuiid <957714080@qq.com>
2021-08-13 16:51:36 +0800

    添加工具脚本

--- script/tools.ps1
+++ script/tools.ps1
@@ -1,1 +1,3 @@
-Enter-PSSession -ComputerName 192.168.20.60 -Credential nickli
+Enter-PSSession -computer  192.168.20.60 -Credential dev
+
+New-Item -Path "D:\Doodle\data" -Name "dubuxiaoyao" -ItemType "SymbolicLink" -Value "\\192.168.10.250\public\DuBuXiaoYao_3"
