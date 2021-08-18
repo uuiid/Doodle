05263fb9edea2832f3acb26ca8f244d6fb4200c1
uuiid <957714080@qq.com>
2021-08-18 09:00:26 +0800

    线程池的返回值不可忽略， 否则无法捕获异常

--- src/DoodleLib/threadPool/ThreadPool.h
+++ src/DoodleLib/threadPool/ThreadPool.h
@@ -59,7 +59,7 @@ inline ThreadPool::ThreadPool(size_t threads)
 
 // add new work item to the pool
 template <class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args)
+[[nodiscard]] auto ThreadPool::enqueue(F&& f, Args&&... args)
     -> std::future<typename std::invoke_result<F, Args...>::type> {
   using return_type = typename std::invoke_result<F, Args...>::type;
 
