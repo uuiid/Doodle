4733b4f37428475cb888937466fd1824740da4d7
uuiid <957714080@qq.com>
2021-08-26 11:05:49 +0800

    添加更新日志

---
+++ doc/updata.h
@@ -0,0 +1,14 @@
+/**
+ * @page updata_log 更新日志
+ * @tableofcontents
+ * @section updata_log_3 版本3
+ * 
+ * @subsection updata_log_309 版本3.0.9
+ * 
+ * @li 更新maya自动解算组件： 导出拍屏位置为mov文件夹
+ * @li 更新maya自动解算组件： 拍屏优先使用quicktime h264解码
+ * @li 更新maya自动解算组件： 保存文件位置为文件名称所在文件夹
+ * @li 更新maya自动解算组件： 拍屏开始时间确定为1001开始帧 
+ * @li 更新ue4 批量导入功能： 导入时启用多进程导入
+ * 
+ */
