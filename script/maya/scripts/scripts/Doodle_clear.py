f89ee04885101eda636bf542834a07bb8e6f0a7a
uuiid <957714080@qq.com>
2021-08-26 11:07:41 +0800

    添加maya场景清理工具，删除maya文件健康处理脚本

--- script/maya/scripts/scripts/Doodle_clear.py
+++ script/maya/scripts/scripts/Doodle_clear.py
@@ -1,4 +1,5 @@
 import maya.cmds
+import pymel.core
 
 
 class clearAndUpload(object):
@@ -6,17 +7,30 @@ class clearAndUpload(object):
         self.not_nu_plug = ["mayaHlK"]
 
     def clearScane(self):
-        un_know_plugs = maya.cmds.unknownPlugin(query=True, list=True)
+        un_know_plugs = pymel.core.unknownPlugin(query=True, list=True)
         if un_know_plugs:
             for un_plug in un_know_plugs:
                 if un_plug in self.not_nu_plug:
                     continue
-                maya.cmds.unknownPlugin(un_plug, remove=True)
+                print("Remove unknown plugin {}".format(un_plug))
+                pymel.core.unknownPlugin(un_plug, remove=True)
 
-        plugs = maya.cmds.pluginInfo(query=True, listPlugins=True)
+        plugs = pymel.core.pluginInfo(query=True, listPlugins=True)
         if plugs:
             for plug in plugs:
                 if plug in self.not_nu_plug:
                     continue
-                maya.cmds.pluginInfo(plug, edit=True, writeRequires=False)
+                print("Cancel{} Automatic loading of plug-in writing".format(plug))
+                pymel.core.pluginInfo(plug, edit=True, writeRequires=False)
         print("=" * 30 + "clear ok" + "=" * 30)
+
+        self.calar_vaccine_gene()
+
+    def calar_vaccine_gene(self):
+        for s in pymel.core.ls(type="script"):
+            if s.name() == "vaccine_gene" or s.name() == "breed_gene":
+                pymel.core.delete(s)
+                path = pymel.core.Path(
+                    pymel.core.internalVar(userAppDir=True) + "scripts")
+                for f in path.files():
+                    f.remove_p()
