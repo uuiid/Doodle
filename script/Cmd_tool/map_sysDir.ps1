d05c23a92bf4ec3f96a4083628d848301ea68aaf
uuiid <957714080@qq.com>
2021-09-02 13:14:18 +0800

    更改盘符映射方式和创建文件夹标题

--- script/Cmd_tool/map_sysDir.ps1
+++ script/Cmd_tool/map_sysDir.ps1
@@ -13,9 +13,9 @@ $map_item = @(
     @("C:\sy\JianJi_8", "\\192.168.10.250\public\11-剪辑", "8幢_剪辑"),
     @("C:\sy\HouQi_8", "\\192.168.10.250\public\HouQi", "8幢_后期"),
     @("C:\sy\MeiYiGiao_8", "\\192.168.10.250\public\美易高", "美易高"),
-    @("C:\sy\LianQiShiWanNian_8", "\\192.168.10.250\public\LianQiShiWanNian", "念气十万年"),
-    @("C:\sy\WuJinShenYu_8", "\\192.168.10.250\public\WuJinShenYu", "无尽神域"),
-    @("C:\sy\WuDiJianHun_8", "\\192.168.10.250\public\WuDiJianHun", "无敌剑魂"),
+    @("C:\sy\LianQiShiWanNian_8", "\\192.168.10.250\public\LianQiShiWanNian", "8幢_念气十万年"),
+    @("C:\sy\WuJinShenYu_8", "\\192.168.10.250\public\WuJinShenYu", "8幢_无尽神域"),
+    @("C:\sy\WuDiJianHun_8", "\\192.168.10.250\public\WuDiJianHun", "8幢_无敌剑魂"),
 
     @("C:\sy\WanYuFengShen_9", "\\192.168.10.218\WanYuFengShen", "9幢_万域封神"),
     @("C:\sy\KuangShenMoZun_9", "\\192.168.10.218\KuangShenMoZun", "9幢_狂神魔尊"),
@@ -47,6 +47,37 @@ $map_item = @(
 # $folder | Get-member
 # $folder.GetFileSystemInfos()
 # $folder.SubFolders
+if ( Test-Path -Path "C:\sy") {
+}
+else {
+    New-Item "C:\sy" -ItemType Directory
+}
+
+$main_ini = @"
+[.ShellClassInfo]
+InfoTip=@Shell32.dll,-12688
+IconFile=%SystemRoot%\system32\mydocs.dll
+IconIndex=-101
+IconResource=C:\WINDOWS\System32\SHELL32.dll,43
+
+[{F29F85E0-4FF9-1068-AB91-08002B27B3D9}]
+    Prop2 = 31,索以文化
+    Prop3 = 31,secret
+    Prop4 = 31,John Doe
+    Prop5 = 31,how it works
+    Prop6 = 31,this is comment
+
+[ViewState]
+Mode = 
+Vid = {137E7700-3573-11CF-AE69-08002B2E1262}
+FolderType=
+Logo =
+
+"@
+Set-Content -Path "C:\sy\desktop.ini" -Value $main_ini -Encoding "unicode" -Force
+$file = Get-Item -Path "C:\sy\desktop.ini" -Force
+$file.Attributes = 'Archive, System, Hidden'
+
 
 foreach ($item in $map_item) {
     try {
@@ -57,7 +88,6 @@ foreach ($item in $map_item) {
                 $fod.Delete()
             }
             New-Item -ItemType SymbolicLink -Path $item[0] -Target $item[1]
-
         }
     }
     catch [System.Management.Automation.ActionPreferenceStopException] {
@@ -65,6 +95,39 @@ foreach ($item in $map_item) {
     }
     catch {
         Write-Host "catch all 目录 " $item[1] " 没有访问权限， 取消映射"
+    }
+}
+
+exit
+
+Write-Host "开始创建 目录 " $item[1] " 的别名"
+foreach ($item in $map_item) {
+    $ini = $item[1] + "\desktop.ini"
+    $name = $item[2]
+    $con = @"
+[.ShellClassInfo]
+InfoTip=@Shell32.dll,-12688
+IconFile=%SystemRoot%\system32\mydocs.dll
+IconIndex=-101
+IconResource=C:\WINDOWS\System32\SHELL32.dll,43
+
+[{F29F85E0-4FF9-1068-AB91-08002B27B3D9}]
+    Prop2 = 31,$name
+    Prop3 = 31,secret
+    Prop4 = 31,John Doe
+    Prop5 = 31,how it works
+    Prop6 = 31,this is comment
+
+"@
+
+    try {
+        Test-Path -Path $ini -ErrorAction Stop
+        Set-Content -Path $ini -Value $con -Encoding "unicode" -Force
+        $file = Get-Item -Path $ini -Force
+        $file.Attributes = 'Archive, System, Hidden'
+    }
+    catch [System.Management.Automation.ActionPreferenceStopException] {
+        Write-Host "目录" $item[1] "没有访问权限， 不写入"
     }
 }
 # ps2exe c:\Users\TD\Source\Doodle\script\Cmd_tool\map_sysDir.ps1 c:\Users\TD\Source\Doodle\script\Cmd_tool\run4.exe -requireAdmin
