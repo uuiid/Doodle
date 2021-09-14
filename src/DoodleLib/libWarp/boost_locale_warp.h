416cb1ef1246e9366a0fae2e36bbf02d21285a59
uuiid <957714080@qq.com>
2021-09-14 17:49:08 +0800

    添加本地转换的包装

--- src/DoodleLib/libWarp/boost_locale_warp.h
+++ src/DoodleLib/libWarp/boost_locale_warp.h
@@ -5,9 +5,36 @@
 #pragma once
 
 #include <boost/locale.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/locale/util.hpp>
 namespace doodle {
 namespace conv {
 using namespace boost::locale::conv;
+
+template <class CharOut,class CharIn>
+std::basic_string<CharOut>
+locale_to_utf(std::basic_string<CharIn> const &str,method_type how = default_method)
+{
+  static auto gen = ::boost::locale::generator();
+  auto locale_str = ::boost::locale::util::get_system_locale();
+  std::vector<std::string> strs{};
+  ::boost::split(strs,locale_str,boost::is_any_of("."));
+  return to_utf<CharOut>(str,gen(strs.back()),how);
+}
+
+template <class CharOut,class CharIn>
+std::basic_string<CharOut>
+utf_to_locale(std::basic_string<CharIn> const &str,method_type how = default_method)
+{
+  static auto gen = ::boost::locale::generator();
+  auto locale_str = ::boost::locale::util::get_system_locale();
+  std::vector<std::string> strs{};
+  ::boost::split(strs,locale_str,boost::is_any_of("."));
+  return from_utf<CharOut>(str,gen(strs.back()),how);
+}
+
+
+
 }
 
 }  // namespace doodle
