9794926e9b42a2af791cf19827a24a12aa83ee87
uuiid <957714080@qq.com>
2021-09-15 19:28:08 +0800

    添加断点和视图样式窗口

--- src/DoodleLib/Gui/main_windwos.cpp
+++ src/DoodleLib/Gui/main_windwos.cpp
@@ -10,6 +10,9 @@
 namespace doodle {
 main_windows::main_windows()
     : p_setting_show(std::make_shared<bool>(false)),
+      p_debug_show(std::make_shared<bool>(false)),
+      p_about_show(std::make_shared<bool>(false)),
+      p_style_show(std::make_shared<bool>(false)),
       p_quit(std::make_shared<bool>(false)),
       p_title(fmt::format(
           u8"doodle {}.{}.{}.{}",
@@ -22,6 +25,13 @@ main_windows::main_windows()
 void main_windows::frame_render(const bool_ptr& is_show) {
   if (*p_setting_show)
     p_setting->frame_render(p_setting_show);
+  if (*p_debug_show) imgui::ShowMetricsWindow();
+  if (*p_about_show) imgui::ShowAboutWindow();
+  if (*p_style_show) {
+    dear::Begin{"界面样式编辑", p_style_show.get()} && []() {
+      imgui::ShowStyleEditor();
+    };
+  }
 
   dear::Begin{
       p_title.c_str(),
@@ -29,8 +39,8 @@ void main_windows::frame_render(const bool_ptr& is_show) {
       ImGuiWindowFlags_MenuBar} &&
       [this]() {
         dear::MenuBar{} && [this]() {
-          dear::Menu{"file"} && [this]() { this->main_menu_file(); };
-          dear::Menu{"tool"} && [this] { main_menu_tool(); };
+          dear::Menu{"文件"} && [this]() { this->main_menu_file(); };
+          dear::Menu{"工具"} && [this] { main_menu_tool(); };
         };
 
         ImGui::Spacing();
@@ -42,6 +52,11 @@ void main_windows::frame_render(const bool_ptr& is_show) {
 }
 void main_windows::main_menu_file() {
   dear::MenuItem(u8"设置", p_setting_show.get());
+  ImGui::Separator();
+  dear::MenuItem(u8"调试", p_debug_show.get());
+  dear::MenuItem(u8"样式设置", p_style_show.get());
+  dear::MenuItem(u8"关于", p_about_show.get());
+  ImGui::Separator();
   if (dear::MenuItem(u8"退出")) {
     doodle_app::Get()->p_done = true;
   }
--- src/DoodleLib/Gui/main_windwos.h
+++ src/DoodleLib/Gui/main_windwos.h
@@ -9,6 +9,9 @@ namespace doodle {
 
 class DOODLELIB_API main_windows : public base_windows {
   bool_ptr p_setting_show;
+  bool_ptr p_debug_show;
+  bool_ptr p_about_show;
+  bool_ptr p_style_show;
   bool_ptr p_quit;
   std::string p_title;
   setting_windows_ptr p_setting;
