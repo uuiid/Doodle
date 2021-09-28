5e75b21679fe88f03a6edca6bec98af53da0e143
uuiid <957714080@qq.com>
2021-09-28 17:11:22 +0800

    更加科学的选择方式

--- src/DoodleLib/Gui/widgets/assets_widget.cpp
+++ src/DoodleLib/Gui/widgets/assets_widget.cpp
@@ -15,7 +15,9 @@ namespace doodle {
 
 assets_widget::assets_widget()
     : p_root(),
-      p_meta() {
+      p_meta(),
+      p_all_old_selected(),
+      p_all_selected() {
   p_factory    = new_object<attr_assets>();
   p_class_name = "资产";
 }
@@ -25,6 +27,7 @@ void assets_widget::frame_render() {
       load_meta(p_root);
     }
   };
+  p_all_old_selected = p_all_selected;
 }
 
 void assets_widget::set_metadata(const metadata_ptr& in_ptr) {
@@ -39,38 +42,75 @@ void assets_widget::load_meta(const metadata_ptr& in_ptr) {
 
     for (const auto& i : in_ptr->child_item) {
       auto flsge = base_flags;
-      if (p_meta == i)
+      if (is_select(i))
         flsge |= ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_Selected;
 
       if (i->has_child() || i->has_file()) {
+        bool checked = false;  //使用这个变量标记为只检查一次
         dear::TreeNodeEx{
             i->get_uuid().c_str(),
             flsge,
             i->show_str().c_str()} &&
-            [i, this]() {
-              // imgui::SameLine();
-              if (imgui::IsItemClicked())
-                set_select(i);
+            [i, this, &checked]() {
+              if (!checked) {
+                check_item_clicked(i);
+                checked = true;
+              }
+              //添加文件标志
               if (i->has_file())
                 imgui::BulletText("files");
               load_meta(i);
             };
+        if (!checked)
+          check_item_clicked(i);
+        check_item(i);
       } else {
         dear::TreeNodeEx{
             i->get_uuid().c_str(),
             flsge | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen,
             i->show_str().c_str()} &&
             [this, i]() {
-              if (imgui::IsItemClicked()) {
-                set_select(i);
-              }
             };
+        check_item_clicked(i);
+        check_item(i);
       }
     }
   }
 }
+
+bool assets_widget::is_select(const metadata_ptr& in_ptr) {
+  return std::any_of(p_all_old_selected.begin(), p_all_old_selected.end(), [&](const metadata_ptr& in_) {
+    return in_ == in_ptr;
+  });
+}
+
+void assets_widget::check_item(const metadata_ptr& in_ptr) {
+  // if (imgui::IsItemHovered()) {
+  //   DOODLE_LOG_DEBUG("ok");
+  // }
+  if (imgui::GetIO().KeyShift && imgui::IsItemHovered()) {
+    p_all_selected.insert(in_ptr);
+  }
+  // if (imgui::GetIO().KeyCtrl && imgui::IsItemHovered()) {
+  //   p_all_selected.erase(in_ptr);
+  // }
+}
+
+void assets_widget::check_item_clicked(const metadata_ptr& in_ptr) {
+  if (imgui::IsItemClicked()) {
+    if (!imgui::GetIO().KeyCtrl) {
+      p_all_selected.clear();
+      p_all_old_selected.clear();
+    } else {
+      p_all_selected.erase(in_ptr);
+      return;
+    }
+    set_select(in_ptr);
+  }
+}
 void assets_widget::set_select(const metadata_ptr& in_ptr) {
   p_meta = in_ptr;
+  p_all_selected.insert(p_meta);
   p_meta->attribute_widget(p_factory);
   select_change(p_meta);
 }
--- src/DoodleLib/Gui/widgets/assets_widget.h
+++ src/DoodleLib/Gui/widgets/assets_widget.h
@@ -11,16 +11,71 @@ namespace doodle {
 
 class DOODLELIB_API assets_widget : public metadata_widget {
   metadata_ptr p_root;
+  /**
+   * @brief  我们在这里使用上一帧的选中物体开始测试是否选中而不是使用这一帧的
+   * 
+   */
+  std::set<metadata_ptr> p_all_old_selected;
+  /**
+   * @brief 递归的加载树节点
+   * 
+   * @param in_ptr 传入父物体
+   */
   void load_meta(const metadata_ptr& in_ptr);
+  /**
+   * @brief 判断是否是选中对象,
+   * 
+   * @param in_ptr 是否是选中的
+   * @return true 选中了
+   * @return false 没有选中
+   */
+  bool is_select(const metadata_ptr& in_ptr);
+
+  /**
+   * @brief 多重选择时使用, 多重选择是不发出任何信号的
+   * 
+   * @param in_ptr 传入判断小部件成功时需要的数据，
+   *  主要时加入 p_all_selected 属性和 p_meta属性
+   */
+  void check_item(const metadata_ptr& in_ptr);
+  /**
+   * @brief 在去除选择和选择时使用, 去除选择时, 不发出选择更改信号
+   * 
+   * @param in_ptr 检查点击, 这个要和检查项目分开判断
+   */
+  void check_item_clicked(const metadata_ptr& in_ptr);
 
  public:
   assets_widget();
   void frame_render() override;
-
+  /**
+   * @brief 设置这个小部件要创建的树的根属性
+   * 
+   * @param in_ptr 
+   */
   void set_metadata(const metadata_ptr& in_ptr);
 
+  /**
+   * @brief 设置选择, 这里是设置单选的方式
+   * 
+   * @param in_ptr 
+   */
   void set_select(const metadata_ptr& in_ptr);
+
+  /**
+   * @brief 多选的物体
+   * 
+   */
+  std::set<metadata_ptr> p_all_selected;
+  /**
+   * @brief 单选的物体
+   * 
+   */
   metadata_ptr p_meta;
+  /**
+   * @brief 单选时切换的信号
+   * 
+   */
   boost::signals2::signal<void(const metadata_ptr&)> select_change;
 };
 }  // namespace doodle
