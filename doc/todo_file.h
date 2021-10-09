1246df378a33ea46a54daa6b42d2861285fd57fc
uuiid <957714080@qq.com>
2021-10-09 13:44:33 +0800

    文档更新

--- doc/todo_file.h
+++ doc/todo_file.h
@@ -11,9 +11,10 @@
  * @todo 添加更好的maya fbx摄像机过滤和击中功能
  * @todo 更改maya 解算标记按钮 添加gui(maya插件要重写一下)
  * @todo 添加解算时使用的属性伸缩标记功能
- * 
+ * @todo 解算是同时导出fbx文件
  * 
  * @todo 添加拍屏审核功能
+ * @todo 将拍屏放入maya和ue插件中
  * 
  * 
  * @todo 批量文件扫描添加
