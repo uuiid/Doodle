efaddf682cc244dc9eb9bacc73e69afd2ab95272
uuiid <957714080@qq.com>
2021-10-20 14:56:30 +0800

    修改保存时的异常抛出方式

--- src/doodle_lib/core/core_set.cpp
+++ src/doodle_lib/core/core_set.cpp
@@ -273,16 +273,6 @@ bool core_set_init::read_file() {
 }
 bool core_set_init::write_file() {
   DOODLE_LOG_INFO("写入配置文件");
-
-  p_set.p_ue4_setting.test_value();
-  if (p_set.p_ue4_setting.has_path() && !FSys::exists(p_set.p_ue4_setting.get_path() / staticValue::ue_path_obj())) {
-    p_set.p_ue4_setting.set_path({});
-    throw file_error{p_set.p_ue4_setting.get_path(), " 在路径中没有找到ue,不保存"};
-  }
-  if (!FSys::exists(p_set.p_mayaPath / "maya.exe")) {
-    throw file_error{p_set.p_mayaPath, " 在路径中没有找到maya,不保存"};
-  }
-
   FSys::ofstream l_ofstream{p_set.p_doc / p_set.config_file_name(), std::ios::out | std::ios::binary};
   boost::archive::text_oarchive l_out{l_ofstream};
   l_out << p_set;
