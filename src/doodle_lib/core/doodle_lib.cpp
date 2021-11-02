c3a12c677716c2948af1be5577ac45301565f627
uuiid <957714080@qq.com>
2021-11-02 09:35:28 +0800

    修复gui初始化中错误的线程池线程数

--- src/doodle_lib/core/doodle_lib.cpp
+++ src/doodle_lib/core/doodle_lib.cpp
@@ -104,6 +104,7 @@ doodle_lib::~doodle_lib() {
   p_project_vector.clear();
 }
 void doodle_lib::init_gui() {
+  p_thread_pool = new_object<thread_pool>(core_set::getSet().p_max_thread);
   auto k_ip = fmt::format("{}:{:d}", core_set::getSet().get_server_host(), core_set::getSet().get_meta_rpc_port());
 
   DOODLE_LOG_DEBUG(k_ip);
