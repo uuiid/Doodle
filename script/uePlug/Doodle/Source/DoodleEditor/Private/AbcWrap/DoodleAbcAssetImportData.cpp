9a4e1c51c591d5e8607aa9b39bcef5e7d8cc2e64
uuiid <957714080@qq.com>
2021-11-12 10:12:08 +0800

    去除abc包裹功能

--- script/uePlug/Doodle/Source/DoodleEditor/Private/AbcWrap/DoodleAbcAssetImportData.cpp
+++
@@ -1,6 +0,0 @@
-#include "AbcWrap/DoodleAbcAssetImportData.h"
-
-UDoodleAbcAssetImportData::UDoodleAbcAssetImportData(const FObjectInitializer &ObjectInitializer)
-    : Super(ObjectInitializer)
-{
-}
