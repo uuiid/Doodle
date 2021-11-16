4d1340b2002a1efc9d87330946412b14c6a03043
uuiid <957714080@qq.com>
2021-11-16 18:36:23 +0800

    添加maya导出视频命令

---
+++ src/doodle_exe/maya_plug/gui/action/comm_play_blast.cpp
@@ -0,0 +1,7 @@
+//
+// Created by TD on 2021/11/16.
+//
+
+#include "comm_play_blast.h"
+namespace doodle::maya_plug {
+} 
---
+++ src/doodle_exe/maya_plug/gui/action/comm_play_blast.h
@@ -0,0 +1,11 @@
+//
+// Created by TD on 2021/11/16.
+//
+
+
+#pragma once
+
+
+#include <doodle_lib/doodle_lib_fwd.h>
+namespace doodle::maya_plug{
+}
