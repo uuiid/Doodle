2815f4a8f8b59b1547b447db008d1a57949fa986
uuiid <957714080@qq.com>
2021-11-23 09:41:01 +0800

    更改maya 插件注册方式

--- src/doodle_exe/maya_plug/MayaPlug.cpp
+++ src/doodle_exe/maya_plug/MayaPlug.cpp
@@ -69,6 +69,8 @@ MStatus initializePlugin(MObject obj) {
   CHECK_MSTATUS_AND_RETURN_IT(status);
 
   switch (k_st) {
+    case MGlobal::MMayaState::kBaseUIMode: {
+    }
     case MGlobal::MMayaState::kInteractive: {
       p_doodle_lib = doodle::new_object<doodle::doodle_lib>();
       doodle::logger_ctrl::get_log().set_log_name("doodle_maya_plug.txt");
@@ -116,48 +118,44 @@ MStatus initializePlugin(MObject obj) {
           nullptr, &status);
 
       CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      /// 注册自定义节点
-      status = k_plugin.registerNode(
-          doolde_hud_render_node.c_str(),
-          doodle::maya_plug::doodle_info_node::doodle_id,
-          &doodle::maya_plug::doodle_info_node::creator,
-          &doodle::maya_plug::doodle_info_node::initialize,
-          MPxNode::kLocatorNode,
-          &doodle::maya_plug::doodle_info_node::drawDbClassification);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-      status = MHWRender::MDrawRegistry::registerDrawOverrideCreator(
-          doodle::maya_plug::doodle_info_node::drawDbClassification,
-          doodle::maya_plug::doodle_info_node::drawRegistrantId,
-          doodle::maya_plug::doodle_info_node_draw_override::Creator);
-
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      status = k_plugin.registerCommand(
-          ::doodle::maya_plug::comm_play_blast_maya::comm_name,
-          &::doodle::maya_plug::comm_play_blast_maya::creator);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      status = k_plugin.registerCommand(
-          ::doodle::maya_plug::create_hud_node_maya::comm_name,
-          &::doodle::maya_plug::create_hud_node_maya::creator);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      // MHWRender::MRenderer* k_r = MHWRender::MRenderer::theRenderer();
-      // if (k_r) {
-      //   auto k_o_r = new hud_render_override(doolde_hud_render_override.c_str());
-      //   if (k_o_r) {
-      //     status = k_r->registerOverride(k_o_r);
-      //     CHECK_MSTATUS_AND_RETURN_IT(status);
-      //   }
-      // }
-
       break;
     }
 
+    case MGlobal::MMayaState::kBatch: {
+      break;
+    }
+    case MGlobal::MMayaState::kLibraryApp: {
+      break;
+    }
     default:
-      return status;
+      break;
   }
+  /// 注册自定义hud节点
+  status = k_plugin.registerNode(
+      doolde_hud_render_node.c_str(),
+      doodle::maya_plug::doodle_info_node::doodle_id,
+      &doodle::maya_plug::doodle_info_node::creator,
+      &doodle::maya_plug::doodle_info_node::initialize,
+      MPxNode::kLocatorNode,
+      &doodle::maya_plug::doodle_info_node::drawDbClassification);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+  /// 注册自定义渲染覆盖显示hud
+  status = MHWRender::MDrawRegistry::registerDrawOverrideCreator(
+      doodle::maya_plug::doodle_info_node::drawDbClassification,
+      doodle::maya_plug::doodle_info_node::drawRegistrantId,
+      doodle::maya_plug::doodle_info_node_draw_override::Creator);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+
+  /// 注册拍屏命令
+  status = k_plugin.registerCommand(
+      ::doodle::maya_plug::comm_play_blast_maya::comm_name,
+      &::doodle::maya_plug::comm_play_blast_maya::creator);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+  /// 注册创建hud命令
+  status = k_plugin.registerCommand(
+      ::doodle::maya_plug::create_hud_node_maya::comm_name,
+      &::doodle::maya_plug::create_hud_node_maya::creator);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
   return status;
 }
 
@@ -170,6 +168,8 @@ MStatus uninitializePlugin(MObject obj) {
   CHECK_MSTATUS_AND_RETURN_IT(status);
 
   switch (k_st) {
+    case MGlobal::MMayaState::kBaseUIMode: {
+    }
     case MGlobal::MMayaState::kInteractive: {
       //这里要关闭窗口
       doodle_maya_clear();
@@ -186,51 +186,35 @@ MStatus uninitializePlugin(MObject obj) {
       menuItems.append(doodle_windows.c_str());
       status = k_plugin.removeMenuItem(menuItems);
       CHECK_MSTATUS_AND_RETURN_IT(status);
+      break;
+    }
 
-      //   MString pythonResult{};
-      //   MGlobal::executePythonCommand(R"(import maya.cmds
-      // def doodleDeleteMenu():
-      //     for me in maya.cmds.lsUI(menus=True,long=True):
-      //     	  if maya.cmds.menu(me,q=True,label=True) == "doodle":
-      //     		    maya.cmds.menu(me,e=True,deleteAllItems=True)
-      //     		    maya.cmds.deleteUI(me)
-      // )");
-
-      //   MGlobal::executePythonCommand("doodleDeleteMenu()");
-      //   CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      // 卸载命令
-      status = k_plugin.deregisterCommand(doodle_create.c_str());
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-      status = MDrawRegistry::deregisterGeometryOverrideCreator(
-          doodle::maya_plug::doodle_info_node::drawDbClassification,
-          doodle::maya_plug::doodle_info_node::drawRegistrantId);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-      status = k_plugin.deregisterNode(doodle::maya_plug::doodle_info_node::doodle_id);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      status = k_plugin.deregisterCommand(
-          ::doodle::maya_plug::comm_play_blast_maya::comm_name);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      status = k_plugin.deregisterCommand(
-          ::doodle::maya_plug::create_hud_node_maya::comm_name);
-      CHECK_MSTATUS_AND_RETURN_IT(status);
-
-      // MHWRender::MRenderer* k_r = MHWRender::MRenderer::theRenderer();
-      // if (k_r) {
-      //   auto k_o_r = k_r->findRenderOverride(doolde_hud_render_override.c_str());
-      //   if (k_o_r) {
-      //     status = k_r->deregisterOverride(k_o_r);
-      //     CHECK_MSTATUS_AND_RETURN_IT(status);
-      //     delete k_o_r;
-      //   }
-      // }
+    case MGlobal::MMayaState::kBatch: {
+      break;
+    }
+    case MGlobal::MMayaState::kLibraryApp: {
       break;
     }
     default:
-      return status;
+      break;
   }
+  // 卸载命令
+  status = k_plugin.deregisterCommand(doodle_create.c_str());
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+  status = MDrawRegistry::deregisterGeometryOverrideCreator(
+      doodle::maya_plug::doodle_info_node::drawDbClassification,
+      doodle::maya_plug::doodle_info_node::drawRegistrantId);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+  status = k_plugin.deregisterNode(doodle::maya_plug::doodle_info_node::doodle_id);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+
+  status = k_plugin.deregisterCommand(
+      ::doodle::maya_plug::comm_play_blast_maya::comm_name);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+
+  status = k_plugin.deregisterCommand(
+      ::doodle::maya_plug::create_hud_node_maya::comm_name);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
 
   return status;
 }
