12e9a44e7ccf189270d80620045d67079bde507f
uuiid <957714080@qq.com>
2021-11-24 15:11:57 +0800

    更改盘符映射方案

--- script/Cmd_tool/map_sysDir_server.ps1
+++ script/Cmd_tool/map_sysDir_server.ps1
@@ -2,20 +2,20 @@
 
 
 $map_item = @(
-    @("ChangAnHuanJie", "\\192.168.10.250\public\changanhuanjie", "8幢_长安幻街"),
-    @("DuBuXiaoYao", "\\192.168.10.250\public\DuBuXiaoYao_3", "8幢_独步逍遥v3"),
-    @("ChengXv", "\\192.168.10.250\public\Prism_projects", "8幢_程序开发"),
-    @("donghuagongxiang", "\\192.168.10.250\public\动画共享", "8幢_动画共享"),
-    @("CangFeng", "\\192.168.10.250\public\CangFeng", "8幢_藏锋"),
-    @("WanGuShenHua", "\\192.168.10.250\public\WanGuShenHua", "8幢_万古神话"),
-    @("RenJianZuiDeYi", "\\192.168.10.250\public\renjianzuideyi", "8幢_人间最得意"),
-    @("WuDiJianHun", "\\192.168.10.250\public\WuDiJianHun", "8幢_无敌剑魂"),
-    @("MeiYiGiao", "\\192.168.10.250\public\美易高", "美易高"),
-    @("LianQiShiWanNian", "\\192.168.10.250\public\LianQiShiWanNian", "8幢_炼气十万年"),
-    @("WuJinShenYu", "\\192.168.10.250\public\WuJinShenYu", "8幢_无尽神域"),
+    @("ChangAnHuanJie", "\\192.168.10.250\public\changanhuanjie\03_Workflow", "8幢_长安幻街"),
+    @("DuBuXiaoYao", "\\192.168.10.250\public\DuBuXiaoYao_3\03_Workflow", "8幢_独步逍遥v3"),
+    @("ChengXv", "\\192.168.10.250\public\Prism_projects\03_Workflow", "8幢_程序开发"),
+    @("donghuagongxiang", "\\192.168.10.250\public\动画共享\03_Workflow", "8幢_动画共享"),
+    @("CangFeng", "\\192.168.10.250\public\CangFeng\03_Workflow", "8幢_藏锋"),
+    @("WanGuShenHua", "\\192.168.10.250\public\WanGuShenHua\03_Workflow", "8幢_万古神话"),
+    @("RenJianZuiDeYi", "\\192.168.10.250\public\renjianzuideyi\03_Workflow", "8幢_人间最得意"),
+    @("WuDiJianHun", "\\192.168.10.250\public\WuDiJianHun\03_Workflow", "8幢_无敌剑魂"),
+    @("MeiYiGiao", "\\192.168.10.250\public\美易高\03_Workflow", "美易高"),
+    @("LianQiShiWanNian", "\\192.168.10.250\public\LianQiShiWanNian\03_Workflow", "8幢_炼气十万年"),
+    @("WuJinShenYu", "\\192.168.10.250\public\WuJinShenYu\03_Workflow", "8幢_无尽神域"),
 
-    @("WanYuFengShen", "\\192.168.10.218\WanYuFengShen", "9幢_万域封神"),
-    @("KuangShenMoZun", "\\192.168.10.218\KuangShenMoZun", "9幢_狂神魔尊")
+    @("WanYuFengShen", "\\192.168.10.218\WanYuFengShen\03_Workflow", "9幢_万域封神"),
+    @("KuangShenMoZun", "\\192.168.10.218\KuangShenMoZun\03_Workflow", "9幢_狂神魔尊")
 )
 # Get-ItemProperty "D:\Autodesk\test\Gumu.ma"
 
@@ -37,8 +37,14 @@ $map_item = @(
 
 foreach ($item in $map_item) {
     try {
-        $k_p = $PWD.Path + "\" + $item[0] + " to " + $item[1]
+        $k_p = Get-Item -Path $item[0]
+        if ($k_p.Exists) {
+            $k_p.Delete()
+        }
         Write-Host $k_p
+        if (!(Test-Path -Path $item[1])) {
+            New-Item -ItemType Directory -Path  $item[1]
+        }
         New-Item -ItemType SymbolicLink -Path $item[0] -Target $item[1]
     }
     catch [System.Management.Automation.ActionPreferenceStopException] {
