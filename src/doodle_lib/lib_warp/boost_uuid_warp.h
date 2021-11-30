6951dcdbb6b85d61d1b2d339143f9717ffc18ebe
uuiid <957714080@qq.com>
2021-11-30 17:37:41 +0800

    更改uuid库包装

--- src/doodle_lib/lib_warp/boost_uuid_warp.h
+++ src/doodle_lib/lib_warp/boost_uuid_warp.h
@@ -3,5 +3,8 @@
 #define BOOST_UUID_RANDOM_PROVIDER_FORCE_WINCRYPT
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_hash.hpp>
+#include <boost/uuid/uuid_serialize.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #undef BOOST_UUID_RANDOM_PROVIDER_FORCE_WINCRYPT
+#include <boost/lexical_cast.hpp>
