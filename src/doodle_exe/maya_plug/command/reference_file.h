eb82e96cc25e8d7f69a5c9773353f6cfac2419e6
uuiid <957714080@qq.com>
2021-12-01 17:20:26 +0800

    更新maya文件模型

--- src/doodle_exe/maya_plug/command/reference_file.h
+++ src/doodle_exe/maya_plug/command/reference_file.h
@@ -3,21 +3,19 @@
 //
 #pragma once
 #include <doodle_lib/doodle_lib_fwd.h>
-
 namespace doodle::maya_plug {
 
 class reference_file {
-
  public:
   string path;
   bool use_sim;
   bool high_speed_sim;
+  std::vector<string> k_collision_ref_file;
+  std::vector<string> k_collision_model;
 
-
-
+  uuid p_prj_ref;
 
   reference_file();
-
 };
 
 }  // namespace doodle::maya_plug
