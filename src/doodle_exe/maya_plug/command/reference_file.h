d39fc39d2877047a98a9efd88f0ff2864b201f25
uuiid <957714080@qq.com>
2021-12-02 16:01:53 +0800

    完成引用文件属性建模

--- src/doodle_exe/maya_plug/command/reference_file.h
+++ src/doodle_exe/maya_plug/command/reference_file.h
@@ -10,12 +10,30 @@ class reference_file {
   string path;
   bool use_sim;
   bool high_speed_sim;
-  std::vector<string> k_collision_ref_file;
-  std::vector<string> k_collision_model;
+  std::vector<string> collision_ref_file;
+  std::vector<string> collision_model;
 
-  uuid p_prj_ref;
+  uuid prj_ref;
 
-  reference_file();
+  reference_file(){};
+
+ private:
+  friend void to_json(nlohmann::json &j, const reference_file &p) {
+    j["path"]               = p.path;
+    j["use_sim"]            = p.use_sim;
+    j["high_speed_sim"]     = p.high_speed_sim;
+    j["collision_ref_file"] = p.collision_ref_file;
+    j["collision_model"]    = p.collision_model;
+    j["prj_ref"]            = p.prj_ref;
+  }
+  friend void from_json(const nlohmann::json &j, reference_file &p) {
+    j.at("path").get_to(p.path);
+    j.at("use_sim").get_to(p.use_sim);
+    j.at("prj_ref").get_to(p.prj_ref);
+    j.at("high_speed_sim").get_to(p.high_speed_sim);
+    j.at("collision_ref_file").get_to(p.collision_ref_file);
+    j.at("collision_model").get_to(p.collision_model);
+  }
 };
 
 }  // namespace doodle::maya_plug
