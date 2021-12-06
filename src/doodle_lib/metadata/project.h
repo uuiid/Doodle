371c279a779ad703d09da3689d9fec213d9ce8b1
uuiid <957714080@qq.com>
2021-12-06 15:35:07 +0800

    添加解算新配置

--- src/doodle_lib/metadata/project.h
+++ src/doodle_lib/metadata/project.h
@@ -19,12 +19,30 @@ class DOODLELIB_API project {
     std::float_t time_scale;
     std::float_t length_scale;
 
+    /**
+     * @brief 解算形状(这个是解算器生成的布料节点,具有缓存属性等)
+     */
+    std::string cloth_shape;
+    /**
+     * @brief 定位布料所需节点名称,我们使用这个节点的outMesh属性拉动节点计算,
+     * 需要解算布料节点的下游节点
+     */
+    std::string cloth_proxy;
+    /**
+     * @brief 导出整个解算文件所需要的选择组
+     * 我们使用这个组导出整个解算结果
+     */
+    std::string export_group;
+
     friend void to_json(nlohmann::json& j, const cloth_config& p) {
       j["vfx_cloth_sim_path"] = p.vfx_cloth_sim_path;
       j["simple_subsampling"] = p.simple_subsampling;
       j["frame_samples"]      = p.frame_samples;
       j["time_scale"]         = p.time_scale;
       j["length_scale"]       = p.length_scale;
+      j["cloth_shape"]        = p.cloth_shape;
+      j["cloth_proxy"]        = p.cloth_proxy;
+      j["export_group"]       = p.export_group;
     }
     friend void from_json(const nlohmann::json& j, cloth_config& p) {
       j.at("vfx_cloth_sim_path").get_to(p.vfx_cloth_sim_path);
@@ -32,6 +50,9 @@ class DOODLELIB_API project {
       j.at("frame_samples").get_to(p.frame_samples);
       j.at("time_scale").get_to(p.time_scale);
       j.at("length_scale").get_to(p.length_scale);
+      j.at("cloth_shape").get_to(p.cloth_shape);
+      j.at("cloth_proxy").get_to(p.cloth_proxy);
+      j.at("export_group").get_to(p.export_group);
     }
   };
 
@@ -69,8 +90,6 @@ class DOODLELIB_API project {
   bool operator!=(const project& in_rhs) const;
 
  private:
-
-
   friend void to_json(nlohmann::json& j, const project& p) {
     j["name"] = p.p_name;
     j["path"] = p.p_path;
@@ -83,4 +102,3 @@ class DOODLELIB_API project {
 };
 
 }  // namespace doodle
-
