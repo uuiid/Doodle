12d0fb7d5011f3a21d4eef3ef6793db7d031e5d9
uuiid <957714080@qq.com>
2021-12-08 11:56:30 +0800

    添加maya测试

---
+++ src/doodle_exe/main/test/test_maya.py
@@ -0,0 +1,21 @@
+import maya.cmds
+import maya_fun_tool
+
+
+class doodle_main(object):
+    maya_version = str(maya.cmds.about(api=True))[0:4]
+    doodle_plug = "doodle_plug_{}".format(str(maya.cmds.about(api=True))[0:4])
+    qcloth = "qualoth_{}_x64".format(str(maya.cmds.about(api=True))[0:4])
+
+    def __init__(self):
+        super().__init__()
+        maya.cmds.loadPlugin(doodle_main.doodle_plug)
+        maya.cmds.loadPlugin(doodle_main.qcloth)
+
+    def open(self):
+        pass
+
+
+def doodle_test():
+    print("run test")
+    maya_fun_tool.open_file()
