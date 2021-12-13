b45449f9a837f88824fef9f1fd58cf0a8e20ee3d
uuiid <957714080@qq.com>
2021-12-13 10:55:07 +0800

    添加maya camera模型

---
+++ src/doodle_exe/maya_plug/data/maya_camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2021/12/13.
+//
+
+#include "maya_camera.h"
---
+++ src/doodle_exe/maya_plug/data/maya_camera.h
@@ -0,0 +1,9 @@
+//
+// Created by TD on 2021/12/13.
+//
+
+#include <doodle_lib/doodle_lib_fwd.h>
+
+class maya_camera {
+};
+
