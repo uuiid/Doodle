2911e1988dc396b4c6a20441f3f8c771dbea6277
uuiid <957714080@qq.com>
2021-12-14 16:39:11 +0800

    添加制作解算类

---
+++ src/doodle_exe/maya_plug/data/sim_cloth_tools.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2021/12/14.
+//
+
+#include "sim_cloth_tools.h"
---
+++ src/doodle_exe/maya_plug/data/sim_cloth_tools.h
@@ -0,0 +1,9 @@
+//
+// Created by TD on 2021/12/14.
+//
+
+#pragma once
+
+namespace doodle::maya_plug {
+class qcloth_;
+}
