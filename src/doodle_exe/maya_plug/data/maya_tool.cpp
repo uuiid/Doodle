4d81bd9aae07eb734ee24e9a78fd96478dabe821
uuiid <957714080@qq.com>
2021-12-20 19:05:56 +0800

    调整maya属性查找方案

--- src/doodle_exe/maya_plug/data/maya_tool.cpp
+++ src/doodle_exe/maya_plug/data/maya_tool.cpp
@@ -15,23 +15,35 @@ MPlug get_plug(const MObject& in_node, const std::string& in_name) {
   MStatus k_s{};
   MFnDependencyNode l_node{in_node, &k_s};
   MPlug l_plug{};
+
+  try {
+    l_plug = l_node.findPlug(d_str{in_name}, false, &k_s);
+    DOODLE_CHICK(k_s);
+  } catch (const maya_InvalidParameter& error) {
+    DOODLE_LOG_INFO("没有在这个节点中找到属性")
+  }
+
   if (in_node.hasFn(MFn::kDagNode)) {
     MFnDagNode l_dag_node{in_node, &k_s};
     DOODLE_CHICK(k_s);
     MDagPath l_path{};
     k_s = l_dag_node.getPath(l_path);
     DOODLE_CHICK(k_s);
-    k_s = l_path.extendToShape();
-    DOODLE_CHICK(k_s);
-    MFnDagNode l_dag_node_shape{l_path, &k_s};
-    l_plug = l_dag_node_shape.findPlug(d_str{in_name}, false, &k_s);
-    DOODLE_CHICK(k_s)
-  } else if (in_node.hasFn(MFn::kDependencyNode)) {
-    l_plug = l_node.findPlug(d_str{in_name}, false, &k_s);
-    DOODLE_CHICK(k_s);
-  } else {
-    throw doodle_error{"无法附加到功能集"};
+    try {
+      k_s = l_path.extendToShape();
+      DOODLE_CHICK(k_s);
+      MFnDagNode l_dag_node_shape{l_path, &k_s};
+      l_plug = l_dag_node_shape.findPlug(d_str{in_name}, false, &k_s);
+      DOODLE_CHICK(k_s)
+    } catch (const maya_InvalidParameter& error) {
+      DOODLE_LOG_INFO("节点下方没有 shape 形状节点, 不需要寻找形状节点")
+    }
   }
+  if(l_plug.isNull(&k_s)){
+    DOODLE_CHICK(k_s);
+    throw doodle_error{"无法找到属性"};
+  }
+
   return l_plug;
 }
 MObject get_shading_engine(const MObject& in_node) {
@@ -65,22 +77,22 @@ MObject get_shading_engine(const MObject& in_node) {
     //    DOODLE_LOG_INFO(fmt::format("找到节点 {}", d_str{k_node.name()}.str()));
     return obj;
   }
-//  for (MItDependencyGraph i{k_obj,
-//                            MFn::Type::kInvalid,
-//                            MItDependencyGraph::Direction::kUpstream,
-//                            MItDependencyGraph::Traversal::kDepthFirst,
-//                            MItDependencyGraph::Level::kNodeLevel,
-//                            &k_s};
-//       !i.isDone();
-//       i.next()) {
-//    DOODLE_CHICK(k_s);
-//    auto obj = i.currentItem(&k_s);
-//    DOODLE_CHICK(k_s);
-//    MFnDependencyNode k_node{};
-//    k_node.setObject(obj);
-//    DOODLE_LOG_INFO(fmt::format("找到节点 {}", d_str{k_node.name()}.str()));
-//    //    return obj;
-//  }
+  //  for (MItDependencyGraph i{k_obj,
+  //                            MFn::Type::kInvalid,
+  //                            MItDependencyGraph::Direction::kUpstream,
+  //                            MItDependencyGraph::Traversal::kDepthFirst,
+  //                            MItDependencyGraph::Level::kNodeLevel,
+  //                            &k_s};
+  //       !i.isDone();
+  //       i.next()) {
+  //    DOODLE_CHICK(k_s);
+  //    auto obj = i.currentItem(&k_s);
+  //    DOODLE_CHICK(k_s);
+  //    MFnDependencyNode k_node{};
+  //    k_node.setObject(obj);
+  //    DOODLE_LOG_INFO(fmt::format("找到节点 {}", d_str{k_node.name()}.str()));
+  //    //    return obj;
+  //  }
   throw doodle_error{"没有找到节点"};
 }
 MObject get_first_mesh(const MObject& in_node) {
