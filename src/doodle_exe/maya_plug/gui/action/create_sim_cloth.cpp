ec97471f5781027521762dd5c1a1f7e823e615ce
uuiid <957714080@qq.com>
2021-12-20 13:50:03 +0800

    添加解算制作工具

---
+++ src/doodle_exe/maya_plug/gui/action/create_sim_cloth.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2021/12/20.
+//
+
+#include "create_sim_cloth.h"
---
+++ src/doodle_exe/maya_plug/gui/action/create_sim_cloth.h
@@ -0,0 +1,9 @@
+//
+// Created by TD on 2021/12/20.
+//
+
+#pragma once
+
+namespace doodle::maya_plug{
+
+}
