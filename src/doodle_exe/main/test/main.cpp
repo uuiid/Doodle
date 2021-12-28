f058ccdfa1dca529f14d6d3a39705dfb077b3d2c
uuiid <957714080@qq.com>
2021-12-28 10:34:50 +0800

    单文件测试

--- src/doodle_exe/main/test/main.cpp
+++ src/doodle_exe/main/test/main.cpp
@@ -3,17 +3,31 @@
 //
 
 #include <doodle_lib/doodle_lib_all.h>
-#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_RUNNER 
 #include <catch2/catch.hpp>
 
-int main(int argc, char *argv[]) {
-  //初始化测试环境
-  std::setlocale(LC_CTYPE, ".UTF8");
-  auto k_doodle = doodle::new_object<doodle::doodle_lib>();
-  doodle::core_set_init k_init{};
-  k_init.config_to_user();
-  k_init.find_cache_dir();
-  doodle::logger_ctrl::get_log().set_log_name("doodle_test.txt");
-  int k_r = Catch::Session().run(argc, argv);
-  return k_r;
-}
+//struct init_main : public Catch::TestEventListenerBase {
+// public:
+//  using TestEventListenerBase::TestEventListenerBase;
+//  void testRunStarting(Catch::TestRunInfo const& testRunInfo) override {
+//    std::setlocale(LC_CTYPE, ".UTF8");
+//    auto k_doodle = doodle::new_object<doodle::doodle_lib>();
+//    doodle::core_set_init k_init{};
+//    k_init.config_to_user();
+//    k_init.find_cache_dir();
+//    doodle::logger_ctrl::get_log().set_log_name("doodle_test.txt");
+//  }
+//};
+//CATCH_REGISTER_LISTENER(init_main);
+
+ int main(int argc, char *argv[]) {
+   //初始化测试环境
+   std::setlocale(LC_CTYPE, ".UTF8");
+   auto k_doodle = doodle::new_object<doodle::doodle_lib>();
+   doodle::core_set_init k_init{};
+   k_init.config_to_user();
+   k_init.find_cache_dir();
+   doodle::logger_ctrl::get_log().set_log_name("doodle_test.txt");
+   int k_r = Catch::Session().run(argc, argv);
+   return k_r;
+ }
