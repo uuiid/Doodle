1bf13b5a8ae0e38b1fd3bd78b731cbb486c50cc5
uuiid <957714080@qq.com>
2022-01-04 10:41:13 +0800

    添加文件对话框

--- src/doodle_lib/core/open_file_dialog.cpp
+++ src/doodle_lib/core/open_file_dialog.cpp
@@ -6,22 +6,25 @@
 
 #include <doodle_lib/core/doodle_lib.h>
 #include <doodle_lib/doodle_app.h>
+#include <doodle_lib/lib_warp/imgui_warp.h>
+
+#include <imfilebrowser.h>
 namespace doodle {
 
-void open_file_dialog::show(const std::function<void(const std::vector<FSys::path>&)>& in_fun) {
+void open_file_dialog::show(const std::function<void(const std::vector<FSys::path> &)> &in_fun) {
   doodle_app::Get()->main_loop.connect_extended(
-      [kes = this->p_vKey, fun = in_fun](const doodle_app::connection& in) {
+      [kes = this->p_vKey, fun = in_fun](const doodle_app::connection &in) {
         dear::OpenFileDialog{kes} && [k_fun = fun, &in]() {
           auto ig = ImGuiFileDialog::Instance();
           if (ig->IsOk()) {
-            auto filter           = ig->GetCurrentFilter();
+            auto filter             = ig->GetCurrentFilter();
             FSys::path k_get_curr_p = ig->GetFilePathName();
-            auto k_get_curr            = k_get_curr_p.parent_path();
+            auto k_get_curr         = k_get_curr_p.parent_path();
             std::vector<FSys::path> k_list{};
             auto selected = ig->GetSelection();
             std::transform(selected.begin(), selected.end(),
                            std::back_inserter(k_list),
-                           [&k_get_curr](const auto& k) {
+                           [&k_get_curr](const auto &k) {
                              return k_get_curr / k.first;
                            });
             if (selected.empty())
@@ -32,4 +35,75 @@ void open_file_dialog::show(const std::function<void(const std::vector<FSys::pat
         };
       });
 };
+
+class file_dialog::impl {
+ public:
+  explicit impl(::ImGuiFileBrowserFlags in_flags) : p_file_dialog(in_flags){};
+  imgui::FileBrowser p_file_dialog;
+  imgui::FileDialog p_dialog{};
+  select_sig p_sig;
+};
+file_dialog::file_dialog(const file_dialog::select_sig &in_sig,
+                         const string &in_title,
+                         std::int32_t in_flags,
+                         const std::vector<string> &in_filters,
+                         const FSys::path &in_pwd)
+    : p_i(std::make_unique<impl>(in_flags)) {
+  p_i->p_sig = in_sig;
+  p_i->p_file_dialog.SetTitle(in_title);
+  p_i->p_file_dialog.SetTypeFilters(in_filters);
+  p_i->p_file_dialog.SetPwd(in_pwd);
+}
+file_dialog::file_dialog(const file_dialog::select_sig &in_function,
+                         const string &in_title,
+                         const FSys::path &in_pwd)
+    : file_dialog(in_function,
+                  in_title,
+                  (in_function.index() == 1
+                       ? flags::ImGuiFileBrowserFlags_MultipleSelection
+                       : flags::ImGuiFileBrowserFlags_SelectDirectory) |
+                      flags::ImGuiFileBrowserFlags_SelectDirectory,
+                  {},
+                  in_pwd) {
+}
+file_dialog::file_dialog(const file_dialog::select_sig &in_function,
+                         const string &in_title,
+                         const std::vector<string> &in_filters,
+                         const FSys::path &in_pwd)
+    : file_dialog(in_function,
+                  in_title,
+                  (in_function.index() == 1
+                       ? flags::ImGuiFileBrowserFlags_MultipleSelection
+                       : 0),
+                  in_filters,
+                  in_pwd) {
+}
+file_dialog::~file_dialog() = default;
+
+void file_dialog::init() {
+  p_i->p_file_dialog.Open();
+}
+void file_dialog::update(chrono::duration<chrono::system_clock::rep, chrono::system_clock::period>, void *data) {
+  p_i->p_file_dialog.Display();
+  if (p_i->p_file_dialog.HasSelected()) {
+    std::visit(entt::overloaded{
+                   [&](const one_sig &in_sig) -> void {
+                     in_sig(p_i->p_file_dialog.GetSelected());
+                     this->succeed();
+                   },
+                   [&](const mult_sig &in_sig) -> void {
+                     in_sig(p_i->p_file_dialog.GetMultiSelected());
+                     this->succeed();
+                   }},
+               p_i->p_sig);
+
+  }
+
+}
+void file_dialog::succeeded() {
+}
+void file_dialog::failed() {
+}
+void file_dialog::aborted() {
+}
 }  // namespace doodle
--- src/doodle_lib/core/open_file_dialog.h
+++ src/doodle_lib/core/open_file_dialog.h
@@ -27,4 +27,73 @@ class DOODLELIB_API open_file_dialog
 
   void show(const std::function<void(const std::vector<FSys::path>&)>& in_fun);
 };
+using ImGuiFileBrowserFlags = int;
+class DOODLELIB_API file_dialog : public process_t<file_dialog> {
+  class impl;
+  std::unique_ptr<impl> p_i;
+
+ public:
+  enum flags {
+    ImGuiFileBrowserFlags_SelectDirectory   = 1 << 0,  // select directory instead of regular file
+    ImGuiFileBrowserFlags_EnterNewFilename  = 1 << 1,  // allow user to enter new filename when selecting regular file
+    ImGuiFileBrowserFlags_NoModal           = 1 << 2,  // file browsing window is modal by default. specify this to use a popup window
+    ImGuiFileBrowserFlags_NoTitleBar        = 1 << 3,  // hide window title bar
+    ImGuiFileBrowserFlags_NoStatusBar       = 1 << 4,  // hide status bar at the bottom of browsing window
+    ImGuiFileBrowserFlags_CloseOnEsc        = 1 << 5,  // close file browser when pressing 'ESC'
+    ImGuiFileBrowserFlags_CreateNewDir      = 1 << 6,  // allow user to create new directory
+    ImGuiFileBrowserFlags_MultipleSelection = 1 << 7,  // allow user to select multiple files. this will hide ImGuiFileBrowserFlags_EnterNewFilename
+  };
+  using base_type  = process_t<file_dialog>;
+  using one_sig    = std::function<void(const FSys::path& in_select_path)>;
+  using mult_sig   = std::function<void(const std::vector<FSys::path>& in_select_path)>;
+  using select_sig = std::variant<one_sig, mult_sig>;
+
+  /**
+   * @brief 最为齐全的参数
+   * @param in_sig 信号
+   * @param in_title 标题
+   * @param in_flags 标志
+   * @param in_filters 过滤器
+   * @param in_pwd 当前路径
+   */
+  explicit file_dialog(const select_sig& in_sig,
+                       const std::string& in_title,
+                       std::int32_t in_flags,
+                       const std::vector<string>& in_filters,
+                       const FSys::path& in_pwd);
+
+  /**
+   * @brief 这个是传入时 使用单个文件并且使用目录的对话框, 所以不需要传入过滤器
+   * 并且使用 ImGuiFileBrowserFlags_SelectDirectory
+   * @param in_function 传入的回调函数
+   * @param in_title 传入的标题
+   * @param in_pwd 打开时的路径
+   */
+  explicit file_dialog(
+      const select_sig& in_function,
+      const std::string& in_title,
+      const FSys::path& in_pwd = FSys::current_path());
+
+  /**
+   * @brief 传入时使用单文件目录选择, 默认过滤器
+   * @param in_function
+   * @param in_title
+   * @param in_filters
+   * @param in_pwd
+   */
+  explicit file_dialog(
+      const select_sig& in_function,
+      const std::string& in_title,
+      const std::vector<string>& in_filters,
+      const FSys::path& in_pwd = FSys::current_path());
+
+  ~file_dialog() override;
+
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(base_type::delta_type, void* data);
+};
+
 }  // namespace doodle
