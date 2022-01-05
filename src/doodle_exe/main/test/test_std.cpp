05692af578f322b3b122c876d88d7a4494f6f413
uuiid <957714080@qq.com>
2022-01-05 13:43:55 +0800

    添加测试

---
+++ src/doodle_exe/main/test/test_std.cpp
@@ -0,0 +1,12 @@
+//
+// Created by TD on 2021/7/27.
+//
+#include <doodle_lib/doodle_lib_all.h>
+
+#include <catch.hpp>
+
+TEST_CASE("for", "[cpp]") {
+  for (int l_i = 10; l_i; --l_i) {
+    std::cout << l_i << std::endl;
+  }
+}
