1c6b648d5f6caf3cd37083c15a046aacf8928401
uuiid <957714080@qq.com>
2022-01-11 18:03:15 +0800

    更新数据库生成和添加长时间任务

--- src/doodle_lib/core/000-metadatatab.sql
+++ src/doodle_lib/core/000-metadatatab.sql
@@ -2,14 +2,15 @@ create table if not exists metadatatab
 (
     id          bigint unsigned auto_increment
         primary key,
-    parent      bigint unsigned                    null,
-    uuidPath    text                               null,
+    parent      bigint unsigned    null,
+    uuidPath    text               null,
+    user_data   text               null,
     update_time datetime default CURRENT_TIMESTAMP not null,
-    meta_type   bigint   default 0                 null,
-    episode     int                                null,
-    shot        int                                null,
-    season      int                                null,
-    assets_p    text                               null,
+    meta_type   bigint   default 0 null,
+    episode     int                null,
+    shot        int                null,
+    season      int                null,
+    assets_p    text               null,
     constraint fk_test_id
         foreign key (parent) references metadatatab (id)
             on delete cascade
