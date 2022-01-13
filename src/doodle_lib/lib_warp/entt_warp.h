61e0d003aafab6b59c2c8aa9ca9a9dd2468cadc1
uuiid <957714080@qq.com>
2022-01-13 18:39:37 +0800

    进行兼容性序列化更改更改

--- src/doodle_lib/lib_warp/entt_warp.h
+++ src/doodle_lib/lib_warp/entt_warp.h
@@ -15,7 +15,7 @@ template <class Component, class Archive>
 void _save_(entt::handle &in_handle, std::size_t in_size, Archive &in_archive) {
   in_archive["size"] = in_size;
   while (in_size--) {
-    in_archive[typeid(Component).name()] = in_handle.template get<Component>();
+    in_archive[in_size] = in_handle.template get<Component>();
   }
 }
 
@@ -23,10 +23,12 @@ template <class Component, class Archive>
 void _load_(entt::handle &in_handle, Archive &in_archive) {
   std::size_t l_size{};
   l_size = in_archive.at("size").template get<std::size_t>();
-
   while (l_size--) {
     Component l_component{};
-    in_archive.at(typeid(Component).name()).get_to(l_component);
+    if (in_archive.contains(typeid(Component).name()))
+      in_archive.at(typeid(Component).name()).get_to(l_component);
+    else
+      in_archive.at(l_size).get_to(l_component);
     in_handle.template emplace_or_replace<Component>(std::move(l_component));
   }
 }
@@ -35,7 +37,7 @@ template <class... Component, class Archive, std::size_t... Index>
 void _save_comm_(entt::handle &in_handle, Archive &in_archive, std::index_sequence<Index...>) {
   std::array<std::size_t, sizeof...(Index)> size{};
   ((in_handle.template any_of<Component>() ? ++(size[Index]) : 0u), ...);
-  (_save_<Component>(in_handle, size[Index], in_archive[typeid(Component).name()]), ...);
+  ((size[Index] ? _save_<Component>(in_handle, size[Index], in_archive[typeid(Component).name()]) : 0u), ...);
 }
 
 template <class... Component, class Archive, std::size_t... Index>
