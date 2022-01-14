02b63bea2adab4595bcea368e3b74ad984632359
uuiid <957714080@qq.com>
2022-01-14 16:58:37 +0800

    添加状态栏

--- src/doodle_lib/gui/main_status_bar.h
+++ src/doodle_lib/gui/main_status_bar.h
@@ -6,9 +6,22 @@
 
 #include <doodle_lib/doodle_lib_fwd.h>
 
-namespace doodle{
-class DOODLELIB_API main_status_bar :public process_t<main_status_bar> {
+namespace doodle {
+class DOODLELIB_API main_status_bar : public process_t<main_status_bar> {
+ private:
+  class impl;
+  std::unique_ptr<impl> p_i;
+
  public:
+  main_status_bar();
+  ~main_status_bar() override;
+
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(delta_type, void* data);
+
 
 };
-}
+}  // namespace doodle
