b59973901349ae1c0745a3c5d07d4d584e716d72
uuiid <957714080@qq.com>
2022-01-14 15:54:48 +0800

    添加主菜单

--- src/doodle_lib/gui/main_menu_bar.cpp
+++ src/doodle_lib/gui/main_menu_bar.cpp
@@ -3,4 +3,95 @@
 //
 
 #include "main_menu_bar.h"
+#include <lib_warp/imgui_warp.h>
+#include <doodle_lib/doodle_app.h>
+#include <doodle_lib/core/core_set.h>
+#include <doodle_lib/client/client.h>
+#include <doodle_lib/long_task/process_pool.h>
+#include <doodle_lib/core/open_file_dialog.h>
 
+namespace doodle {
+class main_menu_bar::impl {
+ public:
+  bool p_debug_show{false};
+  bool p_style_show{false};
+  bool p_about_show{false};
+};
+
+main_menu_bar::main_menu_bar()
+    : p_i(std::make_unique<impl>()) {
+}
+
+main_menu_bar::~main_menu_bar() = default;
+
+void main_menu_bar::menu_file() {
+  if (dear::MenuItem("新项目"s)) {
+    g_main_loop().attach<file_dialog>(
+        [](const FSys::path &in_path) {
+          core::client{}.add_project(in_path);
+          g_reg()->set<project>(in_path, "none");
+          core_set::getSet().add_recent_project(in_path);
+        },
+        "选择目录"s);
+  }
+  if (dear::MenuItem("打开项目"s)) {
+    g_main_loop().attach<file_dialog>(
+        [](const FSys::path &in_path) {
+          g_reg()->set<project>(in_path, "temp_project");
+          core::client{}.open_project(in_path);
+          core_set::getSet().add_recent_project(in_path);
+        },
+        "选择目录"s);
+  }
+  dear::Menu{"最近的项目"} && []() {
+    auto &k_list = core_set::getSet().project_root;
+    for (int l_i = 0; l_i < k_list.size(); ++l_i) {
+      auto &l_p = k_list[l_i];
+      if (!l_p.empty())
+        if (dear::MenuItem(fmt::format("{0}##{1}", l_p.generic_string(), l_i))) {
+          g_reg()->set<project>(l_p, "temp_project");
+          core::client{}.open_project(l_p);
+        }
+    }
+  };
+
+  ImGui::Separator();
+  dear::MenuItem("调试"s, &p_i->p_debug_show);
+  dear::MenuItem("样式设置"s, &p_i->p_style_show);
+  dear::MenuItem("关于"s, &p_i->p_about_show);
+  ImGui::Separator();
+  if (dear::MenuItem(u8"退出")) {
+    doodle_app::Get()->p_done = true;
+  }
+}
+void main_menu_bar::menu_windows() {
+}
+void main_menu_bar::menu_edit() {
+}
+void main_menu_bar::menu_tool() {
+}
+
+void main_menu_bar::init() {
+  g_reg()->set<main_menu_bar &>(*this);
+}
+void main_menu_bar::succeeded() {
+}
+void main_menu_bar::failed() {
+}
+void main_menu_bar::aborted() {
+}
+void main_menu_bar::update(
+    std::chrono::duration<std::chrono::system_clock::rep, std::chrono::system_clock::period>,
+    void *data) {
+  dear::MainMenuBar{} && [this]() {
+    dear::Menu{"文件"} && [this]() { this->menu_file(); };
+    dear::Menu{"窗口"} && [this]() { this->menu_windows(); };
+    dear::Menu{"编辑"} && [this]() { this->menu_edit(); };
+    dear::Menu{"工具"} && [this]() { this->menu_tool(); };
+#ifndef NDEBUG
+    ImGui::Text("%.3f ms/%.1f FPS", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+#endif
+  };
+}
+
+}  // namespace doodle
--- src/doodle_lib/gui/main_menu_bar.h
+++ src/doodle_lib/gui/main_menu_bar.h
@@ -7,11 +7,24 @@
 
 namespace doodle {
 class DOODLELIB_API main_menu_bar : public process_t<main_menu_bar> {
- public:
-  using base_type = process_t<main_menu_bar>;
+ private:
+  class impl;
+  std::unique_ptr<impl> p_i;
 
+ protected:
+  virtual void menu_file();
+  virtual void menu_windows();
+  virtual void menu_edit();
+  virtual void menu_tool();
+
+ public:
   main_menu_bar();
   ~main_menu_bar() override;
 
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(delta_type, void* data);
 };
 }  // namespace doodle
