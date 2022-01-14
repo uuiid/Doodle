78fde8bd8e15bdc387c2ae951f18fffa29245dc9
uuiid <957714080@qq.com>
2022-01-14 11:11:52 +0800

    客户端清理函数添加

--- src/doodle_lib/client/client.cpp
+++ src/doodle_lib/client/client.cpp
@@ -8,6 +8,7 @@
 #include <doodle_lib/long_task/process_pool.h>
 #include <doodle_lib/long_task/database_task.h>
 #include <doodle_lib/thread_pool/long_term.h>
+#include <doodle_lib/metadata/metadata.h>
 #include <sqlpp11/sqlpp11.h>
 #include <sqlpp11/sqlite3/sqlite3.h>
 #include <sqlpp11/ppgen.h>
@@ -24,6 +25,7 @@ SQLPP_DECLARE_TABLE(
 
 namespace doodle::core {
 void client::add_project(const std::filesystem::path& in_path) {
+  clear_project();
   auto k_conn = core_sql::Get().get_connection(in_path / doodle_config::doodle_db_name);
   k_conn->execute(R"(
 create table if not exists metadatatab
@@ -93,9 +95,14 @@ create table if not exists doodle_info
                        l_info.version_minor = Doodle_VERSION_MINOR));
 }
 void client::open_project(const FSys::path& in_path) {
+  clear_project();
   auto k_h = make_handle();
   k_h.emplace<process_message>();
   g_main_loop().attach<database_task_select>(k_h, in_path);
+}
+void client::clear_project() {
+  auto k_view = g_reg()->view<database>();
+  g_reg()->destroy(k_view.begin(), k_view.end());
 }
 
 }  // namespace doodle::core
