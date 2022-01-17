b2da4575a747ee8af3e443ef0f8e5221a5f73acc
uuiid <957714080@qq.com>
2022-01-17 17:22:38 +0800

    添加maya特有的菜单栏

---
+++ src/doodle_exe/maya_plug/gui/maya_menu_bar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2022/1/17.
+//
+
+#include "maya_menu_bar.h"
---
+++ src/doodle_exe/maya_plug/gui/maya_menu_bar.h
@@ -0,0 +1,13 @@
+//
+// Created by TD on 2022/1/17.
+//
+
+#include <maya_plug_fwd.h>
+
+namespace doodle::maya_plug {
+class maya_menu_bar : process_t<maya_menu_bar> {
+ public:
+ 
+};
+
+}  // namespace doodle::maya_plug
