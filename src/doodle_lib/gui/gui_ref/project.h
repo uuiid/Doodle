be17e67102f4aaa1ef44cab9dc53e693f7d8e153
uuiid <957714080@qq.com>
2022-01-20 18:54:02 +0800

    gui更新

--- src/doodle_lib/gui/gui_ref/project.h
+++ src/doodle_lib/gui/gui_ref/project.h
@@ -6,12 +6,13 @@
 #include <doodle_lib/metadata/project.h>
 #include <doodle_lib/gui/gui_ref/path.h>
 
-namespace doodle{
+namespace doodle {
 namespace gui {
 template <>
 struct adl_render<project> : adl_render_data<project> {
   adl_render<FSys::path> p_path_render;
+  using adl_render_data<project>::adl_render_data ;
   bool render(const entt::handle& in_handle);
 };
 }  // namespace gui
-}
+}  // namespace doodle
