60302527247515c5a07517d49915f0207db63b6c
uuiid <957714080@qq.com>
2022-01-21 11:47:57 +0800

    测试更新

--- src/doodle_exe/main/test/test_gui.cpp
+++ src/doodle_exe/main/test/test_gui.cpp
@@ -2,23 +2,24 @@
 // Created by TD on 2021/7/27.
 //
 #include <doodle_lib/doodle_lib_all.h>
-
+#include <doodle_lib/app/app.h>
+#include <doodle_lib/gui/get_input_dialog.h>
 #include <catch.hpp>
 
+using namespace doodle;
 TEST_CASE("test_gui", "[gui]") {
-  using namespace doodle;
   FSys::path k_voide_file1{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0001.mp4)"};
   FSys::path k_voide_file2{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0002.mp4)"};
   FSys::path k_voide_file3{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0003.mp4)"};
   FSys::path k_voide_file4{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0004.mp4)"};
 
-  auto& k_d    = doodle_lib::Get();
+  auto& k_d  = doodle_lib::Get();
 
-  auto k_app   = new_object<app>();
+  auto k_app = std::make_shared<app>();
 
-  auto k_reg   = g_reg();
+  auto k_reg = g_reg();
 
-  auto k_h     = make_handle(k_reg->create());
+  auto k_h   = make_handle(k_reg->create());
   k_h.emplace<opencv_read_player>().open_file(k_voide_file1);
 
   k_h = make_handle(k_reg->create());
@@ -30,4 +31,12 @@ TEST_CASE("test_gui", "[gui]") {
   k_h = make_handle(k_reg->create());
   k_h.emplace<opencv_read_player>().open_file(k_voide_file4);
   k_app->run();
+}
+
+TEST_CASE("test get input") {
+  app l_app{};
+  auto k_h = make_handle();
+  k_h.emplace<project>();
+  g_main_loop().attach<get_input_project_dialog>(k_h);
+  l_app.run();
 }
