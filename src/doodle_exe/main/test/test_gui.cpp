dd9b820c8ea6059b8d5cbfb5dc71701223674fe6
uuiid <957714080@qq.com>
2022-01-22 17:15:39 +0800

    完成测试修改

--- src/doodle_exe/main/test/test_gui.cpp
+++ src/doodle_exe/main/test/test_gui.cpp
@@ -9,31 +9,31 @@
 #include <catch.hpp>
 
 using namespace doodle;
-TEST_CASE("test_gui", "[gui]") {
-  FSys::path k_voide_file1{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0001.mp4)"};
-  FSys::path k_voide_file2{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0002.mp4)"};
-  FSys::path k_voide_file3{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0003.mp4)"};
-  FSys::path k_voide_file4{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0004.mp4)"};
-
-  auto& k_d  = doodle_lib::Get();
-
-  auto k_app = std::make_shared<app>();
-
-  auto k_reg = g_reg();
-
-  auto k_h   = make_handle(k_reg->create());
-  k_h.emplace<opencv_read_player>().open_file(k_voide_file1);
-
-  k_h = make_handle(k_reg->create());
-  k_h.emplace<opencv_read_player>().open_file(k_voide_file2);
-
-  k_h = make_handle(k_reg->create());
-  k_h.emplace<opencv_read_player>().open_file(k_voide_file3);
-
-  k_h = make_handle(k_reg->create());
-  k_h.emplace<opencv_read_player>().open_file(k_voide_file4);
-  k_app->run();
-}
+// TEST_CASE("test_gui", "[gui]") {
+//   FSys::path k_voide_file1{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0001.mp4)"};
+//   FSys::path k_voide_file2{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0002.mp4)"};
+//   FSys::path k_voide_file3{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0003.mp4)"};
+//   FSys::path k_voide_file4{R"(D:\Doodle\data\case_tset\ep0000\sc0000\tset_0\None_\user_ep0000_sc0004.mp4)"};
+//
+//   auto& k_d  = doodle_lib::Get();
+//
+//   auto k_app = std::make_shared<app>();
+//
+//   auto k_reg = g_reg();
+//
+//   auto k_h   = make_handle(k_reg->create());
+//   k_h.emplace<opencv_read_player>().open_file(k_voide_file1);
+//
+//   k_h = make_handle(k_reg->create());
+//   k_h.emplace<opencv_read_player>().open_file(k_voide_file2);
+//
+//   k_h = make_handle(k_reg->create());
+//   k_h.emplace<opencv_read_player>().open_file(k_voide_file3);
+//
+//   k_h = make_handle(k_reg->create());
+//   k_h.emplace<opencv_read_player>().open_file(k_voide_file4);
+//   k_app->run();
+// }
 
 class test_app : public app {
  public:
