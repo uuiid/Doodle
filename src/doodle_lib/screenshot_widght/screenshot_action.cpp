ff3b3795bca37b92994bac0af6eee97d44149b5c
uuiid <957714080@qq.com>
2022-01-22 11:48:50 +0800

    去除不规范的文件

--- src/doodle_lib/screenshot_widght/screenshot_action.cpp
+++
@@ -1,7 +0,0 @@
-#include <Exception/exception.h>
-#include <screenshot_widght/screenshot_action.h>
-
-#include <boost/filesystem.hpp>
-
-namespace doodle {
-}
--- src/doodle_lib/screenshot_widght/screenshot_widght.cpp
+++
@@ -1,8 +0,0 @@
-#include <doodle_lib/Exception/exception.h>
-#include <doodle_lib/Logger/logger.h>
-#include <doodle_lib/screenshot_widght/screenshot_action.h>
-#include <doodle_lib/screenshot_widght/screenshot_widght.h>
-
-namespace doodle {
-
-}
