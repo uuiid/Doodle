6480f3f035ce61f1b072ee7be92096e745dc87d5
uuiid <957714080@qq.com>
2022-01-28 14:12:03 +0800

    新的静态变量

--- src/doodle_lib/core/static_value.h
+++ src/doodle_lib/core/static_value.h
@@ -3,9 +3,12 @@
 //
 
 #pragma once
+#include <string>
 namespace doodle {
 namespace doodle_config {
 constexpr const char doodle_db_name[] = "project.doodle_db";
 constexpr const char ue_path_obj[]    = "Engine/Binaries/Win64/UE4Editor.exe";
+constexpr const std::string_view drop_imgui_id{"drop_imgui_id"};
+
 }  // namespace doodle_config
 }  // namespace doodle
