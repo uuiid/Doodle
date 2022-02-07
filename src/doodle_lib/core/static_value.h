1afeae89f66bd0897a54c502e053eace06b16975
uuiid <957714080@qq.com>
2022-02-07 14:18:16 +0800

    添加新配置

--- src/doodle_lib/core/static_value.h
+++ src/doodle_lib/core/static_value.h
@@ -9,6 +9,7 @@ namespace doodle_config {
 constexpr const char doodle_db_name[] = "project.doodle_db";
 constexpr const char ue_path_obj[]    = "Engine/Binaries/Win64/UE4Editor.exe";
 constexpr const std::string_view drop_imgui_id{"drop_imgui_id"};
+constexpr const std::size_t rand_block{256};
 
 }  // namespace doodle_config
 }  // namespace doodle
