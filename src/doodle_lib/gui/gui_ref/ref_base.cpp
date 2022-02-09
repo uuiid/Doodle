f4154bfbf9bbe024c00cc349fd7d6916eb25a185
uuiid <957714080@qq.com>
2022-02-09 17:48:41 +0800

    添加需要保存函数

--- src/doodle_lib/gui/gui_ref/ref_base.cpp
+++ src/doodle_lib/gui/gui_ref/ref_base.cpp
@@ -1,12 +1,15 @@
 #include "ref_base.h"
-
+#include <metadata/metadata.h>
 namespace doodle::gui {
 void base_edit::init(const entt::handle &in) {
   is_modify = false;
   init_(in);
 }
-void base_edit::save(const entt::handle &in)  {
+void base_edit::save(const entt::handle &in) {
   if (is_modify) {
+    if (in.all_of<database>()) {
+      in.patch<database>(database::save{});
+    }
     save_(in);
     is_modify = false;
   }
