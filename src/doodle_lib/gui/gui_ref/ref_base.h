2248943a017921ad2cc2ce5d1be4a99894029b4f
uuiid <957714080@qq.com>
2022-02-09 12:00:31 +0800

    添加编辑基类

--- src/doodle_lib/gui/gui_ref/ref_base.h
+++ src/doodle_lib/gui/gui_ref/ref_base.h
@@ -7,11 +7,18 @@ namespace doodle::gui {
 class DOODLELIB_API base_render {
  public:
   base_render() = default;
-  virtual bool operator()(const entt::handle& in_handle) {};
+  virtual bool operator()(const entt::handle &in_handle){};
 };
 
 template <class T>
 base_render make_render() {
 }
 
+class base_edit {
+ public:
+  bool is_modify{false};
+  virtual void init(const entt::handle &in)       = 0;
+  virtual void render(const entt::handle &in)     = 0;
+  virtual void save(const entt::handle &in) const = 0;
+};
 }  // namespace doodle::gui
