4df0f3173bde0e8f27cb9f838ab6f7e31ecbd8ca
uuiid <957714080@qq.com>
2022-02-10 17:10:42 +0800

    添加gui编辑

--- src/doodle_lib/gui/gui_ref/project.cpp
+++ src/doodle_lib/gui/gui_ref/project.cpp
@@ -6,8 +6,60 @@
 
 #include <lib_warp/imgui_warp.h>
 #include <gui/gui_ref/path.h>
+#include <metadata/project.h>
+
+#include <doodle_lib/lib_warp/imgui_warp.h>
+
 namespace doodle {
 namespace gui {
+class project_edit::impl {
+ public:
+  project p_prj;
+  std::string path;
+  project::cloth_config cloth_config;
+  std::string cloth_config_path;
+};
+project_edit::project_edit()
+    : p_i(std::make_unique<impl>()) {
+}
 
+void project_edit::init_(const entt::handle& in) {
+  auto& l_prj            = in.get<project>();
+  p_i->p_prj             = l_prj;
+  p_i->path              = l_prj.get_path().generic_string();
+
+  p_i->cloth_config      = in.get_or_emplace<project::cloth_config>();
+  p_i->cloth_config_path = p_i->cloth_config.vfx_cloth_sim_path.generic_string();
+}
+void project_edit::save_(const entt::handle& in) const {
+  in.emplace_or_replace<project>(p_i->p_prj);
+  in.emplace_or_replace<project::cloth_config>(p_i->cloth_config);
+  g_reg()->set<project>(in.get<project>());
+}
+void project_edit::render(const entt::handle& in) {
+  ImGui::Text("基本配置");
+  dear::Text(fmt::format("路径: {}", p_i->path));
+
+  if (ImGui::InputText("名称", &p_i->p_prj.p_name)) {
+    is_modify = true;
+  }
+
+  dear::TreeNode{"解算配置"} &&
+      [&]() {
+        if (imgui::InputText("解算路径", &(p_i->cloth_config_path))) {
+          in.patch<project::cloth_config>([&](project::cloth_config& in) {
+            in.vfx_cloth_sim_path = p_i->cloth_config_path;
+          });
+        }
+
+        if (imgui::InputText("导出节点", &(p_i->cloth_config.export_group))) {
+          in.patch<project::cloth_config>([&](project::cloth_config& in) {
+            in.export_group = p_i->cloth_config.export_group;
+          });
+        }
+      };
+}
+
+project_edit::~project_edit() = default;
 }  // namespace gui
 }  // namespace doodle
--- src/doodle_lib/gui/gui_ref/project.h
+++ src/doodle_lib/gui/gui_ref/project.h
@@ -3,11 +3,23 @@
 //
 
 #pragma once
-#include <doodle_lib/metadata/project.h>
-#include <doodle_lib/gui/gui_ref/path.h>
+#include <doodle_lib/gui/gui_ref/ref_base.h>
 
 namespace doodle {
 namespace gui {
+
+class DOODLELIB_API project_edit : public base_edit {
+  class impl;
+  std::unique_ptr<impl> p_i;
+
+  virtual void init_(const entt::handle &in) override;
+  virtual void save_(const entt::handle &in) const override;
+
+ public:
+  project_edit();
+  ~project_edit();
+  virtual void render(const entt::handle &in) override;
+};
 
 }  // namespace gui
 }  // namespace doodle
