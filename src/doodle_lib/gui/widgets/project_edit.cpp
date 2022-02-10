a51b9c53b9e2455737b845ee2f386732b0a91615
uuiid <957714080@qq.com>
2022-02-10 17:12:39 +0800

    添加project编辑

--- src/doodle_lib/gui/widgets/project_edit.cpp
+++ src/doodle_lib/gui/widgets/project_edit.cpp
@@ -5,18 +5,14 @@
 #include "project_edit.h"
 #include <metadata/project.h>
 #include <doodle_lib/lib_warp/imgui_warp.h>
+#include <doodle_lib/gui/gui_ref/project.h>
 
 namespace doodle {
 
 class project_edit::impl {
  public:
-  impl() = default;
+  gui::project_edit p_edit;
   entt::handle p_h;
-
-  project p_prj;
-  std::string path;
-  project::cloth_config cloth_config;
-  std::string cloth_config_path;
 };
 
 project_edit::project_edit()
@@ -25,12 +21,9 @@ project_edit::project_edit()
 project_edit::~project_edit() = default;
 
 void project_edit::init() {
-  p_i->p_h               = project::get_current();
+  p_i->p_h = project::get_current();
 
-  p_i->p_prj             = p_i->p_h.get<project>();
-  p_i->path              = p_i->p_prj.p_path.generic_string();
-  p_i->cloth_config      = p_i->p_h.get_or_emplace<project::cloth_config>();
-  p_i->cloth_config_path = p_i->cloth_config.vfx_cloth_sim_path.generic_string();
+  p_i->p_edit.init(p_i->p_h);
 }
 void project_edit::succeeded() {
 }
@@ -41,29 +34,7 @@ void project_edit::aborted() {
 void project_edit::update(const chrono::duration<chrono::system_clock::rep,
                                                  chrono::system_clock::period>&,
                           void* data) {
-  ImGui::Text("基本配置");
-  dear::Text(fmt::format("路径: {}", p_i->path));
-
-  if (ImGui::InputText("名称", &p_i->p_prj.p_name)) {
-    p_i->p_h.patch<project>([&](project& in) {
-      in.set_name(p_i->p_prj.p_name);
-    });
-    g_reg()->set<project>(p_i->p_h.get<project>());
-  }
-
-  dear::TreeNode{"解算配置"} &&
-      [&]() {
-        if (imgui::InputText("解算路径", &(p_i->cloth_config_path))) {
-          p_i->p_h.patch<project::cloth_config>([&](project::cloth_config& in) {
-            in.vfx_cloth_sim_path = p_i->cloth_config_path;
-          });
-        }
-
-        if (imgui::InputText("导出节点", &(p_i->cloth_config.export_group))) {
-          p_i->p_h.patch<project::cloth_config>([&](project::cloth_config& in) {
-            in.export_group = p_i->cloth_config.export_group;
-          });
-        }
-      };
+  p_i->p_edit.render(p_i->p_h);
+  p_i->p_edit.save(p_i->p_h);
 }
 }  // namespace doodle
