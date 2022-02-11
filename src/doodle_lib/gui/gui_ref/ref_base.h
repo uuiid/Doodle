4ace03c4b3c6d87f6915ed0bd6cb648f4f1c1d19
uuiid <957714080@qq.com>
2022-02-11 13:50:48 +0800

    添加缓存类

--- src/doodle_lib/gui/gui_ref/ref_base.h
+++ src/doodle_lib/gui/gui_ref/ref_base.h
@@ -25,4 +25,46 @@ class base_edit {
   virtual void render(const entt::handle &in) = 0;
   void save(const entt::handle &in);
 };
+
+namespace details {
+template <class T>
+class gui_cache {
+ public:
+  T data;
+  std::string name;
+  explicit gui_cache(const T &in_data, const std::string &in_name)
+      : data(in_data),
+        name(fmt::format("{}##{}", in_name, fmt::ptr(this))){};
+
+  explicit gui_cache(const T &in_data)
+      : gui_cache(in_data, fmt::to_string(in_data)) {}
+
+  bool operator<(const gui_cache &in_rhs) const {
+    return data < in_rhs.data;
+  }
+  bool operator>(const gui_cache &in_rhs) const {
+    return in_rhs < *this;
+  }
+  bool operator<=(const gui_cache &in_rhs) const {
+    return !(in_rhs < *this);
+  }
+  bool operator>=(const gui_cache &in_rhs) const {
+    return !(*this < in_rhs);
+  }
+  bool operator==(const gui_cache &in_rhs) const {
+    return data == in_rhs.data;
+  }
+  bool operator!=(const gui_cache &in_rhs) const {
+    return !(in_rhs == *this);
+  }
+
+  constexpr operator T &() {
+    return data;
+  }
+  constexpr operator const T &() const {
+    return data;
+  }
+};
+}  // namespace details
+
 }  // namespace doodle::gui
