a5245ce94ff77805bac36edc642e04ffef332895
uuiid <957714080@qq.com>
2022-02-11 18:55:22 +0800

    添加模板函数细节

--- src/doodle_lib/gui/widgets/assets_widget.cpp
+++ src/doodle_lib/gui/widgets/assets_widget.cpp
@@ -16,358 +16,57 @@
 #include <gui/gui_ref/ref_base.h>
 
 namespace doodle {
-#ifdef DOODLE_DIS_FUN
-class assets_widget::impl {
- public:
-  static ImGuiTreeNodeFlags base_flags;
-
-  using select_obj = std::variant<season,
-                                  episodes,
-                                  shot,
-                                  string>;
-
-  std::deque<string> p_select_curr_path_;
-  std::deque<string> p_select_path_;
-
-  struct select_guard {
-    impl* self;
-    select_guard(impl* in_self, const string& in_str)
-        : self(in_self) {
-      self->p_select_curr_path_.push_back(in_str);
-    };
-    ~select_guard() {
-      self->p_select_curr_path_.pop_back();
-    };
-  };
-
-  entt::handle p_root;
-  std::set<select_obj> p_all_selected;
-  std::set<select_obj> p_all_old_selected;
-  handle_list p_ctx_list;
-  entt::observer p_season_obs;
-  entt::observer p_episodes_obs;
-  entt::observer p_shot_obs;
-  entt::observer p_assets_obs;
-
-  assets_widget* self;
-
-  bool only_rand{false};
-  std::vector<std::unique_ptr<gui::filter_base>> filter_list;
-
-  impl(assets_widget* in)
-      : self(in),
-        p_root() {
-    p_season_obs.connect(*g_reg(), entt::collector.group<season>());
-    p_episodes_obs.connect(*g_reg(), entt::collector.group<episodes>());
-    p_shot_obs.connect(*g_reg(), entt::collector.group<shot>());
-    p_assets_obs.connect(*g_reg(), entt::collector.group<assets>());
-  };
-
-  bool is_select() {
-    return p_select_curr_path_ == p_select_path_;
-    // return std::any_of(p_all_old_selected.begin(), p_all_old_selected.end(),
-    //                    [&](const select_obj& in_) {
-    //                      return in_ == in_ptr;
-    //                    });
-  }
-
-  void check_item(const select_obj& in_ptr) {
-    // if (ImGui::IsItemHovered()) {
-    //   DOODLE_LOG_DEBUG("ok");
-    // }
-    if (ImGui::GetIO().KeyShift && ImGui::IsItemHovered()) {
-      p_all_selected.insert(in_ptr);
-    }
-    // if (ImGui::GetIO().KeyCtrl && ImGui::IsItemHovered()) {
-    //   p_all_selected.erase(in_ptr);
-    // }
-  }
-  void check_item_clicked(const select_obj& in_ptr) {
-    if (ImGui::IsItemClicked()) {
-      if (!ImGui::GetIO().KeyCtrl) {
-        p_all_selected.clear();
-        p_all_old_selected.clear();
-      } else {
-        p_all_selected.erase(in_ptr);
-        return;
-      }
-    }
-  }
-
-  void set_select(const std::vector<entt::handle>& in_ptr, const select_obj& in_obj) {
-    p_select_path_ = p_select_curr_path_;
-    // p_all_selected.clear();
-    // p_all_selected.insert(in_obj);
-    auto k_reg     = g_reg();
-    p_ctx_list     = in_ptr;
-    g_reg()->ctx<core_sig>().filter_handle(p_ctx_list);
-  }
-
-  void observer_main() {
-    observer_(p_season_obs);
-    observer_(p_episodes_obs);
-    observer_(p_shot_obs);
-    observer_(p_assets_obs);
-  }
-  void observer_(entt::observer& in_obs) {
-    /// 检查创建的大小， 太多不显示， 之间清除
-    if (in_obs.size() > 10) {
-      in_obs.clear();
-      return;
-    }
-    bool set_ctx{false};
-    for (auto i : in_obs) {
-      auto k_h = make_handle(i);
-      /// 开始测试是否在选择中包含
-      if (!std::any_of(p_ctx_list.begin(), p_ctx_list.end(), [&](auto& in) { return k_h == in; })) {
-        set_ctx = true;
-        p_ctx_list.push_back(make_handle(k_h));
-      }
-    }
-
-    in_obs.clear();
-  }
-
-  /**
-   * @brief  加载树节点
-   *
-   */
-  void render() {
-    render_season();
-    handle_list k_l{};
-    auto k_g = g_reg();
-    for (auto& k : k_g->view<episodes, shot>(entt::exclude<season>)) {
-      k_l.push_back(make_handle(k));
-    }
-    render_eps(k_l);
-    k_l.clear();
-    for (auto& k : k_g->view<shot>(entt::exclude<season, episodes>)) {
-      k_l.push_back(make_handle(k));
-    }
-    render_shot(k_l);
-    render_assets();
-    // observer_main();
-
-    p_all_old_selected = p_all_selected;
-  }
-
-  void render_assets() {
-    handle_list k_list{};
-    for (auto& in : g_reg()->view<assets>(entt::exclude<season, episodes, shot>)) {
-      k_list.push_back(make_handle(in));
-    }
-    render_assets_recursion(k_list, 0);
-  }
-
-  void render_assets_recursion(const std::vector<entt::handle>& in_list, std::size_t in_deep) {
-    if (in_list.empty())
-      return;
-
-    std::set<string> k_list{};
-    std::multimap<string, entt::handle> k_map{};
-    bool is_leaf{true};
-
-    for (auto& k_h : in_list) {
-      if (!k_h.all_of<assets>())
-        continue;
-      auto& k_ass = k_h.get<assets>();
-      if (k_ass.get_path_component().size() <= in_deep)
-        continue;
-
-      is_leaf &= ((k_ass.get_path_component().size() - 1) == in_deep);
-      k_list.insert(k_ass.get_path_component()[in_deep]);
-      k_map.insert(std::make_pair(k_ass.get_path_component()[in_deep], k_h));
-    }
-
-    for (auto& k_ass_str : k_list) {
-      auto k_f = base_flags;
-      select_guard k_guard{this, k_ass_str};
-
-      if (is_select())
-        k_f |= ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_Selected;
-
-      if (is_leaf)
-        k_f = k_f | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
-      {
-        dear::TreeNodeEx k_node{k_ass_str.c_str(),
-                                k_f,
-                                k_ass_str.c_str()};
-        if (ImGui::IsItemClicked()) {
-          auto k_r = k_map.equal_range(k_ass_str);
-          handle_list k_h_list{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_h_list),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          set_select(k_h_list, k_ass_str);
-        }
-        k_node&& [&]() {
-          auto k_r = k_map.equal_range(k_ass_str);
-          handle_list k_h_list{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_h_list),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          if (k_h_list.empty())
-            return;
-          render_assets_recursion(k_h_list, in_deep + 1);
-        };
-      }
-    }
-  }
-
-  void render_season() {
-    auto k_g = g_reg();
-    auto k_v = k_g->view<season, episodes, shot>();
-    std::set<season> k_list;
-    std::multimap<season, entt::handle> k_map;
-    for (auto& k_ : k_v) {
-      k_list.insert(k_v.get<season>(k_));
-      k_map.insert(std::make_pair(k_v.get<season>(k_), make_handle(k_)));
-    }
-
-    for (auto& k_season : k_list) {
-      auto k_f = base_flags;
-      select_guard k_guard{this, k_season.str()};
-
-      if (is_select())
-        k_f |= ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_Selected;
-
-      {
-        dear::TreeNodeEx k_node{k_season.str().c_str(),
-                                k_f,
-                                k_season.str().c_str()};
-
-        if (ImGui::IsItemClicked()) {
-          auto k_r = k_map.equal_range(k_season);
-          handle_list k_list{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_list),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          set_select(k_list, k_season);
-        }
-        k_node&& [&]() {
-          auto k_r = k_map.equal_range(k_season);
-          handle_list k_list{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_list),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          render_eps(k_list);
-        };
-      }
-    }
-  }
-
-  void render_eps(const handle_list& in_list) {
-    std::set<episodes> k_list;
-    std::multimap<episodes, entt::handle> k_map;
-    for (auto& k_e : in_list) {
-      if (k_e.all_of<episodes>()) {
-        k_list.insert(k_e.get<episodes>());
-        k_map.insert(std::make_pair(k_e.get<episodes>(), k_e));
-      }
-    }
-
-    for (auto& k_episodes : k_list) {
-      auto k_f = base_flags;
-      select_guard k_guard{this, k_episodes.str()};
-
-      if (is_select())
-        k_f |= ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_Selected;
-      {
-        dear::TreeNodeEx k_node{k_episodes.str().c_str(),
-                                k_f,
-                                k_episodes.str().c_str()};
-        if (ImGui::IsItemClicked()) {
-          auto k_r = k_map.equal_range(k_episodes);
-          handle_list k_list{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_list),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          set_select(k_list, k_episodes);
-        }
-        k_node&& [&]() {
-          auto k_r = k_map.equal_range(k_episodes);
-          handle_list k_list{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_list),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          render_shot(k_list);
-        };
-      }
-    }
-  }
-  void render_shot(const handle_list& in_list) {
-    std::set<shot> k_list;
-    std::multimap<shot, entt::handle> k_map;
-    for (auto& k_e : in_list) {
-      if (k_e.all_of<shot>()) {
-        k_list.insert(k_e.get<shot>());
-        k_map.insert(std::make_pair(k_e.get<shot>(), k_e));
-      }
-    }
-
-    for (auto& k_shot : k_list) {
-      auto k_f = base_flags;
-      {
-        select_guard k_guard{this, k_shot.str()};
-        if (is_select())
-          k_f |= ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_Selected;
-
-        handle_list k_list{};
-        boost::copy(
-            k_map.equal_range(k_shot) |
-                boost::adaptors::transformed([](auto& k_p) {
-                  return k_p.second;
-                }) |
-                boost::adaptors::filtered([](const entt::handle& in) {
-                  return in.all_of<assets>();
-                }),
-            std::back_inserter(k_list));
-        if (k_list.empty())
-          k_f = k_f | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
-
-        dear::TreeNodeEx k_node{
-            k_shot.str().c_str(),
-            k_f,
-            k_shot.str().c_str()};
-        if (ImGui::IsItemClicked()) {
-          auto k_r = k_map.equal_range(k_shot);
-          handle_list k_list_s{};
-          std::transform(k_r.first, k_r.second, std::back_inserter(k_list_s),
-                         [](auto& in) {
-                           return in.second;
-                         });
-          set_select(k_list_s, k_shot);
-        }
-
-        k_node&& [&]() {
-          render_assets_recursion(k_list, 0);
-        };
-      }
-    }
-  }
-};
-ImGuiTreeNodeFlags assets_widget::impl::base_flags{ImGuiTreeNodeFlags_OpenOnArrow |
-                                                   ImGuiTreeNodeFlags_OpenOnDoubleClick |
-                                                   ImGuiTreeNodeFlags_SpanAvailWidth};
-#endif
 
 namespace gui {
+class filter_factory_base::impl {
+ public:
+  std::vector<boost::signals2::scoped_connection> p_conns;
+  bool need_init{false};
+};
+
+filter_factory_base::filter_factory_base()
+    : p_i(std::make_unique<impl>()),
+      is_disabled(false),
+      p_obs(),
+      is_edit(false) {
+  connection_sig();
+}
+
+filter_factory_base::~filter_factory_base() {
+  p_obs.disconnect();
+};
+
 std::unique_ptr<filter_base> filter_factory_base::make_filter() {
+  is_edit = false;
   return make_filter_();
 }
-bool filter_factory_base::refresh() {
-  auto k_r = refresh_();
+void filter_factory_base::refresh() {
+  if (!is_disabled)
+    refresh_();
+
   p_obs.clear();
-  return k_r;
+
+  if (p_i->need_init) {
+    this->init();
+    p_i->need_init = false;
+  }
 }
-bool filter_factory_base::connection_sig() {
-  return connection_sig_();
+void filter_factory_base::connection_sig() {
+  auto k_conn = g_reg()
+                    ->ctx<core_sig>()
+                    .project_begin_open.connect(
+                        [&](const std::filesystem::path&) {
+                          this->is_disabled = true;
+                        });
+  p_i->p_conns.push_back(std::move(k_conn));
+  k_conn = g_reg()
+               ->ctx<core_sig>()
+               .project_end_open.connect(
+                   [&](const entt::handle&, const doodle::project&) {
+                     this->is_disabled = false;
+                     p_i->need_init    = true;
+                   });
+  p_i->p_conns.push_back(std::move(k_conn));
 }
 }  // namespace gui
 
@@ -412,117 +111,45 @@ class time_filter : public gui::filter_base {
   };
 };
 
-class season_filter_factory : public gui::filter_factory_base {
+class season_filter_factory : public gui::filter_factory_t<season> {
  public:
-  using data_type = season;
-  using gui_cache = gui::details::gui_cache<data_type>;
-
-  std::unique_ptr<gui::filter_base> make_filter_() override {
-    if (p_cur_select) {
-      return std::make_unique<gui::filter<data_type>>(p_cur_select->data);
-    } else {
-      return {};
-    }
-  }
-
-  bool refresh_() {
-    for (auto&& i : p_obs) {
-      auto k_h = make_handle();
-      p_edit.emplace_back(k_h.get<data_type>());
-    }
-    boost::unique_erase(boost::sort(p_edit));
-  }
-
- public:
-  std::vector<gui_cache> p_edit;
-  gui_cache* p_cur_select;
-
-  std::string select_name{"null"};
-
   bool render() {
     dear::Combo{"季数", select_name.c_str()} && [&]() {
       for (auto&& i : p_edit) {
         if (ImGui::Selectable(i.name_id.c_str())) {
           select_name  = i.name;
-          p_cur_select = &i;
+          p_cur_select = i;
+          is_edit      = true;
         }
       }
     };
   }
 };
 
-class episodes_filter_factory : public gui::filter_factory_base {
+class episodes_filter_factory : public gui::filter_factory_t<episodes> {
  public:
-  using data_type = episodes;
-  using gui_cache = gui::details::gui_cache<data_type>;
-
-  std::unique_ptr<gui::filter_base> make_filter_() override {
-    if (p_cur_select) {
-      return std::make_unique<gui::filter<data_type>>(p_cur_select->data);
-    } else {
-      return {};
-    }
-  }
-
-  bool refresh_() {
-    for (auto&& i : p_obs) {
-      auto k_h = make_handle();
-      p_edit.emplace_back(k_h.get<data_type>());
-    }
-    boost::unique_erase(boost::sort(p_edit));
-  }
-
- public:
-  std::vector<gui_cache> p_edit;
-  gui_cache* p_cur_select;
-
-  std::string select_name{"null"};
-
   bool render() {
     dear::Combo{"集数", select_name.c_str()} && [&]() {
       for (auto&& i : p_edit) {
         if (ImGui::Selectable(i.name_id.c_str())) {
           select_name  = i.name;
-          p_cur_select = &i;
+          p_cur_select = i;
+          is_edit      = true;
         }
       }
     };
   }
 };
 
-class shot_filter_factory : public gui::filter_factory_base {
+class shot_filter_factory : public gui::filter_factory_t<shot> {
  public:
-  using data_type = shot;
-  using gui_cache = gui::details::gui_cache<data_type>;
-
-  std::unique_ptr<gui::filter_base> make_filter_() override {
-    if (p_cur_select) {
-      return std::make_unique<gui::filter<data_type>>(p_cur_select->data);
-    } else {
-      return {};
-    }
-  }
-
-  bool refresh_() {
-    for (auto&& i : p_obs) {
-      auto k_h = make_handle();
-      p_edit.emplace_back(k_h.get<data_type>());
-    }
-    boost::unique_erase(boost::sort(p_edit));
-  }
-
- public:
-  std::vector<gui_cache> p_edit;
-  gui_cache* p_cur_select;
-
-  std::string select_name{"null"};
-
   bool render() {
     dear::Combo{"镜头", select_name.c_str()} && [&]() {
       for (auto&& i : p_edit) {
         if (ImGui::Selectable(i.name_id.c_str())) {
           select_name  = i.name;
-          p_cur_select = &i;
+          p_cur_select = i;
+          is_edit      = true;
         }
       }
     };
@@ -531,30 +158,34 @@ class shot_filter_factory : public gui::filter_factory_base {
 
 class assets_filter_factory : public gui::filter_factory_base {
  public:
-  using data_type = assets;
+    using data_type = assets;
   using gui_cache = gui::details::gui_cache<FSys::path>;
+  std::vector<std::vector<gui_cache>> p_tree;
 
   std::unique_ptr<gui::filter_base> make_filter_() override {
-    if (p_cur_select) {
-      return std::make_unique<path_filter>(p_cur_select->data);
-    } else {
-      return {};
-    }
+    return std::make_unique<path_filter>(p_cur_select.data);
   }
 
-  bool refresh_() {
+  void refresh_() {
     for (auto&& i : p_obs) {
       auto k_h = make_handle();
-      p_edit.emplace_back(k_h.get<data_type>());
+      for (auto&& j : k_h.get<FSys::path>()) {
+      }
     }
     boost::unique_erase(boost::sort(p_edit));
   }
 
  public:
-  std::vector<gui_cache> p_edit;
-  gui_cache* p_cur_select;
+  assets_filter_factory()
+      : p_cur_select(data_type{}),
+        select_name(p_cur_select.name),
+        p_edit() {
+    p_obs.connect(*g_reg(), entt::collector.update<data_type>());
+  }
+  gui_cache p_cur_select;
 
   std::string select_name{"null"};
+  std::vector<gui_cache> p_edit;
 
   bool render() {
   }
@@ -571,6 +202,9 @@ class assets_widget::impl {
  public:
   bool only_rand{false};
   std::vector<boost::signals2::scoped_connection> p_conns;
+
+  std::vector<std::unique_ptr<gui::filter_factory_base>> p_filter_factorys;
+  std::vector<std::unique_ptr<gui::filter_base>> p_filters;
 };
 
 assets_widget::assets_widget()
@@ -590,6 +224,10 @@ void assets_widget::init() {
           [&](const entt::handle&, const doodle::project&) {
             p_impl->only_rand = false;
           }));
+  p_impl->p_filter_factorys.emplace_back(std::make_unique<season_filter_factory>());
+  p_impl->p_filter_factorys.emplace_back(std::make_unique<episodes_filter_factory>());
+  p_impl->p_filter_factorys.emplace_back(std::make_unique<shot_filter_factory>());
+  // p_impl->p_filter_factorys.emplace_back(std::make_unique<assets_filter_factory>());
 }
 void assets_widget::succeeded() {
 }
