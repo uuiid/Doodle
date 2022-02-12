b5da41ab5009d94e72c4fe20ffef06260560a213
uuiid <957714080@qq.com>
2022-02-12 16:21:27 +0800

    添加treee node容器

--- src/doodle_lib/core/tree_node.h
+++ src/doodle_lib/core/tree_node.h
@@ -4,6 +4,53 @@
 
 namespace doodle {
 
+namespace details {
+template <typename It>
+class iterable_adaptor final {
+ public:
+  /**
+   * @brief 获取迭代器返回值
+   *
+   */
+  using value_type     = typename std::iterator_traits<It>::value_type;
+  /**
+   * @brief 迭代器别名
+   *
+   */
+  using iterator       = It;
+  /**
+   * @brief 常量迭代器
+   *
+   */
+  using const_iterator = const iterator;
+
+  iterable_adaptor()   = default;
+  iterable_adaptor(iterator from, iterator to)
+      : first{from},
+        last{to} {}
+
+  [[nodiscard]] const_iterator begin() const ENTT_NOEXCEPT {
+    return first;
+  }
+
+  [[nodiscard]] const_iterator end() const ENTT_NOEXCEPT {
+    return last;
+  }
+
+  [[nodiscard]] const_iterator cbegin() const ENTT_NOEXCEPT {
+    return begin();
+  }
+
+  [[nodiscard]] const_iterator cend() const ENTT_NOEXCEPT {
+    return end();
+  }
+
+ private:
+  iterator first;
+  iterator last;
+}
+}  // namespace details
+
 template <class T>
 class DOODLELIB_API tree_node {
  public:
@@ -11,6 +58,10 @@ class DOODLELIB_API tree_node {
   using data_type_ptr       = data_type*;
   using data_type_ref       = data_type&;
   using const_data_type_ref = const data_type&;
+
+  tree_node* parent;
+  std::vector<std::shared_ptr<tree_node>> child;
+  T data;
 
   struct iterator_up {
     using iterator_category = std::forward_iterator_tag;
@@ -41,11 +92,41 @@ class DOODLELIB_API tree_node {
     tree_node m_ptr;
   };
 
-  
+
+  tree_node(T in_data)
+      : parent(),
+        child(),
+        data(std::move(in_data)) {}
+
+  tree_node() : tree_node(T{}){};
+
+  bool is_root() {
+    return parent == nullptr;
+  }
+
+  details::iterable_adaptor<iterator_up> each_up() {
+    return {this, nullptr};
+  }
+
+  details::iterable_adaptor<decltype(child)::iterator> each_child() {
+    return {child.begin(), child.end()};
+  }
+
+  details::iterable_adaptor<decltype(child)::iterator> each_current_layer() {
+    if (is_root())
+      return {};
+    else
+      return {parent->child.begin(), parent->child.end()};
+  }
+
+
+  constexpr operator const T&() const {
+    return data;
+  }
+  constexpr operator T&() {
+    return data;
+  }
 
  private:
-  tree_node* parent;
-  std::vector<std::shared_ptr<tree_node>> child;
-  T data;
 };
 }  // namespace doodle
