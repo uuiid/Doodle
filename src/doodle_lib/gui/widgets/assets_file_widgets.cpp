9b43e6f7ee936385802eb6c92366e7732f1b4088
uuiid <957714080@qq.com>
2022-02-16 16:23:18 +0800

    更新显示 方式

--- src/doodle_lib/gui/widgets/assets_file_widgets.cpp
+++ src/doodle_lib/gui/widgets/assets_file_widgets.cpp
@@ -16,12 +16,55 @@
 #include <doodle_lib/long_task/drop_file_data.h>
 #include <doodle_lib/core/core_sig.h>
 
+#include <gui/gui_ref/ref_base.h>
+
 namespace doodle {
 
 class assets_file_widgets::impl {
  public:
   boost::signals2::scoped_connection p_sc;
   std::vector<entt::handle> handle_list;
+
+  using cache_image  = gui::gui_cache<std::shared_ptr<void>>;
+  using cache_name   = gui::gui_cache<std::string>;
+  using cache_select = gui::gui_cache<bool>;
+  class data {
+   public:
+    entt::handle handle_;
+    cache_image image;
+    cache_name name;
+    cache_select select;
+
+    explicit data(const entt::handle& in_h)
+        : handle_(in_h),
+          image("image"s, nullptr),
+          name("null"s, "name"s),
+          select(std::string{}, false) {
+      if (handle_.all_of<assets_file>()) {
+        /// @brief 渲染名称
+        name = handle_.get<assets_file>().show_str();
+      } else {
+        /// @brief 否则渲染id
+        if (handle_.all_of<database>())
+          name = handle_.get<database>().get_id_str();
+      }
+    };
+
+    void load_image() {
+      image_loader k_load{};
+      if (handle_.any_of<image_icon>()) {
+        /// @brief 如果有图标就渲染
+        auto&& k_icon = handle_.get<image_icon>();
+        if (!k_icon.image)
+          k_load.load(handle_);
+        image = k_icon.image;
+      } else {
+        /// @brief 否则默认图标
+        image = k_load.default_image();
+      }
+    };
+  };
+  std::vector<data> lists;
 };
 
 void assets_file_widgets::set_select(const entt::handle& in) {
@@ -39,6 +82,10 @@ void assets_file_widgets::init() {
                   .filter_handle.connect(
                       [this](const std::vector<entt::handle>& in) {
                         p_i->handle_list = in;
+                        p_i->lists.clear();
+                        boost::transform(in, std::back_inserter(p_i->lists), [](const entt::handle& in) -> impl::data {
+                          return impl::data{in};
+                        });
                       });
 }
 void assets_file_widgets::succeeded() {
@@ -51,7 +98,7 @@ void assets_file_widgets::aborted() {
   g_reg()->unset<assets_file_widgets&>();
 }
 void assets_file_widgets::update(chrono::duration<chrono::system_clock::rep, chrono::system_clock::period>, void* data) {
-  if (p_i->handle_list.empty())
+  if (p_i->lists.empty())
     return;
 
   const static auto l_size{5u};
@@ -60,53 +107,22 @@ void assets_file_widgets::update(chrono::duration<chrono::system_clock::rep, chr
   image_loader k_load{};
 
   ImGuiListClipper clipper{};
-  clipper.Begin((boost::numeric_cast<std::int32_t>(p_i->handle_list.size()) / l_size) + 1);
+  clipper.Begin((boost::numeric_cast<std::int32_t>(p_i->lists.size()) / l_size) + 1);
   while (clipper.Step()) {
     for (int l_i = clipper.DisplayStart; l_i < clipper.DisplayEnd; ++l_i) {
       for (int l_j = 0; l_j < l_size; ++l_j) {
-        if ((l_i * l_size + l_j) < p_i->handle_list.size()) {
-          auto&& i = p_i->handle_list[l_i * l_size + l_j];
-          std::shared_ptr<void> l_image{};
-          std::string l_name{};
-          if (i.any_of<image_icon>()) {
-            /// @brief 如果有图标就渲染
-            auto&& k_icon = i.get<image_icon>();
-            if (!k_icon.image)
-              k_load.load(i);
-            l_image = k_icon.image;
-          } else {
-            l_image = k_load.default_image();
-            /// @brief 否则默认图标
-          }
-          if (i.all_of<assets_file>()) {
-            /// @brief 渲染名称
-            l_name = i.get<assets_file>().show_str();
-          } else {
-            /// @brief 否则渲染id
-            if (i.all_of<database>())
-              l_name = i.get<database>().get_id_str();
-          }
-          dear::IDScope(magic_enum::enum_integer(i.entity())) && [&]() {
-            if (imgui::ImageButton(l_image.get(), {64.f, 64.f})) {
-              p_current_select = i;
-              g_reg()->ctx<core_sig>().select_handle(p_current_select);
-            }
-            dear::PopupContextItem{} && [this, i]() {
-              render_context_menu(i);
-            };
-            dear::Text(l_name);
+        if ((l_i * l_size + l_j) < p_i->lists.size()) {
+          auto&& i   = p_i->lists[l_i * l_size + l_j];
+
+          auto l_pos = ImGui::GetCursorPos();
+          ImGui::Selectable(i.select.name_id.c_str(), &i.select.data, ImGuiSelectableFlags_AllowItemOverlap, {66.f, 66.f});
+          dear::PopupContextItem{} && [this, i]() {
+            render_context_menu(i.handle_);
           };
+          ImGui::SetCursorPos(l_pos);
+          ImGui::Image(i.image.data.get(), {64.f, 64.f});
+          dear::Text(i.name);
         }
-        // else {
-        //   dear::IDScope(l_j) && [&]() {
-        //     if (imgui::ImageButton(k_load.default_image().get(), {64.f, 64.f})) {
-        //     }
-        //     dear::PopupContextItem{} && [this]() {
-        //       DOODLE_LOG_INFO("ok");
-        //     };
-        //     dear::Text("null");
-        //   };
-        // }
         imgui::NextColumn();
       }
     }
