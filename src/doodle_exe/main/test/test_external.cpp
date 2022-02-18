5f45302b80f53c7a4812ce39fa0f27d785f76650
uuiid <957714080@qq.com>
2022-02-18 11:24:02 +0800

    添加新测试

--- src/doodle_exe/main/test/test_external.cpp
+++ src/doodle_exe/main/test/test_external.cpp
@@ -9,47 +9,47 @@ TEST_CASE("date time", "[time]") {
   using namespace doodle;
   date::current_zone();
 }
-#include <boost/type_erasure/any.hpp>
-#include <boost/type_erasure/any_cast.hpp>
-#include <boost/type_erasure/builtin.hpp>
-#include <boost/type_erasure/free.hpp>
-#include <boost/type_erasure/member.hpp>
-#include <boost/type_erasure/operators.hpp>
-BOOST_TYPE_ERASURE_MEMBER(push_back)
+//#include <boost/type_erasure/any.hpp>
+//#include <boost/type_erasure/any_cast.hpp>
+//#include <boost/type_erasure/builtin.hpp>
+//#include <boost/type_erasure/free.hpp>
+//#include <boost/type_erasure/member.hpp>
+//#include <boost/type_erasure/operators.hpp>
+// BOOST_TYPE_ERASURE_MEMBER(push_back)
+//
+// class test_to_entt {
+//  std::int32_t p_t;
+//
+// public:
+//  bool render() {
+//    using namespace doodle;
+//    auto k_h = make_handle(*this);
+//    REQUIRE(k_h);
+//    return true;
+//  }
+//};
+//
+// TEST_CASE("type_erasure", "[boost]") {
+//  using namespace doodle;
+//  auto reg  = g_reg();
+//  auto k_h  = make_handle(reg->create());
+//  auto& k_w = k_h.emplace<test_to_entt>(test_to_entt{});
+//  REQUIRE(to_entity(k_w) == k_h.entity());
+//  k_w.render();
+//  std::any k_any;
+//  entt::enum_as_bitmask<metadata_type>{};
+//}
 
-class test_to_entt {
-  std::int32_t p_t;
-
- public:
-  bool render() {
-    using namespace doodle;
-    auto k_h = make_handle(*this);
-    REQUIRE(k_h);
-    return true;
-  }
-};
-
-TEST_CASE("type_erasure", "[boost]") {
-  using namespace doodle;
-  auto reg  = g_reg();
-  auto k_h  = make_handle(reg->create());
-  auto& k_w = k_h.emplace<test_to_entt>(test_to_entt{});
-  REQUIRE(to_entity(k_w) == k_h.entity());
-  k_w.render();
-  std::any k_any;
-  entt::enum_as_bitmask<metadata_type>{};
-}
-
-struct test_external {
-  std::float_t x;
-  std::float_t y;
-  template <class Archive>
-  void serialize(Archive& ar, const std::uint32_t version) {
-    ar& BOOST_SERIALIZATION_NVP(x);
-    ar& BOOST_SERIALIZATION_NVP(y);
-  };
-};
-BOOST_CLASS_EXPORT(test_external);
+// struct test_external {
+//   std::float_t x;
+//   std::float_t y;
+//   template <class Archive>
+//   void serialize(Archive& ar, const std::uint32_t version) {
+//     ar& BOOST_SERIALIZATION_NVP(x);
+//     ar& BOOST_SERIALIZATION_NVP(y);
+//   };
+// };
+// BOOST_CLASS_EXPORT(test_external);
 
 class serializeion_warp {
  public:
@@ -80,26 +80,26 @@ class serializeion_warp {
 
 void fun(const entt::registry& in_reg, entt::entity in) {}
 
-TEST_CASE("entt load", "[boost]") {
-  using namespace doodle;
-  // auto& set = core_set::getSet();
-  entt::registry reg{};
-
-  auto a = reg.create();
-  reg.emplace<test_external>(a, 1.f, 1.f);
-  auto& k_s = reg.emplace<shot>(a);
-
-  serializeion_warp output;
-
-  reg.destroy(reg.create());
-
-  auto e1 = reg.create();
-  auto e3 = reg.create();
-  reg.emplace<entt::tag<"empty"_hs>>(e3);
-
-  reg.emplace<test_external>(e1, .8f, .0f);
-  entt::snapshot{reg}.entities(output).component<test_external, entt::tag<"empty"_hs>>(output);
-}
+// TEST_CASE("entt load", "[boost]") {
+//   using namespace doodle;
+//   // auto& set = core_set::getSet();
+//   entt::registry reg{};
+//
+//   auto a = reg.create();
+//   reg.emplace<test_external>(a, 1.f, 1.f);
+//   auto& k_s = reg.emplace<shot>(a);
+//
+//   serializeion_warp output;
+//
+//   reg.destroy(reg.create());
+//
+//   auto e1 = reg.create();
+//   auto e3 = reg.create();
+//   reg.emplace<entt::tag<"empty"_hs>>(e3);
+//
+//   reg.emplace<test_external>(e1, .8f, .0f);
+//   entt::snapshot{reg}.entities(output).component<test_external, entt::tag<"empty"_hs>>(output);
+// }
 
 template <typename Type>
 [[nodiscard]] constexpr auto stripped_type_name() ENTT_NOEXCEPT {
@@ -123,8 +123,10 @@ TEST_CASE("boost combine") {
 
   std::cout << " boost::combine(list_1, list_2):"
             << "\n";
-  for (auto [tup1, tup2] : boost::combine(list_1, list_2))
-    std::cout << tup1 << " ," << tup2 << "\n";
+  //  for (auto [tup1, tup2] : boost::combine(list_1, list_2))
+  //    std::cout << tup1 << " ," << tup2 << "\n";
+  for (auto it : boost::combine(list_1, list_2))
+    std::cout << it.get_head() << "\n";
 
   //  std::cout << " boost::zip(list_1, list_2):"
   //            << "\n";
