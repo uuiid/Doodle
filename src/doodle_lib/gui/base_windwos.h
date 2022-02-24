7f20dd1af0554c3cfc67de5774fe4a6397deeb35
uuiid <957714080@qq.com>
2022-02-24 16:48:30 +0800

    doc

--- src/doodle_lib/gui/base_windwos.h
+++ src/doodle_lib/gui/base_windwos.h
@@ -19,32 +19,68 @@ class DOODLELIB_API base_window : public process_t<base_window<Panel>> {
   using base_type    = process_t<base_window<Panel>>;
   using derived_type = Panel;
 
+  /**
+   * @brief Construct a new base window object
+   *
+   * @tparam Args
+   * @param args 面板构造函数转发
+   */
   template <class... Args>
   explicit base_window(Args&&... args)
       : panel_(std::forward<Args>(args)...) {}
 
+  /**
+   * @brief 面板渲染属性
+   *
+   */
   Panel panel_;
+  /**
+   * @brief 是否显示面板
+   *
+   */
   bool show{true};
+  /**
+   * @brief 初始化函数， 将自身添加到上下文中，并将显示值写入 coreset
+   *
+   */
   [[maybe_unused]] virtual void init() {
     g_reg()->template set<self_type&>(*this);
     core_set::getSet().widget_show[std::string{panel_.name}] = show;
     panel_.init();
   }
+  /**
+   * @brief 成功后调用
+   *
+   */
   [[maybe_unused]] virtual void succeeded() {
     g_reg()->template unset<self_type>();
     core_set::getSet().widget_show[std::string{panel_.name}] = show;
     panel_.succeeded();
   }
+  /**
+   * @brief 失败后调用
+   *
+   */
   [[maybe_unused]] virtual void failed() {
     g_reg()->template unset<self_type>();
     core_set::getSet().widget_show[std::string{panel_.name}] = show;
     panel_.failed();
   }
+  /**
+   * @brief 用户停止时调用
+   *
+   */
   [[maybe_unused]] virtual void aborted() {
     g_reg()->template unset<self_type>();
     core_set::getSet().widget_show[std::string{panel_.name}] = show;
     panel_.aborted();
   }
+  /**
+   * @brief 渲染函数
+   *
+   * @param in_dalta 传入的间隔时间
+   * @param in_data 用户数据
+   */
   [[maybe_unused]] virtual void update(typename base_type::delta_type in_dalta, void* in_data) {
     if (!show)
       this->succeed();
@@ -55,19 +91,18 @@ class DOODLELIB_API base_window : public process_t<base_window<Panel>> {
         };
   }
 };
+/**
+ * @brief 创建窗口模板方法
+ *
+ * @tparam Panel 窗口内容类
+ * @tparam Args
+ * @param args 实例化内容类初始化转发
+ * @return auto viod
+ */
 template <class Panel, class... Args>
 auto make_windows(Args&&... args) {
   if (auto k_panel = g_reg()->try_ctx<Panel>(); !k_panel)
     g_main_loop().attach<base_window<Panel>>(std::forward<Args>(args)...);
 }
-
-// BOOST_TYPE_ERASURE_MEMBER(render);
-
-// using widget_ = boost::type_erasure::any<
-//     boost::mpl::vector<
-//         has_render<bool()>,
-//         boost::type_erasure::copy_constructible<>,
-//         boost::type_erasure::typeid_<>,
-//         boost::type_erasure::relaxed>>;
 
 }  // namespace doodle
