ce337b882fbc4ae7017f0e82a3f38cfc43c37bb9
uuiid <957714080@qq.com>
2022-02-25 15:30:47 +0800

    添加基本的函数

--- src/doodle_lib/long_task/image_load_task.cpp
+++ src/doodle_lib/long_task/image_load_task.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "image_load_task.h"
+
+namespace doodle {
+class image_load_task::impl {
+ public:
+};
+image_load_task::image_load_task(const entt::handle &in_handle)
+    : p_i(std::make_unique<impl>()) {
+}
+void image_load_task::init() {
+}
+void image_load_task::succeeded() {
+}
+void image_load_task::failed() {
+}
+void image_load_task::aborted() {
+}
+void image_load_task::update(const chrono::duration<chrono::system_clock::rep, chrono::system_clock::period> &, void *data) {
+}
+image_load_task::~image_load_task() = default;
+}  // namespace doodle
--- src/doodle_lib/long_task/image_load_task.h
+++ src/doodle_lib/long_task/image_load_task.h
@@ -3,8 +3,23 @@
 //
 #pragma once
 
+#include <doodle_lib/doodle_lib_fwd.h>
+
 namespace doodle {
 
-class image_load_task {
+class DOODLELIB_API image_load_task : public process_t<image_load_task> {
+ private:
+  class impl;
+  std::unique_ptr<impl> p_i;
+
+ public:
+  explicit image_load_task(const entt::handle& in_handle);
+  ~image_load_task();
+
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(const delta_type&, void* data);
 };
 }  // namespace doodle
