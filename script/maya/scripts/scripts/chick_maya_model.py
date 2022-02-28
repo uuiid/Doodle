304e3c678d57587047cee17ec7be9607b48a430b
uuiid <957714080@qq.com>
2022-02-28 11:00:36 +0800

    去除无用的文件

--- script/maya/scripts/scripts/chick_maya_model.py
+++ script/maya/scripts/scripts/chick_maya_model.py
@@ -6,24 +6,6 @@ import pymel.core
 import pymel.all
 import maya.mel
 
-# # import inspect
-# # __Doodle__ = os.path.dirname(inspect.getsourcefile(lambda: 0))
-# # __Doodle__ = os.path.dirname(os.path.dirname(__Doodle__))
-# # print("add path {}".format(__Doodle__))
-# # sys.path.append(__Doodle__)
-# import argsTools
-
-# print("{} {}".format(argsTools.ARGS.path,
-#                      argsTools.ARGS.exportpath))
-# # maya.cmds.file(new=True, force=True)
-# # maya.cmds.file(os.path.join(args.path), o=True)
-
-# if os.path.exists(os.path.dirname(argsTools.ARGS.path) + "/workspace.mel"):
-#     pymel.all.workspace.open(os.path.dirname(argsTools.ARGS.path))
-#     pymel.all.workspace.save()
-
-# pymel.core.newFile(force=True, type="mayaAscii")
-# pymel.core.openFile(os.path.abspath(argsTools.ARGS.path), force=True)
 
 
 class materal():
