8cd840097bf547b88994da62e11d3d877ebe348e
uuiid <957714080@qq.com>
2022-02-28 11:01:49 +0800

    去除无用的文件

--- script/tools.ps1
+++
@@ -1,7 +0,0 @@
-Enter-PSSession -computer  192.168.20.60 -Credential dev
-
-Invoke-Command -ComputerName  192.168.20.60 -Credential dev -FilePath C:\Users\TD\Source\Doodle\script\install_rpc_service.ps1
-
-Exit-PSSession
-
-New-Item -Path "D:\Doodle\data" -Name "dubuxiaoyao" -ItemType "SymbolicLink" -Value "\\192.168.10.250\public\DuBuXiaoYao_3"
