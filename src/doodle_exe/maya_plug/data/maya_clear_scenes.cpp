339232c0e6efc9f675c650c07daef601847753b5
uuiid <957714080@qq.com>
2022-02-28 13:39:43 +0800

    添加场景清理类

---
+++ src/doodle_exe/maya_plug/data/maya_clear_scenes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2022/2/28.
+//
+
+#include "maya_clear_scenes.h"
---
+++ src/doodle_exe/maya_plug/data/maya_clear_scenes.h
@@ -0,0 +1,13 @@
+//
+// Created by TD on 2022/2/28.
+//
+
+#pragma once
+
+#include <maya_plug/maya_plug_fwd.h>
+namespace doodle::maya_plug {
+class maya_clear_scenes {
+ public:
+
+};
+}  // namespace doodle::maya_plug
