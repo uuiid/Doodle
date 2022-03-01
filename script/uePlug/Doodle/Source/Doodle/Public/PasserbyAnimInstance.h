1ccbb531aeb598a83f953c26aa9a710f54e415f6
uuiid <957714080@qq.com>
2022-03-01 14:18:31 +0800

    添加ue4 动画实例类

---
+++ script/uePlug/Doodle/Source/Doodle/Public/PasserbyAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PasserbyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOODLE_PLUG_API UPasserbyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
