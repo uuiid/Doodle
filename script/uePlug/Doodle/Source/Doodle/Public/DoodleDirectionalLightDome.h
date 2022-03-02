2563fb81a7b0d62a89e527bb12a48cf06e922e8d
uuiid <957714080@qq.com>
2022-03-02 12:22:22 +0800

    ue4 将灯光属性公开到定序器

--- script/uePlug/Doodle/Source/Doodle/Public/DoodleDirectionalLightDome.h
+++ script/uePlug/Doodle/Source/Doodle/Public/DoodleDirectionalLightDome.h
@@ -1,4 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project
+// Settings.
 
 #pragma once
 
@@ -12,85 +13,100 @@
 #include "DoodleDirectionalLightDome.generated.h"
 class UDirectionalLightComponent;
 UCLASS()
-class DOODLE_API ADoodleDirectionalLightDome : public AActor
-{
+class DOODLE_API ADoodleDirectionalLightDome : public AActor {
   GENERATED_BODY()
 
-public:
+ public:
   // Sets default values for this actor's properties
   ADoodleDirectionalLightDome();
   // 经度曲线  竖线
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "longitudeCurve")
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "longitudeCurve")
   FRuntimeFloatCurve p_longitude;
 
   // 纬度曲线  横线
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "latitudeCurve", meta = (UIMin = "0"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "latitudeCurve", meta = (UIMin = "0"))
   FRuntimeFloatCurve p_latitude;
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "light max", meta = (UIMin = "0"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "light max", meta = (UIMin = "0"))
   float p_light_max;
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "light min", meta = (UIMin = "0"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "light min", meta = (UIMin = "0"))
   float p_light_min;
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "night", meta = (UIMin = "0"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "night", meta = (UIMin = "0"))
   float p_night;
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "specular min", meta = (UIMin = "0"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "specular min", meta = (UIMin = "0"))
   float p_specular_min;
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "投射阴影")
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "投射阴影")
   bool p_castShadow;
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "阴影量", meta = (UIMin = "0", UIMax = "1"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "阴影量", meta = (UIMin = "0", UIMax = "1"))
   float p_shadowAmout;
 
   // 镜像强度映射
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "light to specular", meta = (UIMin = "0"))
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle_Light",
+            DisplayName = "light to specular", meta = (UIMin = "0"))
   FRuntimeFloatCurve p_specular_curve;
 
-  /** 
-	 * Angle subtended by light source in degrees (also known as angular diameter).
-	 * Defaults to 0.5357 which is the angle for our sun.
-	 */
-  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Light, meta = (UIMin = "0", UIMax = "5"), DisplayName = "Source Angle")
+  /**
+   * Angle subtended by light source in degrees (also known as angular
+   * diameter). Defaults to 0.5357 which is the angle for our sun.
+   */
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Interp, Category = Light,
+            meta = (UIMin = "0", UIMax = "5"), DisplayName = "Source Angle")
   float LightSourceAngle;
 
-  /** 
-	 * Angle subtended by soft light source in degrees.
-	 */
-  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Light, meta = (UIMin = "0", UIMax = "5"), DisplayName = "Source Soft Angle")
+  /**
+   * Angle subtended by soft light source in degrees.
+   */
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Interp, Category = Light,
+            meta = (UIMin = "0", UIMax = "5"),
+            DisplayName = "Source Soft Angle")
   float LightSourceSoftAngle;
 
-  /** 
-	 * Channels that this light should affect.  
-	 * These channels only apply to opaque materials, direct lighting, and dynamic lighting and shadowing.
-	 */
+  /**
+   * Channels that this light should affect.
+   * These channels only apply to opaque materials, direct lighting, and dynamic
+   * lighting and shadowing.
+   */
   UPROPERTY(EditAnywhere, AdvancedDisplay, BlueprintReadOnly, Category = Light)
   FLightingChannels LightingChannels;
 
-  /** 
-	 * Filter color of the light.
-	 * Note that this can change the light's effective intensity.
-	 */
-  UPROPERTY(BlueprintReadOnly, interp, Category = Light, meta = (HideAlphaChannel))
+  /**
+   * Filter color of the light.
+   * Note that this can change the light's effective intensity.
+   */
+  UPROPERTY(BlueprintReadOnly, interp, Category = Light,
+            meta = (HideAlphaChannel))
   FColor LightColor;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle_Light", DisplayName = "lightArray")
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle_Light",
+            DisplayName = "lightArray")
   TArray<UDirectionalLightComponent *> p_array_light;
 
 #if WITH_EDITOR
-  void PostEditChangeProperty(FPropertyChangedEvent &PropertyChangeEvent) override;
-#endif //WITH_EDITOR
+  void PostEditChangeProperty(
+      FPropertyChangedEvent &PropertyChangeEvent) override;
+#endif  // WITH_EDITOR
 
-protected:
+ protected:
   // Called when the game starts or when spawned
   virtual void BeginPlay() override;
 
-private:
+ private:
   void set_light();
 
-public:
+ public:
   // Called every frame
   // virtual void Tick(float DeltaTime) override;
 };
