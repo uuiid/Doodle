4a82801fb701a313bc7af22fb1a4d51a7d5781b6
uuiid <957714080@qq.com>
2022-03-02 12:21:26 +0800

    添加新的火光

---
+++ script/uePlug/Doodle/Source/Doodle/Public/DoodleFireLight.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+//#include "Animation/AnimInstance.h"
+#include "DoodleConfigLightActor.h"
+#include "DoodleFireLight.generated.h"
+
+UCLASS()
+class DOODLE_API ADoodleFireLight : public ADoodleConfigLightActor {
+ public:
+  GENERATED_BODY()
+
+};
