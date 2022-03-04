f2a6b331983eb9e97b449bc897964690b1e76bea
uuiid <957714080@qq.com>
2022-03-04 17:58:44 +0800

    [maya_plug] 添加寻找重复模型的类

---
+++ src/doodle_exe/maya_plug/data/find_duplicate_poly.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2022/3/4.
+//
+
+#include "find_duplicate_poly.h"
---
+++ src/doodle_exe/maya_plug/data/find_duplicate_poly.h
@@ -0,0 +1,11 @@
+//
+// Created by TD on 2022/3/4.
+//
+
+#pragma once
+
+
+
+class find_duplicate_poly {
+};
+
