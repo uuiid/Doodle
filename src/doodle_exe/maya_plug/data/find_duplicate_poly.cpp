4274dee9624144483546efab4d16410684e2788a
uuiid <957714080@qq.com>
2022-03-05 14:26:54 +0800

    [maya_plug] 添加maya新的寻找相同网格的方法

--- src/doodle_exe/maya_plug/data/find_duplicate_poly.cpp
+++ src/doodle_exe/maya_plug/data/find_duplicate_poly.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "find_duplicate_poly.h"
+namespace doodle::maya_plug {
+
+std::vector<std::pair<MObject, MObject>> find_duplicate_poly::operator()(const MObjectArray& in_array) {
+  std::vector<std::pair<MObject, MObject>> l_vector{};
+  return l_vector;
+}
+}  // namespace doodle::maya_plug
