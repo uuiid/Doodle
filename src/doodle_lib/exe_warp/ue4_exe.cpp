fbecfba3aeb2b5f4da366d6db57560f0a28cef4d
uuiid <957714080@qq.com>
2022-03-09 17:35:47 +0800

    添加ue4过程

--- src/doodle_lib/exe_warp/ue4_exe.cpp
+++ src/doodle_lib/exe_warp/ue4_exe.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "ue4_exe.h"
+#include <thread_pool/process_message.h>
+#include <doodle_lib/core/filesystem_extend.h>
+#include <doodle_lib/core/core_set.h>
+
+#include <doodle_lib/thread_pool/thread_pool.h>
+namespace doodle {
+namespace ue4_comm {
+
+sequencer_comm::sequencer_comm(
+    const entt::handle &in_handle,
+    std::vector<entt::handle> &in_vector) {
+}
+void sequencer_comm::init() {
+}
+void sequencer_comm::succeeded() {
+}
+void sequencer_comm::failed() {
+}
+void sequencer_comm::aborted() {
+}
+void sequencer_comm::update(const chrono::duration<chrono::system_clock::rep, chrono::system_clock::period> &, void *data) {
+}
+}  // namespace ue4_comm
+}  // namespace doodle
--- src/doodle_lib/exe_warp/ue4_exe.h
+++ src/doodle_lib/exe_warp/ue4_exe.h
@@ -3,7 +3,48 @@
 //
 
 #include <doodle_lib/doodle_lib_fwd.h>
-namespace doodle::details {
+namespace doodle {
+namespace ue4_comm {
+class ue4_comm_str {
+ public:
+  std::string comm;
+};
+
+class DOODLELIB_API sequencer_comm : public process_t<sequencer_comm> {
+ private:
+
+ public:
+  using base_type = process_t<sequencer_comm>;
+  /**
+   * @brief 生成ue4命令，
+   * @param in_handle 具有消息组件的句柄
+   * @param in_vector 传入的具有 episodes 和 shot 组件的句柄列表
+   */
+  explicit sequencer_comm(const entt::handle &in_handle, std::vector<entt::handle> &in_vector);
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(const base_type::delta_type &, void *data);
+};
+
+class DOODLELIB_API import_file_comm : public process_t<import_file_comm> {
+ public:
+  using base_type = process_t<import_file_comm>;
+  /**
+   * @brief 生成ue4命令，
+   * @param in_handle 具有消息组件的句柄
+   * @param in_vector 传入的具有 episodes 和 shot 以及 assets_file 组件的句柄列表
+   */
+  explicit import_file_comm(const entt::handle &in_handle, std::vector<entt::handle> &in_vector);
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(const base_type::delta_type &, void *data);
+};
+}  // namespace ue4_comm
+
 /**
  * @brief
  *
@@ -15,24 +56,48 @@ class DOODLELIB_API ue4_exe : public process_t<ue4_exe> {
  public:
   using base_type = process_t<ue4_exe>;
 
+  class DOODLELIB_API import_file {
+   public:
+    entt::handle handle_;
+    FSys::path in_path;
+  };
+
+  /**
+   * @brief 使用 ue4 运行任意命令
+   * @param in_handle 具有消息组件 和 命令组件 的句柄
+   */
+  explicit ue4_exe(const entt::handle &in_handle);
   /**
    * @brief 使用 ue4 运行任意命令
    * @param in_handle 具有消息组件的的句柄
-   * @param in_file 传入的命令(不要包括 ue4.exe)
+   * @param in_comm 传入的命令(不要包括 ue4.exe)
    */
-  explicit ue4_exe(const entt::handle &in_handle, const string &in_file);
+  explicit ue4_exe(const entt::handle &in_handle, const string &in_comm);
+
+  /**
+   * @brief 使用 ue4 运行任意命令
+   * @param in_handle 具有消息组件的的句柄
+   * @param in_comm 传入的命令(不要包括 ue4.exe)
+   */
+  explicit ue4_exe(const entt::handle &in_handle, const std::shared_ptr<string> &in_comm);
   /**
    * @brief 创建ue4 关卡序列
-   * @param in_handle 具有消息组件的的句柄
-   * @param in_file 具有 project 和 shot episodes 组件的句柄
-   *
+   * @param in_handle 具有消息组件 和 project 组件的句柄
+   * @param in_vector 传入的导入文件的类列表
    */
-  explicit ue4_exe(const entt::handle &in_handle, const std::vector<entt::handle> &in_file);
+  explicit ue4_exe(const entt::handle &in_handle, const std::vector<import_file> &in_vector);
+
+  /**
+   * @brief 导入文件的命令
+   * @param in_handle 具有消息组件 和 project 和
+   * @param in_vector
+   */
+  //  explicit ue4_exe(const entt::handle &in_handle, const std::vector<entt::handle> &in_vector);
 
   [[maybe_unused]] void init();
   [[maybe_unused]] void succeeded();
   [[maybe_unused]] void failed();
   [[maybe_unused]] void aborted();
-  [[maybe_unused]] void update(base_type::delta_type, void *data);
+  [[maybe_unused]] void update(const base_type::delta_type &, void *data);
 };
-}  // namespace doodle::details
+}  // namespace doodle
