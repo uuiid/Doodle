7d3c5dd218572fe10d53220497c2f3cd3c2b53f9
uuiid <957714080@qq.com>
2022-03-10 09:02:12 +0800

    添加ue4定义

--- src/doodle_lib/exe_warp/ue4_exe.h
+++ src/doodle_lib/exe_warp/ue4_exe.h
@@ -12,7 +12,6 @@ class ue4_comm_str {
 
 class DOODLELIB_API sequencer_comm : public process_t<sequencer_comm> {
  private:
-
  public:
   using base_type = process_t<sequencer_comm>;
   /**
@@ -56,43 +55,11 @@ class DOODLELIB_API ue4_exe : public process_t<ue4_exe> {
  public:
   using base_type = process_t<ue4_exe>;
 
-  class DOODLELIB_API import_file {
-   public:
-    entt::handle handle_;
-    FSys::path in_path;
-  };
-
   /**
    * @brief 使用 ue4 运行任意命令
    * @param in_handle 具有消息组件 和 命令组件 的句柄
    */
   explicit ue4_exe(const entt::handle &in_handle);
-  /**
-   * @brief 使用 ue4 运行任意命令
-   * @param in_handle 具有消息组件的的句柄
-   * @param in_comm 传入的命令(不要包括 ue4.exe)
-   */
-  explicit ue4_exe(const entt::handle &in_handle, const string &in_comm);
-
-  /**
-   * @brief 使用 ue4 运行任意命令
-   * @param in_handle 具有消息组件的的句柄
-   * @param in_comm 传入的命令(不要包括 ue4.exe)
-   */
-  explicit ue4_exe(const entt::handle &in_handle, const std::shared_ptr<string> &in_comm);
-  /**
-   * @brief 创建ue4 关卡序列
-   * @param in_handle 具有消息组件 和 project 组件的句柄
-   * @param in_vector 传入的导入文件的类列表
-   */
-  explicit ue4_exe(const entt::handle &in_handle, const std::vector<import_file> &in_vector);
-
-  /**
-   * @brief 导入文件的命令
-   * @param in_handle 具有消息组件 和 project 和
-   * @param in_vector
-   */
-  //  explicit ue4_exe(const entt::handle &in_handle, const std::vector<entt::handle> &in_vector);
 
   [[maybe_unused]] void init();
   [[maybe_unused]] void succeeded();
