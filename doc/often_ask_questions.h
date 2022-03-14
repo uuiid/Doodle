4fd61841a8172ab77e39dc3629c2739eac580e57
uuiid <957714080@qq.com>
2022-03-14 17:58:09 +0800

    doc

--- doc/often_ask_questions.h
+++ doc/often_ask_questions.h
@@ -15,11 +15,18 @@
  * @li 功能查看 @ref doc/schedule.md "描述"
  * @li 软件工作原理 具体查看文档中的类描述
  *
- * @subsection 软件中的新项目是什么 
+ * @subsection 软件中的新项目是什么
  * @li 你可以认为是一个储存文件的目录， 这个文件记录了软件本身产生的数据
  *
- * @subsection 如何查询计算时间 
+ * @subsection 如何查询计算时间
  * @li 按照选中的实体， 将他们按照时间排序， 然后计算两者的时间差
  * @note 选中的物体必须有路径组件的存在才行
+ *
+ *
+ * 计算路径方式:
+ * 项目路径(绝对路径 E:/test/biao 保存的文件的路径) 和 拖入的路径 -> 计算相对路径 -> 相对路径为空
+ * E:/file/image.png 和 F:/tmp 相对 -> 空
+ * E:/file/image.png 和 E:/tmp 相对 -> ../image.png
+ *
  *
  */
