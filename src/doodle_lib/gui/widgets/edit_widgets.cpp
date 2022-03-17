f11e75cd2fbddfeaaf9fd08fe586b98f72f7de45
uuiid <957714080@qq.com>
2022-03-17 10:55:43 +0800

    优化路径计算

--- src/doodle_lib/gui/widgets/edit_widgets.cpp
+++ src/doodle_lib/gui/widgets/edit_widgets.cpp
@@ -313,7 +313,7 @@ class add_assets_for_file : public base_render {
                     auto k_h = make_handle();
                     FSys::path l_path{in_path};
                     auto l_prj_path = g_reg()->ctx<project>().p_path;
-                    if (!use_abs_path.data && l_prj_path.root_path() == in_path.root_path()) {
+                    if (!use_abs_path.data || l_prj_path.root_path() == in_path.root_path()) {
                       l_path = in_path.lexically_relative(g_reg()->ctx<project>().p_path);
                     }
 
