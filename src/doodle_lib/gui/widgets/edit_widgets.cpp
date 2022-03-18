01e8fc2718f20ed1b1f1d5113affc661765a3792
uuiid <957714080@qq.com>
2022-03-18 10:33:08 +0800

    添加等级编辑

--- src/doodle_lib/gui/widgets/edit_widgets.cpp
+++ src/doodle_lib/gui/widgets/edit_widgets.cpp
@@ -21,6 +21,7 @@
 #include <doodle_lib/metadata/assets_file.h>
 #include <doodle_lib/metadata/metadata_cpp.h>
 #include <doodle_lib/metadata/image_icon.h>
+#include <doodle_lib/metadata/importance.h>
 #include <doodle_lib/metadata/project.h>
 #include <core/tree_node.h>
 namespace doodle {
@@ -297,6 +298,26 @@ class time_edit : public gui::edit_interface {
 };
 
 namespace gui {
+class importance_edit : public edit_interface {
+  gui_cache<std::string> p_importance;
+
+ public:
+  importance_edit()
+      : p_importance("等级"s, ""s) {}
+  void init_(const entt::handle &in) override {
+    if (in.any_of<importance>())
+      p_importance.data = in.get<importance>().cutoff_p;
+  }
+  void render(const entt::handle &in) override {
+    if (ImGui::InputText(*p_importance.gui_name, &p_importance.data)) {
+      set_modify(true);
+    }
+  }
+  void save_(const entt::handle &in) const override {
+    in.emplace_or_replace<importance>(p_importance.data);
+  }
+};
+
 class command_edit : public edit_interface {
   gui_cache<std::string> p_command;
 
@@ -524,6 +545,7 @@ edit_widgets::edit_widgets()
   p_i->p_edit.emplace_back("镜头编辑"s, std::make_unique<shot_edit>());
   p_i->p_edit.emplace_back("文件编辑"s, std::make_unique<assets_file_edit>());
   p_i->p_edit.emplace_back("备注"s, std::make_unique<gui::command_edit>());
+  p_i->p_edit.emplace_back("等级"s, std::make_unique<gui::importance_edit>());
   auto *l_edit     = p_i->p_edit.emplace_back("资产类别"s, std::make_unique<gui::assets_edit>()).data.get();
 
   p_i->assets_edit = dynamic_cast<gui::assets_edit *>(l_edit);
