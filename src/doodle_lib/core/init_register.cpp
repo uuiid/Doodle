585866958f9b66a65c71241288d332a9abb8e617
uuiid <957714080@qq.com>
2022-03-21 16:44:44 +0800

    更新初始化函数

--- src/doodle_lib/core/init_register.cpp
+++ src/doodle_lib/core/init_register.cpp
@@ -1,5 +1,5 @@
 #include "init_register.h"
-
+#include <long_task/process_pool.h>
 namespace doodle {
 
 std::multimap<std::int32_t, std::function<void()>>& init_register::registered_functions() {
@@ -7,9 +7,18 @@ std::multimap<std::int32_t, std::function<void()>>& init_register::registered_fu
   return rel;
 }
 void init_register::begin_init() {
+  auto l_then = g_main_loop().attach<one_process_t>([]() {
+    DOODLE_LOG_INFO("开始初始化队列");
+  });
   for (auto&& i : registered_functions()) {
-    i.second();
+    l_then = l_then.then<one_process_t>([&]() {
+      DOODLE_LOG_INFO("初始化优先级 {}", i.first);
+      i.second();
+    });
   }
+  l_then.then<one_process_t>([&]() {
+    DOODLE_LOG_INFO("结束初始化");
+  });
 }
 init_register::init_register()  = default;
 init_register::~init_register() = default;
