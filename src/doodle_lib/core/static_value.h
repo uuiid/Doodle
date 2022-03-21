73f888168c729088b073e6678768d4591601f843
uuiid <957714080@qq.com>
2022-03-21 10:55:57 +0800

    更新默认字体

--- src/doodle_lib/core/static_value.h
+++ src/doodle_lib/core/static_value.h
@@ -10,5 +10,5 @@ constexpr const std::string_view ue_path_obj{"Engine/Binaries/Win64/UE4Editor.ex
 constexpr const std::string_view drop_imgui_id{"drop_imgui_id"};
 constexpr const std::string_view drop_handle_list{"drop_handle_list"};
 constexpr const std::size_t rand_block{256};
-constexpr const std::string_view font_default{R"(C:/Windows/Fonts/simhei.ttf)"};
+constexpr const std::string_view font_default{R"(C:/Windows/Fonts/simkai.ttf)"};
 }  // namespace doodle::doodle_config
