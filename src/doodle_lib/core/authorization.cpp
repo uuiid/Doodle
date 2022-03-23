2272d412efba65892a5cd411a9e3970bbc62f0b0
uuiid <957714080@qq.com>
2022-03-23 17:40:48 +0800

    添加授权类

---
+++ src/doodle_lib/core/authorization.cpp
@@ -0,0 +1,8 @@
+//
+// Created by TD on 2022/3/23.
+//
+
+#include "authorization.h"
+namespace doodle{
+
+}
---
+++ src/doodle_lib/core/authorization.h
@@ -0,0 +1,11 @@
+//
+// Created by TD on 2022/3/23.
+//
+#pragma once
+
+#include <doodle_lib/doodle_lib_fwd.h>
+
+namespace doodle {
+class DOODLELIB_API authorization {
+};
+}  // namespace doodle
