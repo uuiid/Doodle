af41979d4a0975766feb922cce3b84143be57775
uuiid <957714080@qq.com>
2022-03-28 15:18:34 +0800

    更新接口元数据说明符

--- script/uePlug/Doodle/Source/Doodle/Public/UDoodleImportUilt.h
+++ script/uePlug/Doodle/Source/Doodle/Public/UDoodleImportUilt.h
@@ -20,20 +20,20 @@ public:
     UFUNCTION(BlueprintCallable)
     static UDoodleImportUilt *Get();
 
-    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf, HideSelfPin))
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf = "Target", HideSelfPin = "Target"))
     void create_camera(
         const ULevelSequence *in_level,
         const ACineCameraActor *in_camera) const;
 
-    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf, HideSelfPin))
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf = "Target", HideSelfPin = "Target"))
     void add_movie_scene_track(
         const UMovieSceneTrack *in_track,
         FName in_name,
         const FString &in_path) const;
 
-    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf, HideSelfPin))
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf = "Target", HideSelfPin = "Target"))
     bool add_skin_scene(const UAnimSequence *in_anim);
 
-    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf, HideSelfPin))
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, meta = (DefaultToSelf = "Target", HideSelfPin = "Target"))
     bool add_geo_cache_scene(const UAnimSequence *in_anim);
 };
