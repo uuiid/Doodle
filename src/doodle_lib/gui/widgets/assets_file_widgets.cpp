4ff12a8aafe96d288b55e7aee468b418d876521d
uuiid <957714080@qq.com>
2022-03-30 18:39:52 +0800

    添加列渲染

--- src/doodle_lib/gui/widgets/assets_file_widgets.cpp
+++ src/doodle_lib/gui/widgets/assets_file_widgets.cpp
@@ -35,18 +35,26 @@ class assets_file_widgets::impl {
   using cache_image  = gui::gui_cache<std::shared_ptr<void>, image_data>;
   using cache_name   = gui::gui_cache<std::string>;
   using cache_select = gui::gui_cache<bool>;
-  class icon_data {
+
+  class base_data {
    public:
+    explicit base_data(const entt::handle& in_h)
+        : handle_(in_h),
+          select(std::string{}, false) {}
+    virtual ~base_data(){};
     entt::handle handle_;
+    cache_select select;
+  };
+
+  class icon_data : public base_data {
+   public:
     cache_image image;
     cache_name name;
-    cache_select select;
 
     explicit icon_data(const entt::handle& in_h)
-        : handle_(in_h),
+        : base_data(in_h),
           image("image"s, nullptr),
-          name("null"s, "name"s),
-          select(std::string{}, false) {
+          name("null"s, "name"s) {
       this->init(in_h);
     };
 
@@ -112,7 +120,36 @@ class assets_file_widgets::impl {
       }
     };
   };
-  std::vector<icon_data> lists;
+
+  class info_data : public base_data {
+   public:
+    explicit info_data(const entt::handle& in_h)
+        : base_data(in_h) {
+      init(in_h);
+    }
+    template <typename T>
+    void to_s(std::string& in_p, const entt::handle& in_h) {
+      if (in_h.all_of<T>())
+        in_p = fmt::to_string(in_h.get<T>());
+      else
+        in_p = {};
+    }
+
+    void init(const entt::handle& in_h) {
+      id = fmt::to_string(in_h.get<database>().get_id());
+      to_s<assets>(ass_p, in_h);
+      to_s<episodes>(eps_p, in_h);
+      to_s<shot>(shot_p, in_h);
+      to_s<assets_file>(file_path_p, in_h);
+    }
+
+    std::string id;
+    std::string ass_p;
+    std::string eps_p;
+    std::string shot_p;
+    std::string file_path_p;
+  };
+  std::vector<base_data> lists;
   std::size_t select_index{};
 
   // std::float_t windows_width{0};
@@ -126,7 +163,15 @@ class assets_file_widgets::impl {
 
 assets_file_widgets::assets_file_widgets()
     : p_i(std::make_unique<impl>()) {
-  p_i->render_list = [this]() { this->render_by_icon() };
+  this->switch_rander();
+}
+
+void assets_file_widgets::switch_rander() {
+  if (p_i->render_icon) {
+    p_i->render_list = [this]() { this->render_by_icon(); };
+  } else {
+    p_i->render_list = [this]() { this->render_by_info(); };
+  }
 }
 
 void assets_file_widgets::refresh(const std::vector<entt::handle>& in_list) {
@@ -138,12 +183,18 @@ void assets_file_widgets::init() {
   p_i->p_sc.emplace_back(l_sig.filter_handle.connect(
       [this](const std::vector<entt::handle>& in) {
         p_i->handle_list = in;
-        p_i->lists.clear();
-        boost::transform(
-            in, std::back_inserter(p_i->lists),
-            [](const entt::handle& in) -> impl::icon_data {
-              return impl::icon_data{in};
-            });
+        if (p_i->render_icon)
+          p_i->lists =
+              in | ranges::views::transform([](const entt::handle& in) -> impl::base_data {
+                return impl::icon_data{in};
+              }) |
+              ranges::to_vector;
+        else
+          p_i->lists =
+              in | ranges::views::transform([](const entt::handle& in) -> impl::base_data {
+                return impl::info_data{in};
+              }) |
+              ranges::to_vector;
       }));
   p_i->p_sc.emplace_back(l_sig.project_begin_open.connect(
       [&](const std::filesystem::path&) {
@@ -179,55 +230,16 @@ void assets_file_widgets::update(chrono::duration<chrono::system_clock::rep, chr
   /// 渲染数据
   dear::Disabled l_d{p_i->only_rand};
 
-  const static auto l_size{5u};
-  auto k_length                        = (ImGui::GetCurrentWindow()->InnerClipRect.GetWidth() / l_size) - ImGui::GetStyle().ItemInnerSpacing.x * 3;
   const float footer_height_to_reserve = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
   dear::Child{"ScrollingRegion", ImVec2(0, -footer_height_to_reserve), false} && [&]() {
     if (p_i->lists.empty())
       return;
-    if (p_i->render_icon)
-      ImGui::Columns(l_size, "assets_file_widgets", false);
-
-    ImGuiListClipper clipper{};
-    clipper.Begin((boost::numeric_cast<std::int32_t>(p_i->lists.size() / l_size)) + 1);
-    while (clipper.Step()) {
-      for (int l_i = clipper.DisplayStart; l_i < clipper.DisplayEnd; ++l_i) {
-        for (int l_j = 0; l_j < l_size; ++l_j) {
-          if ((l_i * l_size + l_j) < p_i->lists.size()) {
-            std::size_t l_index{l_i * l_size + l_j};
-            auto&& i = p_i->lists[l_index];
-            i.load_image(k_length);
-            auto l_pos_image = ImGui::GetCursorPos();
-
-            ImGui::PushStyleColor(ImGuiCol_Header, (ImVec4)ImColor::HSV(7.0f, 0.6f, 0.8f));
-
-            if (ImGui::Selectable(*i.select.gui_name,
-                                  i.select.data,
-                                  ImGuiSelectableFlags_AllowDoubleClick,
-                                  {k_length, k_length}))
-              set_select(l_index);
-            dear::PopupContextItem{} && [this, i]() {
-              render_context_menu(i.handle_);
-            };
-            ImGui::PopStyleColor();
-
-            dear::DragDropSource{} && [this, l_index]() {
-              this->open_drag(l_index);
-            };
-            auto l_pos_select = ImGui::GetCursorPos();
-            ImGui::SetCursorPos(l_pos_image);
-            ImGui::Image(i.image.data.get(), {i.image.icon_size2d_.width - 2, i.image.icon_size2d_.height - 2});
-            ImGui::SetCursorPos(l_pos_select);
-            dear::Text(i.name);
-          }
-          imgui::NextColumn();
-        }
-      }
-    }
+    p_i->render_list();
   };
 
   if (ImGui::Button(ICON_FA_BATTERY_QUARTER)) {
     p_i->render_icon = !p_i->render_icon;
+    void switch_rander();
   }
   g_reg()->ctx<status_info>().show_size = p_i->lists.size();
 }
@@ -246,30 +258,34 @@ void assets_file_widgets::render_context_menu(const entt::handle& in_) {
   }
   ImGui::Separator();
   if (dear::MenuItem("删除")) {
-    std::vector<entt::handle> l_list = p_i->lists | ranges::views::filter([](const impl::icon_data& in_data) {
-                                         return in_data.select && in_data.handle_;
-                                       }) |
-                                       ranges::views::transform([](const impl::icon_data& in_data) -> entt::handle {
-                                         return in_data.handle_;
-                                       }) |
-                                       ranges::to_vector |
-                                       ranges::actions::push_back(in_) |
-                                       ranges::actions::unique |
-                                       ranges::to_vector;
+    std::vector<entt::handle> l_list =
+        p_i->lists |
+        ranges::views::filter([](const impl::base_data& in_data) {
+          return in_data.select && in_data.handle_;
+        }) |
+        ranges::views::transform([](const impl::base_data& in_data) -> entt::handle {
+          return in_data.handle_;
+        }) |
+        ranges::to_vector |
+        ranges::actions::push_back(in_) |
+        ranges::actions::unique |
+        ranges::to_vector;
     ranges::for_each(l_list, [](const entt::handle& in_handle) {
       in_handle.patch<database>(database::delete_);
     });
 
-    g_reg()->ctx<core_sig>().save_begin.connect([this, in_, l_list](const std::vector<entt::handle>&) {
-      g_main_loop().attach<one_process_t>(
-          [this, in_, l_list]() {
-            p_i->lists = p_i->lists | ranges::views::remove_if([in_, l_list](const impl::icon_data& in_data) {
-                           return ranges::contains(l_list, in_data.handle_);
-                           //                           return in_data.handle_ == in_;
-                         }) |
-                         ranges::to_vector;
-          });
-    });
+    g_reg()->ctx<core_sig>().save_begin.connect(
+        [this, in_, l_list](const std::vector<entt::handle>&) {
+          g_main_loop().attach<one_process_t>(
+              [this, in_, l_list]() {
+                p_i->lists = p_i->lists |
+                             ranges::views::remove_if([in_, l_list](const impl::base_data& in_data) {
+                               return ranges::contains(l_list, in_data.handle_);
+                               //                           return in_data.handle_ == in_;
+                             }) |
+                             ranges::to_vector;
+              });
+        });
   }
 }
 void assets_file_widgets::set_select(std::size_t in_size) {
@@ -282,10 +298,10 @@ void assets_file_widgets::set_select(std::size_t in_size) {
   } else {  /// 单击鼠标时
     if (k_io.KeyCtrl) {
       i.select.data = !i.select.data;
-      l_handle_list = p_i->lists | ranges::views::filter([](impl::icon_data& in) {
+      l_handle_list = p_i->lists | ranges::views::filter([](impl::base_data& in) {
                         return in.select;
                       }) |
-                      ranges::views::transform([](impl::icon_data& in) {
+                      ranges::views::transform([](impl::base_data& in) {
                         return in.handle_;
                       }) |
                       ranges::to_vector;
@@ -294,14 +310,14 @@ void assets_file_widgets::set_select(std::size_t in_size) {
                       ranges::views::slice(
                           std::min(p_i->select_index, in_size),
                           std::max(p_i->select_index, in_size) + 1) |
-                      ranges::views::transform([](impl::icon_data& in) {
+                      ranges::views::transform([](impl::base_data& in) {
                         in.select = true;
                         return in.handle_;
                       }) |
                       ranges::to_vector;
     } else {
       boost::for_each(p_i->lists,
-                      [](impl::icon_data& in) {
+                      [](impl::base_data& in) {
                         in.select = false;
                       });
       i.select = true;
@@ -323,11 +339,11 @@ void assets_file_widgets::open_drag(std::size_t in_size) {
   std::vector<entt::handle> l_lists{};
   l_lists = ranges::to_vector(
       this->p_i->lists |
-      ranges::views::filter([](const impl::icon_data& in) -> bool {
+      ranges::views::filter([](const impl::base_data& in) -> bool {
         return in.select;
       }) |
       ranges::views::transform(
-          [](const impl::icon_data& in) -> entt::handle {
+          [](const impl::base_data& in) -> entt::handle {
             return in.handle_;
           }));
   l_lists.emplace_back(l_item.handle_);
@@ -339,15 +355,74 @@ void assets_file_widgets::open_drag(std::size_t in_size) {
 }
 
 void assets_file_widgets::render_by_icon() {
+  const static auto l_size{5u};
+  ImGui::Columns(l_size, "assets_file_widgets", false);
+  auto k_length = (ImGui::GetCurrentWindow()->InnerClipRect.GetWidth() / l_size) - ImGui::GetStyle().ItemInnerSpacing.x * 3;
+  ImGuiListClipper clipper{};
+  clipper.Begin((boost::numeric_cast<std::int32_t>(p_i->lists.size() / l_size)) + 1);
+  while (clipper.Step()) {
+    for (int l_i = clipper.DisplayStart; l_i < clipper.DisplayEnd; ++l_i) {
+      for (int l_j = 0; l_j < l_size; ++l_j) {
+        if ((l_i * l_size + l_j) < p_i->lists.size()) {
+          std::size_t l_index{l_i * l_size + l_j};
+          auto&& i = dynamic_cast<impl::icon_data&>(p_i->lists[l_index]);
+          i.load_image(k_length);
+          auto l_pos_image = ImGui::GetCursorPos();
+
+          ImGui::PushStyleColor(ImGuiCol_Header, (ImVec4)ImColor::HSV(7.0f, 0.6f, 0.8f));
+
+          if (ImGui::Selectable(*i.select.gui_name,
+                                i.select.data,
+                                ImGuiSelectableFlags_AllowDoubleClick,
+                                {k_length, k_length}))
+            set_select(l_index);
+          dear::PopupContextItem{} && [this, i]() {
+            render_context_menu(i.handle_);
+          };
+          ImGui::PopStyleColor();
+
+          dear::DragDropSource{} && [this, l_index]() {
+            this->open_drag(l_index);
+          };
+          auto l_pos_select = ImGui::GetCursorPos();
+          ImGui::SetCursorPos(l_pos_image);
+          ImGui::Image(i.image.data.get(), {i.image.icon_size2d_.width - 2, i.image.icon_size2d_.height - 2});
+          ImGui::SetCursorPos(l_pos_select);
+          dear::Text(i.name);
+        }
+        imgui::NextColumn();
+      }
+    }
+  }
 }
 void assets_file_widgets::render_by_icon(std::size_t in_index) {
-
 }
 void assets_file_widgets::render_by_info() {
+  const static auto l_size{5u};
 
+  dear::Table{
+      "list",
+      l_size,
+      ImGuiTableFlags_::ImGuiTableFlags_ScrollY |
+          ImGuiTableFlags_::ImGuiTableFlags_RowBg |
+          ImGuiTableFlags_::ImGuiTableFlags_BordersOuter |
+          ImGuiTableFlags_::ImGuiTableFlags_BordersV |
+          ImGuiTableFlags_::ImGuiTableFlags_Resizable |
+          ImGuiTableFlags_::ImGuiTableFlags_Reorderable |
+          ImGuiTableFlags_::ImGuiTableFlags_Hideable,
+  } && [&]() {
+    ImGui::TableSetupScrollFreeze(0, 1);  // Make top row always visible
+    ImGui::TableSetupColumn("id", ImGuiTableColumnFlags_None);
+    ImGui::TableSetupColumn("分类", ImGuiTableColumnFlags_None);
+    ImGui::TableSetupColumn("集数", ImGuiTableColumnFlags_None);
+    ImGui::TableSetupColumn("镜头", ImGuiTableColumnFlags_None);
+    ImGui::TableSetupColumn("路径", ImGuiTableColumnFlags_None);
+    ImGui::TableHeadersRow();
+
+      ImGuiListClipper clipper{};
+  };
 }
 void assets_file_widgets::render_by_info(std::size_t in_index) {
-
 }
 
 assets_file_widgets::~assets_file_widgets() = default;
