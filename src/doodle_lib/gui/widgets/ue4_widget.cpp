ecba0d66db7e79f4ae906be89a4381c7284462ed
uuiid <957714080@qq.com>
2022-04-01 17:25:43 +0800

    添加ue4模块基本数据

--- src/doodle_lib/gui/widgets/ue4_widget.cpp
+++ src/doodle_lib/gui/widgets/ue4_widget.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "ue4_widget.h"
+#include <doodle_lib/gui/gui_ref/ref_base.h>
+namespace doodle {
+class ue4_widget::impl {
+ public:
+  impl()             = default;
+  using ue4_file_gui = gui::gui_cache<std::string>;
+
+  gui::gui_cache<std::string, gui::gui_cache_path> ue4_prj{"ue4项目"s, FSys::path{}};
+  std::vector<ue4_file_gui> import_list_files;
+
+  gui::gui_cache<bool> import_cam{"导入cam"s, true};
+  gui::gui_cache<bool> import_abc{"导入abc"s, true};
+  gui::gui_cache<bool> import_fbx{"导入fbx"s, true};
+  gui::gui_cache<bool> quit_{"生成并退出"s, true};
+  gui::gui_cache_name_id import_{"导入"};
+};
+
+ue4_widget::ue4_widget()
+    : p_i(std::make_unique<impl>()) {
+}
+ue4_widget::~ue4_widget() = default;
+void ue4_widget::init() {
+  g_reg()->set<ue4_widget &>(*this);
+}
+void ue4_widget::succeeded() {
+  g_reg()->unset<ue4_widget>();
+}
+void ue4_widget::failed() {
+  g_reg()->unset<ue4_widget>();
+}
+void ue4_widget::aborted() {
+  g_reg()->unset<ue4_widget>();
+}
+void ue4_widget::update(
+    const std::chrono::duration<std::chrono::system_clock::rep,
+                                std::chrono::system_clock::period> &,
+    void *data) {
+
+}
+}  // namespace doodle
--- src/doodle_lib/gui/widgets/ue4_widget.h
+++ src/doodle_lib/gui/widgets/ue4_widget.h
@@ -4,9 +4,24 @@
 
 #pragma once
 
+#include <doodle_lib/doodle_lib_fwd.h>
 namespace doodle {
 
-class ue4_widget {
+class DOODLELIB_API ue4_widget : public process_t<ue4_widget> {
+  class impl;
+  std::unique_ptr<impl> p_i;
+
+
+ public:
+  ue4_widget();
+  ~ue4_widget() override;
+  constexpr static std::string_view name{"ue4工具"};
+
+  [[maybe_unused]] void init();
+  [[maybe_unused]] void succeeded();
+  [[maybe_unused]] void failed();
+  [[maybe_unused]] void aborted();
+  [[maybe_unused]] void update(const delta_type&, void* data);
 };
 
 }  // namespace doodle
