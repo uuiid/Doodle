76de291cbbe969fea5137046cbd509e789fa986c
uuiid <957714080@qq.com>
2022-04-01 16:46:13 +0800

    添加一些计算时间的辅助函数和类

--- src/doodle_lib/core/work_clock.h
+++ src/doodle_lib/core/work_clock.h
@@ -8,9 +8,36 @@
 #include <chrono>
 
 namespace doodle {
+namespace business {
 
-struct DOODLELIB_API work_clock {
-
+class DOODLELIB_API adjust_work {
+ public:
+  chrono::local_time_pos start_;
+  chrono::local_time_pos end_;
 };
 
+class DOODLELIB_API adjust_rest {
+ public:
+  chrono::local_time_pos start_;
+  chrono::local_time_pos end_;
+};
+
+class DOODLELIB_API rules {
+ public:
+  std::set<chrono::weekday> work_weekdays;
+  std::set<
+      std::pair<
+          chrono::seconds,
+          chrono::seconds>>
+      work_pair;
+  std::vector<adjust_work> extra_work;
+  std::vector<adjust_rest> extra_rest;
+
+};
+}  // namespace business
+
+chrono::hours_double work_duration(
+    const chrono::local_time_pos& in_s,
+    const chrono::local_time_pos& in_e,
+    const business::rules& in_rules);
 }  // namespace doodle
