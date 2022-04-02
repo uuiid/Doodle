564ea235e7c913679eef7de98b8bf8d42b10aaf5
uuiid <957714080@qq.com>
2022-04-02 13:50:35 +0800

    更新静态变量

--- src/doodle_lib/core/static_value.h
+++ src/doodle_lib/core/static_value.h
@@ -12,10 +12,12 @@ constexpr const std::string_view authorization_data{"doodle"};
 constexpr const std::array<unsigned char, 32> cryptopp_key{"cryptopp_key.uuiid.doodle.v.3.1"};
 constexpr const std::array<unsigned char, 16> cryptopp_iv{"iv.uuiid.doodle"};
 constexpr const std::int16_t cryptopp_tag_size{16};
-constexpr const std::string_view ue_path_obj{"Engine/Binaries/Win64/UE4Editor.exe"};
 constexpr const std::string_view drop_imgui_id{"drop_imgui_id"};
 constexpr const std::string_view drop_handle_list{"drop_handle_list"};
 constexpr const std::size_t rand_block{256};
 constexpr const std::string_view font_default{R"(C:/Windows/Fonts/simkai.ttf)"};
 constexpr const std::string_view image_folder_name{R"(image)"};
+
+constexpr const std::string_view ue_path_obj{"Engine/Binaries/Win64/UE4Editor.exe"};
+constexpr const std::string_view ue4_content{R"(Content)"};
 }  // namespace doodle::doodle_config
