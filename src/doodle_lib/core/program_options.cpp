4c80f1aa646b6e69930f9a45f56e30534719f32d
uuiid <957714080@qq.com>
2022-04-06 15:21:53 +0800

    解析时去除路径双引号

--- src/doodle_lib/core/program_options.cpp
+++ src/doodle_lib/core/program_options.cpp
@@ -19,7 +19,7 @@ program_options::program_options()
       p_config_file(),
       p_max_thread(std::make_pair(false, std::thread::hardware_concurrency() - 2)),
       p_root(std::make_pair(false, "C:/")),
-      p_ue4outpath(FSys::temp_directory_path().generic_string()) {
+      p_ue4outpath() {
   DOODLE_LOG_INFO("开始构建命令行");
 
   p_opt_positional.add(input_project, 1);
@@ -38,7 +38,7 @@ program_options::program_options()
       boost::program_options::value(&p_config_file),
       "配置文件的路径")(
       ue4outpath,
-      boost::program_options::value(&p_ue4outpath),
+      boost::program_options::value(&p_ue4outpath)->default_value(FSys::temp_directory_path().generic_string()),
       "导出ue4导入配置文件的路径");
 
   p_opt_gui.add_options()(
@@ -87,6 +87,11 @@ bool program_options::command_line_parser(const std::vector<string>& in_arg) {
   } else {
     if (p_project_path.front() == '"') {
       p_project_path = p_project_path.substr(1, p_project_path.size() - 2);
+    }
+  }
+  if (p_vm.count(ue4outpath)) {
+    if (p_ue4outpath.front() == '"') {
+      p_ue4outpath = p_ue4outpath.substr(1, p_ue4outpath.size() - 2);
     }
   }
 
