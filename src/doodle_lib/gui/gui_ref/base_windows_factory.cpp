c6267f2eb756115a54129a8d2a973df98122ef10
uuiid <957714080@qq.com>
2022-04-08 15:04:18 +0800

    添加窗口工厂类

--- src/doodle_lib/gui/gui_ref/base_windows_factory.cpp
+++ src/doodle_lib/gui/gui_ref/base_windows_factory.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "base_windows_factory.h"
+namespace doodle::gui {
+bool base_windows_factory::is_show() {
+  return window_;
+}
+}  // namespace doodle::gui
--- src/doodle_lib/gui/gui_ref/base_windows_factory.h
+++ src/doodle_lib/gui/gui_ref/base_windows_factory.h
@@ -7,10 +7,14 @@
 namespace doodle::gui {
 class base_window;
 class DOODLELIB_API base_windows_factory {
+ protected:
+  base_window* window_{};
+
  public:
   base_windows_factory()               = default;
   virtual ~base_windows_factory()      = default;
 
+  bool is_show();
   virtual base_window* make_windows()  = 0;
   virtual base_window* close_windows() = 0;
 };
