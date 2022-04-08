212490c72644e569e657388c754369cf0ef6fe8a
uuiid <957714080@qq.com>
2022-04-08 15:57:40 +0800

    去除无用的类

--- src/doodle_lib/gui/gui_ref/base_windows_factory.h
+++ src/doodle_lib/gui/gui_ref/base_windows_factory.h
@@ -5,18 +5,5 @@
 #include <doodle_lib/doodle_lib_fwd.h>
 
 namespace doodle::gui {
-class base_window;
-class DOODLELIB_API base_windows_factory {
- protected:
-  base_window* window_{};
-
- public:
-  base_windows_factory()               = default;
-  virtual ~base_windows_factory()      = default;
-
-  bool is_show();
-  virtual base_window* make_windows()  = 0;
-  virtual base_window* close_windows() = 0;
-};
 
 }  // namespace doodle::gui
