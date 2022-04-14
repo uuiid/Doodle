214430b5bb7e12bfc769f39f6791f520d45270fc
uuiid <957714080@qq.com>
2022-04-14 11:11:12 +0800

    添加布局窗口

--- src/doodle_lib/gui/gui_ref/layout_window.cpp
+++ src/doodle_lib/gui/gui_ref/layout_window.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "layout_window.h"
+namespace doodle::gui {
+class layout_window::impl {
+ public:
+};
+layout_window::layout_window()
+    : p_i(std::make_unique<impl>()) {}
+
+const string &layout_window::title() const {
+  static std::string l_title{"layout_window"};
+  return l_title;
+}
+void layout_window::init() {
+}
+void layout_window::succeeded() {
+}
+void layout_window::update(const chrono::system_clock::duration &in_duration,
+                           void *in_data) {
+  const ImGuiViewport *viewport = ImGui::GetMainViewport();
+  ImGui::SetNextWindowPos(viewport->WorkPos);
+  ImGui::SetNextWindowSize(viewport->WorkSize);
+  dear::Begin("main_windows", &show_,
+              ImGuiWindowFlags_NoDecoration |
+                  ImGuiWindowFlags_NoMove |
+                  ImGuiWindowFlags_NoResize |
+                  ImGuiWindowFlags_NoSavedSettings) &&
+      []() {
+
+      };
+}
+layout_window::~layout_window() = default;
+}  // namespace doodle::gui
--- src/doodle_lib/gui/gui_ref/layout_window.h
+++ src/doodle_lib/gui/gui_ref/layout_window.h
@@ -3,7 +3,24 @@
 //
 #pragma once
 
+#include <doodle_lib/doodle_lib_fwd.h>
+#include <doodle_lib/gui/gui_ref/base_window.h>
+namespace doodle::gui {
 
+class DOODLELIB_API layout_window
+    : public base_window,
+      public process_t<layout_window>{
+  class impl;
+  std::unique_ptr<impl> p_i;
 
-class layout_window {
+ public:
+  layout_window();
+  ~layout_window() override;
+  [[nodiscard]] const string &title() const override;
+  void init() override;
+  void succeeded() override;
+  void update(const chrono::system_clock::duration &in_duration,
+              void *in_data) override;
+  
 };
+}  // namespace doodle::gui
