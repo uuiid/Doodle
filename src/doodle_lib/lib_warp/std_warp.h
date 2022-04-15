2683c36d2f69d69cf5bc367394a21b8103422ad5
uuiid <957714080@qq.com>
2022-04-15 13:13:54 +0800

    添加std 路径序列化

--- src/doodle_lib/lib_warp/std_warp.h
+++ src/doodle_lib/lib_warp/std_warp.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <fmt/format.h>
 #include <boost/hana.hpp>
 #include <memory>
 namespace doodle {
@@ -11,18 +12,35 @@ namespace details {
 
 }  // namespace details
 
-template <class ClassIn, class... Args>
-std::shared_ptr<ClassIn> new_object(Args&&... in_args) {
-  // post_constructor
-  constexpr auto has_make_this =
-      boost::hana::is_valid(
-          [](auto&& obj)
-              -> decltype(obj->post_constructor()) {});
-  auto ptr              = std::make_shared<ClassIn>(std::forward<Args>(in_args)...);
-  using has_make_this_v = decltype(has_make_this(ptr));
-  if constexpr (has_make_this_v{}) {
-    ptr->post_constructor();
-  }
-  return ptr;
-}
+ template <class ClassIn, class... Args>
+ std::shared_ptr<ClassIn> new_object(Args&&... in_args) {
+   // post_constructor
+   constexpr auto has_make_this =
+       boost::hana::is_valid(
+           [](auto&& obj)
+               -> decltype(obj->post_constructor()) {});
+   auto ptr              = std::make_shared<ClassIn>(std::forward<Args>(in_args)...);
+   using has_make_this_v = decltype(has_make_this(ptr));
+   if constexpr (has_make_this_v{}) {
+     ptr->post_constructor();
+   }
+   return ptr;
+ }
 }  // namespace doodle
+
+namespace fmt {
+/**
+ * @brief 格式化::std::filesystem::path
+ *
+ * @tparam  ::std::filesystem::path
+ */
+template <>
+struct formatter<::std::filesystem::path> : formatter<string_view> {
+  template <typename FormatContext>
+  auto format(const ::std::filesystem::path &in_, FormatContext &ctx) -> decltype(ctx.out()) {
+    return formatter<fmt::string_view>::format(
+        in_.generic_string(),
+        ctx);
+  }
+};
+}
