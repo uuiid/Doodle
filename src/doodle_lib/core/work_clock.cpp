498f53b84d5909f34549f2d93664b91c8c37441f
uuiid <957714080@qq.com>
2022-04-22 14:12:53 +0800

    添加工作时间计算

--- src/doodle_lib/core/work_clock.cpp
+++ src/doodle_lib/core/work_clock.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "work_clock.h"
+#include <date/tz.h>
+
+namespace doodle {
+
+namespace business {
+
+chrono::local_time_pos rules::get_work_time(const chrono::local_time_pos& in_s) {
+  auto l_time = chrono::make_zoned(chrono::current_zone(), in_s);
+  /// \brief 首先查看在那天开始工作
+
+  return doodle::chrono::local_time_pos();
+}
+std::vector<std::pair<
+    chrono::seconds,
+    chrono::seconds>>
+rules::operator()(const chrono::year_month_day& in_day) {
+  /// \brief 本年本月的最后一天
+
+  chrono::weekday l_weekday{in_day};
+  std::vector<std::pair<chrono::seconds, chrono::seconds>> result{};
+  if (work_weekdays[l_weekday.c_encoding() - 1]) {
+    result.insert(result.begin(), work_pair.begin(), work_pair.end());
+  }
+  for (const auto& i : extra_work) {
+    //    chrono::year_month_day l_day{i.start_};
+
+    for (auto& j : result) {
+    }
+  }
+
+  return std::vector<std::pair<chrono::seconds, chrono::seconds>>();
+}
+}  // namespace business
+chrono::local_time_pos next_time(const chrono::local_time_pos& in_s,
+                                 const std::chrono::milliseconds& in_du_time,
+                                 const business::rules& in_rules) {
+  return chrono::local_time_pos();
+}
+
+}  // namespace doodle
--- src/doodle_lib/core/work_clock.h
+++ src/doodle_lib/core/work_clock.h
@@ -5,9 +5,10 @@
 #pragma once
 
 #include <doodle_lib/doodle_lib_fwd.h>
-#include <chrono>
+#include <bitset>
 
 namespace doodle {
+
 namespace business {
 
 class DOODLELIB_API adjust_work {
@@ -24,7 +25,8 @@ class DOODLELIB_API adjust_rest {
 
 class DOODLELIB_API rules {
  public:
-  std::set<chrono::weekday> work_weekdays;
+  /// \brief 工作日 从周一到周日
+  std::bitset<7> work_weekdays;
   std::set<
       std::pair<
           chrono::seconds,
@@ -32,12 +34,28 @@ class DOODLELIB_API rules {
       work_pair;
   std::vector<adjust_work> extra_work;
   std::vector<adjust_rest> extra_rest;
+  chrono::local_time_pos get_work_time(const chrono::local_time_pos& in_s);
 
+  /**
+   * @brief 获取当天的工作时间段
+   * @param in_day 传入的天数
+   * @return
+   */
+  std::vector<std::pair<
+      chrono::seconds,
+      chrono::seconds>>
+  operator()(const chrono::year_month_day& in_day);
 };
+
 }  // namespace business
 
 chrono::hours_double work_duration(
     const chrono::local_time_pos& in_s,
     const chrono::local_time_pos& in_e,
+    const business::rules& in_rules);
+
+chrono::local_time_pos next_time(
+    const chrono::local_time_pos& in_s,
+    const chrono::milliseconds& in_du_time,
     const business::rules& in_rules);
 }  // namespace doodle
