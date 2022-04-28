f08b72a5e275161987f564f69e2319c83cf7c85a
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-04-28 16:19:17 +0800

    分离核心库 修正类型

--- src/doodle_core/doodle_core.h
+++ src/doodle_core/doodle_core.h
@@ -4,8 +4,21 @@
 
 #pragma once
 
-#include <doodle_core_fwd.h>
+#include <doodle_core/doodle_core_fwd.h>
 
 #include <doodle_core/configure/config.h>
 #include <doodle_core/pin_yin/convert.h>
 #include <doodle_core/lib_warp/cmrcWarp.h>
+
+#include <doodle_core/core/core_set.h>
+#include <doodle_core/core/core_sig.h>
+#include <doodle_core/core/doodle_lib.h>
+#include <doodle_core/logger/logger.h>
+#include <doodle_core/metadata/project.h>
+#include <doodle_core/metadata/metadata.h>
+#include <doodle_core/metadata/episodes.h>
+#include <doodle_core/metadata/shot.h>
+#include <doodle_core/metadata/season.h>
+#include <doodle_core/thread_pool/process_pool.h>
+#include <doodle_core/thread_pool/thread_pool.h>
+#include <doodle_core/thread_pool/process_message.h>
