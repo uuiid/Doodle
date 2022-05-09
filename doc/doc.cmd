b4acbd539837fa9f8858fe8f5174d03b3ff90946
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-09 10:12:47 +0800

    doc更新脚本

---
+++ doc/doc.cmd
@@ -0,0 +1,21 @@
+set my_pwd=%~dp0
+echo %my_pwd%
+
+echo -----------------set env--------------------
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+cd %my_pwd%/..
+
+echo -----------------config doxygen--------------------
+"C:\Program Files\CMake\bin\cmake.exe" ^
+-S. ^
+--preset Ninja_release
+
+echo -----------------build doxygen--------------------
+"C:\Program Files\CMake\bin\cmake.exe" ^
+--build ^
+--preset doxygen
+
+echo -----------------copy file--------------------
+robocopy build\Ninja_release\html build\html /s /NFL /NDL
+Exit 0
