5249f0912b2e9975965bcbe232014a18466d898e
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-10 09:27:33 +0800

    添加maya回调守卫

---
+++ src/doodle_exe/maya_plug/data/maya_call_guard.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/5/10.
+//
+
+#include "maya_call_guard.h"
+
+namespace doodle {
+namespace maya_plug {
+}  // namespace maya_plug
+}  // namespace doodle
---
+++ src/doodle_exe/maya_plug/data/maya_call_guard.h
@@ -0,0 +1,27 @@
+//
+// Created by TD on 2022/5/10.
+//
+
+#pragma once
+
+#include <maya/MMessage.h>
+#include <maya_plug/maya_plug_fwd.h>
+namespace doodle {
+namespace maya_plug {
+
+// template <typename T>
+class maya_call_guard {
+ public:
+  MCallbackId call_id{};
+  explicit maya_call_guard(MCallbackId&& in_id)
+      : call_id(std::move(in_id)) {}
+  ~maya_call_guard() {
+    auto k_s = MMessage::removeCallback(call_id);
+    if (!k_s) {
+      DOODLE_LOG_ERROR("卸载回调出错");
+    }
+  }
+};
+
+}  // namespace maya_plug
+}  // namespace doodle
