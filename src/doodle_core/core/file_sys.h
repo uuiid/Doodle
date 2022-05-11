6ded14ebc44b74b2bbbde32aa3ccc09771975cdf
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-11 11:04:17 +0800

    添加u8 str类

---
+++ src/doodle_core/core/file_sys.h
@@ -0,0 +1,97 @@
+//
+// Created by TD on 2022/5/11.
+//
+
+#pragma once
+#include <filesystem>
+namespace doodle::FSys {
+using namespace std::filesystem;
+class path_u8 : public path {
+  inline static std::locale locale_u8{};
+
+ public:
+  template <class Source>
+  path_u8(const Source& source, format fmt = auto_format)
+      : path(source, locale_u8){};
+  template <class InputIt>
+  path_u8(InputIt first, InputIt last, format fmt = auto_format)
+      : path(first, last, locale_u8){};
+
+  using path::path;
+
+  ~path_u8() = default;
+
+  // assignments
+
+  template <class Source>
+  path_u8& operator=(const Source& source) {
+    path::operator=(path_u8{source});
+    return *this;
+  };
+  template <class Source>
+  path_u8& assign(const Source& source) {
+    path::assign(path_u8{source});
+    return *this;
+  };
+  template <class InputIt>
+  path_u8& assign(InputIt first, InputIt last) {
+    path::assign(path_u8{first, last});
+    return *this;
+  };
+
+  // appends
+
+  template <class Source>
+  path_u8& operator/=(const Source& source) {
+    path::operator/=(path_u8{source});
+    return *this;
+  };
+  template <class Source>
+  path_u8& append(const Source& source) {
+    path::operator/=(path_u8{source});
+    return *this;
+  };
+  template <class InputIt>
+  path_u8& append(InputIt first, InputIt last) {
+    path::operator/=(path_u8{first, last});
+    return *this;
+  };
+
+  // concatenation
+  path_u8& operator+=(const path_u8& x) {
+    path::operator+=(x);
+    return *this;
+  };
+  template <class Source>
+  path_u8& operator+=(const Source& x) {
+    path::operator+=(path_u8{x});
+    return *this;
+  };
+  template <class ECharT>
+  path_u8& operator+=(ECharT x) {
+    path::operator+=(path_u8{x});
+    return *this;
+  };
+  template <class Source>
+  path_u8& concat(const Source& x) {
+    path::operator+=(path_u8{x});
+    return *this;
+  };
+  template <class InputIt>
+  path_u8& concat(InputIt first, InputIt last) {
+    path::operator+=(path_u8{first, last});
+    return *this;
+  };
+
+  // modifiers
+  void swap(path_u8& rhs) noexcept {
+    path::swap(rhs);
+  };
+
+  // non-member operators
+  friend path_u8 operator/(const path_u8& lhs, const path_u8& rhs) {
+    return operator/(lhs, rhs);
+  };
+};
+
+}  // namespace doodle::FSys
--- src/doodle_core/doodle_core_fwd.h
+++ src/doodle_core/doodle_core_fwd.h
@@ -11,6 +11,7 @@
 #include <doodle_core/doodle_core_pch.h>
 #include <date/date.h>
 #include <entt/entt.hpp>
+#include <doodle_core/core/file_sys.h>
 #include <doodle_core/lib_warp/json_warp.h>
 #include <doodle_core/lib_warp/cmrcWarp.h>
 #include <doodle_core/lib_warp/entt_warp.h>
@@ -45,10 +46,10 @@ using local_time_pos = time_point<local_t, seconds>;
  *
  * @todo 这里我们暂时使用周六和周日作为判断, 但是实际上还有各种假期和其他情况要计入
  */
-bool is_rest_day(const sys_days &in_days);
-bool is_rest_day(const local_days &in_days);
+bool is_rest_day(const sys_days& in_days);
+bool is_rest_day(const local_days& in_days);
 template <class dur>
-std::time_t to_time_t(const time_point<local_t, dur> &in_timePoint) {
+std::time_t to_time_t(const time_point<local_t, dur>& in_timePoint) {
   return duration_cast<seconds>(in_timePoint.time_since_epoch()).count();
 };
 }  // namespace chrono
@@ -61,14 +62,14 @@ using ifstream = std::ifstream;
 using ofstream = std::ofstream;
 using ostream  = std::ostream;
 
-DOODLE_CORE_EXPORT std::time_t last_write_time_t(const path &in_path);
-DOODLE_CORE_EXPORT chrono::sys_time_pos last_write_time_point(const path &in_path);
-DOODLE_CORE_EXPORT void last_write_time_point(const path &in_path, const std::chrono::system_clock::time_point &in_time_point);
-DOODLE_CORE_EXPORT path add_time_stamp(const path &in_path);
-DOODLE_CORE_EXPORT void open_explorer(const path &in_path);
-DOODLE_CORE_EXPORT void backup_file(const path &source);
-DOODLE_CORE_EXPORT std::vector<path> list_files(const path &in_dir);
-DOODLE_CORE_EXPORT bool is_sub_path(const path &in_parent, const path &in_child);
+DOODLE_CORE_EXPORT std::time_t last_write_time_t(const path& in_path);
+DOODLE_CORE_EXPORT chrono::sys_time_pos last_write_time_point(const path& in_path);
+DOODLE_CORE_EXPORT void last_write_time_point(const path& in_path, const std::chrono::system_clock::time_point& in_time_point);
+DOODLE_CORE_EXPORT path add_time_stamp(const path& in_path);
+DOODLE_CORE_EXPORT void open_explorer(const path& in_path);
+DOODLE_CORE_EXPORT void backup_file(const path& source);
+DOODLE_CORE_EXPORT std::vector<path> list_files(const path& in_dir);
+DOODLE_CORE_EXPORT bool is_sub_path(const path& in_parent, const path& in_child);
 
 }  // namespace FSys
 
@@ -101,9 +102,9 @@ using process_t      = entt::process<Derived, std::chrono::system_clock::duratio
 using scheduler_t    = scheduler<std::chrono::system_clock::duration>;
 using bounded_pool_t = scheduler<std::chrono::system_clock::duration, pool_n::bounded_limiter>;
 
-DOODLE_CORE_EXPORT registry_ptr &g_reg();
-DOODLE_CORE_EXPORT scheduler_t &g_main_loop();
-DOODLE_CORE_EXPORT bounded_pool_t &g_bounded_pool();
-DOODLE_CORE_EXPORT thread_pool &g_thread_pool();
+DOODLE_CORE_EXPORT registry_ptr& g_reg();
+DOODLE_CORE_EXPORT scheduler_t& g_main_loop();
+DOODLE_CORE_EXPORT bounded_pool_t& g_bounded_pool();
+DOODLE_CORE_EXPORT thread_pool& g_thread_pool();
 
 }  // namespace doodle
