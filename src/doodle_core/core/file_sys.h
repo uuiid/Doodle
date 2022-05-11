9fd16e7fa2afa8ce409cc2aa8a7d20b22acb2f3b
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-11 13:31:10 +0800

    path

--- src/doodle_core/core/file_sys.h
+++ src/doodle_core/core/file_sys.h
@@ -8,9 +8,36 @@
 #include <fmt/format.h>
 
 #include <nlohmann/json_fwd.hpp>
+#include <boost/filesystem.hpp>
+#include <doodle_core/core/chrono_.h>
+#include <doodle_core/configure/doodle_core_export.h>
+
+namespace doodle::chrono {
+using namespace std::chrono;
+}
 
 namespace doodle::FSys {
+#ifdef USE_STD_FSYS
 using namespace std::filesystem;
+using fstream  = std::fstream;
+using istream  = std::istream;
+using ifstream = std::ifstream;
+using ofstream = std::ofstream;
+using ostream  = std::ostream;
+#else
+
+using namespace boost::filesystem;
+
+#endif
+
+DOODLE_CORE_EXPORT std::time_t last_write_time_t(const path& in_path);
+DOODLE_CORE_EXPORT chrono::sys_time_pos last_write_time_point(const path& in_path);
+DOODLE_CORE_EXPORT void last_write_time_point(const path& in_path, const std::chrono::system_clock::time_point& in_time_point);
+DOODLE_CORE_EXPORT path add_time_stamp(const path& in_path);
+DOODLE_CORE_EXPORT void open_explorer(const path& in_path);
+DOODLE_CORE_EXPORT void backup_file(const path& source);
+DOODLE_CORE_EXPORT std::vector<path> list_files(const path& in_dir);
+DOODLE_CORE_EXPORT bool is_sub_path(const path& in_parent, const path& in_child);
 
 }  // namespace doodle::FSys
 namespace fmt {
--- src/doodle_core/doodle_core_fwd.h
+++ src/doodle_core/doodle_core_fwd.h
@@ -19,6 +19,7 @@
 
 #include <doodle_core/core/core_help_impl.h>
 #include <doodle_core/lib_warp/std_warp.h>
+
 //#include <>
 namespace doodle {
 class convert;
@@ -28,51 +29,6 @@ class nullptr_error;
 class serialization_error;
 class component_error;
 class file_error;
-namespace chrono {
-namespace literals {
-using namespace std::chrono_literals;
-using namespace date::literals;
-
-}  // namespace literals
-using namespace std::chrono;
-using namespace date;
-
-using hours_double   = duration<std::double_t, std::ratio<3600>>;
-using days_double    = duration<std::double_t, std::ratio<28800>>;
-using sys_time_pos   = time_point<system_clock>;
-using local_time_pos = time_point<local_t, seconds>;
-
-/**
- * @brief 判断是否是休息日 周六日
- *
- * @todo 这里我们暂时使用周六和周日作为判断, 但是实际上还有各种假期和其他情况要计入
- */
-bool is_rest_day(const sys_days& in_days);
-bool is_rest_day(const local_days& in_days);
-template <class dur>
-std::time_t to_time_t(const time_point<local_t, dur>& in_timePoint) {
-  return duration_cast<seconds>(in_timePoint.time_since_epoch()).count();
-};
-}  // namespace chrono
-
-namespace FSys {
-using namespace std::filesystem;
-using fstream  = std::fstream;
-using istream  = std::istream;
-using ifstream = std::ifstream;
-using ofstream = std::ofstream;
-using ostream  = std::ostream;
-
-DOODLE_CORE_EXPORT std::time_t last_write_time_t(const path& in_path);
-DOODLE_CORE_EXPORT chrono::sys_time_pos last_write_time_point(const path& in_path);
-DOODLE_CORE_EXPORT void last_write_time_point(const path& in_path, const std::chrono::system_clock::time_point& in_time_point);
-DOODLE_CORE_EXPORT path add_time_stamp(const path& in_path);
-DOODLE_CORE_EXPORT void open_explorer(const path& in_path);
-DOODLE_CORE_EXPORT void backup_file(const path& source);
-DOODLE_CORE_EXPORT std::vector<path> list_files(const path& in_dir);
-DOODLE_CORE_EXPORT bool is_sub_path(const path& in_parent, const path& in_child);
-
-}  // namespace FSys
 
 using namespace std::literals;
 using namespace date::literals;
