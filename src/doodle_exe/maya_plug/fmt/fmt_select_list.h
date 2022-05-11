3b0fdf0882bd436d545e35bfc4a33c55aaf9207c
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-11 14:57:11 +0800

    添加选择列表格式化

---
+++ src/doodle_exe/maya_plug/fmt/fmt_select_list.h
@@ -0,0 +1,31 @@
+//
+// Created by TD on 2022/5/11.
+//
+#pragma once
+
+#include <maya/MSelectionList.h>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
+
+namespace fmt {
+/**
+ * @brief 格式化maya字符串
+ *
+ * @tparam
+ */
+template <>
+struct formatter<MSelectionList> : formatter<string_view> {
+  template <typename FormatContext>
+  auto format(const MSelectionList& in_, FormatContext& ctx) -> decltype(ctx.out()) {
+    MStringArray l_array{};
+    in_.getSelectionStrings(l_array);
+    return formatter<string_view>::format(
+        to_string(l_array),
+        ctx);
+  }
+};
+}  // namespace fmt
