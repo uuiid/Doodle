79d8e2fd7f44b20a0240e6222f70a0717288ec8e
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-11 11:49:16 +0800

    删除路径

--- src/doodle_core/core/file_sys.cpp
+++ src/doodle_core/core/file_sys.cpp
@@ -5,11 +5,5 @@
 
 #include <nlohmann/json.hpp>
 namespace doodle::FSys {
-void to_json(nlohmann::json& j, const path_u8& p) {
-  j = p.generic_string();
-};
-void from_json(const nlohmann::json& j, path_u8& p) {
-  p = j.get<std::string>();
-};
 
 }  // namespace doodle::FSys
