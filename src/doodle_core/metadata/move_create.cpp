2c3a32b964168db5a2c4d2a6abd36a4917908e38
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-16 12:27:24 +0800

    添加image move 类型

--- src/doodle_core/metadata/move_create.cpp
+++ src/doodle_core/metadata/move_create.cpp
@@ -4,16 +4,29 @@
 
 #include "move_create.h"
 
+#include <doodle_core/doodle_core_fwd.h>
+#include <doodle_core/logger/logger.h>
+
 #include <range/v3/range.hpp>
 #include <range/v3/view/all.hpp>
 #include <range/v3/view/transform.hpp>
+#include <range/v3/algorithm/all_of.hpp>
+
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/range_for.hpp>
+#include <range/v3/algorithm/for_each.hpp>
+
 namespace doodle::move {
 
 namespace {
 class image_attr_auxiliary {
  public:
   image_attr_auxiliary() = default;
-  explicit image_attr_auxiliary(image_attr in_image) : image(std::move(in_image)){};
+  explicit image_attr_auxiliary(image_attr in_image)
+      : image(std::move(in_image)) {
+    extract_num_list();
+  };
 
   std::vector<std::int32_t> num_list;
   image_attr image;
@@ -37,7 +50,31 @@ void image_attr::extract_num(std::vector<image_attr> &in_image_list) {
   auto l_list = in_image_list |
                 ranges::views::transform([](auto in_item) -> image_attr_auxiliary {
                   return image_attr_auxiliary{in_item};
-                });
+                }) |
+                ranges::to_vector;
+  const auto k_size = l_list.front().num_list.size();
+
+  chick_true<doodle_error>(
+      ranges::all_of(l_list,
+                     [k_size](const image_attr_auxiliary &in) -> bool {
+                       return in.num_list.size() == k_size;
+                     }),
+      DOODLE_LOC, "序列不匹配");
+  chick_true<doodle_error>(l_list.size() >= 2, DOODLE_LOC, "单个文件, 无法搜索帧号");
+  auto &one   = l_list[0].num_list;
+  auto &tow   = l_list[1].num_list;
+  auto l_item = ranges::views::ints(std::size_t{0}, k_size) |
+                ranges::views::filter([&](const std::size_t &in_tuple) {
+                  return one[in_tuple] != tow[in_tuple];
+                }) |
+                ranges::to_vector;
+
+  chick_true<doodle_error>(!l_item.empty(), DOODLE_LOC, "没有找到帧索引");
+  auto l_index = l_item.front();
+  ranges::for_each(l_list,
+                   [&](image_attr_auxiliary &in_attribute) {
+                     in_attribute.image.num = in_attribute.num_list[l_index];
+                   });
 }
 
 bool image_attr::operator<(const image_attr &in_rhs) const {
