4fc0e5812b1ad88c45d79b9ed8025c21af330b4c
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-19 16:59:00 +0800

    修正编译错误

--- script/uePlug/Doodle/Source/Doodle/Public/DoodleMatrixLight.h
+++ script/uePlug/Doodle/Source/Doodle/Public/DoodleMatrixLight.h
@@ -8,13 +8,14 @@
 //这个必须在最后面
 #include "DoodleMatrixLight.generated.h"
 
+class USpotLightComponent;
 
 UCLASS()
-class DOODLE_API ADoodleMatrixLight : public AActor {
+class DOODLE_API ADoodleMatrixLight : public AActor
+{
 	GENERATED_BODY()
 public:
 	ADoodleMatrixLight();
-
 
 	bool Enabled;
 
@@ -22,61 +23,65 @@ public:
 	 * Total energy that the light emits.
 	 */
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "Intensity", UIMin = "0.0", UIMax = "20.0"))
-		float Intensity;
+			  meta = (DisplayName = "Intensity", UIMin = "0.0", UIMax = "20.0"))
+	float Intensity;
 
 	/**
 	 * Filter color of the light.
 	 * Note that this can change the light's effective intensity.
 	 */
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (HideAlphaChannel))
-		FColor LightColor;
+			  meta = (HideAlphaChannel))
+	FColor LightColor;
 
 	/**
 	 *
 	 */
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "FocalAngleOuter"))
-		float FocalAngleOuter;
+			  meta = (DisplayName = "FocalAngleOuter"))
+	float FocalAngleOuter;
 
 	/**
 	 *
 	 */
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "FocalAngleInner"))
-		float FocalAngleInner;
+			  meta = (DisplayName = "FocalAngleInner"))
+	float FocalAngleInner;
 
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "AttenuationDistance"))
-		float AttenuationDistance;
+			  meta = (DisplayName = "AttenuationDistance"))
+	float AttenuationDistance;
 
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "LightWidth"))
-		float LightWidth;
+			  meta = (DisplayName = "LightWidth"))
+	float LightWidth;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "LightLength"))
-		float LightLength;
+			  meta = (DisplayName = "LightLength"))
+	float LightLength;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "CastShadows"))
-		bool CastShadows;
+			  meta = (DisplayName = "CastShadows"))
+	bool CastShadows;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "LightSamplesSquared"))
-		int LightSamplesSquared;
+			  meta = (DisplayName = "LightSamplesSquared"))
+	int LightSamplesSquared;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "SourceRadiusMult"))
-		float SourceRadiusMult;
+			  meta = (DisplayName = "SourceRadiusMult"))
+	float SourceRadiusMult;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "CenterOfInterestLength"))
-		float CenterOfInterestLength;
+			  meta = (DisplayName = "CenterOfInterestLength"))
+	float CenterOfInterestLength;
 
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "Channels"))
-		FLightingChannels Channels;
+			  meta = (DisplayName = "Channels"))
+	FLightingChannels Channels;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "SoftRadius"))
-		float SoftRadius;
+			  meta = (DisplayName = "SoftRadius"))
+	float SoftRadius;
 	UPROPERTY(BlueprintReadOnly, interp, Category = Light,
-		meta = (DisplayName = "ShadowBias"))
-		float ShadowBias;
+			  meta = (DisplayName = "ShadowBias"))
+	float ShadowBias;
+
+private:
+	UPROPERTY()
+	TArray<USpotLightComponent *> LightList;
 };
