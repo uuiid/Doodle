aca9b95128b3dc81196c1dd479f34c77b67b1173
uuiid <957714080@qq.com>
2022-05-27 11:50:53 +0800

    去除多余的导入

--- src/doodle_exe/main/limited_exe/gui/app.cpp
+++ src/doodle_exe/main/limited_exe/gui/app.cpp
@@ -4,24 +4,10 @@
 
 #include "app.h"
 
-#include <doodle_core/core/core_set.h>
-#include <doodle_core/thread_pool/process_pool.h>
-#include <doodle_core/thread_pool/asio_pool.h>
-#include <doodle_lib/lib_warp/imgui_warp.h>
 #include <doodle_lib/gui/main_menu_bar.h>
 #include <doodle_lib/gui/main_status_bar.h>
-#include <doodle_lib/gui/gui_ref/layout_window.h>
-#include <doodle_lib/long_task/database_task.h>
-#include <doodle_lib/platform/win/drop_manager.h>
-#include <doodle_lib/long_task/short_cut.h>
-#include <doodle_lib/core/image_loader.h>
-#include <doodle_core/core/core_sig.h>
-#include <doodle_core/core/init_register.h>
-#include <doodle_lib/gui/main_proc_handle.h>
-#include <doodle_lib/gui/get_input_dialog.h>
 
 #include <doodle_lib/core/program_options.h>
-#include <doodle_lib/lib_warp/icon_font_macro.h>
 
 #include <gui/window.h>
 using namespace doodle;
