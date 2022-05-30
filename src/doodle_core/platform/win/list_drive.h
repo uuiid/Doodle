592b6d6e77c92a32158ebfc9e6375616f3b44879
uuiid <957714080@qq.com>
2022-05-30 18:41:24 +0800

    修正构建

--- src/doodle_core/platform/win/list_drive.h
+++ src/doodle_core/platform/win/list_drive.h
@@ -3,6 +3,7 @@
 //
 #pragma once
 
+#include <doodle_core/doodle_core_fwd.h>
 #include <doodle_core/configure/doodle_core_export.h>
 
 namespace doodle::win {
--- src/doodle_lib/platform/win/wnd_proc.h
+++ src/doodle_lib/platform/win/wnd_proc.h
@@ -3,12 +3,12 @@
 //
 
 #pragma once
-#include <doodle_core/configure/doodle_core_export.h>
+#include <doodle_lib/doodle_lib_fwd.h>
 #include <doodle_core/platform/win/windows_alias.h>
 
 namespace doodle::win {
 
-class DOODLE_CORE_EXPORT d3d_device {
+class DOODLE_API d3d_device {
   static d3d_device* self;
 
  public:
