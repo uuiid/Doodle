5f8d3b35681703d19030f43b5056a01574f03f95
uuiid <957714080@qq.com>
2022-05-30 18:35:44 +0800

    添加entt测试

---
+++ src/doodle_exe/main/test/test_entt_type.cpp
@@ -0,0 +1,11 @@
+//
+// Created by TD on 2022/5/30.
+//
+#include <catch2/catch_all.hpp>
+
+#include <doodle_lib/doodle_lib_fwd.h>
+#include <doodle_lib/app/app.h>
+
+TEST_CASE("entt type"){
+  std::cout <<
+}
