e54662f73b87cb6b1ce726f4471bc3914a669bd6
uuiid <957714080@qq.com>
2022-05-30 16:35:49 +0800

    添加选中

--- src/doodle_core/database_task/select.cpp
+++ src/doodle_core/database_task/select.cpp
@@ -3,8 +3,52 @@
 //
 
 #include "select.h"
-
+#include <thread_pool/process_message.h>
+#include <doodle_core/core/doodle_lib.h>
+#include <thread_pool/thread_pool.h>
 namespace doodle {
 namespace database_n {
+class select::impl {
+ public:
+  FSys::path project;
+  bool only_ctx;
+  std::future<void> result;
+};
+
+select::select(const select::arg& in_arg) : p_i(std::make_unique<impl>()) {
+  p_i->project  = in_arg.project_path;
+  p_i->only_ctx = in_arg.only_ctx;
+}
+select::~select() = default;
+
+void select::init() {
+  auto& k_msg = g_reg()->ctx().emplace<process_message>();
+  k_msg.set_name("加载数据");
+  k_msg.set_state(k_msg.run);
+  p_i->result = g_thread_pool().enqueue([this]() {
+
+    if (!p_i->only_ctx) {
+      this->select_db();
+    }
+    this->select_ctx();
+  });
+}
+void select::succeeded() {
+}
+void select::failed() {
+}
+void select::aborted() {
+}
+void select::update(chrono::duration<chrono::system_clock::rep, chrono::system_clock::period>, void* data) {
+}
+void select::select_db() {
+}
+void select::select_ctx() {
+}
+bool select::chick_table() {
+  return false;
+}
+void select::update() {
+}
 }  // namespace database_n
 }  // namespace doodle
--- src/doodle_core/database_task/select.h
+++ src/doodle_core/database_task/select.h
@@ -6,7 +6,33 @@
 namespace doodle {
 namespace database_n {
 
-class DOODLE_CORE_EXPORT select {
+class DOODLE_CORE_EXPORT select : public process_t<select> {
+ private:
+  class impl;
+  std::unique_ptr<impl> p_i;
+
+ public:
+  using base_type = process_t<select>;
+  class DOODLE_CORE_EXPORT arg {
+   public:
+    FSys::path project_path;
+    bool only_ctx;
+  };
+  explicit select(const arg& in_arg);
+
+  ~select() override;
+  void init();
+  void succeeded();
+  void failed();
+  void aborted();
+  void update(base_type::delta_type, void* data);
+
+ private:
+  void select_db();
+  void select_ctx();
+
+  bool chick_table();
+  void update();
 };
 
 }  // namespace database_n
