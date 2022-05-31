bf39a9aab69a353ea3c9065ae2199e487e8261df
uuiid <957714080@qq.com>
2022-05-31 17:57:45 +0800

    添加插入实现

--- src/doodle_core/database_task/inster.cpp
+++ src/doodle_core/database_task/inster.cpp
@@ -6,5 +6,21 @@
 
 namespace doodle {
 namespace database_n {
+
+class inster::impl {
+ public:
+};
+
+inster::~inster() = default;
+void inster::init() {
+}
+void inster::succeeded() {
+}
+void inster::failed() {
+}
+void inster::aborted() {
+}
+void inster::update(chrono::duration<chrono::system_clock::rep, chrono::system_clock::period>, void *data) {
+}
 }  // namespace database_n
 }  // namespace doodle
