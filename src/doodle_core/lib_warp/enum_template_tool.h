e4d924ac6707a57a7261969a44e1b909d754d69c
uuiid <957714080@qq.com>
2022-06-01 11:08:07 +0800

    添加枚举模板辅助函数

--- src/doodle_core/lib_warp/enum_template_tool.h
+++ src/doodle_core/lib_warp/enum_template_tool.h
@@ -3,3 +3,17 @@
 //
 
 #pragma once
+
+#include <type_traits>
+
+namespace doodle {
+template <typename Num_T, typename Enum_T>
+auto num_to_enum(Num_T in_t) {
+  return static_cast<Enum_T>(in_t);
+}
+
+template <typename Enum_T>
+auto enum_to_num(Enum_T in_t) {
+  return static_cast<typename std::underlying_type_t<Enum_T>>(in_t);
+}
+}  // namespace doodle
