7c4a43c358941f925b78386048ae8bcdf12a3574
uuiid <957714080@qq.com>
2022-06-01 15:31:53 +0800

    添加更新框架

--- src/doodle_core/database_task/update.cpp
+++ src/doodle_core/database_task/update.cpp
@@ -4,7 +4,22 @@
 
 #include "update.h"
 
-namespace doodle {
-namespace database_n {
-}  // namespace database_n
-}  // namespace doodle
+namespace doodle::database_n {
+
+class update_data::impl {
+};
+update_data::update_data(const std::vector<entt::entity> &in_data)
+    : p_i(std::make_unique<impl>()) {
+}
+update_data::~update_data() = default;
+void update_data::init() {
+}
+void update_data::succeeded() {
+}
+void update_data::failed() {
+}
+void update_data::aborted() {
+}
+void update_data::update(chrono::duration<chrono::system_clock::rep, chrono::system_clock::period>, void *data) {
+}
+}  // namespace doodle::database_n
--- src/doodle_core/database_task/update.h
+++ src/doodle_core/database_task/update.h
@@ -3,12 +3,23 @@
 //
 #pragma once
 #include <doodle_core/doodle_core_fwd.h>
-namespace doodle {
-namespace database_n {
+namespace doodle::database_n {
 
-class DOODLE_CORE_EXPORT update {
+class DOODLE_CORE_EXPORT update_data : public process_t<update_data> {
+ private:
+  class impl;
+  std::unique_ptr<impl> p_i;
+
  public:
+  using base_type = process_t<update_data>;
+  explicit update_data(const std::vector<entt::entity>& in_data);
+
+  ~update_data() override;
+  void init();
+  void succeeded();
+  void failed();
+  void aborted();
+  void update(base_type::delta_type, void* data);
 };
 
-}  // namespace database_n
-}  // namespace doodle
+}  // namespace doodle::database_n
