aaacebca552ad48456f37b06f90d4481b627c090
uuiid <957714080@qq.com>
2022-06-01 18:20:00 +0800

    添加上下文更新

--- src/doodle_core/database_task/details/update_ctx.cpp
+++ src/doodle_core/database_task/details/update_ctx.cpp
@@ -3,13 +3,27 @@
 //
 
 #include "update_ctx.h"
+#include <doodle_core/thread_pool/process_message.h>
+#include <doodle_core/core/doodle_lib.h>
+#include <doodle_core/thread_pool/thread_pool.h>
+#include <doodle_core/logger/logger.h>
+#include <doodle_core/core/core_sql.h>
 
-namespace doodle {
-namespace database_n {
-namespace details {
+#include <doodle_core/metadata/metadata_cpp.h>
+#include <doodle_core/metadata/image_icon.h>
+#include <doodle_core/metadata/importance.h>
+#include <doodle_core/metadata/organization.h>
+#include <doodle_core/metadata/redirection_path_info.h>
+
+#include <doodle_core/generate/core/sql_sql.h>
+
+#include <sqlpp11/sqlpp11.h>
+#include <sqlpp11/sqlite3/sqlite3.h>
+
+#include <range/v3/all.hpp>
+
+namespace doodle::database_n::details {
 void update_ctx::ctx(const entt::registry& in_registry,
                      sqlpp::sqlite3::connection& in_connection) {
 }
-}  // namespace details
-}  // namespace database_n
 }  // namespace doodle
