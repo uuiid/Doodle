20428d745a90974571d97ab3dd3a21b03c090567
uuiid <957714080@qq.com>
2022-06-01 12:27:21 +0800

    完成数据转化

--- src/doodle_core/database_task/insert.cpp
+++ src/doodle_core/database_task/insert.cpp
@@ -30,18 +30,71 @@
 
 #include <boost/asio.hpp>
 namespace doodle::database_n {
+namespace sql = doodle_database;
 
 class insert::impl {
  public:
   std::vector<entt::entity> entt_list{};
 
-  std::vector<std::pair<std::int32_t, std::string>> entity_tabls;
-  std::vector<std::pair<std::int32_t, std::string>> com_tabls;
+  class com_data {
+   public:
+    std::int64_t com_id;
+    std::string json_data;
+  };
+
+  class entity_data {
+   public:
+    entt::entity entt_;
+    std::uint64_t l_id;
+    std::string uuid_data;
+    std::vector<com_data> l_coms;
+  };
+
+  std::vector<std::shared_ptr<entity_data>> main_tabls;
+  std::vector<std::pair<std::int32_t, std::string>> ctx_tabls;
 
   using boost_strand = boost::asio::strand<decltype(g_thread_pool().pool_)::executor_type>;
 
   std::vector<boost_strand> strands_{};
   std::atomic_bool stop{false};
+
+  void insert_db(sqlpp::sqlite3::connection &in_db) {
+    {
+      sql::Entity l_tabl{};
+      auto l_pre = in_db.prepare(
+          sqlpp::insert_into(l_tabl)
+              .set(
+                  l_tabl.uuidData = sqlpp::parameter(l_tabl.uuidData)));
+
+      for (auto &&i : main_tabls) {
+        l_pre.params.uuidData = i->uuid_data;
+        i->l_id                = in_db(l_pre);
+      }
+    }
+    {
+      sql::ComEntity l_tabl{};
+      auto l_pre = in_db.prepare(
+          sqlpp::insert_into(
+              l_tabl)
+              .set(
+                  l_tabl.jsonData = sqlpp::parameter(l_tabl.jsonData),
+                  l_tabl.comHash  = sqlpp::parameter(l_tabl.comHash),
+                  l_tabl.entityId = sqlpp::parameter(l_tabl.entityId)));
+      for (auto &&i : main_tabls) {
+        for (auto &&j : i->l_coms) {
+          l_pre.params.jsonData = j.json_data;
+          l_pre.params.comHash  = j.com_id;
+          l_pre.params.entityId = i->l_id;
+          auto l_size           = in_db(l_pre);
+          DOODLE_LOG_INFO("插入数据 id {}", l_size);
+        }
+      }
+    }
+  }
+
+  void create_entt_data() {
+  }
+  void create_com_data() {}
 };
 insert::insert(const std::vector<entt::entity> &in_inster)
     : p_i(std::make_unique<impl>()) {
