ca6bcce0b7f0be8e1224e6164940282d9aa90dc6
uuiid <957714080@qq.com>
2022-06-01 11:13:31 +0800

    安全转换枚举

--- src/doodle_core/lib_warp/enum_template_tool.h
+++ src/doodle_core/lib_warp/enum_template_tool.h
@@ -5,11 +5,11 @@
 #pragma once
 
 #include <type_traits>
-
+#include <boost/numeric/conversion/cast.hpp>
 namespace doodle {
 template <typename Enum_T, typename Num_T>
 Enum_T num_to_enum(Num_T in_t) {
-  return static_cast<Enum_T>(in_t);
+  return static_cast<Enum_T>(boost::numeric_cast<typename std::underlying_type_t<Enum_T>>(in_t));
 }
 
 template <typename Enum_T>
