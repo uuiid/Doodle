b070bfbc76a783627d3ba6fefe67bf6873c9ea99
uuiid <957714080@qq.com>
2022-06-16 18:18:05 +0800

    完成时间调整gui

--- src/doodle_lib/gui/widgets/time_sequencer_widget.cpp
+++ src/doodle_lib/gui/widgets/time_sequencer_widget.cpp
@@ -64,6 +64,15 @@ class time_sequencer_widget::impl {
   /// \brief 工作时间计算
   doodle::business::work_clock work_clock_{};
 
+  bool into_select{false};
+  bool outto_select{false};
+  bool set_view1{false};
+  bool set_view2{false};
+  ImPlotRect rect_select_{};
+
+  std::size_t index_begin_{0};
+  std::size_t index_end_{0};
+
   bool find_selects(const ImPlotRect& in_rect) {
     using tmp_value_t = decltype((time_list | ranges::views::enumerate).begin())::value_type;
     auto l_index      = ranges::views::ints(0, (std::int32_t)time_list.size());
@@ -221,6 +230,32 @@ class time_sequencer_widget::impl {
     refresh(time_list);
     refresh_work_time(time_list);
   }
+
+  void refresh_index() {
+    if (into_select && outto_select) {
+      auto l_ben         = ranges::find_if(time_list_x,
+                                           [&](const std::double_t& in_) -> bool {
+                                     return rect_select_.X.Contains(in_);
+                                   });
+      auto l_begin_index = ranges::distance(time_list_x.begin(), l_ben);
+      auto l_re_list     = time_list_x | ranges::views::reverse;
+      auto l_end         = ranges::find_if(l_re_list,
+                                           [&](const std::double_t& in_) -> bool {
+                                     return rect_select_.X.Contains(in_);
+                                   });
+      auto l_end_index   = ranges::distance(l_re_list.begin(), l_end);
+      auto l_tmp         = l_begin_index;
+      l_begin_index      = std::min(l_tmp, l_end_index);
+      l_end_index        = std::max(l_tmp, l_end_index);
+      index_begin_       = 0;
+      index_end_         = 0;
+      index_begin_       = std::max(std::size_t(0), std::size_t(l_begin_index));
+      index_end_         = std::min(time_list.size(), std::size_t(l_end_index));
+      index_end_         = std::min(index_end_, index_begin_ + 10);
+      into_select = outto_select = false;
+      DOODLE_LOG_INFO(" index {}->{} ", index_begin_, index_end_);
+    }
+  }
 };
 
 time_sequencer_widget::time_sequencer_widget()
@@ -262,6 +297,10 @@ void time_sequencer_widget::update(
   if (p_i->time_list.empty())
     return;
   /// \brief 时间折线
+
+  std::size_t l_index_begin{0};
+  std::size_t l_index_end{0};
+
   if (ImPlot::BeginPlot("##time")) {
     /// 设置州为时间轴
     ImPlot::SetupAxis(ImAxis_X1, nullptr, ImPlotAxisFlags_Time);
@@ -276,39 +315,39 @@ void time_sequencer_widget::update(
     ImPlot::SetupAxisLimits(ImAxis_X1, t_min, t_max);
     //    ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
 
-    if (ImPlot::IsPlotHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left) && ImGui::GetIO().KeyCtrl) {
-      auto l_select = ImPlot::GetPlotSelection();
-      p_i->modify_time();
-      p_i->find_selects(l_select);
-    }
-
     ImPlot::PlotLine("Time Series",
                      p_i->time_list_x.data(),
                      p_i->time_list_y.data(),
                      p_i->time_list.size());
-    if (ImPlot::IsPlotSelected()) {
-      if (ImGui::IsMouseClicked(ImPlot::GetInputMap().SelectCancel)) {
-        ImPlot::CancelPlotSelection();
-      }
-    }
 
-    if (p_i->rect_.X.Size() != 0 && p_i->rect_.Y.Size() != 0) {
-      if (ImPlot::DragRect(2344,
-                           &p_i->rect_.X.Min,
-                           &p_i->rect_.Y.Min,
-                           &p_i->rect_.X.Max,
-                           &p_i->rect_.Y.Max,
-                           ImVec4(1, 0, 1, 1)))
-        p_i->modify_time_refresh(p_i->rect_);
+    if (ImPlot::IsPlotSelected()) {
+      //      auto l_e   = ImPlot::GetPlotLimits().X;
+      p_i->rect_select_ = ImPlot::GetPlotSelection();
+
+      p_i->into_select = true;
+
+    } else if (p_i->into_select && !ImPlot::IsPlotSelected()) {
+      p_i->outto_select = true;
     }
+    p_i->refresh_index();
+
+    //    if (p_i->rect_.X.Size() != 0 && p_i->rect_.Y.Size() != 0) {
+    //      if (ImPlot::DragRect(2344,
+    //                           &p_i->rect_.X.Min,
+    //                           &p_i->rect_.Y.Min,
+    //                           &p_i->rect_.X.Max,
+    //                           &p_i->rect_.Y.Max,
+    //                           ImVec4(1, 0, 1, 1)))
+    //        p_i->modify_time_refresh(p_i->rect_);
+    //    }
     // 绘制可调整点
     {
       auto l_linm = ImPlot::GetPlotLimits().X;
-      for (int l_i = 0; l_i < p_i->time_list.size(); ++l_i) {
-        auto l_org_x = doodle::chrono::floor<chrono::seconds>(
-                           p_i->time_list[l_i].time_point_.zoned_time_.get_sys_time())
-                           .time_since_epoch()
-                           .count();
+      for (int l_i = p_i->index_begin_; l_i < p_i->index_end_; ++l_i) {
+        std::double_t l_org_x = doodle::chrono::floor<chrono::seconds>(
+                                    p_i->time_list[l_i].time_point_.zoned_time_.get_sys_time())
+                                    .time_since_epoch()
+                                    .count();
         if (ImPlot::DragPoint((std::int32_t)l_i,
                               (std::double_t*)&(p_i->time_list_x[l_i]),
                               &(p_i->time_list_y[l_i]), ImVec4{0, 0.9f, 0, 1})) {
