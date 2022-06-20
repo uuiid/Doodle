9c37fa3900ccf986f4650b00bd0c9aa3708eca43
uuiid <957714080@qq.com>
2022-06-20 10:38:13 +0800

    添加gui测试

---
+++ src/doodle_exe/main/test/test_gui_template.cpp
@@ -0,0 +1,12 @@
+//
+// Created by TD on 2022/6/20.
+//
+#include <doodle_lib/doodle_lib_all.h>
+#include <doodle_lib/app/app.h>
+#include <doodle_core/core/core_sig.h>
+#include <doodle_core/metadata/metadata_cpp.h>
+#include <doodle_core/metadata/time_point_wrap.h>
+#include <doodle_lib/core/work_clock.h>
+
+#include <catch.hpp>
+#include <catch2/catch_approx.hpp>
