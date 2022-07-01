ad2cb2e50a7478ed2e354f661d5989cfb9e3473d
uuiid <957714080@qq.com>
2022-07-01 16:29:45 +0800

    添加新类

---
+++ src/maya_plug/maya_comm/dem_bones_add_weight.h
@@ -0,0 +1,25 @@
+//
+// Created by TD on 2022/7/1.
+//
+
+#pragma once;
+
+#include <maya_plug/maya_plug_fwd.h>
+namespace doodle::maya_plug {
+
+namespace dem_bones_add_weight_ns {
+constexpr char name[] = "doodle_comm_dem_bones_weiget";
+MSyntax syntax();
+}
+
+class dem_bones_add_weight : public doodle::TemplateAction<
+                                 dem_bones_add_weight,
+                                 dem_bones_add_weight_ns::name,
+                                 dem_bones_add_weight_ns::syntax> {
+ public:
+  dem_bones_add_weight();
+  ~dem_bones_add_weight() override;
+  MStatus doIt(const MArgList& in_arg) override;
+};
+
+}  // namespace doodle::maya_plug
