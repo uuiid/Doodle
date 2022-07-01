ab48be482feb5a44bdffcb54d5803eca11899c2e
uuiid <957714080@qq.com>
2022-07-01 16:00:51 +0800

    重构

--- src/maya_plug/data/dem_bones_ex.h
+++ src/maya_plug/data/dem_bones_ex.h
@@ -11,25 +11,25 @@ class dem_bones_ex : public ::Dem::DemBonesExt<std::double_t, std::double_t> {
   dem_bones_ex()  = default;
   ~dem_bones_ex() = default;
   // 初始化中每次分割骨簇之前调用回调函数。
-  void cbInitSplitBegin() override{};
+  void cbInitSplitBegin() override;
   // 初始化中每次对骨簇进行分割后都会调用回调函数
-  void cbInitSplitEnd() override{};
+  void cbInitSplitEnd() override;
   // 在每次全局迭代更新之前调用回调函数
-  void cbIterBegin() override{};
+  void cbIterBegin() override;
   // 在每次全局迭代更新后调用回调函数，如果返回true，则停止迭代。
-  bool cbIterEnd() override { return false; };
+  bool cbIterEnd() override;
   // 在每个外观权重更新之前调用的回调函数
-  void cbWeightsBegin() override{};
+  void cbWeightsBegin() override;
   // 每次蒙皮权重更新后调用的回调函数
-  void cbWeightsEnd() override{};
+  void cbWeightsEnd() override;
   // 在每次骨骼转换更新之前调用的回调函数
-  void cbTranformationsBegin() override{};
+  void cbTranformationsBegin() override;
   // 每次骨骼转换更新后调用的回调函数
-  void cbTransformationsEnd() override{};
+  void cbTransformationsEnd() override;
 
   // 每个局部骨骼转换更新迭代后调用的回调函数，如果返回true，则停止迭代
-  void cbWeightsIterBegin() override{};
-  //	在每个局部权重更新迭代后调用的回调函数，如果返回true，则停止迭代。
-  bool cbWeightsIterEnd() override { return false; };
+  void cbWeightsIterBegin() override;
+  // 在每个局部权重更新迭代后调用的回调函数
+  bool cbWeightsIterEnd() override;
 };
 }  // namespace doodle::maya_plug
