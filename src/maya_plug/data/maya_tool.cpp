1328e3224a2e3c5993a66bc68454b18879127037
uuiid <957714080@qq.com>
2022-07-01 15:13:28 +0800

    maya工具修正

--- src/maya_plug/data/maya_tool.cpp
+++ src/maya_plug/data/maya_tool.cpp
@@ -24,6 +24,7 @@ MPlug get_plug(const MObject& in_node, const std::string& in_name) {
   try {
     l_plug = l_node.findPlug(d_str{in_name}, false, &k_s);
     DOODLE_CHICK(k_s);
+    return l_plug;
   } catch (const maya_InvalidParameter& error) {
     DOODLE_LOG_INFO("没有在这个节点中找到属性")
   }
