cff4b21e3976ef81c84bc3fc4c41e5aaf08a4731
uuiid <957714080@qq.com>
2022-07-07 13:46:57 +0800

    取消python中文界面

--- script/maya/scripts/scripts/dem_cloth_to_fbx.py
+++ script/maya/scripts/scripts/dem_cloth_to_fbx.py
@@ -3,28 +3,3 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
-class dem_cloth_to_fbx(QWidget):
-
-    def __init__(self):
-        super(dem_cloth_to_fbx, self).__init__()
-
-        self.nbone = 30
-
-        self.setObjectName("dem_cloth_to_fbx")
-        self.setWindowTitle("转换骨骼物体")
-        self.setGeometry(50, 50, 250, 150)
-        self.initUI()
-
-    def initUI(self):
-        self.layout = QHBoxLayout(self)
-        self.lable = QLabel().setText("骨骼数")
-        self.layout.addWidget(self.lable)
-        self.nbone_w = QSpinBox(self)
-        self.nbone_w.setValue(self.nbone)
-        self.nbone_w.valueChanged.connect(lambda x: self.set_bones(x))
-        self.layout.addWidget(self.nbone_w)
-
-        self.setLayout(self.layout)
-
-    def set_bones(self, n):
-        self.nbone = n
