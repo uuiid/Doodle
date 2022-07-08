30a506e8ed57884c29dd0dd3c34417f4268df642
uuiid <957714080@qq.com>
2022-07-08 17:28:16 +0800

    添加ue4集群ui

---
+++ script/uePlug/Doodle/Source/DoodleClusterSequencer/Private/DoodleClusterSequencerModule.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "GeometryCacheSequencerModule.h"
+#include "ISequencerModule.h"
+#include "Modules/ModuleInterface.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "DoodleClusterSequencer"
+
+IMPLEMENT_MODULE(FDoodleClusterSequencerModule, DoodleClusterSequencer);
+
+#undef LOCTEXT_NAMESPACE
