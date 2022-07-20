0749aa74c23aa7f3773059603b532f236b86d23d
uuiid <957714080@qq.com>
2022-07-20 10:34:30 +0800

    添加解算新配置

---
+++ src/doodle_core/metadata/qcloth_sim_attr.cpp
@@ -0,0 +1,8 @@
+//
+// Created by TD on 2022/7/20.
+//
+
+#include "qcloth_sim_attr.h"
+
+namespace doodle {
+}  // namespace doodle
---
+++ src/doodle_core/metadata/qcloth_sim_attr.h
@@ -0,0 +1,13 @@
+//
+// Created by TD on 2022/7/20.
+//
+
+#pragma once
+
+#include <doodle_lib/doodle_lib_fwd.h>
+namespace doodle {
+
+class qcloth_sim_attr {
+};
+
+}  // namespace doodle
