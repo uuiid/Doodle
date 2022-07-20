f5ba7be2e7693fdcb6b2a3b420855540ac5f067b
uuiid <957714080@qq.com>
2022-07-20 11:23:55 +0800

    添加数据

--- src/doodle_core/metadata/qcloth_sim_attr.cpp
+++ src/doodle_core/metadata/qcloth_sim_attr.cpp
@@ -5,4 +5,20 @@
 #include "qcloth_sim_attr.h"
 
 namespace doodle {
+void to_json(nlohmann::json& j, const qcloth_sim_attr& p) {
+  j["simple_subsampling"] = p.simple_subsampling;
+  j["frame_samples"]      = p.frame_samples;
+  j["time_scale"]         = p.time_scale;
+  j["length_scale"]       = p.length_scale;
+  j["max_cg_iteration"]   = p.max_cg_iteration;
+  j["cg_accuracy"]        = p.cg_accuracy;
+}
+void from_json(const nlohmann::json& j, qcloth_sim_attr& p) {
+  j.at("simple_subsampling").get_to(p.simple_subsampling);
+  j.at("frame_samples").get_to(p.frame_samples);
+  j.at("time_scale").get_to(p.time_scale);
+  j.at("length_scale").get_to(p.length_scale);
+  j.at("max_cg_iteration").get_to(p.max_cg_iteration);
+  j.at("cg_accuracy").get_to(p.cg_accuracy);
+}
 }  // namespace doodle
--- src/doodle_core/metadata/qcloth_sim_attr.h
+++ src/doodle_core/metadata/qcloth_sim_attr.h
@@ -6,8 +6,25 @@
 
 #include <doodle_lib/doodle_lib_fwd.h>
 namespace doodle {
+class qcloth_sim_attr;
+void to_json(nlohmann::json& j, const qcloth_sim_attr& p);
+void from_json(const nlohmann::json& j, qcloth_sim_attr& p);
 
-class qcloth_sim_attr {
+class DOODLE_CORE_EXPORT qcloth_sim_attr {
+ public:
+  bool simple_subsampling{true};
+
+  std::int32_t frame_samples{6};
+  std::float_t time_scale{1.0f};
+  std::float_t length_scale{1.0f};
+  std::int32_t max_cg_iteration{1000};
+  std::int32_t cg_accuracy{9};
+
+  qcloth_sim_attr() = default;
+
+ private:
+  friend void to_json(nlohmann::json& j, const qcloth_sim_attr& p);
+  friend void from_json(const nlohmann::json& j, qcloth_sim_attr& p);
 };
 
 }  // namespace doodle
