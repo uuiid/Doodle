47266d4cc621f84a09c7a7fd75aa11a5e3b9192e
uuiid <957714080@qq.com>
2022-07-21 11:31:56 +0800

    添加命令

--- src/maya_plug/maya_comm/create_qcloth_assets.cpp
+++ src/maya_plug/maya_comm/create_qcloth_assets.cpp
@@ -5,8 +5,42 @@
 #include "create_qcloth_assets.h"
 
 namespace doodle::maya_plug {
-MSyntax create_qcloth_assets_ns::syntax() {
-  return MSyntax();
+namespace create_qcloth_assets_ns {
+
+char cloth[]       = {"-c"};
+char cloth_l[]     = {"-cloth"};
+
+char high[]        = {"-h"};
+char high_l[]      = {"-high"};
+
+char collision[]   = {"-co"};
+char collision_l[] = {"-collision"};
+
+MSyntax syntax() {
+  MSyntax l_syntax{};
+  l_syntax.addFlag(cloth, cloth_l, MSyntax::MArgType::kString);
+  l_syntax.addFlag(high, high_l, MSyntax::MArgType::kString);
+  l_syntax.addFlag(collision, collision_l, MSyntax::MArgType::kString);
+
+  l_syntax.makeFlagMultiUse(cloth);
+  l_syntax.makeFlagMultiUse(high);
+  l_syntax.makeFlagMultiUse(collision);
+  return l_syntax;
+}
+}  // namespace create_qcloth_assets_ns
+
+class create_qcloth_assets::impl {
+ public:
+  
+
+
+
+};
+
+create_qcloth_assets::create_qcloth_assets()
+    : p_i(std::make_unique<impl>()) {
+}
+void create_qcloth_assets::parse_arg(const MArgList& in_arg) {
 }
 MStatus create_qcloth_assets::doIt(const MArgList& in_arg) {
   return redoIt();
@@ -21,4 +55,5 @@ bool create_qcloth_assets::isUndoable() const {
   return true;
 }
 
+create_qcloth_assets::~create_qcloth_assets() = default;
 }  // namespace doodle::maya_plug
--- src/maya_plug/maya_comm/create_qcloth_assets.h
+++ src/maya_plug/maya_comm/create_qcloth_assets.h
@@ -13,11 +13,19 @@ MSyntax syntax();
 class create_qcloth_assets : public TemplateAction<create_qcloth_assets,
                                                    create_qcloth_assets_ns::name,
                                                    create_qcloth_assets_ns::syntax> {
+  class impl;
+  std::unique_ptr<impl> p_i;
+
+  void parse_arg(const MArgList& in_arg);
+
  public:
+  create_qcloth_assets();
+  ~create_qcloth_assets() override;
   [[maybe_unused]] MStatus doIt(const MArgList& in_arg) override;
+
   [[maybe_unused]] MStatus undoIt() override;
   [[maybe_unused]] MStatus redoIt() override;
   [[maybe_unused]] [[nodiscard]] bool isUndoable() const override;
 };
 
-}  // namespace doodle
+}  // namespace doodle::maya_plug
