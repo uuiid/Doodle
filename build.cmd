4c50e1a172ed7bd7fe44525d3d8b1f2a2b3b18a6
uuiid <957714080@qq.com>
2022-07-27 17:16:38 +0800

    修正退出

--- build.cmd
+++ build.cmd
@@ -11,24 +11,34 @@ echo -----------------config main exe--------------------
 -S%my_pwd% ^
 --preset Ninja_release
 
+if %errorlevel% NEQ 0 exit 1
+
 echo -----------------build main exe--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
 --preset release_exe
 
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------install main exe--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component exe_com
 
+
+if %errorlevel% NEQ 0 exit 1
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component ue4_plug
 
+
+if %errorlevel% NEQ 0 exit 1
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component doodle_houdini_com
 
+
+if %errorlevel% NEQ 0 exit 1
 
 echo -----------------clear main--------------------
 del  %my_pwd%build\Ninja_release\CMakeCache.txt
@@ -38,14 +48,20 @@ echo -----------------config maya 2018--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 -S%my_pwd% ^
 --preset Ninja_release -DMaya_Version=2018
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------build maya 2018--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
 --preset release_exe --target doodle_maya
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------install maya 2018--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component maya_plug_com
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------clear maya 2018--------------------
 rmdir /q /s %my_pwd%build\Ninja_release\src\maya_plug
 rmdir /q /s %my_pwd%build\Ninja_release\plug
@@ -56,14 +72,20 @@ echo -----------------config maya 2019--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 -S%my_pwd% ^
 --preset Ninja_release -DMaya_Version=2019
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------build maya 2019--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
 --preset release_exe --target doodle_maya
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------install maya 2019--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component maya_plug_com
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------clear maya 2019--------------------
 rmdir /q /s %my_pwd%build\Ninja_release\src\maya_plug
 rmdir /q /s %my_pwd%build\Ninja_release\plug
@@ -74,14 +96,20 @@ echo -----------------config maya 2020--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 -S%my_pwd% ^
 --preset Ninja_release -DMaya_Version=2020
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------build maya 2020--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
 --preset release_exe --target doodle_maya
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------install maya 2020--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component maya_plug_com
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------clear maya 2020--------------------
 rmdir /q /s %my_pwd%build\Ninja_release\src\maya_plug
 rmdir /q /s %my_pwd%build\Ninja_release\plug
@@ -91,10 +119,14 @@ echo -----------------config pack--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 -S%my_pwd% ^
 --preset Ninja_release
+
+if %errorlevel% NEQ 0 exit 1
 echo -----------------pack---------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
 --preset cmake_pack
+
+if %errorlevel% NEQ 0 exit 1
 
 echo -----------------copy file--------------------
 robocopy %my_pwd%build\Ninja_release\html %my_pwd%build\html /s /NFL /NDL
