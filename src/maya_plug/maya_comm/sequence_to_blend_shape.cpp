3f424231805599b57a76e096bc9d51c7d8e6a4f4
uuiid <957714080@qq.com>
2022-07-29 10:56:09 +0800

    添加队列转换为融合变形

--- src/maya_plug/maya_comm/sequence_to_blend_shape.cpp
+++ src/maya_plug/maya_comm/sequence_to_blend_shape.cpp
@@ -6,5 +6,44 @@
 
 namespace doodle {
 namespace maya_plug {
+
+constexpr char startFrame_f[]  = "-sf";
+constexpr char startFrame_lf[] = "-startFrame";
+constexpr char endFrame_f[]    = "-ef";
+constexpr char endFrame_lf[]   = "-endFrame";
+
+constexpr char bindFrame_f[]   = "-bf";
+constexpr char bindFrame_lf[]  = "-bindFrame";
+
+constexpr char parent_f[]      = "-p";
+constexpr char parent_lf[]     = "-parent";
+namespace sequence_to_blend_shape_ns {
+MSyntax syntax() {
+  MSyntax syntax{};
+  syntax.addFlag(startFrame_f, startFrame_lf, MSyntax::kTime);
+  syntax.addFlag(endFrame_f, endFrame_lf, MSyntax::kTime);
+  syntax.addFlag(bindFrame_f, bindFrame_lf, MSyntax::kTime);
+  syntax.addFlag(parent_f, parent_lf, MSyntax::kString);
+  return syntax;
+}
+
+}  // namespace sequence_to_blend_shape_ns
+
+class sequence_to_blend_shape::impl {
+ public:
+};
+
+sequence_to_blend_shape::sequence_to_blend_shape()
+    : p_i(std::make_unique<impl>()) {
+}
+
+MStatus sequence_to_blend_shape::doIt(const MArgList& in_arg) {
+  return MStatus();
+}
+void sequence_to_blend_shape::get_arg(const MArgList& in_arg) {
+}
+
+sequence_to_blend_shape::~sequence_to_blend_shape() = default;
+
 }  // namespace maya_plug
 }  // namespace doodle
--- src/maya_plug/maya_comm/sequence_to_blend_shape.h
+++ src/maya_plug/maya_comm/sequence_to_blend_shape.h
@@ -2,15 +2,27 @@
 // Created by TD on 2022/7/29.
 //
 #pragma once
-
+#include <maya_plug/maya_plug_fwd.h>
 namespace doodle {
 namespace maya_plug {
 
 namespace sequence_to_blend_shape_ns {
+constexpr char name[] = "doodle_sequence_to_blend_shape";
+MSyntax syntax();
+}  // namespace sequence_to_blend_shape_ns
 
-}
+class sequence_to_blend_shape : public doodle::TemplateAction<
+                                    sequence_to_blend_shape,
+                                    sequence_to_blend_shape_ns::name,
+                                    sequence_to_blend_shape_ns::syntax> {
+  class impl;
+  std::unique_ptr<impl> p_i;
+  void get_arg(const MArgList& in_arg);
+ public:
+  sequence_to_blend_shape();
+  ~sequence_to_blend_shape() override;
 
-class sequence_to_blend_shape {
+  MStatus doIt(const MArgList& in_arg) override;
 };
 
 }  // namespace maya_plug
