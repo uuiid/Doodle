4c8ea59bc63e682b2a520a625d66e3c536af2f53
uuiid <957714080@qq.com>
2022-07-29 09:48:01 +0800

    添加队列转换为融合变形

---
+++ src/maya_plug/maya_comm/sequence_to_blend_shape.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/7/29.
+//
+
+#include "sequence_to_blend_shape.h"
+
+namespace doodle {
+namespace maya_plug {
+}  // namespace maya_plug
+}  // namespace doodle
---
+++ src/maya_plug/maya_comm/sequence_to_blend_shape.h
@@ -0,0 +1,17 @@
+//
+// Created by TD on 2022/7/29.
+//
+#pragma once
+
+namespace doodle {
+namespace maya_plug {
+
+namespace sequence_to_blend_shape_ns {
+
+}
+
+class sequence_to_blend_shape {
+};
+
+}  // namespace maya_plug
+}  // namespace doodle
