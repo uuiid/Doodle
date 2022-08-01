0bc6c400c5c12b157c672556c001125194e1abc1
uuiid <957714080@qq.com>
2022-08-01 11:08:54 +0800

    log

--- src/doodle_core/database_task/update.cpp
+++ src/doodle_core/database_task/update.cpp
@@ -82,6 +82,7 @@ class update_data::impl {
         inster_map[std::ref(i)] = true;
         break;
       }
+      DOODLE_LOG_INFO("检查实体 {} 组件 {}  --> {}", i.entt_, i.com_id, inster_map[std::ref(i)]);
     }
 
     /// \brief 不存在就插入
@@ -98,7 +99,7 @@ class update_data::impl {
         l_pre_inster.params.entityId = main_tabls[i.first.get().entt_];
         l_pre_inster.params.jsonData = i.first.get().json_data;
         auto l_s                     = in_db(l_pre_inster);
-        DOODLE_LOG_INFO("检查数据库id {} 存在(不存在时插入)", l_s);
+        DOODLE_LOG_INFO("插入数据库id {} -> 实体 {} 组件 {} ", l_s, i.first.get().entt_, i.first.get().com_id);
         g_reg()->ctx().emplace<process_message>().progress_step({1, size * 2});
       }
     }
@@ -117,7 +118,7 @@ class update_data::impl {
         l_pre.params.comHash  = i.first.get().com_id;
         l_pre.params.entityId = main_tabls[i.first.get().entt_];
         auto l_s              = in_db(l_pre);
-        DOODLE_LOG_INFO("更新数据库id {}", l_s);
+        DOODLE_LOG_INFO("插入数据库id {} -> 实体 {} 组件 {} ", l_s, i.first.get().entt_, i.first.get().com_id);
         g_reg()->ctx().emplace<process_message>().progress_step({1, size * 2});
       }
     }
