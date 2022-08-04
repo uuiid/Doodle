7248c46d84a01c7d542283e53e678ee43e0cab33
uuiid <957714080@qq.com>
2022-08-04 17:46:55 +0800

    工作时钟制作

--- src/doodle_core/time_tool/work_clock.cpp
+++ src/doodle_core/time_tool/work_clock.cpp
@@ -23,8 +23,8 @@ namespace business {
 work_clock::work_clock() = default;
 
 chrono::hours_double work_clock::operator()(
-    const chrono::local_time_pos& in_min,
-    const chrono::local_time_pos& in_max) const {
+    const time_type& in_min,
+    const time_type& in_max) const {
   auto l_d = discrete_interval_time::right_open(in_min,
                                                 in_max);
   auto l_l = interval_set_time_ & l_d;
@@ -35,8 +35,9 @@ chrono::hours_double work_clock::operator()(
   return l_len;
 }
 
-chrono::local_time_pos work_clock::next_time(const chrono::local_time_pos& in_begin,
-                                             const chrono::local_time_pos::duration& in_du) const {
+work_clock::time_type work_clock::next_time(
+    const time_type& in_begin,
+    const time_type::duration& in_du) const {
   auto l_d = discrete_interval_time::right_open(in_begin, in_begin.max());
   auto l_l = interval_set_time_ & l_d;
   chrono::hours_double l_len{};
@@ -57,19 +58,17 @@ void work_clock::gen_rules_(const discrete_interval_time& in_time) {
   /// \brief 如果已经包含就直接返回
   //  if (!boost::icl::contains(boost::icl::hull(interval_set_time_), in_time)) return;
 
-  auto l_begin = doodle::chrono::floor<doodle::chrono::days>(boost::icl::first(in_time));
-  auto l_end   = doodle::chrono::floor<doodle::chrono::days>(boost::icl::last(in_time));
+  auto l_begin = boost::icl::first(in_time);
+  auto l_end   = boost::icl::last(in_time);
 
   interval_set_time l_r;
   for (;
        l_begin <= l_end;
        l_begin += chrono::days{1}) {
     /// \brief 加入工作日规定时间
-    chrono::local_days l_local_days{l_begin};
-    chrono::weekday l_weekday{l_begin};
-    if (rules_.work_weekdays()[l_weekday.c_encoding()]) {
+    if (rules_.work_weekdays()[l_begin.get_week_int()]) {
       ranges::for_each(rules_.work_time(), [&](const std::pair<chrono::seconds,
-                                                             chrono::seconds>& in_pair) {
+                                                               chrono::seconds>& in_pair) {
         l_r += discrete_interval_time::right_open(l_begin + in_pair.first,
                                                   l_begin + in_pair.second);
       });
@@ -77,28 +76,28 @@ void work_clock::gen_rules_(const discrete_interval_time& in_time) {
   }
   /// \brief 减去节假日
   ranges::for_each(
-      rules_.extra_holidays,
-      [&](const decltype(rules_.extra_holidays)::value_type& in_) {
+      rules_.extra_holidays(),
+      [&](const std::decay_t<decltype(rules_.extra_holidays())>::value_type& in_) {
         l_r -= discrete_interval_time::right_open(in_.first, in_.second);
       });
   /// \brief 减去调休
   ranges::for_each(
-      rules_.extra_rest,
-      [&](const decltype(rules_.extra_rest)::value_type& in_) {
+      rules_.extra_rest(),
+      [&](const std::decay_t<decltype(rules_.extra_rest())>::value_type& in_) {
         l_r -= discrete_interval_time::right_open(in_.first, in_.second);
       });
   /// \brief 加上加班
   ranges::for_each(
-      rules_.extra_work,
-      [&](const decltype(rules_.extra_work)::value_type& in_) {
+      rules_.extra_work(),
+      [&](const std::decay_t<decltype(rules_.extra_work())>::value_type& in_) {
         l_r += discrete_interval_time::right_open(in_.first, in_.second);
       });
   interval_set_time_ = l_r;
 }
 
 void work_clock::generate_interval_map_time_(const discrete_interval_time& in_time) {
-  auto l_begin = doodle::chrono::floor<doodle::chrono::days>(boost::icl::first(in_time));
-  auto l_end   = doodle::chrono::floor<doodle::chrono::days>(boost::icl::last(in_time));
+  //  auto l_begin = boost::icl::first(in_time);
+  //  auto l_end   = boost::icl::last(in_time);
 
   interval_map_time l_r;
   //  for (;
@@ -119,22 +118,22 @@ void work_clock::generate_interval_map_time_(const discrete_interval_time& in_ti
 
   /// \brief 减去节假日
   ranges::for_each(
-      rules_.extra_holidays,
-      [&](const decltype(rules_.extra_holidays)::value_type& in_) {
+      rules_.extra_holidays(),
+      [&](const std::decay_t<decltype(rules_.extra_holidays())>::value_type& in_) {
         l_r += std::make_pair(discrete_interval_time::right_open(in_.first, in_.second),
                               info_type{"节假日"});
       });
   /// \brief 减去调休
   ranges::for_each(
-      rules_.extra_rest,
-      [&](const decltype(rules_.extra_rest)::value_type& in_) {
+      rules_.extra_rest(),
+      [&](const std::decay_t<decltype(rules_.extra_rest())>::value_type& in_) {
         l_r += std::make_pair(discrete_interval_time::right_open(in_.first, in_.second),
                               info_type{in_.info});
       });
   /// \brief 加上加班
   ranges::for_each(
-      rules_.extra_work,
-      [&](const decltype(rules_.extra_work)::value_type& in_) {
+      rules_.extra_work(),
+      [&](const std::decay_t<decltype(rules_.extra_work())>::value_type& in_) {
         l_r += std::make_pair(discrete_interval_time::right_open(in_.first, in_.second),
                               info_type{in_.info});
       });
@@ -146,17 +145,19 @@ void work_clock::set_rules(const rules& in_rules) {
   interval_set_time_.clear();
   interval_map_time_.clear();
 }
-void work_clock::set_interval(const chrono::local_time_pos& in_min,
-                              const chrono::local_time_pos& in_max) {
+void work_clock::set_interval(const time_type& in_min,
+                              const time_type& in_max) {
   auto l_item = discrete_interval_time::right_open(in_min,
                                                    in_max);
   gen_rules_(l_item);
   generate_interval_map_time_(l_item);
 }
-std::vector<std::pair<work_clock::time_d_t, work_clock::time_d_t>> work_clock::get_work_du(
-    const chrono::local_time_pos& in_min,
-    const chrono::local_time_pos& in_max) const {
-  std::vector<std::pair<time_d_t, time_d_t>> l_r{};
+
+std::vector<std::pair<work_clock::time_type, work_clock::time_type>>
+work_clock::get_work_du(
+    const time_type& in_min,
+    const time_type& in_max) const {
+  std::vector<std::pair<time_type, time_type>> l_r{};
   auto l_d = discrete_interval_time::right_open(in_min,
                                                 in_max);
   auto l_l = interval_set_time_ & l_d;
@@ -173,10 +174,9 @@ std::string work_clock::debug_print() {
                      interval_map_time_);
 }
 std::optional<std::string> work_clock::get_time_info(
-    const time_point_wrap& in_min,
-    const time_point_wrap& in_max) {
-  auto l_d    = discrete_interval_time::right_open(chrono::floor<time_d_t::duration>(in_min.zoned_time_.get_local_time()),
-                                                   chrono::floor<time_d_t::duration>(in_max.zoned_time_.get_local_time()));
+    const time_type& in_min,
+    const time_type& in_max) {
+  auto l_d    = discrete_interval_time::right_open(in_min, in_max);
 
   auto l_item = interval_map_time_ & l_d;
 
@@ -189,26 +189,5 @@ std::optional<std::string> work_clock::get_time_info(
 }
 
 }  // namespace business
-
-namespace detail {
-
-chrono::hours_double work_duration(const chrono::local_time_pos& in_s,
-                                   const chrono::local_time_pos& in_e,
-                                   const business::rules& in_rules) {
-  business::work_clock l_c{};
-  l_c.set_rules(in_rules);
-  l_c.set_interval(in_s, in_e);
-  return l_c(in_s, in_e);
-}
-chrono::local_time_pos next_time(const chrono::local_time_pos& in_s,
-                                 const chrono::local_time_pos::duration& in_du_time,
-                                 const business::rules& in_rules) {
-  business::work_clock l_c{};
-  l_c.set_rules(in_rules);
-  l_c.set_interval(in_s, in_s + doodle::chrono::days{33});
-  return l_c.next_time(in_s,
-                       doodle::chrono::floor<chrono::local_time_pos::duration>(in_du_time));
-}
-}  // namespace detail
 
 }  // namespace doodle
--- src/doodle_core/time_tool/work_clock.h
+++ src/doodle_core/time_tool/work_clock.h
@@ -15,18 +15,18 @@
 #include <boost/icl/discrete_interval.hpp>
 #include <boost/icl/interval_map.hpp>
 #include <boost/icl/interval_set.hpp>
-
+#include <doodle_core/metadata/detail/boost_lcl_time_point_adaptation.h>
 namespace doodle {
 
 namespace business {
 
 class DOODLE_CORE_EXPORT work_clock {
   rules rules_;
-  using time_d_t               = doodle::chrono::local_time_pos;
+  using time_type              = doodle::time_point_wrap;
   using info_type              = std::set<std::string>;
-  using discrete_interval_time = boost::icl::discrete_interval<time_d_t>;
-  using interval_set_time      = boost::icl::interval_set<time_d_t>;
-  using interval_map_time      = boost::icl::interval_map<time_d_t, info_type>;
+  using discrete_interval_time = boost::icl::discrete_interval<time_type>;
+  using interval_set_time      = boost::icl::interval_set<time_type>;
+  using interval_map_time      = boost::icl::interval_map<time_type, info_type>;
 
   void gen_rules_(const discrete_interval_time& in_time);
   void generate_interval_map_time_(const discrete_interval_time& in_time);
@@ -37,18 +37,13 @@ class DOODLE_CORE_EXPORT work_clock {
   work_clock();
 
   void set_rules(const rules& in_rules);
-  void set_interval(const chrono::local_time_pos& in_min,
-                    const chrono::local_time_pos& in_max);
   /**
    * @brief 设置工作时间时钟的开始和结束(缓存)
    * @param in_min
    * @param in_max
    */
-  inline void set_interval(const doodle::time_point_wrap& in_min,
-                           const doodle::time_point_wrap& in_max) {
-    set_interval(doodle::chrono::floor<chrono::local_time_pos::duration>(in_min.zoned_time_.get_local_time()),
-                 doodle::chrono::floor<chrono::local_time_pos::duration>(in_max.zoned_time_.get_local_time()));
-  };
+  void set_interval(const time_type& in_min,
+                    const time_type& in_max);
 
   /**
    * @brief 获取两个时间点点工作时间(按照规则获取)
@@ -56,29 +51,8 @@ class DOODLE_CORE_EXPORT work_clock {
    * @param in_max 结束时间
    * @return 工作时间
    */
-  chrono::hours_double operator()(const chrono::local_time_pos& in_min,
-                                  const chrono::local_time_pos& in_max) const;
-
-  inline chrono::hours_double operator()(const doodle::time_point_wrap& in_min,
-                                         const doodle::time_point_wrap& in_max) const {
-    return (*this)(doodle::chrono::floor<chrono::local_time_pos::duration>(in_min.zoned_time_.get_local_time()),
-                   doodle::chrono::floor<chrono::local_time_pos::duration>(in_max.zoned_time_.get_local_time()));
-  };
-  /**
-   * @copybrief chrono::hours_double operator()(const chrono::local_time_pos& in_min, const chrono::local_time_pos& in_max) const
-   * @tparam Duration_  模板时间段
-   */
-  template <typename Duration_, std::enable_if_t<
-                                    !std::is_same_v<chrono::time_point<chrono::local_t, Duration_>,
-                                                    chrono::local_time_pos>,
-                                    bool> = true>
-  chrono::hours_double operator()(
-      const chrono::time_point<chrono::local_t, Duration_>& in_s,
-      const chrono::time_point<chrono::local_t, Duration_>& in_e) {
-    return (*this)(
-        chrono::floor<chrono::seconds>(in_s),
-        chrono::floor<chrono::seconds>(in_e));
-  };
+  chrono::hours_double operator()(const time_type& in_min,
+                                  const time_type& in_max) const;
 
   /**
    * @brief 根据传入的开始时间和工作时间段获取下一个时间点
@@ -86,34 +60,8 @@ class DOODLE_CORE_EXPORT work_clock {
    * @param in_du 时间段
    * @return 下一个时间点
    */
-  chrono::local_time_pos next_time(const chrono::local_time_pos& in_begin,
-                                   const chrono::local_time_pos::duration& in_du) const;
-
-  template <typename Duration_, typename Duration2_,
-            std::enable_if_t<
-                !std::is_same_v<chrono::time_point<chrono::local_t, Duration_>,
-                                chrono::local_time_pos>,
-                bool> = true>
-  chrono::time_point<chrono::local_t, Duration_> next_time(
-      const chrono::time_point<chrono::local_t, Duration_>& in_s,
-      const Duration2_& in_du_time) {
-    return next_time(
-        chrono::floor<chrono::seconds>(in_s),
-        chrono::floor<chrono::seconds>(in_du_time));
-  };
-
-  template <typename Duration_,
-            std::enable_if_t<
-                !std::is_same_v<chrono::time_point<chrono::local_t, Duration_>,
-                                chrono::local_time_pos>,
-                bool> = true>
-  chrono::time_point<chrono::local_t, Duration_> next_time(
-      const time_point_wrap& in_s,
-      const Duration_& in_du_time) {
-    return next_time(
-        chrono::floor<chrono::seconds>(in_s.zoned_time_.get_local_time()),
-        chrono::floor<chrono::seconds>(in_du_time));
-  };
+  time_type next_time(const time_type& in_begin,
+                      const time_type::duration& in_du) const;
 
   /**
    * @brief 获取两个时间点之间点时间分段( 休息时间段 -> 工作时间段)
@@ -121,15 +69,9 @@ class DOODLE_CORE_EXPORT work_clock {
    * @param in_max 结束时间
    * @return 时间段
    */
-  [[nodiscard("")]] std::vector<std::pair<time_d_t, time_d_t>> get_work_du(
-      const chrono::local_time_pos& in_min,
-      const chrono::local_time_pos& in_max) const;
-
-  [[nodiscard("")]] inline auto get_work_du(const doodle::time_point_wrap& in_min,
-                                            const doodle::time_point_wrap& in_max) const {
-    return get_work_du(doodle::chrono::floor<chrono::local_time_pos::duration>(in_min.zoned_time_.get_local_time()),
-                       doodle::chrono::floor<chrono::local_time_pos::duration>(in_max.zoned_time_.get_local_time()));
-  };
+  [[nodiscard("")]] std::vector<std::pair<time_type, time_type>> get_work_du(
+      const time_type& in_min,
+      const time_type& in_max) const;
 
   /**
    * @brief 获取当前点所在时间段段的备注
@@ -137,45 +79,10 @@ class DOODLE_CORE_EXPORT work_clock {
    * @return 可选段备注
    */
   std::optional<std::string> get_time_info(
-      const doodle::time_point_wrap& in_min,
-      const doodle::time_point_wrap& in_max);
+      const time_type& in_min,
+      const time_type& in_max);
 
   std::string debug_print();
 };
 }  // namespace business
-namespace detail {
-chrono::hours_double work_duration(
-    const chrono::local_time_pos& in_s,
-    const chrono::local_time_pos& in_e,
-    const business::rules& in_rules);
-
-chrono::local_time_pos next_time(
-    const chrono::local_time_pos& in_s,
-    const chrono::local_time_pos::duration& in_du_time,
-    const business::rules& in_rules);
-}  // namespace detail
-
-template <typename Duration_>
-chrono::hours_double work_duration(
-    const chrono::time_point<chrono::local_t, Duration_>& in_s,
-    const chrono::time_point<chrono::local_t, Duration_>& in_e,
-    const business::rules& in_rules) {
-  return detail::work_duration(
-      chrono::floor<chrono::seconds>(in_s),
-      chrono::floor<chrono::seconds>(in_e),
-      in_rules);
-};
-
-template <typename Duration_, typename Duration2_>
-chrono::time_point<chrono::local_t, Duration_> next_time(
-    const chrono::time_point<chrono::local_t, Duration_>& in_s,
-    const Duration2_& in_du_time,
-    const business::rules& in_rules) {
-  return detail::next_time(
-      chrono::floor<chrono::seconds>(in_s),
-      chrono::floor<chrono::seconds>(in_du_time),
-      in_rules);
-};
 }  // namespace doodle
-
-
