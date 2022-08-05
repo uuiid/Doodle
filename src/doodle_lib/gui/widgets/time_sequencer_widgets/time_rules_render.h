e033656046b84a0411c5f72d3c406f258c0fa397
uuiid <957714080@qq.com>
2022-08-05 09:11:23 +0800

    ...

--- src/doodle_lib/gui/widgets/time_sequencer_widgets/time_rules_render.h
+++ src/doodle_lib/gui/widgets/time_sequencer_widgets/time_rules_render.h
@@ -2,18 +2,17 @@
 // Created by TD on 2022/8/4.
 //
 
-#ifndef DOODLE_SRC_DOODLE_LIB_GUI_WIDGETS_TIME_SEQUENCER_WIDGETS_TIME_RULES_RENDER_H_
-#define DOODLE_SRC_DOODLE_LIB_GUI_WIDGETS_TIME_SEQUENCER_WIDGETS_TIME_RULES_RENDER_H_
+#pragma once
+
+#include <doodle_lib/doodle_lib_fwd.h>
 
 namespace doodle {
 namespace gui {
 namespace time_sequencer_widget_ns {
 
-class time_rules_render {
+class DOODLELIB_API time_rules_render {
 };
 
 }  // namespace time_sequencer_widget_ns
 }  // namespace gui
 }  // namespace doodle
-
-#endif  // DOODLE_SRC_DOODLE_LIB_GUI_WIDGETS_TIME_SEQUENCER_WIDGETS_TIME_RULES_RENDER_H_
