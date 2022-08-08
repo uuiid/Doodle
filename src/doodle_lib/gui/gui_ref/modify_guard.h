f7dbdcab75c8b41b8e29b592af178dc860cbca83
uuiid <957714080@qq.com>
2022-08-08 09:09:02 +0800

    添加检查类

--- src/doodle_lib/gui/gui_ref/modify_guard.h
+++ src/doodle_lib/gui/gui_ref/modify_guard.h
@@ -7,39 +7,60 @@
 #include <doodle_lib/doodle_lib_fwd.h>
 
 #include <boost/signals2.hpp>
-
+#include <boost/noncopyable.hpp>
 namespace doodle::gui {
 template <typename Data_Type>
-class modify_guard {
+class modify_guard : boost::noncopyable {
  public:
   Data_Type data{};
-  std::bitset<3> flag;
+  using flag_type    = std::bitset<3>;
   using sig_type     = boost::signals2::signal<void(const Data_Type&)>;
   using solt_type    = typename sig_type::slot_type;
   using connect_type = boost::signals2::connection;
+  flag_type flag;
 
  private:
   sig_type sig_attr;
 
   bool modify{false};
 
-  void call_fun() {
+  void call_fun(const Data_Type& in_data) {
+    if (chick_call())
+      sig_attr(in_data);
+  }
+
+  void move_flag() {
+    flag <<= 1;
+  };
+
+  bool chick_call() {
+    const static flag_type s_f1{0b001};
+    const static flag_type s_f2{0b101};
+    auto l_chick = (flag ^ s_f1) | (flag ^ s_f2);
+    return l_chick.all();
   }
 
   class grard {
    public:
     modify_guard& self;
     explicit grard(modify_guard& in_modify_guard)
-        : self(in_modify_guard){
-
-          };
+        : self(in_modify_guard) {
+      self.move_flag();
+    };
 
     virtual ~grard() {
-      self.call_fun();
+      self.move_flag();
     }
   };
 
  public:
+  modify_guard()          = default;
+  virtual ~modify_guard() = default;
+
+  void operator()(const Data_Type& in_data) {
+    call_fun(in_data);
+  }
+
   modify_guard& operator=(bool in) {
     modify |= in;
     return *this;
