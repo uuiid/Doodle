aa1c81d0325abd20f0e53a01abff2ae8493cb16f
uuiid <957714080@qq.com>
2022-08-08 13:43:31 +0800

    添加用户保存类

---
+++ src/doodle_core/metadata/detail/user_set_data.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/8/8.
+//
+
+#include "user_set_data.h"
+
+namespace doodle {
+namespace detail {
+}  // namespace detail
+}  // namespace doodle
---
+++ src/doodle_core/metadata/detail/user_set_data.h
@@ -0,0 +1,15 @@
+//
+// Created by TD on 2022/8/8.
+//
+
+#pragma once
+#include <doodle_core/doodle_core_fwd.h>
+
+namespace doodle {
+namespace detail {
+
+class DOODLE_CORE_EXPORT user_set_data {
+};
+
+}  // namespace detail
+}  // namespace doodle
