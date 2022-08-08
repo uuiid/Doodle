af750ef57d92856184528d5aaa67c78762b7bc5d
uuiid <957714080@qq.com>
2022-08-08 13:45:34 +0800

    添加用户保存类

--- src/doodle_core/metadata/detail/user_set_data.h
+++ src/doodle_core/metadata/detail/user_set_data.h
@@ -5,10 +5,21 @@
 #pragma once
 #include <doodle_core/doodle_core_fwd.h>
 
+#include <doodle_core/metadata/metadata.h>
+#include <doodle_core/metadata/rules.h>
+#include <doodle_core/metadata/user.h>
+
 namespace doodle {
 namespace detail {
 
 class DOODLE_CORE_EXPORT user_set_data {
+ public:
+  user_set_data()          = default;
+  virtual ~user_set_data() = default;
+
+  database data_ref{};
+  ::doodle::business::rules rules_attr{};
+  user user_data{};
 };
 
 }  // namespace detail
