19c38e5c60ee0c9178bcbf15fa8fea012cb2eefc
uuiid <957714080@qq.com>
2022-08-09 18:32:00 +0800

    添加user 更新

---
+++ src/doodle_lib/gui/widgets/edit_widgets/edit_user.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/8/9.
+//
+
+#include "edit_user.h"
+
+namespace doodle {
+namespace gui {
+}  // namespace gui
+}  // namespace doodle
---
+++ src/doodle_lib/gui/widgets/edit_widgets/edit_user.h
@@ -0,0 +1,15 @@
+//
+// Created by TD on 2022/8/9.
+//
+
+#pragma once
+
+namespace doodle {
+namespace gui {
+
+class edit_user {
+};
+
+}  // namespace gui
+}  // namespace doodle
+
