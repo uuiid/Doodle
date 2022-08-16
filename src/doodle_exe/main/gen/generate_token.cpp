62478a0f8e1e40b42be39f5ad2fc93107bd7d830
uuiid <957714080@qq.com>
2022-08-16 15:07:44 +0800

    修正生成许可文件exe

--- src/doodle_exe/main/gen/generate_token.cpp
+++ src/doodle_exe/main/gen/generate_token.cpp
@@ -6,6 +6,9 @@
 #include <doodle_lib/core/app_command_base.h>
 int main(int argc, char *argv[]) {
   doodle::app_command_base l_command_base{};
+  for (int l_i = 0; l_i < 10; ++l_i) {
+    l_command_base.poll_one();
+  }
   if (argc > 1) {
     doodle::authorization::generate_token(argv[1]);
   } else {
