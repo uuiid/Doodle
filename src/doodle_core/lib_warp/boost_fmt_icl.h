ade5be12fa1ca149b46c8bd5808e912ca6a3cfc6
uuiid <957714080@qq.com>
2022-08-17 13:57:04 +0800

    boost icl fmt

--- src/doodle_core/lib_warp/boost_fmt_icl.h
+++ src/doodle_core/lib_warp/boost_fmt_icl.h
@@ -41,31 +41,40 @@ namespace fmt {
 //                                             Alloc>>
 //     : ostream_formatter {};
 
-
 template <>
 struct formatter<::boost::icl::interval_bounds>
     : ostream_formatter {};
 
- template <typename Type>
- struct formatter<Type,
-                  std::enable_if_t<::boost::icl::is_discrete_interval<Type>::value, char>>
-     : ostream_formatter {};
+// template <typename Type>
+// struct formatter<Type,
+//                  std::enable_if_t<::boost::icl::is_discrete_interval<Type>::value, char>>
+//     : ostream_formatter {};
 
-//template <typename Type>
-//struct formatter<Type,
-//                 std::enable_if_t<::boost::icl::is_interval_map<Type>::value, char>>
-//    : formatter<fmt::string_view> {
-//  template <typename FormatContext>
-//  auto format(const Type &in_interval_map, FormatContext &in_ctx) const
-//      -> decltype(in_ctx.out()) {
-//    fmt::format_to(in_ctx.out(), "{{");
-//    for (auto &&i_interval : in_interval_map) {
-//      fmt::format_to(in_ctx.out(), "({}->{})", i_interval.first, i_interval.second);
-//    }
-//    fmt::format_to(in_ctx.out(), "}}");
-//    return in_ctx.out();
-//  }
-//};
+template <typename Type>
+struct formatter<Type,
+                 std::enable_if_t<::boost::icl::is_interval<Type>::value, char>>
+    : ostream_formatter {};
+
+// template <typename Type>
+// struct formatter<Type,
+//                  std::enable_if_t<::boost::icl::is_interval_map<Type>::value, char>>
+//     : ostream_formatter {};
+
+// template <typename Type>
+// struct formatter<Type,
+//                  std::enable_if_t<::boost::icl::is_interval_map<Type>::value, char>>
+//     : formatter<fmt::string_view> {
+//   template <typename FormatContext>
+//   auto format(const Type &in_interval_map, FormatContext &in_ctx) const
+//       -> decltype(in_ctx.out()) {
+//     fmt::format_to(in_ctx.out(), "{{");
+//     for (auto &&i_interval : in_interval_map) {
+//       fmt::format_to(in_ctx.out(), "({}->{})", i_interval.first, i_interval.second);
+//     }
+//     fmt::format_to(in_ctx.out(), "}}");
+//     return in_ctx.out();
+//   }
+// };
 
 template <typename Type>
 struct formatter<Type,
