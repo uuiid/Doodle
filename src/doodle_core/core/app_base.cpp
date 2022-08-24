8a6ec9a77ed265ea8a6e1af8aaa51241169ef631
uuiid <957714080@qq.com>
2022-08-24 13:46:14 +0800

    ...

--- src/doodle_core/core/app_base.cpp
+++ src/doodle_core/core/app_base.cpp
@@ -114,7 +114,6 @@ void app_base::load_project(const FSys::path& in_path) const {
       FSys::is_regular_file(in_path) &&
       in_path.extension() == doodle_config::doodle_db_name.data()) {
     auto l_open = std::make_shared<database_n::sqlite_file>();
-    g_reg()->ctx().emplace<decltype(l_open)>(l_open);
     l_open
         ->async_open(in_path,
                      [](bsys::error_code) -> void {
