f52707bb9dd797b622e50da23484c7fa03c4d68a
uuiid <957714080@qq.com>
2022-08-24 17:48:04 +0800

    修正令牌生成

--- src/doodle_exe/main/gen/generate_token.cpp
+++ src/doodle_exe/main/gen/generate_token.cpp
@@ -5,7 +5,8 @@
 #include <doodle_lib/core/authorization.h>
 #include <doodle_lib/core/app_command_base.h>
 int main(int argc, char *argv[]) {
-  doodle::app_command_base l_command_base{};
+  doodle::app_command_base l_command_base{
+      doodle::app_command_base::in_app_args{nullptr, std::vector<std::string>{}}};
   for (int l_i = 0; l_i < 10; ++l_i) {
     l_command_base.poll_one();
   }
