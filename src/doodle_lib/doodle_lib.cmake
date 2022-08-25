d2c75d241c30377eafe61977bcc3d9a59f7542a0
uuiid <957714080@qq.com>
2022-08-25 16:36:27 +0800

    添加渲染函数

--- src/doodle_lib/doodle_lib.cmake
+++ src/doodle_lib/doodle_lib.cmake
@@ -1,6 +1,5 @@
 set(
         DOODLELIB_HEADER
-        app/app.h
         core/ContainerDevice.h
         core/filesystem_extend.h
         core/observable_container.h
@@ -8,15 +7,9 @@ set(
         core/util.h
         core/image_loader.h
         core/authorization.h
-
-
         file_warp/opencv_read_player.h
-
-
         lib_warp/boost_locale_warp.h
         lib_warp/imgui_warp.h
-
-
         toolkit/toolkit.h
         exe_warp/maya_exe.h
         exe_warp/ue4_exe.h
@@ -24,8 +17,6 @@ set(
         long_task/join_move.h
         long_task/short_cut.h
         long_task/image_load_task.h
-
-
         core/app_command_base.h
         doodle_lib_all.h
         doodle_lib_fwd.h
@@ -33,22 +24,13 @@ set(
 )
 set(
         DOODLELIB_SOURCE
-        app/app.cpp
-
-
         core/filesystem_extend.cpp
         core/program_options.cpp
         core/util.cpp
         core/image_loader.cpp
         core/authorization.cpp
-
-
         file_warp/opencv_read_player.cpp
-
-
         lib_warp/imgui_warp.cpp
-
-
         toolkit/toolkit.cpp
         exe_warp/maya_exe.cpp
         exe_warp/ue4_exe.cpp
@@ -56,8 +38,6 @@ set(
         long_task/join_move.cpp
         long_task/short_cut.cpp
         long_task/image_load_task.cpp
-
-
         core/app_command_base.cpp
         doodle_lib_all.cpp
 )
