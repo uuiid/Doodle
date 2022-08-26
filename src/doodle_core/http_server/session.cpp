c8143d194596fa1a3db55e2370fc6e2feb3833d3
uuiid <957714080@qq.com>
2022-08-26 14:51:21 +0800

    添加新的http服务器

---
+++ src/doodle_core/http_server/session.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/8/26.
+//
+
+#include "session.h"
+
+namespace doodle {
+namespace http {
+}  // namespace http
+}  // namespace doodle
---
+++ src/doodle_core/http_server/session.h
@@ -0,0 +1,17 @@
+//
+// Created by TD on 2022/8/26.
+//
+
+#ifndef DOODLE_SRC_DOODLE_CORE_HTTP_SERVER_SESSION_H_
+#define DOODLE_SRC_DOODLE_CORE_HTTP_SERVER_SESSION_H_
+
+namespace doodle {
+namespace http {
+
+class session {
+};
+
+}  // namespace http
+}  // namespace doodle
+
+#endif  // DOODLE_SRC_DOODLE_CORE_HTTP_SERVER_SESSION_H_
