b978d1df8929f35f8255512632f699b410ca90c9
uuiid <957714080@qq.com>
2022-08-26 18:20:08 +0800

    完成基本添加

--- src/doodle_core/http_server/listener.cpp
+++ src/doodle_core/http_server/listener.cpp
@@ -3,8 +3,74 @@
 //
 
 #include "listener.h"
-
+#include <doodle_core/http_server/session.h>
 namespace doodle {
 namespace http_server {
+class listener::impl {
+ public:
+  boost::asio::ip::tcp::acceptor acceptor_{g_io_context()};
+};
+listener::listener(boost::asio::ip::tcp::endpoint in_end_point)
+    : ptr(std::make_unique<impl>()) {
+  boost::beast::error_code ec;
+
+  // Open the acceptor
+  ptr->acceptor_.open(in_end_point.protocol(), ec);
+  if (ec) {
+    //    fail(ec, "open");
+    return;
+  }
+
+  // Allow address reuse
+  ptr->acceptor_.set_option(boost::asio::socket_base::reuse_address(true), ec);
+  if (ec) {
+    //    fail(ec, "set_option");
+    return;
+  }
+
+  // Bind to the server address
+  ptr->acceptor_.bind(in_end_point, ec);
+  if (ec) {
+    //    fail(ec, "bind");
+    return;
+  }
+
+  // Start listening for connections
+  ptr->acceptor_.listen(
+      boost::asio::socket_base::max_listen_connections, ec
+  );
+  if (ec) {
+    //    fail(ec, "listen");
+    return;
+  }
+}
+void listener::run() {
+  do_accept();
+}
+void listener::do_accept() {
+  // The new connection gets its own strand
+  ptr->acceptor_.async_accept(
+      boost::asio::make_strand(g_io_context()),
+      boost::beast::bind_front_handler(
+          &listener::on_accept,
+          shared_from_this()
+      )
+  );
+};
+
+void listener::on_accept(boost::beast::error_code ec, boost::asio::ip::tcp::socket socket) {
+  if (ec) {
+    //      fail(ec, "accept");
+    return;  // To avoid infinite loop
+  } else {
+    // Create the session and run it
+    std::make_shared<session>(
+        std::move(socket)
+    )
+        ->run();
+  }
+  // Accept another connection
+  do_accept();
+};
 }  // namespace http_server
 }  // namespace doodle
--- src/doodle_core/http_server/listener.h
+++ src/doodle_core/http_server/listener.h
@@ -4,12 +4,29 @@
 #pragma once
 
 #include <doodle_core/doodle_core_fwd.h>
+#include <boost/beast.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/strand.hpp>
 
-namespace doodle {
-namespace http_server {
+namespace doodle::http_server {
 
-class listener {
+class DOODLE_CORE_EXPORT listener : public std::enable_shared_from_this<listener> {
+ private:
+  class impl;
+  std::unique_ptr<impl> ptr;
+
+ public:
+  explicit listener(boost::asio::ip::tcp::endpoint in_end_point);
+  void run();
+
+ private:
+  void do_accept();
+  void on_accept(boost::beast::error_code ec, boost::asio::ip::tcp::socket socket);
 };
 
-}  // namespace http_server
-}  // namespace doodle
+}  // namespace doodle::http_server
