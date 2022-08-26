3e14998026148afd66429eeb67f04398d68fcf5f
uuiid <957714080@qq.com>
2022-08-26 10:44:59 +0800

    去除无用声名

--- src/doodle_core/core/doodle_lib.h
+++ src/doodle_core/core/doodle_lib.h
@@ -12,12 +12,6 @@ class thread_pool;
 }  // namespace boost::asio
 
 namespace doodle {
-DOODLE_CORE_EXPORT registry_ptr& g_reg();
-DOODLE_CORE_EXPORT scheduler_t& g_main_loop();
-DOODLE_CORE_EXPORT bounded_pool_t& g_bounded_pool();
-DOODLE_CORE_EXPORT boost::asio::io_context& g_io_context();
-DOODLE_CORE_EXPORT boost::asio::thread_pool& g_thread();
-
 class DOODLE_CORE_EXPORT doodle_lib : public details::no_copy,
                                       boost::equality_comparable<doodle_lib> {
  private:
@@ -32,13 +26,13 @@ class DOODLE_CORE_EXPORT doodle_lib : public details::no_copy,
   static doodle_lib& Get();
   static FSys::path create_time_database();
 
-  thread_pool_ptr get_thread_pool() const;
+  [[nodiscard]] thread_pool_ptr get_thread_pool() const;
 
-  registry_ptr& reg_attr() const;
-  scheduler_t& main_loop_attr() const;
-  bounded_pool_t& bounded_pool_attr() const;
-  boost::asio::io_context& io_context_attr() const;
-  boost::asio::thread_pool& thread_attr() const;
+  [[nodiscard]] registry_ptr& reg_attr() const;
+  [[nodiscard]] scheduler_t& main_loop_attr() const;
+  [[nodiscard]] bounded_pool_t& bounded_pool_attr() const;
+  [[nodiscard]] boost::asio::io_context& io_context_attr() const;
+  [[nodiscard]] boost::asio::thread_pool& thread_attr() const;
 
   bool operator==(const doodle_lib& in_rhs) const;
 };
