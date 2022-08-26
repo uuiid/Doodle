693c41a4c75ccd8b7d481c0309d22452523232e5
uuiid <957714080@qq.com>
2022-08-26 10:05:28 +0800

    添加新的库基本功能测试

---
+++ src/test/core/lib_create.cpp
@@ -0,0 +1,16 @@
+//
+// Created by TD on 2022/8/26.
+//
+
+#define BOOST_TEST_MODULE doodle lib
+#include <boost/test/included/unit_test.hpp>
+#include <doodle_core/pin_yin/convert.h>
+
+using namespace doodle;
+BOOST_AUTO_TEST_CASE(free_test_function)
+/* Compare with void free_test_function() */
+{
+  BOOST_TEST(convert::Get().toEn("测试"s) == "ceshi");
+  BOOST_TEST(convert::Get().toEn("林奇"s) == "linqi");
+  BOOST_TEST(convert::Get().toEn("dsa??__测试"s) == "dsa??__ceshi");
+}
