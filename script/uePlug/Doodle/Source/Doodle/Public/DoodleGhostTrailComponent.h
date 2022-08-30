b8ee88ff0d315ecceb1d7ce705260ed8ced2dd39
uuiid <957714080@qq.com>
2022-08-30 13:18:30 +0800

    添加 ue4 残影插件中的属性 可被定序器设置关键帧行为

--- script/uePlug/Doodle/Source/Doodle/Public/DoodleGhostTrailComponent.h
+++ script/uePlug/Doodle/Source/Doodle/Public/DoodleGhostTrailComponent.h
@@ -29,26 +29,26 @@ class DOODLE_API UDoodleGhostTrailComponent : public UActorComponent {
       FActorComponentTickFunction *ThisTickFunction
   ) override;
 
-  UPROPERTY(EditAnywhere, Category = "Doodle", DisplayName = "骨骼名称")
+  UPROPERTY(EditAnywhere, Interp, Category = "Doodle", DisplayName = "骨骼名称")
   FName BoneName;
 
-  UPROPERTY(EditAnywhere, Category = "Doodle", DisplayName = "残影距离")
+  UPROPERTY(EditAnywhere, Interp, Category = "Doodle", DisplayName = "残影距离")
   float Distance{30.0f};
 
-  UPROPERTY(EditAnywhere, Category = "Doodle", DisplayName = "最大个数")
+  UPROPERTY(EditAnywhere, Interp, Category = "Doodle", DisplayName = "最大个数")
   int MaxCount{50};
 
-  UPROPERTY(EditAnywhere, Category = "Doodle", DisplayName = "残影寿命")
+  UPROPERTY(EditAnywhere, Interp, Category = "Doodle", DisplayName = "残影寿命")
   float Life{1.5f};
 
-  UPROPERTY(EditAnyWhere, BlueprintReadOnly, Interp, Category = "Doodle", DisplayName = "透明曲线")
+  UPROPERTY(EditAnyWhere, Interp, BlueprintReadOnly, Interp, Category = "Doodle", DisplayName = "透明曲线")
   FRuntimeFloatCurve TransparentCurve;
 
-  UPROPERTY(EditAnywhere, Category = "Doodle", DisplayName = "材质透明参数")
+  UPROPERTY(EditAnywhere, Interp, Category = "Doodle", DisplayName = "材质透明参数")
   FName TransparentName{TEXT("TransparentName")};
 
   /** If true, this component will be rendered in the CustomDepth pass (usually used for outlines) */
-  UPROPERTY(EditAnywhere, AdvancedDisplay, BlueprintReadOnly, Category = "Doodle", meta = (DisplayName = "Render CustomDepth Pass"))
+  UPROPERTY(EditAnywhere, Interp, AdvancedDisplay, BlueprintReadOnly, Category = "Doodle", meta = (DisplayName = "Render CustomDepth Pass"))
   uint8 bRenderCustomDepth : 1;
 
  private:
