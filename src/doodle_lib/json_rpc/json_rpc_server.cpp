4e7f7459fc323af22e8f9a85118263d38b34dc44
uuiid <957714080@qq.com>
2022-08-31 14:38:27 +0800

    json 更新

--- src/doodle_lib/json_rpc/json_rpc_server.cpp
+++ src/doodle_lib/json_rpc/json_rpc_server.cpp
@@ -17,15 +17,14 @@ json_rpc_server::json_rpc_server()
     : ptr(std::make_unique<impl>()) {
 }
 
-void json_rpc_server::create_movie(
-    const image_to_move_sig& in_skin,
+json_rpc::args::rpc_json_progress json_rpc_server::create_movie(
     const std::vector<movie::image_attr>& in_arg
 ) {
-  in_skin(json_rpc::args::rpc_json_progress{"test 0.3"s, "test 0.3"s, 0.3});
-  in_skin(json_rpc::args::rpc_json_progress{"test 0.4"s, "test 0.4"s, 0.4});
-  in_skin(json_rpc::args::rpc_json_progress{"test 0.5"s, "test 0.5"s, 0.5});
-  in_skin(json_rpc::args::rpc_json_progress{"test 0.6"s, "test 0.6"s, 0.6});
-  in_skin(json_rpc::args::rpc_json_progress{"test 1.0"s, "test 1.0"s, 1.0});
+  return json_rpc::args::rpc_json_progress{"test 0.3"s, "test 0.3"s, 0.3};
+  //  json_rpc::args::rpc_json_progress{"test 0.4"s, "test 0.4"s, 0.4};
+  //  json_rpc::args::rpc_json_progress{"test 0.5"s, "test 0.5"s, 0.5};
+  //  json_rpc::args::rpc_json_progress{"test 0.6"s, "test 0.6"s, 0.6};
+  //  json_rpc::args::rpc_json_progress{"test 1.0"s, "test 1.0"s, 1.0};
 }
 
 json_rpc_server::~json_rpc_server() = default;
--- src/doodle_lib/json_rpc/json_rpc_server.h
+++ src/doodle_lib/json_rpc/json_rpc_server.h
@@ -14,8 +14,7 @@ class DOODLELIB_API json_rpc_server : public json_rpc_server_i {
   json_rpc_server();
   ~json_rpc_server() override;
 
-  void create_movie(
-      const image_to_move_sig& in_skin,
+  json_rpc::args::rpc_json_progress create_movie(
       const std::vector<movie::image_attr>& in_arg
   ) override;
 };
