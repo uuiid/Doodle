0870c6f368229ba180d29f5abebaf56205b31c77
uuiid <957714080@qq.com>
2022-09-02 12:23:13 +0800

    添加属性记录

--- src/doodle_core/metadata/assets_file.cpp
+++ src/doodle_core/metadata/assets_file.cpp
@@ -63,6 +63,7 @@ assets_file::assets_file(const FSys::path& in_path)
   p_i->path   = in_path;
   p_i->p_name = in_path.stem().generic_string();
   user_attr(user::get_current_handle());
+  p_i->organization_p = core_set::getSet().organization_name;
 }
 
 assets_file::assets_file(const FSys::path& in_path, std::string in_name, std::uint64_t in_version)
