8435e17fb0e9ce773519aa834cabafebd0cee4bd
uuiid <957714080@qq.com>
2022-09-06 13:45:01 +0800

    更新ue4构建方法

--- src/build/build_ue4_files.cmd
+++ src/build/build_ue4_files.cmd
@@ -1,5 +1,16 @@
 call ./set_venv.cmd
 
+
+echo -----------------config ue4--------------------
+"C:\Program Files\CMake\bin\cmake.exe" ^
+-S%my_pwd% ^
+--preset Ninja_release
+
+if %errorlevel% NEQ 0 exit 1
+
+
+
+echo -----------------install ue4--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release ^
 --component ue4_plug
