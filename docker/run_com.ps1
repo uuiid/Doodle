65ccaa88d6c2b8cd38617d6623bd5b0b95eb2993
uuiid <957714080@qq.com>
2022-09-06 13:55:19 +0800

    去除无用

--- docker/run_com.ps1
+++
@@ -1,4 +0,0 @@
-docker-compose.exe -f "docker-compose.mysql.yml" up -d --build
-&"C:\Program Files\Docker\Docker\DockerCli.exe" -SwitchDaemon
-docker-compose.exe -f "docker-compose.rpc.yml" up -d --build
-&"C:\Program Files\Docker\Docker\DockerCli.exe" -SwitchDaemon
