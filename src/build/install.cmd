8c8e02d340a460809c01ae066ec3bc81a50db7ef
uuiid <957714080@qq.com>
2022-09-06 18:01:18 +0800

    添加文件存在性检查

--- src/build/install.cmd
+++ src/build/install.cmd
@@ -14,7 +14,7 @@ echo -----------------pack---------------------
 if %errorlevel% NEQ 0 exit 1
 
 echo -----------------copy file--------------------
-mkdir %my_pwd%\build\html\file
+if not exit "%my_pwd%\build\html\file" mkdir %my_pwd%\build\html\file
 robocopy %my_pwd%\build\install %my_pwd%\build\html\file *.msi > %tmp%/doodle_install_copy1.txt
 robocopy %my_pwd%\build\install %my_pwd%\build\html\file *.7z > %tmp%/doodle_install_copy2.txt
 py %my_pwd%/docs/generate_directory_index_caddystyle.py %my_pwd%/build/html/file
