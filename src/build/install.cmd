1be15be88b7676a30de384ffcc43c765bd2ee324
uuiid <957714080@qq.com>
2022-09-06 18:01:05 +0800

    添加更新doc文件命令

--- src/build/install.cmd
+++ src/build/install.cmd
@@ -17,5 +17,6 @@ echo -----------------copy file--------------------
 mkdir %my_pwd%\build\html\file
 robocopy %my_pwd%\build\install %my_pwd%\build\html\file *.msi > %tmp%/doodle_install_copy1.txt
 robocopy %my_pwd%\build\install %my_pwd%\build\html\file *.7z > %tmp%/doodle_install_copy2.txt
-
+py %my_pwd%/docs/generate_directory_index_caddystyle.py %my_pwd%/build/html/file
+py %my_pwd%/docs/generate_updata_log.py %my_pwd%/build/html/update.html
 
