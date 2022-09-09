a984a24f21b168fe705132bfae60491e2853d700
uuiid <957714080@qq.com>
2022-09-09 11:24:57 +0800

    更新测试

--- src/test/dingding/base.cpp
+++ src/test/dingding/base.cpp
@@ -4,6 +4,11 @@
 #define BOOST_TEST_MODULE dingding
 #include <boost/test/included/unit_test.hpp>
 #include <doodle_dingding/client/client.h>
+#include <doodle_dingding/client/dingding_api.h>
+
+#include <doodle_core/doodle_core.h>
+#include <doodle_dingding/fmt_lib/boost_beast_fmt.h>
+
 #include <boost/asio/ssl.hpp>
 #include <boost/asio.hpp>
 using namespace doodle;
@@ -16,15 +21,26 @@ BOOST_AUTO_TEST_CASE(client_base_tset) {
   /// 验证远程服务器的证书
   l_context.set_verify_mode(boost::asio::ssl::verify_peer);
   l_context.set_options(
-      boost::asio::ssl::context::default_workarounds
-      | boost::asio::ssl::context::no_sslv2
-      | boost::asio::ssl::context::no_sslv3
+      boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 | boost::asio::ssl::context::no_sslv3
   );
   l_context.set_default_verify_paths();
   using namespace std::literals;
 
-  std::make_shared<dingding::client>(boost::asio::make_strand(l_io_context), l_context)
-      ->run("www.baidu.com"s, "/"s);
+  auto l_r = std::make_shared<dingding::client>(
+      boost::asio::make_strand(l_io_context), l_context
+  );
+
+  dingding::client_ns::http_req_res<
+      boost::beast::http::request<boost::beast::http::empty_body>,
+      boost::beast::http::response<boost::beast::http::string_body> >
+      l_http_req_res{l_r->shared_from_this()};
+  l_http_req_res.req_attr.method(boost::beast::http::verb::get);
+  l_http_req_res.url_attr = boost::url{"www.baidu.com/"s};
+  l_http_req_res.read_fun = [](auto&& in) {
+    DOODLE_LOG_INFO(in);
+  };
+  l_r->run(l_http_req_res);
+
   l_io_context.run();
 }
 
@@ -36,14 +52,12 @@ BOOST_AUTO_TEST_CASE(client_get_gettoken) {
   /// 验证远程服务器的证书
   l_context.set_verify_mode(boost::asio::ssl::verify_peer);
   l_context.set_options(
-      boost::asio::ssl::context::default_workarounds
-      | boost::asio::ssl::context::no_sslv2
-      | boost::asio::ssl::context::no_sslv3
+      boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 | boost::asio::ssl::context::no_sslv3
   );
   l_context.set_default_verify_paths();
   using namespace std::literals;
 
-  std::make_shared<dingding::client>(boost::asio::make_strand(l_io_context), l_context)
+  std::make_shared<dingding::dingding_api>(boost::asio::make_strand(l_io_context), l_context)
       ->gettoken();
   l_io_context.run();
 }
