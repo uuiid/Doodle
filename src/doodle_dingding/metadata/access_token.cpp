5b592a62742f80af4e8e154bb1ff501b129ecb32
uuiid <957714080@qq.com>
2022-09-09 18:24:24 +0800

    修正json转换

--- src/doodle_dingding/metadata/access_token.cpp
+++ src/doodle_dingding/metadata/access_token.cpp
@@ -10,8 +10,8 @@ namespace doodle {
 namespace dingding {
 
 void from_json(const nlohmann::json& j, access_token& p) {
-  p.token      = j["access_token"];
-  p.expires_in = j["expires_in"].;
+  p.token      = j["access_token"].get<std::string>();
+  p.expires_in = j["expires_in"].get<std::int32_t>();
 };
 }  // namespace dingding
 }  // namespace doodle
