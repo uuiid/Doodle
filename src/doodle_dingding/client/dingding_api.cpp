3e988e1fc828b667c003072066fac6fa3109e0de
uuiid <957714080@qq.com>
2022-09-13 14:01:53 +0800

    添加钉钉获取部门接口

--- src/doodle_dingding/client/dingding_api.cpp
+++ src/doodle_dingding/client/dingding_api.cpp
@@ -4,6 +4,9 @@
 
 #include "dingding_api.h"
 #include <doodle_dingding/configure/config.h>
+#include <doodle_dingding/metadata/department.h>
+#include <doodle_core/doodle_core.h>
+
 #include <nlohmann/json.hpp>
 
 namespace doodle {
@@ -25,7 +28,7 @@ void dingding_api::async_get_token(
 
   client_ns::http_req_res<
       boost::beast::http::request<boost::beast::http::empty_body>,
-      boost::beast::http::response<boost::beast::http::string_body> >
+      boost::beast::http::response<boost::beast::http::string_body>>
       l_http_req_res{shared_from_this()};
   l_http_req_res.req_attr.method(boost::beast::http::verb::get);
 
@@ -46,6 +49,49 @@ void dingding_api::async_get_token(
   run(l_http_req_res);
   //  run(l_url);
   //  return ptr->res_.body();
+}
+void dingding_api::async_get_departments(
+    const department_query& in_query,
+    const access_token& in_token,
+    dingidng_call_list_fun&& in_fun
+) {
+  boost::url l_url{};
+  boost::url l_method{"topapi/v2/department/listsub"};
+  l_method.params().set("access_token", in_token.token);
+
+  client_ns::http_req_res<
+      boost::beast::http::request<boost::beast::http::string_body>,
+      boost::beast::http::response<boost::beast::http::string_body>>
+      l_http_req_res{shared_from_this()};
+  l_http_req_res.req_attr.method(boost::beast::http::verb::post);
+  nlohmann::json l_json          = in_query;
+  l_http_req_res.res_attr.body() = l_json.dump();
+
+  boost::urls::resolve(
+      boost::urls::url_view{dingding_host},
+      l_method,
+      l_http_req_res.url_attr
+  );
+  l_http_req_res.read_fun = [l_fun = std::move(in_fun), this](const decltype(l_http_req_res.res_attr)& in) {
+    auto l_j = nlohmann::json::parse(in.body());
+    if (l_j["errcode"] != 0) {
+      throw_exception(doodle_error{"{} {}", l_j["errcode"], l_j["errmsg"]});
+    }
+    auto l_r   = l_j["result"].get<std::vector<department>>();
+    auto l_msg = l_r |
+                 ranges::view::transform([](const department& in) -> entt::handle {
+                   auto l_handle = doodle::make_handle();
+                   l_handle.emplace<department>(in);
+                   return l_handle;
+                 }) |
+                 ranges::to_vector;
+    boost::asio::post(
+        this->get_executor(),
+        [fun = std::move(l_fun), meg = std::move(l_msg)]() { fun(meg); }
+    );
+  };
+
+  run(l_http_req_res);
 }
 }  // namespace dingding
 }  // namespace doodle
--- src/doodle_dingding/client/dingding_api.h
+++ src/doodle_dingding/client/dingding_api.h
@@ -5,11 +5,14 @@
 
 #include <doodle_dingding/client/client.h>
 #include <nlohmann/json_fwd.hpp>
+#include <doodle_core/lib_warp/entt_warp.h>
 
 #include <doodle_dingding/metadata/access_token.h>
-
 namespace doodle::dingding {
 class access_token;
+class department_query;
+using dingidng_call_fun  = std::function<void(const entt::handle& in)>;
+using dingidng_call_list_fun = std::function<void(const std::vector<entt::handle>& in)>;
 
 class DOODLE_DINGDING_API dingding_api : public client {
  private:
@@ -23,6 +26,12 @@ class DOODLE_DINGDING_API dingding_api : public client {
   void async_get_token(
       read_access_token_fun&& in
   );
+
+  void async_get_departments(
+      const department_query& in_query,
+      const access_token& in_token,
+      dingidng_call_list_fun&& in_fun
+  );
 };
 
-}  // namespace doodle
+}  // namespace doodle::dingding
