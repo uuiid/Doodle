3544652f955a55a18b93a4f1d315b623de870149
uuiid <957714080@qq.com>
2022-09-13 13:20:27 +0800

    钉钉部门类型设置

---
+++ src/doodle_dingding/metadata/department.h
@@ -0,0 +1,14 @@
+//
+// Created by TD on 2022/9/13.
+//
+
+#pragma once
+#include <doodle_dingding/doodle_dingding_fwd.h>
+
+
+namespace doodle::dingding {
+
+class DOODLE_DINGDING_API department {
+};
+
+}  // namespace doodle::dingding
