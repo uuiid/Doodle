67c6120d3124a15a624c928a6c26b79745ceca09
uuiid <957714080@qq.com>
2022-09-13 14:02:14 +0800

    添加部门接口

--- src/doodle_dingding/metadata/department.h
+++ src/doodle_dingding/metadata/department.h
@@ -5,10 +5,27 @@
 #pragma once
 #include <doodle_dingding/doodle_dingding_fwd.h>
 
-
+#include <nlohmann/json_fwd.hpp>
+#include <nlohmann/json.hpp>
 namespace doodle::dingding {
 
+class DOODLE_DINGDING_API department_query {
+ public:
+  std::int32_t dept_id{1};
+  std::string language{"zh_CN"};
+
+  NLOHMANN_DEFINE_TYPE_INTRUSIVE(department_query, dept_id, language)
+};
+
 class DOODLE_DINGDING_API department {
+ public:
+  department() = default;
+  std::int32_t dept_id{};
+  std::string name{};
+  std::int32_t parent_id{};
+  bool create_dept_group{};
+  bool auto_add_user{};
+  NLOHMANN_DEFINE_TYPE_INTRUSIVE(department, dept_id, name, parent_id, create_dept_group, auto_add_user)
 };
 
 }  // namespace doodle::dingding
