85b6e4a9d102faa13473098442a005b8a4bc5153
uuiid <957714080@qq.com>
2022-09-14 11:57:36 +0800

    调整客户端

--- src/test/dingding/base.cpp
+++ src/test/dingding/base.cpp
@@ -13,21 +13,29 @@
 #include <boost/asio.hpp>
 using namespace doodle;
 
-BOOST_AUTO_TEST_CASE(client_base_tset) {
-  boost::asio::io_context l_io_context{};
-  /// \brief SSL 上下文是必需的，并持有证书
-  boost::asio::ssl::context l_context{
+struct loop_fixtures {
+  loop_fixtures()  = default;
+  ~loop_fixtures() = default;
+  boost::asio::io_context io_context_attr{};
+  boost::asio::ssl::context context_attr{
       boost::asio::ssl::context::sslv23};
-  /// 验证远程服务器的证书
-  l_context.set_verify_mode(boost::asio::ssl::verify_peer);
-  l_context.set_options(
-      boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 | boost::asio::ssl::context::no_sslv3
-  );
-  l_context.set_default_verify_paths();
+  void setup() {
+    context_attr.set_verify_mode(boost::asio::ssl::verify_peer);
+    context_attr.set_options(
+        boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 | boost::asio::ssl::context::no_sslv3
+    );
+    context_attr.set_default_verify_paths();
+  };
+  void teardown() {
+    io_context_attr.run();
+  };
+};
+
+BOOST_FIXTURE_TEST_CASE(client_base_tset, loop_fixtures) {
   using namespace std::literals;
 
   auto l_r = std::make_shared<dingding::client>(
-      boost::asio::make_strand(l_io_context), l_context
+      boost::asio::make_strand(io_context_attr), context_attr
   );
 
   dingding::client_ns::http_req_res<
@@ -41,69 +49,23 @@ BOOST_AUTO_TEST_CASE(client_base_tset) {
     DOODLE_LOG_INFO(in);
   };
   l_r->run(l_http_req_res);
-
-  l_io_context.run();
 }
 
-struct loop_fixtures {
-  loop_fixtures()  = default;
-  ~loop_fixtures() = default;
-  boost::asio::io_context io_context_attr{};
-  boost::asio::ssl::context context_attr{
-      boost::asio::ssl::context::sslv23};
-  void setup();
-  void teardown();
-};
-
-BOOST_AUTO_TEST_SUITE(dingding_base)
+BOOST_FIXTURE_TEST_SUITE(dingding_base, loop_fixtures)
 static dingding::access_token l_token{};
 
 BOOST_AUTO_TEST_CASE(client_get_gettoken) {
-  boost::asio::io_context l_io_context{};
-  /// \brief SSL 上下文是必需的，并持有证书
-  boost::asio::ssl::context l_context{
-      boost::asio::ssl::context::sslv23};
-  /// 验证远程服务器的证书
-  l_context.set_verify_mode(boost::asio::ssl::verify_peer);
-  l_context.set_options(
-      boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 | boost::asio::ssl::context::no_sslv3
-  );
-  l_context.set_default_verify_paths();
   using namespace std::literals;
-
-  std::make_shared<dingding::dingding_api>(boost::asio::make_strand(l_io_context), l_context)
+  std::make_shared<dingding::dingding_api>(boost::asio::make_strand(io_context_attr), context_attr)
       ->async_get_token([](const dingding::access_token& in) {
         DOODLE_LOG_INFO(in.token);
         l_token = in;
       });
-  l_io_context.run();
+}
+BOOST_AUTO_TEST_CASE(client_get_dep) {
+  using namespace std::literals;
+
+  auto l_st = boost::asio::make_strand(io_context_attr);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
-namespace client_get_dep_ns {
-void get_dep(const dingding::access_token& in) {
-}
-}  // namespace client_get_dep_ns
-
-BOOST_AUTO_TEST_CASE(client_get_dep) {
-  boost::asio::io_context l_io_context{};
-  /// \brief SSL 上下文是必需的，并持有证书
-  boost::asio::ssl::context l_context{
-      boost::asio::ssl::context::sslv23};
-  /// 验证远程服务器的证书
-  l_context.set_verify_mode(boost::asio::ssl::verify_peer);
-  l_context.set_options(
-      boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 | boost::asio::ssl::context::no_sslv3
-  );
-  l_context.set_default_verify_paths();
-  using namespace std::literals;
-
-  auto l_st = boost::asio::make_strand(l_io_context);
-
-  std::make_shared<dingding::dingding_api>(l_st, l_context)
-      ->async_get_token([](const dingding::access_token& in) {
-        DOODLE_LOG_INFO(in.token);
-      });
-  l_io_context.run();
-}
