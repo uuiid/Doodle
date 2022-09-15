911a7c2d380e6762b1a8c2fae3fbce464c567797
uuiid <957714080@qq.com>
2022-09-15 16:22:32 +0800

    去除无用的类

--- src/doodle_dingding/client/client.cpp
+++ src/doodle_dingding/client/client.cpp
@@ -65,144 +65,25 @@ client::executor_type client::get_executor() noexcept {
   return ptr->io_executor_;
 }
 
-void client::run(const std::string& in_host, const std::string& in_target) {
-  boost::url l_url{fmt::format("{}:{}{}", in_host, "443"s, in_target)};
-  client_ns::http_req_res<
-      boost::beast::http::request<boost::beast::http::empty_body>,
-      boost::beast::http::response<boost::beast::http::string_body>>
-      l_http_req_res{shared_from_this()};
-  l_http_req_res.req_attr.method(boost::beast::http::verb::get);
-  l_http_req_res.url_attr = l_url;
-  return run(l_http_req_res);
-}
-
-void client::run(
-    const boost::url& in_url,
-    const config_type& in_config_type
-) {
-  const std::string host{in_url.host()};
-  set_openssl(host);
-  const std::string port{in_url.has_port()  //
-                             ? std::string{in_url.port()}
-                             : "443"s};
-  ptr->config = in_config_type;
-  /// \brief 如果已经连接, 复用 连接
-  if (ptr->is_connect) {
-    // 设置操作超时
-    boost::beast::get_lowest_layer(ptr->ssl_stream)
-        .expires_after(std::chrono::seconds(30));
-
-    // 向远程主机发送 HTTP 请求
-    ptr->config.async_write(ptr->ssl_stream);
-  } else {
-    // Look up the domain name
-    ptr->resolver_.async_resolve(
-        host,
-        port,
-        boost::beast::bind_front_handler(&client::on_resolve, shared_from_this())
-    );
-  }
-}
-
-void client::on_resolve(
-    boost::system::error_code ec,
-    const boost::asio::ip::basic_resolver<
-        boost::asio::ip::tcp, boost::asio::any_io_executor>::results_type& results
-) {
-  if (ec) {
-    throw_exception(boost::system::system_error{ec});
-  }
-
-  /// \brief 超时设置
-  boost::beast::get_lowest_layer(
-      ptr->ssl_stream
-  )
-      .expires_after(30s);
-
-  ptr->ssl_stream.set_verify_mode(boost::asio::ssl::verify_peer);
-
-  ptr->ssl_stream.set_verify_callback(
-      [](bool preverified,
-         boost::asio::ssl::verify_context& ctx) -> bool {
-        char subject_name[256];
-        X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
-        X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
-        DOODLE_LOG_INFO("Verifying {}", subject_name)
-
-        return true;
-      }
-  );
-
-  boost::beast::get_lowest_layer(ptr->ssl_stream)
-      .async_connect(
-          results,
-          boost::beast::bind_front_handler(&client::on_connect, shared_from_this())
-      );
-}
-
-void client::on_connect(
-    boost::system::error_code ec,
-    const boost::asio::ip::basic_endpoint<boost::asio::ip::tcp>&
-) {
-  if (ec) {
-    throw_exception(boost::system::system_error{ec});
-  }
-
-  // 执行SSL握手
-  ptr->ssl_stream.async_handshake(
-      boost::asio::ssl::stream_base::client,
-      boost::beast::bind_front_handler(
-          &client::on_handshake,
-          shared_from_this()
-      )
-  );
-}
-
-void client::on_handshake(boost::system::error_code ec) {
-  if (ec) {
-    throw_exception(boost::system::system_error{ec});
-  }
-
-  // 设置操作超时
-  boost::beast::get_lowest_layer(ptr->ssl_stream)
-      .expires_after(std::chrono::seconds(30));
-  ptr->is_connect = true;
-  // 向远程主机发送 HTTP 请求
-  ptr->config.async_write(ptr->ssl_stream);
-}
-
-void client::on_write(boost::system::error_code ec, std::size_t bytes_transferred) const {
-  boost::ignore_unused(bytes_transferred);
-
-  if (ec) {
-    throw_exception(boost::system::system_error{ec});
-  }
-
-  // 接收HTTP响应
-  ptr->config.async_read(ptr->ssl_stream);
-}
-
 void client::async_shutdown() {
   // 异步关闭流
   ptr->is_connect = false;
   ptr->ssl_stream.async_shutdown(
-      boost::beast::bind_front_handler(
-          &client::on_shutdown,
-          shared_from_this()
-      )
-  );
-}
-void client::on_shutdown(boost::system::error_code ec) {
-  if (ec == boost::asio::error::eof) {
-    // Rationale:
-    // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
-    ec = {};
-  }
-  if (ec) {
-    throw_exception(boost::system::system_error{ec});
-  }
+      [l_c = shared_from_this()](
+          boost::system::error_code ec
+      ) {
+        if (ec == boost::asio::error::eof) {
+          // Rationale:
+          // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+          ec = {};
+        }
+        if (ec) {
+          throw_exception(boost::system::system_error{ec});
+        }
 
-  // 成功关机
+        // 成功关机
+      }
+  );
 }
 boost::beast::ssl_stream<boost::beast::tcp_stream>& client::ssl_stream() {
   return ptr->ssl_stream;
--- src/doodle_dingding/client/client.h
+++ src/doodle_dingding/client/client.h
@@ -43,7 +43,7 @@ struct async_http_req_res {
   boost::beast::ssl_stream<boost::beast::tcp_stream>& ssl_stream;
   std::shared_ptr<client> self_attr;
   using data_type = async_http_req_res_data<Req, Res>;
-  using data_ptr = std::shared_ptr<data_type>;
+  using data_ptr  = std::shared_ptr<data_type>;
   data_ptr p_data;
 
   enum {
@@ -128,7 +128,7 @@ struct async_http_req_res {
       }
 
       case on_resolve: {
-        state_   = on_connected;
+        state_ = on_connected;
         ssl_stream.set_verify_mode(boost::asio::ssl::verify_peer);
         ssl_stream.set_verify_callback(
             [](bool preverified,
@@ -247,24 +247,6 @@ class DOODLE_DINGDING_API client
   [[nodiscard("")]] boost::asio::ip::tcp::resolver& resolver();
   boost::asio::ip::tcp::resolver::results_type results_attr{};
 
-  void on_resolve(
-      boost::system::error_code ec,
-      const boost::asio::ip::tcp::resolver::results_type& results
-  );
-
-  void on_connect(
-      boost::system::error_code ec,
-      const boost::asio::ip::tcp::resolver::results_type::endpoint_type&
-  );
-
-  void on_handshake(boost::system::error_code ec);
-  void on_write(
-      boost::system::error_code ec,
-      std::size_t bytes_transferred
-  ) const;
-
-  void on_shutdown(boost::system::error_code ec);
-
   void async_shutdown();
 
  public:
@@ -272,23 +254,6 @@ class DOODLE_DINGDING_API client
       const boost::asio::any_io_executor& in_executor,
       boost::asio::ssl::context& in_ssl_context
   );
-
-  void run(
-      const std::string& in_host,
-      const std::string& in_target
-  );
-  template <typename HttpReqResType>
-  void run(HttpReqResType&& in) {
-    other_data = std::move(in);
-    config_type l_c{
-        [this](boost::beast::ssl_stream<boost::beast::tcp_stream>& in) {
-          std::any_cast<HttpReqResType>(this->other_data).async_write(in);
-        },
-        [this](boost::beast::ssl_stream<boost::beast::tcp_stream>& in) {
-          std::any_cast<HttpReqResType>(this->other_data).async_read(in);
-        }};
-    return run(std::any_cast<HttpReqResType>(this->other_data).url_attr, l_c);
-  }
 
   executor_type get_executor() noexcept;
 
@@ -325,128 +290,9 @@ class DOODLE_DINGDING_API client
   };
 
  protected:
-  /**
-   * @brief 这个是主要的运行类,
-   * @param in_url 传入的url, 用来分解 host, 和 post(post可也是默认 443 )
-   * @param in_config_type 配置, 这个主要是用来指针函数对象, 在异步读写时需要
-   */
-  void run(
-      const boost::url& in_url,
-      const config_type& in_config_type
-  );
-
  public:
   virtual ~client() noexcept;
 };
 
-namespace client_ns {
-// template <class T>
-// class base_http_req_res {
-//  public:
-//   void on_write(boost::system::error_code ec, std::size_t bytes_transferred) {
-//     boost::ignore_unused(bytes_transferred);
-//
-//     if (ec) {
-//       ;
-//     }
-//
-//     // Receive the HTTP response
-//     boost::beast::http::async_read(
-//         ptr->ssl_stream, ptr->buffer_, ptr->res_,
-//         boost::beast::bind_front_handler(&on_read, shared_from_this())
-//     );
-//   }
-//   void on_read(boost::system::error_code ec, std::size_t bytes_transferred) {
-//     boost::ignore_unused(bytes_transferred);
-//     if (ec) {
-//       DOODLE_LOG_INFO("read {}", ec.what());
-//       return;
-//     }
-//
-//     // 打印消息
-//     DOODLE_LOG_INFO(ptr->res_.body())
-//
-//     // Set a timeout on the operation
-//     boost::beast::get_lowest_layer(ptr->ssl_stream)
-//         .expires_after(30s);
-//
-//     // Gracefully close the stream
-//     ptr->ssl_stream.async_shutdown(
-//         boost::beast::bind_front_handler(
-//             &client::on_shutdown,
-//             shared_from_this()
-//         )
-//     );
-//   }
-// };
-template <typename Req, typename Res>
-class http_req_res {
-  boost::beast::flat_buffer buffer_{};
-  boost::beast::ssl_stream<boost::beast::tcp_stream>* ssl_stream{};
 
- public:
-  explicit http_req_res(const std::shared_ptr<client>& in_self)
-      : self(in_self) {}
-
-  std::weak_ptr<client> self;
-  Req req_attr;
-  Res res_attr;
-  boost::url url_attr;
-
-  void async_write(
-      boost::beast::ssl_stream<boost::beast::tcp_stream>& in_ssl_stream
-  ) {
-    boost::url l_url{url_attr};
-    l_url.remove_origin();  /// \brief 去除一部分
-    /// \brief 设置http的一些通用方法
-    req_attr.version(11);
-    //    req_attr.method(boost::beast::http::verb::get);
-    req_attr.target(l_url.c_str());
-    req_attr.set(boost::beast::http::field::host, url_attr.host());
-    req_attr.set(
-        boost::beast::http::field::user_agent,
-        BOOST_BEAST_VERSION_STRING
-    );
-    req_attr.prepare_payload();
-
-    boost::beast::http::async_write(
-        in_ssl_stream, req_attr,
-        boost::beast::bind_front_handler(
-            &client::on_write, self.lock()->shared_from_this()
-        )
-    );
-  }
-
-  void async_read(
-      boost::beast::ssl_stream<boost::beast::tcp_stream>& in_ssl_stream
-  ) {
-    ssl_stream = std::addressof(in_ssl_stream);
-    // 接收HTTP响应
-    boost::beast::http::async_read(
-        in_ssl_stream, buffer_, res_attr,
-        [self = self.lock()->shared_from_this(), this](auto&&... in) {
-          this->on_read(std::forward<decltype(in)>(in)...);
-        }
-    );
-  }
-  void on_read(boost::system::error_code ec, std::size_t bytes_transferred) {
-    auto l_self = self.lock();
-    boost::ignore_unused(bytes_transferred);
-    if (ec) {
-      throw_exception(boost::system::system_error{ec});
-    }
-    /// 设置超时
-    using namespace std::literals;
-    boost::beast::get_lowest_layer(*ssl_stream)
-        .expires_after(30s);
-
-    read_fun(res_attr);
-    if (!res_attr.keep_alive())
-      l_self->async_shutdown();
-  }
-
-  std::function<void(const Res&)> read_fun{};
-};
-
-}  // namespace client_ns
 }  // namespace doodle::dingding
