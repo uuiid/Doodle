d9139c30b54a4cc151636abca9b3134883bdf1cf
uuiid <957714080@qq.com>
2022-09-16 13:05:05 +0800

    修正动画实例的创建

--- script/uePlug/Doodle/Source/Doodle/Private/DoodleAnimInstance.cpp
+++ script/uePlug/Doodle/Source/Doodle/Private/DoodleAnimInstance.cpp
@@ -1,104 +1,98 @@
 #include "DoodleAnimInstance.h"
 
 UDoodleAnimInstance::UDoodleAnimInstance()
-    : Super()
-{
+    : Super() {
 }
 
-void UDoodleAnimInstance::DoodleCalculateSpeed()
-{
+void UDoodleAnimInstance::DoodleCalculateSpeed() {
+  APawn *LPawn = TryGetPawnOwner();
 
-    APawn *LPawn = TryGetPawnOwner();
+  if (LPawn) {
+    FVector LVelocity = LPawn->GetVelocity();
+    DirectionAttrXY   = CalculateDirection(
+          LVelocity,
+          LPawn->GetBaseAimRotation()
+      );
+  }
+}
 
-    if (LPawn)
-    {
-        FVector LVelocity = LPawn->GetVelocity();
-        DirectionAttrXY = CalculateDirection(
-            LVelocity,
-            LPawn->GetBaseAimRotation());
+void UDoodleAnimInstance::DoodleLookAtObject(const AActor *InActor) {
+  // const APawn *LInPawn = Cast<const APawn>(InActor);
+  APawn *LSelfPawn = TryGetPawnOwner();
+  // AActor *LSelfPawn = GetOwningActor();
+  if (LSelfPawn) {
+    FVector LInVector   = InActor->GetActorLocation();
+    FVector LSelfVector = LSelfPawn->GetActorLocation();
+
+    FVector LVector     = LInVector - LSelfVector;
+    // LVector.Normalize();
+    // FRotator LRot = LVector.Rotation();
+    // DirectionAttrXY = LRot.Yaw;
+    // DirectionAttrZ = LRot.Pitch;
+    // UE_LOG(LogTemp, Log, TEXT("LRot: %s"), *(LRot.ToString()));
+    if (!LVector.IsNearlyZero()) {
+      // 此处代码是根据 CalculateDirection 函数来的
+      FMatrix LSelfRot = FRotationMatrix{
+          LSelfPawn->GetBaseAimRotation()};
+
+      FVector LXYNormal     = LVector.GetSafeNormal2D();
+      FVector LForward      = LSelfRot.GetScaledAxis(EAxis::X);
+      // UE_LOG(LogTemp, Log, TEXT("LForward: %s"), *(LForward.ToString()));
+
+      // 获取向前矢量和速度(XY平面) 的cos -> 弧度值
+      float ForwardCosAngle = FVector::DotProduct(LForward, LXYNormal);
+      // 将弧度值转换为度
+      DirectionAttrXY       = FMath::RadiansToDegrees(FMath::Acos(ForwardCosAngle));
+
+      // 判断向前轴和速度方向, 根据结果进行翻转
+      float RightCosAngle   = FVector::DotProduct(LSelfRot.GetScaledAxis(EAxis::Y), LXYNormal);
+      if (RightCosAngle < 0) {
+        DirectionAttrXY *= -1;
+      }
+
+      // 这下面是上下角度的代码
+      // FVector2D LZNormal2D{LVector.X, LVector.Z};
+      // LZNormal2D.Normalize();
+      // FVector LXZNormal{LZNormal2D.X, 0.0f, LZNormal2D.Y};
+      FVector LXZNormal = FVector::VectorPlaneProject(LVector, LSelfRot.GetScaledAxis(EAxis::Y));
+      LXZNormal.Normalize();
+      // UE_LOG(LogTemp, Log, TEXT("LXZNormal: %s"), *(LXZNormal.ToString()));
+
+      // 获取向前矢量和速度(XZ平面) 的cos -> 弧度值
+      ForwardCosAngle = FVector::DotProduct(LForward, LXZNormal);
+      // 将弧度值转换为度
+      DirectionAttrZ  = FMath::RadiansToDegrees(FMath::Acos(ForwardCosAngle));
+
+      // 判断向前轴和速度方向, 根据结果进行翻转
+      RightCosAngle   = FVector::DotProduct(LSelfRot.GetScaledAxis(EAxis::Z), LXZNormal);
+      if (RightCosAngle < 0) {
+        DirectionAttrZ *= -1;
+      }
+      // UE_LOG(LogTemp, Log, TEXT("DirectionAttrXY %f DirectionAttrZ: %f"), DirectionAttrXY, DirectionAttrZ);
     }
+  }
 }
 
-void UDoodleAnimInstance::DoodleLookAtObject(const AActor *InActor)
-{
-    // const APawn *LInPawn = Cast<const APawn>(InActor);
-    APawn *LSelfPawn = TryGetPawnOwner();
-    // AActor *LSelfPawn = GetOwningActor();
-    if (LSelfPawn)
-    {
-        FVector LInVector = InActor->GetActorLocation();
-        FVector LSelfVector = LSelfPawn->GetActorLocation();
+void UDoodleAnimInstance::DoodleRandom() {
+  // 获得物体创建时间
+  float time  = GetOwningActor()->GetGameTimeSinceCreation() / 2.0f + RandomAttr_InstallValue;
 
-        FVector LVector = LInVector - LSelfVector;
-        // LVector.Normalize();
-        // FRotator LRot = LVector.Rotation();
-        // DirectionAttrXY = LRot.Yaw;
-        // DirectionAttrZ = LRot.Pitch;
-        // UE_LOG(LogTemp, Log, TEXT("LRot: %s"), *(LRot.ToString()));
-        if (!LVector.IsNearlyZero())
-        {
-            // 此处代码是根据 CalculateDirection 函数来的
-            FMatrix LSelfRot = FRotationMatrix{
-                LSelfPawn->GetBaseAimRotation()};
+  int l_min   = FMath::Min(RandomAttrMax, RandomAttrMin);
+  int l_max   = FMath::Max(RandomAttrMax, RandomAttrMin);
 
-            FVector LXYNormal = LVector.GetSafeNormal2D();
-            FVector LForward = LSelfRot.GetScaledAxis(EAxis::X);
-            // UE_LOG(LogTemp, Log, TEXT("LForward: %s"), *(LForward.ToString()));
+  // 生成噪波函数
+  float noise = FMath::PerlinNoise1D(time);
+  noise       = FMath::Abs(noise) * (float)(l_max - l_min) + l_min;
+  // FMath::DivideAndRoundNearest()
 
-            // 获取向前矢量和速度(XY平面) 的cos -> 弧度值
-            float ForwardCosAngle = FVector::DotProduct(LForward, LXYNormal);
-            // 将弧度值转换为度
-            DirectionAttrXY = FMath::RadiansToDegrees(FMath::Acos(ForwardCosAngle));
-
-            // 判断向前轴和速度方向, 根据结果进行翻转
-            float RightCosAngle = FVector::DotProduct(LSelfRot.GetScaledAxis(EAxis::Y), LXYNormal);
-            if (RightCosAngle < 0)
-            {
-                DirectionAttrXY *= -1;
-            }
-
-            // 这下面是上下角度的代码
-            // FVector2D LZNormal2D{LVector.X, LVector.Z};
-            // LZNormal2D.Normalize();
-            // FVector LXZNormal{LZNormal2D.X, 0.0f, LZNormal2D.Y};
-            FVector LXZNormal = FVector::VectorPlaneProject(LVector, LSelfRot.GetScaledAxis(EAxis::Y));
-            LXZNormal.Normalize();
-            // UE_LOG(LogTemp, Log, TEXT("LXZNormal: %s"), *(LXZNormal.ToString()));
-
-            // 获取向前矢量和速度(XZ平面) 的cos -> 弧度值
-            ForwardCosAngle = FVector::DotProduct(LForward, LXZNormal);
-            // 将弧度值转换为度
-            DirectionAttrZ = FMath::RadiansToDegrees(FMath::Acos(ForwardCosAngle));
-
-            // 判断向前轴和速度方向, 根据结果进行翻转
-            RightCosAngle = FVector::DotProduct(LSelfRot.GetScaledAxis(EAxis::Z), LXZNormal);
-            if (RightCosAngle < 0)
-            {
-                DirectionAttrZ *= -1;
-            }
-            // UE_LOG(LogTemp, Log, TEXT("DirectionAttrXY %f DirectionAttrZ: %f"), DirectionAttrXY, DirectionAttrZ);
-        }
-    }
+  // 获得最大和最小
+  // UE_LOG(LogTemp, Log, TEXT("noise %f "), noise);
+  RandomAttr  = FMath::Clamp((int)noise, l_min, l_max);
+  UE_LOG(LogTemp, Log, TEXT("RandomAttr %d "), RandomAttr);
 }
 
-void UDoodleAnimInstance::DoodleRandom()
-{
-    // 获得物体创建时间
-    float time = GetOwningActor()->GetGameTimeSinceCreation() / 2.0f + RandomAttr_InstallValue;
-
-    // 生成噪波函数
-    float noise = FMath::PerlinNoise1D(time);
-    noise = FMath::Abs(noise);
-    // FMath::DivideAndRoundNearest()
-
-    // 获得最大和最小猪
-    float tmp_Clamp = FMath::Clamp(noise, 0.25f, 0.75f);
-    RandomAttr = FMath::GetMappedRangeValueClamped({0.25f, 0.75f}, {0.0f, 1.0f}, noise);
-    UE_LOG(LogTemp, Log, TEXT("RandomAttr %f "), RandomAttr);
-}
-
-void UDoodleAnimInstance::NativeBeginPlay()
-{
-    UAnimInstance::NativeBeginPlay();
-    RandomAttr_InstallValue = FMath::RandRange(0.0, 256.0f);
+void UDoodleAnimInstance::NativeBeginPlay() {
+  UAnimInstance::NativeBeginPlay();
+  RandomAttr_InstallValue = FMath::RandRange(0.0, 256.0f);
+  UE_LOG(LogTemp, Log, TEXT("RandomAttr_InstallValue: %f"), RandomAttr_InstallValue);
 }
--- script/uePlug/Doodle/Source/Doodle/Public/DoodleAnimInstance.h
+++ script/uePlug/Doodle/Source/Doodle/Public/DoodleAnimInstance.h
@@ -3,36 +3,40 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
-#include "TimerManager.h" //定时器
+#include "TimerManager.h"  //定时器
 #include "Animation/AnimInstance.h"
 // 这个必须最后导入
 #include "DoodleAnimInstance.generated.h"
 
 UCLASS(Blueprintable)
-class DOODLE_API UDoodleAnimInstance : public UAnimInstance
-{
-public:
-    GENERATED_BODY()
-    UDoodleAnimInstance();
-    UFUNCTION(BlueprintCallable, Category = "Doodle")
-    void DoodleCalculateSpeed();
+class DOODLE_API UDoodleAnimInstance : public UAnimInstance {
+ public:
+  GENERATED_BODY()
+  UDoodleAnimInstance();
+  UFUNCTION(BlueprintCallable, Category = "Doodle")
+  void DoodleCalculateSpeed();
 
-    UFUNCTION(BlueprintCallable, Category = "Doodle")
-    void DoodleLookAtObject(const AActor *InActor);
+  UFUNCTION(BlueprintCallable, Category = "Doodle")
+  void DoodleLookAtObject(const AActor *InActor);
 
-    void DoodleRandom();
+  void DoodleRandom();
 
-    void NativeBeginPlay() override;
+  void NativeBeginPlay() override;
 
-protected:
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
-    float VelocityAttr{0.f};
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
-    float DirectionAttrXY{0.f};
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
-    float DirectionAttrZ{0.f};
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
-    float RandomAttr{0.f};
+ protected:
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
+  float VelocityAttr{0.f};
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
+  float DirectionAttrXY{0.f};
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Doodle")
+  float DirectionAttrZ{0.f};
+  UPROPERTY(EditAnywhere, Interp, BlueprintReadOnly, Category = "Doodle")
+  int RandomAttr{};
 
-    float RandomAttr_InstallValue;
+  UPROPERTY(EditAnywhere, Interp, BlueprintReadOnly, Category = "Doodle", Meta = (ClampMin = 0))
+  int RandomAttrMin{0};
+  UPROPERTY(EditAnywhere, Interp, BlueprintReadOnly, Category = "Doodle", Meta = (ClampMin = 0))
+  int RandomAttrMax{4};
+
+  float RandomAttr_InstallValue;
 };
