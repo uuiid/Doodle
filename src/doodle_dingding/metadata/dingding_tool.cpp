789d59f07496d9b06f7d34a789cb3b4f69eb3311
uuiid <957714080@qq.com>
2022-09-19 10:52:33 +0800

    添加新类

---
+++ src/doodle_dingding/metadata/dingding_tool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2022/9/19.
+//
+
+#include "dingding_tool.h"
---
+++ src/doodle_dingding/metadata/dingding_tool.h
@@ -0,0 +1,15 @@
+//
+// Created by TD on 2022/9/19.
+//
+
+#pragma once
+
+#include <doodle_dingding/doodle_dingding_fwd.h>
+namespace doodle::dingding::detail {
+
+class DOODLE_DINGDING_API tool {
+ public:
+
+};
+
+}  // namespace doodle::dingding::detail
