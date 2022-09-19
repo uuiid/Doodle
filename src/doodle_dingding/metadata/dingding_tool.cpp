d4d23ac2ed0286df417f1451fe5db3793b27907c
uuiid <957714080@qq.com>
2022-09-19 11:10:48 +0800

    添加剂新的时间换算

--- src/doodle_dingding/metadata/dingding_tool.cpp
+++ src/doodle_dingding/metadata/dingding_tool.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "dingding_tool.h"
+#include <doodle_core/metadata/time_point_wrap.h>
+#include <nlohmann/json.hpp>
+namespace doodle::dingding::detail {
+time_point_wrap tool::parse_dingding_time(const nlohmann::json& time_obj) {
+  chrono::local_seconds time;
+  std::istringstream l_time{time_obj.get<std::string>()};
+  l_time >> chrono::parse("%Y-%m-%d %H:%M:%S", time);
+  return time_point_wrap{time};
+}
+time_point_wrap tool::parse_dingding_Date(const nlohmann::json& time_obj) {
+  chrono::microseconds l_time{time_obj.get<chrono::microseconds::rep>()};
+  return time_point_wrap{chrono::sys_time<chrono::microseconds>{l_time}};
+}
+
+}  // namespace doodle::dingding::detail
--- src/doodle_dingding/metadata/dingding_tool.h
+++ src/doodle_dingding/metadata/dingding_tool.h
@@ -5,11 +5,17 @@
 #pragma once
 
 #include <doodle_dingding/doodle_dingding_fwd.h>
+#include <nlohmann/json_fwd.hpp>
+namespace doodle {
+class time_point_wrap;
+}
+
 namespace doodle::dingding::detail {
 
 class DOODLE_DINGDING_API tool {
  public:
-
+  static time_point_wrap parse_dingding_time(const nlohmann::json& time_obj);
+  static time_point_wrap parse_dingding_Date(const nlohmann::json& time_obj);
 };
 
 }  // namespace doodle::dingding::detail
