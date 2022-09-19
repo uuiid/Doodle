0257784fcaf6c200b701f65ee8db5b57242449c2
uuiid <957714080@qq.com>
2022-09-19 10:35:41 +0800

    去除无用的函数

--- src/doodle_core/lib_warp/std_warp.h
+++ src/doodle_core/lib_warp/std_warp.h
@@ -7,27 +7,6 @@
 #include <fmt/format.h>
 #include <boost/hana.hpp>
 #include <memory>
-namespace doodle {
-namespace details {
-
-}  // namespace details
-
-template <class ClassIn, class... Args>
-std::shared_ptr<ClassIn> new_object(Args &&...in_args) {
-  // post_constructor
-  constexpr auto has_make_this =
-      boost::hana::is_valid(
-          [](auto &&obj)
-              -> decltype(obj->post_constructor()) {}
-      );
-  auto ptr              = std::make_shared<ClassIn>(std::forward<Args>(in_args)...);
-  using has_make_this_v = decltype(has_make_this(ptr));
-  if constexpr (has_make_this_v{}) {
-    ptr->post_constructor();
-  }
-  return ptr;
-}
-}  // namespace doodle
 
 namespace fmt {
 /**
