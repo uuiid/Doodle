44ce5a8159a7e8101aa74abdf993c701996d3f88
uuiid <957714080@qq.com>
2022-09-20 18:09:07 +0800

    调整钉钉json

--- src/doodle_dingding/metadata/dingding_tool.cpp
+++ src/doodle_dingding/metadata/dingding_tool.cpp
@@ -8,34 +8,7 @@
 
 #include <date/tz.h>
 
-
 #include <fmt/chrono.h>
-
-
-//namespace fmt{
-//template <typename Char, typename Duration>
-//struct formatter<std::chrono::time_point<date::local_t, Duration>,
-//                 Char> : formatter<std::tm, Char> {
-//  FMT_CONSTEXPR formatter() {
-//    this->do_parse(default_specs,
-//                   default_specs + sizeof(default_specs) / sizeof(Char));
-//  }
-//
-//  template <typename ParseContext>
-//  FMT_CONSTEXPR auto parse(ParseContext& ctx) -> decltype(ctx.begin()) {
-//    return this->do_parse(ctx.begin(), ctx.end(), true);
-//  }
-//
-//  template <typename FormatContext>
-//  auto format(std::chrono::time_point<std::chrono::system_clock> val,
-//              FormatContext& ctx) const -> decltype(ctx.out()) {
-//    return formatter<std::tm, Char>::format(localtime(val), ctx);
-//  }
-//
-//  static constexpr const Char default_specs[] = {'%', 'F', ' ', '%', 'T'};
-//};
-//}
-
 
 namespace doodle::dingding::detail {
 time_point_wrap tool::parse_dingding_time(const nlohmann::json& time_obj) {
@@ -49,7 +22,7 @@ time_point_wrap tool::parse_dingding_Date(const nlohmann::json& time_obj) {
   return time_point_wrap{chrono::sys_time<chrono::microseconds>{l_time}};
 }
 std::string tool::print_dingding_time(const time_point_wrap& in_time) {
-  return fmt::format("{:%Y-%m-%d %H:%M:%S}", chrono::clock_cast<chrono::system_clock>(in_time.get_local_time()));
+  return fmt::format("{:L%Y-%m-%d %H:%M:%S}", in_time);
 }
 
 }  // namespace doodle::dingding::detail
