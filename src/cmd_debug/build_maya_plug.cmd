5420d805b1b7e9a27c4273c7700f7e1abbcdafa2
uuiid <957714080@qq.com>
2022-09-23 19:45:18 +0800

    添加构建debug maya cmd

---
+++ src/cmd_debug/build_maya_plug.cmd
@@ -0,0 +1,13 @@
+call %~dp0/../build/set_venv.cmd
+
+echo -----------------------------------------------------
+echo -----------------config maya 2018--------------------
+"C:\Program Files\CMake\bin\cmake.exe" ^
+-S%my_pwd% ^
+--preset doodle_maya
+
+if %errorlevel% NEQ 0 exit 1
+echo -----------------build maya 2018--------------------
+"C:\Program Files\CMake\bin\cmake.exe" ^
+--build ^
+--preset doodle_maya --target doodle_maya
