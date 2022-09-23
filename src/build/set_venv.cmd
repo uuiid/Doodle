4a35fae1792033b1535b84ea45d9db1a21c9c57e
uuiid <957714080@qq.com>
2022-09-23 17:01:04 +0800

    调整构建脚本

--- src/build/set_venv.cmd
+++ src/build/set_venv.cmd
@@ -1,7 +1,9 @@
-if exist "%cd%/.git/config" goto end
-if not exist "%cd%/.git/config" goto set_pwd
-goto end
+@echo off
 
+if exist "%my_pwd%/src/doodle_core" goto end
+if not exist "%my_pwd%/src/doodle_core" goto set_pwd
+
+goto end
 
 :set_pwd
 set my_pwd=%~dp0
@@ -10,7 +12,7 @@ echo %my_pwd%
 echo -----------------set env--------------------
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-cd %my_pwd%/../..
+cd %~dp0/../..
 
 set my_pwd=%cd%
 call %my_pwd%/.venv/Scripts/activate.bat
