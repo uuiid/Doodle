e6e6b20fcc71f43316aa645676b18bdf1819859d
uuiid <957714080@qq.com>
2022-09-23 17:53:37 +0800

    ide设置

--- src/test/core/bvh.cpp
+++ src/test/core/bvh.cpp
@@ -2,17 +2,11 @@
 // Created by TD on 2022/9/23.
 //
 
-#define _CRTDBG_MAP_ALLOC
+ 
 #include <cstdlib>
 #include <crtdbg.h>
 
-#ifdef _DEBUG
-#define DBG_NEW new (_NORMAL_BLOCK, __FILE__, __LINE__)
-// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
-// allocations to be of _CLIENT_BLOCK type
-#else
-#define DBG_NEW new
-#endif
+ 
 
 #define BOOST_TEST_MODULE doodle lib
 
