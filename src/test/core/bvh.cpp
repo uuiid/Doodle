b93f0e96e904db0eb488c0164ec5b5a4db2add46
uuiid <957714080@qq.com>
2022-09-25 18:37:19 +0800

    完成部分bvh解析

--- src/test/core/bvh.cpp
+++ src/test/core/bvh.cpp
@@ -42,6 +42,7 @@ using namespace doodle;
 #include <boost/spirit/include/phoenix1.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/support.hpp>
+#include <boost/spirit/repository/include/qi_kwd.hpp>
 // #include <boost/spirit/home/classic.hpp>
 #include <boost/spirit/home/karma.hpp>
 #include <boost/spirit/home/lex.hpp>
@@ -54,13 +55,45 @@ using namespace doodle;
 #include <boost/phoenix/stl.hpp>
 #include <boost/mpl/range_c.hpp>
 #include <boost/mpl/for_each.hpp>
+#include <boost/variant/recursive_variant.hpp>
 
 // #include <boost/>
 
 namespace doodle::bvh::detail {
 namespace qi    = boost::spirit::qi;
 namespace ascii = boost::spirit::ascii;
-struct DOODLE_CORE_API offset {
+
+struct node;
+
+using rational_int = boost::rational<std::size_t>;
+// using rational_int = std::float_t;
+
+struct node {
+  std::string name;
+  std::double_t x;
+  std::double_t y;
+  std::double_t z;
+  std::vector<std::string> channels_attr;
+  std::vector<node> childs;
+};
+
+struct bvh_tree {
+  bvh_tree() = default;
+
+  explicit bvh_tree(
+      std::size_t in_frames,
+      rational_int in_frame_time,
+      node in_root
+  ) : frames(in_frames),
+      frame_time(in_frame_time),
+      root(in_root) {}
+
+  std::size_t frames;
+  rational_int frame_time;
+  node root;
+};
+
+struct offset {
   std::double_t x;
   std::double_t y;
   std::double_t z;
@@ -86,7 +119,8 @@ struct offset_parser_impl : qi::grammar<Iterator, offset(), ascii::space_type> {
         double_[phoe::at_c<0>(qi::_val) = qi::_1] >>
         double_[phoe::at_c<1>(qi::_val) = qi::_1] >>
         double_[phoe::at_c<2>(qi::_val) = qi::_1] >>
-        qi::string("CHANNELS") >> int_ >> *text[phoe::push_back(phoe::at_c<3>(qi::_val), qi::_1)];
+        qi::string("CHANNELS") >> int_ >>
+        *text[phoe::push_back(phoe::at_c<3>(qi::_val), qi::_1)];
   }
 
   qi::rule<Iterator, offset(), ascii::space_type> start;
@@ -109,11 +143,58 @@ BOOST_FUSION_ADAPT_STRUCT(
     (std::double_t, z)
     (std::vector<std::string>, channels_attr)
 )
+BOOST_FUSION_ADAPT_STRUCT(
+    doodle::bvh::detail::node,
+    (std::string, name)
+    (std::double_t, x)
+    (std::double_t, y)
+    (std::double_t, z)
+    (std::vector<std::string>, channels_attr)
+    (std::vector<doodle::bvh::detail::node>, childs)
+)
+BOOST_FUSION_ADAPT_STRUCT(
+    doodle::bvh::detail::bvh_tree,
+    (std::size_t, frames)
+    (doodle::bvh::detail::rational_int, frame_time)
+    (doodle::bvh::detail::node, root)
+)
 
-// clang-format onn
+// clang-format on
 
+namespace doodle::bvh::detail {
+template <typename Iterator>
+struct bvh_tree_impl : qi::grammar<Iterator, bvh_tree(), ascii::space_type> {
+  bvh_tree_impl() : bvh_tree_impl::base_type(start) {
+    using ascii::char_;
+    using boost::spirit::repository::qi::kwd;
+    using qi::double_;
+    using qi::int_;
+    using qi::lexeme;
+    using qi::lit;
+    using qi::no_skip;
 
+    namespace phoe = boost::phoenix;
+    namespace lab  = qi::labels;
 
+    channel %= lexeme[+(char_ - ' ')];
+
+    start = qi::string("HIERARCHY") >>
+            node_rule[phoe::at_c<2>(qi::_val) = qi::_1] >>
+            qi::string("MOTION") >>
+            qi::string("Frames:") >> qi::int_[phoe::at_c<1>(qi::_val) = qi::_1] >>
+            qi::string("Frame") >> qi::string("Time:") >>
+            qi::float_[phoe::at_c<1>(qi::_val) = rational_int{}];  // std::float_t(qi::_1 * 1000), 1000.0f
+  }
+
+  qi::rule<Iterator, bvh_tree(), ascii::space_type> start;
+  qi::rule<Iterator, node(), ascii::space_type> node_rule;
+  qi::rule<Iterator, std::string(), ascii::space_type> channel;
+
+  //  qi::rule<Iterator, void(std::string), ascii::space_type> end_tag;
+};
+
+using bvh_tree_parse = bvh_tree_impl<std::string::const_iterator>;
+}  // namespace doodle::bvh::detail
 
 struct loop_fixtures {
   loop_fixtures()  = default;
@@ -125,8 +206,8 @@ struct loop_fixtures {
     // doodle_lib::create_time_database();
     BOOST_TEST_MESSAGE("完成夹具设置");
   };
-  void teardown() {
-    // l_lib.reset();
+  void teardown(){
+      // l_lib.reset();
   };
 };
 
@@ -167,41 +248,32 @@ BOOST_AUTO_TEST_CASE(test_base_spirit) {
   }
 }
 
-BOOST_AUTO_TEST_CASE(bvh_mpl){
-  doodle::bvh::detail::offset l_w{0.1,0.2,0.3,{"dsad"s,"dsadsa"s}};
-  auto l_str =  boost::fusion::as_vector(l_w);
+BOOST_AUTO_TEST_CASE(bvh_mpl) {
+  doodle::bvh::detail::bvh_tree l_w{
+      100ull,
+      doodle::bvh::detail::rational_int{60},
+      doodle::bvh::detail::node{}};
+  auto l_str = boost::fusion::as_vector(l_w);
 
   BOOST_TEST_MESSAGE(boost::fusion::at_c<0>(l_str));
-  BOOST_TEST_MESSAGE(boost::fusion::at_c<1>(l_str));
-  BOOST_TEST_MESSAGE(boost::fusion::at_c<2>(l_str));
-  BOOST_TEST_MESSAGE(boost::fusion::at_c<3>(l_str)[0]);
-  BOOST_TEST_MESSAGE(boost::fusion::at_c<3>(l_str)[1]);
-
+  // BOOST_TEST_MESSAGE(boost::rational_cast<std::double_t>(boost::fusion::at_c<1>(l_str)));
+  // BOOST_TEST_MESSAGE(boost::fusion::at_c<2>(l_str));
 }
 
 BOOST_AUTO_TEST_CASE(bvh_offest, *boost::unit_test::tolerance(0.00001)) {
-  std::string l_data{"OFFSET 0.000 91.545 0.000 \n CHANNELS 6 Xposition Yposition Zposition Zrotation Xrotation Yrotation"};
-const auto l_str_list = std::vector{"Xposition"s,
-"Yposition"s,
-"Zposition"s,
-"Zrotation"s,
-"Xrotation"s,
-"Yrotation"s};
+  std::string l_data{"HIERARCHY ROOT Hips"};
+
   namespace qi    = boost::spirit::qi;
   namespace ascii = boost::spirit::ascii;
 
+  doodle::bvh::detail::bvh_tree_parse l_p{};
+  doodle::bvh::detail::bvh_tree l_w{};
+  auto l_r = boost::spirit::qi::phrase_parse(
+      l_data.begin(), l_data.end(), l_p, ascii::space, l_w
+  );
 
-   doodle::bvh::detail::offset_parser l_p{};
-   doodle::bvh::detail::offset l_w{};
-   auto l_r = boost::spirit::qi::phrase_parse(
-       l_data.begin(), l_data.end(), l_p, ascii::space, l_w
-   );
-
-   BOOST_TEST(l_r);
-   BOOST_TEST(l_w.x == 0.000);
-   BOOST_TEST(l_w.y == 91.545);
-   BOOST_TEST(l_w.z == 0.000);
-   BOOST_TEST(l_w.channels_attr == l_str_list);
+  BOOST_TEST(l_r);
+  // BOOST_TEST(boost::rational_cast<std::double_t>(l_w.frame_time) == 0.000);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
