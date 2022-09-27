8b8e098b472a1d60e9282d3cedc6b490c4be2f79
uuiid <957714080@qq.com>
2022-09-27 16:09:36 +0800

    调整时间初始化类型

--- src/doodle_dingding/metadata/dingding_tool.cpp
+++ src/doodle_dingding/metadata/dingding_tool.cpp
@@ -19,7 +19,7 @@ time_point_wrap tool::parse_dingding_time(const nlohmann::json& time_obj) {
 }
 time_point_wrap tool::parse_dingding_Date(const nlohmann::json& time_obj) {
   chrono::milliseconds l_time{time_obj.get<chrono::milliseconds::rep>()};
-  return time_point_wrap{chrono::local_time_pos{l_time}};
+  return time_point_wrap{time_point_wrap::time_local_point{l_time}};
 }
 std::string tool::print_dingding_time(const time_point_wrap& in_time) {
   return fmt::format("{:L%Y-%m-%d %H:%M:%S}", in_time);
