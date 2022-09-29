a725c9cc5034e4e5b4a1486e8c1f0bc32736a52b
uuiid <957714080@qq.com>
2022-09-29 10:04:47 +0800

    分离app command类

--- src/doodle_lib/core/app_command_base.h
+++ src/doodle_lib/core/app_command_base.h
@@ -9,28 +9,6 @@
 
 namespace doodle {
 
-/**
- * @brief 基本的命令行类
- *
- */
-class DOODLELIB_API app_command_base : public app_base {
- protected:
-  cmd_string_type cmd_str;
 
-  void load_back_end() override;
-  virtual bool chick_authorization();
-
- public:
-  explicit app_command_base(const app_base::in_app_args& in_instance);
-
-  static app_command_base& Get();
-  //    std::vector<std::string> l_str{argv, argv + argc};
-
-  bool chick_authorization(const FSys::path& in_path);
-  program_options_ptr options_;
-
- protected:
-  virtual void post_constructor() override;
-};
 
 }  // namespace doodle
