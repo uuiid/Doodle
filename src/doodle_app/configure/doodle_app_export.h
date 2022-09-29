a0522f33f9ee597a45977b48a34c151fc01d0886
uuiid <957714080@qq.com>
2022-09-29 12:02:56 +0800

    调整宏

--- src/doodle_app/configure/doodle_app_export.h
+++ src/doodle_app/configure/doodle_app_export.h
@@ -4,12 +4,11 @@
 
 #pragma once
 
-
 #ifdef DOODLE_APP_EXPORT
 /* We are building this library */
 #define DOODLE_APP_API __declspec(dllexport)
 #else
 /* We are using this library */
-#define DOODLE_APP_API __declspec(dllimport)
+// #define DOODLE_APP_API __declspec(dllimport)
+#define DOODLE_APP_API
 #endif
-
