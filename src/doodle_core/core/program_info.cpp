5156fc5f400f84d96851f8ebc631d21e5f92bba2
uuiid <957714080@qq.com>
2022-09-30 11:54:53 +0800

    添加程序信息类

---
+++ src/doodle_core/core/program_info.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/9/30.
+//
+
+#include "program_info.h"
+
+namespace doodle {
+namespace core {
+}  // namespace core
+}  // namespace doodle
---
+++ src/doodle_core/core/program_info.h
@@ -0,0 +1,17 @@
+//
+// Created by TD on 2022/9/30.
+//
+
+#ifndef DOODLE_SRC_DOODLE_CORE_CORE_PROGRAM_INFO_H_
+#define DOODLE_SRC_DOODLE_CORE_CORE_PROGRAM_INFO_H_
+
+namespace doodle {
+namespace core {
+
+class program_info {
+};
+
+}  // namespace core
+}  // namespace doodle
+
+#endif  // DOODLE_SRC_DOODLE_CORE_CORE_PROGRAM_INFO_H_
