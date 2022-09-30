3cb75238b8a6293a9307ce151e0dd85811a6ed92
uuiid <957714080@qq.com>
2022-09-30 10:41:36 +0800

    添加构面接口

--- src/doodle_core/core/app_facet.h
+++ src/doodle_core/core/app_facet.h
@@ -4,6 +4,30 @@
 
 #pragma once
 
-namespace doodle {
+#include <doodle_core/doodle_core_fwd.h>
 
-}
+namespace doodle {
+namespace detail {
+class DOODLE_CORE_API app_facet_interface {
+ public:
+  virtual ~app_facet_interface()                   = default;
+
+  /**
+   * 返回构面的名称以用来初始化命令行
+   * @return 构面名称
+   */
+  [[nodiscard]] virtual const std::string& name() const noexcept = 0;
+
+  /**
+   * 初始化
+   */
+  virtual void operator()()                        = 0;
+
+  /**
+   * 结束清理
+   */
+  virtual void deconstruction()                    = 0;
+};
+
+}  // namespace detail
+}  // namespace doodle
