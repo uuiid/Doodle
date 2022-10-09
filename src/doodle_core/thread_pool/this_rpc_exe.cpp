4442e6b2f547b44e8151fd3581452c1c87e3bc65
uuiid <957714080@qq.com>
2022-10-09 09:39:27 +0800

    添加新类

---
+++ src/doodle_core/thread_pool/this_rpc_exe.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2022/10/9.
+//
+
+#include "this_rpc_exe.h"
---
+++ src/doodle_core/thread_pool/this_rpc_exe.h
@@ -0,0 +1,12 @@
+//
+// Created by TD on 2022/10/9.
+//
+#pragma once
+
+#include <doodle_core/doodle_core_fwd.h>
+
+namespace doodle {
+
+class this_rpc_exe {
+};
+}  // namespace doodle
