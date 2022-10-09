5a85dd1c2d71b108fe539f8782c04fec8eabd323
uuiid <957714080@qq.com>
2022-10-09 09:21:35 +0800

    添加客户端函数

--- src/doodle_core/json_rpc/json_rpc_client.cpp
+++ src/doodle_core/json_rpc/json_rpc_client.cpp
@@ -7,6 +7,7 @@
 #include <json_rpc/json_rpc_static_value.h>
 #include <json_rpc/args/rpc_json_progress.h>
 #include <metadata/move_create.h>
+#include <doodle_core/lib_warp/entt_warp.h>
 #include <metadata/project.h>
 
 namespace doodle {
@@ -16,14 +17,11 @@ json_rpc_client::json_rpc_client(boost::asio::io_context& in_context, const std:
 void json_rpc_client::image_to_move(const std::vector<movie::image_attr>& in_list) {
   call_fun<void, false>(json_rpc::rpc_fun_name::image_to_move, in_list);
 }
-json_rpc::args::rpc_json_progress json_rpc_client::get_progress() {
-  return call_fun<json_rpc::args::rpc_json_progress>(json_rpc::rpc_fun_name::get_progress);
+process_message json_rpc_client::get_progress(entt::entity in_id) {
+  return call_fun<process_message>(json_rpc::rpc_fun_name::get_progress, in_id);
 }
-void json_rpc_client::open_project(const FSys::path& in_path) {
-  call_fun<void, false>(json_rpc::rpc_fun_name::open_project, in_path);
-}
-project_config::base_config json_rpc_client::get_project_config() {
-  return call_fun<project_config::base_config, false>(json_rpc::rpc_fun_name::get_project_config);
+void json_rpc_client::stop_app() {
+  return call_fun<void, true>(json_rpc::rpc_fun_name::stop_app);
 }
 
 }  // namespace doodle
--- src/doodle_core/json_rpc/json_rpc_client.h
+++ src/doodle_core/json_rpc/json_rpc_client.h
@@ -10,6 +10,7 @@
 #include <doodle_core/json_rpc/args/rpc_json_progress.h>
 
 #include <doodle_core/metadata/project.h>
+#include <doodle_core/thread_pool/process_message.h>
 namespace doodle {
 
 class DOODLE_CORE_API json_rpc_client : public json_rpc::rpc_client {
@@ -17,10 +18,8 @@ class DOODLE_CORE_API json_rpc_client : public json_rpc::rpc_client {
   json_rpc_client(boost::asio::io_context& in_context, const std::string& in_host, std::uint16_t in_post);
 
   void image_to_move(const std::vector<movie::image_attr>& in_list);
-  json_rpc::args::rpc_json_progress get_progress();
-
-  void open_project(const FSys::path& in_path);
-  project_config::base_config get_project_config();
+  process_message get_progress(entt::entity in_id);
+  void stop_app();
 
  public:
 };
