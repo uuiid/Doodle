02fede297b97ce0853569fa0bbd398ec0a151125
uuiid <957714080@qq.com>
2022-10-09 09:20:30 +0800

    添加新的名称

--- src/doodle_core/json_rpc/json_rpc_static_value.h
+++ src/doodle_core/json_rpc/json_rpc_static_value.h
@@ -11,5 +11,6 @@ constexpr static auto image_to_move{"image_to_move"};
 constexpr static auto get_progress{"get_progress"};
 constexpr static auto open_project{"open_project"};
 constexpr static auto get_project_config{"get_project_config"};
+constexpr static auto stop_app{"stop_app"};
 
 }  // namespace doodle::json_rpc::rpc_fun_name
