b895ba055d3895aacbf0a0ddafe1810d55d6e7e1
uuiid <957714080@qq.com>
2022-10-10 18:34:32 +0800

    添加ai 艺术家

--- src/ai_art/main.py
+++ src/ai_art/main.py
@@ -1,3 +1,8 @@
 from discoart import create
 
-# da = create()
+# 先安装 py torch
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 --proxy 127.0.0.1:10808
+# pip3 install --force-reinstall --no-cache-dir torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 --proxy 127.0.0.1:10808
+da = create()
+
+
