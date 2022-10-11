34c4614913420ebccee1292df83786f9c1a4c587
uuiid <957714080@qq.com>
2022-10-11 13:34:16 +0800

    添加一条新提示

--- src/ai_art/main.py
+++ src/ai_art/main.py
@@ -2,6 +2,7 @@ from discoart import create
 
 # 先安装 py torch
 # pip3 install --force-reinstall --no-cache-dir torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 --proxy 127.0.0.1:10808
+# The magic mirror after the end of the world
 da = create()
 
 
