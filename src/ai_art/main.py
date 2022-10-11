2823df7cfa9a91f62460f808a580fd1d3b442b32
uuiid <957714080@qq.com>
2022-10-11 08:54:40 +0800

    调整方法

--- src/ai_art/main.py
+++ src/ai_art/main.py
@@ -1,7 +1,6 @@
 from discoart import create
 
 # 先安装 py torch
-# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 --proxy 127.0.0.1:10808
 # pip3 install --force-reinstall --no-cache-dir torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 --proxy 127.0.0.1:10808
 da = create()
 
