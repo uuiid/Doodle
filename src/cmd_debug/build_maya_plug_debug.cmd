986f46646df735b2580b951693b9b7d69117db75
zhang <957714080@qq.com>
2022-10-13 17:24:56 +0800

    debug修正

--- src/cmd_debug/build_maya_plug_debug.cmd
+++ src/cmd_debug/build_maya_plug_debug.cmd
@@ -11,3 +11,5 @@ echo -----------------build maya 2018--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
 --preset debug_maya_plug --target doodle_maya
+
+robocopy %my_pwd%\build\doodle_maya\bin %my_pwd%\build\doodle_maya\plug\maya\plug-ins *.dll > %tmp%/build_maya_plug_debug.txt
