77bc925946e8ca863df4a01b1255b0f3a545fe76
zhang <957714080@qq.com>
2022-10-17 15:39:27 +0800

    添加加载实体命令

--- src/maya_plug/maya_comm/add_entt.cpp
+++ src/maya_plug/maya_comm/add_entt.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "add_entt.h"
+
+#include <doodle_core/metadata/assets_file.h>
+#include <doodle_core/metadata/redirection_path_info.h>
+
+#include <maya/MArgDatabase.h>
+#include <maya/MStatus.h>
+#include <nlohmann/json.hpp>
+namespace doodle::maya_plug {
+
+namespace add_entt_ns {
+
+constexpr auto json      = "-j";
+constexpr auto json_long = "-josn";
+
+MSyntax syntax() {
+  MSyntax l_synntax{};
+  l_synntax.addFlag(json, json_long, MSyntax::kString);
+  return l_synntax;
+}
+}  // namespace add_entt_ns
+MStatus add_entt::doIt(const MArgList& in_arg_list) {
+  MStatus l_s{};
+  MArgDatabase l_arg_database{syntax(), in_arg_list, &l_s};
+  DOODLE_MAYA_CHICK(l_s);
+
+  if (l_arg_database.isFlagSet(add_entt_ns::json, &l_s)) {
+    DOODLE_MAYA_CHICK(l_s);
+
+    MString l_str{};
+    l_s = l_arg_database.getFlagArgument(add_entt_ns::json, 0, l_str);
+    DOODLE_MAYA_CHICK(l_s);
+
+    auto l_json = nlohmann::json::parse(l_str.asUTF8());
+    auto l_h    = make_handle();
+    entt_tool::load_comm<assets_file, redirection_path_info>(l_h, l_json);
+    DOODLE_LOG_INFO("加载实体 {}", l_h);
+  }
+}
+}  // namespace doodle::maya_plug
--- src/maya_plug/maya_comm/add_entt.h
+++ src/maya_plug/maya_comm/add_entt.h
@@ -6,7 +6,13 @@
 #include <maya_plug/main/maya_plug_fwd.h>
 
 namespace doodle::maya_plug {
-
-class add_entt {};
+namespace add_entt_ns {
+constexpr char name[] = "doodle_add_entt";
+MSyntax syntax();
+}  // namespace add_entt_ns
+class add_entt : public TemplateAction<add_entt, add_entt_ns::name, add_entt_ns::syntax> {
+ public:
+  MStatus doIt(const MArgList& in_arg_list) override;
+};
 
 }  // namespace doodle::maya_plug
