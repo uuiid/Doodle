d0f7ccb47e948b10cdbe14ce7afea157515f3289
zhang <957714080@qq.com>
2022-10-18 16:03:14 +0800

    去除无用的文件

--- src/doodle_core/resource/maya_colth_export.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-\n
-import maya_fun_tool
-
-k_f = maya_fun_tool.open_file()
-k_f.config_ = """[{"export_path":"","path":"E:/tmp/test_fbx_11_26/DBXY_EP169_SC030_AN.ma","use_all_ref":false}]"""
-k_f()
