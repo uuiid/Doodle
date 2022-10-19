43e9b0d11a6eafc14d2ebf0b6307a391120feeaf
zhang <957714080@qq.com>
2022-10-19 17:57:02 +0800

    添加ui回调

--- src/maya_plug/gui/action/reference_attr_setting.cpp
+++ src/maya_plug/gui/action/reference_attr_setting.cpp
@@ -5,37 +5,33 @@
 #include "reference_attr_setting.h"
 
 #include <doodle_core/lib_warp/entt_warp.h>
-#include <doodle_app/lib_warp/imgui_warp.h>
 #include <doodle_core/metadata/metadata.h>
-#include <doodle_core/lib_warp/entt_warp.h>
-#include <doodle_app/gui/base/ref_base.h>
 
-#include <maya/MTime.h>
+#include <doodle_app/gui/base/ref_base.h>
+#include <doodle_app/lib_warp/imgui_warp.h>
+
+#include <main/maya_plug_fwd.h>
+#include <maya_plug/data/maya_file_io.h>
+#include <maya_plug/data/reference_file.h>
+#include <maya_plug/data/sim_cover_attr.h>
+
 #include <maya/MDagPath.h>
 #include <maya/MFileIO.h>
 #include <maya/MItDependencyNodes.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MNamespace.h>
+#include <maya/MTime.h>
 #include <maya/MUuid.h>
 #include <maya/adskDataAssociations.h>
 #include <maya/adskDataStream.h>
 #include <maya/adskDebugPrint.h>
-#include <maya/MNamespace.h>
-#include <maya/MItSelectionList.h>
-
-#include <maya_plug/data/maya_file_io.h>
-#include <maya_plug/data/reference_file.h>
-#include <main/maya_plug_fwd.h>
-#include <maya_plug/data/sim_cover_attr.h>
 
 namespace doodle::maya_plug {
 
 namespace reference_attr {
 
-bool data::operator==(const data& in_rhs) const {
-  return path == in_rhs.path;
-}
-bool data::operator!=(const data& in_rhs) const {
-  return !(in_rhs == *this);
-}
+bool data::operator==(const data& in_rhs) const { return path == in_rhs.path; }
+bool data::operator!=(const data& in_rhs) const { return !(in_rhs == *this); }
 }  // namespace reference_attr
 
 class reference_attr_setting::impl {
@@ -50,7 +46,8 @@ class reference_attr_setting::impl {
   ::doodle::gui::gui_cache<std::float_t> length_scale{"length scale"s, 1.0f};
   ::doodle::gui::gui_cache<std::int32_t> max_cg_iteration{"max CGIteration"s, 1000};
   ::doodle::gui::gui_cache<std::int32_t> cg_accuracy{"cg accuracy"s, 9};
-  ::doodle::gui::gui_cache<std::array<std::float_t, 3>> gravity{"gravity"s, std::array<std::float_t, 3>{0.0f, -980.0f, 0.0f}};
+  ::doodle::gui::gui_cache<std::array<std::float_t, 3>> gravity{
+      "gravity"s, std::array<std::float_t, 3>{0.0f, -980.0f, 0.0f}};
 
   void refresh_gui_value(const sim_cover_attr& in_value) {
     simple_subsampling = in_value.simple_subsampling;
@@ -62,10 +59,9 @@ class reference_attr_setting::impl {
   }
 };
 
-reference_attr_setting::reference_attr_setting()
-    : p_i(std::make_unique<impl>()) {
-  p_i->title_name_     = std::string{name};
-  auto k_ref_view = g_reg()->view<reference_file>();
+reference_attr_setting::reference_attr_setting() : p_i(std::make_unique<impl>()) {
+  p_i->title_name_ = std::string{name};
+  auto k_ref_view  = g_reg()->view<reference_file>();
   std::transform(k_ref_view.begin(), k_ref_view.end(), std::back_inserter(p_i->p_handles), [](auto& in_e) {
     return make_handle(in_e);
   });
@@ -108,15 +104,13 @@ bool reference_attr_setting::get_file_info() {
   }
 
   auto k_j_str = maya_file_io::get_channel_date();
-  if (k_j_str.empty())
-    return true;
+  if (k_j_str.empty()) return true;
   auto k_j = nlohmann::json::parse(k_j_str);
 
   for (auto& l_i : p_i->p_handles) {
     auto& k_ref = l_i.get<reference_file>();
     auto l_p    = k_ref.path;
-    if (k_j.contains(l_p))
-      entt_tool::load_comm<reference_file, sim_cover_attr>(l_i, k_j.at(l_p));
+    if (k_j.contains(l_p)) entt_tool::load_comm<reference_file, sim_cover_attr>(l_i, k_j.at(l_p));
     l_i.get<reference_file>().init_show_name();
   }
   return true;
@@ -151,8 +145,7 @@ void reference_attr_setting::render() {
       }
       l_node&& [&]() {
         imgui::Checkbox("解算", &(k_ref.use_sim));
-        if (!k_ref.use_sim)
-          return;
+        if (!k_ref.use_sim) return;
 
         if (imgui::Button("添加碰撞")) {
           k_s = MGlobal::getActiveSelectionList(l_select);
@@ -165,6 +158,9 @@ void reference_attr_setting::render() {
         }
 
         if (imgui::Button("设置布料风场")) {
+          k_s = MGlobal::getActiveSelectionList(l_select);
+          DOODLE_MAYA_CHICK(k_s);
+          k_ref.add_field_dag(l_select);
         }
 
         dear::Text("解算碰撞: "s);
@@ -181,32 +177,25 @@ void reference_attr_setting::render() {
           p_i->refresh_gui_value(p_i->p_current_select.get<sim_cover_attr>());
         }
         if (ImGui::Checkbox(*p_i->simple_subsampling, &p_i->simple_subsampling)) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .simple_subsampling = p_i->simple_subsampling;
+          p_i->p_current_select.get<sim_cover_attr>().simple_subsampling = p_i->simple_subsampling;
         }
         if (ImGui::InputInt(*p_i->frame_samples, &p_i->frame_samples)) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .frame_samples = p_i->frame_samples;
+          p_i->p_current_select.get<sim_cover_attr>().frame_samples = p_i->frame_samples;
         }
         if (ImGui::InputFloat(*p_i->time_scale, &p_i->time_scale)) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .time_scale = p_i->time_scale;
+          p_i->p_current_select.get<sim_cover_attr>().time_scale = p_i->time_scale;
         }
         if (ImGui::InputFloat(*p_i->length_scale, &p_i->length_scale)) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .length_scale = p_i->length_scale;
+          p_i->p_current_select.get<sim_cover_attr>().length_scale = p_i->length_scale;
         }
         if (ImGui::InputInt(*p_i->max_cg_iteration, &p_i->max_cg_iteration)) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .max_cg_iteration = p_i->max_cg_iteration;
+          p_i->p_current_select.get<sim_cover_attr>().max_cg_iteration = p_i->max_cg_iteration;
         }
         if (ImGui::InputInt(*p_i->cg_accuracy, &p_i->cg_accuracy)) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .cg_accuracy = p_i->cg_accuracy;
+          p_i->p_current_select.get<sim_cover_attr>().cg_accuracy = p_i->cg_accuracy;
         }
         if (ImGui::InputFloat3(*p_i->gravity, p_i->gravity.data.data())) {
-          p_i->p_current_select.get<sim_cover_attr>()
-              .gravity = p_i->gravity;
+          p_i->p_current_select.get<sim_cover_attr>().gravity = p_i->gravity;
         }
       } else {
         if (ImGui::Button("添加配置")) {
@@ -217,14 +206,8 @@ void reference_attr_setting::render() {
   };
 }
 
-void reference_attr_setting::clear() {
-  destroy_handle(p_i->p_handles);
-}
-reference_attr_setting::~reference_attr_setting() {
-  clear();
-}
-const std::string& reference_attr_setting::title() const {
-  return p_i->title_name_;
-}
+void reference_attr_setting::clear() { destroy_handle(p_i->p_handles); }
+reference_attr_setting::~reference_attr_setting() { clear(); }
+const std::string& reference_attr_setting::title() const { return p_i->title_name_; }
 
 }  // namespace doodle::maya_plug
