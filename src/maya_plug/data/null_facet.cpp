6daa84b8718c2ffa30ca149c92406ba5d7d14b47
zhang <957714080@qq.com>
2022-10-20 10:18:03 +0800

    调整空构面上下文

--- src/maya_plug/data/null_facet.cpp
+++ src/maya_plug/data/null_facet.cpp
@@ -4,17 +4,18 @@
 
 #include "null_facet.h"
 
+#include <maya_plug/data/maya_create_movie.h>
+
 namespace doodle {
 namespace maya_plug {
 const std::string& null_facet::name() const noexcept {
   static std::string l_i{"null_facet"};
   return l_i;
 }
-void null_facet::operator()() {
-  work_lock.emplace(boost::asio::make_work_guard(g_io_context()));
-}
-void null_facet::deconstruction() {
-  work_lock.reset();
+void null_facet::operator()() { work_lock.emplace(boost::asio::make_work_guard(g_io_context())); }
+void null_facet::deconstruction() { work_lock.reset(); }
+null_facet::null_facet() : doodle::detail::app_facet_interface() {
+  g_reg()->ctx().at<image_to_move>() = std::make_shared<detail::maya_create_movie>();
 }
 }  // namespace maya_plug
 }  // namespace doodle
