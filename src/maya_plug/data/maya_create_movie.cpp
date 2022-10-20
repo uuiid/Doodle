9eec2c18e71717d32f93ae4b829f8c150cde5455
zhang <957714080@qq.com>
2022-10-20 10:17:19 +0800

    添加析构函数

--- src/maya_plug/data/maya_create_movie.cpp
+++ src/maya_plug/data/maya_create_movie.cpp
@@ -17,10 +17,10 @@ class maya_create_movie::impl {
 
 maya_create_movie::maya_create_movie() : ptr(std::make_unique<impl>()) {}
 
-void maya_create_movie::create_move(const FSys::path& in_out_path, process_message& in_msg, const std::vector<image_attr>& in_vector) {
-  ptr->doodle_exe_attr.create_move(
-      in_out_path, in_vector, in_msg
-  );
+void maya_create_movie::create_move(
+    const FSys::path& in_out_path, process_message& in_msg, const std::vector<image_attr>& in_vector
+) {
+  ptr->doodle_exe_attr.create_move(in_out_path, in_vector, in_msg);
 }
 FSys::path maya_create_movie::create_out_path(const entt::handle& in_handle) {
   boost::ignore_unused(this);
@@ -31,21 +31,18 @@ FSys::path maya_create_movie::create_out_path(const entt::handle& in_handle) {
   /// \brief 这里我们检查 shot，episode 进行路径的组合
   if (!l_out.has_extension() && in_handle.any_of<episodes, shot>())
     l_out /= fmt::format(
-        "{}_{}.mp4",
-        in_handle.any_of<episodes>() ? fmt::to_string(in_handle.get<episodes>()) : "eps_none"s,
+        "{}_{}.mp4", in_handle.any_of<episodes>() ? fmt::to_string(in_handle.get<episodes>()) : "eps_none"s,
         in_handle.any_of<shot>() ? fmt::to_string(in_handle.get<shot>()) : "sh_none"s
     );
   else if (!l_out.has_extension()) {
-    l_out /= fmt::format(
-        "{}.mp4", core_set::get_set().get_uuid()
-    );
+    l_out /= fmt::format("{}.mp4", core_set::get_set().get_uuid());
   } else
     l_out.extension() == ".mp4" ? void() : throw_exception(doodle_error{"扩展名称不是MP4"});
 
-  if (exists(l_out.parent_path()))
-    create_directories(l_out.parent_path());
+  if (exists(l_out.parent_path())) create_directories(l_out.parent_path());
   return l_out;
 }
+maya_create_movie::~maya_create_movie() = default;
 }  // namespace detail
 }  // namespace maya_plug
 }  // namespace doodle
