8d6d8b14aa88cec0e0a750c6ed5df7c1fdc438bc
zhang <957714080@qq.com>
2022-10-21 09:50:23 +0800

    添加新的规则生成

---
+++ src/doodle_lib/attendance/attendance_rule.cpp
@@ -0,0 +1,9 @@
+//
+// Created by TD on 2022/10/21.
+//
+
+#include "attendance_rule.h"
+
+namespace doodle {
+namespace business {}  // namespace business
+}  // namespace doodle
---
+++ src/doodle_lib/attendance/attendance_rule.h
@@ -0,0 +1,11 @@
+//
+// Created by TD on 2022/10/21.
+//
+
+namespace doodle {
+namespace business {
+
+class attendance_rule {};
+
+}  // namespace business
+}  // namespace doodle
