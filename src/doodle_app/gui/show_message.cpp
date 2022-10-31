c6ee52b044a48e582822e5bf49f8ad313c7d555b
Corgi1018 <yujielee1018@163.com>
2022-10-31 10:42:06 +0800

    显示消息

--- src/doodle_app/gui/show_message.cpp
+++ src/doodle_app/gui/show_message.cpp
@@ -4,6 +4,36 @@
 
 #include "show_message.h"
 
-namespace doodle {
-namespace gui {}  // namespace gui
-}  // namespace doodle
+#include <doodle_app/gui/base/ref_base.h>
+
+#include <lib_warp/imgui_warp.h>
+namespace doodle::gui {
+class show_message::impl {
+ public:
+  std::string title{"消息"};
+  std::string message{};
+  gui::gui_cache_name_id button_{"确认"};
+};
+
+std::int32_t show_message::flags() const {
+  boost::ignore_unused(this);
+  return ImGuiWindowFlags_NoSavedSettings;
+}
+void show_message::set_message(const std::string& in_msg) { p_i->message = in_msg; }
+std::string show_message::get_message() { return p_i->message; }
+
+void show_message::set_attr() const {
+  ImGui::OpenPopup(title().data());
+  ImGui::SetNextWindowSize({640, 360});
+}
+
+void show_message::render() {
+  ImGui::Text(p_i->message.c_str());
+
+  if (ImGui::Button(*p_i->button_)) {
+    ImGui::CloseCurrentPopup();
+    show_attr = false;
+  }
+}
+
+}  // namespace doodle::gui
--- src/doodle_app/gui/show_message.h
+++ src/doodle_app/gui/show_message.h
@@ -10,7 +10,21 @@
 namespace doodle {
 namespace gui {
 
-class show_message : public base_windows<dear::PopupModal, show_message> {};
+class show_message : public base_windows<dear::PopupModal, show_message> {
+  class impl;
+  std::unique_ptr<impl> p_i;
+  std::string Message;
+
+ public:
+  explicit show_message();
+  virtual ~show_message() override;
+  void set_attr() const;
+  std::int32_t flags() const;
+  void render();
+
+  void set_message(const std::string& in_msg);  /// 设置消息内容
+  std::string get_message();
+};
 
 }  // namespace gui
 }  // namespace doodle
