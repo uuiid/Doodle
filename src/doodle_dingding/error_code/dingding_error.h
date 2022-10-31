9f2fa88fb89a79d2f52305fe9620b236b7414117
zhang <957714080@qq.com>
2022-10-31 09:35:57 +0800

    调整错误保存和格式化

--- src/doodle_dingding/error_code/dingding_error.h
+++ src/doodle_dingding/error_code/dingding_error.h
@@ -5,10 +5,15 @@
 #pragma once
 
 #include <boost/system.hpp>
+
+#include <map>
 namespace doodle {
 namespace dingding {
 namespace bsys = boost::system;
 class dingding_category : public bsys::error_category {
+ private:
+  std::map<std::int32_t, std::string> map_err{};
+
  public:
   const char* name() const noexcept;
 
@@ -16,9 +21,12 @@ class dingding_category : public bsys::error_category {
 
   bool failed(int ev) const noexcept;
 
+  void set_message(const int evcode, const std::string& in_string);
+
   bsys::error_condition default_error_condition(int ev) const noexcept;
 
   static const bsys::error_category& get();
+  static dingding_category& get_();
 };
 
 }  // namespace dingding
