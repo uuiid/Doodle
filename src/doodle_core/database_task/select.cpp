6192e63aa344970ba6c5ba3af870999d4087521d
zhang <957714080@qq.com>
2022-11-01 10:40:41 +0800

    调整错误实体删除

--- src/doodle_core/database_task/select.cpp
+++ src/doodle_core/database_task/select.cpp
@@ -3,37 +3,34 @@
 //
 
 #include "select.h"
-#include <doodle_core/thread_pool/process_message.h>
-#include <doodle_core/core/doodle_lib.h>
-#include <doodle_core/logger/logger.h>
-#include <doodle_core/core/core_sql.h>
 
-#include <doodle_core/metadata/metadata_cpp.h>
+#include <doodle_core/core/core_sql.h>
+#include <doodle_core/core/doodle_lib.h>
+#include <doodle_core/database_task/details/update_ctx.h>
+#include <doodle_core/database_task/sql_file.h>
+#include <doodle_core/generate/core/metadatatab_sql.h>
+#include <doodle_core/generate/core/sql_sql.h>
+#include <doodle_core/lib_warp/entt_warp.h>
+#include <doodle_core/lib_warp/enum_template_tool.h>
+#include <doodle_core/logger/logger.h>
+#include <doodle_core/metadata/detail/time_point_info.h>
 #include <doodle_core/metadata/image_icon.h>
 #include <doodle_core/metadata/importance.h>
+#include <doodle_core/metadata/metadata_cpp.h>
 #include <doodle_core/metadata/organization.h>
 #include <doodle_core/metadata/redirection_path_info.h>
-#include <doodle_core/metadata/user.h>
 #include <doodle_core/metadata/rules.h>
-#include <doodle_core/metadata/detail/time_point_info.h>
-
-#include <doodle_core/lib_warp/entt_warp.h>
-#include <doodle_core/database_task/sql_file.h>
-#include <doodle_core/database_task/details/update_ctx.h>
-
-#include <doodle_core/generate/core/sql_sql.h>
-#include <doodle_core/generate/core/metadatatab_sql.h>
-#include <doodle_core/lib_warp/enum_template_tool.h>
-
-#include <sqlpp11/sqlpp11.h>
-#include <sqlpp11/sqlite3/sqlite3.h>
-#include <sqlpp11/ppgen.h>
-
-#include <range/v3/range.hpp>
-#include <range/v3/range_for.hpp>
-#include <range/v3/all.hpp>
+#include <doodle_core/metadata/user.h>
+#include <doodle_core/thread_pool/process_message.h>
 
 #include <boost/asio.hpp>
+
+#include <range/v3/all.hpp>
+#include <range/v3/range.hpp>
+#include <range/v3/range_for.hpp>
+#include <sqlpp11/ppgen.h>
+#include <sqlpp11/sqlite3/sqlite3.h>
+#include <sqlpp11/sqlpp11.h>
 
 namespace doodle::database_n {
 namespace sql = doodle_database;
@@ -76,9 +73,15 @@ struct future_data {
       }
       l_entt_set.emplace(l_entt);
     }
-    if (!l_not_valid_entity.empty())
+    if (!l_not_valid_entity.empty()) {
       DOODLE_LOG_WARN("{} 无效的实体: {} 重复的实体 {}", typeid(T).name(), l_not_valid_entity, l_duplicate_entity);
-    DOODLE_CHICK(ranges::all_of(l_entt_list, [&](const entt::entity& in) { return in_reg->valid(in); }), doodle_error{"无效实体"});
+      for (auto&& d : l_not_valid_entity) {
+        doodle::database::delete_(entt::handle{*in_reg, d});
+      }
+    }
+    DOODLE_CHICK(
+        ranges::all_of(l_entt_list, [&](const entt::entity& in) { return in_reg->valid(in); }), doodle_error{"无效实体"}
+    );
     in_reg->remove<T>(l_entt_list.begin(), l_entt_list.end());
     in_reg->insert<T>(l_entt_list.begin(), l_entt_list.end(), l_data_list.begin());
   };
@@ -119,55 +122,42 @@ class select::impl {
         break;
       }
 
-      for (auto&& row : in_conn(sqlpp::select(sqlpp::all_of(l_metadatatab))
-                                    .from(l_metadatatab)
-                                    .unconditionally())) {
+      for (auto&& row : in_conn(sqlpp::select(sqlpp::all_of(l_metadatatab)).from(l_metadatatab).unconditionally())) {
         auto l_e = num_to_enum<entt::entity>(row.id.value());
         if (!in_reg.valid(l_e)) l_e = in_reg.create(l_e);
 
-        auto l_fun =
-            boost::asio::post(
-                strand_,
-                std::packaged_task<void()>{
-                    [l_e,
-                     in_str  = row.userData.value(),
-                     in_uuid = row.uuidData.value(),
-                     &in_reg,
-                     l_size,
-                     this]() {
-                      if (stop)
-                        return;
-                      entt::handle l_h{in_reg, l_e};
-                      l_h.emplace<database>(in_uuid).set_id(0);
-                      auto k_json = nlohmann::json::parse(in_str);
-                      entt_tool::load_comm<doodle::project, doodle::episodes, doodle::shot, doodle::season, doodle::assets, doodle::assets_file, doodle::time_point_wrap, doodle::comment, doodle::project_config::base_config, doodle::image_icon, doodle::importance, doodle::organization_list, doodle::redirection_path_info>(l_h, k_json);
-                      database::save(l_h);
-                      process_message_->message("开始旧版本兼容转换"s);
-                      process_message_->progress_step({1, l_size});
-                    }}
-            );
+        auto l_fun = boost::asio::post(
+            strand_, std::packaged_task<void()>{[l_e, in_str = row.userData.value(), in_uuid = row.uuidData.value(),
+                                                 &in_reg, l_size, this]() {
+              if (stop) return;
+              entt::handle l_h{in_reg, l_e};
+              l_h.emplace<database>(in_uuid).set_id(0);
+              auto k_json = nlohmann::json::parse(in_str);
+              entt_tool::load_comm<
+                  doodle::project, doodle::episodes, doodle::shot, doodle::season, doodle::assets, doodle::assets_file,
+                  doodle::time_point_wrap, doodle::comment, doodle::project_config::base_config, doodle::image_icon,
+                  doodle::importance, doodle::organization_list, doodle::redirection_path_info>(l_h, k_json);
+              database::save(l_h);
+              process_message_->message("开始旧版本兼容转换"s);
+              process_message_->progress_step({1, l_size});
+            }}
+        );
         results.emplace_back(l_fun.share());
-        if (stop)
-          return;
+        if (stop) return;
       }
-      auto l_fun =
-          boost::asio::post(
-              strand_,
-              std::packaged_task<void()>{
-                  [this]() {
-                    auto l_view = local_reg->view<doodle::project>();
-                    if (!l_view.empty()) {
-                      auto l_h                               = entt::handle{*local_reg, l_view.front()};
-                      local_reg->ctx().at<doodle::project>() = l_h.get<doodle::project>();
-                      local_reg->ctx().at<doodle::project_config::base_config>() =
-                          l_h.any_of<doodle::project_config::base_config>()
-                              ? l_h.get<doodle::project_config::base_config>()
-                              : doodle::project_config::base_config{};
-                    }
-                    process_message_->message("完成旧版本兼容转换"s);
-                    process_message_->progress_clear();
-                  }}
-          );
+      auto l_fun = boost::asio::post(strand_, std::packaged_task<void()>{[this]() {
+                                       auto l_view = local_reg->view<doodle::project>();
+                                       if (!l_view.empty()) {
+                                         auto l_h = entt::handle{*local_reg, l_view.front()};
+                                         local_reg->ctx().at<doodle::project>() = l_h.get<doodle::project>();
+                                         local_reg->ctx().at<doodle::project_config::base_config>() =
+                                             l_h.any_of<doodle::project_config::base_config>()
+                                                 ? l_h.get<doodle::project_config::base_config>()
+                                                 : doodle::project_config::base_config{};
+                                       }
+                                       process_message_->message("完成旧版本兼容转换"s);
+                                       process_message_->progress_clear();
+                                     }});
       results.emplace_back(l_fun.share());
     }
   }
@@ -186,39 +176,24 @@ class select::impl {
 
     auto l_future_data = std::make_shared<future_data<Type>>();
 
-    for (auto&& row : in_conn(
-             sqlpp::select(
-                 l_com_entity.entityId,
-                 l_com_entity.jsonData
-             )
-                 .from(l_com_entity)
-                 .where(
-                     l_com_entity.comHash == entt::type_id<Type>().hash()
-                 )
-         )) {
-      if (stop)
-        return;
+    for (auto&& row : in_conn(sqlpp::select(l_com_entity.entityId, l_com_entity.jsonData)
+                                  .from(l_com_entity)
+                                  .where(l_com_entity.comHash == entt::type_id<Type>().hash()))) {
+      if (stop) return;
       auto l_id  = row.entityId.value();
       auto l_fut = boost::asio::post(
-          l_s,
-          std::packaged_task<Type()>{
-              [in_json = row.jsonData.value(),
-               in_id   = l_id,
-               l_size,
-               this]() {
-                auto l_json = nlohmann::json::parse(in_json);
-                process_message_->progress_step({1, l_size * 2});
-                return l_json.get<Type>();
-              }}
+          l_s, std::packaged_task<Type()>{[in_json = row.jsonData.value(), in_id = l_id, l_size, this]() {
+            auto l_json = nlohmann::json::parse(in_json);
+            process_message_->progress_step({1, l_size * 2});
+            return l_json.get<Type>();
+          }}
       );
 
       l_future_data->data.emplace_back(std::make_tuple(boost::numeric_cast<std::int64_t>(l_id), std::move(l_fut)));
     }
-    list_install.emplace_back(
-        [l_future_data = std::move(l_future_data), this](const registry_ptr& in) mutable {
-          return l_future_data->install_reg(in, id_map);
-        }
-    );
+    list_install.emplace_back([l_future_data = std::move(l_future_data), this](const registry_ptr& in) mutable {
+      return l_future_data->install_reg(in, id_map);
+    });
   }
   template <typename... Type>
   void select_com(entt::registry& in_reg, sqlpp::sqlite3::connection& in_conn) {
@@ -235,46 +210,35 @@ class select::impl {
     }
     auto l_future_data = std::make_shared<future_data<database>>();
 
-    for (auto& row : in_conn(sqlpp::select(sqlpp::all_of(l_entity))
-                                 .from(l_entity)
-                                 .unconditionally())) {
-      if (stop)
-        return;
+    for (auto& row : in_conn(sqlpp::select(sqlpp::all_of(l_entity)).from(l_entity).unconditionally())) {
+      if (stop) return;
       auto l_e = num_to_enum<entt::entity>(row.id.value());
       create_entt.push_back(l_e);
 
       auto l_fut = boost::asio::post(
           strand_,
-          std::packaged_task<database()>{
-              [in_json = row.uuidData.value(),
-               in_id   = row.id,
-               l_size,
-               this]() -> database {
-                database l_database{in_json};
-                l_database.set_id(in_id);
-                process_message_->progress_step({1, l_size * 2});
-                return l_database;
-              }}
+          std::packaged_task<database()>{[in_json = row.uuidData.value(), in_id = row.id, l_size, this]() -> database {
+            database l_database{in_json};
+            l_database.set_id(in_id);
+            process_message_->progress_step({1, l_size * 2});
+            return l_database;
+          }}
       );
-      l_future_data->data.emplace_back(std::make_tuple(boost::numeric_cast<std::int64_t>(enum_to_num(l_e)), std::move(l_fut)));
+      l_future_data->data.emplace_back(
+          std::make_tuple(boost::numeric_cast<std::int64_t>(enum_to_num(l_e)), std::move(l_fut))
+      );
     }
 
-    list_install.emplace_back(
-        [l_future_data = std::move(l_future_data), this](const registry_ptr& in) mutable {
-          return l_future_data->install_reg(in, id_map);
-        }
-    );
+    list_install.emplace_back([l_future_data = std::move(l_future_data), this](const registry_ptr& in) mutable {
+      return l_future_data->install_reg(in, id_map);
+    });
   }
 };
 
 select::select() : p_i(std::make_unique<impl>()) {}
 select::~select() = default;
 
-void select::operator()(
-    entt::registry& in_registry,
-    const FSys::path& in_project_path,
-    conn_ptr& in_connect
-) {
+void select::operator()(entt::registry& in_registry, const FSys::path& in_project_path, conn_ptr& in_connect) {
   p_i->process_message_ = g_reg()->ctx().find<process_message>();
   p_i->only_ctx         = false;
   p_i->project          = in_project_path;
@@ -282,13 +246,10 @@ void select::operator()(
   this->p_i->select_old(*p_i->local_reg, *in_connect);
 
   /// \brief 等待旧的任务完成
-  ranges::for_each(p_i->results, [](const decltype(p_i->results)::value_type& in_) {
-    in_.get();
-  });
+  ranges::for_each(p_i->results, [](const decltype(p_i->results)::value_type& in_) { in_.get(); });
   p_i->results.clear();
 
-  if (auto [l_v, l_i] = doodle::database_n::details::get_version(*in_connect);
-      l_v >= 3 && l_i > 4) {
+  if (auto [l_v, l_i] = doodle::database_n::details::get_version(*in_connect); l_v >= 3 && l_i > 4) {
     if (!p_i->only_ctx) {
       /// \brief 选中实体
       p_i->select_entt(*p_i->local_reg, *in_connect);
