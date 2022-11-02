98242b49377cdf3a9378fe14732ee23bf0534974
zhang <957714080@qq.com>
2022-11-02 16:57:45 +0800

    将maya 插件初始化窗口设为关闭

--- src/maya_plug/gui/maya_plug_app.cpp
+++ src/maya_plug/gui/maya_plug_app.cpp
@@ -4,11 +4,14 @@
 
 #include "maya_plug_app.h"
 
+#include "doodle_core/doodle_core_fwd.h"
 #include <doodle_core/core/core_set.h>
 
 #include <doodle_app/gui/main_menu_bar.h>
 #include <doodle_app/gui/main_proc_handle.h>
 #include <doodle_app/gui/main_status_bar.h>
+
+#include <boost/asio/post.hpp>
 
 #include <maya_plug/data/maya_create_movie.h>
 #include <maya_plug/data/null_facet.h>
@@ -23,22 +26,15 @@ void maya_facet::load_windows() {
   make_handle().emplace<gui::gui_tick>(std::make_shared<maya_layout>());
   make_handle().emplace<gui::gui_tick>(std::make_shared<maya_menu>());
   make_handle().emplace<gui::gui_tick>(std::make_shared<gui::main_status_bar>());
+  boost::asio::post(g_io_context(), [this]() { close_windows(); });
 }
-void maya_facet::close_windows() {
-  ::ShowWindow(p_hwnd, SW_HIDE);
-}
+void maya_facet::close_windows() { ::ShowWindow(p_hwnd, SW_HIDE); }
 maya_facet::maya_facet() : doodle::facet::gui_facet() {
   g_reg()->ctx().at<image_to_move>() = std::make_shared<detail::maya_create_movie>();
 }
 
-maya_plug_app::maya_plug_app() {
-  set_facet();
-}
-maya_plug_app::maya_plug_app(
-    const doodle_main_app::in_gui_arg& inArg
-) : doodle_main_app(inArg) {
-  set_facet();
-}
+maya_plug_app::maya_plug_app() { set_facet(); }
+maya_plug_app::maya_plug_app(const doodle_main_app::in_gui_arg& inArg) : doodle_main_app(inArg) { set_facet(); }
 void maya_plug_app::set_facet() {
   switch (MGlobal::mayaState()) {
     case MGlobal::MMayaState::kBaseUIMode:
