e30c2fb3e3a602bc631c0e52a6414fbfcc702406
zhang <957714080@qq.com>
2022-11-03 10:49:31 +0800

    添加新类

--- src/doodle_core/core/util.h
+++ src/doodle_core/core/util.h
@@ -5,6 +5,8 @@
 #pragma once
 #include <doodle_app/doodle_app_fwd.h>
 
+#include <atomic>
+
 namespace doodle::core {
 /**
  * @brief 标识符生成器， 线程安全
@@ -20,4 +22,18 @@ class DOODLE_CORE_API identifier {
   static identifier& get();
   std::uint64_t id();
 };
-}  // namespace doodle
+
+/**
+ * @brief 这是一个bool锁定类
+ *
+ */
+class bool_mutex {
+ public:
+  std::atomic_bool data{};
+
+  inline void lock() { data = true; }
+  inline void unlock() { data = false; }
+  [[nodiscard]] inline explicit operator bool() const { return data; }
+};
+
+}  // namespace doodle::core
