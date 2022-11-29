7f7595f8e5057cef45d1f5aaba7efc2f2651e0b5
zhang <957714080@qq.com>
2022-11-29 17:01:45 +0800

    添加pac测试

---
+++ src/test/math/pca.cpp
@@ -0,0 +1,13 @@
+//
+// Created by TD on 2022/9/23.
+//
+
+#include <doodle_core/doodle_core.h>
+
+#include <boost/test/unit_test.hpp>
+
+#include <crtdbg.h>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <main_fixtures/lib_fixtures.h>
