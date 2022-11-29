4c98a10db5f6bf0a7f1e9d43efa48ae968160101
zhang <957714080@qq.com>
2022-11-29 15:21:55 +0800

    去除无用的函数

--- src/maya_plug/maya_comm/sequence_to_blend_shape_comm.cpp
+++ src/maya_plug/maya_comm/sequence_to_blend_shape_comm.cpp
@@ -6,22 +6,22 @@
 
 #include <maya_plug/data/sequence_to_blend_shape.h>
 
-#include <maya/MArgDatabase.h>
-#include <maya/MSelectionList.h>
 #include <maya/MAnimControl.h>
-#include <maya/MItMeshVertex.h>
-#include <maya/MFnMesh.h>
-#include <maya/MDagModifier.h>
-#include <maya/MFnIkJoint.h>
-#include <maya/MItDependencyGraph.h>
-#include <maya/MEulerRotation.h>
-#include <maya/MQuaternion.h>
-#include <maya/MDagPath.h>
-#include <maya/MNamespace.h>
-#include <maya/MMatrix.h>
+#include <maya/MArgDatabase.h>
 #include <maya/MBoundingBox.h>
+#include <maya/MDagModifier.h>
+#include <maya/MDagPath.h>
 #include <maya/MDagPathArray.h>
 #include <maya/MDataHandle.h>
+#include <maya/MEulerRotation.h>
+#include <maya/MFnIkJoint.h>
+#include <maya/MFnMesh.h>
+#include <maya/MItDependencyGraph.h>
+#include <maya/MItMeshVertex.h>
+#include <maya/MMatrix.h>
+#include <maya/MNamespace.h>
+#include <maya/MQuaternion.h>
+#include <maya/MSelectionList.h>
 
 namespace doodle::maya_plug {
 
@@ -70,9 +70,7 @@ class sequence_to_blend_shape_comm::impl {
   std::vector<sequence_to_blend_shape> blend_list{};
 };
 
-sequence_to_blend_shape_comm::sequence_to_blend_shape_comm()
-    : p_i(std::make_unique<impl>()) {
-}
+sequence_to_blend_shape_comm::sequence_to_blend_shape_comm() : p_i(std::make_unique<impl>()) {}
 void sequence_to_blend_shape_comm::get_arg(const MArgList& in_arg) {
   MStatus k_s;
   MArgDatabase k_prase{syntax(), in_arg};
@@ -115,9 +113,7 @@ void sequence_to_blend_shape_comm::get_arg(const MArgList& in_arg) {
   p_i->select_list.length() > 0 ? void() : throw_exception(doodle_error{"未获得选中物体"});
 
   /// \brief 生成绑定物体path
-  for (auto i = 0;
-       i < p_i->select_list.length();
-       ++i) {
+  for (auto i = 0; i < p_i->select_list.length(); ++i) {
     sequence_to_blend_shape l_blend_shape{};
     MDagPath l_path{};
     p_i->select_list.getDagPath(i, l_path);
@@ -148,7 +144,8 @@ void sequence_to_blend_shape_comm::get_arg(const MArgList& in_arg) {
       auto l_namespace = MNamespace::getNamespaceFromName(d_str{get_node_name(ctx.select_attr())}, &k_s);
       MSelectionList l_selection_list{};
       try {
-        auto l_select_str = fmt::format("{}:*{}", l_namespace, g_reg()->ctx().at<project_config::base_config>().export_group);
+        auto l_select_str =
+            fmt::format("{}:*{}", l_namespace, g_reg()->ctx().at<project_config::base_config>().export_group);
         DOODLE_LOG_INFO("选择 {}", l_select_str);
         k_s = l_selection_list.add(d_str{l_select_str}, true);
         DOODLE_MAYA_CHICK(k_s);
@@ -188,9 +185,7 @@ MStatus sequence_to_blend_shape_comm::redoIt() {
   }
   return MStatus::kSuccess;
 }
-bool sequence_to_blend_shape_comm::isUndoable() const {
-  return true;
-}
+bool sequence_to_blend_shape_comm::isUndoable() const { return true; }
 void sequence_to_blend_shape_comm::create_mesh() {
   MStatus l_s{};
 
@@ -203,9 +198,7 @@ void sequence_to_blend_shape_comm::create_mesh() {
     }
   }
 
-  for (auto i = p_i->startFrame_p;
-       i <= p_i->endFrame_p;
-       ++i) {
+  for (auto i = p_i->startFrame_p; i <= p_i->endFrame_p; ++i) {
     l_s = MAnimControl::setCurrentTime(MTime{boost::numeric_cast<std::double_t>(i), MTime::uiUnit()});
     DOODLE_MAYA_CHICK(l_s);
     for (auto&& ctx : p_i->blend_list) {
@@ -228,90 +221,6 @@ void sequence_to_blend_shape_comm::create_anim() {
   }
 }
 
-void sequence_to_blend_shape_comm::center_pivot(MDagPath& in_path) {
-  //  DOODLE_LOG_INFO("开始居中轴");
-  MStatus l_s{};
-  auto l_tran_path = get_dag_path(in_path.transform());
-  MFnTransform l_fn_transform{std::as_const(l_tran_path), &l_s};
-  DOODLE_MAYA_CHICK(l_s);
-  //  DOODLE_LOG_INFO("获取选中物体 {}", get_node_full_name(l_tran_path.node()));
-
-  l_s = in_path.extendToShape();
-  DOODLE_MAYA_CHICK(l_s);
-  //  DOODLE_LOG_INFO("获取网格体 {}", get_node_full_name(in_path.node()));
-
-  /// \brief 冻结座标轴
-  /// 获取变换
-  auto l_mat = l_fn_transform.transformationMatrix(&l_s);
-  /// \brief 获取变换
-  //  const auto l_tran = l_fn_transform.getTranslation(MSpace::kWorld, &l_s);
-
-  /// \brief 清除变换
-  //  l_s = l_fn_transform.setTranslation({}, MSpace::kWorld);
-  l_s        = l_fn_transform.resetFromRestPosition();
-  DOODLE_MAYA_CHICK(l_s);
-  /// \brief 变换网格体
-  for (MItMeshVertex l_it_mesh_vertex{std::as_const(in_path), MObject::kNullObj, &l_s};
-       l_s && !l_it_mesh_vertex.isDone();
-       l_it_mesh_vertex.next()) {
-    auto l_point = l_it_mesh_vertex.position(MSpace::kWorld, &l_s);
-    DOODLE_MAYA_CHICK(l_s);
-    l_point = l_point * l_mat;
-    l_s     = l_it_mesh_vertex.setPosition(l_point);
-    //    l_s     = l_it_mesh_vertex.translateBy(l_tran, MSpace::kWorld);
-    DOODLE_MAYA_CHICK(l_s);
-  }
-  DOODLE_MAYA_CHICK(l_s);
-
-  /// \brief 居中座标轴
-  MFnMesh l_mesh{std::as_const(in_path), &l_s};
-  DOODLE_MAYA_CHICK(l_s);
-  auto l_box = l_mesh.boundingBox(&l_s);
-  DOODLE_MAYA_CHICK(l_s);
-
-  auto l_center = l_box.center();
-  //  DOODLE_LOG_INFO("获取中心 {}", l_center);
-  l_s           = l_fn_transform.setScalePivot(l_center, MSpace::kWorld, false);
-  DOODLE_MAYA_CHICK(l_s);
-  l_s = l_fn_transform.setRotatePivot(l_center, MSpace::kWorld, false);
-  DOODLE_MAYA_CHICK(l_s);
-  //  DOODLE_LOG_INFO("完成");
-}
-void sequence_to_blend_shape_comm::center_pivot(MDagPath& in_path, const MMatrix& in_matrix, const MPoint& in_point) {
-  //  DOODLE_LOG_INFO("开始居中轴");
-  MStatus l_s{};
-  auto l_tran_path = get_dag_path(in_path.transform());
-  MFnTransform l_fn_transform{std::as_const(l_tran_path), &l_s};
-  DOODLE_MAYA_CHICK(l_s);
-  //  DOODLE_LOG_INFO("获取选中物体 {}", get_node_full_name(l_tran_path.node()));
-
-  /// \brief 冻结座标轴
-  /// \brief 清除变换
-  l_s = l_fn_transform.resetFromRestPosition();
-  DOODLE_MAYA_CHICK(l_s);
-  /// \brief 变换网格体
-  for (MItMeshVertex l_it_mesh_vertex{std::as_const(in_path), MObject::kNullObj, &l_s};
-       l_s && !l_it_mesh_vertex.isDone();
-       l_it_mesh_vertex.next()) {
-    auto l_point = l_it_mesh_vertex.position(MSpace::kWorld, &l_s);
-    DOODLE_MAYA_CHICK(l_s);
-    l_point = l_point * in_matrix;
-    l_s     = l_it_mesh_vertex.setPosition(l_point);
-    //    l_s     = l_it_mesh_vertex.translateBy(l_tran, MSpace::kWorld);
-    DOODLE_MAYA_CHICK(l_s);
-  }
-  DOODLE_MAYA_CHICK(l_s);
-
-  /// \brief 居中座标轴
-  MFnMesh l_mesh{std::as_const(in_path), &l_s};
-  DOODLE_MAYA_CHICK(l_s);
-
-  l_s = l_fn_transform.setScalePivot(in_point, MSpace::kWorld, false);
-  DOODLE_MAYA_CHICK(l_s);
-  l_s = l_fn_transform.setRotatePivot(in_point, MSpace::kWorld, false);
-  DOODLE_MAYA_CHICK(l_s);
-  //  DOODLE_LOG_INFO("完成");
-}
 void sequence_to_blend_shape_comm::run_blend_shape_comm() {
   for (auto&& ctx : p_i->blend_list) {
     ctx.create_blend_shape();
--- src/maya_plug/maya_comm/sequence_to_blend_shape_comm.h
+++ src/maya_plug/maya_comm/sequence_to_blend_shape_comm.h
@@ -12,8 +12,7 @@ MSyntax syntax();
 }  // namespace sequence_to_blend_shape_comm_ns
 
 class sequence_to_blend_shape_comm : public doodle::TemplateAction<
-                                         sequence_to_blend_shape_comm,
-                                         sequence_to_blend_shape_comm_ns::name,
+                                         sequence_to_blend_shape_comm, sequence_to_blend_shape_comm_ns::name,
                                          sequence_to_blend_shape_comm_ns::syntax> {
   class impl;
   std::unique_ptr<impl> p_i;
@@ -24,22 +23,6 @@ class sequence_to_blend_shape_comm : public doodle::TemplateAction<
   void create_anim();
   void run_blend_shape_comm();
   void add_to_parent();
-
-  /**
-   * @brief 将网格居中在mesh中心
-   * @param in_path
-   */
-  static void
-  center_pivot(MDagPath& in_path);
-  /**
-   * @brief 使用传入的矩阵将座标轴重置在传入的点中
-   *    @li 会对做标进行归零处理
-   *    @li 在试图中体现为归零座标轴, 并将座标轴移动至传入点
-   * @param in_path 变换的路径
-   * @param in_matrix 变换网格的矩阵
-   * @param in_point 传入的点
-   */
-  static void center_pivot(MDagPath& in_path, const MMatrix& in_matrix, const MPoint& in_point);
 
  public:
   sequence_to_blend_shape_comm();
