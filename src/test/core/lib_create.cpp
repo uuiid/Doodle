c29c1bc284ea271c3c668262a7f8fdeef1d73918
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-12-06 11:02:09 +0800

    调整测试

--- src/test/core/lib_create.cpp
+++ src/test/core/lib_create.cpp
@@ -2,10 +2,10 @@
 // Created by TD on 2022/8/26.
 //
 
+#include <doodle_core/doodle_core.h>
+#include <doodle_core/pin_yin/convert.h>
 
 #include <boost/test/unit_test.hpp>
-#include <doodle_core/pin_yin/convert.h>
-#include <doodle_core/doodle_core.h>
 
 using namespace doodle;
 BOOST_AUTO_TEST_CASE(create_lib)
@@ -14,6 +14,11 @@ BOOST_AUTO_TEST_CASE(create_lib)
   doodle_lib l_lib{};
 
   BOOST_TEST((l_lib == doodle_lib::Get()));
+
+  for (auto&& l_s : g_reg()->storage()) {
+    l_s.second.type();;
+  }
+
   //  BOOST_TEST(convert::Get().toEn("林奇"s) == "linqi");
   //  BOOST_TEST(convert::Get().toEn("dsa??__测试"s) == "dsa??__ceshi");
 }
