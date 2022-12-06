9cefefb94d13d48d7cec1f7f32858b55f8bcc8e2
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-12-06 11:08:17 +0800

    调整测试

--- src/test/core/lib_create.cpp
+++ src/test/core/lib_create.cpp
@@ -7,6 +7,9 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <entt/entity/fwd.hpp>
+
+
 using namespace doodle;
 BOOST_AUTO_TEST_CASE(create_lib)
 /* Compare with void free_test_function() */
@@ -14,10 +17,6 @@ BOOST_AUTO_TEST_CASE(create_lib)
   doodle_lib l_lib{};
 
   BOOST_TEST((l_lib == doodle_lib::Get()));
-
-  for (auto&& l_s : g_reg()->storage()) {
-    l_s.second.type();;
-  }
 
   //  BOOST_TEST(convert::Get().toEn("林奇"s) == "linqi");
   //  BOOST_TEST(convert::Get().toEn("dsa??__测试"s) == "dsa??__ceshi");
