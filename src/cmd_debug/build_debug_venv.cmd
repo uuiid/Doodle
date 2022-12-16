d85cbe80ced246501193e2e3153a52fcf3acf3d7
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-12-16 10:14:41 +0800

    添加配置脚本

--- src/cmd_debug/build_debug_venv.cmd
+++ src/cmd_debug/build_debug_venv.cmd
@@ -1,15 +1,15 @@
 call %~dp0/../build/set_venv.cmd
 
 
-@REM echo -----------------config main exe--------------------
-@REM "C:\Program Files\CMake\bin\cmake.exe" ^
-@REM -S%my_pwd% ^
-@REM --preset Ninja_build_DD
+echo -----------------config main exe--------------------
+"C:\Program Files\CMake\bin\cmake.exe" ^
+-S%my_pwd% ^
+--preset Ninja_debug%Doodle_suffix%
 
-@REM if %errorlevel% NEQ 0 exit 1
+if %errorlevel% NEQ 0 exit 1
 
 echo -----------------build main exe--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --build ^
---preset debug_doodle_dingding ^
+--preset Ninja_debug%Doodle_suffix% ^
 --target %*
--- src/cmd_debug/config_cmake.cmd
+++ src/cmd_debug/config_cmake.cmd
@@ -4,4 +4,4 @@ call %~dp0/../build/set_venv.cmd
 echo -----------------config main exe--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 -S%my_pwd% ^
---preset Ninja_build_DD
+--preset Ninja_debug%Doodle_suffix%
