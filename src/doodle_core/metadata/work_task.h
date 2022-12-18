ce25ee8f97601fbe68ccc791c6dbb99d5ba579ca
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-12-18 15:53:52 +0800

    完成调整user引用

--- src/doodle_core/metadata/work_task.h
+++ src/doodle_core/metadata/work_task.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "doodle_core/doodle_core_fwd.h"
 #include "doodle_core/metadata/detail/user_ref.h"
 #include "doodle_core/metadata/metadata.h"
 #include "doodle_core/metadata/time_point_wrap.h"
-#include "doodle_core_fwd.h"
 
 #include <entt/entity/fwd.hpp>
 #include <string>
+
 
 namespace doodle {
 /**
