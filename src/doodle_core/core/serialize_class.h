31aae4fd8d3c28a92f2159c3c14b0e9d12de3e7f
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-12-22 10:26:53 +0800

    添加序列化基类

---
+++ src/doodle_core/core/serialize_class.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "doodle_core/configure/doodle_core_export.h"
+
+namespace doodle::detail {
+
+class DOODLE_CORE_API sql_serialize_class {
+ public:
+
+};
+}  // namespace doodle::detail 
