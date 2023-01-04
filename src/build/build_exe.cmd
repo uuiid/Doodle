7dc047503dd60fa0260c199852d872cf22a7be99
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-01-04 10:38:03 +0800

    ✅batch

--- src/build/build_exe.cmd
+++ src/build/build_exe.cmd
@@ -21,5 +21,6 @@ echo -----------------install main exe--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
 --install %my_pwd%\build\Ninja_release%Doodle_suffix% ^
 --component exe_com
+del %cache_file%
 powershell -command "$value = Get-Content %cache_file% -Encoding UTF8 | Select-String -Pattern 'maya' -NotMatch; Set-Content -Path %cache_file% -Value $value" 
 call  %my_pwd%\src\build\doc.cmd > %tmp%/doodle_doc_info.txt
