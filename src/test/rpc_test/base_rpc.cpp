30b346362995069443157f77272f16a006a2b6f0
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-01-04 09:19:03 +0800

    添加测试

--- src/test/rpc_test/base_rpc.cpp
+++ src/test/rpc_test/base_rpc.cpp
@@ -25,8 +25,10 @@
 #include <boost/asio/static_thread_pool.hpp>
 #include <boost/asio/thread_pool.hpp>
 #include <boost/process.hpp>
+#include <boost/test/tools/interface.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include <fmt/core.h>
 #include <fmt/format.h>
 #include <iostream>
 #include <main_fixtures/lib_fixtures.h>
@@ -40,7 +42,8 @@ BOOST_FIXTURE_TEST_SUITE(rpc, loop_rpc)
 BOOST_AUTO_TEST_CASE(base) {
   l_s.run();
   auto l_w = boost::asio::make_work_guard(g_io_context());
-  boost::asio::post(g_thread(), [this]() {
+  bool run{};
+  boost::asio::post(g_thread(), [this, l_run = &run]() {
     distributed_computing::client l_c{};
     // l_c.list_users();
     l_c.close();
@@ -48,41 +51,30 @@ BOOST_AUTO_TEST_CASE(base) {
               << "\n";
     l_s.work_guard->reset();
     g_io_context().stop();
+    *l_run = true;
   });
   // auto l_timer = std::make_shared<boost::asio::high_resolution_timer>(g_io_context());
   // l_timer->expires_after(1s);
   // l_timer->async_wait([l_timer, &l_s](auto) {});
 
   l_ib.io_context_attr().run();
+  BOOST_TEST(run);
 }
 
 BOOST_AUTO_TEST_CASE(list_users) {
   l_s.run();
-
-  auto l_h = make_handle();
-  l_h.emplace<user>().set_name("t1");
-  l_h.emplace<database>();
-
-  l_h = make_handle();
-  l_h.emplace<user>().set_name("t2");
-  l_h.emplace<database>();
-
-  l_h = make_handle();
-  l_h.emplace<user>().set_name("t3");
-  l_h.emplace<database>();
-
-  l_h = make_handle();
-  l_h.emplace<user>().set_name("t4");
-  l_h.emplace<database>();
-
-  l_h = make_handle();
-  l_h.emplace<user>().set_name("t5");
-  l_h.emplace<database>();
+  for (auto i = 0u; i < 10; ++i) {
+    auto l_h = make_handle();
+    l_h.emplace<user>().set_name(fmt::format("user{}", i));
+    l_h.emplace<database>();
+  }
 
   auto l_w = boost::asio::make_work_guard(g_io_context());
   boost::asio::post(g_thread(), [this]() {
     distributed_computing::client l_c{};
-    for (auto&& l_f : l_c.list_users()) {
+    auto l_users = l_c.list_users();
+    BOOST_TEST(l_users.size() == 10);
+    for (auto&& l_f : l_users) {
       std::cout << "user : " << fmt::to_string(l_f.get<user>()) << std::endl;
     }
     l_c.close();
