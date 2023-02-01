d0f3bd38889b45a9c3780dbac6cac7eda676b9cb
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-01 14:02:12 +0800

    调整maya构建

--- src/build/build_maya_plug.cmd
+++ src/build/build_maya_plug.cmd
@@ -2,8 +2,6 @@ call %~dp0/set_venv.cmd
 
 
 @REM powershell -command "$value = Get-Content %cache_file% -Encoding UTF8 | Select-String -Pattern 'maya' -NotMatch; Set-Content -Path %cache_file% -Value $value" 
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\src\maya_plug
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\plug
 del %cache_file%
 
 echo -----------------------------------------------------
@@ -27,8 +25,6 @@ echo -----------------install maya 2018--------------------
 if %errorlevel% NEQ 0 exit 1
 echo -----------------clear maya 2018--------------------
 @REM powershell -command "$value = Get-Content %cache_file% -Encoding UTF8 | Select-String -Pattern 'maya' -NotMatch; Set-Content -Path %cache_file% -Value $value" 
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\src\maya_plug
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\plug
 del %cache_file%
 
 echo -----------------------------------------------------
@@ -52,8 +48,6 @@ echo -----------------install maya 2019--------------------
 if %errorlevel% NEQ 0 exit 1
 echo -----------------clear maya 2019--------------------
 @REM powershell -command "$value = Get-Content %cache_file% -Encoding UTF8 | Select-String -Pattern 'maya' -NotMatch; Set-Content -Path %cache_file% -Value $value" 
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\src\maya_plug
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\plug
 del %cache_file%
 
 echo -----------------------------------------------------
@@ -77,6 +71,4 @@ echo -----------------install maya 2020--------------------
 if %errorlevel% NEQ 0 exit 1
 echo -----------------clear maya 2020--------------------
 @REM powershell -command "$value = Get-Content %cache_file% -Encoding UTF8 | Select-String -Pattern 'maya' -NotMatch; Set-Content -Path %cache_file% -Value $value" 
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\src\maya_plug
-rmdir /q /s %my_pwd%\build\Ninja_release%Doodle_suffix%\plug
 del %cache_file%
