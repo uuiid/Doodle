a0cc291a2f5e2e18ae9d5c1979c6ceca5d9a5747
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-01 14:45:57 +0800

    调整maya构建

---
+++ src/exe_gui/install_script.cmake
@@ -0,0 +1,14 @@
+
+
+# 修复dll
+include(BundleUtilities)
+fixup_bundle("${DOODLE_MAIN_EXE}" "" "")
+# 添加安装配置
+configure_file(@CMAKE_CURRENT_LIST_DIR@/install.wxs \${CMAKE_INSTALL_PREFIX}/wix/doodle_gui_exe.wxs)
+
+# 生成授权文件
+
+execute_process(
+        COMMAND ${DOODLE_GENERATE_TOKEN_RUN} ${CMAKE_INSTALL_PREFIX}/bin/
+        WORKING_DIRECTORY @PROJECT_BINARY_DIR@/bin
+)
