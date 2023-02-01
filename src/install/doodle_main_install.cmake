bc865a5a3528664d4b0513ed123692e224a8c133
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-01 09:09:17 +0800

    最终完成安装配置

--- src/install/doodle_main_install.cmake
+++ src/install/doodle_main_install.cmake
@@ -8,11 +8,35 @@ file(GLOB 7Z_FOLDER_LISTS
         ${CMAKE_INSTALL_PREFIX}/*
         )
 
-execute_process(COMMAND ${CMAKE_COMMAND}
-        -D CMAKE_CURRENT_LIST_DIR=@CMAKE_CURRENT_LIST_DIR@
-        -D CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-        -P @CMAKE_CURRENT_LIST_DIR@/doodle_wix_config.cmake
-        )
+set(DOODLE_PACKAGE_FEATURE_REF_ID_LIST)
+foreach (files IN LISTS WIX_FILE_LISTS)
+    file(READ ${CMAKE_INSTALL_PREFIX}/wix/${files} DOODLE_TEST_STR)
+    if (DOODLE_TEST_STR MATCHES [[<Feature Id="([A-Za-z0-9_]+)"]])
+        string(REGEX MATCHALL [[<Feature Id="([A-Za-z0-9_]+)"]] DOODLE_TMP_LIST ${DOODLE_TEST_STR})
+        foreach (TMP_STR IN LISTS DOODLE_TMP_LIST)
+            string(REGEX MATCH [[<Feature Id="([A-Za-z0-9_]+)"]] _ ${TMP_STR})
+            list(APPEND DOODLE_PACKAGE_FEATURE_REF_ID_LIST "<FeatureRef Id=\"${CMAKE_MATCH_1}\"/>")
+        endforeach ()
+    endif ()
+endforeach ()
+
+
+list(JOIN DOODLE_PACKAGE_FEATURE_REF_ID_LIST " " DOODLE_PACKAGE_FEATURE_REF_ID_LIST)
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/main.wxs ${CMAKE_INSTALL_PREFIX}/wix/main.wxs)
+list(APPEND WIX_FILE_LISTS main.wxs)
+list(REMOVE_DUPLICATES WIX_FILE_LISTS)
+
+execute_process(
+        COMMAND ${DOODLE_PANDOC_PATH} -s ${PROJECT_SOURCE_DIR}/LICENSE -f textile -o ./wix/LICENSE.rtf
+        WORKING_DIRECTORY \${CMAKE_INSTALL_PREFIX}
+)
+
+#execute_process(COMMAND ${CMAKE_COMMAND}
+#        -D CMAKE_CURRENT_LIST_DIR=@CMAKE_CURRENT_LIST_DIR@
+#        -D CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+#        -P @CMAKE_CURRENT_LIST_DIR@/doodle_wix_config.cmake
+#        )
 
 list(REMOVE_ITEM 7Z_FOLDER_LISTS wix)
 list(TRANSFORM 7Z_FOLDER_LISTS PREPEND ${CMAKE_INSTALL_PREFIX}/)
