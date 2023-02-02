d9d08064a67176fad84d25df8ef96cc68672b09f
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-02 14:25:42 +0800

    更新测试

--- src/test/core/create_move.cpp
+++ src/test/core/create_move.cpp
@@ -2,6 +2,7 @@
 // Created by TD on 2022/10/8.
 //
 
+#include <doodle_core/core/program_info.h>
 #include <doodle_core/doodle_core.h>
 
 #include <doodle_app/app/app_command.h>
@@ -26,6 +27,7 @@ struct loop_ {
 BOOST_FIXTURE_TEST_SUITE(move, loop_, *boost::unit_test::disabled())
 
 BOOST_AUTO_TEST_CASE(create) {
+  program_info::emplace();
   g_reg()->ctx().emplace<image_to_move>(std::make_shared<detail::image_to_move>());
   auto l_h = make_handle();
   FSys::path l_image_path{R"(D:\tmp\image_test_ep002_sc001)"};
@@ -38,7 +40,6 @@ BOOST_AUTO_TEST_CASE(create) {
   auto l_w = boost::asio::make_work_guard(g_io_context());
   g_reg()->ctx().at<image_to_move>()->async_create_move(l_h, l_files, [l_r = &run_test, this, work = &l_w]() {
     *l_r = true;
-    boost::asio::post(g_io_context(), []() { app_base::Get().stop_app(); });
     work->reset();
   });
 
