0177ffdd2e9cbd60738dd1ed3674d652b20c0371
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-02 15:04:42 +0800

    添加创建moive命令行类

---
+++ src/doodle_lib/facet/create_move_facet.cpp
@@ -0,0 +1,9 @@
+//
+// Created by TD on 2023/2/2.
+//
+
+#include "create_move_facet.h"
+
+namespace doodle {
+namespace facet {}  // namespace facet
+}  // namespace doodle
---
+++ src/doodle_lib/facet/create_move_facet.h
@@ -0,0 +1,16 @@
+//
+// Created by TD on 2023/2/2.
+//
+
+#ifndef DOODLE_CREATE_MOVE_FACET_H
+#define DOODLE_CREATE_MOVE_FACET_H
+
+namespace doodle {
+namespace facet {
+
+class create_move_facet {};
+
+}  // namespace facet
+}  // namespace doodle
+
+#endif  // DOODLE_CREATE_MOVE_FACET_H
