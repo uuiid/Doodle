98acc967ce1b0b4a03004829301ece6a86149e95
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-03 10:05:04 +0800

    添加附加到控制台

--- src/doodle_lib/facet/create_move_facet.cpp
+++ src/doodle_lib/facet/create_move_facet.cpp
@@ -14,10 +14,55 @@
 
 #include <boost/program_options.hpp>
 
+#include <spdlog/sinks/stdout_sinks.h>
 // #include <wil/result.h>
 namespace doodle::facet {
+
+namespace {
+void open_console() {
+  /// 释放控制台
+  CONSOLE_SCREEN_BUFFER_INFO con_info;  /// 控制台信息
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &con_info);
+  //    if (conInfo.dwSize.Y < minLength)
+  //      conInfo.dwSize.Y = minLength;
+  SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), con_info.dwSize);
+
+  if (::AttachConsole(ATTACH_PARENT_PROCESS))
+    DOODLE_LOG_INFO("附加到控制台");
+  else
+    ::AllocConsole();
+
+  FILE* fp;
+  // Redirect STDIN if the console has an input handle
+  if (GetStdHandle(STD_INPUT_HANDLE) != INVALID_HANDLE_VALUE)
+    if (freopen_s(&fp, "CONIN$", "r", stdin) != 0)
+      DOODLE_LOG_INFO("打开std 句柄错误");
+    else
+      setvbuf(stdin, NULL, _IONBF, 0);
+
+  // Redirect STDOUT if the console has an output handle
+  if (GetStdHandle(STD_OUTPUT_HANDLE) != INVALID_HANDLE_VALUE)
+    if (freopen_s(&fp, "CONOUT$", "w", stdout) != 0)
+      DOODLE_LOG_INFO("打开std 句柄错误");
+    else
+      setvbuf(stdout, NULL, _IONBF, 0);
+
+  // Redirect STDERR if the console has an error handle
+  if (GetStdHandle(STD_ERROR_HANDLE) != INVALID_HANDLE_VALUE)
+    if (freopen_s(&fp, "CONOUT$", "w", stderr) != 0)
+      DOODLE_LOG_INFO("打开std 句柄错误");
+    else
+      setvbuf(stderr, NULL, _IONBF, 0);
+
+  // Make C++ standard streams point to console as well.
+  std::ios::sync_with_stdio(true);
+}
+}  // namespace
+
 const std::string& create_move_facet::name() const noexcept { return name_; }
 void create_move_facet::operator()() {
+  open_console();
+  logger_ctrl::get_log().add_log_sink(std::make_shared<spdlog::sinks::stdout_sink_mt>());
   if (!g_reg()->ctx().contains<image_to_move>())
     g_reg()->ctx().emplace<image_to_move>(std::make_shared<detail::image_to_move>());
 
