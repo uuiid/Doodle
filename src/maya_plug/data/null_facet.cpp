cb270ea2bbaf074dbfc4dfad82e047b0c64c2845
张玉斌 <46096697+uuiid@users.noreply.github.com>
2023-02-03 20:45:34 +0800

    去除工作守卫

--- src/maya_plug/data/null_facet.cpp
+++ src/maya_plug/data/null_facet.cpp
@@ -12,7 +12,7 @@ const std::string& null_facet::name() const noexcept {
   static std::string l_i{"null_facet"};
   return l_i;
 }
-void null_facet::operator()() { work_lock.emplace(boost::asio::make_work_guard(g_io_context())); }
+void null_facet::operator()() {}
 void null_facet::deconstruction() { work_lock.reset(); }
 null_facet::null_facet() : doodle::detail::app_facet_interface() {
   g_reg()->ctx().emplace<image_to_move>() = std::make_shared<detail::maya_create_movie>();
