d51cf2eb639dfecfac1a44b65b798c8132eb9fe7
张玉斌 <957714080@qq.com>
2023-02-27 16:35:13 +0800

    完成基本的数据结构

--- script/uePlug/Doodle/Source/DoodleEditor/Private/Doodle/CreateCharacter/CoreData/DoodleCreateCharacterInstance.cpp
+++ script/uePlug/Doodle/Source/DoodleEditor/Private/Doodle/CreateCharacter/CoreData/DoodleCreateCharacterInstance.cpp
@@ -8,3 +8,11 @@ void UDoodleCreateCharacterInstance::SetCreateCharacterConfig(const TObjectPtr<U
 FAnimInstanceProxy* UDoodleCreateCharacterInstance::CreateAnimInstanceProxy() {
   return new FDoodleCreateCharacterProxy{this};
 }
+
+void FDoodleCreateCharacterProxy::Initialize(UAnimInstance* InAnimInstance) {}
+
+void FDoodleCreateCharacterProxy::PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds) {}
+
+bool FDoodleCreateCharacterProxy::Evaluate(FPoseContext& Output) { return false; }
+
+void FDoodleCreateCharacterProxy::UpdateAnimationNode(const FAnimationUpdateContext& InContext) {}
