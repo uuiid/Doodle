786ec520c3ee9685bd4fe7a774f4f0413abf0eaf
uuiid <957714080@qq.com>
2023-03-21 10:26:22 +0800

    调整按照方法

--- src/build/install.cmd
+++ src/build/install.cmd
@@ -16,13 +16,13 @@ mkdir %my_pwd%\build\html\file
 
 :copy_file
 echo "%my_pwd%\build\html\file *.msi -> %my_pwd%\build\install\%doodle_install_prefix%"
-robocopy %my_pwd%\build\install\%doodle_install_prefix% %my_pwd%\build\html\file *.msi > %tmp%/doodle_install_copy1.txt
+robocopy %my_pwd%\build\install\%doodle_install_prefix% %my_pwd%\build\html\file *.msi
 
 echo "%my_pwd%\build\html\file *.7z -> %my_pwd%\build\install\%doodle_install_prefix% "
-robocopy %my_pwd%\build\install\%doodle_install_prefix% %my_pwd%\build\html\file *.7z > %tmp%/doodle_install_copy2.txt
+robocopy %my_pwd%\build\install\%doodle_install_prefix% %my_pwd%\build\html\file *.7z
 
-robocopy %my_pwd%\build\install\%doodle_install_prefix% \\192.168.10.250\public\Prism_projects\doodle\ *.msi > %tmp%/doodle_install_copy1.txt
-robocopy %my_pwd%\build\install\%doodle_install_prefix% \\192.168.10.250\public\Prism_projects\doodle\ *.7z > %tmp%/doodle_install_copy2.txt
+@REM robocopy %my_pwd%\build\install\%doodle_install_prefix% \\192.168.10.250\public\Prism_projects\doodle\ *.msi
+@REM robocopy %my_pwd%\build\install\%doodle_install_prefix% \\192.168.10.250\public\Prism_projects\doodle\ *.7z
 
 echo "generate %my_pwd%/build/html/file/index.html"
 py %my_pwd%/docs/generate_directory_index_caddystyle.py %my_pwd%/build/html/file
