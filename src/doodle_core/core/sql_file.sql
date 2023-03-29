c702909c419d4bc0fc816f52944a6f71344ccd88
uuiid <957714080@qq.com>
2023-03-29 16:58:04 +0800

    调整sql

--- src/doodle_core/core/sql_file.sql
+++ src/doodle_core/core/sql_file.sql
@@ -94,154 +94,154 @@ create index if not exists shot_index2 on shot (entity_id);
 
 CREATE TABLE IF NOT EXISTS redirection_path_info
 (
-    id                      integer
+    id                    integer
         primary key,
-    entity_id               integer,
+    entity_id             integer,
 
-    redirection_path        text,
-    redirection_file_name   text,
+    redirection_path      text,
+    redirection_file_name text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists redirection_path_info_index on redirection_path_info(id)
-create index if not exists redirection_path_info_index2 on redirection_path_info(entity_id)
+create index if not exists redirection_path_info_index on redirection_path_info (id);
+create index if not exists redirection_path_info_index2 on redirection_path_info (entity_id);
 
 CREATE TABLE IF NOT EXISTS rpi_search_path
 (
-    id                      integer
+    id        integer
         primary key,
-    entity_id               integer,
+    entity_id integer,
 
-    path                    text,
+    path      text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists rpi_search_path_index on rpi_search_path(id)
-create index if not exists rpi_search_path_index2 on rpi_search_path(entity_id)
+create index if not exists rpi_search_path_index on rpi_search_path (id);
+create index if not exists rpi_search_path_index2 on rpi_search_path (entity_id);
 
 CREATE TABLE IF NOT EXISTS assets
 (
-    id                      integer
+    id          integer
         primary key,
-    entity_id               integer,
+    entity_id   integer,
 
-    assets_path             text,
+    assets_path text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists assets_index on assets(id)
-create index if not exists assets_index2 on assets(entity_id)
+create index if not exists assets_index on assets (id);
+create index if not exists assets_index2 on assets (entity_id);
 
 CREATE TABLE IF NOT EXISTS comment
 (
-    id                      integer
+    id             integer
         primary key,
-    entity_id               integer,
+    entity_id      integer,
 
-    comment_string             text,
-    comment_time               text,
+    comment_string text,
+    comment_time   text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists comment_index on comment(id)
-create index if not exists comment_index2 on comment(entity_id)
+create index if not exists comment_index on comment (id);
+create index if not exists comment_index2 on comment (entity_id);
 
 CREATE TABLE IF NOT EXISTS export_file_info
 (
-    id                      integer
+    id           integer
         primary key,
-    entity_id               integer,
+    entity_id    integer,
 
-    file_path               text,
-    start_frame             integer,
-    end_frame               integer,
-    ref_file                text,
-    export_type_            text,
+    file_path    text,
+    start_frame  integer,
+    end_frame    integer,
+    ref_file     text,
+    export_type_ text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists export_file_info_index on export_file_info(id)
-create index if not exists export_file_info_index2 on export_file_info(entity_id)
+create index if not exists export_file_info_index on export_file_info (id);
+create index if not exists export_file_info_index2 on export_file_info (entity_id);
 
 CREATE TABLE IF NOT EXISTS image_icon
 (
-    id                      integer
+    id        integer
         primary key,
-    entity_id               integer,
+    entity_id integer,
 
-    path                    text,
+    path      text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists image_icon_index on image_icon(id)
-create index if not exists image_icon_index2 on image_icon(entity_id)
+create index if not exists image_icon_index on image_icon (id);
+create index if not exists image_icon_index2 on image_icon (entity_id);
 
 CREATE TABLE IF NOT EXISTS assets_file
 (
-    id                      integer
+    id        integer
         primary key,
-    entity_id               integer,
+    entity_id integer,
 
-    name                    text,
-    path                    text,
-    version                 integer,
-    user_ref                integer,
+    name      text,
+    path      text,
+    version   integer,
+    user_ref  integer,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists assets_file_index on assets_file(id)
-create index if not exists assets_file_index2 on assets_file(entity_id)
+create index if not exists assets_file_index on assets_file (id);
+create index if not exists assets_file_index2 on assets_file (entity_id);
 
 CREATE TABLE IF NOT EXISTS time_point_info
 (
-    id                      integer
+    id            integer
         primary key,
-    entity_id               integer,
+    entity_id     integer,
 
-    first_time              datetime,
-    second_time             datetime,
-    info                    text,
-    is_extra_work           bool,
+    first_time    datetime,
+    second_time   datetime,
+    info          text,
+    is_extra_work bool,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists time_point_info_index on time_point_info(id)
-create index if not exists time_point_info_index2 on time_point_info(entity_id)
+create index if not exists time_point_info_index on time_point_info (id);
+create index if not exists time_point_info_index2 on time_point_info (entity_id);
 
 CREATE TABLE IF NOT EXISTS project_config
 (
-    id                                  integer
-        primary key,            
-    entity_id                           integer,
+    id                                integer
+        primary key,
+    entity_id                         integer,
 
-    sim_path                            text,
-    export_group                        text,
-    cloth_proxy                         text,
-    simple_module_proxy                 text,
-    find_icon_regex                     text,
-    assets_list                         text,
-    icon_extensions                     text,
-    upload_path                         text,
-    season_count                        integer,
-    use_only_sim_cloth                  bool,
-    use_divide_group_export             bool,
-    use_rename_material                 bool,
-    use_merge_mesh                      bool,
-    t_post                              integer,
-    export_anim_time                    integer,
-    export_abc_arg                      integer,
-    maya_camera_select                  bool,
-    use_write_metadata                  bool,
-    abc_export_extract_reference_name   text,
-    abc_export_format_reference_name    text,
-    abc_export_extract_scene_name       text,
-    abc_export_format_scene_name        text,
-    abc_export_add_frame_range          bool,
-    maya_camera_suffix                  text,
-    maya_out_put_abc_suffix             text,
+    sim_path                          text,
+    export_group                      text,
+    cloth_proxy                       text,
+    simple_module_proxy               text,
+    find_icon_regex                   text,
+    assets_list                       text,
+    icon_extensions                   text,
+    upload_path                       text,
+    season_count                      integer,
+    use_only_sim_cloth                bool,
+    use_divide_group_export           bool,
+    use_rename_material               bool,
+    use_merge_mesh                    bool,
+    t_post                            integer,
+    export_anim_time                  integer,
+    export_abc_arg                    integer,
+    maya_camera_select                bool,
+    use_write_metadata                bool,
+    abc_export_extract_reference_name text,
+    abc_export_format_reference_name  text,
+    abc_export_extract_scene_name     text,
+    abc_export_format_scene_name      text,
+    abc_export_add_frame_range        bool,
+    maya_camera_suffix                text,
+    maya_out_put_abc_suffix           text,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
-create index if not exists project_config_index on project_config(id)
-create index if not exists project_config_index2 on project_config(entity_id)
+create index if not exists project_config_index on project_config (id);
+create index if not exists project_config_index2 on project_config (entity_id);
 CREATE TABLE IF NOT EXISTS season
 (
     id        integer
         primary key,
     entity_id integer,
 
-    p_int  integer,
+    p_int     integer,
     foreign key (entity_id) references entity (id) on delete cascade on update cascade
 );
 create index if not exists season_index on season (id);
