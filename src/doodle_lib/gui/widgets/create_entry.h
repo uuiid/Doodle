850fff2707132c6153a98ac45aac290b62bbe4a4
uuiid <957714080@qq.com>
2023-03-30 11:15:14 +0800

    调整接口

--- src/doodle_lib/gui/widgets/create_entry.h
+++ src/doodle_lib/gui/widgets/create_entry.h
@@ -6,14 +6,36 @@
 
 #include <doodle_lib/doodle_lib_fwd.h>
 
+#include "entt/entity/fwd.hpp"
+#include <functional>
 #include <vector>
 namespace doodle::gui {
 
 class create_entry {
-  std::vector<FSys::path> paths_{};
+ public:
+  class init_args {
+    friend class create_entry;
+    std::vector<FSys::path> paths_{};
+    std::function<void(const std::vector<entt::handle>&)> fun_{};
+
+   public:
+    init_args() = default;
+
+    inline init_args& set_create_call(const std::function<void(const std::vector<entt::handle>&)>& in_function) {
+      fun_ = in_function;
+      return *this;
+    }
+    inline init_args& set_paths(const std::vector<FSys::path>& in_paths) {
+      paths_ = in_paths;
+      return *this;
+    };
+  };
+
+ private:
+  init_args args_{};
 
  public:
-  explicit create_entry(const std::vector<FSys::path>& in_paths) : paths_(in_paths) {}
+  explicit create_entry(const init_args& in_args) : args_(in_args) {}
 
   constexpr static std::string_view name{gui::config::menu_w::create_entry_};
 
