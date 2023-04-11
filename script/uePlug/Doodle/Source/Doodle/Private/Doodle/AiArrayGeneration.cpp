33d2a5dd2e2f43d6e41b12561f746a542cfc8ba2
uuiid <957714080@qq.com>
2023-04-11 16:47:11 +0800

    添加ue4集群功能

--- script/uePlug/Doodle/Source/Doodle/Private/Doodle/AiArrayGeneration.cpp
+++ script/uePlug/Doodle/Source/Doodle/Private/Doodle/AiArrayGeneration.cpp
@@ -1,16 +1,86 @@
 #include "Doodle/AiArrayGeneration.h"
 
-#include "Components/BrushComponent.h"
+#include "AI/NavigationSystemBase.h"
+#include "Components/SplineComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "NavigationSystem.h"
 
 ADoodleAiArrayGeneration::ADoodleAiArrayGeneration() {
-  BrushComponent           = CreateDefaultSubobject<UBrushComponent>(TEXT("BrushComponent"));
-  BrushComponent->Mobility = EComponentMobility::Static;
-  BrushComponent->SetGenerateOverlapEvents(false);
-  BrushComponent->SetCanEverAffectNavigation(false);
+  SplineComponent           = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+  SplineComponent->Mobility = EComponentMobility::Static;
+  SplineComponent->SetGenerateOverlapEvents(false);
+  SplineComponent->SetCanEverAffectNavigation(false);
+  SplineComponent->SetClosedLoop(true);
 
-  RootComponent = BrushComponent;
+  SplineComponent->SetSplinePoints(
+      {FVector{100, 0, 0}, FVector{0, 100, 0}, FVector{-100, 0, 0}, FVector{0, -100, 0}}, ESplineCoordinateSpace::World
+  );
+
+  RootComponent = SplineComponent;
   SetHidden(true);
   SetCanBeDamaged(false);
   bCollideWhenPlacing          = true;
   SpawnCollisionHandlingMethod = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+  Row                          = 10;
+  Column                       = 10;
+}
+
+void ADoodleAiArrayGeneration::Tick(float DeltaTime) {}
+
+void ADoodleAiArrayGeneration::GenPoint() {
+  // RandomStream.Initialize();
+  // RandomStream.RandRange(0, 10);
+  Points.Empty(Row * Column);
+  FBox L_Box_         = SplineComponent->GetLocalBounds().TransformBy(GetTransform()).GetBox();
+  auto L_Row_Step     = L_Box_.GetSize();
+  double L_Row_Step_X = L_Row_Step.X / FMath::Clamp((double)Row, 1.0, 1000.0);
+  double L_Row_Step_Y = L_Row_Step.Y / FMath::Clamp((double)Column, 1.0, 1000.0);
+  // UE_LOG(LogTemp, Log, TEXT("L_Row_Step_X %f L_Row_Step_Y %f"), L_Row_Step_X, L_Row_Step_Y);
+
+  // DrawDebugLine(GetWorld(), GetActorTransform().GetLocation(), GetActorTransform().GetLocation() + L_Vector * 50,
+  // FColor::Red, false, 10.f);
+
+  for (auto x = 0.0; x <= FMath::Clamp(Row, 1, 1000); ++x) {
+    for (auto y = 0.0; y <= FMath::Clamp(Column, 1, 1000); ++y) {
+      FVector L_Point       = L_Box_.Min + FVector{L_Row_Step_X * x, L_Row_Step_Y * y, 0};
+      const float L_Param   = SplineComponent->FindInputKeyClosestToWorldLocation(L_Point);
+      FVector L_RightVector = SplineComponent->GetRightVectorAtSplineInputKey(L_Param, ESplineCoordinateSpace::World);
+      FVector L_Vector = SplineComponent->GetLocationAtSplineInputKey(L_Param, ESplineCoordinateSpace::World) - L_Point;
+      if (L_Vector.Dot(L_RightVector) < 0.0) {
+        FHitResult L_HitR{};
+        if (UKismetSystemLibrary::LineTraceSingleForObjects(
+                GetWorld(), L_Point, L_Point - FVector{0, 0, -100},
+                {UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic)}, false, {},
+                EDrawDebugTrace::None, L_HitR, true
+            )) {
+          FVector L_Point_Out{};
+          if (GetRandomPointInRadius(L_HitR.ImpactPoint, L_Point_Out)) {
+            Points.Add(L_Point_Out);
+            DrawDebugPoint(GetWorld(), L_Point_Out, 10.0f, FColor::Green, false, 1.0f);
+          }
+        }
+
+        // DrawDebugPoint(GetWorld(), L_Point, 10.0f, FColor::Red, false, 1.0f);
+      }
+    }
+  }
+}
+
+bool ADoodleAiArrayGeneration::GetRandomPointInRadius(const FVector& Origin, FVector& OutResult) {
+  UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+  if (!NavSys) {
+    return false;
+  }
+
+  FNavLocation Result;
+  bool bSuccess = NavSys->GetRandomReachablePointInRadius(Origin, RandomRadius, Result);
+  // Out
+  OutResult     = Result;
+
+  return bSuccess;
+}
+
+void ADoodleAiArrayGeneration::OnConstruction(const FTransform& Transform) {
+  Super::OnConstruction(Transform);
+  GenPoint();
 }
--- script/uePlug/Doodle/Source/Doodle/Public/Doodle/AiArrayGeneration.h
+++ script/uePlug/Doodle/Source/Doodle/Public/Doodle/AiArrayGeneration.h
@@ -7,7 +7,7 @@
 // 这个必须最后导入
 #include "AiArrayGeneration.generated.h"
 
-class UBrushComponent;
+class USplineComponent;
 
 UCLASS()
 class DOODLE_API ADoodleAiArrayGeneration : public AActor {
@@ -17,7 +17,32 @@ class DOODLE_API ADoodleAiArrayGeneration : public AActor {
 
  private:
   UPROPERTY(Category = Collision, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-  TObjectPtr<class UBrushComponent> BrushComponent;
+  TObjectPtr<class USplineComponent> SplineComponent;
 
  public:
+  FRandomStream RandomStream;
+  UPROPERTY(
+      EditAnywhere, BlueprintReadOnly, Category = "Doodle", DisplayName = "行", meta = (ClampMin = 1, ClampMax = 1000)
+  )
+  int32 Row;
+  UPROPERTY(
+      EditAnywhere, BlueprintReadOnly, Category = "Doodle", DisplayName = "列", meta = (ClampMin = 1, ClampMax = 1000)
+  )
+  int32 Column;
+
+  UPROPERTY(
+      EditAnywhere, BlueprintReadOnly, Category = "Doodle", DisplayName = "随机范围",
+      meta = (ClampMin = -1000.0, ClampMax = 1000)
+  )
+  float RandomRadius;
+
+  virtual void Tick(float DeltaTime) override;
+
+ private:
+  UPROPERTY()
+  TArray<FVector> Points;
+  void GenPoint();
+  bool GetRandomPointInRadius(const FVector &Origin, FVector &OutResult);
+
+  virtual void OnConstruction(const FTransform &Transform) override;
 };
