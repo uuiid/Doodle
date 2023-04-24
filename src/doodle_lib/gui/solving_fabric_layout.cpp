4240ad3a4a726924368df3de16407c5eaff11f74
uuiid <957714080@qq.com>
2023-04-24 14:57:32 +0800

    添加解算布局

---
+++ src/doodle_lib/gui/solving_fabric_layout.cpp
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/4/24.
+//
+
+#include "solving_fabric_layout.h"
+
+namespace doodle {
+namespace gui {}  // namespace gui
+}  // namespace doodle
---
+++ src/doodle_lib/gui/solving_fabric_layout.h
@@ -0,0 +1,12 @@
+//
+// Created by td_main on 2023/4/24.
+//
+#pragma once
+
+namespace doodle {
+namespace gui {
+
+class solving_fabric_layout {};
+
+}  // namespace gui
+}  // namespace doodle
