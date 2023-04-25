97bd0b79dda758589e6eb9c20091205c8f688033
uuiid <957714080@qq.com>
2023-04-25 13:18:44 +0800

    添加解算布料进程

---
+++ src/exe_maya/facet/cloth_sim.cpp
@@ -0,0 +1,5 @@
+//
+// Created by td_main on 2023/4/25.
+//
+
+#include "cloth_sim.h"
---
+++ src/exe_maya/facet/cloth_sim.h
@@ -0,0 +1,11 @@
+//
+// Created by td_main on 2023/4/25.
+//
+#pragma once
+
+namespace doodle {
+class cloth_sim {
+ public:
+};
+
+}  // namespace doodle
