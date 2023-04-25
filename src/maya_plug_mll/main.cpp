8ad9ab674d31b02f276255130a7884d07f99ddb3
uuiid <957714080@qq.com>
2023-04-25 11:51:32 +0800

    调整maya构建

--- src/maya_plug_mll/main.cpp
+++ src/maya_plug_mll/main.cpp
@@ -1,16 +1,9 @@
-/*
- * @Author: your name
- * @Date: 2020-11-16 19:05:15
- * @LastEditTime: 2020-11-30 10:53:01
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \Doodle\doodle_GUI\main.cpp
- */
+//
+// Created by td_main on 2023/4/25.
+//
 #include <doodle_lib/facet/create_move_facet.h>
 #include <doodle_lib/facet/main_facet.h>
 #include <doodle_lib/facet/rpc_server_facet.h>
-// #include <doodle_lib/DoodleApp.h>
-// #include <boost/locale.hpp>
 
 // extern "C" int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR strCmdLine, int nCmdShow) try {
 extern "C" int main() try {
