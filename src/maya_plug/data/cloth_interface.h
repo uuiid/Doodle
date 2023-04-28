ecdc6c50b3d9c5c1434f12d8410e912bc671c5f6
uuiid <957714080@qq.com>
2023-04-28 15:11:55 +0800

    添加布料工场接口和调整布料接口

--- src/maya_plug/data/cloth_interface.h
+++ src/maya_plug/data/cloth_interface.h
@@ -4,27 +4,35 @@
 
 #pragma once
 
+#include <doodle_core/core/file_sys.h>
+
+#include "entt/entity/fwd.hpp"
 #include <entt/entt.hpp>
+#include <memory>
+#include <vector>
 
 namespace doodle::maya_plug {
 namespace details {
 
-class cloth_interface
-    : public entt::type_list<void(const FSys::path& path) const, void() const, void(const entt::handle&) const> {
+class cloth_interface {
  public:
-  template <typename base_t>
-  struct type : base_t {
-    void set_cache_folder(const FSys::path& in_path) const { return entt::poly_call<0>(*this, in_path); };
-    void sim_cloth() const { return entt::poly_call<1>(*this); };
-    void add_field(const entt::handle& in_handle) const { return entt::poly_call<2>(*this, in_handle); };
-  };
+  virtual void set_cache_folder(const FSys::path& in_path) const  = 0;
+  virtual void sim_cloth() const                                  = 0;
+  virtual void add_field(const entt::handle& in_handle) const     = 0;
+  virtual void add_collision(const entt::handle& in_handle) const = 0;
+  virtual void rest() const                                       = 0;
+  virtual void clear_cache() const                                = 0;
+  inline void set_cache_folder() const { set_cache_folder(FSys::path{}); }
+};
 
-  template <typename type_t>
-  using impl = entt::value_list<&type_t::set_cache_folder, &type_t::sim_cloth, &type_t::add_field>;
+class cloth_factory_interface {
+ public:
+  virtual std::vector<entt::handle> create_cloth() const = 0;
 };
 
 }  // namespace details
 
-using cloth_interface = entt::poly<details::cloth_interface>;
+using cloth_interface         = std::shared_ptr<details::cloth_interface>;
+using cloth_factory_interface = std::shared_ptr<details::cloth_factory_interface>;
 
 }  // namespace doodle::maya_plug
