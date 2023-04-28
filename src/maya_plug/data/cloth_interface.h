c1a20bf528e0944bbbafa8cc030d99496de36818
uuiid <957714080@qq.com>
2023-04-28 16:36:08 +0800

    调整接口

--- src/maya_plug/data/cloth_interface.h
+++ src/maya_plug/data/cloth_interface.h
@@ -7,22 +7,25 @@
 #include <doodle_core/core/file_sys.h>
 
 #include "entt/entity/fwd.hpp"
+#include "maya/MApiNamespace.h"
 #include <entt/entt.hpp>
 #include <memory>
 #include <vector>
 
 namespace doodle::maya_plug {
+class reference_file;
 namespace details {
 
 class cloth_interface {
  public:
-  virtual void set_cache_folder(const FSys::path& in_path) const  = 0;
-  virtual void sim_cloth() const                                  = 0;
-  virtual void add_field(const entt::handle& in_handle) const     = 0;
-  virtual void add_collision(const entt::handle& in_handle) const = 0;
-  virtual void rest() const                                       = 0;
-  virtual void clear_cache() const                                = 0;
-  inline void set_cache_folder() const { set_cache_folder(FSys::path{}); }
+  virtual void set_cache_folder(const entt::handle& in_handle, const FSys::path& in_path) const = 0;
+  virtual MObject get_solver(const entt::handle_view<reference_file>& in_handle) const          = 0;
+  virtual void sim_cloth() const                                                                = 0;
+  virtual void add_field(const entt::handle& in_handle) const                                   = 0;
+  virtual void add_collision(const entt::handle& in_handle) const                               = 0;
+  virtual void rest(const entt::handle& in_handle) const                                        = 0;
+  virtual void clear_cache() const                                                              = 0;
+  inline void set_cache_folder(const entt::handle& in_handle) const { set_cache_folder(in_handle, FSys::path{}); }
 };
 
 class cloth_factory_interface {
