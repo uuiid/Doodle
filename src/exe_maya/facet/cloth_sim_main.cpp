65ef970a3bf4b2203eb22cf1e11968a369927000
uuiid <957714080@qq.com>
2023-04-28 15:12:56 +0800

    布料解算构面初步完成

--- src/exe_maya/facet/cloth_sim_main.cpp
+++ src/exe_maya/facet/cloth_sim_main.cpp
@@ -1,23 +1,37 @@
 //
 // Created by td_main on 2023/4/26.
 //
+#include "doodle_core/core/doodle_lib.h"
+
 #include "boost/lambda2/lambda2.hpp"
 #include <boost/lambda2.hpp>
 
+#include "maya_plug/data/cloth_interface.h"
 #include "maya_plug/data/maya_file_io.h"
+#include "maya_plug/data/qcloth_factory.h"
+#include "maya_plug/main/maya_plug_fwd.h"
+#include <maya_plug/data/ncloth_factory.h>
+#include <maya_plug/data/qcloth_factory.h>
 #include <maya_plug/data/reference_file.h>
 #include <maya_plug/data/sim_cover_attr.h>
 
 #include "cloth_sim.h"
+#include "cloth_sim/cloth_sim_factory.h"
+#include "entt/entity/fwd.hpp"
 #include "range/v3/action/remove_if.hpp"
 #include "range/v3/algorithm/for_each.hpp"
 #include <exe_maya/data/play_blast.h>
 #include <filesystem>
 #include <maya/MAnimControl.h>
+#include <memory>
 namespace doodle::maya_plug {
 
-void cloth_sim::create_ref_file() { ref_files_ = doodle_lib::Get().ctx().get<reference_file_factory>().create_ref(); }
+void cloth_sim::create_ref_file() {
+  DOODLE_LOG_INFO("开始扫瞄引用");
+  ref_files_ = doodle_lib::Get().ctx().get<reference_file_factory>().create_ref();
+}
 void cloth_sim::replace_ref_file() {
+  DOODLE_LOG_INFO("开始替换引用");
   auto l_j_str = maya_file_io::get_channel_date();
   nlohmann::json l_j{};
   try {
@@ -49,21 +63,51 @@ void cloth_sim::replace_ref_file() {
 
   ref_files_ |= ranges::action::remove_if(!boost::lambda2::_1);
 }
-void cloth_sim::create_cloth() {}
-void cloth_sim::sim() {}
+void cloth_sim::create_cloth() {
+  DOODLE_LOG_INFO("开始创建布料");
+  cloth_factory_interface l_cf{};
+  if (qcloth_factory::has_cloth())
+    l_cf = std::make_shared<qcloth_factory>();
+  else if (ncloth_factory::has_cloth())
+    l_cf = std::make_shared<ncloth_factory>();
+
+  if (!l_cf) return;
+
+  cloth_lists_ = l_cf->create_cloth();
+
+  ranges::for_each(cloth_lists_, [&](entt::handle& in_handle) {
+    auto l_c = in_handle.get<cloth_interface>();
+    l_c->add_collision({});
+    l_c->add_field({});
+    l_c->clear_cache();
+    l_c->rest();
+    l_c->set_cache_folder();
+  });
+}
+void cloth_sim::sim() {
+  DOODLE_LOG_INFO("开始解算");
+  const MTime k_end_time = MAnimControl::maxTime();
+  for (auto&& i = t_post_time_; i < k_end_time; ++i) {
+    maya_chick(MAnimControl::setCurrentTime(i));
+    ranges::for_each(cloth_lists_, [&](entt::handle& in_handle) {
+      auto l_c = in_handle.get<cloth_interface>();
+      l_c->sim_cloth();
+    });
+  }
+}
 
 void cloth_sim::play_blast() {
   DOODLE_LOG_INFO("开始排屏");
   class play_blast l_p {};
 
-  MTime k_end_time = MAnimControl::maxTime();
+  const MTime k_end_time = MAnimControl::maxTime();
   l_p.conjecture_camera();
   l_p.set_save_dir(maya_file_io::work_path() / "mov");
   l_p.conjecture_ep_sc();
   l_p.play_blast_(anim_begin_time_, k_end_time);
 }
 
-void cloth_sim::export_abc() {}
+void cloth_sim::export_abc() { DOODLE_LOG_INFO("开始导出abc"); }
 
-void cloth_sim::export_fbx() {}
+void cloth_sim::export_fbx() { DOODLE_LOG_INFO("开始导出fbx"); }
 }  // namespace doodle::maya_plug
