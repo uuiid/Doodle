de5d005e4a33b2f479de984ae2b5bfdd5dc0cef7
uuiid <957714080@qq.com>
2023-05-08 16:29:20 +0800

    兼容2018更改

--- src/exe_maya/facet/cloth_sim.cpp
+++ src/exe_maya/facet/cloth_sim.cpp
@@ -15,6 +15,8 @@
 
 #include "doodle_lib/exe_warp/maya_exe.h"
 
+#include <fmt/format.h>
+
 #ifdef fsin
 #undef fsin
 #endif
@@ -70,21 +72,26 @@ bool cloth_sim::post() {
   doodle_lib::Get().ctx().emplace<image_to_move>(std::make_shared<detail::image_to_move>());
   maya_chick(MGlobal::executeCommand(R"(loadPlugin "AbcExport";)"));
   maya_chick(MGlobal::executeCommand(R"(loadPlugin "AbcImport";)"));
+#if MAYA_API_VERSION >= 20190000
   MGlobal::executeCommand(d_str{fmt::format(R"(loadPlugin "qualoth_{}_x64")", MAYA_APP_VERSION)});
+#else
+  MGlobal::executeCommand(d_str{
+      fmt::format(R"(loadPlugin "qualoth_{}_x64")", fmt::to_string(MAYA_API_VERSION).substr(0, 4))});
+#endif
 
-  maya_file_io::set_workspace(l_arg.file_path);
+    maya_file_io::set_workspace(l_arg.file_path);
 
-  maya_file_io::open_file(l_arg.file_path);
+    maya_file_io::open_file(l_arg.file_path);
 
-  doodle_lib::Get().ctx().emplace<reference_file_factory>();
+    doodle_lib::Get().ctx().emplace<reference_file_factory>();
 
-  maya_chick(MAnimControl::setCurrentTime(MTime{boost::numeric_cast<std::double_t>(l_arg.t_post), MTime::uiUnit()}));
+    maya_chick(MAnimControl::setCurrentTime(MTime{boost::numeric_cast<std::double_t>(l_arg.t_post), MTime::uiUnit()}));
 
-  auto l_s = boost::asio::make_strand(g_io_context());
+    auto l_s = boost::asio::make_strand(g_io_context());
 
-  boost::asio::post(l_s, [l_s, this]() { this->create_ref_file(); });
+    boost::asio::post(l_s, [l_s, this]() { this->create_ref_file(); });
 
-  if ((l_arg.bitset_ & maya_exe_ns::flags::k_replace_ref_file).any()) {
+    if ((l_arg.bitset_ & maya_exe_ns::flags::k_replace_ref_file).any()) {
     DOODLE_LOG_INFO("安排替换引用");
     boost::asio::post(l_s, [l_s, this]() { this->replace_ref_file(); });
   }
