5f13424e07c1640fd6781564bcdeedae59d2ef88
uuiid <957714080@qq.com>
2023-06-07 16:52:35 +0800

    添加基本的导出命令

---
+++ src/maya_plug/maya_comm/ref_file_export.cpp
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/6/7.
+//
+
+#include "ref_file_export.h"
+
+namespace doodle {
+namespace maya_plug {}  // namespace maya_plug
+}  // namespace doodle
---
+++ src/maya_plug/maya_comm/ref_file_export.h
@@ -0,0 +1,12 @@
+//
+// Created by td_main on 2023/6/7.
+//
+#pragma once
+
+namespace doodle {
+namespace maya_plug {
+
+class ref_file_export {};
+
+}  // namespace maya_plug
+}  // namespace doodle
