fbd61d39a4c911b02a2370b6a3e3cc8ca9dc69da
uuiid <957714080@qq.com>
2023-06-08 09:55:44 +0800

    调整插件编译

--- script/uePlug/build_5.1.cmd
+++
@@ -1,6 +0,0 @@
-
-set UE_5_1="D:\Program Files\Epic Games\UE_5.2\Engine\Build\BatchFiles\RunUAT.bat"
-set work_dir=%~dp0
-call %UE_5_1% BuildPlugin -Plugin=%work_dir%Doodle\Doodle.uplugin -TargetPlatforms=Win64 -Package=%work_dir%5.2\Plugins\Doodle -Rocket
-rmdir /Q /S %work_dir%5.2\Plugins\Doodle\Intermediate
-rmdir /Q /S %work_dir%5.2\Plugins\Doodle\Source
--- script/uePlug/build_5.cmd
+++ script/uePlug/build_5.cmd
@@ -1,6 +1,6 @@
-set UE_VERSION=4.27
+set UE_VERSION=5.2
 set UE_5="D:\Program Files\Epic Games\UE_%UE_VERSION%\Engine\Build\BatchFiles\RunUAT.bat"
 set work_dir=%~dp0
-call %UE_5% BuildPlugin -Plugin=%work_dir%Doodle\Doodle.uplugin -TargetPlatforms=Win64 -Package=%work_dir%%UE_VERSION%\Plugins\Doodle -Rocket -VS2019
+call %UE_5% BuildPlugin -Plugin=%work_dir%Doodle\Doodle.uplugin -TargetPlatforms=Win64 -Package=%work_dir%%UE_VERSION%\Plugins\Doodle -Rocket
 rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Intermediate
 rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Source
