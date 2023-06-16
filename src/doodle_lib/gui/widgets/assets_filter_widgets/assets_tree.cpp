e43fefacaf9314234331e53ccba178850e012707
uuiid <957714080@qq.com>
2023-06-16 13:10:14 +0800

    添加树视图

---
+++ src/doodle_lib/gui/widgets/assets_filter_widgets/assets_tree.cpp
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/6/16.
+//
+
+#include "assets_tree.h"
+
+namespace doodle {
+namespace gui {}  // namespace gui
+}  // namespace doodle
---
+++ src/doodle_lib/gui/widgets/assets_filter_widgets/assets_tree.h
@@ -0,0 +1,13 @@
+//
+// Created by td_main on 2023/6/16.
+//
+
+#pragma once
+
+namespace doodle {
+namespace gui {
+
+class assets_tree {};
+
+}  // namespace gui
+}  // namespace doodle
