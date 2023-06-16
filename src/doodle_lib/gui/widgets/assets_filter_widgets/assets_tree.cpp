45d5a264005b6092c316c23a18f3cae62c7b8bc8
uuiid <957714080@qq.com>
2023-06-16 14:29:12 +0800

    基础的视图

--- src/doodle_lib/gui/widgets/assets_filter_widgets/assets_tree.cpp
+++ src/doodle_lib/gui/widgets/assets_filter_widgets/assets_tree.cpp
@@ -4,6 +4,46 @@
 
 #include "assets_tree.h"
 
-namespace doodle {
-namespace gui {}  // namespace gui
-}  // namespace doodle
+#include <doodle_core/configure/static_value.h>
+
+#include <doodle_app/lib_warp/imgui_warp.h>
+
+namespace doodle::gui {
+
+constexpr const static ImGuiTreeNodeFlags assets_tree_node_base_flags{
+    ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth};
+
+bool assets_tree::assets_tree_node::operator<(const doodle::gui::assets_tree::assets_tree_node &rhs) const {
+  return name < rhs.name;
+}
+
+bool assets_tree::render() {
+  for (auto it = tree_.begin_fixed(tree_.begin(), 0); it != tree_.end_fixed(tree_.begin(), 0); ++it) {
+    ImGuiTreeNodeFlags k_f{assets_tree_node_base_flags};
+    if (it->has_select) k_f |= ImGuiTreeNodeFlags_Selected;
+    if (it.number_of_children() != 0) k_f |= ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_Leaf;
+
+    if (const auto l_root_node = dear::TreeNodeEx{it->name.c_str(), assets_tree_node_base_flags}) {
+      if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen()) {
+        it->has_select = true;
+      }
+      if (auto l_popen_menu = dear::PopupContextItem{}) {
+        popen_menu(it);
+      }
+      if (auto l_drag_drop = dear::DragDropTarget{}) {
+        if (const auto *l_hs = ImGui::AcceptDragDropPayload(doodle_config::drop_handle_list.data()); l_hs) {
+          auto *l_list = reinterpret_cast<std::vector<entt::handle> *>(l_hs->Data);
+          for (auto &&l_h : *l_list) {
+            //          l_h.emplace_or_replace<assets>(i->data.data);
+          }
+          // @todo 这里要写拖拽
+        }
+      }
+      render_child(it);
+    }
+  }
+  return false;
+}
+void assets_tree::popen_menu(const tree<assets_tree_node>::iterator_base &in) {}
+bool assets_tree::render_child(const tree<assets_tree_node>::iterator &in) { return false; }
+}  // namespace doodle::gui
--- src/doodle_lib/gui/widgets/assets_filter_widgets/assets_tree.h
+++ src/doodle_lib/gui/widgets/assets_filter_widgets/assets_tree.h
@@ -3,11 +3,29 @@
 //
 
 #pragma once
+#include "boost/operators.hpp"
 
-namespace doodle {
-namespace gui {
+#include <string>
+#include <treehh/tree.hh>
+namespace doodle::gui {
 
-class assets_tree {};
+class assets_tree {
+  struct assets_tree_node : boost::less_than_comparable<assets_tree_node> {
+    std::string name;
+    bool has_select{};
 
-}  // namespace gui
-}  // namespace doodle
+    bool operator<(const assets_tree_node& rhs) const;
+  };
+  tree<assets_tree_node> tree_;
+
+  bool render_child(const tree<assets_tree_node>::iterator& in);
+
+  void popen_menu(const tree<assets_tree_node>::iterator_base& in);
+
+ public:
+  assets_tree() = default;
+
+  bool render();
+};
+
+}  // namespace doodle::gui
