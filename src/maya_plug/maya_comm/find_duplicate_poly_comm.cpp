08524010e3629d4c081e006ee0d5597e896bd07d
uuiid <957714080@qq.com>
2023-06-17 15:42:54 +0800

    调整判断

--- src/maya_plug/maya_comm/find_duplicate_poly_comm.cpp
+++ src/maya_plug/maya_comm/find_duplicate_poly_comm.cpp
@@ -59,12 +59,12 @@ MStatus find_duplicate_poly_comm::doIt(const MArgList& in_list) {
     //      if (l_name_space[0] == ':') l_name_space = l_name_space.substr(1);
     l_set_list.emplace(l_name_space);
   }
-  DOODLE_LOG_INFO("获取选中 {}", l_set_list);
+  DOODLE_LOG_INFO("获取引用 {} 获取选中 {}", l_ref_map, l_set_list);
 
   for (auto&& i_ns : l_set_list) {
     for (auto l_h : l_cloth) {
       auto l_c = l_h.get<cloth_interface>();
-      if (l_ref_map.find(i_ns) != l_ref_map.end()) l_c->rest(l_ref_map[i_ns]);
+      if (l_c->get_namespace() == i_ns) l_c->rest(l_ref_map[i_ns]);
     }
   }
 
