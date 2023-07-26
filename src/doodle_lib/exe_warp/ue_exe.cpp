0454d773c554481ecc7cfb5a4cdbb6b45ac6df17
uuiid <957714080@qq.com>
2023-07-26 11:28:45 +0800

    [灯光自动化] 添加添加ue 指令

---
+++ src/doodle_lib/exe_warp/ue_exe.cpp
@@ -0,0 +1,7 @@
+//
+// Created by td_main on 2023/7/26.
+//
+
+#include "ue_exe.h"
+
+namespace doodle {}  // namespace doodle
---
+++ src/doodle_lib/exe_warp/ue_exe.h
@@ -0,0 +1,11 @@
+//
+// Created by td_main on 2023/7/26.
+//
+
+#pragma once
+
+namespace doodle {
+
+class ue_exe {};
+
+}  // namespace doodle
