8207dbf6316531984fa8b5903b82027e8aa16db3
uuiid <957714080@qq.com>
2023-08-03 10:12:21 +0800

    [灯光自动化] 添加基础工作机

---
+++ src/doodle_lib/render_farm/working_machine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by td_main on 2023/8/3.
+//
+
+#include "working_machine.h"
---
+++ src/doodle_lib/render_farm/working_machine.h
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/8/3.
+//
+#pragma once
+namespace doodle::render_farm {
+
+class working_machine {};
+
+}  // namespace doodle::render_farm
