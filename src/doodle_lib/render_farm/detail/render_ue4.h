72640b399b54b715b5b44e3349082b1e54c29a74
uuiid <957714080@qq.com>
2023-08-09 10:03:19 +0800

    [灯光自动化] 添加接受输出路径

--- src/doodle_lib/render_farm/detail/render_ue4.h
+++ src/doodle_lib/render_farm/detail/render_ue4.h
@@ -17,11 +17,15 @@ class render_ue4 {
     std::string ProjectPath;
     std::string Args;
     std::string ManifestValue;
+
+    std::string out_file_path;
+
     // to json
     friend void to_json(nlohmann::json& j, const arg& in_arg) {
       j["projectPath"]   = in_arg.ProjectPath;
       j["args"]          = in_arg.Args;
       j["manifestValue"] = in_arg.ManifestValue;
+      j["outFilePath"]   = in_arg.out_file_path;
     }
 
     // form json
@@ -29,6 +33,7 @@ class render_ue4 {
       j.at("projectPath").get_to(in_arg.ProjectPath);
       j.at("args").get_to(in_arg.Args);
       j.at("manifestValue").get_to(in_arg.ManifestValue);
+      j.at("outFilePath").get_to(in_arg.out_file_path);
     }
   };
 
