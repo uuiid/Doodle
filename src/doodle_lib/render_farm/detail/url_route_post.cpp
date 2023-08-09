e2039bc4b9342eae003043da7496ba2bd195891b
uuiid <957714080@qq.com>
2023-08-09 15:54:16 +0800

    [灯光自动化] 添加http post 类

---
+++ src/doodle_lib/render_farm/detail/url_route_post.cpp
@@ -0,0 +1,7 @@
+//
+// Created by td_main on 2023/8/9.
+//
+
+#include "url_route_post.h"
+
+namespace doodle::render_farm {}  // namespace doodle::render_farm
---
+++ src/doodle_lib/render_farm/detail/url_route_post.h
@@ -0,0 +1,12 @@
+//
+// Created by td_main on 2023/8/9.
+//
+
+#pragma once
+namespace doodle {
+namespace render_farm {
+
+class url_route_post {};
+
+}  // namespace render_farm
+}  // namespace doodle
