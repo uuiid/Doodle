44842475b3f27bb0800d097e2fed201645f36500
uuiid <957714080@qq.com>
2023-08-11 13:56:28 +0800

    [灯光自动化] 调整计算机队列

--- src/doodle_lib/render_farm/detail/computer.cpp
+++ src/doodle_lib/render_farm/detail/computer.cpp
@@ -4,6 +4,18 @@
 
 #include "computer.h"
 
+#include <doodle_core/doodle_core_fwd.h>
 namespace doodle {
-namespace render_farm {}  // namespace render_farm
+namespace render_farm {
+void computer::delay() {
+  status_ = computer_status::idle;
+  if (!timer_) {
+    timer_ = std::make_shared<boost::asio::system_timer>(boost::asio::make_strand(g_io_context()));
+  }
+  timer_->expires_from_now(std::chrono::seconds(5));
+  timer_->async_wait([this](auto e) {
+    if (!e) status_ = computer_status::lost;
+  });
+}
+}  // namespace render_farm
 }  // namespace doodle
--- src/doodle_lib/render_farm/detail/computer.h
+++ src/doodle_lib/render_farm/detail/computer.h
@@ -3,25 +3,41 @@
 //
 
 #pragma once
-#include <nlohmann/json.hpp>
+#include <boost/asio.hpp>
 
+#include <nlohmann/json.hpp>
 namespace doodle::render_farm {
+
+enum class computer_status : std::int32_t {
+  idle,   // 空转
+  busy,   // 忙碌
+  lost,   // 失联
+  error,  // 错误
+};
 
 class computer {
  public:
   computer()  = default;
   ~computer() = default;
 
+  [[nodiscard]] inline std::string name() const { return name_; }
+  inline void set_name(std::string in_name) { name_ = std::move(in_name); }
+
+  [[nodiscard]] inline computer_status status() const { return status_; }
+  inline void set_status(computer_status in_status) { status_ = in_status; }
+
+  // 延期
+  void delay();
+
   // to_json
   friend void to_json(nlohmann::json& j, const computer& p) { j["name"] = p.name_; }
   // from_json
   friend void from_json(const nlohmann::json& j, computer& p) { j.at("name").get_to(p.name_); }
 
-  [[nodiscard]] std::string name() const { return name_; }
-  void set_name(std::string in_name) { name_ = std::move(in_name); }
-
  private:
   std::string name_;
+  computer_status status_{computer_status::lost};
+  std::shared_ptr<boost::asio::system_timer> timer_;
 };
 
 }  // namespace doodle::render_farm
