9774f0ed6b7ebfae1d0be13fd098c966bafdd07b
uuiid <957714080@qq.com>
2023-08-11 16:03:06 +0800

    [灯光自动化] 调整ue4任务

--- src/doodle_lib/render_farm/detail/render_ue4.h
+++ src/doodle_lib/render_farm/detail/render_ue4.h
@@ -9,33 +9,35 @@
 #include <nlohmann/json.hpp>
 namespace doodle::render_farm {
 namespace detail {
+
+struct ue4_arg {
+ public:
+  std::string ProjectPath;
+  std::string Args;
+  std::string ManifestValue;
+
+  std::string out_file_path;
+
+  // to json
+  friend void to_json(nlohmann::json& j, const ue4_arg& in_arg) {
+    j["projectPath"]   = in_arg.ProjectPath;
+    j["args"]          = in_arg.Args;
+    j["manifestValue"] = in_arg.ManifestValue;
+    j["outFilePath"]   = in_arg.out_file_path;
+  }
+
+  // form json
+  friend void from_json(const nlohmann::json& j, ue4_arg& in_arg) {
+    j.at("projectPath").get_to(in_arg.ProjectPath);
+    j.at("args").get_to(in_arg.Args);
+    j.at("manifestValue").get_to(in_arg.ManifestValue);
+    j.at("outFilePath").get_to(in_arg.out_file_path);
+  }
+};
+
 class render_ue4 {
  public:
-  // 此处对于ue4的渲染器的参数进行了封装
-  struct arg {
-   public:
-    std::string ProjectPath;
-    std::string Args;
-    std::string ManifestValue;
-
-    std::string out_file_path;
-
-    // to json
-    friend void to_json(nlohmann::json& j, const arg& in_arg) {
-      j["projectPath"]   = in_arg.ProjectPath;
-      j["args"]          = in_arg.Args;
-      j["manifestValue"] = in_arg.ManifestValue;
-      j["outFilePath"]   = in_arg.out_file_path;
-    }
-
-    // form json
-    friend void from_json(const nlohmann::json& j, arg& in_arg) {
-      j.at("projectPath").get_to(in_arg.ProjectPath);
-      j.at("args").get_to(in_arg.Args);
-      j.at("manifestValue").get_to(in_arg.ManifestValue);
-      j.at("outFilePath").get_to(in_arg.out_file_path);
-    }
-  };
+  using arg = ue4_arg;
 
   explicit render_ue4(entt::handle self_handle, arg in_arg)
       : arg_(std::move(in_arg)), self_handle_(std::move(self_handle)) {
