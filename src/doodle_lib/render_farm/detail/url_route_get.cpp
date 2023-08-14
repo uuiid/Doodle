e35cda9c6144befa44655e1d7feb85554a7cabac
uuiid <957714080@qq.com>
2023-08-14 15:28:14 +0800

    [灯光自动化] 调整git方法

--- src/doodle_lib/render_farm/detail/url_route_get.cpp
+++ src/doodle_lib/render_farm/detail/url_route_get.cpp
@@ -11,22 +11,16 @@
 #include <doodle_lib/render_farm/detail/render_ue4.h>
 namespace doodle::render_farm::detail {
 
-http_method<boost::beast::http::verb::get>::http_method()
-    : map_action{
-          {"get_log"s, [this](const entt::handle& in_h, boost::urls::params_ref) { return get_log(in_h); }},
-          {"get_err"s, [this](const entt::handle& in_h, boost::urls::params_ref) { return get_err(in_h); }},
-          {"render_job"s, [this](const entt::handle&, boost::urls::params_ref) { return render_job(); }},
-          {"computer"s, [this](const entt::handle&, boost::urls::params_ref) { return computer_reg(); }},
-      } {}
-void http_method<boost::beast::http::verb::get>::run(const std::shared_ptr<working_machine_session>& in_session) {
-  auto [l_handle, l_method] = parser(chick_url(in_session->url_.segments()));
-  keep_alive_               = in_session->request_parser_.keep_alive();
+void http_method<boost::beast::http::verb::get>::run(const entt::handle& in_handle) {
+  auto& l_session           = in_handle.get<working_machine_session>();
+  auto [l_handle, l_method] = parser(chick_url(l_session.url_.segments()));
   if (map_action.count(l_method) == 0) {
     boost::beast::http::response<boost::beast::http::empty_body> l_response{boost::beast::http::status::not_found, 11};
     l_response.keep_alive(false);
-    in_session->send_response(boost::beast::http::message_generator{std::move(l_response)});
+    l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
   } else {
-    in_session->send_response(map_action.at(l_method)(l_handle, in_session->url_.params()));
+    in_handle.emplace<http_method_get_handle>(l_handle);
+    map_action.at(l_method)(in_handle);
   }
 }
 
@@ -56,12 +50,13 @@ std::tuple<entt::handle, std::string> http_method<boost::beast::http::verb::get>
   return {l_handle, *l_begin};
 }
 
-boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::render_job() const {
-  auto l_view = g_reg()->view<render_farm::render_ue4_ptr>();
-  auto l_ids  = l_view | ranges::to_vector;
+boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::render_job(const entt::handle& in_h) {
+  auto l_view     = g_reg()->view<render_farm::render_ue4_ptr>();
+  auto l_ids      = l_view | ranges::to_vector;
+  auto& l_session = in_h.get<working_machine_session>();
   boost::beast::http::response<basic_json_body> l_response{boost::beast::http::status::ok, 11};
   l_response.body() = l_ids;
-  l_response.keep_alive(keep_alive_);
+  l_response.keep_alive(l_session.request_parser_.keep_alive());
   l_response.insert(boost::beast::http::field::content_type, "application/json");
   return {std::move(l_response)};
 }
@@ -79,7 +74,8 @@ struct computer_tmp {
   }
 };
 }  // namespace
-boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::computer_reg() const {
+boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::computer_reg(const entt::handle& in_h
+) {
   auto l_view = g_reg()->view<render_farm::computer>().each();
   auto l_ids  = l_view | ranges::views::transform([](auto&& in_item) -> computer_tmp {
                  auto& l_computer = std::get<1>(in_item);
@@ -87,24 +83,25 @@ boost::beast::http::message_generator http_method<boost::beast::http::verb::get>
                }) |
                ranges::to_vector;
   boost::beast::http::response<basic_json_body> l_response{boost::beast::http::status::ok, 11};
+  auto& l_session   = in_h.get<working_machine_session>();
   l_response.body() = l_ids;
-  l_response.keep_alive(keep_alive_);
+  l_response.keep_alive(l_session.request_parser_.keep_alive());
   l_response.insert(boost::beast::http::field::content_type, "application/json");
   return {std::move(l_response)};
 }
 
-boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::get_err(const entt::handle& in_h
-) const {
+boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::get_err(const entt::handle& in_h) {
   boost::beast::http::response<boost::beast::http::string_body> l_response{boost::beast::http::status::ok, 11};
-  l_response.keep_alive(keep_alive_);
-  l_response.body() = in_h.get<process_message>().err();
+  auto& l_session = in_h.get<working_machine_session>();
+  l_response.keep_alive(l_session.request_parser_.keep_alive());
+  l_response.body() = in_h.get<http_method_get_handle>().handle_.get<process_message>().err();
   return l_response;
 }
-boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::get_log(const entt::handle& in_h
-) const {
+boost::beast::http::message_generator http_method<boost::beast::http::verb::get>::get_log(const entt::handle& in_h) {
   boost::beast::http::response<boost::beast::http::string_body> l_response{boost::beast::http::status::ok, 11};
-  l_response.keep_alive(keep_alive_);
-  l_response.body() = in_h.get<process_message>().log();
+  auto& l_session = in_h.get<working_machine_session>();
+  l_response.keep_alive(l_session.request_parser_.keep_alive());
+  l_response.body() = in_h.get<http_method_get_handle>().handle_.get<process_message>().log();
   return l_response;
 }
 
--- src/doodle_lib/render_farm/detail/url_route_get.h
+++ src/doodle_lib/render_farm/detail/url_route_get.h
@@ -2,6 +2,8 @@
 // Created by td_main on 2023/8/9.
 //
 #pragma once
+#include <doodle_core/doodle_core_fwd.h>
+
 #include <doodle_lib/render_farm/detail/basic_json_body.h>
 #include <doodle_lib/render_farm/detail/url_route_base.h>
 #include <doodle_lib/render_farm/working_machine_session.h>
@@ -13,22 +15,37 @@ namespace doodle::render_farm::detail {
 
 template <>
 class http_method<boost::beast::http::verb::get> {
-  using map_actin_type = std::map<
-      std::string, std::function<boost::beast::http::message_generator(const entt::handle&, boost::urls::params_ref)>>;
+  using map_actin_type = std::map<std::string, std::function<void(const entt::handle&)>>;
   const map_actin_type map_action;
 
-  bool keep_alive_;
+  using http_method_get_handle = doodle::detail::entt_handle<entt::tag<"http_method_get_handle"_hs>>;
 
  public:
-  http_method();
+  http_method(map_actin_type in_map_actin_type) : map_action{std::move(in_map_actin_type)} {};
+  // http_method()
+  //      : map_action{
+  //            {"get_log"s, [this](const entt::handle& in_h, boost::urls::params_ref) { return get_log(in_h); }},
+  //            {"get_err"s, [this](const entt::handle& in_h, boost::urls::params_ref) { return get_err(in_h); }},
+  //            {"render_job"s, [this](const entt::handle&, boost::urls::params_ref) { return render_job(); }},
+  //            {"computer"s, [this](const entt::handle&, boost::urls::params_ref) { return computer_reg(); }},
+  //        } {}
 
-  void run(const std::shared_ptr<working_machine_session>& in_session);
+  inline static auto make_server() {
+    return http_method{
+        {"get_log"s, [](const entt::handle& in_h) { return get_log(in_h); }},
+        {"get_err"s, [](const entt::handle& in_h) { return get_err(in_h); }},
+        {"render_job"s, [](const entt::handle& in_h) { return render_job(in_h); }},
+        {"computer"s, [](const entt::handle& in_h) { return computer_reg(in_h); }},
+    };
+  }
 
-  [[nodiscard("")]] boost::beast::http::message_generator get_log(const entt::handle& in_h) const;
+  void run(const entt::handle& in_session);
 
-  [[nodiscard("")]] boost::beast::http::message_generator get_err(const entt::handle& in_h) const;
-  [[nodiscard("")]] boost::beast::http::message_generator render_job() const;
-  [[nodiscard("")]] boost::beast::http::message_generator computer_reg() const;
+  [[nodiscard("")]] static boost::beast::http::message_generator get_log(const entt::handle& in_h);
+
+  [[nodiscard("")]] static boost::beast::http::message_generator get_err(const entt::handle& in_h);
+  [[nodiscard("")]] static boost::beast::http::message_generator render_job(const entt::handle& in_h);
+  [[nodiscard("")]] static boost::beast::http::message_generator computer_reg(const entt::handle& in_h);
 
   static std::tuple<entt::handle, std::string> parser(
       const std::pair<boost::urls::segments_ref::iterator, boost::urls::segments_ref::iterator>& in_segments
