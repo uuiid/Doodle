8bfc9836040559552acb10d3f425e826fc5f5372
uuiid <957714080@qq.com>
2023-08-14 14:58:33 +0800

    [灯光自动化] 调整调整post方法

--- src/doodle_lib/render_farm/detail/url_route_post.cpp
+++ src/doodle_lib/render_farm/detail/url_route_post.cpp
@@ -7,70 +7,78 @@
 #include <doodle_core/doodle_core_fwd.h>
 #include <doodle_core/exception/exception.h>
 
+#include <doodle_lib/render_farm/detail/aync_read_body.h>
 #include <doodle_lib/render_farm/detail/computer.h>
+#include <doodle_lib/render_farm/detail/forward_to_server.h>
 #include <doodle_lib/render_farm/detail/render_ue4.h>
+#include <doodle_lib/render_farm/detail/ue4_task.h>
 namespace doodle::render_farm {
 namespace detail {
 
-void http_method<boost::beast::http::verb::post>::run(std::shared_ptr<working_machine_session> in_session) {
-  auto l_m = parser(chick_url(in_session->url_.segments()));
+void http_method<boost::beast::http::verb::post>::run(const entt::handle& in_handle) {
+  auto& l_session = in_handle.get<working_machine_session>();
+  auto l_m        = parser(chick_url(l_session.url_.segments()));
 
   if (map_action.count(l_m) == 0) {
     boost::beast::http::response<boost::beast::http::empty_body> l_response{boost::beast::http::status::not_found, 11};
-    in_session->send_response(boost::beast::http::message_generator{std::move(l_response)});
+    l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
   } else {
-    map_action.at(l_m)(in_session, in_session->url_.params());
+    map_action.at(l_m)(in_handle);
   }
 }
 
-void http_method<boost::beast::http::verb::post>::render_job(std::shared_ptr<working_machine_session> in_session) {
+void http_method<boost::beast::http::verb::post>::render_job(const entt::handle& in_handle) {
+  auto& l_session        = in_handle.get<working_machine_session>();
   using json_parser_type = boost::beast::http::request_parser<detail::basic_json_body>;
-  auto l_parser_ptr      = std::make_shared<json_parser_type>(std::move(in_session->request_parser_));
+  auto l_parser_ptr      = std::make_shared<json_parser_type>(std::move(l_session.request_parser_));
   boost::beast::http::async_read(
-      in_session->stream_, in_session->buffer_, *l_parser_ptr,
-      [self = in_session, l_parser_ptr](boost::system::error_code ec, std::size_t bytes_transferred) {
+      l_session.stream_, l_session.buffer_, *l_parser_ptr,
+      [in_handle, l_parser_ptr](boost::system::error_code ec, std::size_t bytes_transferred) {
         boost::ignore_unused(bytes_transferred);
+        auto& l_session = in_handle.get<working_machine_session>();
         if (ec == boost::beast::http::error::end_of_stream) {
-          return self->do_close();
+          return l_session.do_close();
         }
         if (ec) {
           DOODLE_LOG_ERROR("on_read error: {}", ec.message());
-          self->send_error_code(ec);
+          l_session.send_error_code(ec);
           return;
         }
 
         auto l_h = entt::handle{*g_reg(), g_reg()->create()};
         l_h.emplace<process_message>();
         try {
-          l_h.emplace<render_ue4_ptr>(std::make_shared<render_ue4_ptr ::element_type>(
-              l_h, l_parser_ptr->release().body().get<render_ue4_ptr::element_type::arg>()
+          l_h.emplace<ue4_task_ptr>(std::make_shared<ue4_task_ptr::element_type>(
+              l_h, l_parser_ptr->release().body().get<ue4_task_ptr::element_type::arg>()
           ));
         } catch (const nlohmann::json::exception& e) {
           DOODLE_LOG_ERROR("json parse error: {}", e.what());
-          self->send_error(e);
+          l_session.send_error(e);
           return;
         }
 
         boost::beast::http::response<detail::basic_json_body> l_response{boost::beast::http::status::ok, 11};
         l_response.body() = {{"state", "ok"}, {"id", l_h.entity()}};
         l_response.keep_alive(l_parser_ptr->get().keep_alive());
-        self->send_response(boost::beast::http::message_generator{std::move(l_response)});
+        l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
       }
   );
 }
-void http_method<boost::beast::http::verb::post>::computer_reg(std::shared_ptr<working_machine_session> in_session) {
+void http_method<boost::beast::http::verb::post>::computer_reg(const entt::handle& in_handle) {
+  auto& l_session        = in_handle.get<working_machine_session>();
   using json_parser_type = boost::beast::http::request_parser<detail::basic_json_body>;
-  auto l_parser_ptr      = std::make_shared<json_parser_type>(std::move(in_session->request_parser_));
+  auto l_parser_ptr      = std::make_shared<json_parser_type>(std::move(l_session.request_parser_));
   boost::beast::http::async_read(
-      in_session->stream_, in_session->buffer_, *l_parser_ptr,
-      [self = in_session, l_parser_ptr](boost::system::error_code ec, std::size_t bytes_transferred) {
+      l_session.stream_, l_session.buffer_, *l_parser_ptr,
+      [in_handle, l_parser_ptr](boost::system::error_code ec, std::size_t bytes_transferred) {
         boost::ignore_unused(bytes_transferred);
+        auto& l_session = in_handle.get<working_machine_session>();
         if (ec == boost::beast::http::error::end_of_stream) {
-          return self->do_close();
+          return l_session.do_close();
         }
         if (ec) {
           DOODLE_LOG_ERROR("on_read error: {}", ec.message());
-          self->send_error_code(ec);
+          l_session.send_error_code(ec);
           return;
         }
 
@@ -89,7 +97,7 @@ void http_method<boost::beast::http::verb::post>::computer_reg(std::shared_ptr<w
             });
           } catch (const nlohmann::json::exception& e) {
             DOODLE_LOG_ERROR("json parse error: {}", e.what());
-            self->send_error(e);
+            l_session.send_error(e);
             return;
           }
         }
@@ -105,8 +113,56 @@ void http_method<boost::beast::http::verb::post>::computer_reg(std::shared_ptr<w
         boost::beast::http::response<basic_json_body> l_response{boost::beast::http::status::ok, 11};
         l_response.keep_alive(l_parser_ptr->keep_alive());
         l_response.body() = {{"state", "ok"}, {"id", l_handle.entity()}};
-        self->send_response(boost::beast::http::message_generator{std::move(l_response)});
+        l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
       }
+  );
+}
+void http_method<boost::beast::http::verb::post>::run_job(const entt::handle& in_handle) {
+  using json_parser_type = boost::beast::http::request_parser<detail::basic_json_body>;
+  auto& l_session        = in_handle.get<working_machine_session>();
+  auto l_parser_ptr      = std::make_shared<json_parser_type>(std::move(l_session.request_parser_));
+  boost::beast::http::async_read(
+      l_session.stream_, l_session.buffer_, *l_parser_ptr,
+      [in_handle, l_parser_ptr](boost::system::error_code ec, std::size_t bytes_transferred) {
+        boost::ignore_unused(bytes_transferred);
+        auto& l_session = in_handle.get<working_machine_session>();
+        if (ec == boost::beast::http::error::end_of_stream) {
+          return l_session.do_close();
+        }
+        if (ec) {
+          DOODLE_LOG_ERROR("on_read error: {}", ec.message());
+          l_session.send_error_code(ec);
+          return;
+        }
+
+        auto l_json = l_parser_ptr->release().body();
+
+        auto l_h    = entt::handle{*g_reg(), g_reg()->create()};
+        l_h.emplace<process_message>();
+        l_h.emplace<ue_server_id>(l_json["id"].get<entt::entity>());
+        try {
+          l_h.emplace<ue4_task_ptr>(std::make_shared<render_ue4_ptr::element_type>(
+              l_h, l_json["arg"].get<render_ue4_ptr::element_type::arg>()
+          ));
+        } catch (const nlohmann::json::exception& e) {
+          DOODLE_LOG_ERROR("json parse error: {}", e.what());
+          l_session.send_error(e);
+          return;
+        }
+
+        boost::beast::http::response<basic_json_body> l_response{boost::beast::http::status::ok, 11};
+        l_response.keep_alive(l_parser_ptr->keep_alive());
+        l_response.body() = {{"state", "ok"}};
+        l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
+      }
+  );
+}
+void http_method<boost::beast::http::verb::post>::client_submit_job(const entt::handle& in_handle) {
+  using parser_type = boost::beast::http::request_parser<boost::beast::http::string_body>;
+  auto& l_session   = in_handle.get<working_machine_session>();
+  auto l_parser_ptr = std::make_shared<parser_type>(std::move(l_session.request_parser_));
+  boost::beast::http::async_read(
+      l_session.stream_, l_session.buffer_, *l_parser_ptr, in_handle.emplace<forward_to_server>(in_handle, l_parser_ptr)
   );
 }
 
--- src/doodle_lib/render_farm/detail/url_route_post.h
+++ src/doodle_lib/render_farm/detail/url_route_post.h
@@ -17,31 +17,52 @@ namespace detail {
 
 template <>
 class http_method<boost::beast::http::verb::post> {
-  using map_action_type =
-      std::map<std::string, std::function<void(std::shared_ptr<working_machine_session>, boost::urls::params_ref)>>;
+  using map_action_type = std::map<std::string, std::function<void(const entt::handle &)>>;
   const map_action_type map_action;
 
  public:
-  http_method()
-      : map_action{
-            {"render_job"s, [](std::shared_ptr<working_machine_session> in_session,
-                               boost::urls::params_ref) { return render_job(std::move(in_session)); }},
+  http_method(map_action_type in_map_action) : map_action{std::move(in_map_action)} {}
+  void run(const entt::handle &in_session);
 
-            {"computer"s, [](std::shared_ptr<working_machine_session> in_session, boost::urls::params_ref) {
-               return computer_reg(std::move(in_session));
-             }}} {}
-  void run(std::shared_ptr<working_machine_session> in_session);
+  inline static auto make_client() {
+    return http_method{
+        {{"client_submit_job"s, [](const entt::handle &in_handle) { return client_submit_job(in_handle); }}}};
+  }
+  inline static auto make_server() {
+    return http_method{
+        {{"render_job"s, [](const entt::handle &in_handle) { return render_job(in_handle); }},
+         {"computer_reg"s, [](const entt::handle &in_handle) { return computer_reg(in_handle); }}}};
+  }
+  inline static auto make_work() {
+    return http_method{
+        {{"computer_reg"s, [](const entt::handle &in_handle) { return computer_reg(in_handle); }},
+         {"run_job", [](const entt::handle &in_handle) { return run_job(in_handle); }}}};
+  }
 
   inline static std::string parser(
-      const std::pair<boost::urls::segments_ref::iterator, boost::urls::segments_ref::iterator>& in_segments
+      const std::pair<boost::urls::segments_ref::iterator, boost::urls::segments_ref::iterator> &in_segments
   ) {
     auto [l_begin, l_end] = in_segments;
     return *l_begin;
   }
 
-  static void render_job(std::shared_ptr<working_machine_session> in_session);
+  static void render_job(const entt::handle &in_handle);
+
+  static void client_submit_job(const entt::handle &in_handle);
+
   // 计算机注册
-  static void computer_reg(std::shared_ptr<working_machine_session> in_session);
+  static void computer_reg(const entt::handle &in_handle);
+  /**
+   * @brief 运行任务
+   * @param in_handle 会话
+   *
+   * 传入的json
+   * json["id'] -> entt::entity
+   * json["arg"] -> ue_render::arg
+   *
+   */
+
+  static void run_job(const entt::handle &in_handle);
 };
 }  // namespace detail
 }  // namespace doodle::render_farm
