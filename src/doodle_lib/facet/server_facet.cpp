bed4b0487ad85b33cb525f3fd4d96eab9c5493ac
uuiid <957714080@qq.com>
2023-08-15 09:38:54 +0800

    [灯光自动化] 添加服务器构面

---
+++ src/doodle_lib/facet/server_facet.cpp
@@ -0,0 +1,7 @@
+//
+// Created by td_main on 2023/8/15.
+//
+
+#include "server_facet.h"
+
+namespace doodle {}  // namespace doodle
---
+++ src/doodle_lib/facet/server_facet.h
@@ -0,0 +1,10 @@
+//
+// Created by td_main on 2023/8/15.
+//
+
+#pragma once
+namespace doodle {
+
+class server_facet {};
+
+}  // namespace doodle
