370bbd0eba5cefa42da472afbf41a62a4030463b
uuiid <957714080@qq.com>
2023-08-16 10:32:25 +0800

    [灯光自动化] 去除无用的测试

--- src/doodle_lib/render_farm/detail/basic_json_body.cpp
+++ src/doodle_lib/render_farm/detail/basic_json_body.cpp
@@ -7,31 +7,4 @@
 #include <doodle_core/exception/exception.h>
 
 #include <boost/url.hpp>
-namespace doodle::render_farm::detail {
-std::pair<boost::urls::segments_ref::iterator, boost::urls::segments_ref::iterator> chick_url(
-    boost::urls::segments_ref in_segments_ref
-) {
-  auto l_begin = in_segments_ref.begin();
-  auto l_end   = in_segments_ref.end();
-  if (l_begin == l_end) {
-    throw_exception(doodle_error{"url error"});
-  }
-
-  if (*l_begin != "v1") {
-    throw_exception(doodle_error{"url version error"});
-  }
-  ++l_begin;
-  if (l_begin == l_end) {
-    throw_exception(doodle_error{"url not found render_farm"});
-  }
-  if (*l_begin != "render_farm") {
-    throw_exception(doodle_error{"url not found render_farm"});
-  }
-  ++l_begin;
-  if (l_begin == l_end) {
-    throw_exception(doodle_error{"url not found action"});
-  }
-  return {l_begin, l_end};
-}
-
-}  // namespace doodle::render_farm::detail
+namespace doodle::render_farm::detail {}  // namespace doodle::render_farm::detail
--- src/doodle_lib/render_farm/detail/url_route_base.h
+++ src/doodle_lib/render_farm/detail/url_route_base.h
@@ -9,9 +9,6 @@
 #include <boost/url.hpp>
 namespace doodle::render_farm {
 namespace detail {
-std::pair<boost::urls::segments_ref::iterator, boost::urls::segments_ref::iterator> chick_url(
-    boost::urls::segments_ref in_segments_ref
-);
 
 class http_route {
  public:
