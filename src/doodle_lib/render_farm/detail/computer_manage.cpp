0e784a7f2c6c4c3b5c02d1ac7fc6ce2a6c2ed0bb
uuiid <957714080@qq.com>
2023-08-16 15:32:46 +0800

    [灯光自动化] 添加computer 管理

---
+++ src/doodle_lib/render_farm/detail/computer_manage.cpp
@@ -0,0 +1,25 @@
+//
+// Created by td_main on 2023/8/16.
+//
+
+#include "computer_manage.h"
+
+#include <doodle_core/doodle_core_fwd.h>
+
+#include <doodle_lib/render_farm/detail/computer.h>
+namespace doodle {
+namespace render_farm {
+void computer_manage::run() {
+  timer_ = std::make_shared<timer>(g_io_context());
+  timer_->expires_from_now(1s);
+  timer_->async_wait([this](const boost::system::error_code& ec) {
+    for (auto&& [e, computer] : g_reg()->view<computer>().each()) {
+      if (computer.status() == computer_status::idle) {
+        computer.set_status(computer_status::lost);
+      }
+    }
+    this->run();
+  });
+}
+}  // namespace render_farm
+}  // namespace doodle
