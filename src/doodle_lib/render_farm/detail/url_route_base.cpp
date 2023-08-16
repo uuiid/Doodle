3adf421dc64214d59a2197847c640e50a02d202e
uuiid <957714080@qq.com>
2023-08-16 10:15:22 +0800

    [灯光自动化] 添加路由

--- src/doodle_lib/render_farm/detail/url_route_base.cpp
+++ src/doodle_lib/render_farm/detail/url_route_base.cpp
@@ -5,4 +5,45 @@
 #include "url_route_base.h"
 
 #include <doodle_core/exception/exception.h>
-namespace doodle::render_farm::detail {}  // namespace doodle::render_farm::detail
+namespace doodle::render_farm::detail {
+
+void http_route::capture_url::set_cap_bit() {
+  for (const auto& l_str : capture_vector_) {
+    if (l_str.front() == '{' && l_str.back() == '}') {
+      capture_bitset_.push_back(true);
+    } else {
+      capture_bitset_.push_back(false);
+    }
+  }
+}
+std::tuple<bool, std::map<std::string, std::string>> http_route::capture_url::match_url(
+    boost::urls::segments_ref in_segments_ref
+) const {
+  bool l_result = false;
+  auto l_it     = in_segments_ref.begin();
+  std::map<std::string, std::string> l_str{};
+  for (auto i = 0; l_it != in_segments_ref.end(); ++l_it, ++i) {
+    if (capture_bitset_[i]) {
+      l_str.emplace(capture_vector_[i].substr(1, capture_vector_[i].size() - 1), *l_it);
+    } else {
+      if (capture_vector_[i] != *l_it) {
+        break;
+      }
+    }
+  }
+  return {l_result, l_str};
+}
+
+void http_route::capture_url::operator()(boost::urls::segments_ref in_segments_ref, const entt::handle& in_session) {
+  auto [l_result, l_map] = match_url(in_segments_ref);
+  if (l_result) {
+    action_(in_session, l_map);
+  }
+}
+void http_route::reg(
+    boost::beast::http::verb in_verb, std::vector<std::string> in_vector,
+    http_route::capture_url::action_type in_function
+) {
+  actions[in_verb].emplace_back(std::move(in_vector), std::move(in_function));
+}
+}  // namespace doodle::render_farm::detail
--- src/doodle_lib/render_farm/detail/url_route_base.h
+++ src/doodle_lib/render_farm/detail/url_route_base.h
@@ -5,12 +5,45 @@
 #pragma once
 
 #include <boost/beast.hpp>
+#include <boost/dynamic_bitset.hpp>
 #include <boost/url.hpp>
 namespace doodle::render_farm {
 namespace detail {
 std::pair<boost::urls::segments_ref::iterator, boost::urls::segments_ref::iterator> chick_url(
     boost::urls::segments_ref in_segments_ref
 );
+
+struct http_route {
+ public:
+  class capture_url {
+    std::vector<std::string> capture_vector_;
+    boost::dynamic_bitset<> capture_bitset_{};
+
+    void set_cap_bit();
+    std::tuple<bool, std::map<std::string, std::string>> match_url(boost::urls::segments_ref in_segments_ref) const;
+
+   public:
+    using action_type = std::function<void(const entt::handle&, const std::map<std::string, std::string>&)>;
+
+    action_type action_;
+    explicit capture_url(std::vector<std::string> in_vector, action_type in_function)
+        : capture_vector_{std::move(in_vector)}, action_{std::move(in_function)} {
+      set_cap_bit();
+    };
+
+    void operator()(boost::urls::segments_ref in_segments_ref, const entt::handle& in_session);
+  };
+
+ private:
+  using map_actin_type = std::vector<capture_url>;
+  std::map<boost::beast::http::verb, map_actin_type> actions;
+
+ public:
+  // 注册路由
+  void reg(boost::beast::http::verb in_verb, std::vector<std::string> in_vector, capture_url::action_type in_function);
+  // 路由分发
+  void operator()(boost::beast::http::verb in_verb, const entt::handle& in_session);
+};
 
 }  // namespace detail
 }  // namespace doodle::render_farm
