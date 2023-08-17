796714bfea1107c77545166b346f1b35a44a2bbf
uuiid <957714080@qq.com>
2023-08-17 17:10:37 +0800

    [灯光自动化] 调整为可复制数据结构

--- src/doodle_lib/render_farm/detail/forward_to_server.cpp
+++ src/doodle_lib/render_farm/detail/forward_to_server.cpp
@@ -11,7 +11,7 @@ namespace render_farm {
 namespace detail {
 void forward_to_server::operator()(boost::system::error_code ec, std::size_t bytes_transferred) {
   boost::ignore_unused(bytes_transferred);
-  auto& l_session = handle_.get<working_machine_session>();
+  auto& l_session = ptr_->handle_.get<working_machine_session>();
   if (ec) {
     DOODLE_LOG_ERROR("forward_to_server error:{}", ec.message());
     l_session.send_error_code(ec);
@@ -29,43 +29,45 @@ void forward_to_server::operator()(boost::system::error_code ec, std::size_t byt
   }
 
   boost::asio::ip::tcp::resolver resolver{l_session.stream().get_executor()};
-  stream_              = std::make_shared<boost::beast::tcp_stream>(l_session.stream().get_executor());
+  ptr_->stream_        = std::make_shared<boost::beast::tcp_stream>(l_session.stream().get_executor());
   auto const l_results = resolver.resolve(l_server_ip, "50021");
-  stream_->connect(l_results);
-  auto l_body = parser_->release();
+  ptr_->stream_->connect(l_results);
+  auto l_body       = ptr_->parser_->release();
   auto l_data = l_body.body();
-  request_    = std::make_shared<request_ptr::element_type>(std::move(l_body));
-  request_->target("/v1/render_farm/render_job");
-  request_->body() = l_data;
-  request_->keep_alive(false);
-  request_->prepare_payload();
-  boost::beast::http::async_write(*stream_, *request_, bind_reg_handler(&forward_to_server::on_write, g_reg(), this));
+  ptr_->request_    = std::make_shared<request_ptr::element_type>(std::move(l_body));
+  ptr_->request_->target("/v1/render_farm/render_job");
+  ptr_->request_->body() = l_data;
+  ptr_->request_->keep_alive(false);
+  ptr_->request_->prepare_payload();
+  boost::beast::http::async_write(
+      *ptr_->stream_, *ptr_->request_, bind_reg_handler(&forward_to_server::on_write, g_reg(), this)
+  );
 }
 void forward_to_server::on_write(boost::system::error_code ec, std::size_t bytes_transferred) {
   boost::ignore_unused(bytes_transferred);
   if (ec) {
     DOODLE_LOG_ERROR("forward_to_server error:{}", ec.message());
-    auto& l_session = handle_.get<working_machine_session>();
+    auto& l_session = ptr_->handle_.get<working_machine_session>();
     l_session.send_error(ec);
     return;
   }
 
   boost::beast::http::async_read(
-      *stream_, buffer_, response_, bind_reg_handler(&forward_to_server::on_read, g_reg(), this)
+      *ptr_->stream_, ptr_->buffer_, ptr_->response_, bind_reg_handler(&forward_to_server::on_read, g_reg(), this)
   );
 }
 void forward_to_server::on_read(boost::system::error_code ec, std::size_t bytes_transferred) {
   boost::ignore_unused(bytes_transferred);
   if (ec) {
     DOODLE_LOG_ERROR("forward_to_server error:{}", ec.message());
-    auto& l_session = handle_.get<working_machine_session>();
+    auto& l_session = ptr_->handle_.get<working_machine_session>();
     l_session.send_error_code(ec);
-    stream_->close();
+    ptr_->stream_->close();
     return;
   }
-  auto& l_session = handle_.get<working_machine_session>();
+  auto& l_session = ptr_->handle_.get<working_machine_session>();
   boost::beast::http::response<boost::beast::http::string_body> l_response{boost::beast::http::status::ok, 11};
-  l_response.body() = response_.body();
+  l_response.body() = ptr_->response_.body();
   l_response.keep_alive(false);
   l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
 }
--- src/doodle_lib/render_farm/detail/forward_to_server.h
+++ src/doodle_lib/render_farm/detail/forward_to_server.h
@@ -14,17 +14,23 @@ class forward_to_server {
   using request_parser_ptr = std::shared_ptr<boost::beast::http::request_parser<boost::beast::http::string_body>>;
   using request_ptr        = std::shared_ptr<boost::beast::http::request<boost::beast::http::string_body>>;
   explicit forward_to_server(entt::handle in_session, request_parser_ptr in_parser)
-      : handle_(std::move(in_session)), parser_(std::move(in_parser)) {}
+      : ptr_{std::make_shared<data_type>()} {
+    ptr_->handle_ = in_session;
+    ptr_->parser_ = std::move(in_parser);
+  }
   ~forward_to_server() = default;
   void operator()(boost::system::error_code ec, std::size_t bytes_transferred);
 
  private:
-  entt::handle handle_;
-  request_parser_ptr parser_;
-  request_ptr request_;
-  std::shared_ptr<boost::beast::tcp_stream> stream_;
-  boost::beast::http::response<boost::beast::http::string_body> response_;
-  boost::beast::flat_buffer buffer_;
+  struct data_type {
+    entt::handle handle_;
+    request_parser_ptr parser_;
+    request_ptr request_;
+    std::shared_ptr<boost::beast::tcp_stream> stream_;
+    boost::beast::http::response<boost::beast::http::string_body> response_;
+    boost::beast::flat_buffer buffer_;
+  };
+  std::shared_ptr<data_type> ptr_;
 
   void on_write(boost::system::error_code ec, std::size_t bytes_transferred);
   void on_read(boost::system::error_code ec, std::size_t bytes_transferred);
