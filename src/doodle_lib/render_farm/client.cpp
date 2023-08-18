939e3c6164a74b276648ab0469e5ba9dce9866c0
uuiid <957714080@qq.com>
2023-08-18 13:34:19 +0800

    [灯光自动化] 添加客户端

---
+++ src/doodle_lib/render_farm/client.cpp
@@ -0,0 +1,57 @@
+//
+// Created by td_main on 2023/8/18.
+//
+
+#include "client.h"
+
+#include <doodle_lib/core/bind_front_handler.h>
+
+#include <boost/asio.hpp>
+namespace doodle {
+
+void client::run() {
+  if (ptr_->connect_count_ > 3) {
+    DOODLE_LOG_INFO("连接服务器失败");
+    return;
+  }
+  //  boost::asio::bind_cancellation_slot();
+  ++ptr_->connect_count_;
+  ptr_->timer_.expires_from_now(10s);
+  boost::asio::async_connect(
+      ptr_->socket_, boost::asio::ip::tcp::resolver(g_io_context()).resolve(ptr_->server_ip_, "50021"),
+      bind_reg_handler(&client::on_connect, g_reg(), this)
+  );
+  ptr_->timer_.async_wait(bind_reg_handler(&client::on_connect_timeout, g_reg(), this));
+}
+void client::on_connect(boost::system::error_code ec, boost::asio::ip::tcp::endpoint endpoint) {
+  boost::ignore_unused(endpoint);
+  if (ec) {
+    DOODLE_LOG_INFO("{}", ec.message());
+    return;
+  }
+  DOODLE_LOG_INFO("连接成功服务器");
+}
+void client::on_connect_timeout() {
+  ptr_->socket_.cancel();
+  DOODLE_LOG_INFO("连接服务器超时");
+  run();
+}
+void client::rest_run() {
+  ptr_->connect_count_ = 0;
+  run();
+}
+void client::list_task() {
+  if (!ptr_->socket_.is_open()) {
+    DOODLE_LOG_INFO("socket is not open");
+    return;
+  }
+
+  ptr_->request_ = boost::beast::http::request<render_farm::detail::basic_json_body>{
+      boost::beast::http::verb::get, "/v1/render_farm/render_job", 11};
+  ptr_->request_.keep_alive(true);
+  ptr_->request_.set(boost::beast::http::field::host, fmt::format("{}:50021", ptr_->server_ip_));
+  ptr_->request_.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+  ptr_->request_.set(boost::beast::http::field::content_type, "application/json");
+  ptr_->request_.set(boost::beast::http::field::accept, "application/json");
+}
+}  // namespace doodle
---
+++ src/doodle_lib/render_farm/client.h
@@ -0,0 +1,51 @@
+//
+// Created by td_main on 2023/8/18.
+//
+#pragma once
+#include <doodle_lib/doodle_lib_fwd.h>
+#include <doodle_lib/render_farm/detail/basic_json_body.h>
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+namespace doodle {
+class client {
+ private:
+  struct data_type {
+    std::string server_ip_;
+    boost::asio::ip::tcp::socket socket_{g_io_context()};
+    boost::asio::system_timer timer_{g_io_context()};
+    boost::beast::flat_buffer buffer_;
+    boost::beast::http::response<render_farm::detail::basic_json_body> response;
+    boost::beast::http::request<render_farm::detail::basic_json_body> request_;
+    std::int32_t connect_count_{};
+  };
+  std::shared_ptr<data_type> ptr_;
+
+ public:
+  explicit client(std::string in_server_ip) : ptr_(std::make_shared<data_type>()) {
+    ptr_->server_ip_ = std::move(in_server_ip);
+  }
+  ~client() = default;
+
+  // run
+  void run();
+
+  // rest run
+  void rest_run();
+
+  // server_ip
+  [[nodiscard]] inline std::string& server_ip() { return ptr_->server_ip_; }
+  [[nodiscard]] inline const std::string& server_ip() const { return ptr_->server_ip_; }
+
+  void list_task();
+
+  inline void server_ip(std::string in_server_ip) { ptr_->server_ip_ = std::move(in_server_ip); }
+
+ private:
+  // on_connect
+  void on_connect(boost::system::error_code ec, boost::asio::ip::tcp::endpoint endpoint);
+  // on_connect_timeout
+  void on_connect_timeout();
+};
+
+}  // namespace doodle
