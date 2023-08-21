a947eaabaff7d33f83f78416762c741fbbe3faf8
uuiid <957714080@qq.com>
2023-08-21 09:41:51 +0800

    [灯光自动化] 添加工作端

---
+++ src/doodle_lib/render_farm/work.cpp
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/8/21.
+//
+
+#include "work.h"
+
+namespace doodle {
+namespace render_farm {}  // namespace render_farm
+}  // namespace doodle
---
+++ src/doodle_lib/render_farm/work.h
@@ -0,0 +1,11 @@
+//
+// Created by td_main on 2023/8/21.
+//
+#include <doodle_lib/doodle_lib_fwd.h>
+namespace doodle {
+namespace render_farm {
+
+class work {};
+
+}  // namespace render_farm
+}  // namespace doodle
