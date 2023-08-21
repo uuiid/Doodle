476a9753d357936af169bbc5a3231417438ff5cb
uuiid <957714080@qq.com>
2023-08-21 10:11:11 +0800

    [灯光自动化] 添加工作端

--- src/doodle_lib/render_farm/work.cpp
+++ src/doodle_lib/render_farm/work.cpp
@@ -5,5 +5,54 @@
 #include "work.h"
 
 namespace doodle {
-namespace render_farm {}  // namespace render_farm
+namespace render_farm {
+void work::run() {
+  auto l_s = boost::asio::make_strand(g_io_context());
+
+  if (!ptr_->resolver_) {
+    ptr_->resolver_ = std::make_shared<resolver>(l_s);
+  }
+  if (!ptr_->socket_) {
+    ptr_->socket_ = std::make_shared<socket>(l_s);
+  }
+  if (!ptr_->timer_) {
+    ptr_->timer_ = std::make_shared<timer>(l_s);
+  }
+
+  ptr_->timer_->expires_after(2s);
+  ptr_->timer_->async_wait([this](boost::system::error_code ec) {
+    if (ec) {
+      DOODLE_LOG_INFO("{}", ec.message());
+      return;
+    }
+    run();
+  });
+  if (!ptr_->socket_->is_open()) {
+    ptr_->resolver_->async_resolve(
+        ptr_->server_ip, "50021",
+        [this](boost::system::error_code ec, boost::asio::ip::tcp::resolver::results_type results) {
+          if (ec) {
+            DOODLE_LOG_INFO("{}", ec.message());
+            return;
+          }
+          boost::asio::async_connect(
+              *ptr_->socket_, results,
+              [this](boost::system::error_code ec, boost::asio::ip::tcp::endpoint endpoint) {
+                if (ec) {
+                  DOODLE_LOG_INFO("{}", ec.message());
+                  return;
+                }
+                DOODLE_LOG_INFO("连接成功服务器");
+                do_register();
+              }
+          );
+        }
+    );
+  } else {
+    DOODLE_LOG_INFO("socket is open");
+    do_register();
+  }
+}
+void work::do_register() {}
+}  // namespace render_farm
 }  // namespace doodle
--- src/doodle_lib/render_farm/work.h
+++ src/doodle_lib/render_farm/work.h
@@ -2,10 +2,41 @@
 // Created by td_main on 2023/8/21.
 //
 #include <doodle_lib/doodle_lib_fwd.h>
+
+#include <boost/asio.hpp>
 namespace doodle {
 namespace render_farm {
 
-class work {};
+class work {
+ public:
+  using timer        = boost::asio::system_timer;
+  using timer_ptr    = std::shared_ptr<timer>;
+  using socket       = boost::asio::ip::tcp::socket;
+  using socket_ptr   = std::shared_ptr<socket>;
+  using resolver     = boost::asio::ip::tcp::resolver;
+  using resolver_ptr = std::shared_ptr<resolver>;
+
+ private:
+  struct data_type {
+    std::string server_ip{};
+    timer_ptr timer_{};
+    socket_ptr socket_{};
+    resolver_ptr resolver_{};
+  };
+  std::shared_ptr<data_type> ptr_;
+
+ public:
+  explicit work(std::string in_server_ip) : ptr_{std::make_shared<data_type>()} {
+    ptr_->server_ip = std::move(in_server_ip);
+  }
+
+  ~work() = default;
+
+  void run();
+
+ private:
+  void do_register();
+};
 
 }  // namespace render_farm
 }  // namespace doodle
