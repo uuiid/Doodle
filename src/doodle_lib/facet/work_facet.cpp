14c17a47879b97802bc35038fb3a09e65b7bb45d
uuiid <957714080@qq.com>
2023-08-21 14:36:20 +0800

    [灯光自动化] 添加工作构面

--- src/doodle_lib/facet/work_facet.cpp
+++ src/doodle_lib/facet/work_facet.cpp
@@ -24,6 +24,12 @@ bool work_facet::post() {
     l_r    = true;
     guard_ = std::make_shared<decltype(guard_)::element_type>(boost::asio::make_work_guard(g_io_context()));
     doodle_lib::Get().ctx().emplace<doodle::render_farm::work>(core_set::get_set().server_ip).run();
+    doodle_lib::Get()
+        .ctx()
+        .emplace<doodle::render_farm::working_machine_ptr>(
+            std::make_shared<doodle::render_farm::working_machine>(g_io_context(), 50021)
+        )
+        ->config_work();
   }
   return l_r;
 }
