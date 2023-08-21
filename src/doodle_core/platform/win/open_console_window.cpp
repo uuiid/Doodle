3b7a07647dc57e9bab5501b1fceb8a751d05cfc0
uuiid <957714080@qq.com>
2023-08-21 14:59:41 +0800

    [灯光自动化] 添加新的句柄流

---
+++ src/doodle_core/platform/win/open_console_window.cpp
@@ -0,0 +1,19 @@
+//
+// Created by td_main on 2023/8/21.
+//
+#include <doodle_core/platform/win/windows_alias.h>
+
+#include <wil/result.h>
+namespace doodle::win {
+
+void open_console_window() {
+  if (::GetConsoleWindow() == nullptr) {
+    THROW_IF_WIN32_BOOL_FALSE(::AttachConsole(ATTACH_PARENT_PROCESS));
+    FILE* l_file{};
+    ::freopen_s(&l_file, "CONIN$", "r", stdin);
+    ::freopen_s(&l_file, "CONOUT$", "w", stdout);
+    ::freopen_s(&l_file, "CONERR$", "w", stderr);
+    ::SetFocus(::GetConsoleWindow());
+  }
+}
+}  // namespace doodle::win
