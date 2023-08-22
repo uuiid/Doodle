100611cd9299fb1304b81be091a2a2983c326398
uuiid <957714080@qq.com>
2023-08-22 11:01:57 +0800

    [灯光自动化] 添加gui结构

---
+++ src/doodle_lib/gui/widgets/render_monitor.cpp
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/8/22.
+//
+
+#include "render_monitor.h"
+
+namespace doodle {
+namespace gui {}  // namespace gui
+}  // namespace doodle
---
+++ src/doodle_lib/gui/widgets/render_monitor.h
@@ -0,0 +1,12 @@
+//
+// Created by td_main on 2023/8/22.
+//
+#pragma once
+
+namespace doodle {
+namespace gui {
+
+class render_monitor {};
+
+}  // namespace gui
+}  // namespace doodle
