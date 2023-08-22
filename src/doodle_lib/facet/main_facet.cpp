7d20cfffba1b3d618be3564df2afc89a4b8596f6
uuiid <957714080@qq.com>
2023-08-22 13:52:44 +0800

    [灯光自动化] 注册渲染监控面板

--- src/doodle_lib/facet/main_facet.cpp
+++ src/doodle_lib/facet/main_facet.cpp
@@ -27,6 +27,7 @@
 #include <doodle_lib/gui/widgets/long_time_tasks_widget.h>
 #include <doodle_lib/gui/widgets/maya_tool.h>
 #include <doodle_lib/gui/widgets/project_edit.h>
+#include <doodle_lib/gui/widgets/render_monitor.h>
 #include <doodle_lib/gui/widgets/subtitle_processing.h>
 #include <doodle_lib/gui/widgets/time_sequencer_widget.h>
 #include <doodle_lib/gui/widgets/ue4_widget.h>
@@ -75,6 +76,7 @@ void main_facet::load_windows() {
     g_windows_manage().create_windows_arg(windows_init_arg{}.create_set_title<assets_file_widgets>());
     g_windows_manage().create_windows_arg(windows_init_arg{}.create_set_title<xlsx_export_widgets>());
     g_windows_manage().create_windows_arg(windows_init_arg{}.create_set_title<all_user_view_widget>());
+    g_windows_manage().create_windows_arg(windows_init_arg{}.create_set_title<render_monitor>());
     //    g_windows_manage().create_windows_arg(windows_init_arg{}.create_set_title<upload_files>());
     g_windows_manage().create_windows_arg(
         windows_init_arg{}.create_set_title<setting_windows>().set_init_hide().set_size(640, 320)
