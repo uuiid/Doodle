feb29e8b6ce12a9fe3636492c3edf3e3bf9e1873
uuiid <957714080@qq.com>
2023-08-23 16:36:07 +0800

    [灯光自动化] 添加定时刷新

--- src/doodle_lib/gui/widgets/render_monitor.cpp
+++ src/doodle_lib/gui/widgets/render_monitor.cpp
@@ -4,10 +4,18 @@
 
 #include "render_monitor.h"
 
+#include <doodle_core/lib_warp/boost_fmt_error.h>
+
 #include <doodle_app/lib_warp/imgui_warp.h>
 namespace doodle {
 namespace gui {
-void render_monitor::init() {}
+void render_monitor::init() {
+  p_i->client_ptr_ = std::make_shared<client>(core_set::get_set().server_ip);
+  p_i->strand_ptr_ = std::make_shared<strand_t>(boost::asio::make_strand(g_io_context()));
+  p_i->timer_ptr_  = std::make_shared<timer_t>(*p_i->strand_ptr_);
+
+  do_wait();
+}
 bool render_monitor::render() {
   if (auto l_ = dear::CollapsingHeader{*p_i->component_collapsing_header_id_}) {
     if (auto l_table = dear::Table{*p_i->component_table_id_, 3}) {
@@ -20,7 +28,7 @@ bool render_monitor::render() {
       for (auto& l_computer : p_i->computers_) {
         ImGui::TableNextRow();
         ImGui::TableNextColumn();
-        ImGui::Text("%llu", l_computer.id_);
+        ImGui::Text("%ll", l_computer.id_);
         ImGui::TableNextColumn();
         dear::Text(l_computer.name_);
         ImGui::TableNextColumn();
@@ -41,7 +49,7 @@ bool render_monitor::render() {
       for (auto& l_render_task : p_i->render_tasks_) {
         ImGui::TableNextRow();
         ImGui::TableNextColumn();
-        ImGui::Text("%llu", l_render_task.id_);
+        ImGui::Text("%ll", l_render_task.id_);
         ImGui::TableNextColumn();
         dear::Text(l_render_task.name_);
         ImGui::TableNextColumn();
@@ -51,6 +59,44 @@ bool render_monitor::render() {
   }
 
   return p_i->open_;
+}
+void render_monitor::do_wait() {
+  p_i->timer_ptr_->expires_after(doodle::chrono::seconds{3});
+  p_i->timer_ptr_->async_wait([this](boost::system::error_code ec) {
+    if (ec) {
+      DOODLE_LOG_ERROR("{}", ec);
+      return;
+    }
+    get_remote_data();
+  });
+}
+void render_monitor::get_remote_data() {
+  p_i->client_ptr_->async_computer_list(
+      [this](const boost::system::error_code& in_code, const std::vector<client::computer>& in_vector) {
+        if (in_code) {
+          DOODLE_LOG_ERROR("{}", in_code);
+          do_wait();
+          return;
+        }
+        p_i->computers_ = in_vector | ranges::views::transform([](auto&& in_item) -> computer {
+                            return computer{in_item.id_, in_item.name_, in_item.state_};
+                          }) |
+                          ranges::to_vector;
+      }
+  );
+  p_i->client_ptr_->async_task_list(
+      [this](const boost::system::error_code& in_code, const std::vector<client::task_t>& in_vector) {
+        if (in_code) {
+          DOODLE_LOG_ERROR("{}", in_code);
+          do_wait();
+          return;
+        }
+        p_i->render_tasks_ = in_vector | ranges::views::transform([](auto&& in_item) -> render_task {
+                               return render_task{in_item.id_, in_item.name_, in_item.state_};
+                             }) |
+                             ranges::to_vector;
+      }
+  );
 }
 }  // namespace gui
 }  // namespace doodle
--- src/doodle_lib/gui/widgets/render_monitor.h
+++ src/doodle_lib/gui/widgets/render_monitor.h
@@ -6,6 +6,7 @@
 #include <doodle_app/gui/base/ref_base.h>
 
 #include <doodle_lib/doodle_lib_fwd.h>
+#include <doodle_lib/render_farm/client.h>
 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
@@ -17,15 +18,19 @@ namespace gui {
 class render_monitor {
  private:
   struct computer {
-    std::uint64_t id_{};
+    std::int64_t id_{};
     std::string name_{};
     std::string state_{};
   };
   struct render_task {
-    std::uint64_t id_{};
+    std::int64_t id_{};
     std::string name_{};
     std::string state_{};
   };
+  using timer_t      = boost::asio::system_timer;
+  using timer_ptr_t  = std::shared_ptr<timer_t>;
+  using strand_t     = boost::asio::strand<boost::asio::io_context::executor_type>;
+  using strand_ptr_t = std::shared_ptr<strand_t>;
 
   struct impl {
     bool open_{true};
@@ -35,8 +40,15 @@ class render_monitor {
     gui_cache_name_id render_task_table_id_;
     std::vector<computer> computers_;
     std::vector<render_task> render_tasks_;
+    std::shared_ptr<client> client_ptr_{};
+    strand_ptr_t strand_ptr_{};
+    timer_ptr_t timer_ptr_{};
   };
   std::unique_ptr<impl> p_i;
+
+  void do_wait();
+
+  void get_remote_data();
 
  public:
   render_monitor() : p_i(std::make_unique<impl>()){};
