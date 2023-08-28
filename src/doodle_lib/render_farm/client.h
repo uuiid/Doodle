f20ea7fe87b33b55565d11fb102a02fcc9fef9c2
uuiid <957714080@qq.com>
2023-08-28 13:09:48 +0800

    [灯光自动化导入] 调整json序列化

--- src/doodle_lib/render_farm/client.h
+++ src/doodle_lib/render_farm/client.h
@@ -171,7 +171,7 @@ class client {
         this->complete(false, ec, result_type{});
         return;
       }
- 
+
       ptr_->state_ = connect;
       DOODLE_LOG_INFO("state {}", magic_enum::enum_name(ptr_->state_));
       do_write();
@@ -248,7 +248,7 @@ class client {
     friend void from_json(const nlohmann::json& in_json, computer& out_data) {
       in_json["id"].get_to(out_data.id_);
       in_json["name"].get_to(out_data.name_);
-      in_json["state"].get_to(out_data.state_);
+      in_json["status"].get_to(out_data.state_);
     }
   };
 
