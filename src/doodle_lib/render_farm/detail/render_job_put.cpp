3cb5f2c1eb34fa701359f7f1eb3341f232be9d14
uuiid <957714080@qq.com>
2023-08-28 17:36:17 +0800

    [灯光自动化导入] 调整服务器设置状态函数

--- src/doodle_lib/render_farm/detail/render_job_put.cpp
+++ src/doodle_lib/render_farm/detail/render_job_put.cpp
@@ -21,23 +21,23 @@ void render_job_put::operator()(boost::system::error_code ec, std::size_t bytes_
   }
   auto l_json = ptr_->parser_->release().body();
   if (l_json.contains("state")) {
-    auto l_state = magic_enum::enum_cast<process_message::state>(l_json["state"].get<std::string>());
-    if (l_state) {
-      ptr_->handle_.get<process_message>().set_state(*l_state);
+    auto l_state = l_json["state"].get<process_message::state>();
+    ptr_->handle_.get<process_message>().set_state(l_state);
 
-      auto l_response   = boost::beast::http::response<detail::basic_json_body>{boost::beast::http::status::ok, 11};
-      l_response.body() = {{"state", "ok"}};
-      l_response.keep_alive(ptr_->parser_->keep_alive());
-      l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
-
-    } else {
-      DOODLE_LOG_ERROR("on_read error: state is not valid");
-      auto l_response =
-          boost::beast::http::response<detail::basic_json_body>{boost::beast::http::status::bad_request, 11};
-      l_response.body() = {{"state", "error"}, {"error", "state is not valid"}};
-      l_response.keep_alive(ptr_->parser_->keep_alive());
-      l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
-    }
+    auto l_response   = boost::beast::http::response<detail::basic_json_body>{boost::beast::http::status::ok, 11};
+    l_response.body() = {{"state", "ok"}};
+    l_response.keep_alive(ptr_->parser_->keep_alive());
+    l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
+    //    if (l_state) {
+    //
+    //    } else {
+    //      DOODLE_LOG_ERROR("on_read error: state is not valid");
+    //      auto l_response =
+    //          boost::beast::http::response<detail::basic_json_body>{boost::beast::http::status::bad_request, 11};
+    //      l_response.body() = {{"state", "error"}, {"error", "state is not valid"}};
+    //      l_response.keep_alive(ptr_->parser_->keep_alive());
+    //      l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
+    //    }
   }
   erase_reg_component(this);
 }
