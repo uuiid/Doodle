6208db5ff105b682426dc257e9f8658dcedf78a9
uuiid <957714080@qq.com>
2023-08-29 13:11:43 +0800

    [灯光自动化导入] 添加日志转储

---
+++ src/doodle_core/logger/crash_reporting_thread.cpp
@@ -0,0 +1,7 @@
+//
+// Created by td_main on 2023/8/29.
+//
+
+#include "crash_reporting_thread.h"
+
+namespace doodle {}  // namespace doodle
---
+++ src/doodle_core/logger/crash_reporting_thread.h
@@ -0,0 +1,11 @@
+//
+// Created by td_main on 2023/8/29.
+//
+#pragma once
+
+namespace doodle::detail {
+
+class crash_reporting_thread {};
+
+}  // namespace doodle
+
