e55f03b5fdaa9a97e743bfecfaf2fd31ec1f7262
uuiid <957714080@qq.com>
2023-09-01 14:33:33 +0800

    [灯光自动化导入] 添加udp客户端

---
+++ src/doodle_lib/render_farm/dup_server.cpp
@@ -0,0 +1,7 @@
+//
+// Created by td_main on 2023/9/1.
+//
+
+#include "dup_server.h"
+
+namespace doodle {}  // namespace doodle
---
+++ src/doodle_lib/render_farm/dup_server.h
@@ -0,0 +1,11 @@
+//
+// Created by td_main on 2023/9/1.
+//
+
+#pragma once
+#include <memory>
+namespace doodle {
+
+class dup_server : std::enable_shared_from_this<dup_server> {};
+
+}  // namespace doodle
