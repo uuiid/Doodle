a43f65773d85fa132e907ac6a139efd4251dafb4
uuiid <957714080@qq.com>
2023-09-01 15:14:20 +0800

    [灯光自动化导入] 完成udp协议的添加

--- src/doodle_lib/render_farm/dup_server.cpp
+++ src/doodle_lib/render_farm/dup_server.cpp
@@ -4,4 +4,22 @@
 
 #include "dup_server.h"
 
-namespace doodle {}  // namespace doodle
+namespace doodle {
+void dup_server::run() {
+  socket_.async_receive_from(buffer_, end_point_, [this](boost::system::error_code ec, std::size_t bytes) {
+    if (ec) {
+      return;
+    }
+    do_accept();
+  });
+}
+void dup_server::do_accept() {
+  std::string_view l_view{boost::asio::buffer_cast<const char *>(buffer_.data()), buffer_.size()};
+  if (l_view == "hello world! doodle") {
+    socket_.async_send_to(
+        boost::asio::buffer("hello world! doodle"), end_point_,
+        [this](boost::system::error_code ec, std::size_t bytes) { this->run(); }
+    );
+  }
+}
+}  // namespace doodle
--- src/doodle_lib/render_farm/dup_server.h
+++ src/doodle_lib/render_farm/dup_server.h
@@ -3,9 +3,28 @@
 //
 
 #pragma once
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+
 #include <memory>
 namespace doodle {
 
-class dup_server : std::enable_shared_from_this<dup_server> {};
+class dup_server : std::enable_shared_from_this<dup_server> {
+ public:
+  explicit dup_server(boost::asio::io_context& in_io_context, std::uint16_t in_port = 50022)
+      : socket_{in_io_context, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), in_port)},
+        signal_set_{in_io_context, SIGINT, SIGTERM} {}
+  ~dup_server() = default;
+
+  void run();
+
+ private:
+  void do_accept();
+
+  boost::asio::ip::udp::endpoint end_point_;
+  boost::asio::ip::udp::socket socket_;
+  boost::beast::flat_buffer buffer_;
+  boost::asio::signal_set signal_set_;
+};
 
 }  // namespace doodle
