1c2f78e6401fb8060d3fda400430dc7648cf3291
uuiid <957714080@qq.com>
2023-09-04 13:53:03 +0800

    [灯光自动化导入] log日志

--- src/doodle_lib/render_farm/udp_client.cpp
+++ src/doodle_lib/render_farm/udp_client.cpp
@@ -11,8 +11,9 @@ namespace doodle {
 
 void udp_client::do_send() {
   ++ptr_->recv_size_;
-
+  DOODLE_LOG_INFO("开始第 {} 次发送", ptr_->recv_size_);
   if (ptr_->recv_size_ > 3) {
+    DOODLE_LOG_ERROR("发送次数超过 3 次, 取消发送");
     ptr_->timer_.cancel();
     ptr_->cancel_sig_.emit(boost::asio::cancellation_type::total);
     return;
