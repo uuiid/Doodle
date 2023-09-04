a9dba08dc7e3750e26239c5823db5b47486c57ad
uuiid <957714080@qq.com>
2023-09-04 11:15:27 +0800

    [灯光自动化导入] 完成udp客户端

--- src/doodle_lib/render_farm/udp_client.cpp
+++ src/doodle_lib/render_farm/udp_client.cpp
@@ -4,4 +4,37 @@
 
 #include "udp_client.h"
 
-namespace doodle {}  // namespace doodle
+#include <doodle_core/configure/static_value.h>
+#include <doodle_core/lib_warp/boost_fmt_error.h>
+#include <doodle_core/logger/logger.h>
+namespace doodle {
+
+void udp_client::do_send() {
+  ++ptr_->recv_size_;
+  ptr_->send_socket_.open(boost::asio::ip::udp::v4());
+  ptr_->send_socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+  ptr_->send_socket_.set_option(boost::asio::socket_base::broadcast(true));
+  boost::asio::ip::udp::endpoint const l_endpoint{boost::asio::ip::address_v4::broadcast(), ptr_->remove_port_};
+  ptr_->send_socket_.send_to(
+      boost::asio::buffer(doodle_config::hello_world_doodle.data(), doodle_config::hello_world_doodle.size()),
+      l_endpoint
+  );
+
+  if (ptr_->recv_size_ > 3) {
+    ptr_->timer_.cancel();
+    ptr_->signal_(boost::asio::error::make_error_code(boost::asio::error::timed_out), ptr_->remove_endpoint_);
+    ptr_->signal_.disconnect_all_slots();
+    return;
+  }
+
+  ptr_->timer_.expires_after(std::chrono::seconds(1));
+  ptr_->timer_.async_wait([this](auto&& PH1) {
+    if (PH1) {
+      DOODLE_LOG_ERROR("{}", PH1);
+      return;
+    }
+    do_send();
+  });
+}
+
+}  // namespace doodle
--- src/doodle_lib/render_farm/udp_client.h
+++ src/doodle_lib/render_farm/udp_client.h
@@ -3,14 +3,52 @@
 //
 
 #pragma once
+#include <boost/asio.hpp>
+#include <boost/signals2.hpp>
 namespace doodle {
 
 class udp_client {
- public:
-  udp_client()  = default;
-  ~udp_client() = default;
+  using socket_t   = boost::asio::ip::udp::socket;
+  using ednpoint_t = boost::asio::ip::udp::endpoint;
+  using timer_t    = boost::asio::system_timer;
+  struct impl_t {
+    explicit impl_t(boost::asio::io_context& in_context)
+        : send_socket_(in_context), recv_socket_(in_context), timer_(in_context) {}
 
-  void async_find_server();
+    socket_t send_socket_;
+    socket_t recv_socket_;
+    timer_t timer_;
+
+    ednpoint_t remove_endpoint_;
+    std::uint16_t remove_port_{};
+    char recv_buffer_[1024]{};
+    std::size_t recv_size_{};
+    boost::signals2::signal<void(boost::system::error_code, ednpoint_t)> signal_;
+  };
+  std::unique_ptr<impl_t> ptr_;
+
+  void do_send();
+
+ public:
+  explicit udp_client(boost::asio::io_context& in_context) : ptr_(std::make_unique<impl_t>(in_context)){};
+  ~udp_client() = default;
+  template <typename CompletionHandler>
+  auto async_find_server(std::uint16_t in_port, CompletionHandler&& in_completion) {
+    ptr_->remove_port_ = in_port;
+    ptr_->recv_size_   = 0;
+    ptr_->signal_.connect(std::forward<CompletionHandler>(in_completion));
+    do_send();
+
+    ptr_->recv_socket_.async_receive_from(
+        boost::asio::buffer(ptr_->recv_buffer_), ptr_->remove_endpoint_,
+        [l_ptr = ptr_.get()](boost::system::error_code ec, std::size_t in_size) {
+          l_ptr->timer_.cancel();
+          std::memset(l_ptr->recv_buffer_, 0, in_size);
+          l_ptr->signal_(ec, l_ptr->remove_endpoint_);
+          l_ptr->signal_.disconnect_all_slots();
+        }
+    );
+  };
 };
 
 }  // namespace doodle
