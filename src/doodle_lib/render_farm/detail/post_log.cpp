3ec9c3ab96cf5682c38f023ef31f90d0898d0168
uuiid <957714080@qq.com>
2023-09-05 14:04:56 +0800

    [灯光自动化导入] 添加修改log类

---
+++ src/doodle_lib/render_farm/detail/post_log.cpp
@@ -0,0 +1,11 @@
+//
+// Created by td_main on 2023/9/5.
+//
+
+#include "post_log.h"
+
+namespace doodle {
+namespace render_farm {
+namespace detail {}  // namespace detail
+}  // namespace render_farm
+}  // namespace doodle
---
+++ src/doodle_lib/render_farm/detail/post_log.h
@@ -0,0 +1,14 @@
+//
+// Created by td_main on 2023/9/5.
+//
+#pragma once
+
+namespace doodle {
+namespace render_farm {
+namespace detail {
+
+class post_log {};
+
+}  // namespace detail
+}  // namespace render_farm
+}  // namespace doodle
