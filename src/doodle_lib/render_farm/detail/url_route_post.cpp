77c1689282313d7259af438f712d2266e221d4b1
uuiid <957714080@qq.com>
2023-09-06 13:09:54 +0800

    [灯光自动化导入] 调整post方法

--- src/doodle_lib/render_farm/detail/url_route_post.cpp
+++ src/doodle_lib/render_farm/detail/url_route_post.cpp
@@ -12,6 +12,7 @@
 #include <doodle_lib/render_farm/detail/post_log.h>
 #include <doodle_lib/render_farm/detail/render_ue4.h>
 #include <doodle_lib/render_farm/detail/ue4_task.h>
+#include <doodle_lib/render_farm/work.h>
 namespace doodle::render_farm {
 namespace detail {
 
@@ -114,45 +115,16 @@ void computer_reg_type_post::operator()(const entt::handle& in_handle, const std
   );
 }
 void run_job_post::operator()(const entt::handle& in_handle, const std::map<std::string, std::string>& in_cap) const {
-  using json_parser_type = boost::beast::http::request_parser<detail::basic_json_body>;
   auto& l_session        = in_handle.get<working_machine_session>();
+  using json_parser_type = boost::beast::http::request_parser<detail::basic_json_body>;
   auto l_parser_ptr      = std::make_shared<json_parser_type>(std::move(l_session.request_parser()));
-
-  boost::beast::http::async_read(
-      l_session.stream(), l_session.buffer(), *l_parser_ptr,
-      [in_handle, l_parser_ptr](boost::system::error_code ec, std::size_t bytes_transferred) {
-        boost::ignore_unused(bytes_transferred);
-        auto& l_session = in_handle.get<working_machine_session>();
-        if (ec == boost::beast::http::error::end_of_stream) {
-          return l_session.do_close();
-        }
-        if (ec) {
-          DOODLE_LOG_ERROR("on_read error: {}", ec.message());
-          l_session.send_error_code(ec);
-          return;
-        }
-
-        auto l_json = l_parser_ptr->release().body();
-
-        auto l_h    = entt::handle{*g_reg(), g_reg()->create()};
-        l_h.emplace<process_message>();
-        try {
-          l_h.emplace<ue_server_id>(l_json["id"].get<entt::entity>());
-
-          l_h.emplace<render_ue4>(l_h, l_json["arg"].get<render_ue4::arg_t>()).run();
-        } catch (const nlohmann::json::exception& e) {
-          DOODLE_LOG_ERROR("json parse error: {}", e.what());
-          l_session.send_error(e);
-          return;
-        }
-
-        boost::beast::http::response<basic_json_body> l_response{boost::beast::http::status::ok, 11};
-        l_response.keep_alive(l_parser_ptr->keep_alive());
-        l_response.body() = {{"state", "ok"}};
-        l_response.prepare_payload();
-        l_session.send_response(boost::beast::http::message_generator{std::move(l_response)});
-      }
-  );
+  if (g_ctx().contains<render_farm::work_ptr>()) {
+    g_ctx().get<render_farm::work_ptr>()->run_job(in_handle, in_cap);
+  } else {
+    boost::system::error_code ec{};
+    BOOST_BEAST_ASSIGN_EC(ec, error_enum::not_find_work_class);
+    l_session.send_error_code(ec, boost::beast::http::status::internal_server_error);
+  }
 }
 void get_log_type_post::operator()(const entt::handle& in_handle, const std::map<std::string, std::string>& in_cap)
     const {
@@ -168,10 +140,10 @@ void get_log_type_post::operator()(const entt::handle& in_handle, const std::map
 }
 void get_err_type_post::operator()(const entt::handle& in_handle, const std::map<std::string, std::string>& in_cap)
     const {
-  using str_parser_type  = boost::beast::http::request_parser<boost::beast::http::string_body>;
-  auto& l_session        = in_handle.get<working_machine_session>();
-  auto l_parser_ptr      = std::make_shared<str_parser_type>(std::move(l_session.request_parser()));
-  auto l_h               = entt::handle{*g_reg(), num_to_enum<entt::entity>(std::stoi(in_cap.at("handle")))};
+  using str_parser_type = boost::beast::http::request_parser<boost::beast::http::string_body>;
+  auto& l_session       = in_handle.get<working_machine_session>();
+  auto l_parser_ptr     = std::make_shared<str_parser_type>(std::move(l_session.request_parser()));
+  auto l_h              = entt::handle{*g_reg(), num_to_enum<entt::entity>(std::stoi(in_cap.at("handle")))};
 
   boost::beast::http::async_read(
       l_session.stream(), l_session.buffer(), *l_parser_ptr,
