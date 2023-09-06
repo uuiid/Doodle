69b347d97ca18f2fb1f401c3287d5fef3283b712
uuiid <957714080@qq.com>
2023-09-06 13:59:20 +0800

    [灯光自动化导入] 添加运行ue测试

--- src/test/core/run_ue.cpp
+++ src/test/core/run_ue.cpp
@@ -8,11 +8,12 @@
 #include <boost/process/v2/process.hpp>
 #include <boost/process/v2/stdio.hpp>
 int core_run_ue(int, char** const) {
-  boost::asio::io_context l_io_context;
+  boost::asio::io_context l_io_context{};
   static FSys::path l_ue_path{R"(D:\Program Files\Epic Games\UE_5.2\Engine\Binaries\Win64\UE4Editor.exe)"};
   static std::string l_command_line{R"(--version)"};
   boost::asio::readable_pipe log_{l_io_context};
   boost::asio::readable_pipe err_{l_io_context};
   boost::process::v2::process l_child{
       g_io_context(), ue_path_.string(), l_command_line, boost::process::v2::process_stdio{log_, err_, {}}};
+  l_io_context.run();
 }
