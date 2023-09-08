cdba24ebf2ac0c5214468cb738d00ba4fe49ad24
uuiid <957714080@qq.com>
2023-09-08 14:59:00 +0800

    [灯光自动化导入] 添加修改状态url

--- src/doodle_lib/render_farm/working_machine.cpp
+++ src/doodle_lib/render_farm/working_machine.cpp
@@ -9,6 +9,7 @@
 #include <doodle_lib/render_farm/detail/url_route_base.h>
 #include <doodle_lib/render_farm/detail/url_route_get.h>
 #include <doodle_lib/render_farm/detail/url_route_post.h>
+#include <doodle_lib/render_farm/detail/url_route_put.h>
 #include <doodle_lib/render_farm/working_machine_session.h>
 
 #include <boost/asio.hpp>
@@ -57,15 +58,18 @@ void working_machine::config_server() {
   route_ptr_->reg<detail::get_root_type>();
   route_ptr_->reg<detail::render_job_type_post>();
   route_ptr_->reg<detail::computer_reg_type_post>();
+
   route_ptr_->reg<detail::get_log_type_get>();
   route_ptr_->reg<detail::get_err_type_get>();
   route_ptr_->reg<detail::render_job_type_get>();
-
-  route_ptr_->reg<detail::computer_reg_type_get>();
-  route_ptr_->reg<detail::render_job_type_post>();
   route_ptr_->reg<detail::repository_type_get>();
+  route_ptr_->reg<detail::computer_reg_type_get>();
+
+  route_ptr_->reg<detail::render_job_type_post>();
   route_ptr_->reg<detail::get_log_type_post>();
   route_ptr_->reg<detail::get_err_type_post>();
+
+  route_ptr_->reg<detail::render_job_type_put>();
 
   g_reg()->ctx().emplace<ue_task_manage>().run();
   g_reg()->ctx().emplace<computer_manage>().run();
