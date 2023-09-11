e7addf0aefe1adc54895b4a6e046b642b65a9b4e
uuiid <957714080@qq.com>
2023-09-11 16:40:33 +0800

    [灯光自动化导入] 调整关闭管道时的打印

--- src/doodle_lib/render_farm/detail/render_ue4.cpp
+++ src/doodle_lib/render_farm/detail/render_ue4.cpp
@@ -144,7 +144,7 @@ void render_ue4::do_read_log() {
           do_read_log();
         } else {
           child_ptr->out_attr.close();
-          if (in_code != boost::asio::error::eof) DOODLE_LOG_ERROR(in_code);
+          if (in_code != boost::asio::error::broken_pipe) DOODLE_LOG_ERROR(in_code);
         }
       }
   );
@@ -162,7 +162,7 @@ void render_ue4::do_read_err() {
           do_read_err();
         } else {
           child_ptr->err_attr.close();
-          if (in_code != boost::asio::error::eof) DOODLE_LOG_ERROR(in_code);
+          if (in_code != boost::asio::error::broken_pipe) DOODLE_LOG_ERROR(in_code);
         }
       }
   );
