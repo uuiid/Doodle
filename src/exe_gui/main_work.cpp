8312a898f1e16f45af2246d20ef61e864b0e42bb
uuiid <957714080@qq.com>
2023-09-11 14:43:46 +0800

    [灯光自动化导入] 调整构建错误

--- src/exe_gui/main_work.cpp
+++ src/exe_gui/main_work.cpp
@@ -2,7 +2,7 @@
 // Created by td_main on 2023/9/11.
 //
 
-#include <doodle_lib/facet/work_facet.h.h>
+#include <doodle_lib/facet/work_facet.h>
 
 #include "main_macro.h"
 DOODLE_MAIN_IMPL(doodle::work_facet)
