31e3eba21769bd80838cd79f1735ba8579da500b
uuiid <957714080@qq.com>
2023-09-11 11:36:38 +0800

    [灯光自动化导入] 修正gui启动

--- src/exe_gui/main_gui.cpp
+++ src/exe_gui/main_gui.cpp
@@ -6,30 +6,6 @@
  * @Description: In User Settings Edit
  * @FilePath: \Doodle\doodle_GUI\main.cpp
  */
-#include <doodle_lib/facet/main_facet.h>
-#include <doodle_lib/facet/server_facet.h>
-#include <doodle_lib/facet/work_facet.h>
 
-#include <iostream>
-
-// #include <doodle_lib/DoodleApp.h>
-// #include <boost/locale.hpp>
-
-// extern "C" int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR strCmdLine, int nCmdShow) try {
-extern "C" int main(int argc, const char* const argv[]) try {
-  using main_app = doodle::app_command<doodle::server_facet, doodle::work_facet, doodle::main_facet>;
-  main_app app{argc, argv};
-  try {
-    return app.run();
-  } catch (const std::exception& err) {
-    DOODLE_LOG_WARN(boost::diagnostic_information(err));
-    return 1;
-  } catch (...) {
-    DOODLE_LOG_ERROR(boost::current_exception_diagnostic_information(true));
-    return 1;
-  }
-  return 0;
-} catch (...) {
-  std::cout << boost::current_exception_diagnostic_information(true) << std::endl;
-  return 1;
-}
+#include "main_macro.h"
+DOODLE_MAIN_IMPL(doodle::main_facet)
