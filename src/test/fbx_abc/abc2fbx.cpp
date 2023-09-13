6bc74615bace5842e4a547c9d483e07be98f5a96
uuiid <957714080@qq.com>
2023-09-13 14:32:14 +0800

    [纱闪烁修正] 初步调整fbx和abc之间的关系

--- src/test/fbx_abc/abc2fbx.cpp
+++ src/test/fbx_abc/abc2fbx.cpp
@@ -27,8 +27,66 @@
 #include <Alembic/AbcGeom/OXform.h>
 #include <Alembic/AbcGeom/XformOp.h>
 #include <Alembic/Util/PlainOldDataType.h>
-
+#include <fbxsdk.h>
 namespace doodle {
+void write_fbx_impl(FbxScene* in_scene, const FSys::path& in_fbx_path) {
+  auto* l_manager = in_scene->GetFbxManager();
+  FbxExporter* l_exporter{FbxExporter::Create(in_scene->GetFbxManager(), "")};
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_MATERIAL, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_TEXTURE, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_EMBEDDED, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_SHAPE, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_GOBO, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_ANIMATION, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_FBX_GLOBAL_SETTINGS, true);
+  l_manager->GetIOSettings()->SetBoolProp(EXP_ASCIIFBX, false);
+
+  l_exporter->Initialize(
+      in_fbx_path.string().c_str(), l_manager->GetIOPluginRegistry()->GetNativeWriterFormat(),
+      in_scene->GetFbxManager()->GetIOSettings()
+  );
+  l_exporter->Export(in_scene);
+  l_exporter->Destroy();
+}
+// 写入fbx
+void write_fbx(const FSys::path& in_fbx_path, const Alembic::AbcGeom::IPolyMesh& in_poly) {
+  std::shared_ptr<FbxManager> l_manager{FbxManager::Create(), [](FbxManager* in_manager) { in_manager->Destroy(); }};
+  FbxIOSettings* l_io_settings = FbxIOSettings::Create(l_manager.get(), IOSROOT);
+  l_manager->SetIOSettings(l_io_settings);
+  FbxScene* l_scene{FbxScene::Create(l_manager.get(), "scene")};
+
+  FbxDocumentInfo* l_doc_info{FbxDocumentInfo::Create(l_manager.get(), "info")};
+  l_doc_info->mTitle   = "doodle fbx";
+  l_doc_info->mSubject = "doodle fbx";
+  l_doc_info->mAuthor  = "doodle";
+  l_doc_info->Original_ApplicationVendor.Set("doodle");
+  l_doc_info->Original_ApplicationName.Set("doodle");
+  l_doc_info->Original_ApplicationVersion.Set("1.0.0");
+
+  l_doc_info->LastSaved_ApplicationVendor.Set("doodle");
+  l_doc_info->LastSaved_ApplicationName.Set("doodle");
+  l_doc_info->LastSaved_ApplicationVersion.Set("1.0.0");
+
+  l_scene->SetSceneInfo(l_doc_info);
+
+  l_scene->GetGlobalSettings().SetSystemUnit(FbxSystemUnit::cm);
+  auto anim_stack = FbxAnimStack::Create(l_scene, "anim_stack");
+  auto anim_layer = FbxAnimLayer::Create(l_scene, "anim_layer");
+  anim_stack->AddMember(anim_layer);
+
+  // 写出网格
+  auto* l_mesh         = FbxMesh::Create(l_scene, "mesh");
+  const auto& l_sample = in_poly.getSchema();
+  // 写出顶点
+  auto l_sample_data   = l_sample.getValue();
+  l_mesh->InitControlPoints(l_sample_data.getPositions()->size());
+  auto* l_pos_list = l_mesh->GetControlPoints();
+  for (std::size_t j = 0; j < l_sample_data.getPositions()->size(); ++j) {
+    const auto& l_pos = (*l_sample_data.getPositions())[j];
+    l_pos_list[j]     = FbxVector4{l_pos.x, l_pos.y, l_pos.z};
+  }
+  write_fbx_impl(l_scene, in_fbx_path);
+}
 
 void run(const FSys::path& in_abc_path) {
   Alembic::Abc::IArchive l_archive{Alembic::AbcCoreOgawa::ReadArchive(), in_abc_path.string()};
@@ -43,7 +101,15 @@ void run(const FSys::path& in_abc_path) {
     l_child.getHeader();
     if (Alembic::AbcGeom::IXform::matches(l_child.getHeader())) {
       auto l_xform = Alembic::AbcGeom::IXform{l_child};
-      DOODLE_LOG_INFO("{}", l_xform.getName());
+      DOODLE_LOG_INFO("xform {}", l_xform.getName());
+      for (int l = 0; l < l_xform.getNumChildren(); ++l) {
+        if (Alembic::AbcGeom::IPolyMesh::matches(l_xform.getChild(l).getHeader())) {
+          auto l_poly = Alembic::AbcGeom::IPolyMesh{l_xform.getChild(l)};
+          DOODLE_LOG_INFO("poly {}", l_poly.getName());
+          write_fbx(in_abc_path.parent_path() / (l_poly.getName() + ".fbx"), l_poly);
+          return;
+        }
+      }
     }
   }
 }
@@ -54,7 +120,7 @@ int main(int argc, char* argv[]) {
   using namespace doodle;
   argh::parser l_parser(argc, argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION);
   if (auto l_ss = l_parser("--abc"); l_ss) {
-    FSys::path l_path{l_ss.str()};
+    FSys::path l_path{FSys::from_quotation_marks(l_ss.str())};
     l_path = l_path.make_preferred();
     run(l_path);
   } else {
