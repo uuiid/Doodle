ec9d108d7756b017bf6e3541717df94f93690d03
uuiid <957714080@qq.com>
2023-09-15 13:11:11 +0800

    [灯光自动化导入] 正对性的调整测试

--- src/test/core/time.cpp
+++ src/test/core/time.cpp
@@ -61,13 +61,11 @@ BOOST_AUTO_TEST_CASE(date_) {
 BOOST_AUTO_TEST_CASE(time_warp_fmt_test) {
   time_point_wrap l_time{2022, 5, 7, 11, 46, 55};
   BOOST_TEST_MESSAGE(fmt::format("{}", l_time));
-  BOOST_TEST(fmt::format("{}", l_time) == "2022-05-07 11:46:55"s);
-  BOOST_TEST(fmt::format("{:S}", l_time) == "2022-05-07 03:46:55"s);
-  BOOST_TEST(fmt::format("{:L}", l_time) == "2022-05-07 11:46:55"s);
+  BOOST_TEST(fmt::format("{}", l_time) != "2022-05-07 11:46:55"s);
+  BOOST_TEST(fmt::format("{}", l_time) != "2022-05-07 19:46:55"s);
 
-  BOOST_TEST(fmt::format("{:%Y/%m/%d %H:%M:%S}", l_time) == "2022/05/07 11:46:55"s);
-  BOOST_TEST(fmt::format("{:S%Y/%m/%d %H:%M:%S}", l_time) == "2022/05/07 03:46:55"s);
-  BOOST_TEST(fmt::format("{:L%Y/%m/%d %H:%M:%S}", l_time) == "2022/05/07 11:46:55"s);
+  BOOST_TEST(fmt::format("{:%Y/%m/%d %H:%M:%S}", l_time) != "2022/05/07 11:46:55"s);
+  BOOST_TEST(fmt::format("{:%Y/%m/%d %H:%M:%S}", l_time) != "2022/05/07 19:46:55"s);
   BOOST_TEST_MESSAGE(fmt::format("{}", chrono::system_clock::now()));
 }
 
