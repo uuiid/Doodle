338b991ec445d604efb51ced5c305ee0a028d8bf
uuiid <957714080@qq.com>
2023-09-15 11:03:10 +0800

    [灯光自动化导入] 添加webcocket

--- src/doodle_lib/render_farm/websocket.cpp
+++ src/doodle_lib/render_farm/websocket.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "websocket.h"
+
+namespace doodle::render_farm {
+
+void websocket::run() {
+  impl_ptr_->stream_.async_read(impl_ptr_->buffer_, [](boost::system::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+    if (ec) {
+      return;
+    }
+  });
+}
+}  // namespace doodle::render_farm
--- src/doodle_lib/render_farm/websocket.h
+++ src/doodle_lib/render_farm/websocket.h
@@ -3,3 +3,35 @@
 //
 
 #pragma once
+
+#include <boost/beast.hpp>
+
+namespace doodle::render_farm {
+
+class websocket {
+ private:
+  struct impl {
+    explicit impl(boost::beast::websocket::stream<boost::asio::ip::tcp::socket> in_stream)
+        : stream_(std::move(in_stream)) {}
+    boost::beast::websocket::stream<boost::asio::ip::tcp::socket> stream_;
+    boost::beast::flat_buffer buffer_{};
+  };
+
+  std::unique_ptr<impl> impl_ptr_{};
+  void run();
+
+ public:
+  websocket() = default;
+  explicit websocket(boost::beast::websocket::stream<boost::asio::ip::tcp::socket> in_stream)
+      : impl_ptr_(std::make_unique<impl>(std::move(in_stream))) {}
+  ~websocket()                               = default;
+
+  // copy
+  websocket(const websocket&)                = delete;
+  websocket& operator=(const websocket&)     = delete;
+  // move
+  websocket(websocket&&) noexcept            = default;
+  websocket& operator=(websocket&&) noexcept = default;
+};
+
+}  // namespace doodle::render_farm
