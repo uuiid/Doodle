4aef0bd52d2ac97b20c1143a8a54f19d846c96aa
uuiid <957714080@qq.com>
2023-09-19 13:05:55 +0800

    [灯光自动化] 添加功能注册

---
+++ src/doodle_lib/render_farm/functional_registration_manager.cpp
@@ -0,0 +1,20 @@
+//
+// Created by td_main on 2023/9/19.
+//
+
+#include "functional_registration_manager.h"
+namespace doodle::render_farm {
+
+void functional_registration_manager::register_function(
+    std::string in_name, std::function<nlohmann::json(const nlohmann::json &)> in_function
+) {
+  map_.emplace(std::move(in_name), std::move(in_function));
+}
+
+std::function<nlohmann::json(const nlohmann::json &)> functional_registration_manager::get_function(std::string in_name
+) {
+  if (map_.find(in_name) == map_.end()) return {};
+  return map_.at(std::move(in_name));
+}
+
+}  // namespace doodle::render_farm
---
+++ src/doodle_lib/render_farm/functional_registration_manager.h
@@ -0,0 +1,31 @@
+//
+// Created by td_main on 2023/9/19.
+//
+
+#pragma once
+
+#include <doodle_core/doodle_core_fwd.h>
+
+namespace doodle::render_farm {
+
+class functional_registration_manager {
+ private:
+  std::map<std::string, std::function<nlohmann::json(const nlohmann::json&)>> map_;
+
+ public:
+  functional_registration_manager();
+  ~functional_registration_manager();
+
+  // copy
+  functional_registration_manager(const functional_registration_manager&)                = delete;
+  functional_registration_manager& operator=(const functional_registration_manager&)     = delete;
+  // move
+  functional_registration_manager(functional_registration_manager&&) noexcept            = default;
+  functional_registration_manager& operator=(functional_registration_manager&&) noexcept = default;
+
+  void register_function(std::string in_name, std::function<nlohmann::json(const nlohmann::json&)> in_function);
+
+  std::function<nlohmann::json(const nlohmann::json&)> get_function(std::string in_name);
+};
+
+}  // namespace doodle::render_farm
