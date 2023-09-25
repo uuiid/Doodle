1e2874cb97cf18cb8a9e76c110c30ef2cb91e1b3
uuiid <957714080@qq.com>
2023-09-25 18:02:49 +0800

    [灯光工具] 调整复盖方法

--- script/uePlug/Doodle/Source/Doodle/Private/Doodle/DoodleLightningPost.cpp
+++ script/uePlug/Doodle/Source/Doodle/Private/Doodle/DoodleLightningPost.cpp
@@ -1,8 +1,16 @@
 #include "Doodle/DoodleLightningPost.h"
 
 #include "Components/DirectionalLightComponent.h"
+
 ADoodleLightingPost::ADoodleLightingPost() {
   LightComponent = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("LightComponent"));
   LightComponent->SetupAttachment(RootComponent);
-  LightComponent->SetMobility(EComponentMobility::Static);
+  LightComponent->SetMobility(EComponentMobility::Movable);
+
+  IntensityMultiplier                = 1.0f;
+  SaturationMultiplier               = 1.0f;
+  ContrastMultiplier                 = 1.0f;
+
+  Settings.bOverride_ColorSaturation = true;
+  Settings.bOverride_ColorContrast   = true;
 }
