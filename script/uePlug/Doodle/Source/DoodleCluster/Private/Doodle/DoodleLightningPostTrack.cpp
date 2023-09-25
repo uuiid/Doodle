324f24f6550980f7f32dac6ff8b9b2ffc672dc50
uuiid <957714080@qq.com>
2023-09-25 15:42:41 +0800

    [灯光工具] 添加灯光编辑器轨道类

---
+++ script/uePlug/Doodle/Source/DoodleCluster/Private/Doodle/DoodleLightningPostTrack.cpp
@@ -0,0 +1,62 @@
+﻿#include "Doodle/DoodleLightningPostTrack.h"
+
+#include "Doodle/DoodleLightningPostSection.h"
+// #include "GeometryCacheComponent.h"
+// #include "MovieSceneGeometryCacheSection.h"
+// #include "Compilation/MovieSceneCompilerRules.h"
+#include "Detail/DoodleLightningPostSectionTemplate.h"
+#include "Evaluation/MovieSceneEvalTemplate.h"
+#include "Evaluation/MovieSceneEvaluationTrack.h"
+// #include "MovieSceneGeometryCacheTemplate.h"
+// #include "Compilation/IMovieSceneTemplateGenerator.h"
+// #include "MovieSceneGeometryCacheTemplate.h"
+// #include "MovieScene.h"
+
+UDoodleLightningPostTrack::UDoodleLightningPostTrack(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer) {
+#if WITH_EDITORONLY_DATA
+  TrackTint = FColor(124, 15, 124, 65);
+#endif
+
+  SupportedBlendTypes.Add(EMovieSceneBlendType::Absolute);
+
+  EvalOptions.bCanEvaluateNearestSection = true;
+  EvalOptions.bEvaluateInPreroll         = true;
+}
+
+void UDoodleLightningPostTrack::RemoveAllAnimationData() { AnimationSections.Emplace(); }
+
+bool UDoodleLightningPostTrack::HasSection(const UMovieSceneSection& Section) const {
+  return AnimationSections.Contains(&Section);
+}
+
+void UDoodleLightningPostTrack::AddSection(UMovieSceneSection& Section) { AnimationSections.Add(&Section); }
+
+void UDoodleLightningPostTrack::RemoveSection(UMovieSceneSection& Section) { AnimationSections.Remove(&Section); }
+
+void UDoodleLightningPostTrack::RemoveSectionAt(int32 SectionIndex) { AnimationSections.RemoveAt(SectionIndex); }
+
+bool UDoodleLightningPostTrack::IsEmpty() const { return AnimationSections.IsEmpty(); }
+
+const TArray<UMovieSceneSection*>& UDoodleLightningPostTrack::GetAllSections() const { return AnimationSections; }
+
+bool UDoodleLightningPostTrack::SupportsType(TSubclassOf<UMovieSceneSection> SectionClass) const {
+  return SectionClass == UDoodleLightningPostTrack::StaticClass();
+}
+
+UMovieSceneSection* UDoodleLightningPostTrack::CreateNewSection() {
+  return NewObject<UDoodleLightningPostSection>(
+      this, UDoodleLightningPostSection::StaticClass(), NAME_None, RF_Transactional
+  );
+}
+
+bool UDoodleLightningPostTrack::SupportsMultipleRows() const { return false; }
+
+FMovieSceneEvalTemplatePtr UDoodleLightningPostTrack::CreateTemplateForSection(const UMovieSceneSection& InSection
+) const {
+  return FDoodleLightningPostSectionTemplate{CastChecked<const UDoodleLightningPostSection>(&InSection)};
+}
+
+FText UDoodleLightningPostTrack::GetDefaultDisplayName() const {
+  return FText::FromString("Doodle Lightning Post Track");
+}
---
+++ script/uePlug/Doodle/Source/DoodleCluster/Public/Doodle/DoodleLightningPostTrack.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#pragma once
+
+#include "Compilation/IMovieSceneTrackTemplateProducer.h"
+#include "CoreMinimal.h"
+#include "DoodleLightningPostTrack.generated.h"
+#include "MovieSceneNameableTrack.h"
+#include "UObject/ObjectMacros.h"
+
+UCLASS(MinimalAPI)
+class UDoodleLightningPostTrack : public UMovieSceneNameableTrack, public IMovieSceneTrackTemplateProducer {
+  GENERATED_UCLASS_BODY()
+ public:
+  // UMovieSceneTrack interface
+  virtual void RemoveAllAnimationData() override;
+  virtual bool HasSection(const UMovieSceneSection &Section) const override;
+  virtual void AddSection(UMovieSceneSection &Section) override;
+  virtual void RemoveSection(UMovieSceneSection &Section) override;
+  virtual void RemoveSectionAt(int32 SectionIndex) override;
+  virtual bool IsEmpty() const override;
+  virtual const TArray<UMovieSceneSection *> &GetAllSections() const override;
+  virtual bool SupportsType(TSubclassOf<UMovieSceneSection> SectionClass) const override;
+  virtual UMovieSceneSection *CreateNewSection() override;
+  /// 是否支持多个
+  virtual bool SupportsMultipleRows() const override;
+
+  // ~IMovieSceneTrackTemplateProducer interface
+  /// 创建评估自定义类
+  virtual FMovieSceneEvalTemplatePtr CreateTemplateForSection(const UMovieSceneSection &InSection) const override;
+
+#if WITH_EDITORONLY_DATA
+  virtual FText GetDefaultDisplayName() const override;
+#endif
+
+ private:
+  /** List of all animation sections */
+  UPROPERTY()
+  TArray<UMovieSceneSection *> AnimationSections;
+};
