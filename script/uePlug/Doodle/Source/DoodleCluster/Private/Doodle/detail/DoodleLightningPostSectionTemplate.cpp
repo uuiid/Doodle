14e504c6027fa9258012111e34d508708fd8e634
uuiid <957714080@qq.com>
2023-09-25 15:42:25 +0800

    [灯光工具] 添加灯光编辑器评估模板类和评估类

---
+++ script/uePlug/Doodle/Source/DoodleCluster/Private/Doodle/detail/DoodleLightningPostSectionTemplate.cpp
@@ -0,0 +1,60 @@
+﻿#include "DoodleLightningPostSectionTemplate.h"
+
+#include "Components/DirectionalLightComponent.h"
+#include "Doodle/DoodleLightningPost.h"
+#include "Doodle/DoodleLightningPostSection.h"
+
+struct FLightingPostEventTrackExecutionToken : IMovieSceneExecutionToken {
+  FLightingPostEventTrackExecutionToken(const FDoodleLightningPostSectionTemplate* InSectionTemplate)
+      : SectionTemplate(InSectionTemplate) {}
+
+  virtual void Execute(
+      const FMovieSceneContext& Context, const FMovieSceneEvaluationOperand& Operand,
+      FPersistentEvaluationData& PersistentData, IMovieScenePlayer& Player
+  ) override {
+    MOVIESCENE_DETAILED_SCOPE_CYCLE_COUNTER(MovieSceneEval_EventTrack_TokenExecute_Doodle)
+
+    /// @brief 解析上下文
+    TArray<UObject*> EventContexts = Player.GetEventContexts();
+
+    for (auto i : Player.FindBoundObjects(Operand)) {
+      if (UObject* L_Object = i.Get();
+          SectionTemplate->Params->IntensityCurve && L_Object && L_Object->IsA<ADoodleLightingPost>()) {
+        ADoodleLightingPost* L_LightingPost = CastChecked<ADoodleLightingPost>(L_Object);
+        const float L_Current_Time          = Context.GetTime().GetFrame().Value;
+        const float L_Time_Range_Size       = Context.GetFrameNumberRange().Size<FFrameNumber>().Value;
+        const auto L_Time                   = L_Current_Time / L_Time_Range_Size;
+        const auto L_Curve_Time             = SectionTemplate->Params->IntensityCurve->GetFloatValue(L_Time);
+
+        L_LightingPost->GetLightComponent()->SetIntensity(L_Current_Time * L_LightingPost->IntensityMultiplier);
+        L_LightingPost->Settings.ColorSaturation[3] = L_Curve_Time * L_LightingPost->SaturationMultiplier;
+        L_LightingPost->Settings.ColorContrast[3]   = L_Curve_Time * L_LightingPost->ContrastMultiplier;
+      }
+    }
+  }
+
+  const FDoodleLightningPostSectionTemplate* SectionTemplate;
+};
+
+FDoodleLightningPostSectionTemplate::FDoodleLightningPostSectionTemplate() : Params(nullptr) {}
+
+FDoodleLightningPostSectionTemplate::FDoodleLightningPostSectionTemplate(const UDoodleLightningPostSection* Section)
+    : Params(Section) {}
+
+void FDoodleLightningPostSectionTemplate::Evaluate(
+    const FMovieSceneEvaluationOperand& Operand, const FMovieSceneContext& Context,
+    const FPersistentEvaluationData& PersistentData, FMovieSceneExecutionTokens& ExecutionTokens
+) const {
+  // 模式暂停或者禁音(禁用)状态下不进行评估
+  if (Context.GetStatus() == EMovieScenePlayerStatus::Stopped || Context.IsSilent()) {
+    return;
+  }
+  // 评估范围是否向后
+  const bool bBackwards = Context.GetDirection() == EPlayDirection::Backwards;
+  if (Params) {
+    TRange<FFrameNumber> L_Range = Params->GetRange();
+    if (L_Range.Contains(Context.GetTime().GetFrame())) {
+      ExecutionTokens.Add(FLightingPostEventTrackExecutionToken{this});
+    }
+  }
+}
---
+++ script/uePlug/Doodle/Source/DoodleCluster/Private/Doodle/detail/DoodleLightningPostSectionTemplate.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DoodleLightningPostSectionTemplate.generated.h"
+#include "Evaluation/MovieSceneEvalTemplate.h"
+#include "UObject/ObjectMacros.h"
+
+class UDoodleLightningPostSection;
+
+USTRUCT()
+struct FDoodleLightningPostSectionTemplate : public FMovieSceneEvalTemplate {
+  GENERATED_BODY()
+  FDoodleLightningPostSectionTemplate();
+  FDoodleLightningPostSectionTemplate(const UDoodleLightningPostSection* Section);
+
+  virtual UScriptStruct& GetScriptStructImpl() const override { return *StaticStruct(); }
+  virtual void Evaluate(
+      const FMovieSceneEvaluationOperand& Operand, const FMovieSceneContext& Context,
+      const FPersistentEvaluationData& PersistentData, FMovieSceneExecutionTokens& ExecutionTokens
+  ) const override;
+
+  //   void Setup(FPersistentEvaluationData &InPersistentData, IMovieScenePlayer &InPlayer) const override;
+
+  UPROPERTY()
+  const UDoodleLightningPostSection* Params;
+};
