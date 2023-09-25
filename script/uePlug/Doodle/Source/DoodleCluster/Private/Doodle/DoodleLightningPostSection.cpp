9f47efc112565cf91b94512020310a5b115122dd
uuiid <957714080@qq.com>
2023-09-25 15:41:51 +0800

    [灯光工具] 添加灯光编辑器部分类

---
+++ script/uePlug/Doodle/Source/DoodleCluster/Private/Doodle/DoodleLightningPostSection.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Doodle\DoodleLightningPostSection.h"
+
+void UDoodleLightningPostSection::ImportEntityImpl(
+    UMovieSceneEntitySystemLinker* EntityLinker, const FEntityImportParams& Params, FImportedEntity* OutImportedEntity
+) {}
---
+++ script/uePlug/Doodle/Source/DoodleCluster/Public/Doodle/DoodleLightningPostSection.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DoodleLightningPostSection.generated.h"
+#include "EntitySystem/IMovieSceneEntityProvider.h"
+#include "MovieSceneSection.h"
+
+class UMovieSceneSignedObject;
+
+class UMovieSceneEntitySystemLinker;
+class FEntityImportParams;
+class FImportedEntity;
+
+/**
+ *
+ */
+UCLASS()
+class DOODLECLUSTER_API UDoodleLightningPostSection : public UMovieSceneSection, public IMovieSceneEntityProvider {
+  GENERATED_BODY()
+ public:
+ public:
+  /// 开始 IMovieSceneEntityProvider 接口
+  virtual void ImportEntityImpl(
+      UMovieSceneEntitySystemLinker *EntityLinker, const FEntityImportParams &Params, FImportedEntity *OutImportedEntity
+  ) override;
+
+ protected:
+  // 开始 UMovieSceneSection 接口
+  // virtual TOptional<TRange<FFrameNumber>> GetAutoSizeRange() const override;
+  // virtual void TrimSection(FQualifiedFrameTime TrimTime, bool bTrimLeft, bool bDeleteKeys) override;
+  // virtual UMovieSceneSection *SplitSection(FQualifiedFrameTime SplitTime, bool bDeleteKeys) override;
+  // virtual void GetSnapTimes(TArray<FFrameNumber> &OutSnapTimes, bool bGetSectionBorders) const override;
+  // virtual TOptional<FFrameTime> GetOffsetTime() const override;
+
+ private:
+ public:
+  /** 强度曲线 */
+  UPROPERTY(EditAnywhere, Category = "Section")
+  TObjectPtr<UCurveFloat> IntensityCurve;
+};
