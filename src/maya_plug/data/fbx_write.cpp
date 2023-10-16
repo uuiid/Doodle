99d922e46b88435a295110a482281c0a7e811dad
uuiid <957714080@qq.com>
2023-10-16 13:09:13 +0800

    [自动化导出] 提取fbx写出类

---
+++ src/maya_plug/data/fbx_write.cpp
@@ -0,0 +1,9 @@
+//
+// Created by td_main on 2023/10/16.
+//
+
+#include "fbx_write.h"
+
+namespace doodle {
+namespace maya_plug {}  // namespace maya_plug
+}  // namespace doodle
---
+++ src/maya_plug/data/fbx_write.h
@@ -0,0 +1,12 @@
+//
+// Created by td_main on 2023/10/16.
+//
+
+#pragma once
+namespace doodle {
+namespace maya_plug {
+
+class fbx_write {};
+
+}  // namespace maya_plug
+}  // namespace doodle
