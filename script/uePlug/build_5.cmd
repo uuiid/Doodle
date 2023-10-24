e25e0eb3205220b7caa36ee686873045a0343a85
uuiid <957714080@qq.com>
2023-10-24 10:33:43 +0800

    [自动化灯光] 升级插件到5.3

--- script/uePlug/build_5.cmd
+++ script/uePlug/build_5.cmd
@@ -1,4 +1,4 @@
-set UE_VERSION=5.2
+set UE_VERSION=5.3
 set UE_5="D:\Program Files\Epic Games\UE_%UE_VERSION%\Engine\Build\BatchFiles\RunUAT.bat"
 set work_dir=%~dp0
 call %UE_5% BuildPlugin -Plugin=%work_dir%Doodle\Doodle.uplugin -TargetPlatforms=Win64 -Package=%work_dir%%UE_VERSION%\Plugins\Doodle -Rocket
