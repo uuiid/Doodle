463d4db023c770fb0d0ff7de1ee39f2ccfb1d469
uuiid <957714080@qq.com>
2023-10-26 11:56:49 +0800

    [自动化灯光] 调整测试

--- src/test/core/time.cpp
+++ src/test/core/time.cpp
@@ -31,29 +31,6 @@ struct time_cpp_suite {
 }  // namespace
 BOOST_FIXTURE_TEST_SUITE(tset_time, time_cpp_suite)
 
-BOOST_AUTO_TEST_CASE(date_) {
-  using namespace std::literals;
-  /// utc时间为 2022/05/07 3:46:55
-  auto l_time_l = time_point_wrap{date::local_days{2022_y / 5 / 7_d} + 11h + 46min + 55s};
-  /// utc时间为 2022/05/07 11:46:55
-  auto l_time_s = time_point_wrap{date::sys_days{2022_y / 5 / 7_d} + 11h + 46min + 55s};
-  /// utc时间为 2022/05/07 11:46:55
-  auto l_time_3 = chrono::time_point<chrono::system_clock, chrono::seconds>{
-      chrono::round<chrono::seconds>(l_time_l.get_local_time().time_since_epoch())};
-  BOOST_TEST(
-      date::format("%Y/%m/%d %H:%M:%S", date::local_days{2022_y / 5 / 7_d} + 11h + 46min + 55s) ==
-      "2022/05/07 11:46:55"s
-  );
-  BOOST_TEST(
-      date::format("%Y/%m/%d %H:%M:%S", date::sys_days{2022_y / 5 / 7_d} + 11h + 46min + 55s) == "2022/05/07 11:46:55"s
-  );
-
-  BOOST_TEST(date::format("%Y/%m/%d %H:%M:%S", l_time_l.get_sys_time()) == "2022/05/07 03:46:55.0000000"s);
-  BOOST_TEST(date::format("%Y/%m/%d %H:%M:%S", l_time_s.get_sys_time()) == "2022/05/07 11:46:55.0000000"s);
-  BOOST_TEST(date::format("%Y/%m/%d %H:%M:%S", l_time_3) == "2022/05/07 11:46:55"s);
-
-  BOOST_TEST(date::format("%Y/%m/%d %H:%M:%S", l_time_3) == fmt::format("{:%Y/%m/%d %H:%M:%S}", l_time_3));
-}
 
 BOOST_AUTO_TEST_CASE(time_warp_fmt_test) {
   time_point_wrap l_time{2022, 5, 7, 11, 46, 55};
@@ -68,9 +45,9 @@ BOOST_AUTO_TEST_CASE(time_warp_fmt_test) {
 
 BOOST_AUTO_TEST_CASE(time_warp_local_to_sys) {
   using namespace chrono::literals;
-  time_point_wrap l_sys_time{time_point_wrap::time_point{chrono::sys_days{2022_y / 7 / 30_d} + chrono::hours{7}}};
+  time_point_wrap l_sys_time{time_point_wrap::time_point{chrono::sys_days{2022y / 7 / 30d} + chrono::hours{7}}};
   time_point_wrap l_local_time{
-      time_point_wrap::time_local_point{chrono::local_days{2022_y / 7 / 30_d} + chrono::hours{7}}};
+      time_point_wrap::time_local_point{chrono::local_days{2022y / 7 / 30d} + chrono::hours{7}}};
 
   auto l_time_du = chrono::duration_cast<chrono::hours>(l_sys_time - l_local_time);
 
