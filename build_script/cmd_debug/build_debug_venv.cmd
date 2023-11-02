c2893ccb6b4219fca9815d3b4e4f5ce6c05b3ae5
uuiid <957714080@qq.com>
2023-11-02 11:34:08 +0800

    [自动化灯光] 完成调整安装逻辑

--- build_script/cmd_debug/build_debug_venv.cmd
+++ build_script/cmd_debug/build_debug_venv.cmd
@@ -1,15 +1,5 @@
 call %~dp0/../build/set_venv.cmd
 
-
-echo -----------------config main exe--------------------
-"C:\Program Files\CMake\bin\cmake.exe" ^
--S%my_pwd% ^
---preset Ninja_debug%Doodle_suffix%
-
-if %errorlevel% NEQ 0 exit 1
-
 echo -----------------build main exe--------------------
 "C:\Program Files\CMake\bin\cmake.exe" ^
---build ^
---preset Ninja_debug%Doodle_suffix% ^
---target %*
+--workflow --preset build_release%Doodle_suffix%
