5c78fcb09be23a7b7c090fedadc2c3108de04fe6
uuiid <957714080@qq.com>
2023-11-07 11:16:30 +0800

    [自动化灯光] 添加初始化窗口

---
+++ src/doodle_app/gui/open_project.cpp
@@ -0,0 +1,6 @@
+//
+// Created by td_main on 2023/11/7.
+//
+
+#include "open_project.h"
+namespace doodle::gui {}
---
+++ src/doodle_app/gui/open_project.h
@@ -0,0 +1,13 @@
+//
+// Created by td_main on 2023/11/7.
+//
+#pragma once
+
+namespace doodle::gui {
+class open_project {
+ public:
+  open_project()  = default;
+  ~open_project() = default;
+};
+
+}  // namespace doodle::gui
