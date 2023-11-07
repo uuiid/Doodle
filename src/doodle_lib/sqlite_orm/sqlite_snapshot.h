25a269adec52817c494bebdd500fa1b00a9e0f5a
uuiid <957714080@qq.com>
2023-11-07 09:24:06 +0800

    [自动化灯光] 调整快照方法

--- src/doodle_lib/sqlite_orm/sqlite_snapshot.h
+++ src/doodle_lib/sqlite_orm/sqlite_snapshot.h
@@ -10,26 +10,50 @@
 #include <entt/entt.hpp>
 namespace doodle::snapshot {
 
-template <typename... Component>
 class sqlite_snapshot {
-  std::tuple<std::deque<std::pair<entt::entity, Component>>...> tuple_{};
+  //  std::tuple<std::deque<std::pair<entt::entity, Component>>...> tuple_{};
   std::underlying_type_t<entt::entity> current_component_size_{};
   entt::entity current_entity_{};
-  static constexpr auto hana_tup_ = boost::hana::make_tuple(boost::hana::type_c<Component>...);
+  //  static constexpr auto hana_tup_ = boost::hana::make_tuple(boost::hana::type_c<Component>...);
 
   FSys::path data_path_{};
+
+  class base_data {
+   public:
+    virtual ~base_data()                              = default;
+
+    [[nodiscard]] virtual std::size_t size() const    = 0;
+    [[nodiscard]] virtual entt::entity entity() const = 0;
+  };
+  std::shared_ptr<base_data> current_data_{};
+  std::deque<std::shared_ptr<base_data>> data_deque_{};
+
+  template <typename Component>
+  class data_impl : base_data {
+   public:
+    using data_tuple        = std::tuple<entt::entity, Component>;
+    using data_tuple_vector = std::vector<data_tuple>;
+    using data_iter         = typename data_tuple_vector::iterator;
+    data_tuple_vector data_{};
+    mutable data_iter begin_{};
+    data_impl()          = default;
+    virtual ~data_impl() = default;
+
+    inline void next() { ++begin_; }
+
+   private:
+    [[nodiscard]] inline size_t size() const override {
+      return data_.size();
+      begin_ = data_.begin();
+    }
+    [[nodiscard]] inline entt::entity entity() const override { return std::get<0>(*begin_); }
+  };
 
  public:
   explicit sqlite_snapshot(FSys::path in_data_path)
       : data_path_(in_data_path.empty() ? FSys::path{":memory:"} : std::move(in_data_path)) {}
 
   virtual ~sqlite_snapshot() = default;
-
-  template <typename It, typename... Component_1>
-  void save(
-      const entt::registry& in_registry,
-      const boost::hana::map<boost::hana::pair<Component_1, std::pair<It, It>>...>& in_map_iter
-  );
 
   // 先是大小
   void operator()(std::underlying_type_t<entt::entity> in_underlying_type);
@@ -39,55 +63,30 @@ class sqlite_snapshot {
   void operator()(std::underlying_type_t<entt::entity>& in_underlying_type);
   void operator()(entt::entity& in_entity);
 
+  template <typename... Component>
+  void load_(entt::snapshot_loader& in_loader) {
+    (in_loader.get<Component>(*this), ...);
+  }
+
   // 组件的加载和保存
   template <typename T>
   void operator()(const T& in_t) {
-    if (current_component_size_ != 0) {
-      std::get<std::vector<T>>(tuple_).reserve(current_component_size_);
-      current_component_size_ = 0;
+    if (!current_data_) {
+      current_data_ = data_deque_.emplace_back(std::make_shared<data_impl<T>>());
+      std::dynamic_pointer_cast<data_impl<T>&>(current_data_).data_.reserve(current_component_size_);
     }
-    std::get<std::vector<T>>(tuple_).emplace_back(current_entity_, in_t);
+
+    if (current_data_) {
+      auto& l_data = std::dynamic_pointer_cast<data_impl<T>&>(current_data_);
+      l_data.data_.emplace_back(current_entity_, in_t);
+    }
   }
   template <typename T>
   void operator()(T& in_t) {
-    auto& l_vector = std::get<std::vector<T>>(tuple_);
-    in_t           = std::move(l_vector.front().second);
-    l_vector.pop_front();
-
-    if (l_vector.empty()) {
-      constexpr auto l_index = *boost::hana::index_if(hana_tup_, boost::hana::is_a<T>);
-      if constexpr (l_index + 1 < sizeof...(Component)) {
-        current_component_size_ = std::get<l_index + 1>(tuple_).size();
-        current_entity_         = std::get<l_index + 1>(tuple_).front().first;
-      }
-    } else {
-      current_entity_ = l_vector.front().first;
-    }
+    auto l_data = std::dynamic_pointer_cast<data_impl<T>>(current_data_);
+    in_t        = std::get<1>(l_data->data_.front());
+    l_data->next();
   }
 };
-
-template <typename... Component>
-void sqlite_snapshot<Component...>::operator()(std::underlying_type_t<entt::entity> in_underlying_type) {
-  current_component_size_ = in_underlying_type;
-}
-template <typename... Component>
-void sqlite_snapshot<Component...>::operator()(entt::entity in_entity) {
-  current_entity_ = in_entity;
-}
-template <typename... Component>
-void sqlite_snapshot<Component...>::operator()(std::underlying_type_t<entt::entity>& in_underlying_type) {
-  in_underlying_type = current_component_size_;
-}
-template <typename... Component>
-void sqlite_snapshot<Component...>::operator()(entt::entity& in_entity) {
-  in_entity = current_entity_;
-}
-
-template <typename... Component>
-template <typename It, typename... Component_1>
-void sqlite_snapshot<Component...>::save(
-    const entt::registry& in_registry,
-    const boost::hana::map<boost::hana::pair<Component_1, std::pair<It, It>>...>& in_map_iter
-) {}
 
 }  // namespace doodle::snapshot
