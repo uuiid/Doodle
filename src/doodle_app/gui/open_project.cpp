2972135813b982ee9763dc7b664e630b349f0605
uuiid <957714080@qq.com>
2023-11-08 09:39:54 +0800

    [自动化灯光] 调整初始化窗口授权显示

--- src/doodle_app/gui/open_project.cpp
+++ src/doodle_app/gui/open_project.cpp
@@ -8,9 +8,12 @@
 #include <doodle_core/database_task/sqlite_client.h>
 
 #include <doodle_app/app/app_command.h>
+#include <doodle_app/app/authorization.h>
 #include <doodle_app/gui/base/ref_base.h>
 #include <doodle_app/gui/open_file_dialog.h>
 #include <doodle_app/lib_warp/imgui_warp.h>
+
+#include <fmt/chrono.h>
 namespace doodle::gui {
 #ifdef DNDEBUG
 constexpr auto main_project{"//192.168.10.250/public/Prism_projects/db_file/doodle_main.doodle_db"};
@@ -18,7 +21,18 @@ constexpr auto main_project{"//192.168.10.250/public/Prism_projects/db_file/dood
 constexpr auto main_project{"E:/cache/doodle_main.doodle_db"};
 #endif
 
+open_project::open_project() : auth_ptr_(std::make_shared<authorization>()) {
+  const auto l_time = chrono::floor<chrono::days>(auth_ptr_->get_expire_time());
+  //  expire_time_str_  = fmt::vformat("还有 {:%d} 天到期", fmt::make_format_args(l_time));
+  expire_time_str_  = fmt::format("还有 {} 天到期", l_time);
+}
+
 bool open_project::render() {
+  ImGui::Text(expire_time_str_.c_str());
+  if (ImGui::InputText(*auth_code_, &auth_code_)) {
+    auth_ptr_->load_authorization_data(auth_code_);
+    expire_time_str_ = fmt::format("还有 {} 天到期", chrono::floor<chrono::days>(auth_ptr_->get_expire_time()));
+  }
   if (ImGui::Button("主项目")) {
     g_ctx().get<database_n::file_translator_ptr>()->async_open(main_project);
     open = false;
--- src/doodle_app/gui/open_project.h
+++ src/doodle_app/gui/open_project.h
@@ -4,12 +4,20 @@
 #pragma once
 #include <doodle_core/doodle_core_fwd.h>
 
+#include <doodle_app/doodle_app_fwd.h>
+#include <doodle_app/gui/base/ref_base.h>
+
 namespace doodle::gui {
 class open_project {
   bool open{true};
+  std::shared_ptr<authorization> auth_ptr_;
+  // 到期时间
+  std::string expire_time_str_;
+  // 授权码
+  gui_cache<std::string> auth_code_{"授权码"};
 
  public:
-  open_project()  = default;
+  open_project();
   ~open_project() = default;
   constexpr static std::string_view name{"初始化"};
   static constexpr std::array<float, 2> sizexy{640, 360};
