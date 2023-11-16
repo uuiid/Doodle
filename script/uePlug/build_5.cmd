583860a66c7e73f31248a265896a1bd792880448
uuiid <957714080@qq.com>
2023-11-16 14:25:34 +0800

    [自动化灯光] 添加构建备份脚本

--- script/uePlug/build_5.cmd
+++ script/uePlug/build_5.cmd
@@ -1,6 +1,11 @@
 set UE_VERSION=5.2
 set UE_5="D:\Program Files\Epic Games\UE_%UE_VERSION%\Engine\Build\BatchFiles\RunUAT.bat"
 set work_dir=%~dp0
+set l_time=%DATE:/=_%%TIME::=_%
+set l_time=%l_time:.=_%
+set l_time=%l_time: =%
 call %UE_5% BuildPlugin -Plugin=%work_dir%Doodle\Doodle.uplugin -TargetPlatforms=Win64 -Package=%work_dir%%UE_VERSION%\Plugins\Doodle -Rocket
-@REM rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Intermediate
+robocopy %work_dir%%UE_VERSION%\Plugins\Doodle %work_dir%%UE_VERSION%\Plugins\Doodle_%l_time% /mir /ndl /np /njh /njs /ns /nc /nfl
+
+rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Intermediate
 rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Source
