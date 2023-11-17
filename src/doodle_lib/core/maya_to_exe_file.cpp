01a7bd94f470df24fa98ad79f2312c80cce598d0
uuiid <957714080@qq.com>
2023-11-17 17:34:16 +0800

    [自动化灯光] 添加maya到ue类

---
+++ src/doodle_lib/core/maya_to_exe_file.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2023/11/17.
+//
+
+#include "maya_to_exe_file.h"
---
+++ src/doodle_lib/core/maya_to_exe_file.h
@@ -0,0 +1,12 @@
+//
+// Created by TD on 2023/11/17.
+//
+
+#pragma once
+namespace doodle {
+
+class maya_to_exe_file {
+ public:
+};
+
+}  // namespace doodle
