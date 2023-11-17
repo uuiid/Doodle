e994110c11f38efee12e8d18b22f59681be676f1
uuiid <957714080@qq.com>
2023-11-17 16:52:38 +0800

    [自动化灯光] 添加测试

--- src/test/core/folder_is_save.cpp
+++ src/test/core/folder_is_save.cpp
@@ -3,4 +3,16 @@
 //
 
 #include <doodle_core/doodle_core_fwd.h>
-int core_folder_is_save(int argc, char* argv[]) { return 0; }
+#include <doodle_core/logger/logger.h>
+int core_folder_is_save(int argc, char* argv[]) {
+  using namespace doodle;
+  if (FSys::folder_is_save("//192.168.20.7/test_delte/test.txt")) {
+    std::cout << "test_folder_is_save true" << std::endl;
+  } else
+    return 1;
+  if (!FSys::folder_is_save("//192.168.20.7/test_read_write/test.txt")) {
+    std::cout << "test_folder_is_save false" << std::endl;
+  } else
+    return 1;
+  return 0;
+}
