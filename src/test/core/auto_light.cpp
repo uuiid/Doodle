54d40b530936ff3e5c1cde59c797324aa54a1d34
uuiid <957714080@qq.com>
2023-11-24 17:24:03 +0800

    [自动化灯光] 调整测试

--- src/test/core/auto_light.cpp
+++ src/test/core/auto_light.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <doodle_core/database_task/sqlite_client.h>
+#include <doodle_core/lib_warp/boost_fmt_error.h>
 #include <doodle_core/metadata/assets_file.h>
 #include <doodle_core/metadata/file_association.h>
 #include <doodle_core/metadata/main_map.h>
@@ -79,8 +80,24 @@ void test_fun3(boost::system::error_code in_code) {
   k_arg.out_path_file_   = l_out_path;
   k_arg.export_anim_time = g_reg()->ctx().get<project_config::base_config>().export_anim_time;
   k_arg.project_         = g_ctx().get<database_n::file_translator_ptr>()->get_project_path();
+  auto l_ue_msg          = entt::handle{*g_reg(), g_reg()->create()};
   l_maya_exe->async_run_maya(
-      entt::handle{*g_reg(), g_reg()->create()}, k_arg, maya_to_exe_file{FSys::path{l_out_path}}
+      entt::handle{*g_reg(), g_reg()->create()}, k_arg,
+      maya_to_exe_file{entt::handle{}, FSys::path{l_out_path}}.set_ue_call_fun(boost::asio::bind_executor(
+          g_io_context(),
+          [l_ue_msg](boost::system::error_code in_code) {
+            auto& l_msg = l_ue_msg.get<process_message>();
+            if (in_code) {
+              auto l_msg_str = fmt::format("render error:{}", in_code);
+              log_error(l_msg_str);
+              l_msg.message(l_msg_str);
+              l_msg.set_state(l_msg.fail);
+              return;
+            }
+            l_msg.set_name("排屏完成");
+            l_msg.set_state(l_msg.success);
+          }
+      ))
   );
 }
 
@@ -90,11 +107,13 @@ void test_fun2(boost::system::error_code in_code) {
   l_handle.emplace<assets_file>().path_attr("C:/sy/LianQiShiWanNian_8/6-moxing/Ch/JDCh_05/Ch426A/Rig/Ch426A_rig_wxc.ma"
   );
   FSys::software_flag_file(l_handle.get<assets_file>().path_attr(), l_handle.get<database>().uuid());
+  log_info(fmt::format("Ch426A_rig_wxc uuid {}", l_handle.get<database>().uuid()));
 
   auto l_handle_ue{entt::handle{*g_reg(), g_reg()->create()}};
-  l_handle.emplace<database>();
-  l_handle.emplace<assets_file>().path_attr("C:/sy/LianQiShiWanNian_8/6-moxing/Ch/JDCh_05/Ch426A/BuJiaBan_UE5");
+  l_handle_ue.emplace<database>();
+  l_handle_ue.emplace<assets_file>().path_attr("C:/sy/LianQiShiWanNian_8/6-moxing/Ch/JDCh_05/Ch426A/BuJiaBan_UE5");
   FSys::software_flag_file(l_handle_ue.get<assets_file>().path_attr(), l_handle_ue.get<database>().uuid());
+  log_info(fmt::format("BuJiaBan_UE5 uuid {}", l_handle_ue.get<database>().uuid()));
 
   auto l_handle2{entt::handle{*g_reg(), g_reg()->create()}};
   l_handle2.emplace<database>();
@@ -102,6 +121,7 @@ void test_fun2(boost::system::error_code in_code) {
       "C:/sy/LianQiShiWanNian_8/6-moxing/BG/JD_05/BG027C/Mod/YuDaoZong_TingYuan_Low.ma"
   );
   FSys::software_flag_file(l_handle2.get<assets_file>().path_attr(), l_handle2.get<database>().uuid());
+  log_info(fmt::format("YuDaoZong_TingYuan_Low uuid {}", l_handle2.get<database>().uuid()));
 
   auto l_handle_ue2{entt::handle{*g_reg(), g_reg()->create()}};
   l_handle_ue2.emplace<database>();
@@ -109,6 +129,7 @@ void test_fun2(boost::system::error_code in_code) {
 
   l_handle_ue2.emplace<ue_main_map>(R"(/Game/YuDaoZong_TingYuan/Map/YuDaoZong_TingYuan)");
   FSys::software_flag_file(l_handle_ue2.get<assets_file>().path_attr(), l_handle_ue2.get<database>().uuid());
+  log_info(fmt::format("YuDaoZong_TingYuan uuid {}", l_handle_ue2.get<database>().uuid()));
 
   auto l_handle3{entt::handle{*g_reg(), g_reg()->create()}};
   l_handle3.emplace<database>();
@@ -125,7 +146,7 @@ void test_fun2(boost::system::error_code in_code) {
 
   l_handle2.emplace<file_association_ref>(l_handle4);
   l_handle_ue2.emplace<file_association_ref>(l_handle4);
-  next_time_run({0, 0, 1}, doodle::test_fun2);
+  doodle::test_fun3({});
 }
 
 void test_fun(boost::system::error_code in_code) {
