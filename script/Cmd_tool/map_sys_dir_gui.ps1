6f5229d8573418d76706a46c0c1777f6695e891b
uuiid <957714080@qq.com>
2023-11-29 17:40:42 +0800

    [自动化灯光] 调整外包映射

--- script/Cmd_tool/map_sys_dir_gui.ps1
+++ script/Cmd_tool/map_sys_dir_gui.ps1
@@ -11,7 +11,7 @@ $map_item = @(
 @( "C:\sy\WuJinShenYu_8", "", "无尽神域" ),
 @( "C:\sy\WanYuFengShen_9", "", "万域封神" ),
 @( "C:\sy\KuangShenMoZun_9", "", "狂神魔尊" ),
-@( "C:\sy\DuBuXiaoYao_3", "", "独步万古" )
+@( "C:\sy\WGXD", "", "独步万古" )
 )
 
 function Add-SyDir
