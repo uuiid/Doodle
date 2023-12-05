5d9368721c0b60df7ef88ebf72caa2107b0c8cbe
uuiid <957714080@qq.com>
2023-12-05 10:01:20 +0800

    [自动化灯光] 添加数据库工具面板

--- src/doodle_lib/gui/widgets/database_tool.cpp
+++ src/doodle_lib/gui/widgets/database_tool.cpp
@@ -3,3 +3,85 @@
 //
 
 #include "database_tool.h"
+
+#include <doodle_core/metadata/assets_file.h>
+#include <doodle_core/metadata/metadata.h>
+
+#include <doodle_app/lib_warp/imgui_warp.h>
+
+#include "doodle_lib_fwd.h"
+
+#include <entt/entt.hpp>
+namespace doodle::gui {
+
+namespace {}
+
+void database_tool_t::list_repeat() {
+  auto l_view = g_reg()->view<database, assets_file>().each();
+
+  auto l_uuid_map =
+      ranges::views::transform(
+          l_view,
+          [](const decltype(*l_view.begin())& in_tuple) {
+            return repeat_item{
+                std::get<database&>(in_tuple).uuid(), std::get<entt::entity>(in_tuple),
+                std::get<assets_file&>(in_tuple).path_attr(), std::get<assets_file&>(in_tuple).name_attr(), ""s};
+          }
+      ) |
+      ranges::to_vector;
+  std::set<uuid> l_uuid_set{};
+  for (auto&& l_item : l_uuid_map) {
+    if (l_uuid_set.contains(l_item.uuid_)) {
+      l_item.info_ = "重复的uuid"s;
+    }
+    l_uuid_set.insert(l_item.uuid_);
+  }
+
+  l_uuid_map |= ranges::actions::stable_sort([](const repeat_item& in_l, const repeat_item& in_r) {
+    return in_l.uuid_ < in_r.uuid_;
+  });
+
+  l_uuid_map |= ranges::actions::stable_sort([](const repeat_item& in_l, const repeat_item& in_r) {
+    return in_l.path_ < in_r.path_;
+  });
+  repeat_list_ = std::move(l_uuid_map);
+  repeat_list_gui_ =
+      ranges::views::transform(
+          repeat_list_,
+          [](const repeat_item& in_item) {
+            return repeat_item_gui{
+                fmt::format("##{}", in_item.uuid_), in_item.path_.generic_string(), in_item.name_, in_item.info_};
+          }
+      ) |
+      ranges::to_vector;
+}
+
+bool database_tool_t::render() {
+  if (ImGui::Button(*list_repeat_id)) {
+    list_repeat();
+  }
+  //  ImGui::SameLine();
+  //  if (ImGui::Button(*list_repeat_name_id)) {
+  //  }
+
+  if (auto l_table = dear::Table{*list_repeat_table_id, 4}; l_table) {
+    ImGui::TableSetupColumn("uuid");
+    ImGui::TableSetupColumn("路径");
+    ImGui::TableSetupColumn("名称");
+    ImGui::TableSetupColumn("信息");
+    for (auto&& l_item : repeat_list_gui_) {
+      ImGui::TableNextRow();
+      ImGui::TableNextColumn();
+      dear::Text(l_item.id_);
+      ImGui::TableNextColumn();
+      dear::Text(l_item.path_);
+      ImGui::TableNextColumn();
+      dear::Text(l_item.name_);
+      ImGui::TableNextColumn();
+      dear::Text(l_item.info_);
+    }
+  }
+
+  return is_open;
+}
+}  // namespace doodle::gui
--- src/doodle_lib/gui/widgets/database_tool.h
+++ src/doodle_lib/gui/widgets/database_tool.h
@@ -2,5 +2,40 @@
 // Created by TD on 2023/12/4.
 //
 #pragma once
+#include <doodle_app/gui/base/ref_base.h>
+namespace doodle::gui {
+class DOODLELIB_API database_tool_t {
+  bool is_open{false};
+  // 列出重复的文件条目
+  gui_cache_name_id list_repeat_id{"列出重复的文件条目"s};
+  // 列出重复名称的文件条目
+  gui_cache_name_id list_repeat_name_id{"列出重复名称的文件条目"s};
 
-namespace doodle::gui {}
+  // 列出重复的文件条目
+
+  void list_repeat();
+  struct repeat_item {
+    uuid uuid_{};
+    entt::entity handle_{};
+    FSys::path path_{};
+    std::string name_{};
+    std::string info_{};
+  };
+  struct repeat_item_gui {
+    std::string id_{};
+    std::string path_{};
+    std::string name_{};
+    std::string info_{};
+  };
+
+  // 重复条目
+  std::vector<repeat_item> repeat_list_{};
+  std::vector<repeat_item_gui> repeat_list_gui_{};
+  gui_cache_name_id list_repeat_table_id{"重复条目列表"s};
+
+ public:
+  database_tool_t() = default;
+
+  bool render();
+};
+}  // namespace doodle::gui
