67377f7818ee91cd7485876b4571048ad816245b
uuiid <957714080@qq.com>
2023-12-06 09:40:53 +0800

    [自动化灯光] 调整取消

--- src/doodle_lib/exe_warp/ue_exe.cpp
+++ src/doodle_lib/exe_warp/ue_exe.cpp
@@ -31,31 +31,21 @@ class ue_exe::run_ue : public std::enable_shared_from_this<ue_exe::run_ue> {
 
   boost::asio::streambuf out_strbuff_attr{};
   boost::asio::streambuf err_strbuff_attr{};
-  boost::signals2::scoped_connection cancel_attr{};
 
  public:
+  boost::signals2::scoped_connection cancel_attr{};
   boost::process::child child_attr{};
   ue_exe::call_fun_type call_attr{};
   FSys::path ue_path{};
-  entt::handle mag_attr{};
   std::string arg_attr{};
   logger_ptr logger_attr{};
   ue_exe *self_{};
   void run() {
     if (ue_path.empty() || !FSys::exists(ue_path)) throw_exception(doodle_error{"ue_exe path is empty or not exists"});
-
-    auto &l_mag = mag_attr.patch<process_message>();
-
-    l_mag.set_state(l_mag.run);
     logger_attr->log(log_loc(), level::info, "开始运行 ue_exe: {} {}", ue_path, arg_attr);
-    cancel_attr = l_mag.aborted_sig.connect([this]() {
-      auto &&l_msg = mag_attr.get<process_message>();
-      l_msg.set_state(l_msg.fail);
-      l_msg.message("进程被主动结束\n");
-      cancel();
-    });
-    ue_path     = ue_path.lexically_normal();
-    child_attr  = boost::process::child{
+
+    ue_path    = ue_path.lexically_normal();
+    child_attr = boost::process::child{
         g_io_context(),
         //        boost::process::exe  = ue_path.generic_string(),
         //        boost::process::args = arg_attr,
@@ -63,8 +53,6 @@ class ue_exe::run_ue : public std::enable_shared_from_this<ue_exe::run_ue> {
         boost::process::std_err > err_attr,
         boost::process::on_exit =
             [this, l_self = shared_from_this()](int in_exit, const std::error_code &in_error_code) {
-              auto &&l_msg = mag_attr.get<process_message>();
-              l_msg.set_state(in_exit == 0 ? l_msg.success : l_msg.fail);
               logger_attr->log(log_loc(), level::err, "运行结束 ue_exe: {} 退出代码 {}", ue_path, in_exit);
               boost::asio::post(std::bind(std::move(call_attr), in_error_code));
               self_->notify_run();
@@ -78,13 +66,11 @@ class ue_exe::run_ue : public std::enable_shared_from_this<ue_exe::run_ue> {
     boost::asio::async_read_until(
         out_attr, out_strbuff_attr, '\n',
         [this, l_self = shared_from_this()](boost::system::error_code in_code, std::size_t in_n) {
-          auto &&l_msg = mag_attr.get<process_message>();
           if (!in_code) {
             /// @brief 此处在主线程调用
             std::string l_ine;
             std::istream l_istream{&out_strbuff_attr};
             std::getline(l_istream, l_ine);
-            l_msg.progress_step({1, 300});
             logger_attr->log(log_loc(), level::info, conv::to_utf<char>(l_ine, "GBK"));
             read_out();
           } else {
@@ -98,18 +84,16 @@ class ue_exe::run_ue : public std::enable_shared_from_this<ue_exe::run_ue> {
     boost::asio::async_read_until(
         err_attr, err_strbuff_attr, '\n',
         [this, l_self = shared_from_this()](boost::system::error_code in_code, std::size_t in_n) {
-          auto &&l_msg = mag_attr.get<process_message>();
           if (!in_code) {
             std::string l_line{};
             std::istream l_istream{&err_strbuff_attr};
             std::getline(l_istream, l_line);
             /// @brief 此处在主线程调用
-            l_msg.progress_step({1, 20000});
             logger_attr->log(log_loc(), level::warn, conv::to_utf<char>(l_line, "GBK"));
             read_err();
           } else {
             err_attr.close();
-            default_logger_raw()->error(conv::to_utf<char>(in_code.what(), "GBK"));
+            logger_attr->error(conv::to_utf<char>(in_code.what(), "GBK"));
           }
         }
     );
@@ -134,11 +118,16 @@ void ue_exe::queue_up(const entt::handle &in_msg, const std::string &in_command_
   if (ue_path_.empty()) find_ue_exe();
   auto l_run         = queue_list_.emplace(std::make_shared<run_ue>());
   l_run->ue_path     = ue_path_;
-  l_run->mag_attr    = in_msg;
   l_run->arg_attr    = in_command_line;
   l_run->call_attr   = std::move(in_call_fun);
   l_run->self_       = this;
   l_run->logger_attr = in_msg.patch<process_message>().logger();
+  l_run->cancel_attr =
+      in_msg.patch<process_message>().aborted_sig.connect([l_run_weak_ptr = l_run->weak_from_this()]() {
+        if (auto l_ptr = l_run_weak_ptr.lock(); l_ptr) {
+          l_ptr->cancel();
+        }
+      });
   notify_run();
 }
 void ue_exe::find_ue_exe() {
