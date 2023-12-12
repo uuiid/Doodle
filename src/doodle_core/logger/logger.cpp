564890c746b47b945506bc0839adec5f820b5810
uuiid <957714080@qq.com>
2023-12-12 14:57:44 +0800

    [自动化灯光] 调整日志等级

--- src/doodle_core/logger/logger.cpp
+++ src/doodle_core/logger/logger.cpp
@@ -81,6 +81,7 @@ void logger_ctrl::init_temp_log() {
 
   spdlog::flush_every(3s);
   spdlog::set_level(spdlog::level::trace);
+  spdlog::should_log(spdlog::level::trace);
 }
 
 logger_ctrl::async_logger_ptr logger_ctrl::make_log(const std::string &in_name, bool out_console) {
