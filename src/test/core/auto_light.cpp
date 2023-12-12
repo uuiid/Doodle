400f9674e54731483c1893b85a2c831556c4cf06
uuiid <957714080@qq.com>
2023-12-12 14:00:39 +0800

    [自动化灯光] 调整命令行

--- src/test/core/auto_light.cpp
+++ src/test/core/auto_light.cpp
@@ -169,7 +169,7 @@ void test_fun2(boost::system::error_code in_code) {
 
 void test_fun(boost::system::error_code in_code) {
   g_ctx().emplace<maya_exe_ptr>() = std::make_shared<maya_exe_test>();
-  g_ctx().emplace<ue_exe_ptr>()   = std::make_shared<ue_exe_test>();
+  //  g_ctx().emplace<ue_exe_ptr>()   = std::make_shared<ue_exe_test>();
 
   g_ctx().emplace<database_n::file_translator_ptr>()->set_only_open(true).async_open("E:/cache/doodle_main2.doodle_db");
   next_time_run({0, 0, 1}, doodle::test_fun2);
