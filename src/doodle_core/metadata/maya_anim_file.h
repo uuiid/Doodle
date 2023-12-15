f820e335e190796f231d09252b3d73d984bc7188
uuiid <957714080@qq.com>
2023-12-15 17:20:35 +0800

    [自动化灯光] 添加maya动画文件结构

--- src/doodle_core/metadata/maya_anim_file.h
+++ src/doodle_core/metadata/maya_anim_file.h
@@ -3,4 +3,25 @@
 //
 
 #pragma once
-namespace doodle {}  // namespace doodle
+#include <entt/entt.hpp>
+#include <vector>
+namespace doodle {
+/**
+ * 这个文件的路径储存在 asset_file 中
+ * 一个 maya 文件可能会有多个 maya_rig_file,
+ * 不一定每个 rig 都会有 sim_file
+ */
+
+class maya_anim_file {
+ public:
+  /// 开始帧
+  std::int32_t begin_frame_{};
+  /// 结束帧
+  std::int32_t end_frame_{};
+  /// maya绑定文件, 特指人物
+  std::vector<entt::handle> maya_rig_file_{};
+  /// 场景只有一个
+  entt::handle maya_scene_file_{};
+};
+
+}  // namespace doodle
