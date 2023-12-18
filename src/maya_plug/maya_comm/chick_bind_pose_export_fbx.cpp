ac7e8b17a8c8eaba78bcbcc995712d1753dcff50
uuiid <957714080@qq.com>
2023-12-18 17:03:50 +0800

    [自动化灯光] 添加maya检查方法

---
+++ src/maya_plug/maya_comm/chick_bind_pose_export_fbx.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2023/12/18.
+//
+
+#include "chick_bind_pose_export_fbx.h"
---
+++ src/maya_plug/maya_comm/chick_bind_pose_export_fbx.h
@@ -0,0 +1,8 @@
+//
+// Created by TD on 2023/12/18.
+//
+
+#pragma once
+
+#include <maya_plug/main/maya_plug_fwd.h>
+namespace doodle::maya_plug {}
