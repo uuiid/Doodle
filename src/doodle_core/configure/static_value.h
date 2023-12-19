f5c17d09aa7172761ddb97355def530065e01b95
uuiid <957714080@qq.com>
2023-12-19 14:36:14 +0800

    [自动化灯光] 添加id标签

--- src/doodle_core/configure/static_value.h
+++ src/doodle_core/configure/static_value.h
@@ -53,6 +53,7 @@ constexpr const std::string_view all_user_view_widget{"用户列表"};
 constexpr const std::string_view render_monitor{"渲染监控"};
 constexpr const std::string_view work_hour_filling{"工时填报"};
 constexpr const std::string_view database_tool{"库管理工具"};
+constexpr const std::string_view scan_assets{"扫瞄添加资产"};
 
 }  // namespace gui::config::menu_w
 }  // namespace doodle
--- src/doodle_lib/gui/widgets/scan_assets.cpp
+++ src/doodle_lib/gui/widgets/scan_assets.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "scan_assets.h"
+
+#include <doodle_app/lib_warp/imgui_warp.h>
+namespace doodle::gui {
+
+namespace details {}
+
+void scan_assets_t::start_scan() {}
+
+bool scan_assets_t::render() {
+  if (ImGui::Button(*start_scan_id)) {
+    start_scan();
+  }
+  return is_open;
+}
+}  // namespace doodle::gui
