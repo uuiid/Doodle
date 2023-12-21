3689d0b86cdecdeed7cbc441036b4f698ad82989
uuiid <957714080@qq.com>
2023-12-21 17:31:39 +0800

    [创建视屏] 添加合成工作区

---
+++ src/doodle_lib/gui/create_video_layout.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2023/12/21.
+//
+
+#include "create_video_layout.h"
---
+++ src/doodle_lib/gui/create_video_layout.h
@@ -0,0 +1,8 @@
+//
+// Created by TD on 2023/12/21.
+//
+#pragma once
+
+#include <doodle_app/doodle_app_fwd.h>
+#include <doodle_app/gui/layout_window_base.h>
+namespace doodle::gui {}
