c6dfc84c31c6ff166ec274e0fcfff9a24e747194
uuiid <957714080@qq.com>
2023-12-23 14:28:07 +0800

    [自动化灯光] 添加扫瞄表格

--- src/doodle_lib/gui/widgets/scan_assets.cpp
+++ src/doodle_lib/gui/widgets/scan_assets.cpp
@@ -10,6 +10,8 @@
 
 #include <doodle_app/lib_warp/imgui_warp.h>
 
+#include <doodle_lib/gui/widgets/scan_assets/character_scan_category.h>
+#include <doodle_lib/gui/widgets/scan_assets/prop_scan_category.h>
 #include <doodle_lib/gui/widgets/scan_assets/scene_scan_category.h>
 namespace doodle::gui {
 
@@ -22,7 +24,8 @@ class scan_assets_config {
   // 扫瞄分类
 
   std::vector<std::unique_ptr<scan_category_t>> scan_categorys_;
-  std::unique_ptr<std::map<uuid, entt::entity>> assets_file_map_;
+  std::vector<scan_category_data_ptr> scam_datas_;
+  std::map<uuid, entt::entity> assets_file_map_;
 
   // 独步消遥            {"//192.168.10.250/public/DuBuXiaoYao_3", "独步逍遥" },
   // 人间最得意           {"//192.168.10.240/public/renjianzuideyi", "人间最得意" },
@@ -45,24 +48,130 @@ class scan_assets_config {
                        {"//192.168.10.240/public/LongMaiWuShen", "龙脉武神"},
                        {"//192.168.10.218/WanYuFengShen", "万域封神"}},
         scan_categorys_{},
-        assets_file_map_{std::make_unique<std::map<uuid, entt::entity>>()} {
-    scan_categorys_.emplace_back(std::make_unique<scene_scan_category_t>());
+        assets_file_map_{} {}
+
+  void start_sacn() {
+    for (auto&& l_root : project_roots_) {
+      for (auto&& l_data : scan_categorys_) {
+        scam_datas_ |= ranges::actions::push_back(l_data->scan(l_root));
+      }
+    }
   }
-
-  void start_sacn() {}
-
-  // 检查路径
-  void check_path() {}
 };
 
 }  // namespace details
 
-void scan_assets_t::start_scan() {}
+void scan_assets_t::init_scan_categories() {
+  project_roots_ = {
+      project_root_gui_t{"//192.168.10.250/public/DuBuXiaoYao_3", "独步逍遥", true},
+      {"//192.168.10.240/public/renjianzuideyi", "人间最得意", true},
+      {"//192.168.10.240/public/WuJinShenYu", "无尽神域", true},
+      {"//192.168.10.240/public/WuDiJianHun", "无敌剑魂", true},
+      {"//192.168.10.240/public/WanGuShenHua", "万古神话", true},
+      {"//192.168.10.240/public/LianQiShiWanNian", "炼气十万年", true},
+      {"//192.168.10.240/public/WGXD", "万古邪帝", true},
+      {"//192.168.10.240/public/LongMaiWuShen", "龙脉武神", true},
+      {"//192.168.10.218/WanYuFengShen", "万域封神", true}
+  };
+  scan_categories_factory_vec_ = {
+      scan_categories_factory_t{
+          gui_cache_name_id{"扫描角色"}, true,
+          []() -> std::shared_ptr<details::scan_category_t> {
+            return std::make_shared<details::character_scan_category_t>();
+          }
+      },
+      scan_categories_factory_t{
+          gui_cache_name_id{"扫描场景"}, true,
+          []() -> std::shared_ptr<details::scan_category_t> {
+            return std::make_shared<details::scene_scan_category_t>();
+          }
+      },
+      scan_categories_factory_t{
+          gui_cache_name_id{"扫描道具"}, true,
+          []() -> std::shared_ptr<details::scan_category_t> {
+            return std::make_shared<details::prop_scan_category_t>();
+          }
+      }
+  };
+}
+
+void scan_assets_t::create_scan_categories() {
+  scan_categories_.clear();
+  for (auto&& l_factory : scan_categories_factory_vec_) {
+    if (!l_factory.has_) continue;
+    scan_categories_.emplace_back(l_factory.factory_());
+  }
+}
+
+void scan_assets_t::start_scan() {
+  scam_data_vec_.clear();
+  for (auto&& l_root : project_roots_) {
+    if (!l_root.has_) continue;
+    for (auto&& l_data : scan_categories_) {
+      scam_data_vec_ |= ranges::actions::push_back(l_data->scan(l_root));
+    }
+  }
+}
 
 bool scan_assets_t::render() {
+  bool l_changed{};
+
+  if (auto l_table = dear::Table{"项目列表", 3}; l_table) {
+    for (auto&& l_root : project_roots_) {
+      ImGui::TableNextColumn();
+      if (ImGui::Checkbox(l_root.name_.c_str(), &l_root.has_)) {
+        l_changed = true;
+      }
+    }
+  }
+
+  for (auto&& l_factory : scan_categories_factory_vec_) {
+    if (ImGui::Checkbox(*l_factory.name_id_, &l_factory.has_)) {
+      l_changed = true;
+    }
+    ImGui::SameLine();
+  }
+  ImGui::NewLine();
+  if (l_changed) {
+    create_scan_categories();
+  }
+
   if (ImGui::Button(*start_scan_id)) {
     start_scan();
   }
+
+  if (auto l_table = dear::Table{*assets_table_id_, boost::numeric_cast<std::int32_t>(assets_table_header_.size())};
+      l_table) {
+    for (auto&& l_header : assets_table_header_) {
+      ImGui::TableSetupColumn(l_header.c_str());
+    }
+    ImGui::TableHeadersRow();
+
+    ImGuiListClipper clipper{};
+    clipper.Begin(boost::numeric_cast<std::int32_t>(assets_table_data_.size()));
+    while (clipper.Step()) {
+      for (auto l_i = clipper.DisplayStart; l_i < clipper.DisplayEnd; ++l_i) {
+        std::size_t l_index{boost::numeric_cast<std::size_t>(l_i)};
+        auto&& i = assets_table_data_[l_index];
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        dear::Text(i.name_);
+        ImGui::TableNextColumn();
+        dear::Text(i.season_);
+        ImGui::TableNextColumn();
+        dear::Text(i.version_name_);
+        ImGui::TableNextColumn();
+        dear::Text(i.ue_path_);
+        ImGui::TableNextColumn();
+        dear::Text(i.maya_rig_path_);
+        ImGui::TableNextColumn();
+        dear::Text(i.project_root_);
+        ImGui::TableNextColumn();
+        dear::Text(i.info_);
+      }
+    }
+  }
+
   return is_open;
 }
 }  // namespace doodle::gui
--- src/doodle_lib/gui/widgets/scan_assets.h
+++ src/doodle_lib/gui/widgets/scan_assets.h
@@ -5,18 +5,58 @@
 #pragma once
 #include <doodle_app/gui/base/ref_base.h>
 
-namespace doodle::gui {
+#include <doodle_lib/gui/widgets/scan_assets/base.h>
 
+namespace doodle::gui {
+namespace details {
+class scan_assets_config;
+}
 class DOODLELIB_API scan_assets_t {
   bool is_open{true};
 
+  struct project_root_gui_t : details::scan_category_t::project_root_t {
+    bool has_;
+  };
+  // 工厂
+  struct scan_categories_factory_t {
+    gui_cache_name_id name_id_;
+    bool has_;
+    std::function<std::shared_ptr<details::scan_category_t>()> factory_;
+  };
+
   // 开始扫瞄资产
   gui_cache_name_id start_scan_id;
+  std::shared_ptr<details::scan_assets_config> config_;
+  std::vector<details::scan_category_data_ptr> scam_data_vec_;
+  std::vector<std::shared_ptr<details::scan_category_t>> scan_categories_;
+  std::array<project_root_gui_t, 9> project_roots_;
+  std::array<scan_categories_factory_t, 3> scan_categories_factory_vec_;
+
+  // 扫描后gui数据结构
+  struct scan_gui_data_t {
+    std::string name_;           // 名称
+    std::string season_;         // 所属季数
+    std::string version_name_;   // 版本名称
+    std::string ue_path_;        // 主要的ue路径
+    std::string maya_rig_path_;  // 对应的maya rig路径
+    std::string project_root_;   // 项目根目录 + 项目名称
+    std::string info_;           // 信息
+  };
+  // 资产表
+  gui_cache_name_id assets_table_id_;
+  std::vector<scan_gui_data_t> assets_table_data_;  // 资产表主要数据
+  // 资产表头
+  std::array<std::string, 7> assets_table_header_{"名称",         "季数",       "版本名称", "ue路径",
+                                                  "maya rig路径", "项目根目录", "信息"};
 
   void start_scan();
+  void init_scan_categories();
+  void create_scan_categories();
 
  public:
-  scan_assets_t() : start_scan_id{"开始扫瞄资产"} {};
+  scan_assets_t() : start_scan_id{"开始扫瞄资产"}, project_roots_{}, scan_categories_factory_vec_{} {
+    init_scan_categories();
+  };
 
   constexpr static std::string_view name{gui::config::menu_w::scan_assets};
   bool render();
