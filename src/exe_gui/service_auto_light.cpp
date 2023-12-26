86c878adacb623c44b817711ada6989dbe3d56db
uuiid <957714080@qq.com>
2023-12-26 13:41:32 +0800

    [自动化灯光] 添加gui监控

--- src/exe_gui/service_auto_light.cpp
+++ src/exe_gui/service_auto_light.cpp
@@ -1,3 +1,7 @@
 //
 // Created by TD on 2023/12/26.
 //
+#include <doodle_lib/facet/windwos_service_auto_light.h>
+
+#include "main_macro.h"
+DOODLE_MAIN_IMPL(doodle::windwos_service_auto_light_facet_t)
