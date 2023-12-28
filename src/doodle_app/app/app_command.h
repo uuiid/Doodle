ccf5f479755be4a7bd072b138919b8d6a2a4cb2d
uuiid <957714080@qq.com>
2023-12-28 09:54:15 +0800

    [自动化灯光] 调整类所在位置

--- src/doodle_app/app/app_command.h
+++ src/doodle_app/app/app_command.h
@@ -16,30 +16,4 @@
 #include <array>
 #include <tuple>
 
-namespace doodle {
-
-/**
- * @brief 基本的命令行类
- */
-template <typename... Facet_>
-class app_command : public app_base {
- public:
-  app_command() : app_base() { run_facet(); };
-
-  app_command(int argc, const char* const argv[]) : app_command(argc, argv) { run_facet(); }
-  virtual ~app_command() override = default;
-
-  void run_facet() {
-    std::array<bool, sizeof...(Facet_)> l_r{
-        Facet_{}(arg_, facets_)...,
-    };
-    stop_ = std::any_of(l_r.begin(), l_r.end(), [](bool i) { return i; });
-  }
-
- protected:
-};
-
-template <typename... Facet_>
-using app_plug = app_command<Facet_...>;
-
-}  // namespace doodle
+namespace doodle {}  // namespace doodle
