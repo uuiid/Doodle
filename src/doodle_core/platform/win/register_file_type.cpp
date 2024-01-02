7faa351b391febce4df6e74244c83865062a3e18
uuiid <957714080@qq.com>
2024-01-02 17:46:19 +0800

    [自动化灯光] 调整注册表

--- src/doodle_core/platform/win/register_file_type.cpp
+++ src/doodle_core/platform/win/register_file_type.cpp
@@ -14,18 +14,18 @@ register_file_type::register_file_type() = default;
 
 void register_file_type::register_type() {
   winreg::RegKey l_key{};
-  l_key.Create(HKEY_CURRENT_USER, L"SOFTWARE\\Classes\\doodle.main.1\\shell\\open\\command");
+  l_key.Create(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Classes\\doodle.main.1\\shell\\open\\command");
   l_key.SetStringValue({}, L"");
 }
 std::optional<database::ref_data> register_file_type::get_ref_uuid() {
-  winreg::RegKey l_key{HKEY_CURRENT_USER, L""};
+  winreg::RegKey l_key{HKEY_LOCAL_MACHINE, L""};
   return std::optional<database::ref_data>();
 }
 
 FSys::path register_file_type::get_main_project() {
   try {
     winreg::RegKey l_key{};
-    l_key.Open(HKEY_CURRENT_USER, LR"(SOFTWARE\Doodle\MainConfig)", KEY_QUERY_VALUE | KEY_WOW64_64KEY);
+    l_key.Open(HKEY_LOCAL_MACHINE, LR"(SOFTWARE\Doodle\MainConfig)", KEY_QUERY_VALUE | KEY_WOW64_64KEY);
     auto l_path = l_key.GetStringValue(LR"(main_project)");
     return l_path;
 
@@ -37,7 +37,7 @@ FSys::path register_file_type::get_main_project() {
 
 FSys::path register_file_type::get_update_path() {
   winreg::RegKey l_key{};
-  l_key.Open(HKEY_CURRENT_USER, LR"(SOFTWARE\Doodle\MainConfig)", KEY_QUERY_VALUE | KEY_WOW64_64KEY);
+  l_key.Open(HKEY_LOCAL_MACHINE, LR"(SOFTWARE\Doodle\MainConfig)", KEY_QUERY_VALUE | KEY_WOW64_64KEY);
   auto l_path = l_key.GetStringValue(LR"(update_path)");
   return l_path;
 }
