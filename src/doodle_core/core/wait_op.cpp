3eafcb977a005d8cc98a20ea18811564c795e778
uuiid <957714080@qq.com>
2024-01-06 15:01:13 +0800

    [自动化灯光] 添加异步操作

---
+++ src/doodle_core/core/wait_op.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2024/1/6.
+//
+
+#include "wait_op.h"
--- src/doodle_lib/core/down_auto_light_file.h
+++ src/doodle_lib/core/down_auto_light_file.h
@@ -3,11 +3,29 @@
 //
 
 #pragma once
+#include <doodle_core/doodle_core_fwd.h>
+#include <doodle_core/thread_pool/process_message.h>
 
+#include <boost/asio.hpp>
 namespace doodle {
 class down_auto_light_file {
+  entt::handle msg_{};
+  FSys::path maya_out_file_{};
+
+  struct wait_op {
+   public:
+    using func_type = void (*)(wait_op *op);
+
+   protected:
+    boost::system::error_code ec_{};
+    func_type func_{};  // The function to be called when the operation completes.
+    wait_op(func_type func) : func_(func) {}
+    ~wait_op() = default;
+  };
+
  public:
-  down_auto_light_file()  = default;
+  explicit down_auto_light_file(entt::handle in_msg, FSys::path in_maya_out_file)
+      : msg_(std::move(in_msg)), maya_out_file_(std::move(in_maya_out_file)){};
   ~down_auto_light_file() = default;
 };
 }  // namespace doodle
