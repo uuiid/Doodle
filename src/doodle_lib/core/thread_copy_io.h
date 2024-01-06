87c2e2b4fc67640a7150e87abc13e45d51b53941
uuiid <957714080@qq.com>
2024-01-06 10:13:12 +0800

    [自动化灯光] 添加线程复制

--- src/doodle_lib/core/thread_copy_io.h
+++ src/doodle_lib/core/thread_copy_io.h
@@ -2,7 +2,27 @@
 // Created by TD on 2024/1/6.
 //
 
-#ifndef DOODLE_THREAD_COPY_IO_H
-#define DOODLE_THREAD_COPY_IO_H
+#pragma once
+#include <doodle_core/doodle_core_fwd.h>
 
-#endif  // DOODLE_THREAD_COPY_IO_H
+namespace doodle {
+
+class thread_copy_io {
+ public:
+  thread_copy_io()  = default;
+  ~thread_copy_io() = default;
+
+  /**
+   * @brief 这个是差异复制, 会自动比较文件的修改时间, 和大小 如果源文件的修改时间或者大小不同, 则会复制, 否则不会复制
+   * @tparam CompletionHandler
+   * @param from
+   * @param to
+   * @param in_options
+   * @param handler
+   * @return
+   */
+  template <typename CompletionHandler>
+  auto copy(const FSys::path& from, const FSys::path& to, FSys::copy_options in_options, CompletionHandler&& handler) {}
+};
+
+}  // namespace doodle
