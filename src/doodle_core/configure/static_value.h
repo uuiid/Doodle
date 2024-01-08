902648042d25e44299f3e9bdfc6bbcfba3d382d0
uuiid <957714080@qq.com>
2024-01-08 13:23:55 +0800

    [自动化灯光] 添加全部变量

--- src/doodle_core/configure/static_value.h
+++ src/doodle_core/configure/static_value.h
@@ -26,6 +26,10 @@ constexpr const std::string_view image_folder_name{R"(image)"};
 
 constexpr const std::string_view ue_path_obj{"Engine/Binaries/Win64/UnrealEditor-Cmd.exe"};
 constexpr const std::string_view ue4_content{R"(Content)"};
+constexpr const std::string_view ue4_config{R"(Config)"};
+constexpr const std::string_view ue4_uproject_ext{R"(.uproject)"};
+constexpr const std::string_view ue4_saved{R"(Saved)"};
+constexpr const std::string_view ue4_movie_renders{R"(MovieRenders)"};
 constexpr const std::string_view ue4_game{R"(/Game)"};
 constexpr const std::string_view ue4_shot{R"(Shot)"};
 constexpr const std::string_view hello_world_doodle{R"(hello world! doodle)"};                // 客户端发送
