72418694f2b33df0dabf453c93450caa91ad9fea
uuiid <957714080@qq.com>
2024-01-09 16:16:16 +0800

    [自动化灯光] 添加文件上传

---
+++ src/doodle_lib/core/up_auto_light_file.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2024/1/9.
+//
+
+#include "up_auto_light_file.h"
---
+++ src/doodle_lib/core/up_auto_light_file.h
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2024/1/9.
+//
+
+#pragma once
+namespace doodle {
+
+// 上传自动灯光文件
+class up_auto_light_anim_file {};
+}  // namespace doodle
