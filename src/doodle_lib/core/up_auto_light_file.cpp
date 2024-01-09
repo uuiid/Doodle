da6bd4d973fb977964ddb14943b54d125c17d189
uuiid <957714080@qq.com>
2024-01-09 17:20:43 +0800

    [自动化灯光] 调整参数保存位置

--- src/doodle_lib/core/up_auto_light_file.cpp
+++ src/doodle_lib/core/up_auto_light_file.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "up_auto_light_file.h"
+
+#include <doodle_core/metadata/project.h>
+
+namespace doodle {
+void up_auto_light_anim_file::init() { data_->logger_ = msg_.get<process_message>().logger(); }
+void up_auto_light_anim_file::operator()(boost::system::error_code in_error_code, FSys::path in_gen_path) const {
+  if (in_error_code) {
+    data_->logger_->error("上传自动灯光文件失败:{}", in_error_code.message());
+    wait_op_->ec_ = in_error_code;
+    wait_op_->complete();
+    return;
+  }
+  data_->logger_->info("开始上传文件夹 :{}", in_gen_path.generic_string());
+
+  auto l_rem_path = msg_.get<project>().
+}
+}  // namespace doodle
--- src/doodle_lib/core/up_auto_light_file.h
+++ src/doodle_lib/core/up_auto_light_file.h
@@ -3,8 +3,66 @@
 //
 
 #pragma once
+#include <doodle_core/core/wait_op.h>
+#include <doodle_core/doodle_core_fwd.h>
+#include <doodle_core/logger/logger.h>
+#include <doodle_core/thread_pool/process_message.h>
+
+#include <boost/asio.hpp>
 namespace doodle {
 
 // 上传自动灯光文件
-class up_auto_light_anim_file {};
+class up_auto_light_anim_file {
+ public:
+ private:
+  template <typename Handler>
+  struct wait_handle : detail::wait_op {
+   public:
+    explicit wait_handle(Handler&& handler)
+        : detail::wait_op(&wait_handle::on_complete, std::make_shared<Handler>(handler)) {}
+    ~wait_handle() = default;
+    FSys::path up_info_{};
+    static void on_complete(wait_op* op) {
+      auto l_self = static_cast<wait_handle*>(op);
+      boost::asio::post(
+          boost::asio::prepend(std::move(*static_cast<Handler*>(l_self->handler_.get())), l_self->ec_, l_self->up_info_)
+      );
+    }
+  };
+  using set_path_t = std::function<void(FSys::path)>;
+
+  struct data_impl_t {
+    logger_ptr logger_{};
+  };
+
+  entt::handle msg_{};
+  std::shared_ptr<detail::wait_op> wait_op_{};
+  set_path_t set_info_;
+  std::shared_ptr<data_impl_t> data_{};  // 用于存储数据
+
+  void init();
+
+ public:
+  explicit up_auto_light_anim_file(entt::handle in_msg) : data_(std::make_shared<data_impl_t>()), msg_(in_msg) {
+    init();
+  }
+  ~up_auto_light_anim_file() = default;
+
+  template <typename CompletionHandler>
+  auto async_end(CompletionHandler&& handler) {
+    return boost::asio::async_compose<CompletionHandler, void(boost::system::error_code, FSys::path)>(
+        [this](auto&& handler) {
+          wait_op_ =
+              std::make_shared<wait_handle<std::decay_t<decltype(handler)>>>(std::forward<decltype(handler)>(handler));
+          set_info_ = [this](FSys::path in_path) {
+            auto l_wait_op      = std::dynamic_pointer_cast<wait_handle<std::decay_t<decltype(handler)>>>(wait_op_);
+            l_wait_op->up_info_ = in_path;
+          };
+        },
+        handler
+    );
+  }
+
+  void operator()(boost::system::error_code in_error_code, FSys::path in_gen_path) const;
+};
 }  // namespace doodle
