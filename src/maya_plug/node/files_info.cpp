b72347b6e854e17e8bfb0863ed60055b2b063490
uuiid <957714080@qq.com>
2024-01-11 14:59:29 +0800

    [自动化灯光] 添加基本的fileinfo

--- src/maya_plug/node/files_info.cpp
+++ src/maya_plug/node/files_info.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "files_info.h"
+
+#include <maya/MTypeId.h>
+namespace doodle::maya_plug {
+MTypeId doodle_file_info::doodle_id = MTypeId{0x00000001};
+
+void* doodle_file_info::creator() { return new doodle_file_info{}; }
+MStatus doodle_file_info::initialize() { return MStatus::kSuccess; }
+
+}  // namespace doodle::maya_plug
--- src/maya_plug/node/files_info.h
+++ src/maya_plug/node/files_info.h
@@ -3,3 +3,19 @@
 //
 
 #pragma once
+
+#include <maya_plug/maya_plug_fwd.h>
+
+#include <maya/MPxNode.h>
+namespace doodle::maya_plug {
+class doodle_file_info : public MPxNode {
+ public:
+  static MTypeId doodle_id;
+  const static constexpr auto node_type = MPxNode::Type::kDependNode;
+  static void* creator();
+  static MStatus initialize();
+
+  const static constexpr std::string_view node_name{"doodle_file_info"};
+};
+
+}  // namespace doodle::maya_plug
