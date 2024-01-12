c9dd0bed4f07083a236e314354ff0c3b2680be40
uuiid <957714080@qq.com>
2024-01-12 09:52:48 +0800

    [自动化灯光] 添加文件引用

--- src/maya_plug/maya_comm/file_info_edit.cpp
+++ src/maya_plug/maya_comm/file_info_edit.cpp
@@ -3,3 +3,89 @@
 //
 
 #include "file_info_edit.h"
+
+#include <maya_plug/node/files_info.h>
+
+#include <maya/MArgDatabase.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFnReference.h>
+#include <maya/MItDependencyNodes.h>
+
+namespace doodle::maya_plug {
+
+MSyntax file_info_edit_syntax() {
+  MSyntax l_syntax{};
+  //  l_syntax.addFlag("-rf", "-reference_file", MSyntax::kString);
+  //  l_syntax.addFlag("-rnp", "-reference_file_namespace", MSyntax::kString);
+  //  l_syntax.addFlag("-rp", "-reference_file_path", MSyntax::kString);
+  //  l_syntax.addFlag("-is", "-is_solve", MSyntax::kBoolean);
+  //  l_syntax.addFlag("-co", "-collision_objects", MSyntax::kString);
+  //  l_syntax.addFlag("-wf", "-wind_fields", MSyntax::kString);
+  // 刷新
+  l_syntax.addFlag("-r", "-refresh", MSyntax::kNoArg);
+  // 强制
+  l_syntax.addFlag("-f", "-force", MSyntax::kNoArg);
+
+  return l_syntax;
+}
+
+MStatus file_info_edit::doIt(const MArgList &in_list) {
+  MStatus l_status{};
+  MArgDatabase l_arg_data{syntax(), in_list, &l_status};
+  maya_chick(l_status);
+
+  if (l_arg_data.isFlagSet("-f")) {
+    is_force = true;
+  }
+
+  if (l_arg_data.isFlagSet("-r")) {
+    l_status = todo();
+  }
+
+  return l_status;
+}
+MStatus file_info_edit::todo() {
+  MStatus l_status{};
+  if (has_node() && !is_force) {
+    displayError("has node, use -f to force");
+    return MS::kSuccess;
+  }
+  if (is_force) {
+    l_status = delete_node();
+    maya_chick(l_status);
+  }
+  for (MItDependencyNodes l_it{MFn::kReference, &l_status}; !l_it.isDone(); l_it.next()) {
+    MObject l_node = dg_modifier_.createNode(doodle_file_info::doodle_id, &l_status);
+    maya_chick(l_status);
+    MFnDependencyNode l_fn_node{l_node, &l_status};
+    maya_chick(l_status);
+
+    maya_chick(dg_modifier_.connect(get_plug(l_it.thisNode(), "message"), get_plug(l_node, "reference_file")));
+    MFnReference l_fn_ref{l_it.thisNode(), &l_status};
+    get_plug(l_node, "reference_file_path").setValue(l_fn_ref.fileName(false, false, false, &l_status));
+    maya_chick(l_status);
+    get_plug(l_node, "reference_file_namespace").setValue(l_fn_ref.associatedNamespace(false, &l_status));
+    maya_chick(l_status);
+  }
+}
+
+MStatus file_info_edit::delete_node() {
+  MStatus l_status{};
+  for (MItDependencyNodes l_it{MFn::kPluginDependNode, &l_status}; !l_it.isDone(); l_it.next()) {
+    if (l_it.thisNode().apiTypeStr() == doodle_file_info::node_name.data()) {
+      maya_chick(dg_modifier_.deleteNode(l_it.thisNode(&l_status)));
+      maya_chick(l_status);
+    }
+  }
+  return l_status;
+}
+
+bool file_info_edit::has_node() const {
+  MStatus l_status{};
+  for (MItDependencyNodes l_it{MFn::kPluginDependNode, &l_status}; !l_it.isDone(); l_it.next()) {
+    if (l_it.thisNode().apiTypeStr() == doodle_file_info::node_name.data()) return true;
+  }
+  return false;
+}
+
+}  // namespace doodle::maya_plug
--- src/maya_plug/maya_comm/file_info_edit.h
+++ src/maya_plug/maya_comm/file_info_edit.h
@@ -6,3 +6,28 @@
 #include <maya_plug/main/maya_plug_fwd.h>
 
 #include "maya/MApiNamespace.h"
+#include "maya/MDGModifier.h"
+namespace doodle::maya_plug {
+
+namespace file_info_edit_ns {
+constexpr char file_info_edit[]{"doodle_file_info_edit"};
+}  // namespace file_info_edit_ns
+
+MSyntax file_info_edit_syntax();
+class file_info_edit : public TemplateAction<file_info_edit, file_info_edit_ns::file_info_edit, file_info_edit_syntax> {
+ public:
+  file_info_edit();
+  ~file_info_edit() override;
+  MStatus doIt(const MArgList& in_list) override;
+
+ private:
+  MStatus todo();
+  MStatus delete_node();
+  bool has_node() const;
+
+  MDGModifier dg_modifier_{};
+  // 强制
+  bool is_force{false};
+};
+
+}  // namespace doodle::maya_plug
