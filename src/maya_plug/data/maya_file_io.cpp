b8235b02e75ccf279d6adab4aad6733efa41a060
uuiid <957714080@qq.com>
2024-01-16 14:20:56 +0800

    [自动化灯光] 去除无用的函数

--- src/maya_plug/data/maya_file_io.cpp
+++ src/maya_plug/data/maya_file_io.cpp
@@ -33,57 +33,7 @@ FSys::path maya_file_io::work_path(const FSys::path& in_path) {
   k_obj.setResolveMethod(MFileObject::MFileResolveMethod::kNone);
   return k_obj.resolvedFullName().asUTF8();
 }
-std::string maya_file_io::get_channel_date() {
-  MStatus k_status{};
-  adsk::Data::Associations k_meta{MFileIO::metadata(&k_status)};
-  DOODLE_MAYA_CHICK(k_status);
-  auto k_channel = k_meta.channel("doodle_sim_json");
-  if (!k_channel.empty()) {
-    auto k_stream = k_channel.dataStream("json_stream");
-    adsk::Data::Handle k_h{k_stream->element(0)};
-    if (!k_h.hasData()) return {};
-    return k_h.str(0);
-  }
-  return {};
-}
-bool maya_file_io::chick_channel() {
-  MStatus k_status{};
-  adsk::Data::Associations k_meta{MFileIO::metadata(&k_status)};
-  DOODLE_MAYA_CHICK(k_status);
-  auto k_channel = k_meta.channel("doodle_sim_json");
-  if (!k_channel.dataStream("json_stream")) {
-    auto k_s = adsk::Data::Structure::create();              // 添加结构
-    k_s->setName("json_structure");                          // 设置结构名称
-    k_s->addMember(adsk::Data::Member::kString, 1, "json");  // 添加结构成员
-    adsk::Data::Structure::registerStructure(*k_s);          // 注册结构
 
-    adsk::Data::Stream k_stream{*k_s, "json_stream"};
-    k_channel.setDataStream(k_stream);  // 设置流
-    MFileIO::setMetadata(k_meta);
-  }
-  return true;
-}
-bool maya_file_io::replace_channel_date(const std::string& in_string) {
-  chick_channel();
-  MStatus k_status{};
-  adsk::Data::Associations k_meta{MFileIO::metadata(&k_status)};
-  DOODLE_MAYA_CHICK(k_status);
-  auto k_json   = k_meta.channel("doodle_sim_json");  /// 获得元数据通道
-  auto k_stream = k_json.dataStream("json_stream");   /// 获得元数据流
-
-  adsk::Data::Handle k_h{k_stream->structure()};
-
-  std::string str_err{};
-  DOODLE_LOG_INFO(in_string);
-  if (k_h.fromStr(in_string, 0, str_err) != 0) DOODLE_LOG_ERROR(str_err);
-  k_stream->setElement(0, k_h);
-  MFileIO::setMetadata(k_meta);
-
-  //  adsk::Debug::Print k_p{std::cout};
-  //  decltype(k_meta)::Debug(&k_meta, k_p);
-  //  k_p.endSection();
-  return true;
-}
 bool maya_file_io::save_file(const FSys::path& in_file_path) {
   MStatus k_s{};
   if (!exists(in_file_path.parent_path())) {
@@ -129,17 +79,7 @@ bool maya_file_io::upload_file(const FSys::path& in_source_path, const FSys::pat
   }
   return result;
 }
-void maya_file_io::import_reference_file(const reference_file& in_ref, bool preserve_references) {
-  if (!in_ref.p_m_object.isNull()) {
-    DOODLE_LOG_INFO("开始加载引用文件 {}", in_ref.get_abs_path());
-    MStatus l_s{};
-    //    auto l_obj = in_ref.p_m_object;
-    //    MFileIO::loadReferenceByNode(l_obj, &l_s);
-    auto l_com = fmt::format("file -importReference -referenceNode {}", get_node_full_name(in_ref.p_m_object));
-    l_s        = MGlobal::executeCommand(d_str{l_com});
-    DOODLE_MAYA_CHICK(l_s);
-  }
-}
+
 void maya_file_io::set_workspace(const FSys::path& in_path) {
   FSys::path l_path{in_path.parent_path()};
   if (!FSys::exists(l_path / "workspace.mel")) {
@@ -147,9 +87,9 @@ void maya_file_io::set_workspace(const FSys::path& in_path) {
   }
 
   if (!FSys::exists(l_path / "workspace.mel")) {
-    std::string const l_s{fmt::format(
-        R"(workspace -baseWorkspace "default" -openWorkspace "{}")", in_path.parent_path().generic_string()
-    )};
+    std::string const l_s{
+        fmt::format(R"(workspace -baseWorkspace "default" -openWorkspace "{}")", in_path.parent_path().generic_string())
+    };
 
     maya_chick(MGlobal::executeCommand(d_str{l_s}));
     maya_chick(MGlobal::executeCommand(R"(workspace -saveWorkspace)"));
--- src/maya_plug/data/maya_file_io.h
+++ src/maya_plug/data/maya_file_io.h
@@ -18,16 +18,11 @@ class maya_file_io {
 
   static FSys::path work_path(const FSys::path& in_path = ".");
   static void set_workspace(const FSys::path& in_path);
-  static std::string get_channel_date();
-  static bool chick_channel();
-  static bool replace_channel_date(const std::string& in_string);
   static bool save_file(const FSys::path& in_file_path);
   static void open_file(
       const FSys::path& in_file_path, MFileIO::ReferenceMode in_mode = MFileIO::ReferenceMode::kLoadDefault
   );
 
   static bool upload_file(const FSys::path& in_source_path, const FSys::path& in_prefix);
-
-  static void import_reference_file(const reference_file& in_path, bool preserve_references);
 };
 }  // namespace doodle::maya_plug
