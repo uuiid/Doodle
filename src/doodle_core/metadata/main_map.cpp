2a104a053cd4a88879e85dc7349d054931ea3bde
uuiid <957714080@qq.com>
2024-01-25 17:38:34 +0800

    [UE工具] 调整ip路径检查ue文件方法顺序

--- src/doodle_core/metadata/main_map.cpp
+++ src/doodle_core/metadata/main_map.cpp
@@ -10,8 +10,8 @@ namespace doodle {
 namespace {
 
 FSys::path find_u_pej(const FSys::path& in_path) {
-  if (!FSys::exists(in_path)) return {};
   if (in_path.root_path() == in_path) return {};
+  if (!FSys::exists(in_path)) return {};
   if (!FSys::is_directory(in_path)) return find_u_pej(in_path.parent_path());
 
   for (auto&& l_file : FSys::directory_iterator(in_path)) {
