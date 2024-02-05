ab30705271b03cff7b5e7c369b718895d3a99f7b
uuiid <957714080@qq.com>
2024-02-05 10:29:41 +0800

    [自动化灯光] 调整为循环强制打开

--- src/doodle_core/database_task/sqlite_client.cpp
+++ src/doodle_core/database_task/sqlite_client.cpp
@@ -40,7 +40,15 @@ registry_ptr file_translator::load_new_file(const FSys::path& in_path) {
   obs_all l_obs{};
   l_reg->ctx().emplace<database_info>().path_ = in_path;
   auto l_con                                  = l_reg->ctx().get<database_info>().get_connection_const();
-  l_obs.open(l_reg, l_con);
+  do {
+    try {
+      l_obs.open(l_reg, l_con);
+      break;
+    } catch (const sqlpp::exception& in_error) {
+      std::this_thread::sleep_for(std::chrono::microseconds{1});
+      default_logger_raw()->log(log_loc(), level::err, "打开文件 {} 失败 {}", in_path, in_error.what());
+    }
+  } while (true);
   l_reg->ctx().emplace<project_config::base_config>(project_config::base_config::get_default());
   l_reg->ctx().emplace<project>("tmp", in_path, "tmp", "tmp");
   return l_reg;
