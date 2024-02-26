d2478bc5290f795a7d0c24c3c70cc12616129f7e
uuiid <957714080@qq.com>
2024-02-26 17:42:04 +0800

    [自动化灯光] 添加注册方法

--- src/doodle_lib/launch/http_distributed_tasks.cpp
+++ src/doodle_lib/launch/http_distributed_tasks.cpp
@@ -10,7 +10,10 @@
 
 #include <doodle_lib/core/http/http_listener.h>
 #include <doodle_lib/core/http/http_route.h>
+#include <doodle_lib/http_method/computer.h>
 namespace doodle::launch {
+
+void reg_func(doodle::http::http_route &in_route) { http::computer::reg(in_route); }
 
 bool http_distributed_tasks::operator()(const argh::parser &in_arh, std::vector<std::shared_ptr<void>> &in_vector) {
   using signal_t     = boost::asio::signal_set;
@@ -26,6 +29,7 @@ bool http_distributed_tasks::operator()(const argh::parser &in_arh, std::vector<
   in_vector.emplace_back(l_signal_ptr);
   default_logger_raw()->log(log_loc(), level::warn, "开始服务器");
   auto l_rout_ptr = std::make_shared<http::http_route>();
+  reg_func(*l_rout_ptr);
   auto l_listener = std::make_shared<http::http_listener>(g_io_context(), l_rout_ptr);
   l_listener->run();
   in_vector.emplace_back(l_listener);
