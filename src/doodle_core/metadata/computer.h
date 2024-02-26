e37bb265b55ee647eba3b079b21b6dfd267f86b6
uuiid <957714080@qq.com>
2024-02-26 17:57:25 +0800

    [自动化灯光] 添加json转换

--- src/doodle_core/metadata/computer.h
+++ src/doodle_core/metadata/computer.h
@@ -3,5 +3,27 @@
 //
 
 #pragma once
+#include <doodle_core/doodle_core_fwd.h>
+namespace doodle {
+class computer {
+ public:
+  computer()  = default;
+  ~computer() = default;
 
-namespace doodle {}
+  std::string name_;
+  std::string ip_;
+
+ private:
+  // to json
+  friend void to_json(nlohmann::json& j, const computer& p) {
+    j["name"] = p.name_;
+    j["ip"]   = p.ip_;
+  }
+  // from json
+  friend void from_json(const nlohmann::json& j, computer& p) {
+    j.at("name").get_to(p.name_);
+    j.at("ip").get_to(p.ip_);
+  }
+};
+
+}  // namespace doodle
--- src/doodle_lib/http_method/computer.cpp
+++ src/doodle_lib/http_method/computer.cpp
@@ -4,19 +4,24 @@
 
 #include "computer.h"
 
-#include "doodle_lib/core/http/http_session_data.h"
+#include <doodle_core/metadata/computer.h>
 
-#include "core/http/http_session_data.h"
+#include "doodle_lib/core/http/http_session_data.h"
 namespace doodle::http {
 void computer::list_computers(const entt::handle in_handle) {
-  auto &l_req = in_handle.get<http_session_data>().request_parser_->get();
+  std::vector<doodle::computer> l_computers{};
+  for (auto &&[l_e, l_c] : g_reg()->view<doodle::computer>().each()) {
+    l_computers.emplace_back(l_c);
+  }
+  nlohmann::json l_json = l_computers;
+  auto &l_req           = in_handle.get<http_session_data>().request_parser_->get();
   boost::beast::http::response<boost::beast::http::string_body> l_response{
       boost::beast::http::status::ok, l_req.version()
   };
   l_response.result(boost::beast::http::status::ok);
   l_response.keep_alive(l_req.keep_alive());
   l_response.set(boost::beast::http::field::content_type, "application/json");
-  l_response.body() = R"([{"name":"computer1","ip":"192.168.20.1"}])";
+  l_response.body() = l_json.dump();
   l_response.prepare_payload();
   in_handle.get<http_session_data>().seed(std::move(l_response));
 }
