37c3889ea7686d4f48b757b6efe9724a81f5c5d7
uuiid <957714080@qq.com>
2024-02-27 09:29:20 +0800

    [自动化灯光] 添加json主体实现

--- src/doodle_lib/core/http/json_body.cpp
+++ src/doodle_lib/core/http/json_body.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "json_body.h"
+namespace doodle::http {
+void basic_json_body::reader::init(boost::optional<std::uint64_t> const& length, boost::system::error_code& ec) {
+  if (length) {
+    if (*length > json_str_.max_size()) {
+      BOOST_BEAST_ASSIGN_EC(ec, boost::beast::http::error::buffer_overflow);
+      return;
+    }
+    json_str_.reserve(boost::beast::detail::clamp(*length));
+  }
+  ec = {};
+}
+
+void basic_json_body::reader::finish(boost::system::error_code& ec) {
+  if (json_type ::accept(json_str_)) {
+    body_ = json_type::parse(json_str_);
+    ec    = {};
+  } else {
+    BOOST_BEAST_ASSIGN_EC(ec, error_enum::bad_json_string);
+  }
+}
+
+void basic_json_body::writer::init(boost::system::error_code& ec) {
+  json_str_ = body_.dump();
+  ec        = {};
+}
+
+boost::optional<std::pair<basic_json_body::writer::const_buffers_type, bool>> basic_json_body::writer::get(
+    boost::system::error_code& ec
+) {
+  ec = {};
+  return {{const_buffers_type{json_str_.data(), json_str_.size()}, false}};
+}
+}  // namespace doodle::http
--- src/doodle_lib/core/http/json_body.h
+++ src/doodle_lib/core/http/json_body.h
@@ -5,4 +5,78 @@
 #pragma once
 
 #include <boost/beast.hpp>
-namespace doodle::http {}
+
+#include <nlohmann/json.hpp>
+namespace doodle::http {
+
+struct basic_json_body {
+ public:
+  /** The type of container used for the body
+
+  */
+  using json_type  = nlohmann::json;
+  using value_type = json_type;
+
+  /** Returns the payload size of the body
+   */
+  static std::uint64_t size(value_type const& body) { return body.dump().size(); }
+
+  /** The algorithm for parsing the body
+
+      Meets the requirements of <em>BodyReader</em>.
+  */
+
+  class reader {
+    value_type& body_;
+    std::string json_str_;
+
+   public:
+    template <bool isRequest, class Fields>
+    explicit reader(boost::beast::http::header<isRequest, Fields>&, value_type& b) : body_(b) {}
+
+    void init(boost::optional<std::uint64_t> const& length, boost::system::error_code& ec);
+
+    template <class ConstBufferSequence>
+    std::size_t put(ConstBufferSequence const& buffers, boost::system::error_code& ec) {
+      auto const extra = boost::beast::buffer_bytes(buffers);
+      auto const size  = json_str_.size();
+      if (extra > json_str_.max_size() - size) {
+        BOOST_BEAST_ASSIGN_EC(ec, boost::beast::http::error::buffer_overflow);
+        return 0;
+      }
+
+      json_str_.resize(size + extra);
+      ec         = {};
+      char* dest = &json_str_[size];
+      for (auto b : boost::beast::buffers_range_ref(buffers)) {
+        std::char_traits<char>::copy(dest, static_cast<char const*>(b.data()), b.size());
+        dest += b.size();
+      }
+      return extra;
+    }
+
+    void finish(boost::system::error_code& ec);
+  };
+
+  /** The algorithm for serializing the body
+
+      Meets the requirements of <em>BodyWriter</em>.
+  */
+
+  class writer {
+    value_type const& body_;
+    std::string json_str_;
+
+   public:
+    using const_buffers_type = boost::asio::const_buffer;
+
+    template <bool isRequest, class Fields>
+    explicit writer(boost::beast::http::header<isRequest, Fields> const&, value_type const& b) : body_(b) {}
+
+    void init(boost::system::error_code& ec);
+
+    boost::optional<std::pair<const_buffers_type, bool>> get(boost::system::error_code& ec);
+  };
+};
+
+}  // namespace doodle::http
