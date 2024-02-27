55e25875014f5303c79b4fe18c11821ff3e6b10b
uuiid <957714080@qq.com>
2024-02-27 09:26:27 +0800

    [自动化灯光] 添加json主体

---
+++ src/doodle_lib/core/http/json_body.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2024/2/27.
+//
+
+#include "json_body.h"
---
+++ src/doodle_lib/core/http/json_body.h
@@ -0,0 +1,8 @@
+//
+// Created by TD on 2024/2/27.
+//
+
+#pragma once
+
+#include <boost/beast.hpp>
+namespace doodle::http {}
