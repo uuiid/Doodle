47b1767f40422cf765e87b916c0d979c8797921f
uuiid <957714080@qq.com>
2024-02-27 17:14:00 +0800

    [自动化灯光] 添加新的方法注册

--- src/doodle_lib/launch/http_distributed_tasks.cpp
+++ src/doodle_lib/launch/http_distributed_tasks.cpp
@@ -11,13 +11,17 @@
 #include <doodle_lib/core/http/http_listener.h>
 #include <doodle_lib/core/http/http_route.h>
 #include <doodle_lib/http_method/computer.h>
+#include <doodle_lib/http_method/task_info.h>
 namespace doodle::launch {
 
-void reg_func(doodle::http::http_route &in_route) { http::computer::reg(in_route); }
+void reg_func(doodle::http::http_route &in_route) {
+  http::computer::reg(in_route);
+  http::task_info::reg(in_route);
+}
 
 bool http_distributed_tasks::operator()(const argh::parser &in_arh, std::vector<std::shared_ptr<void>> &in_vector) {
-  using signal_t     = boost::asio::signal_set;
-  auto l_signal_ptr  = std::make_shared<signal_t>(g_io_context(), SIGINT, SIGTERM);
+  using signal_t    = boost::asio::signal_set;
+  auto l_signal_ptr = std::make_shared<signal_t>(g_io_context(), SIGINT, SIGTERM);
   l_signal_ptr->async_wait([l_signal_ptr](boost::system::error_code ec, int signal) {
     if (ec) {
       default_logger_raw()->log(log_loc(), level::warn, "signal_set_ error: {}", ec);
