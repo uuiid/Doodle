c140f18661dd4757cb3475b1956d03eb9866766b
uuiid <957714080@qq.com>
2024-02-28 15:42:57 +0800

    [自动化灯光] 添加task分配

---
+++ src/doodle_lib/http_method/task_server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2024/2/28.
+//
+
+#include "task_server.h"
---
+++ src/doodle_lib/http_method/task_server.h
@@ -0,0 +1,8 @@
+//
+// Created by TD on 2024/2/28.
+//
+
+#ifndef DOODLE_TASK_SERVER_H
+#define DOODLE_TASK_SERVER_H
+
+#endif  // DOODLE_TASK_SERVER_H
