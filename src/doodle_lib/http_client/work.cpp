e9e5b1c1fb59f93c96c3d2f177ee7ae968ed084d
uuiid <957714080@qq.com>
2024-02-29 10:42:51 +0800

    [自动化灯光] 添加客户端

---
+++ src/doodle_lib/http_client/work.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 2024/2/29.
+//
+
+#include "work.h"
---
+++ src/doodle_lib/http_client/work.h
@@ -0,0 +1,12 @@
+//
+// Created by TD on 2024/2/29.
+//
+
+#pragma once
+namespace doodle::http {
+class http_work {
+ public:
+  http_work()  = default;
+  ~http_work() = default;
+};
+}  // namespace doodle::http
