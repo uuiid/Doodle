df34a3fb2b43351d42f3e16d015ed50e26e16b11
uuiid <957714080@qq.com>
2024-02-29 15:23:18 +0800

    [自动化灯光] 添加返回值

--- src/doodle_lib/launch/auto_light_service.cpp
+++ src/doodle_lib/launch/auto_light_service.cpp
@@ -25,6 +25,7 @@ bool auto_light_service_t::operator()(const argh::parser &in_arh, std::vector<st
   auto scan_win_service_ptr_ = std::make_shared<scan_win_service_t>();
   in_vector.emplace_back(scan_win_service_ptr_);
   boost::asio::post(g_io_context(), [scan_win_service_ptr_]() { scan_win_service_ptr_->start(); });
+  return false;
 }
 
 }  // namespace doodle::launch
--- src/exe_gui/service_auto_light.cpp
+++ src/exe_gui/service_auto_light.cpp
@@ -2,7 +2,9 @@
 // Created by TD on 2023/12/26.
 //
 
+#include <doodle_core/core/app_service.h>
+
 #include <doodle_lib/launch/auto_light_service.h>
 
 #include "main_macro.h"
-DOODLE_WMAIN_IMPL(doodle::launch::auto_light_service_t);
+DOODLE_SERVICE_MAIN_IMPL(app_service_t, doodle::launch::auto_light_service_t);
