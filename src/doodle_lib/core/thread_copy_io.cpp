f2fa4de778b216a8edf7e28a5acdd76cd88cec29
uuiid <957714080@qq.com>
2024-03-01 16:14:12 +0800

    [自动化灯光] 添加复制文件网格波动导致的错误重试

--- src/doodle_lib/core/thread_copy_io.cpp
+++ src/doodle_lib/core/thread_copy_io.cpp
@@ -30,42 +30,54 @@ boost::system::error_code thread_copy_io_service::copy_impl(
     copy_fun_ptr in_fun_ptr
 ) const {
   boost::system::error_code l_ec{};
-  try {
-    in_logger->log(log_loc(), spdlog::level::info, "复制 {} -> {}", from, to);
-    if (FSys::is_regular_file(from)) {
-      in_fun_ptr(from, to);
-      return l_ec;
-    }
+  static std::error_code l_error_code_NETNAME_DELETED{ERROR_NETNAME_DELETED, std::system_category()};
+  for (int i = 0; i < 10; ++i) {
+    try {
+      in_logger->log(log_loc(), spdlog::level::info, "复制 {} -> {}", from, to);
+      if (FSys::is_regular_file(from)) {
+        in_fun_ptr(from, to);
+        return l_ec;
+      }
 
-    if (in_options == FSys::copy_options::recursive) {
-      for (auto &&l_file : FSys::recursive_directory_iterator(from)) {
-        auto l_to_file = to / l_file.path().lexically_proximate(from);
-        if (l_file.is_regular_file()) {
-          in_fun_ptr(l_file.path(), l_to_file);
+      if (in_options == FSys::copy_options::recursive) {
+        for (auto &&l_file : FSys::recursive_directory_iterator(from)) {
+          auto l_to_file = to / l_file.path().lexically_proximate(from);
+          if (l_file.is_regular_file()) {
+            in_fun_ptr(l_file.path(), l_to_file);
+          }
+        }
+      } else {
+        for (auto &&l_file : FSys::directory_iterator(from)) {
+          auto l_to_file = to / l_file.path().lexically_proximate(from);
+          if (l_file.is_regular_file()) {
+            in_fun_ptr(l_file.path(), l_to_file);
+          }
         }
       }
-    } else {
-      for (auto &&l_file : FSys::directory_iterator(from)) {
-        auto l_to_file = to / l_file.path().lexically_proximate(from);
-        if (l_file.is_regular_file()) {
-          in_fun_ptr(l_file.path(), l_to_file);
-        }
+      return l_ec;
+    } catch (const FSys::filesystem_error &in_error) {
+      if (in_error.code() == l_error_code_NETNAME_DELETED) {
+        in_logger->log(log_loc(), spdlog::level::warn, "复制文件网络错误 开始重试第 {}次 {}, ", i++, in_error.what());
+      } else {
+        in_logger->log(log_loc(), spdlog::level::err, "复制文件错误 {}", in_error.what());
+        l_ec = in_error.code();
+        BOOST_ASIO_ERROR_LOCATION(l_ec);
+        break;
       }
+
+    } catch (const std::system_error &in_error) {
+      in_logger->log(log_loc(), spdlog::level::err, in_error.what());
+      l_ec = in_error.code();
+      BOOST_ASIO_ERROR_LOCATION(l_ec);
+      break;
+    } catch (...) {
+      in_logger->log(log_loc(), spdlog::level::err, "未知错误 {}", boost::current_exception_diagnostic_information());
+      l_ec = boost::system::errc::make_error_code(boost::system::errc::io_error);
+      BOOST_ASIO_ERROR_LOCATION(l_ec);
+      break;
     }
-  } catch (const FSys::filesystem_error &in_error) {
-    in_logger->log(log_loc(), spdlog::level::err, "复制文件错误 {}", in_error.what());
-    l_ec = in_error.code();
-    BOOST_ASIO_ERROR_LOCATION(l_ec);
-  } catch (const std::system_error &in_error) {
-    in_logger->log(log_loc(), spdlog::level::err, in_error.what());
-    l_ec = in_error.code();
-    BOOST_ASIO_ERROR_LOCATION(l_ec);
-  } catch (...) {
-    in_logger->log(log_loc(), spdlog::level::err, "未知错误 {}", boost::current_exception_diagnostic_information()
-    );
-    l_ec = boost::system::errc::make_error_code(boost::system::errc::io_error);
-    BOOST_ASIO_ERROR_LOCATION(l_ec);
   }
+
   return l_ec;
 }
 
