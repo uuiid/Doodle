7b94cd9f814fdbdbbbd29f8490fa54eae729f0b3
uuiid <957714080@qq.com>
2024-03-04 17:24:15 +0800

    [自动化灯光] 调整log日志读取和写入

--- src/doodle_core/metadata/server_task_info.cpp
+++ src/doodle_core/metadata/server_task_info.cpp
@@ -13,9 +13,12 @@ std::string server_task_info::read_log(level::level_enum in_level) const {
   FSys::ifstream l_file{l_path};
   return std::string{std::istream_iterator<char>{l_file}, std::istream_iterator<char>{}};
 }
-void server_task_info::write_log(level::level_enum in_level, std::string_view in_msg) const {
-  auto l_path =
-      core_set::get_set().get_cache_root(log_path_) / fmt::format("{}.log.txt", magic_enum::enum_name(in_level));
-  FSys::ofstream{l_path, std::ios::app} << in_msg;
+void server_task_info::write_log(level::level_enum in_level, std::string_view in_msg) {
+  if (!log_file_out_.is_open()) {
+    auto l_path =
+        core_set::get_set().get_cache_root(log_path_) / fmt::format("{}.log.txt", magic_enum::enum_name(in_level));
+    log_file_out_.open(l_path, std::ios::app | std::ios::binary);
+  }
+  log_file_out_ << in_msg;
 }
 }  // namespace doodle
--- src/doodle_core/metadata/server_task_info.h
+++ src/doodle_core/metadata/server_task_info.h
@@ -78,9 +78,10 @@ class server_task_info {
 
   // 任务日志储存
   FSys::path log_path_{};
+  FSys::ofstream log_file_out_{};
 
   std::string read_log(level::level_enum in_level) const;
-  void write_log(level::level_enum in_level, std::string_view in_msg) const;
+  void write_log(level::level_enum in_level, std::string_view in_msg);
 
  private:
   // to json
