236ed1f2f6477a765a3979c7a9b0f15b1ba9f1e7
uuiid <957714080@qq.com>
2024-03-04 16:22:58 +0800

    [自动化灯光] 调整工作

--- src/doodle_lib/http_client/work.cpp
+++ src/doodle_lib/http_client/work.cpp
@@ -7,14 +7,53 @@
 #include <doodle_core/core/app_base.h>
 #include <doodle_core/lib_warp/boost_fmt_error.h>
 #include <doodle_core/metadata/computer.h>
+#include <doodle_core/metadata/server_task_info.h>
 
+#include <doodle_lib/core/auto_light_render_video.h>
+#include <doodle_lib/core/down_auto_light_anim_file.h>
 #include <doodle_lib/core/http/http_websocket_data.h>
+#include <doodle_lib/core/up_auto_light_file.h>
+#include <doodle_lib/exe_warp/import_and_render_ue.h>
+#include <doodle_lib/exe_warp/maya_exe.h>
+#include <doodle_lib/exe_warp/ue_exe.h>
 #include <doodle_lib/http_method/computer.h>
+
+#include "exe_warp/maya_exe.h"
+#include <spdlog/async.h>
+#include <spdlog/sinks/basic_file_sink.h>
 namespace doodle::http {
+
+class websocket_sink_mt : public spdlog::sinks::base_sink<std::mutex> {
+  http_work *work_{};
+
+ public:
+  explicit websocket_sink_mt(http_work *in_work) : work_(in_work) {}
+  void sink_it_(const spdlog::details::log_msg &msg) override {
+    if (!work_) return;
+    if (!work_->handle_) return;
+    if (!work_->handle_.any_of<http_websocket_data>()) return;
+    auto &l_websocket = work_->handle_.get<http_websocket_data>();
+    spdlog::memory_buf_t formatted;
+    spdlog::sinks::base_sink<std::mutex>::formatter_->format(msg, formatted);
+    l_websocket.seed(nlohmann::json{
+        {"type", "logger"},
+        {"level", msg.level},
+        {"task_id", work_->task_info_.task_id_},
+        {"msg", fmt::to_string(formatted)}
+    });
+  }
+  void flush_() override {}
+};
+
 void http_work::run(const std::string &in_server_address, std::uint16_t in_port) {
   timer_          = std::make_shared<timer>(g_io_context());
   server_address_ = in_server_address;
   port_           = in_port;
+
+  g_ctx().emplace<maya_exe_ptr>(std::make_shared<maya_exe>());
+  g_ctx().emplace<ue_exe_ptr>(std::make_shared<ue_exe>());
+  core_set_init{}.config_to_user();
+
   do_connect();
 }
 void http_work::do_connect() {
@@ -83,7 +122,6 @@ void http_work::read_task_info(const nlohmann::json &in_json, const entt::handle
   send_state();
   run_task();
 }
-void http_work::run_task() {}
 void http_work::stop() {
   if (handle_) {
     handle_.get<http_websocket_data>().do_close();
@@ -91,5 +129,74 @@ void http_work::stop() {
   timer_->cancel();
   logger_->log(log_loc(), level::info, "http_work stop");
   logger_->flush();
+}
+void http_work::run_task() {
+  if (task_info_.task_info_.is_null()) {
+    return;
+  }
+  core_set_init{}.read_file();
+
+  if (task_info_.task_info_.contains("type")) {
+    auto l_type = task_info_.task_info_["type"].get<run_task_type>();
+    switch (l_type) {
+      case run_task_type::maya_exe: {
+        logger_->log(log_loc(), level::info, "开始运行maya exe任务");
+        end_task({});
+        break;
+      }
+      case run_task_type::UE_exe: {
+        logger_->log(log_loc(), level::info, "开始运行UE exe任务");
+        end_task({});
+        break;
+      }
+      case run_task_type::auto_light_task: {
+        run_auto_light_task();
+        break;
+      }
+    }
+  }
+}
+void http_work::end_task(boost::system::error_code in_error_code) {
+  if (in_error_code) {
+    logger_->log(log_loc(), level::err, "任务执行失败: {}", in_error_code);
+  } else {
+    logger_->log(log_loc(), level::info, "任务执行成功");
+  }
+  if (!handle_) return;
+
+  handle_.get<http_websocket_data>().seed(nlohmann::json{
+      {"type", computer_websocket_fun::set_task},
+      {"status", in_error_code ? server_task_info_status::failed : server_task_info_status::completed}
+  });
+  task_info_.task_id_   = 0;
+  task_info_.task_info_ = nlohmann::json{};
+  send_state();
+}
+
+void http_work::run_auto_light_task() {
+  logger_->log(log_loc(), level::info, "开始运行自动灯光任务");
+  auto l_arg             = maya_exe_ns::export_fbx_arg{};
+  l_arg.file_path        = task_info_.task_info_["file_path"].get<std::string>();
+  l_arg.export_anim_time = task_info_.task_info_["export_anim_time"].get<std::int32_t>();
+  entt::handle l_msg{*g_reg(), g_reg()->create()};
+  l_msg.emplace<process_message>(l_arg.file_path.filename().generic_string());
+  l_msg.emplace<episodes>(task_info_.task_info_["episodes"].get<episodes>());
+  l_msg.emplace<shot>(task_info_.task_info_["shot"].get<shot>());
+  l_msg.emplace<project>(task_info_.task_info_["project"].get<project>());
+
+  down_auto_light_anim_file l_down_anim_file{l_msg};
+  import_and_render_ue l_import_and_render_ue{l_msg};
+  auto_light_render_video l_auto_light_render_video{l_msg};
+  up_auto_light_anim_file l_up_auto_light_file{l_msg};
+  l_up_auto_light_file.async_end(
+      boost::asio::bind_executor(g_io_context(), boost::beast::bind_front_handler(&http_work::end_task, this))
+  );
+  l_auto_light_render_video.async_end(boost::asio::bind_executor(g_io_context(), std::move(l_up_auto_light_file)));
+  l_import_and_render_ue.async_end(boost::asio::bind_executor(g_io_context(), std::move(l_auto_light_render_video)));
+  l_down_anim_file.async_down_end(boost::asio::bind_executor(g_io_context(), std::move(l_import_and_render_ue)));
+
+  g_ctx().get<maya_exe_ptr>()->async_run_maya(
+      l_msg, l_arg, boost::asio::bind_executor(g_io_context(), std::move(l_down_anim_file))
+  );
 }
 }  // namespace doodle::http
--- src/doodle_lib/http_client/work.h
+++ src/doodle_lib/http_client/work.h
@@ -8,17 +8,40 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 namespace doodle::http {
+/**
+ * @brief http_work  主要是接受任务, 并开始执行任务的类
+ *
+ * 任务 : maya_exe, UE_exe, auto_light_task
+ *
+ */
+
+enum class run_task_type {
+  maya_exe,         // 运行maya任务, 这个不会转发到 maya.exe, 会转发给 我们自己编译的exe中
+  UE_exe,           // 运行UE任务, 这个会直接转发到 UE-cmd.exe 中
+  auto_light_task,  // 这个是组合任务, 会自动执行一些任务
+};
+
+NLOHMANN_JSON_SERIALIZE_ENUM(
+    run_task_type,
+    {
+        {run_task_type::maya_exe, "maya_exe"},
+        {run_task_type::UE_exe, "UE_exe"},
+        {run_task_type::auto_light_task, "auto_light_task"},
+    }
+)
+
 class http_work {
   //  using client_core     = doodle::detail::client_core;
   //  using client_core_ptr = std::shared_ptr<client_core>;
   //  boost::asio::basic_io_object;
-  using timer          = boost::asio::high_resolution_timer;
-  using timer_ptr      = std::shared_ptr<timer>;
+  using timer           = boost::asio::high_resolution_timer;
+  using timer_ptr       = std::shared_ptr<timer>;
 
-  using response_type  = boost::beast::http::response<boost::beast::http::string_body>;
-  using request_type   = boost::beast::http::request<boost::beast::http::string_body>;
-  using signal_set     = boost::asio::signal_set;
-  using signal_set_ptr = std::shared_ptr<signal_set>;
+  using response_type   = boost::beast::http::response<boost::beast::http::string_body>;
+  using request_type    = boost::beast::http::request<boost::beast::http::string_body>;
+  using signal_set      = boost::asio::signal_set;
+  using signal_set_ptr  = std::shared_ptr<signal_set>;
+  using logger_sink_ptr = std::shared_ptr<spdlog::sinks::sink>;
 
   struct task_info_t {
     std::int32_t task_id_{};
@@ -36,6 +59,10 @@ class http_work {
   task_info_t task_info_{};
 
   bool is_connect_{false};
+  logger_sink_ptr seed_server_sink_{};
+
+  friend class websocket_sink_mt;
+
   void do_connect();
 
   void do_wait();
@@ -45,6 +72,10 @@ class http_work {
   void read_task_info(const nlohmann::json& in_json, const entt::handle& in_handle);
 
   void run_task();
+
+  void end_task(boost::system::error_code in_error_code);
+
+  void run_auto_light_task();
 
  public:
   http_work()  = default;
