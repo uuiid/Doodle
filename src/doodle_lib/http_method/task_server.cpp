e596a26fef169ad7e5fc80042e46550184e538f6
uuiid <957714080@qq.com>
2024-03-05 14:10:47 +0800

    [自动化灯光] 调整数据发送结构

--- src/doodle_lib/http_method/task_server.cpp
+++ src/doodle_lib/http_method/task_server.cpp
@@ -47,6 +47,7 @@ bool task_server::assign_task() {
 
       l_json["id"]   = l_e;
       l_json["data"] = l_task.data_;
+      l_json["type"] = "task";
       l_websocket.seed(l_json);
       logger_ptr_->log(
           log_loc(), level::info, "分配任务 {}_{} 给 {}({})", l_task.name_, l_e, l_computer.name_, l_computer.ip_
