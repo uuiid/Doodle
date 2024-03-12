e553ebdd12cfb5a245cb5ec73c2bf9bf6555706b
uuiid <957714080@qq.com>
2024-03-12 14:48:54 +0800

    [自动化灯光] 添加快照接口

--- src/doodle_core/sqlite_orm/sqlite_snapshot.cpp
+++ src/doodle_core/sqlite_orm/sqlite_snapshot.cpp
@@ -5,14 +5,4 @@
 #include "sqlite_snapshot.h"
 
 #include <doodle_core/sqlite_orm/sqlite_base.h>
-namespace doodle::snapshot {
-
-void sqlite_snapshot::operator()(std::underlying_type_t<entt::entity> in_underlying_type) {
-}
-void sqlite_snapshot::operator()(entt::entity in_entity) {}
-void sqlite_snapshot::operator()(std::underlying_type_t<entt::entity>& in_underlying_type) {
-
-}
-void sqlite_snapshot::operator()(entt::entity& in_entity) {}
-
-}  // namespace doodle::snapshot
+namespace doodle::snapshot {}  // namespace doodle::snapshot
--- src/doodle_core/sqlite_orm/sqlite_snapshot.h
+++ src/doodle_core/sqlite_orm/sqlite_snapshot.h
@@ -3,84 +3,137 @@
 //
 
 #pragma once
+#include <doodle_core/core/core_sql.h>
 #include <doodle_core/doodle_core_fwd.h>
 
 #include <boost/hana.hpp>
 
+#include <core/core_sql.h>
 #include <entt/entt.hpp>
+#include <sqlpp11/sqlite3/sqlite3.h>
+#include <sqlpp11/sqlpp11.h>
 namespace doodle::snapshot {
 
 class sqlite_snapshot {
+  struct save_snapshot_t {
+    entt::snapshot snapshot_;
+    entt::meta_func begin_save_func;
+    entt::meta_func save_func;
+    entt::entity entity_{};
+    entt::meta_any any_data_;
+    bool is_entity_{false};
+    conn_ptr conn_ptr;
+    explicit save_snapshot_t(entt::registry& in_registry, conn_ptr in_conn)
+        : snapshot_{in_registry}, conn_ptr{std::move(in_conn)} {}
+
+    template <typename Component>
+    auto save() {
+      set_save_func<Component>();
+      is_entity_ = std::is_same_v<Component, entt::entity>;
+      if (begin_save_func) any_data_ = begin_save_func.invoke({}, conn_ptr);
+      snapshot_.get<Component>(*this);
+      return *this;
+    }
+    template <typename Component, typename It>
+      requires(!std::is_same_v<Component, entt::entity>)
+    auto save(It first, It last) {
+      set_save_func<Component>();
+      is_entity_ = std::is_same_v<Component, entt::entity>;
+      if (begin_save_func) any_data_ = begin_save_func.invoke({}, conn_ptr);
+      snapshot_.get<Component>(*this, first, last);
+      return *this;
+    }
+
+   private:
+    template <typename Component>
+    void set_save_func() {
+      auto l_mate     = entt::resolve<Component>();
+      begin_save_func = l_mate.func("begin_save"_hs);
+      save_func       = l_mate.func("save"_hs);
+    }
+
+    inline void operator()(std::underlying_type_t<entt::entity> in_underlying_type) {}
+    // 然后是实体和对应组件的循环
+    inline void operator()(entt::entity in_entity) {
+      entity_ = in_entity;
+      entt::meta_any l_any[2]{any_data_, entt::forward_as_meta(conn_ptr)};
+      if (is_entity_) save_func.invoke(in_entity, l_any, 2);
+    }
+
+    // 组件的加载和保存
+    template <typename T>
+    void operator()(const T& in_t) {
+      entt::meta_any l_any[3]{entity_, any_data_, entt::forward_as_meta(conn_ptr)};
+      save_func.invoke(in_t, l_any, 3);
+    }
+  };
+
+  struct load_snapshot_t {
+    entt::snapshot_loader loader_;
+    entt::meta_func begin_load_func;
+    entt::meta_func load_func;
+    entt::meta_func get_size_func;
+    entt::meta_any any_data_;
+    conn_ptr conn_ptr;
+    explicit load_snapshot_t(entt::registry& in_registry, conn_ptr in_conn)
+        : loader_{in_registry}, conn_ptr{std::move(in_conn)} {}
+
+    template <typename Registry>
+    friend class entt::basic_snapshot_loader;
+
+    template <typename Component>
+    auto load() {
+      set_load_func<Component>();
+      if (begin_load_func) any_data_ = begin_load_func.invoke({});
+      loader_.get<Component>(*this);
+      return *this;
+    }
+
+    template <typename Component>
+    void set_load_func() {
+      auto l_mate     = entt::resolve<Component>();
+      get_size_func   = l_mate.func("get_size"_hs);
+      begin_load_func = l_mate.func("begin_load"_hs);
+      load_func       = l_mate.func("load"_hs);
+    }
+    // load
+    inline void operator()(std::underlying_type_t<entt::entity>& in_underlying_type) {
+      in_underlying_type = get_size_func.invoke({}).cast<std::underlying_type_t<entt::entity>>();
+    }
+    inline void operator()(entt::entity& in_entity) {
+      in_entity = load_func.invoke({}, any_data_).cast<entt::entity>();
+    }
+    template <typename T>
+    void operator()(T& in_t) {
+      load_func.invoke(in_t, any_data_);
+    }
+  };
+
   FSys::path data_path_{};
   entt::registry& registry_;
-  std::unique_ptr<entt::snapshot> snapshot_{};
-  std::unique_ptr<entt::snapshot_loader> loader_{};
-  template <typename Registry>
-  friend class entt::basic_snapshot;
-  template <typename Registry>
-  friend class basic_snapshot_loader;
 
  public:
   explicit sqlite_snapshot(FSys::path in_data_path, entt::registry& in_registry)
       : data_path_(in_data_path.empty() ? FSys::path{":memory:"} : std::move(in_data_path)), registry_{in_registry} {}
 
   virtual ~sqlite_snapshot() = default;
-
-  template <typename Component>
-    requires std::is_same_v<Component, entt::entity>
-  auto load() {
-    if (!loader_) {
-      loader_ = std::make_unique<entt::snapshot_loader>(registry_);
-    }
-    loader_->get<Component>(*this);
-    return *this;
-  }
-  template <typename Component>
-    requires(!std::is_same_v<Component, entt::entity>)
-  auto load() {
-    if (!loader_) {
-      loader_ = std::make_unique<entt::snapshot_loader>(registry_);
-    }
-    loader_->get<Component>(*this);
-    return *this;
-  }
-  template <typename Component>
-    requires std::is_same_v<Component, entt::entity>
-  auto save() {
-    if (!snapshot_) {
-      snapshot_ = std::make_unique<entt::snapshot>(registry_);
-    }
-
-    snapshot_->get<Component>(*this);
-    return *this;
-  }
-  template <typename Component>
-    requires(!std::is_same_v<Component, entt::entity>)
-  auto save() {
-    if (!snapshot_) {
-      snapshot_ = std::make_unique<entt::snapshot>(registry_);
-    }
-    snapshot_->get<Component>(*this);
-    return *this;
+  template <typename Component...>
+  void save() {
+    database_info l_info{};
+    l_info.path_ = data_path_;
+    save_snapshot_t l_save{registry_, l_info.get_connection()};
+    auto l_tx = sqlpp::start_transaction(*l_save.conn_ptr);
+    (l_save.template save<Component>(), ...);
   }
 
- private:
-  // 先是大小
-  void operator()(std::underlying_type_t<entt::entity> in_underlying_type);
-  // 然后是实体和对应组件的循环
-  void operator()(entt::entity in_entity);
-
-  void operator()(std::underlying_type_t<entt::entity>& in_underlying_type);
-  void operator()(entt::entity& in_entity);
-
-  template <typename Component>
-  void save(FSys::path in_data_path, const std::vector<std::int64_t>& in_delete_id);
-
-  // 组件的加载和保存
-  template <typename T>
-  void operator()(const T& in_t) {}
-  template <typename T>
-  void operator()(T& in_t) {}
+  template <typename Component...>
+  void load() {
+    database_info l_info{};
+    l_info.path_ = data_path_;
+    load_snapshot_t l_load{registry_, l_info.get_connection_const()};
+    auto l_tx = sqlpp::start_transaction(*l_load.conn_ptr);
+    (l_load.template load<Component>(), ...);
+  }
 };
 
 }  // namespace doodle::snapshot
