57be2c0c0196fa657b24ad1699012662ea964f6d
uuiid <957714080@qq.com>
2024-03-13 13:45:07 +0800

    [自动化灯光] 添加加载测试

--- src/test/core/database_sqlite3.cpp
+++ src/test/core/database_sqlite3.cpp
@@ -182,4 +182,8 @@ BOOST_AUTO_TEST_CASE(test_sqlite3_snapshot) {
   create_test_database();
   snapshot::sqlite_snapshot l_snap{"D:/test.db", *g_reg()};
   l_snap.save<entt::entity>();
+
+  entt::registry l_reg{};
+  snapshot::sqlite_snapshot l_snap2{"D:/test.db", l_reg};
+  l_snap2.load<entt::entity>();
 }
