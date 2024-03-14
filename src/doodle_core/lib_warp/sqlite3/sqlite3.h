0d582fed962ac5e796f185bad73e8bf1855a678f
uuiid <957714080@qq.com>
2024-03-14 10:28:14 +0800

    [自动化灯光] 调整基本的辅助类

--- src/doodle_core/lib_warp/sqlite3/sqlite3.h
+++ src/doodle_core/lib_warp/sqlite3/sqlite3.h
@@ -3,6 +3,8 @@
 //
 
 #pragma once
+#include <sqlpp11/sqlite3/sqlite3.h>
+//
 #include <doodle_core/lib_warp/sqlite3/excluded.h>
 #include <doodle_core/lib_warp/sqlite3/insert.h>
 #include <doodle_core/lib_warp/sqlite3/on_conflict.h>
