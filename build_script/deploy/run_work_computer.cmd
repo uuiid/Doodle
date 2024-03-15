f7d9cc6743997b232ac467e482ded50c4d54d022
uuiid <957714080@qq.com>
2024-03-15 16:22:40 +0800

    [自动化灯光] 调整辅助脚本和添加只构建脚本

--- build_script/deploy/run_work_computer.cmd
+++ build_script/deploy/run_work_computer.cmd
@@ -1,11 +1,4 @@
-REM Set-NetFirewallRule -DisplayGroup “File And Printer Sharing” -Enabled True -Profile Private
-cd %~dp0
-
-set current_branch=
-for /F "delims=" %%n in ('git describe --tags --match "v*"') do set "current_branch=%%n"
-
-for /f "tokens=1 delims=-" %%i in ("%current_branch%") do (set current_branch=%%i)
-set current_branch=%current_branch:~1%
-set Doodle_Name=Doodle-%current_branch%-win64
+call %~dp0/../tool/get_build_name.cmd
+cd /D %~dp0
 PsExec.exe \\192.168.20.7 -u administrator -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\server_cmd.cmd %Doodle_Name%"
 
