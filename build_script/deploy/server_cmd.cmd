6bfe6b115cea27ad39c5b530e483833eb17bae8a
uuiid <957714080@qq.com>
2024-03-15 14:44:27 +0800

    [自动化灯光] 调整服务关闭

--- build_script/deploy/server_cmd.cmd
+++ build_script/deploy/server_cmd.cmd
@@ -5,9 +5,13 @@ set PATH=%PATH%;C:\Program Files\7-Zip\
 7z.exe x //192.168.20.59/Doodle2/build/Ninja_release/%DoodleName%.7z -o%DoodleTmpDir% -y
 @REM 停止服务
 net stop doodle_scan_win_service
+net stop doodle_http_service
+
 @REM 复制文件
 robocopy %DoodleTmpDir%/%DoodleName% D:/doodle_exe/ /MIR
 rmdir /Q /S %DoodleTmpDir%
 
 @REM 启动服务
 net start doodle_scan_win_service
+net start doodle_http_service
+
