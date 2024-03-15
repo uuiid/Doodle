2b2b1622713c8fc85104eaa101b5a9762c789ae7
uuiid <957714080@qq.com>
2024-03-15 15:50:25 +0800

    [自动化灯光] 调整运行测试客户端位置

--- build_script/deploy/run_work_computer.cmd
+++ build_script/deploy/run_work_computer.cmd
@@ -7,5 +7,5 @@ for /F "delims=" %%n in ('git describe --tags --match "v*"') do set "current_bra
 for /f "tokens=1 delims=-" %%i in ("%current_branch%") do (set current_branch=%%i)
 set current_branch=%current_branch:~1%
 set Doodle_Name=Doodle-%current_branch%-win64
-PsExec.exe \\192.168.20.2 -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\server_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.7 -u administrator -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\server_cmd.cmd %Doodle_Name%"
 
