c57eca27b3ad83290a7957b3ff0c6d993a9e0a5a
uuiid <957714080@qq.com>
2024-03-15 10:48:33 +0800

    [自动化灯光] 添加基础的部署

--- build_script/deploy/run_server.cmd
+++ build_script/deploy/run_server.cmd
@@ -1,6 +1,4 @@
 REM Set-NetFirewallRule -DisplayGroup “File And Printer Sharing” -Enabled True -Profile Private
 cd %~dp0
-PsExec.exe \\192.168.20.2 -u auto_light -p root -i cmd \\192.168.20.59\doodle2\build_script\deploy\server_cmd.cmd
-PsExec.exe \\192.168.20.2 -u auto_light -p root -i cmd.exe "/C \\192.168.20.59\doodle2\build_script\deploy\server_cmd.cmd"
 PsExec.exe \\192.168.20.2 -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\server_cmd.cmd"
 
--- build_script/deploy/server_cmd.cmd
+++ build_script/deploy/server_cmd.cmd
@@ -1,1 +1,13 @@
-echo fasfr
+set DoodleName=Doodle-3.6.183-win64
+set DoodleTmpDir=D:\doodle_exe_tmp
+set PATH=%PATH%;C:\Program Files\7-Zip\
+
+7z.exe x //192.168.20.59/Doodle2/build/Ninja_release/%DoodleName%.7z -o%DoodleTmpDir% -y
+REM 停止服务
+net stop doodle_scan_win_service
+REM 复制文件
+robocopy %DoodleTmpDir%/%DoodleName% D:/doodle_exe/ /MIR
+rmdir /Q /S %DoodleTmpDir%
+
+REM 启动服务
+net start doodle_scan_win_service
