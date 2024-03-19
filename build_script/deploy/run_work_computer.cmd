48fa55c1284df4921c74831270b928be1eafb82d
uuiid <957714080@qq.com>
2024-03-19 16:09:38 +0800

    [自动化灯光] 调整部署错误

--- build_script/deploy/run_work_computer.cmd
+++ build_script/deploy/run_work_computer.cmd
@@ -1,4 +1,4 @@
 call %~dp0/../tool/get_build_name.cmd
 cd /D %~dp0
-PsExec.exe \\192.168.20.7 -u administrator -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\server_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.7 -u administrator -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
 
