963dbfd84913e3e7aba29399cb2b980deca35d99
uuiid <957714080@qq.com>
2024-03-27 09:55:31 +0800

    [自动化灯光] 调整部署方法

--- build_script/deploy/detail/work_computer_cmd.cmd
+++ build_script/deploy/detail/work_computer_cmd.cmd
@@ -2,13 +2,15 @@ set DoodleName=%1
 set DoodleTmpDir=D:\doodle_exe_tmp
 set PATH=%PATH%;C:\Program Files\7-Zip\
 
+mkdir %DoodleTmpDir%
+
 7z.exe x //192.168.20.59/Doodle2/build/Ninja_release/%DoodleName%.7z -o%DoodleTmpDir% -y
 @REM net stop doodle_http_client_service
 
 reg import //192.168.20.59/Doodle2/script/sy.reg
 
 @REM 复制文件
-robocopy %DoodleTmpDir%/%DoodleName% D:/doodle_exe/ /MIR
+robocopy %DoodleTmpDir%/%DoodleName% D:/doodle_exe/ /MIR /xd cgru
 rmdir /Q /S %DoodleTmpDir%
 
 @REM net start doodle_http_client_service
--- build_script/deploy/run_work_computer.cmd
+++ build_script/deploy/run_work_computer.cmd
@@ -1,4 +1,15 @@
 call %~dp0/../tool/get_build_name.cmd
 cd /D %~dp0
-PsExec.exe \\192.168.20.7 -u administrator -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+@REM PsExec.exe \\192.168.20.7 -u administrator -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+@REM PsExec.exe \\192.168.20.66  -u auto_light         -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+@REM PsExec.exe \\192.168.20.65  -u auto_light         -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+@REM PsExec.exe \\192.168.20.190 -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
 
+
+PsExec.exe \\192.168.20.44  -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.46  -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.48  -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.51  -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.52  -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+PsExec.exe \\192.168.20.56  -u auto_light -p root -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
+@REM PsExec.exe \\192.168.20.188 -u auto_light         -i cmd.exe /C "\\192.168.20.59\doodle2\build_script\deploy\detail\work_computer_cmd.cmd %Doodle_Name%"
