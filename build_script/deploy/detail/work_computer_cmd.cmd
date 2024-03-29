0602c94c28e6bbcbf4f9fe405a5ee464268f0589
uuiid <957714080@qq.com>
2024-03-29 11:51:59 +0800

    [自动化灯光] 调整文件部署

--- build_script/deploy/detail/work_computer_cmd.cmd
+++ build_script/deploy/detail/work_computer_cmd.cmd
@@ -1,19 +1,10 @@
 set DoodleName=%1
-robocopy //192.168.20.59/Doodle2/build/7z D:/7z /MIR
-
-set DoodleTmpDir=D:\doodle_exe_tmp
-set PATH=%PATH%;D:/7z
-
-mkdir %DoodleTmpDir%
-
-7za.exe x //192.168.20.59/Doodle2/build/Ninja_release/%DoodleName%.7z -o%DoodleTmpDir% -y
-@REM net stop doodle_http_client_service
+set DoodleSource=//192.168.20.59/Doodle2/build/Ninja_release/_CPack_Packages/win64/7Z/%DoodleName%
 
 reg import //192.168.20.59/Doodle2/script/sy.reg
 
 @REM 复制文件
-robocopy %DoodleTmpDir%/%DoodleName% D:/doodle_exe/ /MIR /xd cgru
-rmdir /Q /S %DoodleTmpDir%
+robocopy %DoodleSource% D:/doodle_exe/ /MIR /xd cgru
 
 @REM net start doodle_http_client_service
 
