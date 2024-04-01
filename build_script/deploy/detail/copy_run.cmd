e8333f79417998817e3c64e54dcc0ec72845fb70
uuiid <957714080@qq.com>
2024-04-01 11:04:16 +0800

    [自动化灯光] 添加部署

--- build_script/deploy/detail/copy_run.cmd
+++ build_script/deploy/detail/copy_run.cmd
@@ -1,3 +1,4 @@
 copy \\192.168.20.59\doodle2\script\Cmd_tool\run2.7.exe "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run.exe"
 copy \\192.168.20.59\doodle2\script\Cmd_tool\run4.8.exe "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run4.exe"
 copy "\\192.168.20.59\doodle2\build_script\deploy\detail\start_cgru.cmd" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start.cmd"
+copy "\\192.168.20.59\doodle2\build_script\deploy\detail\start_cgru.cmd" "%USERPROFILE%\Desktop\运行农场注册.cmd"
