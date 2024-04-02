4c93fd826e12525e5798c0cabc120a504068aec2
uuiid <957714080@qq.com>
2024-04-02 14:44:59 +0800

    [自动化灯光] 添加usd vcpkg 构建

---
+++ build_script/usd/build.cmd
@@ -0,0 +1,4 @@
+pip install setuptools
+pip install pySide6
+pip install PyOpenGL
+python "E:\Doodle\external\USD\OpenUSD\build_scripts\build_usd.py" --embree --openimageio --opencolorio --alembic --hdf5  --materialx  ../build
