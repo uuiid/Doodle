eddabfc22af3dc8b502fb03e72034a5aaac36a80
uuiid <957714080@qq.com>
2024-04-02 17:06:58 +0800

    [自动化灯光] 添加usd 插件库

--- src/usd/rendering_backend/src/rendererPlugin.cpp
+++ src/usd/rendering_backend/src/rendererPlugin.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "rendererPlugin.h"
+
+#include "pxr/imaging/hd/rendererPluginRegistry.h"
+#include "pxr/imaging/hdSt/renderDelegate.h"
+#include "pxr/pxr.h"
+namespace pxr {
+TF_REGISTRY_FUNCTION(pxr::TfType) { pxr::HdRendererPluginRegistry::Define<doodle::usd::HdStormRendererPlugin>(); }
+}  // namespace pxr
+namespace doodle::usd {
+pxr::HdRenderDelegate* HdStormRendererPlugin::CreateRenderDelegate() { return new pxr::HdStRenderDelegate(); }
+
+pxr::HdRenderDelegate* HdStormRendererPlugin::CreateRenderDelegate(pxr::HdRenderSettingsMap const& settingsMap) {
+  return new pxr::HdStRenderDelegate(settingsMap);
+}
+
+void HdStormRendererPlugin::DeleteRenderDelegate(pxr::HdRenderDelegate* renderDelegate) { delete renderDelegate; }
+
+bool HdStormRendererPlugin::IsSupported(bool gpuEnabled) const {
+  return gpuEnabled ? pxr::HdStRenderDelegate::IsSupported() : false;
+}
+}  // namespace doodle::usd
--- src/usd/rendering_backend/src/rendererPlugin.h
+++ src/usd/rendering_backend/src/rendererPlugin.h
@@ -3,3 +3,23 @@
 //
 
 #pragma once
+#include "pxr/imaging/hd/rendererPlugin.h"
+#include "pxr/pxr.h"
+namespace doodle::usd {
+class HdStormRendererPlugin final : public pxr::HdRendererPlugin {
+ public:
+  HdStormRendererPlugin()          = default;
+  virtual ~HdStormRendererPlugin() = default;
+
+  virtual pxr::HdRenderDelegate *CreateRenderDelegate() override;
+  virtual pxr::HdRenderDelegate *CreateRenderDelegate(pxr::HdRenderSettingsMap const &settingsMap) override;
+
+  virtual void DeleteRenderDelegate(pxr::HdRenderDelegate *renderDelegate) override;
+
+  virtual bool IsSupported(bool gpuEnabled = true) const override;
+
+ private:
+  HdStormRendererPlugin(const HdStormRendererPlugin &)            = delete;
+  HdStormRendererPlugin &operator=(const HdStormRendererPlugin &) = delete;
+};
+}  // namespace doodle::usd
