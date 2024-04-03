e2cc8ea3b6100883baca914106d54e09175fdfe9
uuiid <957714080@qq.com>
2024-04-03 15:28:35 +0800

    [自动灯光] 调整部署自启动

--- build_script/usd/build.cmd
+++ build_script/usd/build.cmd
@@ -1,4 +1,17 @@
 pip install setuptools
 pip install pySide6
 pip install PyOpenGL
-python "E:\Doodle\external\USD\OpenUSD\build_scripts\build_usd.py" --embree --openimageio --opencolorio --alembic --hdf5  --materialx  ../build
+cd E:\Doodle\external\USD\OpenUSD
+python "E:\Doodle\external\USD\OpenUSD\build_scripts\build_usd.py" --embree --openimageio --opencolorio --alembic --hdf5  --materialx --build-variant debug --build ../build ../debug
+python "E:\Doodle\external\USD\OpenUSD\build_scripts\build_usd.py" --embree --openimageio --opencolorio --alembic --hdf5  --materialx --build-variant release --build ../build ../release
+
+
+
+
+
+    The following in your PYTHONPATH environment variable:
+    E:\Doodle\external\USD\debug\lib\python
+
+    The following in your PATH environment variable:
+    E:\Doodle\external\USD\debug\bin
+    E:\Doodle\external\USD\debug\lib
