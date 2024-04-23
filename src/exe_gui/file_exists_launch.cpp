2d7bb36389b4c24b317587b586d8078f301afc83
uuiid <957714080@qq.com>
2024-04-23 14:52:30 +0800

    [kitsu] 添加exe编译

---
+++ src/exe_gui/file_exists_launch.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 24-4-23.
+//
+#include <doodle_core/core/app_service.h>
+
+#include <doodle_lib/launch/file_exists_launch.h>
+
+#include "main_macro.h"
+
+DOODLE_SERVICE_MAIN_IMPL(app_service_t, doodle::launch::file_exists_launch_t);
