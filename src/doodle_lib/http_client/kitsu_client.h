f09d684e16f67799bbff3e7966e4a5e9924a2fd7
uuiid <957714080@qq.com>
2024-05-15 13:24:34 +0800

    添加查询task

--- src/doodle_lib/http_client/kitsu_client.h
+++ src/doodle_lib/http_client/kitsu_client.h
@@ -4,6 +4,8 @@
 
 #include <doodle_lib/core/http/json_body.h>
 
+#include <boost/algorithm/string.hpp>
+
 namespace doodle::kitsu {
 class kitsu_client;
 
@@ -11,36 +13,14 @@ class kitsu_request_header_operator {
  public:
   kitsu_client* kitsu_client_ptr_{};
   template <typename T, typename ResponeType>
-  void operator()(T* in_http_client_core, ResponeType& in_req) {
-    in_req.set(boost::beast::http::field::accept, "application/json");
-    in_req.set(boost::beast::http::field::content_type, "application/json");
-    in_req.set(boost::beast::http::field::host, boost::asio::ip::host_name());
-    in_req.set(boost::beast::http::field::authorization, "Bearer " + kitsu_client_ptr_->access_token_);
-    if(!kitsu_client_ptr_->session_cookie_.empty()) {
-      in_req.set(boost::beast::http::field::cookie, kitsu_client_ptr_->session_cookie_);
-    }
-    in_req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-    in_req.keep_alive(true);
-    in_req.prepare_payload();
-  }
+  void operator()(T* in_http_client_core, ResponeType& in_req);
 };
 
 class kitsu_response_header_operator {
  public:
   kitsu_client* kitsu_client_ptr_{};
   template <typename T, typename ResponeType>
-  void operator()(T* in_http_client_core, ResponeType& in_req) {
-    auto l_set_cookie = in_req[boost::beast::http::field::set_cookie];
-    if (!l_set_cookie.empty()) {
-      std::vector<std::string> l_cookie;
-      boost::split(l_cookie, l_set_cookie, boost::is_any_of(";"));
-      for (auto& l_item : l_cookie) {
-        if (l_item.starts_with("session=")) {
-          kitsu_client_ptr_->session_cookie_ = l_item;
-        }
-      }
-    }
-  }
+  void operator()(T* in_http_client_core, ResponeType& in_req);
 };
 
 class kitsu_client {
@@ -56,7 +36,10 @@ class kitsu_client {
 
  public:
   kitsu_client(std::string in_ip, std::string in_port)
-      : http_client_core_ptr_(std::make_shared<http_client_core>(std::move(in_ip), std::move(in_port))) {}
+      : http_client_core_ptr_(std::make_shared<http_client_core>(std::move(in_ip), std::move(in_port))) {
+    http_client_core_ptr_->request_header_operator().kitsu_client_ptr_  = this;
+    http_client_core_ptr_->response_header_operator().kitsu_client_ptr_ = this;
+  }
   ~kitsu_client() = default;
 
   template <typename CompletionHandler>
@@ -75,7 +58,7 @@ class kitsu_client {
                      l_com(ec, nlohmann::json{});
                      return;
                    }
-                   res[boost::beast::http::field::content_type];
+
                    if (res.result() != boost::beast::http::status::ok) {
                      http_client_core_ptr_->logger()->log(
                          log_loc(), level::err, "login failed: {}", magic_enum::enum_integer(res.result())
@@ -86,12 +69,68 @@ class kitsu_client {
                    auto l_json = res.body();
                    if (l_json["login"].get<bool>()) {
                      http_client_core_ptr_->logger()->info("login success");
-                     http_client_core_ptr_->access_token_ = l_json["access_token"].get<std::string>();
+                     access_token_ = l_json["access_token"].get<std::string>();
                    }
                    l_com(ec, std::move(l_json));
                  }
              )
     );
   }
+
+  template <typename CompletionHandler>
+  void get_task(std::string in_uuid, CompletionHandler&& in_completio) {
+    boost::beast::http::request<boost::beast::http::empty_body> req{
+        boost::beast::http::verb::get, fmt::format("/api/data/tasks/{}/full", in_uuid), 11
+    };
+    http_client_core_ptr_->async_read<boost::beast::http::response<boost::beast::http::string_body>>(
+        req, boost::asio::bind_executor(
+                 g_io_context().get_executor(),
+                 [l_com = std::move(in_completio),
+                  this](boost::system::error_code ec, boost::beast::http::response<boost::beast::http::string_body> res) {
+                   if (ec) {
+                     http_client_core_ptr_->logger()->log(log_loc(), level::err, "get task failed: {}", ec.message());
+                     l_com(ec, nlohmann::json{});
+                     return;
+                   }
+
+                   if (res.result() != boost::beast::http::status::ok) {
+                     http_client_core_ptr_->logger()->log(
+                         log_loc(), level::err, "get task failed: {}", magic_enum::enum_integer(res.result())
+                     );
+                     l_com(ec, nlohmann::json{});
+                     return;
+                   }
+                   auto l_json = nlohmann::json::parse(res.body());
+                   l_com(ec, l_json);
+                 }
+             )
+    );
+  }
 };
+template <typename T, typename ResponeType>
+void kitsu_request_header_operator::operator()(T* in_http_client_core, ResponeType& in_req) {
+  in_req.set(boost::beast::http::field::accept, "application/json");
+  in_req.set(boost::beast::http::field::content_type, "application/json");
+  in_req.set(boost::beast::http::field::host, boost::asio::ip::host_name());
+  in_req.set(boost::beast::http::field::authorization, "Bearer " + kitsu_client_ptr_->access_token_);
+  if (!kitsu_client_ptr_->session_cookie_.empty()) {
+    in_req.set(boost::beast::http::field::cookie, kitsu_client_ptr_->session_cookie_ + ";");
+  }
+  in_req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+  in_req.keep_alive(true);
+  in_req.prepare_payload();
+}
+template <typename T, typename ResponeType>
+void kitsu_response_header_operator::operator()(T* in_http_client_core, ResponeType& in_req) {
+  auto l_set_cookie = in_req[boost::beast::http::field::set_cookie];
+  if (!l_set_cookie.empty()) {
+    std::vector<std::string> l_cookie;
+    boost::split(l_cookie, l_set_cookie, boost::is_any_of(";"));
+    for (auto& l_item : l_cookie) {
+      if (l_item.starts_with("session=")) {
+        kitsu_client_ptr_->session_cookie_ = l_item;
+      }
+    }
+  }
+}
 }  // namespace doodle::kitsu
--- src/test/kitsu/auth.cpp
+++ src/test/kitsu/auth.cpp
@@ -17,3 +17,19 @@ BOOST_AUTO_TEST_CASE(kitsu_login) {
   });
   g_io_context().run();
 }
+
+BOOST_AUTO_TEST_CASE(kitsu_get_task) {
+  doodle_lib l_lib{};
+  auto l_c = std::make_shared<doodle::kitsu::kitsu_client>("192.168.40.182", "80");
+  l_c->longin("957714080@qq.com", "8jO6sJm5EYAZSuZ7wy3P", [l_c](boost::system::error_code ec, nlohmann::json in_json) {
+    BOOST_TEST(!ec);
+    BOOST_TEST_MESSAGE(in_json.dump());
+    BOOST_TEST(in_json["login"].get<bool>());
+
+    l_c->get_task("e5ece525-3e5b-4c71-be85-32e32d336e4e", [](boost::system::error_code ec, nlohmann::json in_json) {
+      BOOST_TEST(!ec);
+      BOOST_TEST_MESSAGE(in_json.dump());
+    });
+  });
+  g_io_context().run();
+}
