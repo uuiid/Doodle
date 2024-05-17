f5e36be46710837264922c8f5797491be25154e0
uuiid <957714080@qq.com>
2024-05-17 15:08:10 +0800

    添加任务分配

--- src/doodle_lib/http_method/task_server.cpp
+++ src/doodle_lib/http_method/task_server.cpp
@@ -10,53 +10,69 @@
 #include <doodle_core/metadata/server_task_info.h>
 
 #include <doodle_lib/core/http/http_websocket_data.h>
+#include <doodle_lib/http_method/computer_reg_data.h>
 namespace doodle::http {
 task_server::task_server()
     : logger_ptr_(g_logger_ctrl().make_log("task_server")), timer_ptr_(std::make_shared<timer_t>(g_io_context())) {}
 
 void task_server::run() {
-  if (!assign_task()) return;
+  if (is_running_) return;
+  boost::asio::post(g_io_context(), [this] { begin_assign_task(); });
+}
 
+void task_server::begin_assign_task() {
   timer_ptr_->expires_after(std::chrono::seconds(1));
-  timer_ptr_->async_wait([this](boost::system::error_code ec) {
+  timer_ptr_->async_wait([this, l_g = run_guard_t{this}](boost::system::error_code ec) {
     if (ec) {
       logger_ptr_->log(log_loc(), level::warn, "timer_ptr_ error: {}", ec);
       return;
     }
-    run();
+    if (assign_task()) {
+      run();
+    }
   });
 }
 
 bool task_server::assign_task() {
-  bool has_task = false;
-  for (auto&& [l_e, l_task] : g_reg()->view<server_task_info>().each()) {
-    if (l_task.status_ != server_task_info_status::submitted) continue;
-    has_task = true;
+  bool has_task    = false;
+  auto l_computers = g_websocket_data_manager().get_list();
+  for (auto&& [id_, l_task] : task_map_) {
+    if (l_task->status_ != server_task_info_status::submitted) continue;
 
-    for (auto&& [l_e2, l_computer, l_websocket] : g_reg()->view<computer, http_websocket_data>().each()) {
-      if (l_computer.server_status_ != computer_status::free || l_computer.client_status_ != computer_status::free)
-        continue;
+    for (auto& l_computer : l_computers) {
+      if (auto l_computer_data = std::static_pointer_cast<computer_reg_data>(l_computer->user_data_); l_computer_data) {
+        if (l_computer_data->computer_data_.server_status_ != computer_status::free ||
+            l_computer_data->computer_data_.client_status_ != computer_status::free)
+          continue;
 
-      l_computer.server_status_ = computer_status::busy;
-      l_task.status_            = server_task_info_status::assigned;
-      l_task.run_computer_      = l_computer.name_;
-      l_task.run_time_          = std::chrono::system_clock::now();
-      l_task.run_computer_ip_   = l_computer.ip_;
-      g_reg()->patch<server_task_info>(l_e);
+        l_computer_data->computer_data_.server_status_ = computer_status::busy;
+        l_task->status_                                = server_task_info_status::assigned;
+        l_task->run_computer_                          = l_computer_data->computer_data_.name_;
+        l_task->run_time_                              = std::chrono::system_clock::now();
+        l_task->run_computer_ip_                       = l_computer_data->computer_data_.ip_;
+        {
+          auto l_db = g_pool_db().get_connection();
+          l_task->install_db(l_db);
+        }
+        nlohmann::json l_json{};
 
-      nlohmann::json l_json{};
-
-      l_json["id"]   = l_e;
-      l_json["data"] = l_task.data_;
-      l_json["type"] = "task";
-      l_websocket.seed(l_json);
-      logger_ptr_->log(
-          log_loc(), level::info, "分配任务 {}_{} 给 {}({})", l_task.name_, l_e, l_computer.name_, l_computer.ip_
-      );
-      entt::handle l_computer_handle{*g_reg(), l_e2};
-      l_computer_handle.emplace_or_replace<task_ref>(entt::handle{*g_reg(), l_e});
+        l_json["id"]   = id_;
+        l_json["data"] = l_task->data_;
+        l_json["type"] = "task";
+        l_computer->seed(l_json);
+        logger_ptr_->log(
+            log_loc(), level::info, "分配任务 {}_{} 给 {}({})", l_task->name_, id_,
+            l_computer_data->computer_data_.name_, l_computer_data->computer_data_.ip_
+        );
+        l_computer_data->task_info_ = l_task;
+        has_task                    = true;
+        break;
+      }
     }
   }
+
+  
+
   return has_task;
 }
 
--- src/doodle_lib/http_method/task_server.h
+++ src/doodle_lib/http_method/task_server.h
@@ -5,7 +5,9 @@
 #pragma once
 
 #include <doodle_lib/doodle_lib_fwd.h>
-
+namespace doodle {
+class server_task_info;
+}
 namespace doodle::http {
 
 class task_server {
@@ -18,11 +20,24 @@ class task_server {
   // 分配任务
   bool assign_task();
 
+  bool is_running_ = false;
+  // 运行守卫
+  struct run_guard_t {
+    explicit run_guard_t(task_server* in_ptr) : ptr_(in_ptr) { ptr_->is_running_ = true; }
+    ~run_guard_t() { ptr_->is_running_ = false; }
+    task_server* ptr_;
+  };
+
+  void begin_assign_task();
+
  public:
   task_server();
   ~task_server() = default;
 
+  // 此处可进行多线程调用
   void run();
+
+  std::map<boost::uuids::uuid, std::shared_ptr<doodle::server_task_info>> task_map_{};  // 任务列表
 };
 
 }  // namespace doodle::http
