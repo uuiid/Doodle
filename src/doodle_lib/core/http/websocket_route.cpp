28f214ce7de045efbee55e109581ddbf50c47534
uuiid <957714080@qq.com>
2024-05-23 15:06:07 +0800

    添加websocket路由

--- src/doodle_lib/core/http/websocket_route.cpp
+++ src/doodle_lib/core/http/websocket_route.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "websocket_route.h"
+#include <doodle_lib/core/http/http_websocket_data.h>
+namespace doodle::http {
+websocket_route::map_actin_type websocket_route::operator()(const std::string& in_name) const {
+  auto l_iter = actions_.find(in_name);
+  if (l_iter == actions_.end()) return not_function_;
+  return l_iter->second;
+}
+
+websocket_route& websocket_route::reg(const std::string& in_name, const map_actin_type& in_function) {
+  actions_[in_name] = in_function;
+  return *this;
+}
+
+void websocket_route::not_function(const http_websocket_data_ptr&  in_data, const nlohmann::json& in_json) {
+  in_data->logger_->log(log_loc(), level::err, "websocket not found {}", in_json["type"].get<std::string>());
+}
+}  // namespace doodle::http
--- src/doodle_lib/core/http/websocket_route.h
+++ src/doodle_lib/core/http/websocket_route.h
@@ -3,10 +3,43 @@
 //
 
 #pragma once
+#include <doodle_core/doodle_core_fwd.h>
 namespace doodle::http {
+class http_websocket_data;
+using http_websocket_data_ptr = std::shared_ptr<http_websocket_data>;
 class websocket_route {
+  using map_actin_type = std::shared_ptr<std::function<void(const http_websocket_data_ptr&, const nlohmann::json&)>>;
+  std::map<std::string, map_actin_type> actions_;
+
+  map_actin_type not_function_;
+  static void not_function(const http_websocket_data_ptr& in_data, const nlohmann::json& in_json);
+
  public:
-  websocket_route()  = default;
+  websocket_route()
+      : not_function_{std::make_shared<std::function<void(const http_websocket_data_ptr&, const nlohmann::json&)>>(
+            &not_function
+        )},
+        actions_{} {}
+
   ~websocket_route() = default;
+
+  // 注册路由
+  websocket_route& reg(const std::string& in_name, const map_actin_type& in_function);
+
+  template <typename T>
+  websocket_route& reg(T&& in_fun) {
+    return reg(
+        T::name, std::make_shared<std::function<void(const http_websocket_data_ptr&, const nlohmann::json&)>>(
+                     [l_fun_ptr = std::make_shared<T>(std::move(in_fun)
+                      )](const http_websocket_data_ptr& in_ptr, const nlohmann::json& in_data) {
+                       auto l_data = in_data;
+                       boost::asio::post(boost::asio::prepend(*l_fun_ptr, in_ptr, std::move(l_data)));
+                     }
+                 )
+    );
+  }
+
+  // 路由分发
+  map_actin_type operator()(const std::string& in_name) const;
 };
 }  // namespace doodle::http
