0c82cbc4ad7b8d2177f9b615cb1e858498ff5cc5
uuiid <957714080@qq.com>
2024-05-27 15:28:41 +0800

    添加登陆测试

--- src/test/kitsu/auth.cpp
+++ src/test/kitsu/auth.cpp
@@ -7,7 +7,15 @@
 #include <boost/test/unit_test_log.hpp>
 using namespace doodle;
 
-BOOST_AUTO_TEST_CASE(kitsu_login) {
+BOOST_AUTO_TEST_SUITE(kitsu)
+constexpr static std::string_view g_token{
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+    "eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxNjc5NDgzNywianRpIjoiMzkxNTFiNjEtNzJkZC00MGE3LTk3MmMtMGJhYWIzMmUwMzY4IiwidHlwZSI6Im"
+    "FjY2VzcyIsInN1YiI6ImU5OWMyNjZhLTk1ZjUtNDJmNS1hYmUxLWI0MTlkMjk4MmFiMCIsIm5iZiI6MTcxNjc5NDgzNywiZXhwIjoxNzY0NjMzNjAw"
+    "LCJpZGVudGl0eV90eXBlIjoiYm90In0.sYmTTWsvMNGLiX57_RnfUI9rL04WJL2XSNZElSL4RNY"
+};
+
+BOOST_AUTO_TEST_CASE(login) {
   doodle_lib l_lib{};
   auto l_c = doodle::kitsu::kitsu_client{"192.168.40.182", "80"};
   l_c.longin("957714080@qq.com", "8jO6sJm5EYAZSuZ7wy3P", [](boost::system::error_code ec, nlohmann::json in_json) {
@@ -18,7 +26,17 @@ BOOST_AUTO_TEST_CASE(kitsu_login) {
   g_io_context().run();
 }
 
-BOOST_AUTO_TEST_CASE(kitsu_get_task) {
+BOOST_AUTO_TEST_CASE(authenticated) {
+  doodle_lib l_lib{};
+  auto l_c = doodle::kitsu::kitsu_client{"192.168.40.182", "80"};
+  l_c.authenticated(std::string{g_token}, [](boost::system::error_code ec, nlohmann::json in_json) {
+    BOOST_TEST(!ec);
+    BOOST_TEST_MESSAGE(in_json.dump());
+  });
+  g_io_context().run();
+}
+
+BOOST_AUTO_TEST_CASE(get_task) {
   doodle_lib l_lib{};
   auto l_c = std::make_shared<doodle::kitsu::kitsu_client>("192.168.40.182", "80");
   l_c->longin("957714080@qq.com", "8jO6sJm5EYAZSuZ7wy3P", [l_c](boost::system::error_code ec, nlohmann::json in_json) {
@@ -33,3 +51,5 @@ BOOST_AUTO_TEST_CASE(kitsu_get_task) {
   });
   g_io_context().run();
 }
+
+BOOST_AUTO_TEST_SUITE_END()
