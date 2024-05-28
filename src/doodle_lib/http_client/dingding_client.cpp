7d83ff7cf3048ec066b098fd6d74d60085256297
uuiid <957714080@qq.com>
2024-05-28 11:48:01 +0800

    添加计时器取消

--- src/doodle_lib/http_client/dingding_client.cpp
+++ src/doodle_lib/http_client/dingding_client.cpp
@@ -1,21 +1,25 @@
 #include "dingding_client.h"
 
+#include <doodle_core/core/app_base.h>
+
 namespace doodle::dingding {
 void client::begin_refresh_token(chrono::seconds in_seconds) {
-
   timer_ptr_->expires_after(in_seconds);
-  timer_ptr_->async_wait([this](boost::system::error_code ec) {
-    if (ec) {
-      http_client_core_ptr_->logger()->log(log_loc(), level::warn, "timer_ptr_ error: {}", ec);
-      return;
-    }
-    access_token(app_key, app_secret, [this](boost::system::error_code ec, nlohmann::json in_json) {
-      if (ec) {
-        http_client_core_ptr_->logger()->log(log_loc(), level::err, "refresh_token failed: {}", ec.message());
-        return;
+  timer_ptr_->async_wait(boost::asio::bind_cancellation_slot(
+      app_base::Get().on_cancel.slot(),
+      [this](boost::system::error_code ec) {
+        if (ec) {
+          http_client_core_ptr_->logger()->log(log_loc(), level::warn, "timer_ptr_ error: {}", ec);
+          return;
+        }
+        access_token(app_key, app_secret, [this](boost::system::error_code ec, nlohmann::json in_json) {
+          if (ec) {
+            http_client_core_ptr_->logger()->log(log_loc(), level::err, "refresh_token failed: {}", ec.message());
+            return;
+          }
+          access_token_ = in_json["accessToken"].get<std::string>();
+        });
       }
-      access_token_ = in_json["accessToken"].get<std::string>();
-    });
-  });
+  ));
 }
 }  // namespace doodle::dingding
