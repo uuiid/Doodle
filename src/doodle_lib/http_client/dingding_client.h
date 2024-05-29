c9cb23930eec20d95a433865eb4f7ed2cd1f812f
uuiid <957714080@qq.com>
2024-05-29 15:37:18 +0800

    添加获取详细考勤列表

--- src/doodle_lib/http_client/dingding_client.h
+++ src/doodle_lib/http_client/dingding_client.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <doodle_core/core/http_client_core.h>
+#include <doodle_core/metadata/time_point_wrap.h>
 
 #include <doodle_lib/core/http/json_body.h>
 #include <doodle_lib/doodle_lib_fwd.h>
-
 namespace doodle::dingding {
 
 class client {
@@ -13,7 +13,8 @@ class client {
   using timer_t               = boost::asio::steady_timer;
   using timer_ptr_t           = std::shared_ptr<timer_t>;
 
-  https_client_core_ptr http_client_core_ptr_{};
+  https_client_core_ptr http_client_core_ptr_{};      // 新版本api
+  https_client_core_ptr http_client_core_ptr_old_{};  // 旧版本api
   timer_ptr_t timer_ptr_{};
 
   std::string access_token_;
@@ -24,8 +25,9 @@ class client {
   void begin_refresh_token(chrono::seconds in_seconds = chrono::seconds(7200));
 
  public:
-  explicit client(boost::asio::ssl::context& in_ctx, std::string in_ip, std::string in_port)
-      : http_client_core_ptr_(std::make_shared<https_client_core>(in_ctx, std::move(in_ip), std::move(in_port))){};
+  explicit client(boost::asio::ssl::context& in_ctx)
+      : http_client_core_ptr_(std::make_shared<https_client_core>(in_ctx, "https://api.dingtalk.com/", "443")),
+        http_client_core_ptr_old_(std::make_shared<https_client_core>(in_ctx, "https://oapi.dingtalk.com/", "443")){};
   ~client() = default;
 
   // 初始化, 必须调用, 否则无法使用, 获取授权后将自动2小时刷新一次
@@ -68,9 +70,93 @@ class client {
             l_com(ec, nlohmann::json{});
             return;
           }
-          auto l_json = nlohmann::json::parse(l_json_str);
+          auto l_json   = nlohmann::json::parse(l_json_str);
+          access_token_ = l_json["accessToken"].get<std::string>();
           if (in_auto_expire) begin_refresh_token(chrono::seconds(l_json["expireIn"].get<int>()));
 
+          l_com(ec, nlohmann::json::parse(l_json_str));
+        }
+    );
+  }
+
+  template <typename CompletionHandler>
+  void get_user_by_mobile(const std::string& in_mobile, CompletionHandler&& in_completion) {
+    boost::beast::http::request<boost::beast::http::string_body> req{
+        boost::beast::http::verb::post, fmt::format("/topapi/v2/user/getbymobile?access_token={}", access_token_), 11
+    };
+    req.body() = nlohmann::json{{"mobile", in_mobile}}.dump();
+    req.set(boost::beast::http::field::content_type, "application/json");
+
+    http_client_core_ptr_old_->async_read<boost::beast::http::response<boost::beast::http::string_body>>(
+        req,
+        [l_com = std::move(in_completion),
+         this](boost::system::error_code ec, boost::beast::http::response<boost::beast::http::string_body> res) {
+          if (ec) {
+            http_client_core_ptr_->logger()->log(log_loc(), level::err, "get_user_by_mobile failed: {}", ec.message());
+            l_com(ec, nlohmann::json{});
+            return;
+          }
+
+          if (res.result() != boost::beast::http::status::ok) {
+            http_client_core_ptr_->logger()->log(log_loc(), level::err, "get_user_by_mobile failed: {}", res.body());
+            ec = boost::system::errc::make_error_code(boost::system::errc::bad_message);
+            l_com(ec, nlohmann::json{});
+            return;
+          }
+
+          auto l_json_str = res.body();
+          if (!nlohmann::json::accept(l_json_str)) {
+            http_client_core_ptr_->logger()->log(log_loc(), level::err, "get_user_by_mobile failed: {}", l_json_str);
+            ec = boost::system::errc::make_error_code(boost::system::errc::bad_message);
+            l_com(ec, nlohmann::json{});
+            return;
+          }
+          l_com(ec, nlohmann::json::parse(l_json_str));
+        }
+    );
+  }
+  template <typename CompletionHandler>
+  void get_attendance_updatedata(
+      const std::string& in_user_id, const time_point_wrap& in_work_date, CompletionHandler&& in_completion
+  ) {
+    boost::beast::http::request<boost::beast::http::string_body> req{
+        boost::beast::http::verb::post, fmt::format("/attendance/updatedata?access_token={}", access_token_), 11
+    };
+    req.body() = nlohmann::json{
+        {"userid", in_user_id}, {"work_date", fmt::format("{:%Y-%m-%d}", in_work_date.get_local_time())}
+    }.dump();
+    req.set(boost::beast::http::field::content_type, "application/json");
+
+    http_client_core_ptr_->async_read<boost::beast::http::response<boost::beast::http::string_body>>(
+        req,
+        [l_com = std::move(in_completion),
+         this](boost::system::error_code ec, boost::beast::http::response<boost::beast::http::string_body> res) {
+          if (ec) {
+            http_client_core_ptr_->logger()->log(
+                log_loc(), level::err, "get_attendance_updatedata failed: {}", ec.message()
+            );
+            l_com(ec, nlohmann::json{});
+            return;
+          }
+
+          if (res.result() != boost::beast::http::status::ok) {
+            http_client_core_ptr_->logger()->log(
+                log_loc(), level::err, "get_attendance_updatedata failed: {}", res.body()
+            );
+            ec = boost::system::errc::make_error_code(boost::system::errc::bad_message);
+            l_com(ec, nlohmann::json{});
+            return;
+          }
+
+          auto l_json_str = res.body();
+          if (!nlohmann::json::accept(l_json_str)) {
+            http_client_core_ptr_->logger()->log(
+                log_loc(), level::err, "get_attendance_updatedata failed: {}", l_json_str
+            );
+            ec = boost::system::errc::make_error_code(boost::system::errc::bad_message);
+            l_com(ec, nlohmann::json{});
+            return;
+          }
           l_com(ec, nlohmann::json::parse(l_json_str));
         }
     );
--- src/test/http_client/dingding.cpp
+++ src/test/http_client/dingding.cpp
@@ -16,12 +16,40 @@ BOOST_AUTO_TEST_CASE(access_token) {
   std::string l_app_key    = l_env["DINGDING_APP_KEY"].to_string();
   std::string l_app_secret = l_env["DINGDING_APP_SECRET"].to_string();
 
-  auto l_c                 = std::make_shared<doodle::dingding::client>(l_ctx, "https://api.dingtalk.com/", "443");
+  auto l_c                 = std::make_shared<doodle::dingding::client>(l_ctx );
 
   l_c->access_token(l_app_key, l_app_secret, false, [](auto ec, auto json) {
     BOOST_TEST(!ec);
     BOOST_TEST(json.contains("accessToken"));
     BOOST_TEST(json.contains("expireIn"));
+  });
+  g_io_context().run();
+}
+
+
+BOOST_AUTO_TEST_CASE(get_user_by_mobile) {
+  app_base l_app_base{};
+  boost::asio::ssl::context l_ctx{boost::asio::ssl::context::tlsv12_client};
+
+  auto l_env               = boost::this_process::environment();
+  std::string l_app_key    = l_env["DINGDING_APP_KEY"].to_string();
+  std::string l_app_secret = l_env["DINGDING_APP_SECRET"].to_string();
+  std::string l_mobile     = l_env["DINGDING_MOBILE"].to_string();
+
+  auto l_c                 = std::make_shared<doodle::dingding::client>(l_ctx );
+
+  l_c->access_token(l_app_key, l_app_secret, false, [l_c, l_mobile](auto ec, auto json) {
+    BOOST_TEST(!ec);
+    BOOST_TEST(json.contains("accessToken"));
+    BOOST_TEST(json.contains("expireIn"));
+
+    l_c->get_user_by_mobile(l_mobile,[l_c](auto ec, auto json) {
+      BOOST_TEST(!ec);
+      default_logger_raw()->info("json: {}", json.dump());
+      BOOST_TEST(json.contains("result"));
+      BOOST_TEST(json["result"].contains("userid"));
+      default_logger_raw()->info("userid: {}", json["result"]["userid"].get<std::string>());
+    });
   });
   g_io_context().run();
 }
