5dc6471fd907931fa90adddd8e5573dc914f548a
uuiid <957714080@qq.com>
2024-05-31 13:29:29 +0800

    添加std时钟计算

--- src/doodle_core/time_tool/work_clock.cpp
+++ src/doodle_core/time_tool/work_clock.cpp
@@ -149,4 +149,131 @@ work_clock::time_type work_clock::next_point(const work_clock::time_type& in_poi
 
   return {};
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+work_clock2::work_clock2() = default;
+
+void work_clock2::cut_interval(const time_type& in_min, const time_type& in_max) {
+  auto l_d = discrete_interval_time::closed(in_min, in_max);
+  interval_set_time_ &= l_d;
+  interval_map_time_ &= l_d;
+}
+
+work_clock2::duration_type work_clock2::operator()(const time_type& in_min, const time_type& in_max) const {
+  auto l_d = discrete_interval_time::closed(in_min, in_max);
+  auto l_l = interval_set_time_ & l_d;
+  duration_type l_len{};
+  for (const auto& l_i : l_l) {
+    l_len += (l_i.upper() - l_i.lower());
+    // l_len += (boost::icl::upper(l_i).get_sys_time() - boost::icl::lower(l_i).get_sys_time());
+    // DOODLE_LOG_INFO("{} - {} = {}", l_i.upper(), l_i.lower(), chrono::round<chrono::seconds>(l_len));
+  }
+  return l_len;
+}
+
+work_clock2::time_type work_clock2::next_time(const time_type& in_begin, const duration_type& in_du) const {
+  auto l_d = discrete_interval_time::right_open(in_begin, time_type::max());
+  auto l_l = interval_set_time_ & l_d;
+  duration_type l_len{};
+  for (auto&& l_i : l_l) {
+    auto l_en_t = boost::icl::upper(l_i) - boost::icl::lower(l_i);
+    if ((l_en_t + l_len) >= in_du) {
+      return boost::icl::first(l_i) + doodle::chrono::round<doodle::chrono::seconds>(in_du - l_len);
+    } else {
+      l_len += l_en_t;
+    }
+  }
+
+  return {};
+}
+
+std::vector<std::pair<work_clock2::time_type, work_clock2::time_type>> work_clock2::get_work_du(
+    const time_type& in_min, const time_type& in_max
+) const {
+  std::vector<std::pair<time_type, time_type>> l_r{};
+  auto l_d = discrete_interval_time::closed(in_min, in_max);
+  auto l_l = interval_set_time_ & l_d;
+  for (auto&& l_i : l_l) {
+    l_r.emplace_back(boost::icl::first(l_i), boost::icl::last(l_i));
+  }
+  return l_r;
+}
+
+std::string work_clock2::debug_print() const {
+  return fmt::format("时间段 {}  时间信息 {}", interval_set_time_, interval_map_time_);
+
+  //  for (auto&& i : interval_map_time_) {
+  //    fmt::format("{} ", i.second);
+  //    auto l_item = i.first;
+  //    fmt::format("{} ", i.first);
+  //  }
+  //
+  //  return fmt::format("{}", interval_map_time_);
+}
+std::optional<std::string> work_clock2::get_time_info(const time_type& in_min, const time_type& in_max) const {
+  auto l_d = discrete_interval_time::closed(in_min, in_max);
+  // auto l_item                          = interval_map_time_ & l_d;
+  std::string l_r{};
+  for (auto&& i : interval_map_time_) {
+    auto l_t1 = i.first & l_d;
+    if (!boost::icl::is_empty(l_t1))
+      l_r += fmt::format(
+          R"("{:%Y-%m-%d %H:%M} 到 {:%Y-%m-%d %H:%M}  信息 {}")", (++boost::icl::lower(i.first)),
+          boost::icl::upper(i.first), fmt::join(i.second, " ")
+      );
+  }
+
+  return l_r.empty() ? std::optional<std::string>{} : std::optional{l_r};
+}
+
+work_clock2& work_clock2::operator+=(const std::tuple<time_type, time_type>& in_time) {
+  auto&& [l_time_1, l_time_2] = in_time;
+  auto l_dis                  = discrete_interval_time::closed(l_time_1, l_time_2);
+  interval_set_time_ += l_dis;
+  return *this;
+}
+
+work_clock2& work_clock2::operator-=(const std::tuple<time_type, time_type>& in_time) {
+  auto&& [l_time_1, l_time_2] = in_time;
+  auto l_dis                  = discrete_interval_time::closed(l_time_1, l_time_2);
+  interval_set_time_ -= l_dis;
+
+  return *this;
+}
+
+work_clock2& work_clock2::operator+=(const std::tuple<time_type, time_type, std::string>& in_time) {
+  auto&& [l_time_1, l_time_2, l_info] = in_time;
+  auto l_dis                          = discrete_interval_time::closed(l_time_1, l_time_2);
+  interval_set_time_ += l_dis;
+  interval_map_time_ += std::make_pair(discrete_interval_time::right_open(l_time_1, l_time_2), info_type{l_info});
+  return *this;
+}
+
+work_clock2& work_clock2::operator-=(const std::tuple<time_type, time_type, std::string>& in_time) {
+  auto&& [l_time_1, l_time_2, l_info] = in_time;
+  auto l_dis                          = discrete_interval_time::closed(l_time_1, l_time_2);
+  // DOODLE_LOG_INFO("减去时间 {} -> {} {}", l_time_1, l_time_2, l_info);
+  // DOODLE_LOG_INFO("减去时间2 {} ", l_dis);
+  interval_set_time_ -= l_dis;
+  interval_map_time_ += std::make_pair(discrete_interval_time::right_open(l_time_1, l_time_2), info_type{l_info});
+  // DOODLE_LOG_INFO("时间段 {}", interval_set_time_);
+  return *this;
+}
+void work_clock2::add_info(const std::tuple<time_type, time_type, std::string>& in_time) {
+  auto&& [l_time_1, l_time_2, l_info] = in_time;
+  auto l_dis                          = discrete_interval_time::closed(l_time_1, l_time_2);
+  interval_map_time_ += std::make_pair(discrete_interval_time::right_open(l_time_1, l_time_2), info_type{l_info});
+}
+
+work_clock2::time_type work_clock2::next_point(const work_clock2::time_type& in_point) {
+  auto l_d = discrete_interval_time::right_open(in_point, time_type::max());
+  auto l_l = interval_set_time_ & l_d;
+  for (auto&& l_i : l_l) {
+    return boost::icl::lower(l_i);
+  }
+
+  return {};
+}
+
 }  // namespace doodle::business
--- src/doodle_core/time_tool/work_clock.h
+++ src/doodle_core/time_tool/work_clock.h
@@ -99,4 +99,86 @@ class DOODLE_CORE_API work_clock {
   work_clock& operator-=(const std::tuple<time_point_wrap, time_point_wrap, std::string>& in_time);
   std::string debug_print() const;
 };
+
+class DOODLE_CORE_API work_clock2 {
+  using time_type              = chrono::system_clock::time_point;
+  using duration_type          = time_type::duration;
+  using info_type              = std::set<std::string>;
+  using discrete_interval_time = boost::icl::discrete_interval<time_type>;
+  using interval_set_time      = boost::icl::interval_set<time_type>;
+  using interval_map_time      = boost::icl::interval_map<time_type, info_type>;
+
+  interval_set_time interval_set_time_;
+  interval_map_time interval_map_time_;
+
+ public:
+  work_clock2();
+
+  /**
+   * @brief 设置工作时间时钟的开始和结束(缓存)
+   * @param in_min
+   * @param in_max
+   */
+  void cut_interval(const time_type& in_min, const time_type& in_max);
+
+  /**
+   * @brief 获取两个时间点点工作时间(按照规则获取)
+   * @param in_min 开始时间
+   * @param in_max 结束时间
+   * @return 工作时间
+   */
+  duration_type operator()(const time_type& in_min, const time_type& in_max) const;
+
+  /**
+   * @brief 根据传入的开始时间和工作时间段获取下一个时间点
+   * @param in_begin 开始时间
+   * @param in_du 时间段
+   * @return 下一个时间点
+   */
+  time_type next_time(const time_type& in_begin, const duration_type& in_du) const;
+
+  /**
+   * @brief 获取两个时间点之间点时间分段( 休息时间段 -> 工作时间段)
+   * @param in_min 开始时间
+   * @param in_max 结束时间
+   * @return 时间段
+   */
+  [[nodiscard("")]] std::vector<std::pair<time_type, time_type>> get_work_du(
+      const time_type& in_min, const time_type& in_max
+  ) const;
+
+  /**
+   * @brief 获取当前点所在时间段段的备注
+   * @param in_min 开始时间
+   * @param in_max 结束时间
+   * @return 可选段备注
+   */
+  std::optional<std::string> get_time_info(const time_type& in_min, const time_type& in_max) const;
+  /**
+   * 这个是添加额外的信息, 并不会加入到计算时间中, 只会添加一个额外的辅助信息时间
+   * @param in_time
+   */
+  void add_info(const std::tuple<time_type, time_type, std::string>& in_time);
+
+  /**
+   * @brief 寻找下一段工作开始的时间点
+   *
+   * @param in_point
+   * @return time_type
+   */
+  time_type next_point(const time_type& in_point);
+
+  // /**
+  //  * @brief 这个绝对扣除是扣除的每天 12:00 到13:00 还有 6:00 到 6:30
+  //  *
+  //  */
+  // void absolute_deduction();
+
+  work_clock2& operator+=(const std::tuple<time_type, time_type>& in_time);
+  work_clock2& operator+=(const std::tuple<time_type, time_type, std::string>& in_time);
+  work_clock2& operator-=(const std::tuple<time_type, time_type>& in_time);
+  work_clock2& operator-=(const std::tuple<time_type, time_type, std::string>& in_time);
+  std::string debug_print() const;
+};
+
 }  // namespace doodle::business
