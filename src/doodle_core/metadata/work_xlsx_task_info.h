1f185eaf1dac3ae684b0fc04299526e4d698ab36
uuiid <957714080@qq.com>
2024-06-03 14:36:46 +0800

    添加块保存

--- src/doodle_core/metadata/work_xlsx_task_info.h
+++ src/doodle_core/metadata/work_xlsx_task_info.h
@@ -4,8 +4,6 @@
 namespace doodle {
 class work_xlsx_task_info {
  public:
-  struct data_info {};
-
   work_xlsx_task_info()  = default;
   ~work_xlsx_task_info() = default;
   boost::uuids::uuid id_;
@@ -13,7 +11,7 @@ class work_xlsx_task_info {
   chrono::sys_time_pos start_time_;
   chrono::sys_time_pos end_time_;
   chrono::system_clock::duration duration_;
-  
+
   entt::entity user_refs_;
   std::size_t sort_id_{};
 
@@ -33,4 +31,33 @@ class work_xlsx_task_info {
   static void update(pooled_connection& in_comm, const std::vector<work_xlsx_task_info>& in_task);
   static void delete_by_ids(pooled_connection& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
 };
+
+class work_xlsx_task_info_block {
+ public:
+  work_xlsx_task_info_block()  = default;
+  ~work_xlsx_task_info_block() = default;
+
+  std::vector<work_xlsx_task_info> task_info_;
+  boost::uuids::uuid id_;
+  chrono::year_month year_month_;
+  entt::entity user_refs_;
+  chrono::system_clock::duration duration_;
+
+
+  // 传入user的uuid 和 实体对应数据
+  static std::vector<work_xlsx_task_info_block> select_all(
+      pooled_connection& in_comm, const std::map<boost::uuids::uuid, entt::entity>& in_map_id
+  );
+  static void create_table(pooled_connection& in_comm);
+
+  // 过滤已经存在的任务
+  static std::vector<bool> filter_exist(pooled_connection& in_comm, const std::vector<work_xlsx_task_info_block>& in_task);
+  static void insert(
+      pooled_connection& in_comm, const std::vector<work_xlsx_task_info_block>& in_task,
+      const std::map<entt::entity, boost::uuids::uuid>& in_map_id
+  );
+  static void update(pooled_connection& in_comm, const std::vector<work_xlsx_task_info_block>& in_task);
+  static void delete_by_ids(pooled_connection& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
+};
+
 }  // namespace doodle
