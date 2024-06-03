4d8bf3821b9f8f85cb01ee3eae5e93ae29905135
uuiid <957714080@qq.com>
2024-06-03 13:33:26 +0800

    添加time测试

--- src/test/core/time.cpp
+++ src/test/core/time.cpp
@@ -35,9 +35,12 @@ BOOST_AUTO_TEST_CASE(time_warp_fmt_test) {
   time_point_wrap l_time{2022, 5, 7, 11, 46, 55};
   chrono::sys_time_pos l_time_pos{l_time.get_sys_time()};
   chrono::zoned_time l_zoned_time{chrono::current_zone(), l_time_pos};
+  chrono::zoned_time l_zoned_time2{chrono::current_zone(), chrono::system_clock::now()};
+
   BOOST_TEST_MESSAGE(fmt::format("{}", l_time));
   BOOST_TEST_MESSAGE(fmt::format("{}", l_time_pos));
   BOOST_TEST_MESSAGE(fmt::format("{}", l_zoned_time.get_local_time()));
+  BOOST_TEST_MESSAGE(fmt::format("{}", l_zoned_time2.get_local_time()));
   BOOST_TEST(fmt::format("{}", l_time) != "2022-05-07 11:46:55"s);
   BOOST_TEST(fmt::format("{}", l_time) != "2022-05-07 19:46:55"s);
 
