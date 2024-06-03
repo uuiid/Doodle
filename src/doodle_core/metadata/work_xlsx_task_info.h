01055e6a55119b878963054f94bb568639ab833d
uuiid <957714080@qq.com>
2024-06-03 14:37:43 +0800

    去除旧函数

--- src/doodle_core/metadata/work_xlsx_task_info.h
+++ src/doodle_core/metadata/work_xlsx_task_info.h
@@ -12,24 +12,7 @@ class work_xlsx_task_info {
   chrono::sys_time_pos end_time_;
   chrono::system_clock::duration duration_;
 
-  entt::entity user_refs_;
-  std::size_t sort_id_{};
-
   boost::uuids::uuid kitsu_task_ref_id_;
-  // 传入user的uuid 和 实体对应数据
-  static std::vector<work_xlsx_task_info> select_all(
-      pooled_connection& in_comm, const std::map<boost::uuids::uuid, entt::entity>& in_map_id
-  );
-  static void create_table(pooled_connection& in_comm);
-
-  // 过滤已经存在的任务
-  static std::vector<bool> filter_exist(pooled_connection& in_comm, const std::vector<work_xlsx_task_info>& in_task);
-  static void insert(
-      pooled_connection& in_comm, const std::vector<work_xlsx_task_info>& in_task,
-      const std::map<entt::entity, boost::uuids::uuid>& in_map_id
-  );
-  static void update(pooled_connection& in_comm, const std::vector<work_xlsx_task_info>& in_task);
-  static void delete_by_ids(pooled_connection& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
 };
 
 class work_xlsx_task_info_block {
