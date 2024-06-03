89497d092d25884dcb54812d89ea88d43687729c
uuiid <957714080@qq.com>
2024-06-03 15:06:20 +0800

    调整类转换

--- src/doodle_lib/http_method/computing_time.cpp
+++ src/doodle_lib/http_method/computing_time.cpp
@@ -7,6 +7,34 @@
 #include "doodle_lib/core/http/http_websocket_data.h"
 namespace doodle::http {
 
+struct computing_time_data {
+  struct task_data {
+    boost::uuids::uuid task_id;
+    std::chrono::system_clock::time_point start_time;
+    std::chrono::system_clock::time_point end_time;
+    // form json
+    friend void from_json(const nlohmann::json& j, task_data& p) {
+      p.task_id    = j.at("task_id").get<boost::uuids::uuid>();
+      p.start_time = parse_8601<std::chrono::system_clock::time_point>(j.at("start_time").get<std::string>());
+      p.end_time   = parse_8601<std::chrono::system_clock::time_point>(j.at("end_time").get<std::string>());
+    }
+  };
+  chrono::year_month year_month_;
+  boost::uuids::uuid user_id;
+  std::vector<task_data> data;
+
+  // form json
+  friend void from_json(const nlohmann::json& j, computing_time_data& p) {
+    std::istringstream l_year_month_stream(j.at("year_month").get<std::string>());
+    l_year_month_stream >> chrono::parse("%Y-%m", p.year_month_);
+    if (!l_year_month_stream) {
+      throw nlohmann::json::parse_error::create(101, 0, "year_month 格式错误不是时间格式", j.at("year_month"));
+    }
+    p.user_id = j.at("user_id").get<boost::uuids::uuid>();
+    p.data    = j.at("data").get<std::vector<task_data>>();
+  }
+};
+
 class computing_time_post {
  public:
   computing_time_post()  = default;
@@ -26,63 +54,24 @@ class computing_time_post {
     if (!nlohmann::json::accept(l_str)) {
       l_logger->log(log_loc(), level::err, "json parse error: {}", l_str);
       in_error_code = boost::system::error_code{boost::system::errc::bad_message, boost::system::system_category()};
-      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "错误的json格式");
+      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "不是json字符串");
       return;
     }
     auto l_json = nlohmann::json::parse(l_str);
-    if (!l_json.contains("user_id") || !l_json.contains("time")) {
-      l_logger->log(log_loc(), level::err, "json parse error: {}", l_str);
+
+    computing_time_data l_data{};
+    try {
+      l_data = l_json.get<computing_time_data>();
+    } catch (const nlohmann::json::exception& e) {
+      l_logger->log(log_loc(), level::err, "json parse error: {}", e.what());
       in_error_code = boost::system::error_code{boost::system::errc::bad_message, boost::system::system_category()};
-      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "缺失必须键 time 或 user_id");
+      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, e.what());
       return;
-    }
-    auto& l_time = l_json["time"];
-    if (!l_time.contains("start_time") || !l_time.contains("end_time")) {
-      l_logger->log(log_loc(), level::err, "json parse error: {}", l_str);
+    } catch (const std::exception& e) {
+      l_logger->log(log_loc(), level::err, "json parse error: {}", e.what());
       in_error_code = boost::system::error_code{boost::system::errc::bad_message, boost::system::system_category()};
-      in_handle->seed_error(
-          boost::beast::http::status::bad_request, in_error_code, "缺失必须键 time.start_time 或 time.end_time"
-      );
+      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, e.what());
       return;
-    }
-    if (!l_json.contains("data")) {
-      l_logger->log(log_loc(), level::err, "json parse error: {}", l_str);
-      in_error_code = boost::system::error_code{boost::system::errc::bad_message, boost::system::system_category()};
-      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "缺失必须键 data");
-      return;
-    }
-
-    auto l_user_id = l_json["user_id"].get<std::string>();
-    std::istringstream l_uuid_stream(l_user_id);
-    boost::uuids::uuid l_uuid;
-    l_uuid_stream >> l_uuid;
-    if (!l_uuid_stream) {
-      l_logger->log(log_loc(), level::err, "json parse error: {}", l_str);
-      in_error_code = boost::system::error_code{boost::system::errc::bad_message, boost::system::system_category()};
-      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "user_id 格式错误不是uuid格式");
-      return;
-    }
-    auto l_start_time_str = l_time["start_time"].get<std::string>();
-    auto l_end_time_str   = l_time["end_time"].get<std::string>();
-
-    auto l_start_time     = parse_8601<std::chrono::system_clock::time_point>(l_end_time_str, in_error_code);
-    if (in_error_code) {
-      l_logger->log(log_loc(), level::err, "time parse error: {}", l_str);
-      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "end_time 格式错误不是时间格式");
-      return;
-    }
-    auto l_end_time = parse_8601<std::chrono::system_clock::time_point>(l_start_time_str, in_error_code);
-
-    if (!in_error_code) {
-      l_logger->log(log_loc(), level::err, "time parse error: {}", l_str);
-      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, "start_time 格式错误不是时间格式");
-      return;
-    }
-    
-    for (auto l_task : l_json["data"]) {
-      
-
-
     }
   }
 };
