5f1b2f0c0b1fff61716716930e5b8228374ca6a9
uuiid <957714080@qq.com>
2024-06-04 16:05:01 +0800

    添加保存

---
+++ src/doodle_lib/http_method/sqlite/kitsu_backend_sqlite.cpp
@@ -0,0 +1,176 @@
+#include "kitsu_backend_sqlite.h"
+
+#include <boost/asio.hpp>
+//
+#include <doodle_core/metadata/attendance.h>
+#include <doodle_core/metadata/user.h>
+#include <doodle_core/metadata/work_xlsx_task_info.h>
+namespace doodle::http {
+
+class kitsu_backend_sqlite::kitsu_backend_sqlite_fun {
+ public:
+  using executor_type = boost::asio::any_io_executor;
+  boost::asio::any_io_executor executor_;
+  boost::asio::any_io_executor get_executor() const { return executor_; }
+
+  template <typename T>
+  struct sqlite_save_data {
+    std::vector<T> save_list_{};
+    std::vector<boost::uuids::uuid> destroy_list_{};
+    std::map<entt::entity, boost::uuids::uuid> map_id_{}; // 用于关联
+
+    void operator()(pooled_connection& in_db) const {
+      auto l_indexs = T::filter_exist(in_db, save_list_);
+      std::vector<T> l_insert_list{};
+      std::vector<T> l_update_list{};
+      for (auto i = 0; i < save_list_.size(); ++i) {
+        if (l_indexs[i]) {
+          l_update_list.emplace_back(save_list_[i]);
+        } else {
+          l_insert_list.emplace_back(save_list_[i]);
+        }
+      }
+      T::insert(in_db, l_insert_list);
+      T::update(in_db, l_update_list);
+      T::delete_by_ids(in_db, destroy_list_);
+    }
+
+    void get_data(kitsu_backend_sqlite::observer_data<T>& in_data) {
+      auto [l_save, l_del] = in_data.get_data();
+      // ranges::v;
+      for (auto i = 0; i < l_save.size(); ++i) {
+        save_list_.emplace_back(g_reg()->get<T>(l_save[i]));
+      }
+      destroy_list_ = l_del;
+      in_data.clear();
+    }
+  };
+
+  template <>
+  struct sqlite_save_data<user> {
+    std::vector<user> save_list_{};
+    std::vector<boost::uuids::uuid> destroy_list_{};
+    std::map<entt::entity, boost::uuids::uuid> self_map_id_{}; // 用于关联
+
+
+    void operator()(pooled_connection& in_db) const {
+      auto l_indexs = user::filter_exist(in_db, save_list_);
+      std::vector<user> l_insert_list{};
+      std::vector<user> l_update_list{};
+      for (auto i = 0; i < save_list_.size(); ++i) {
+        if (l_indexs[i]) {
+          l_update_list.emplace_back(save_list_[i]);
+        } else {
+          l_insert_list.emplace_back(save_list_[i]);
+        }
+      }
+      user::insert(in_db, l_insert_list);
+      user::update(in_db, l_update_list);
+      user::delete_by_ids(in_db, destroy_list_);
+    }
+
+    void get_data(kitsu_backend_sqlite::observer_data<user>& in_data) {
+      auto [l_save, l_del] = in_data.get_data();
+      // ranges::v;
+      for (auto i = 0; i < l_save.size(); ++i) {
+        save_list_.emplace_back(g_reg()->get<user>(l_save[i]));
+        self_map_id_[l_save[i]] = save_list_.back().id_;
+      }
+      destroy_list_ = l_del;
+      in_data.clear();
+    }
+  };
+
+  std::shared_ptr<sqlite_save_data<user>> save_user_{};
+  std::shared_ptr<sqlite_save_data<work_xlsx_task_info_block>> save_work_xlsx_task_info_block_{};
+
+  kitsu_backend_sqlite_fun(
+
+  )
+      : executor_(g_thread().get_executor()),
+
+        save_user_(std::make_shared<sqlite_save_data<user>>()),
+        save_work_xlsx_task_info_block_(std::make_shared<sqlite_save_data<work_xlsx_task_info_block>>()) {}
+
+  void get_data(kitsu_backend_sqlite& in_data) {
+    save_user_->get_data(std::get<observer_data<user>>(in_data.observer_data_));
+    save_work_xlsx_task_info_block_->get_data(std::get<observer_data<work_xlsx_task_info_block>>(in_data.observer_data_)
+    );
+    save_work_xlsx_task_info_block_->map_id_ = save_user_->self_map_id_;
+  }
+
+  void operator()() const {
+    auto l_db_conn = g_pool_db().get_connection();
+    auto l_tx      = sqlpp::start_transaction(l_db_conn);
+    (*save_user_)(l_db_conn);
+    (*save_work_xlsx_task_info_block_)(l_db_conn);
+    l_tx.commit();
+  }
+};
+
+void kitsu_backend_sqlite::init(pooled_connection& in_conn) {
+  // 先创建user表, 以便其他外键关联
+  user::create_table(in_conn);
+  work_xlsx_task_info_block::create_table(in_conn);
+  attendance::create_table(in_conn);
+
+  std::map<boost::uuids::uuid, entt::entity> l_map_id{};
+  {  // 先创建user, 其他使用user做关联选择
+    auto l_user = user::select_all(in_conn);
+    std::vector<entt::entity> l_entities{l_user.size()};
+    g_reg()->create(l_entities.begin(), l_entities.end());
+    g_reg()->insert<user>(l_entities.begin(), l_entities.end(), l_user.begin());
+
+    for (auto i = 0; i < l_user.size(); ++i) {
+      l_map_id[l_user[i].id_] = l_entities[i];
+    }
+  }
+
+  // 再选择work_xlsx_task_info_block
+  {
+    auto l_tasks = work_xlsx_task_info_block::select_all(in_conn, l_map_id);
+    std::vector<entt::entity> l_entities{l_tasks.size()};
+    g_reg()->create(l_entities.begin(), l_entities.end());
+    g_reg()->insert<work_xlsx_task_info_block>(l_entities.begin(), l_entities.end(), l_tasks.begin());
+  }
+
+  // todo: 最后选择调休
+
+  connect(*g_reg());
+}
+
+void kitsu_backend_sqlite::connect(entt::registry& in_registry_ptr) {
+  std::apply([&in_registry_ptr](auto&... in_data) { (in_data.connect(in_registry_ptr), ...); }, observer_data_);
+}
+void kitsu_backend_sqlite::disconnect() {
+  std::apply([](auto&... in_data) { (in_data.disconnect(), ...); }, observer_data_);
+}
+
+void kitsu_backend_sqlite::clear() {
+  std::apply([](auto&... in_data) { (in_data.clear(), ...); }, observer_data_);
+}
+
+void kitsu_backend_sqlite::run() {
+  timer_ptr_  = std::make_shared<timer_t>(g_io_context());
+  logger_ptr_ = g_logger_ctrl().make_log("kitsu_backend_sqlite");
+  connect(*g_reg());
+  begin_save();
+}
+void kitsu_backend_sqlite::begin_save() {
+  timer_ptr_->expires_after(std::chrono::seconds(1));
+  timer_ptr_->async_wait([this](const boost::system::error_code& in_ec) {
+    if (in_ec) {
+      logger_ptr_->log(log_loc(), level::warn, "begin_save error:{}", in_ec.message());
+      return;
+    }
+    save();
+    begin_save();
+  });
+}
+
+void kitsu_backend_sqlite::save() {
+  kitsu_backend_sqlite_fun l_save{};
+  l_save.get_data(*this);
+  boost::asio::post(std::move(l_save));
+}
+}  // namespace doodle::http
---
+++ src/doodle_lib/http_method/sqlite/kitsu_backend_sqlite.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <doodle_lib/doodle_lib_fwd.h>
+
+#include <entt/entt.hpp>
+
+namespace doodle {
+class user;
+class work_xlsx_task_info_block;
+
+}  // namespace doodle
+
+namespace doodle::http {
+class kitsu_backend_sqlite : public boost::noncopyable {
+  using timer_t     = boost::asio::steady_timer;
+  using timer_ptr_t = std::shared_ptr<timer_t>;
+  timer_ptr_t timer_ptr_{};
+  
+  class kitsu_backend_sqlite_fun;
+
+  template <typename T>
+  struct observer_data {
+    entt::observer obs_update_;
+    entt::observer obs_create_;
+    std::vector<boost::uuids::uuid> destroy_ids_{};
+    entt::connection conn_{};
+
+    ~observer_data() { disconnect(); }
+
+    void on_destroy(entt::registry& in_reg, entt::entity in_entt) {
+      destroy_ids_.emplace_back(in_reg.get<T>(in_entt).id_);
+    }
+    void connect(entt::registry& in_reg) {
+      obs_update_.connect(in_reg, entt::collector.update<T>());
+      obs_create_.connect(in_reg, entt::collector.group<T>());
+      conn_ = in_reg.on_destroy<T>().connect<&observer_data::on_destroy>(*this);
+    }
+
+    void disconnect() {
+      obs_update_.disconnect();
+      obs_create_.disconnect();
+      if (conn_) conn_.release();
+    }
+
+    void clear() {
+      obs_update_.clear();
+      obs_create_.clear();
+      destroy_ids_.clear();
+    }
+
+    std::tuple<std::vector<entt::entity>, std::vector<boost::uuids::uuid>> get_data() const {
+      std::vector<entt::entity> l_data{};
+      for (const auto& entity : obs_update_) {
+        l_data.push_back(entity);
+      }
+      for (const auto& entity : obs_create_) {
+        l_data.push_back(entity);
+      }
+      l_data |= ranges::actions::unique;
+      auto l_destroy_ids = destroy_ids_;
+      l_destroy_ids |= ranges::actions::unique;
+      return {std::move(l_data), l_destroy_ids};
+    }
+  };
+
+  std::tuple<observer_data<user>, observer_data<work_xlsx_task_info_block>> observer_data_{};
+
+  logger_ptr logger_ptr_{};
+
+  void connect(entt::registry& in_registry_ptr);
+  void disconnect();
+  void clear();
+
+  void begin_save();
+
+  void save();
+
+ public:
+  void init(pooled_connection& in_conn);
+  void run();
+};
+}  // namespace doodle::http
