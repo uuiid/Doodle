9dae3b8e965459f912b90b7a91c3462a0597788b
uuiid <957714080@qq.com>
2024-06-06 10:31:06 +0800

    添加删除方法和返回方法

--- src/doodle_lib/http_method/computing_time.cpp
+++ src/doodle_lib/http_method/computing_time.cpp
@@ -183,8 +183,7 @@ class computing_time_post_impl : public std::enable_shared_from_this<computing_t
 
   // 计算时间
   void computing_time_run() {
-    auto l_end_time =
-        chrono::local_days{(year_month_ + chrono::months{1}) / chrono::day{1}} - chrono::seconds{1};
+    auto l_end_time  = chrono::local_days{(year_month_ + chrono::months{1}) / chrono::day{1}} - chrono::seconds{1};
     auto l_all_works = time_clock_(chrono::local_days{year_month_ / chrono::day{1}}, l_end_time);
 
 #ifndef NDEBUG
@@ -271,10 +270,12 @@ class computing_time_post_impl : public std::enable_shared_from_this<computing_t
     boost::asio::post(
         g_io_context(), boost::beast::bind_front_handler(&computing_time_post_impl::create_block, shared_from_this())
     );
-    nlohmann::json l_json;
-    l_json      = block_.task_info_;
+    nlohmann::json l_json{};
+    l_json["data"]     = block_.task_info_;
+    l_json["id"]       = block_.id_;
+    l_json["duration"] = block_.duration_.count();
 
-    auto& l_req = session_data_->get_msg_body_parser<boost::beast::http::string_body>()->request_parser_->get();
+    auto& l_req        = session_data_->get_msg_body_parser<boost::beast::http::string_body>()->request_parser_->get();
     boost::beast::http::response<boost::beast::http::string_body> l_response{
         boost::beast::http::status::ok, l_req.version()
     };
@@ -357,9 +358,11 @@ class computing_time_post_impl : public std::enable_shared_from_this<computing_t
     }
 
     nlohmann::json l_json;
-    l_json      = l_block.task_info_;
+    l_json["data"]     = block_.task_info_;
+    l_json["id"]       = block_.id_;
+    l_json["duration"] = block_.duration_.count();
 
-    auto& l_req = session_data_->get_msg_body_parser<boost::beast::http::string_body>()->request_parser_->get();
+    auto& l_req        = session_data_->get_msg_body_parser<boost::beast::http::string_body>()->request_parser_->get();
     boost::beast::http::response<boost::beast::http::string_body> l_response{
         boost::beast::http::status::ok, l_req.version()
     };
@@ -487,8 +490,10 @@ class computing_time_get {
 
     for (auto&& [e, l_u] : l_user.each()) {
       if (l_u.id_ == l_user_id && l_u.task_block_.contains(l_year_month)) {
-        auto l_block = std::as_const(*g_reg()).get<work_xlsx_task_info_block>(l_u.task_block_.at(l_year_month));
-        l_json       = l_block.task_info_;
+        auto l_block       = std::as_const(*g_reg()).get<work_xlsx_task_info_block>(l_u.task_block_.at(l_year_month));
+        l_json["data"]     = l_block.task_info_;
+        l_json["id"]       = l_block.id_;
+        l_json["duration"] = l_block.duration_.count();
         l_response.result(boost::beast::http::status::ok);
         l_response.body() = l_json.dump();
         l_response.prepare_payload();
@@ -556,6 +561,62 @@ class computing_time_patch {
   }
 };
 
+class computing_time_patch_delete {
+ public:
+  computing_time_patch_delete() : executor_(g_thread().get_executor()) {}
+  ~computing_time_patch_delete() = default;
+  using executor_type            = boost::asio::any_io_executor;
+  boost::asio::any_io_executor executor_;
+  boost::asio::any_io_executor get_executor() const { return executor_; }
+  void operator()(boost::system::error_code in_error_code, const http_session_data_ptr& in_handle) const {
+    auto l_logger = in_handle->logger_;
+    if (in_error_code) {
+      l_logger->log(log_loc(), level::err, "error: {}", in_error_code.message());
+      in_handle->seed_error(boost::beast::http::status::internal_server_error, in_error_code);
+      return;
+    }
+    auto& l_req                  = in_handle->request_parser_->get();
+
+    auto l_computing_time_id_str = in_handle->capture_->get("computing_time_id");
+    boost::uuids::uuid l_computing_time_id{};
+    try {
+      l_computing_time_id = boost::lexical_cast<boost::uuids::uuid>(l_computing_time_id_str);
+    } catch (const std::exception& e) {
+      l_logger->log(log_loc(), level::err, "url parse error: {}", e.what());
+      in_error_code = boost::system::error_code{boost::system::errc::bad_message, boost::system::generic_category()};
+      in_handle->seed_error(boost::beast::http::status::bad_request, in_error_code, e.what());
+      return;
+    }
+    auto l_v = std::as_const(*g_reg()).view<const work_xlsx_task_info_block>();
+    for (auto&& [e, l_block] : l_v.each()) {
+      if (l_block.id_ == l_computing_time_id) {
+        auto l_user_e = l_block.user_refs_;
+        auto l_y_m    = l_block.year_month_;
+
+        boost::asio::post(g_io_context(), [e, l_user_e, l_y_m, in_handle]() {
+          g_reg()->patch<user>(l_user_e).task_block_.erase(l_y_m);
+          g_reg()->destroy(e);
+
+          boost::beast::http::response<boost::beast::http::empty_body> l_response{
+              boost::beast::http::status::ok, in_handle->request_parser_->get().version()
+          };
+          l_response.keep_alive(in_handle->request_parser_->keep_alive());
+          l_response.prepare_payload();
+          in_handle->seed(std::move(l_response));
+        });
+        return;
+      }
+    }
+
+    boost::beast::http::response<boost::beast::http::empty_body> l_response{
+        boost::beast::http::status::not_found, l_req.version()
+    };
+    l_response.keep_alive(l_req.keep_alive());
+    l_response.prepare_payload();
+    in_handle->seed(std::move(l_response));
+  }
+};
+
 void reg_computing_time(http_route& in_route) {
   in_route
       .reg(std::make_shared<http_function>(
@@ -569,6 +630,10 @@ void reg_computing_time(http_route& in_route) {
       .reg(std::make_shared<http_function>(
           boost::beast::http::verb::patch, "api/doodle/computing_time/{user_id}/{year_month}/{task_id}",
           session::make_http_reg_fun<boost::beast::http::string_body>(computing_time_patch{})
+      ))
+      .reg(std::make_shared<http_function>(
+          boost::beast::http::verb::delete_, "api/doodle/computing_time/{computing_time_id}",
+          session::make_http_reg_fun(computing_time_patch_delete{})
       ))
 
       ;
