f4f3b99d9e6153f339226af210547a4c451c9ed8
uuiid <957714080@qq.com>
2024-06-11 11:03:58 +0800

    调整快引用

--- src/doodle_core/metadata/attendance.h
+++ src/doodle_core/metadata/attendance.h
@@ -41,4 +41,34 @@ class attendance {
   friend void to_json(nlohmann::json& j, const attendance& p);
   // friend void from_json(const nlohmann::json& j, attendance& p);
 };
+
+class attendance_block {
+ public:
+  boost::uuids::uuid id_;
+
+  std::vector<attendance> attendance_block_{};
+  chrono::year_month_day create_date_{};                    //
+  chrono::zoned_time<chrono::microseconds> update_time_{};  // 更新时间
+  entt::entity user_ref_id_;
+
+ public:
+  static std::vector<attendance_block> select_all(
+      pooled_connection& in_comm, const std::map<boost::uuids::uuid, entt::entity>& in_map_id
+  );
+  static void create_table(pooled_connection& in_comm);
+
+  // 过滤已经存在的任务
+  static std::vector<bool> filter_exist(
+      pooled_connection& in_comm, const std::vector<attendance_block>& in_task
+  );
+  static void insert(
+      pooled_connection& in_comm, const std::vector<attendance_block>& in_task,
+      const std::map<entt::entity, boost::uuids::uuid>& in_map_id
+  );
+  static void update(
+      pooled_connection& in_comm, const std::vector<attendance_block>& in_task
+  );
+  static void delete_by_ids(pooled_connection& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
+};
+
 }  // namespace doodle
