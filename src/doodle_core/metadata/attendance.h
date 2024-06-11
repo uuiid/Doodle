c1ece684db084e3ac27f966257bf748af46ddb2d
uuiid <957714080@qq.com>
2024-06-11 11:26:32 +0800

    去除多余的函数

--- src/doodle_core/metadata/attendance.h
+++ src/doodle_core/metadata/attendance.h
@@ -23,20 +23,6 @@ class attendance {
   chrono::year_month_day create_date_{};                    // 创建日期
 
  public:
-  static std::vector<attendance> select_all(
-      pooled_connection& in_comm, const std::map<boost::uuids::uuid, entt::entity>& in_map_id
-  );
-  static void create_table(pooled_connection& in_comm);
-
-  // 过滤已经存在的任务
-  static std::vector<bool> filter_exist(pooled_connection& in_comm, const std::vector<attendance>& in_task);
-  static void insert(
-      pooled_connection& in_comm, const std::vector<attendance>& in_task,
-      const std::map<entt::entity, boost::uuids::uuid>& in_map_id
-  );
-  static void update(pooled_connection& in_comm, const std::vector<attendance>& in_task);
-  static void delete_by_ids(pooled_connection& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
-
   // to_json
   friend void to_json(nlohmann::json& j, const attendance& p);
   // friend void from_json(const nlohmann::json& j, attendance& p);
@@ -58,16 +44,12 @@ class attendance_block {
   static void create_table(pooled_connection& in_comm);
 
   // 过滤已经存在的任务
-  static std::vector<bool> filter_exist(
-      pooled_connection& in_comm, const std::vector<attendance_block>& in_task
-  );
+  static std::vector<bool> filter_exist(pooled_connection& in_comm, const std::vector<attendance_block>& in_task);
   static void insert(
       pooled_connection& in_comm, const std::vector<attendance_block>& in_task,
       const std::map<entt::entity, boost::uuids::uuid>& in_map_id
   );
-  static void update(
-      pooled_connection& in_comm, const std::vector<attendance_block>& in_task
-  );
+  static void update(pooled_connection& in_comm, const std::vector<attendance_block>& in_task);
   static void delete_by_ids(pooled_connection& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
 };
 
