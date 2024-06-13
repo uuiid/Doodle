125c8e94722e97f567e1aee400ec222b4510fdc0
uuiid <957714080@qq.com>
2024-06-13 09:52:57 +0800

    调整启动器

--- src/exe_gui/service_auto_light.cpp
+++ src/exe_gui/service_auto_light.cpp
@@ -7,4 +7,4 @@
 #include <doodle_lib/launch/auto_light_service.h>
 
 #include "main_macro.h"
-DOODLE_SERVICE_MAIN_IMPL(app_service_t, doodle::launch::auto_light_service_t);
+DOODLE_MAIN_IMPL(doodle::launch::auto_light_service_t);
