d3737ffa1deefe349c06873f7f816cb5d19f030a
uuiid <957714080@qq.com>
2024-06-13 10:25:49 +0800

    调整服务启动账号

--- build_script/deploy/detail/create_server.cmd
+++ build_script/deploy/detail/create_server.cmd
@@ -1,1 +1,2 @@
-nssm install doodle_doodle_scan_win_service D:/doodle_exe/bin/doodle_auto_light.exe
+nssm install doodle_scan_win_service D:/doodle_exe/bin/doodle_auto_light.exe
+nssm set doodle_scan_win_service ObjectName auto_light root
