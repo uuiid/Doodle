4359a2a34b73b41d3136472355a3ca2920826a13
uuiid <957714080@qq.com>
2024-06-13 10:40:17 +0800

    调整部署

--- build_script/deploy/detail/server_cmd.cmd
+++ build_script/deploy/detail/server_cmd.cmd
@@ -5,7 +5,7 @@ set DoodleSource=//192.168.40.53/Doodle2/build/Ninja_release/_CPack_Packages/win
 @REM 停止服务
 net stop doodle_scan_win_service
 net stop doodle_file_exists
-@REM net stop doodle_http_service
+net stop doodle_kitsu_supplement
 
 @REM 复制文件
 robocopy %DoodleSource% D:/doodle_exe/ /MIR
@@ -13,5 +13,5 @@ robocopy %DoodleSource% D:/doodle_exe/ /MIR
 @REM 启动服务
 net start doodle_scan_win_service
 net start doodle_file_exists
-@REM net start doodle_http_service
+net start doodle_kitsu_supplement
 
