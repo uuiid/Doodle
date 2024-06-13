c8ab22aa84b3a29ffdc2cc115563ac468e454265
uuiid <957714080@qq.com>
2024-06-13 09:52:49 +0800

    取消服务

--- src/doodle_lib/launch/auto_light_service.cpp
+++ src/doodle_lib/launch/auto_light_service.cpp
@@ -16,12 +16,6 @@
 namespace doodle::launch {
 
 bool auto_light_service_t::operator()(const argh::parser &in_arh, std::vector<std::shared_ptr<void>> &in_vector) {
-  auto &l_app                = static_cast<app_service &>(app_base::Get());
-  l_app.service_name_        = L"doodle_scan_win_service";
-  l_app.display_name_        = L"doodle_scan_win_service";
-  l_app.description_         = L"扫瞄服务器资产并进行确认后提交数据库工具";
-  l_app.command_line_        = L"";
-
   auto scan_win_service_ptr_ = std::make_shared<scan_win_service_t>();
   in_vector.emplace_back(scan_win_service_ptr_);
   boost::asio::post(g_io_context(), [scan_win_service_ptr_]() { scan_win_service_ptr_->start(); });
