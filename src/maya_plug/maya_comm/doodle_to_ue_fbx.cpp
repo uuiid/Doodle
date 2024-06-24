3f294ec89f72586b2bba244acae485a9d5a94e8b
uuiid <957714080@qq.com>
2024-06-24 11:49:06 +0800

    调整导出命令

--- src/maya_plug/maya_comm/doodle_to_ue_fbx.cpp
+++ src/maya_plug/maya_comm/doodle_to_ue_fbx.cpp
@@ -31,6 +31,8 @@ constexpr char export_anim[]   = "-nam";
 constexpr char export_anim_l[] = "-not_export_anim";
 constexpr char ascii_fbx[]     = "-asc";
 constexpr char ascii_fbx_l[]   = "-ascii_fbx";
+constexpr char sim_mesh[]      = "-sm";
+constexpr char sim_mesh_l[]    = "-sim_mesh";
 
 }  // namespace
 MSyntax doodle_to_ue_fbx_syntax() {
@@ -38,6 +40,8 @@ MSyntax doodle_to_ue_fbx_syntax() {
   l_syntax.addFlag(file_path, file_path_l, MSyntax::kString);
   l_syntax.addFlag(export_anim, export_anim_l, MSyntax::kNoArg);
   l_syntax.addFlag(ascii_fbx, ascii_fbx_l, MSyntax::kNoArg);
+  l_syntax.addFlag(sim_mesh, sim_mesh_l, MSyntax::MArgType::kString);
+  l_syntax.makeFlagMultiUse(sim_mesh);
   l_syntax.setObjectType(MSyntax::kSelectionList);
   l_syntax.useSelectionAsDefault(true);
   return l_syntax;
@@ -75,9 +79,22 @@ MStatus doodle_to_ue_fbx::doIt(const MArgList& in_list) {
     maya_chick(l_status);
     l_fbx_write.ascii_fbx();
   }
-  
+
+  MSelectionList l_sim_list{};
+
+  if (l_arg_data.isFlagSet(sim_mesh, &l_status)) {
+    maya_chick(l_status);
+
+    auto l_count = l_arg_data.numberOfFlagUses(sim_mesh);
+    for (auto i = 0; i < l_count; ++i) {
+      MString l_sim_mesh{};
+      maya_chick(l_arg_data.getFlagArgument(sim_mesh, i, l_sim_mesh));
+      maya_chick(l_sim_list.add(l_sim_mesh));
+    }
+  }
+
   l_fbx_write.set_path(l_path);
-  l_fbx_write.write(l_list, l_begin_time, l_end_time);
+  l_fbx_write.write(l_list, l_sim_list, l_begin_time, l_end_time);
 
   return MS::kSuccess;
 }
