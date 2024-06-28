5beeea3619b6c3a61f37e57aa0a894fb956d53ed
uuiid <957714080@qq.com>
2024-06-28 17:28:09 +0800

    调整错误的名称

--- src/doodle_lib/launch/file_association_http.cpp
+++ src/doodle_lib/launch/file_association_http.cpp
@@ -26,7 +26,7 @@ struct file_association_http_args_t {
   std::string kitsu_token_{};
 
   // form json
-  friend void from_json(const nlohmann::json& in_json, file_association_http_t& out_obj) {
+  friend void from_json(const nlohmann::json& in_json, file_association_http_args_t& out_obj) {
     if (in_json.contains("kitsu_ip")) in_json.at("kitsu_ip").get_to(out_obj.kitsu_ip_);
     if (in_json.contains("kitsu_port")) in_json.at("kitsu_port").get_to(out_obj.kitsu_port_);
     if (in_json.contains("kitsu_token")) in_json.at("kitsu_token").get_to(out_obj.kitsu_token_);
@@ -38,12 +38,12 @@ bool file_association_http_t::operator()(const argh::parser& in_arh, std::vector
   auto l_scan = g_ctx().emplace<scan_win_service_t>();
   l_scan.start();
 
-  auto_light_service_args_t l_args{.kitsu_ip_ = "192.168.40.182", .kitsu_port_ = "80", .port_ = 50026};
+  file_association_http_args_t l_args{.kitsu_ip_ = "192.168.40.182", .kitsu_port_ = "80", .port_ = 50026};
 
   if (auto l_file_path = in_arh({"config"}); l_file_path) {
     auto l_json = nlohmann::json::parse(FSys::ifstream{FSys::from_quotation_marks(l_file_path.str())});
     try {
-      l_args = l_json.get<auto_light_service_args_t>();
+      l_args = l_json.get<file_association_http_args_t>();
     } catch (...) {
       default_logger_raw()->log(log_loc(), level::err, boost::current_exception_diagnostic_information());
       return true;
