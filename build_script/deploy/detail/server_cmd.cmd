fb0fb36df5e8c0f26cf9444d56e905c35729adeb
uuiid <957714080@qq.com>
2024-07-01 16:58:14 +0800

    调整服务

--- build_script/deploy/detail/server_cmd.cmd
+++ build_script/deploy/detail/server_cmd.cmd
@@ -3,7 +3,7 @@ set DoodleSource=//192.168.40.53/Doodle2/build/Ninja_release/_CPack_Packages/win
 
 
 @REM 停止服务
-net stop doodle_scan_win_service
+net stop doodle_file_association_http
 net stop doodle_file_exists
 net stop doodle_kitsu_supplement
 
@@ -11,7 +11,7 @@ net stop doodle_kitsu_supplement
 robocopy %DoodleSource% D:/doodle_exe/ /MIR
 
 @REM 启动服务
-net start doodle_scan_win_service
+net start doodle_file_association_http
 net start doodle_file_exists
 net start doodle_kitsu_supplement
 
