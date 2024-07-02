83f9ee9caab86a6af2bb3dbc96a38819ed80458b
uuiid <957714080@qq.com>
2024-07-02 13:14:38 +0800

    调整测试

--- src/test/core/copy_folder.cpp
+++ src/test/core/copy_folder.cpp
@@ -14,7 +14,8 @@ BOOST_AUTO_TEST_CASE(copy_folder) {
   thread_copy_io_service l_copy{};
 
   l_copy.async_delete_remote_not_exit_and_copy_old(
-      {{R"(D:\job)", R"(D:\job2)"}}, {"ModernCSV-Win-v2.0.7"}, FSys::copy_options::recursive, spdlog::default_logger(),
+      {{R"(D:\job)", R"(D:\job2)"}, {R"(D:\test_files)", R"(D:\job2)"}}, {"ModernCSV-Win-v2.0.7"},
+      FSys::copy_options::recursive, spdlog::default_logger(),
       [](const boost::system::error_code& in_ec) {
         if (in_ec) {
           BOOST_TEST_MESSAGE(in_ec.message());
