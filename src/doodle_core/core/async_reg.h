a4e35ba3976b580a43e445984623f3f5369f7ea8
uuiid <957714080@qq.com>
2024-07-05 10:41:37 +0800

    添加一部分协程辅助函数

--- src/doodle_core/core/async_reg.h
+++ src/doodle_core/core/async_reg.h
@@ -1,15 +1,46 @@
 #include <doodle_lib/doodle_lib_fwd.h>
 
+#include <entt/entt.hpp>
 namespace doodle::async_reg {
 
-namespace detail {}
+namespace detail {
+
+template <typename T, typename CompletionHandler>
+class async_get_op
+    : public boost::asio::coroutine,
+      public boost::beast::async_base<CompletionHandler, boost::asio::strand<boost::asio::io_context::executor_type>> {
+ public:
+  using base_type =
+      boost::beast::async_base<CompletionHandler, boost::asio::strand<boost::asio::io_context::executor_type>>;
+  async_get_op(const entt::registry& in_reg, const std::vector<entt::entity>& in_entts, CompletionHandler&& handler)
+      : base_type{std::forward(handler), g_strand()}, reg_{in_reg}, entts_{in_entts} {}
+
+  void operator()() {
+    std::vector<std::optional<T>> l_result;
+    BOOST_ASIO_CORO_REENTER(*this) {
+      for (const auto& l_entt : entts_) {
+        if (reg_.valid(l_entt) && reg_.all_of<T>(l_entt))
+          l_result.emplace_back(reg_.get<T>(l_entt));
+        else
+          l_result.emplace_back(std::nullopt);
+      }
+      this->complete(boost::system::error_code{}, std::move(l_result));
+    }
+  }
+
+ private:
+  const entt::registry& reg_;
+  const std::vector<entt::entity>& entts_;
+};
+
+}  // namespace detail
 
 template <typename T, typename CompletionHandler>
 auto async_get(const entt::registry& in_reg, const std::vector<entt::entity>& in_entts, CompletionHandler&& handler) {
-  return boost::asio::async_initiate<CompletionHandler, void(boost::system::error_code, std::vector<entt::entity>)>(
+  return boost::asio::async_initiate<CompletionHandler, void(boost::system::error_code, std::vector<std::optional<T>>)>(
       [in_reg = &in_reg, in_entts = &in_entts](auto&& handler) {
         auto l_handler = std::make_shared<std::decay_t<decltype(handler)>>(std::forward<decltype(handler)>(handler));
-       },
+      },
       handler, &in_reg, std::move(in_entts)
   );
 }
