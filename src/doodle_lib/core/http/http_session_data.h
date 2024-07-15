81e3e1b06d9da29252beddcaad184fb7e780e149
uuiid <957714080@qq.com>
2024-07-15 17:38:59 +0800

    [自动灯光] 调整错误类别

--- src/doodle_lib/core/http/http_session_data.h
+++ src/doodle_lib/core/http/http_session_data.h
@@ -22,7 +22,6 @@ class http_session_data;
 using http_session_data_ptr = std::shared_ptr<http_session_data>;
 
 namespace detail {
-
 enum class content_type {
   text_plain,
   application_json,
@@ -36,7 +35,7 @@ enum class content_type {
 };
 
 class session_data {
- public:
+public:
   session_data()  = default;
   ~session_data() = default;
   logger_ptr logger_{};
@@ -48,19 +47,24 @@ class session_data {
   bool keep_alive_{};
 
   content_type content_type_{content_type::text_plain};
-  std::variant<std::string, nlohmann::json> body_{};  // std::variant<std::string, nlohmann::json>
+  std::variant<std::string, nlohmann::json> body_{}; // std::variant<std::string, nlohmann::json>
 
   boost::beast::http::message_generator make_error_code_msg(
-      boost::beast::http::status in_status, const boost::system::error_code& ec, const std::string& in_str = ""
+    boost::beast::http::status in_status, const boost::system::error_code& ec, const std::string& in_str = ""
   );
   boost::beast::http::message_generator make_error_code_msg(
-      boost::beast::http::status in_status, const std::string& in_str
+    std::int32_t in_code, const std::string& in_str
   );
+
+  inline boost::beast::http::message_generator make_error_code_msg(
+    boost::beast::http::status in_code, const std::string& in_str
+  ) {
+    return make_error_code_msg(enum_to_num(in_code), in_str);
+  }
 };
 
 boost::asio::awaitable<void> async_session(boost::asio::ip::tcp::socket in_socket, http_route_ptr in_route_ptr);
-}  // namespace detail
+} // namespace detail
 using session_data     = detail::session_data;
 using session_data_ptr = std::shared_ptr<detail::session_data>;
-
-}  // namespace doodle::http
+} // namespace doodle::http
