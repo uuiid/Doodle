2d561dd6cec55174ac0cc7b3c74096df67f2d64a
uuiid <957714080@qq.com>
2024-07-16 18:40:00 +0800

    [自动灯光] 调整为缓冲区旋转

--- src/doodle_lib/core/scan_win_service.h
+++ src/doodle_lib/core/scan_win_service.h
@@ -26,7 +26,7 @@ class scan_win_service_t {
   std::map<uuid, entt::handle> handle_map_;
   std::map<FSys::path, entt::handle> path_map_;
   using scan_category_data_id_map = std::map<boost::uuids::uuid, doodle::details::scan_category_data_ptr>;
-  std::array<scan_category_data_id_map,2> scan_data_maps_;
+  std::array<scan_category_data_id_map, 2> scan_data_maps_;
   std::atomic_int index_{};
 
   void scan();
@@ -44,22 +44,6 @@ public:
 
   const std::map<boost::uuids::uuid, doodle::details::scan_category_data_ptr>& get_scan_data() const {
     return scan_data_maps_[index_];
-  }
-
-  template <typename CompletionHandler>
-  auto async_scan_data(CompletionHandler&& handler) {
-    auto l_init = [this](auto&& in_handle) {
-      using handler_type = std::decay_t<decltype(in_handle)>;
-      boost::asio::post(boost::asio::bind_executor(
-        g_io_context(), [this, in_handle = std::make_shared<handler_type>(std::forward<decltype(in_handle)>(in_handle)
-        )]() {
-          (*in_handle)(boost::system::error_code(), scan_data_map_);
-        }
-      ));
-    };
-    return boost::asio::async_initiate<CompletionHandler, void(boost::system::error_code, decltype(scan_data_map_))>(
-      l_init, handler
-    );
   }
 };
 } // namespace doodle
