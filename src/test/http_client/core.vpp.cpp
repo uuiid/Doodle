4bcd3eae67721a5a48ea103f6afdf4f7f7cfc841
uuiid <957714080@qq.com>
2024-07-18 10:44:11 +0800

    [自动灯光] 调整队列测试

--- src/test/http_client/core.vpp.cpp
+++ src/test/http_client/core.vpp.cpp
@@ -16,21 +16,28 @@ BOOST_AUTO_TEST_CASE(quequ_t) {
   app_base l_app_base{};
   boost::asio::ssl::context l_ctx{boost::asio::ssl::context::tlsv12_client};
   auto l_c = std::make_shared<http::detail::http_client_data_base>(g_io_context());
-  l_c->init("https://baidu.com/", &l_ctx);
+  l_c->init("https://www.baidu.com/", &l_ctx);
   boost::beast::http::request<boost::beast::http::empty_body> l_req{boost::beast::http::verb::get, "/", 11};
+  l_req.keep_alive(true);
   l_req.prepare_payload();
-  auto l_f1 = boost::asio::co_spawn(
-      g_io_context(), http::detail::read_and_write<boost::beast::http::string_body>(l_c, l_req), boost::asio::use_future
-  );
-  auto l_f2 = boost::asio::co_spawn(
-      g_io_context(), http::detail::read_and_write<boost::beast::http::string_body>(l_c, l_req), boost::asio::use_future
-  );
+  std::vector<decltype(boost::asio::co_spawn(
+    g_io_context(), http::detail::read_and_write<boost::beast::http::string_body>(l_c, l_req), boost::asio::use_future
+  ))> l_vs{};
+  for (int i = 0; i < 10; ++i) {
+    l_vs.emplace_back(
+      boost::asio::co_spawn(
+        g_io_context(), http::detail::read_and_write<boost::beast::http::string_body>(l_c, l_req),
+        boost::asio::use_future
+      )
+    );
+  }
   g_io_context().run();
 
-  auto [l_ec1, l_r1] = l_f1.get();
-  BOOST_TEST(!l_ec1);
-  auto [l_ec2, l_r2] = l_f2.get();
-  BOOST_TEST(!l_ec2);
+  for (auto& l_v : l_vs) {
+    auto [l_ex,l_] = l_v.get();
+    BOOST_TEST(!l_ex);
+    BOOST_TEST_MESSAGE(l_ex.message());
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
