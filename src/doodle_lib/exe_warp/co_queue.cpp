d45f22e9d34bddb804801819752cb54c19d6893b
uuiid <957714080@qq.com>
2024-07-19 13:32:53 +0800

    [自动灯光] 添加队列限制器

--- src/doodle_lib/exe_warp/co_queue.cpp
+++ src/doodle_lib/exe_warp/co_queue.cpp
@@ -2,4 +2,27 @@
 // Created by TD on 24-7-19.
 //
 
-#include "co_squeue.h"
+#include "co_queue.h"
+
+namespace doodle {
+void awaitable_queue_limitation::awaitable_queue_impl::await_suspend(std::function<void()> in_handle) {
+  {
+    const std::lock_guard l{lock_};
+    next_list_.emplace(in_handle);
+  }
+}
+
+
+void awaitable_queue_limitation::awaitable_queue_impl::next() {
+  ++run_task_;
+  const std::lock_guard l{lock_};
+  next_list_.front()();
+  next_list_.pop();
+}
+
+void awaitable_queue_limitation::awaitable_queue_impl::maybe_invoke() {
+  if (next_list_.empty()) return;
+  if (run_task_ >= limit_) return;
+  next();
+}
+}
--- src/doodle_lib/exe_warp/co_queue.h
+++ src/doodle_lib/exe_warp/co_queue.h
@@ -3,3 +3,89 @@
 //
 
 #pragma once
+#include <boost/asio.hpp>
+#include <atomic>
+#include <functional>
+#include <memory>
+
+namespace doodle {
+class awaitable_queue_limitation {
+public:
+  class queue_guard;
+  using queue_guard_ptr = std::shared_ptr<queue_guard>;
+
+private:
+  struct awaitable_queue_impl;
+
+  template <typename CompletionToken>
+  struct call_fun_t {
+    boost::asio::any_io_executor executor_{};
+    std::shared_ptr<std::decay_t<CompletionToken>> handler_;
+    awaitable_queue_limitation* queue_{};
+
+
+    void operator()() const {
+      boost::asio::post(boost::asio::bind_executor(executor_, [h = std::move(handler_), q = queue_]() {
+        auto l_guard = std::make_shared<queue_guard>(*q);
+        (*h)(l_guard);
+      }));
+    }
+  };
+
+public:
+  awaitable_queue_limitation()  = default;
+  ~awaitable_queue_limitation() = default;
+
+  class queue_guard {
+    std::shared_ptr<awaitable_queue_impl> impl_;
+
+  public:
+    explicit queue_guard(awaitable_queue_limitation& in_queue) : impl_{in_queue.impl_} {
+    }
+
+    ~queue_guard() {
+      --impl_->run_task_;
+      impl_->maybe_invoke();
+    }
+  };
+
+
+  template <typename CompletionToken>
+  auto queue(CompletionToken&& in_token) {
+    boost::asio::any_io_executor l_exe = boost::asio::get_associated_executor(in_token);
+    return boost::asio::async_initiate<CompletionToken, void(queue_guard_ptr)>(
+      [](auto&& in_compl, awaitable_queue_limitation* in_self, const boost::asio::any_io_executor& in_exe) {
+        call_fun_t<std::decay_t<decltype(in_compl)>> l_fun{in_exe, std::make_shared<std::decay_t<decltype(in_compl)>>(
+                                                             std::forward<decltype(in_compl)>(in_compl)
+                                                           ),
+                                                           in_self
+        };
+        in_self->impl_->await_suspend(l_fun);
+        in_self->impl_->maybe_invoke();
+      }, in_token, this, l_exe);
+  }
+
+  void set_limit(int in_limit) {
+    impl_->limit_ = in_limit;
+  }
+
+private:
+  struct awaitable_queue_impl {
+    std::queue<std::function<void()>> next_list_{};
+    std::recursive_mutex lock_{};
+    std::atomic_int limit_{1};
+    std::atomic_int run_task_{};
+
+    awaitable_queue_impl() = default;
+
+    ~awaitable_queue_impl() = default;
+    void await_suspend(std::function<void()> in_handle);
+
+    void next();
+    void maybe_invoke();
+  };
+
+  std::shared_ptr<awaitable_queue_impl> impl_ = std::make_shared<awaitable_queue_impl>(
+  );
+};
+} // namespace doodle
