56e3d6811b747de76b527078cd22cd0d1b8e3d18
uuiid <957714080@qq.com>
2024-07-19 18:14:25 +0800

    [自动灯光] 添加设置窗口隐藏

---
+++ src/doodle_lib/exe_warp/windows_hide.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 24-7-19.
+//
+
+#include "windows_hide.h"
---
+++ src/doodle_lib/exe_warp/windows_hide.h
@@ -0,0 +1,10 @@
+//
+// Created by TD on 24-7-19.
+//
+
+#pragma once
+#include <windows.h>
+#include <boost/process/v2.hpp>
+namespace doodle::details {
+
+}
