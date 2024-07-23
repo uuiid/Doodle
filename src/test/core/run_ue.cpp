c2fa0b6c57e2e739c54d0501ca31fcfc52c3c856
uuiid <957714080@qq.com>
2024-07-23 17:56:14 +0800

    [自动灯光] 调整测试

--- src/test/core/run_ue.cpp
+++ src/test/core/run_ue.cpp
@@ -6,13 +6,14 @@
 
 #include "doodle_lib/exe_warp/ue_exe.h"
 #include <doodle_lib/doodle_lib_fwd.h>
-
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_suite.hpp>
 #include <boost/asio.hpp>
 using namespace doodle;
 
-int core_run_ue(int, char** const) {
+BOOST_AUTO_TEST_CASE(core_run_ue) {
   app_base l_base{};
   boost::asio::co_spawn(g_io_context(),
                         async_run_ue("--version", spdlog::default_logger()), boost::asio::detached);
-  return 0;
+  g_io_context().run();
 }
