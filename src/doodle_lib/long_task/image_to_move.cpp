b4df95dd2bf2c1d530d1b7c8091bd0087f379154
uuiid <957714080@qq.com>
2024-07-23 11:50:49 +0800

    [自动灯光] 调整包装

--- src/doodle_lib/long_task/image_to_move.cpp
+++ src/doodle_lib/long_task/image_to_move.cpp
@@ -18,9 +18,9 @@ namespace doodle {
 namespace detail {
 namespace {
 // 计算baseline大小
-void watermark_add_image(cv::Mat &in_image, const std::vector<image_to_move::image_watermark> &in_watermark) {
+void watermark_add_image(cv::Mat& in_image, const std::vector<image_to_move::image_watermark>& in_watermark) {
   auto l_string_s = in_watermark |
-                    ranges::views::transform([](const image_to_move::image_watermark &in) { return in.text_attr; }) |
+                    ranges::views::transform([](const image_to_move::image_watermark& in) { return in.text_attr; }) |
                     ranges::to_vector;
   cv::Ptr<cv::freetype::FreeType2> const l_ft2{cv::freetype::createFreeType2()};
   l_ft2->loadFontData(std::string{doodle_config::font_default}, 0);
@@ -29,22 +29,22 @@ void watermark_add_image(cv::Mat &in_image, const std::vector<image_to_move::ima
 
   std::map<std::double_t, std::string> l_string_map{};
   std::map<std::double_t, std::int32_t> l_baseline_map{};
-  for (auto &&l_watermark : in_watermark) {
+  for (auto&& l_watermark : in_watermark) {
     l_string_map[l_watermark.height_proportion_attr] += l_watermark.text_attr;
   }
 
   auto l_image                       = in_image;
   constexpr std::int32_t l_thickness = -1;
-  for (auto &&[l_key, l_string] : l_string_map) {
+  for (auto&& [l_key, l_string] : l_string_map) {
     std::int32_t l_baseline = 0;
 
-    auto textSize           = l_ft2->getTextSize(l_string, l_fontHeight, l_thickness, &l_baseline);
+    auto textSize = l_ft2->getTextSize(l_string, l_fontHeight, l_thickness, &l_baseline);
     if (l_thickness > 0) l_baseline += l_thickness;
     l_baseline_map[l_key] = l_baseline;
   }
 
   std::int32_t l_baseline_tmp = 0;
-  for (auto &&l_watermark : in_watermark) {
+  for (auto&& l_watermark : in_watermark) {
     if (l_watermark.text_attr.empty()) continue;
     auto textSize = l_ft2->getTextSize(l_watermark.text_attr, l_fontHeight, l_thickness, &l_baseline_tmp);
 
@@ -52,50 +52,79 @@ void watermark_add_image(cv::Mat &in_image, const std::vector<image_to_move::ima
     textSize.height += l_baseline_map[l_watermark.height_proportion_attr];
     // center the text
     cv::Point textOrg(
-        (in_image.cols) * l_watermark.width_proportion_attr, (in_image.rows) * l_watermark.height_proportion_attr
+      (in_image.cols) * l_watermark.width_proportion_attr, (in_image.rows) * l_watermark.height_proportion_attr
     );
 
     // draw the box
     cv::rectangle(
-        l_image, textOrg + cv::Point(0, l_baseline_map[l_watermark.height_proportion_attr]),
-        textOrg + cv::Point(textSize.width, -textSize.height), cv::Scalar(30, 31, 34, 75), -1
+      l_image, textOrg + cv::Point(0, l_baseline_map[l_watermark.height_proportion_attr]),
+      textOrg + cv::Point(textSize.width, -textSize.height), cv::Scalar(30, 31, 34, 75), -1
     );
 
     cv::addWeighted(l_image, 0.7, in_image, 0.3, 0, in_image);
     // then put the text itself
     l_ft2->putText(
-        in_image, l_watermark.text_attr, textOrg, l_fontHeight,
-        cv::Scalar{
-            l_watermark.rgba_attr[0], l_watermark.rgba_attr[1], l_watermark.rgba_attr[2], l_watermark.rgba_attr[3]
-        },
-        l_thickness, cv::LineTypes::LINE_AA, true
+      in_image, l_watermark.text_attr, textOrg, l_fontHeight,
+      cv::Scalar{
+          l_watermark.rgba_attr[0], l_watermark.rgba_attr[1], l_watermark.rgba_attr[2], l_watermark.rgba_attr[3]
+      },
+      l_thickness, cv::LineTypes::LINE_AA, true
     );
   }
 }
 
-auto create_gamma_LUT_table(const std::double_t &in_gamma) {
+auto create_gamma_LUT_table(const std::double_t& in_gamma) {
   cv::Mat lookupTable(1, 256, CV_8U);
-  uchar *p = lookupTable.ptr();
+  uchar* p = lookupTable.ptr();
 
   for (int i = 0; i < 256; ++i) p[i] = cv::saturate_cast<uchar>(std::pow(i / 255.0, in_gamma) * 255.0);
   return lookupTable;
 }
-}  // namespace
+} // namespace
 
-class image_to_move::impl {
- public:
-  impl() = default;
-};
 
-image_to_move::~image_to_move() = default;
+FSys::path image_to_move::create_out_path(const entt::handle& in_handle) {
+  boost::ignore_unused(this);
 
-image_to_move::image_to_move() : p_i(std::make_unique<impl>()) {}
-boost::system::error_code image_to_move::create_move(
-    const FSys::path &in_out_path, logger_ptr in_logger, const std::vector<image_to_move::image_attr> &in_vector
+  FSys::path l_out = in_handle.get<out_file_path>().path;
+  detail::create_out_path(
+    l_out, in_handle.get<episodes>(), in_handle.get<shot>(), in_handle.try_get<project>());
+  in_handle.replace<out_file_path>(l_out);
+  return l_out;
+}
+
+FSys::path create_out_path(const FSys::path& in_dir, const episodes& in_eps, const shot& in_shot,
+                           const project* in_project) {
+  FSys::path l_out = in_dir;
+
+  /// \brief 这里我们检查 shot，episode 进行路径的组合
+  if (!l_out.has_extension()) {
+    if (in_project)
+      l_out /= fmt::format(
+        "EP{:04}_SC{:03}{}.mp4", in_eps ? in_eps.p_episodes : 0,
+        in_shot ? in_shot.p_shot : 0,
+        in_shot ? in_shot.p_shot_enum : shot::shot_ab_enum::None
+      );
+    else
+      l_out /= fmt::format(
+        "{}_EP{:04}_SC{:03}{}.mp4", in_project->p_shor_str,
+        in_eps ? in_eps.p_episodes : 0,
+        in_shot ? in_shot.p_shot : 0,
+        in_shot ? in_shot.p_shot_enum : shot::shot_ab_enum::None
+      );
+  } else
+    l_out.extension().replace_extension(".mp4");
+
+  if (exists(l_out.parent_path())) create_directories(l_out.parent_path());
+  return l_out;
+}
+
+boost::system::error_code create_move(
+  const FSys::path& in_out_path, logger_ptr in_logger, const std::vector<movie::image_attr>& in_vector
 ) {
   /// \brief 这里排序组件
   auto l_vector = in_vector;
-  image_attr::extract_num(l_vector);
+  movie::image_attr::extract_num(l_vector);
   std::sort(l_vector.begin(), l_vector.end());
   std::atomic_bool l_stop{};
   boost::system::error_code l_ec{};
@@ -119,16 +148,16 @@ boost::system::error_code image_to_move::create_move(
   }
 
   const static cv::Size k_size{1920, 1080};
-  auto video   = cv::VideoWriter{in_out_path.generic_string(), cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 25, k_size};
+  auto video = cv::VideoWriter{in_out_path.generic_string(), cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 25, k_size};
   auto k_image = cv::Mat{};
-  const auto &k_size_len = l_vector.size();
-  auto l_gamma           = create_gamma_LUT_table(l_vector.empty() ? 1.0 : l_vector.front().gamma_t);
-  for (auto &l_image : l_vector) {
+  const auto& k_size_len = l_vector.size();
+  auto l_gamma = create_gamma_LUT_table(l_vector.empty() ? 1.0 : l_vector.front().gamma_t);
+  for (auto& l_image : l_vector) {
     if (l_stop) {
       in_logger->log(log_loc(), level::err, "合成视频被主动结束 合成视频文件将被主动删除");
       try {
         remove(in_out_path);
-      } catch (const FSys::filesystem_error &err) {
+      } catch (const FSys::filesystem_error& err) {
         in_logger->log(log_loc(), level::err, "合成视频主动删除失败 {} ", boost::diagnostic_information(err));
       }
       in_logger->log(log_loc(), level::off, fmt::to_string(process_message::fail));
@@ -159,35 +188,5 @@ boost::system::error_code image_to_move::create_move(
   in_logger->log(log_loc(), level::off, fmt::to_string(process_message::success));
   return l_ec;
 }
-FSys::path image_to_move::create_out_path(const entt::handle &in_handle) {
-  boost::ignore_unused(this);
-
-  FSys::path l_out{};
-  l_out = in_handle.get<out_file_path>().path;
-
-  /// \brief 这里我们检查 shot，episode 进行路径的组合
-  if (!l_out.has_extension() && in_handle.any_of<episodes, shot>()) {
-    if (!in_handle.any_of<project>())
-      l_out /= fmt::format(
-          "EP{:04}_SC{:03}{}.mp4", in_handle.any_of<episodes>() ? in_handle.get<episodes>().p_episodes : 0,
-          in_handle.any_of<shot>() ? in_handle.get<shot>().p_shot : 0,
-          in_handle.any_of<shot>() ? in_handle.get<shot>().p_shot_enum : shot::shot_ab_enum::None
-      );
-    else
-      l_out /= fmt::format(
-          "{}_EP{:04}_SC{:03}{}.mp4", in_handle.get<project>().p_shor_str,
-          in_handle.any_of<episodes>() ? in_handle.get<episodes>().p_episodes : 0,
-          in_handle.any_of<shot>() ? in_handle.get<shot>().p_shot : 0,
-          in_handle.any_of<shot>() ? in_handle.get<shot>().p_shot_enum : shot::shot_ab_enum::None
-      );
-  } else if (!l_out.has_extension()) {
-    l_out /= fmt::format("{}.mp4", core_set::get_set().get_uuid());
-  } else
-    l_out.extension().replace_extension(".mp4");
-
-  if (exists(l_out.parent_path())) create_directories(l_out.parent_path());
-  in_handle.replace<out_file_path>(l_out);
-  return l_out;
-}
-}  // namespace detail
-}  // namespace doodle
+} // namespace detail
+} // namespace doodle
--- src/doodle_lib/long_task/image_to_move.h
+++ src/doodle_lib/long_task/image_to_move.h
@@ -11,18 +11,19 @@
 #include <boost/asio.hpp>
 
 namespace doodle {
-
 namespace detail {
+FSys::path create_out_path(const FSys::path& in_dir, const episodes& in_eps, const shot& in_shot,
+                           const project* in_project = nullptr);
+boost::system::error_code create_move(
+  const FSys::path& in_out_path, logger_ptr in_msg, const std::vector<movie::image_attr>& in_vector
+);
 
 class DOODLELIB_API image_to_move : public image_to_movie_interface {
- public:
+public:
   using image_attr      = ::doodle::movie::image_attr;
   using image_watermark = ::doodle::movie::image_watermark;
 
- private:
-  class impl;
-  std::unique_ptr<impl> p_i;
-
+private:
   /**
    * 使用句柄检查路径
    * 必须具有组件 FSys::path,和后缀 mp4
@@ -30,16 +31,17 @@ class DOODLELIB_API image_to_move : public image_to_movie_interface {
    * @param in_handle 传入的句柄
    * @return 规范的路径
    */
-  FSys::path create_out_path(const entt::handle &in_handle) override;
+  FSys::path create_out_path(const entt::handle& in_handle) override;
 
- public:
-  image_to_move();
-  virtual ~image_to_move();
+public:
+  image_to_move()          = default;
+  virtual ~image_to_move() = default;
 
   virtual boost::system::error_code create_move(
-      const FSys::path &in_out_path, logger_ptr in_msg, const std::vector<image_attr> &in_vector
-  ) override;
+    const FSys::path& in_out_path, logger_ptr in_msg, const std::vector<image_attr>& in_vector
+  ) override {
+    return detail::create_move(in_out_path, in_msg, in_vector);
+  }
 };
-}  // namespace detail
-
-}  // namespace doodle
+} // namespace detail
+} // namespace doodle
