24a996ac71b1914e6b7e32f4e31ff32985d58dac
uuiid <957714080@qq.com>
2024-07-23 13:15:18 +0800

    [自动灯光] 调整属性包装

--- src/doodle_core/metadata/move_create.h
+++ src/doodle_core/metadata/move_create.h
@@ -6,17 +6,20 @@
 
 #include <doodle_core/doodle_core_fwd.h>
 
+#include <doodle_core/metadata/shot.h>
+#include <doodle_core/metadata/episodes.h>
+
 namespace doodle::movie {
 class DOODLE_CORE_API image_watermark {
- private:
+private:
   friend void DOODLE_CORE_API to_json(nlohmann::json& nlohmann_json_j, const image_watermark& nlohmann_json_t);
   friend void DOODLE_CORE_API from_json(const nlohmann::json& nlohmann_json_j, image_watermark& nlohmann_json_t);
 
- public:
-  using rgba_t      = std::array<std::double_t, 4>;
+public:
+  using rgba_t = std::array<std::double_t, 4>;
   image_watermark() = default;
   image_watermark(
-      std::string in_p_text, double_t in_p_width_proportion, double_t in_p_height_proportion, rgba_t in_rgba
+    std::string in_p_text, double_t in_p_width_proportion, double_t in_p_height_proportion, rgba_t in_rgba
   );
   std::string text_attr{};
   std::double_t width_proportion_attr{};
@@ -26,11 +29,11 @@ class DOODLE_CORE_API image_watermark {
 };
 
 class DOODLE_CORE_API image_attr : boost::totally_ordered<image_attr> {
- private:
+private:
   friend void DOODLE_CORE_API to_json(nlohmann::json& nlohmann_json_j, const image_attr& nlohmann_json_t);
   friend void DOODLE_CORE_API from_json(const nlohmann::json& nlohmann_json_j, image_attr& nlohmann_json_t);
 
- public:
+public:
   image_attr() = default;
   explicit image_attr(FSys::path in_path);
 
@@ -42,11 +45,13 @@ class DOODLE_CORE_API image_attr : boost::totally_ordered<image_attr> {
   static void extract_num(std::vector<image_attr>& in_image_list);
 
   static std::vector<image_attr> make_default_attr(
-      const entt::handle& in_handle, const std::vector<FSys::path>& in_path_list
+    const entt::handle& in_handle, const std::vector<FSys::path>& in_path_list
+  );
+  static std::vector<image_attr> make_default_attr(
+    const episodes* in_episodes, const shot* in_shot, const std::vector<FSys::path>& in_path_list
   );
 
   bool operator<(const image_attr& in_rhs) const noexcept;
   bool operator==(const image_attr& in_rhs) const noexcept;
 };
-
-}  // namespace doodle::movie
+} // namespace doodle::movie
