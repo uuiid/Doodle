7ead536acdbf16aafafd22879b56c4b2af6d903f
uuiid <957714080@qq.com>
2024-07-25 13:56:43 +0800

    [自动灯光] 调整导入头文件

--- src/doodle_lib/core/http/http_session_data.h
+++ src/doodle_lib/core/http/http_session_data.h
@@ -4,9 +4,7 @@
 
 #pragma once
 
-#include <doodle_core/core/wait_op.h>
-#include <doodle_core/lib_warp/boost_fmt_error.h>
-
+#include <doodle_core/doodle_core_fwd.h>
 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
