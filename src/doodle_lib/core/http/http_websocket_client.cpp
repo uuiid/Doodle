1b333b4812ae93384bb5d48a253aa0511282bd4a
uuiid <957714080@qq.com>
2024-07-25 11:10:30 +0800

    [自动灯光] 添加websocket客户端

---
+++ src/doodle_lib/core/http/http_websocket_client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TD on 24-7-25.
+//
+
+#include "http_websocket_client.h"
---
+++ src/doodle_lib/core/http/http_websocket_client.h
@@ -0,0 +1,8 @@
+//
+// Created by TD on 24-7-25.
+//
+
+#ifndef HTTP_WEBSOCKET_CLIENT_H
+#define HTTP_WEBSOCKET_CLIENT_H
+
+#endif //HTTP_WEBSOCKET_CLIENT_H
