ea43bc02f29f138c564b804c92330c592093b6cf
uuiid <957714080@qq.com>
2024-07-25 15:08:05 +0800

    [自动灯光] 调整计算机注册

--- src/doodle_lib/http_method/computer_reg_data.cpp
+++ src/doodle_lib/http_method/computer_reg_data.cpp
@@ -2,3 +2,31 @@
 // Created by TD on 24-7-25.
 //
 #include "computer_reg_data.h"
+
+namespace doodle::http {
+
+
+computer_reg_data_manager& computer_reg_data_manager::get() {
+  static computer_reg_data_manager l_manager;
+  return l_manager;
+}
+void computer_reg_data_manager::reg(computer_reg_data_ptr in_data) {
+  std::lock_guard<std::mutex> l_lock(mutex_);
+  computer_reg_datas_.insert(in_data);
+  clear_old();
+}
+std::set<computer_reg_data_ptr> computer_reg_data_manager::list() {
+  std::lock_guard<std::mutex> l_lock(mutex_);
+  return computer_reg_datas_;
+}
+void computer_reg_data_manager::clear_old() {
+  for (auto it = computer_reg_datas_.begin(); it != computer_reg_datas_.end();) {
+    if ((*it)->websocket_data_.expired()) {
+      it = computer_reg_datas_.erase(it);
+    } else {
+      ++it;
+    }
+  }
+}
+
+}
--- src/doodle_lib/http_method/computer_reg_data.h
+++ src/doodle_lib/http_method/computer_reg_data.h
@@ -1,5 +1,5 @@
 #include <doodle_core/metadata/computer.h>
-
+#include <doodle_lib/core/http/http_session_data.h>
 namespace doodle {
 class server_task_info;
 }
@@ -15,5 +15,20 @@ class computer_reg_data {
   doodle::computer computer_data_;
   std::shared_ptr<doodle::server_task_info> task_info_;
   entt::entity task_info_entity_{};
+  std::weak_ptr<detail::http_websocket_data> websocket_data_;
+};
+using computer_reg_data_ptr = std::shared_ptr<computer_reg_data>;
+class computer_reg_data_manager {
+  std::set<computer_reg_data_ptr> computer_reg_datas_;
+  std::mutex mutex_;
+  void clear_old();
+
+ public:
+  computer_reg_data_manager()  = default;
+  ~computer_reg_data_manager() = default;
+
+  void reg(computer_reg_data_ptr in_data);
+  std::set<computer_reg_data_ptr> list();
+  static computer_reg_data_manager& get();
 };
 }  // namespace doodle::http
