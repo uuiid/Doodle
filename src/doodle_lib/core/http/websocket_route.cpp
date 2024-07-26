a91e890884d062613877dab6afbede93776a14a2
uuiid <957714080@qq.com>
2024-07-26 15:06:59 +0800

    [自动灯光] 调整协程

--- src/doodle_lib/core/http/websocket_route.cpp
+++ src/doodle_lib/core/http/websocket_route.cpp
@@ -9,13 +9,13 @@
 #include <doodle_lib/core/http/http_session_data.h>
 
 namespace doodle::http {
-websocket_route::map_actin_type websocket_route::operator()(const std::string& in_name) const {
+websocket_route::call_fun_type websocket_route::operator()(const std::string& in_name) const {
   auto l_iter = actions_.find(in_name);
   if (l_iter == actions_.end()) return not_function_;
   return l_iter->second;
 }
 
-websocket_route& websocket_route::reg(const std::string& in_name, const map_actin_type& in_function) {
+websocket_route& websocket_route::reg(const std::string& in_name, const call_fun_type& in_function) {
   actions_[in_name] = in_function;
   return *this;
 }
