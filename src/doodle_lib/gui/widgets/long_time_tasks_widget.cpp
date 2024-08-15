72ddee433d40cee7cfb664a67af2ec660f0b0f9c
uuiid <957714080@qq.com>
2024-08-15 15:45:53 +0800

    [自动灯光] 美化日志

--- src/doodle_lib/gui/widgets/long_time_tasks_widget.cpp
+++ src/doodle_lib/gui/widgets/long_time_tasks_widget.cpp
@@ -31,14 +31,100 @@ boost::asio::awaitable<void> weite_tasks_info() {
       co_await boost::asio::this_coro::executor
   };
   while ((co_await boost::asio::this_coro::cancellation_state).cancelled() == boost::asio::cancellation_type::none) {
-    std::string l_info{};
-    for (const auto&& [e, msg] : g_reg()->view<process_message>().each()) {
-      l_info += fmt::format("{} {} \n", msg.get_name(), g_name.at(msg.get_state()));
+    std::string l_info{
+        R"(
+
+<style>
+    html,
+    body {
+        height: 100%;
     }
-    if (!l_info.empty()) {
+
+    body {
+        margin: 0;
+        background: linear-gradient(45deg, #49a09d, #5f2c82);
+        font-family: sans-serif;
+        font-weight: 100;
+    }
+
+    .container {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    table {
+        width: 800px;
+        border-collapse: collapse;
+        overflow: hidden;
+        box-shadow: 0 0 20px rgba(0,0,0,0.1);
+    }
+
+    th,
+    td {
+        padding: 15px;
+        background-color: rgba(255,255,255,0.2);
+        color: #fff;
+    }
+
+    th {
+        text-align: left;
+    }
+
+    thead {
+        th {
+            background-color: #55608f;
+        }
+    }
+
+    tbody {
+        tr {
+            &:hover {
+                background-color: rgba(255,255,255,0.3);
+            }
+        }
+        td {
+            position: relative;
+            &:hover {
+                &:before {
+                    content: "";
+                    position: absolute;
+                    left: 0;
+                    right: 0;
+                    top: -9999px;
+                    bottom: -9999px;
+                    background-color: rgba(255,255,255,0.2);
+                    z-index: -1;
+                }
+            }
+        }
+    }
+</style>
+<div class="container" role="region" tabindex="0">
+  <table>
+    <thead>
+    <tr>
+      <th>名称</th>
+      <th>状态</th>
+    </tr>
+    </thead>
+    <tbody>
+)"
+    };
+    bool has_value{false};
+    for (const auto&& [e, msg] : g_reg()->view<process_message>().each()) {
+      l_info += fmt::format("<tr> <td>{}</td> <td>{}</td> </tr>", msg.get_name(), g_name.at(msg.get_state()));
+      has_value |= true;
+    }
+    l_info += R"(</tbody>
+  </table>
+  <div style="margin-top:8px">Made with <a href="https://www.htmltables.io/" target="_blank">HTML Tables</a></div>
+</div>)";
+    if (has_value) {
       FSys::path l_path{
           FSys::get_cache_path("process_info") /
-          fmt::format("{:%Y_%m_%d_%H_%M_%S}_{}.txt", g_p, boost::this_process::get_id())
+          fmt::format("{:%Y_%m_%d_%H_%M_%S}_{}.html", g_p, boost::this_process::get_id())
       };
       FSys::ofstream{l_path, std::ios::out | std::ios::trunc} << l_info;
     }
