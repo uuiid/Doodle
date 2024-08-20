06c378509a623a522010f6557afa3615cb32e1ef
uuiid <957714080@qq.com>
2024-08-20 14:42:31 +0800

    [自动灯光] 添加连接自定义数据

--- src/doodle_lib/http_method/kitsu/kitsu.h
+++ src/doodle_lib/http_method/kitsu/kitsu.h
@@ -3,14 +3,11 @@
 //
 
 #pragma once
+#include <doodle_core/core/http_client_core.h>
 namespace doodle::http {
 
 struct kitsu_data_t {
-
-
-
-
-
+  std::shared_ptr<detail::http_client_data_base> http_kitsu_{};
 };
 
 }  // namespace doodle::http
