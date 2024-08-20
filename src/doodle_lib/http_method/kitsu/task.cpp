0360e0926493207051aeda229b2df1a2da6d6137
uuiid <957714080@qq.com>
2024-08-20 14:32:05 +0800

    [自动灯光] 添加kitsu 劫持 task url

--- src/doodle_lib/http_method/kitsu/task.cpp
+++ src/doodle_lib/http_method/kitsu/task.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "task.h"
+namespace doodle::http {
+
+namespace {
+boost::asio::awaitable<boost::beast::http::message_generator> get_task_info(session_data_ptr in_handle) {
+  boost::uuids::uuid l_task_id;
+  try {
+    l_task_id = boost::lexical_cast<boost::uuids::uuid>(in_handle->capture_->get("task_id"));
+  } catch (...) {
+    in_handle->logger_->warn("无效的任务id {}", boost::current_exception_diagnostic_information());
+    co_return in_handle->make_error_code_msg(boost::beast::http::status::bad_request, "无效的任务id");
+  }
+
+
+
+  co_return in_handle->make_msg(std::string{});
+}
+}  // namespace
+void kitsu_task_reg(http_route& in_http_route) {
+  in_http_route.reg(
+      std::make_shared<http_function>(boost::beast::http::verb::get, "api/doodle/task/{task_id}", get_task_info)
+  );
+}
+}  // namespace doodle::http
