82bc66abd1f6deb19800c239e101a8dc18e932af
uuiid <957714080@qq.com>
2024-08-26 10:05:53 +0800

    [自动灯光] 调整更新显示错误面板

--- src/exe_gui/doodle_updaters.cpp
+++ src/exe_gui/doodle_updaters.cpp
@@ -6,21 +6,26 @@
 #include <Windows.h>
 std::wstring to_wstr(const std::string& in_str) {
   std::wstring l_ret{};
-  int convertResult = MultiByteToWideChar(CP_UTF8, 0, in_str.c_str(), (int)strlen(in_str.c_str()), NULL, 0);
+  int convertResult = ::MultiByteToWideChar(CP_UTF8, 0, in_str.c_str(), (int)strlen(in_str.c_str()), NULL, 0);
   if (convertResult > 0) {
     l_ret.resize(convertResult);
     convertResult =
-        MultiByteToWideChar(CP_UTF8, 0, in_str.c_str(), (int)strlen(in_str.c_str()), &l_ret[0], l_ret.size());
+        ::MultiByteToWideChar(CP_UTF8, 0, in_str.c_str(), (int)strlen(in_str.c_str()), &l_ret[0], l_ret.size());
     if (convertResult <= 0) {
-      l_ret = TEXT("未知错误");
+      l_ret = TEXT("未知编码,错误消息无法显示");
     }
+  } else {
+    l_ret = TEXT("未知编码,错误消息无法显示");
   }
   return l_ret;
 }
 
+void show_error(const std::string& in_str) {
+  ::MessageBoxW(nullptr, to_wstr(in_str).c_str(), L"错误", MB_OK | MB_ICONERROR);
+}
+
 extern "C" int main(int argc, const char* const argv[]) try {
+
 } catch (...) {
-  ::MessageBox(
-      nullptr, to_wstr(boost::current_exception_diagnostic_information()).c_str(), L"错误", MB_OK | MB_ICONERROR
-  );
+  show_error(boost::current_exception_diagnostic_information());
 }
