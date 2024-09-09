edce3ba5f6796f32869eb92949d432d93812d8ef
uuiid <957714080@qq.com>
2024-09-09 14:46:05 +0800

    [自动灯光] 添加测试上下文初始化

--- src/test/core/run_check.cpp
+++ src/test/core/run_check.cpp
@@ -3,6 +3,8 @@
 //
 #include <doodle_app/app/app_command.h>
 
+#include "doodle_lib/exe_warp/maya_exe.h"
+#include "doodle_lib/exe_warp/ue_exe.h"
 #include <doodle_lib/exe_warp/check_files.h>
 
 #include <boost/asio.hpp>
@@ -17,6 +19,8 @@ BOOST_AUTO_TEST_CASE(base) {
   FSys::path l_check{boost::unit_test::framework::master_test_suite().argv[1]};
   // 将测试的服务器地址改为测试ip
   core_set::get_set().server_ip = "http://192.168.40.180:50026";
+  g_ctx().emplace<maya_ctx>();
+  g_ctx().emplace<ue_ctx>();
   boost::asio::co_spawn(
       g_io_context(), check_files(l_check, spdlog::default_logger()),
       [](std::exception_ptr e, std::tuple<boost::system::error_code, std::string> res) { app_base::Get().stop_app(); }
