b64d6de9c30f093cf142d2371e811455ec8a683d
uuiid <957714080@qq.com>
2024-09-19 14:27:08 +0800

    [自动灯光] 调整保存

--- src/doodle_core/metadata/scan_data_t.cpp
+++ src/doodle_core/metadata/scan_data_t.cpp
@@ -9,117 +9,47 @@
 #include <sqlite_orm/uuid_to_blob.h>
 namespace doodle {
 
-void scan_data_t::ue_path(const FSys::path& in_path) {
-  BOOST_ASSERT(handle_);
+void scan_data_t::seed_to_sql(const entt::registry& in_registry, const std::vector<entt::entity>& in_entity) {
+  for (auto&& l_entity : in_entity) {
+    if (!in_registry.valid(l_entity)) continue;
 
-  auto l_uuid = FSys::software_flag_file(in_path);
-  if (handle_.any_of<additional_data>()) {
-    handle_.patch<additional_data>().ue_path_ = in_path;
-  } else {
-    handle_.emplace<additional_data>().ue_path_ = in_path;
-  }
-  if (auto& l_s = handle_.registry()->storage<uuid>(detail::ue_path_id); l_s.contains(handle_)) {
-    l_s.patch(handle_) = l_uuid;
-  } else {
-    l_s.emplace(handle_, l_uuid);
-  }
-}
-std::tuple<uuid, FSys::path> scan_data_t::ue_path() const {
-  BOOST_ASSERT(handle_);
-  if (auto& l_s = handle_.registry()->storage<uuid>(detail::ue_path_id);
-      l_s.contains(handle_) && handle_.all_of<additional_data>()) {
-    return std::make_tuple(l_s.get(handle_), handle_.get<additional_data>().ue_path_);
-  }
-  return std::make_tuple(uuid{}, FSys::path{});
-}
-
-void scan_data_t::rig_path(const FSys::path& in_path) {
-  BOOST_ASSERT(handle_);
-  auto l_uuid = FSys::software_flag_file(in_path);
-  if (handle_.any_of<additional_data>()) {
-    handle_.patch<additional_data>().rig_path_ = in_path;
-  } else {
-    handle_.emplace<additional_data>().rig_path_ = in_path;
-  }
-  if (auto& l_s = handle_.registry()->storage<uuid>(detail::rig_path_id); l_s.contains(handle_)) {
-    l_s.patch(handle_) = l_uuid;
-  } else {
-    l_s.emplace(handle_, l_uuid);
-  }
-}
-
-std::tuple<uuid, FSys::path> scan_data_t::rig_path() const {
-  BOOST_ASSERT(handle_);
-  if (auto& l_s = handle_.registry()->storage<uuid>(detail::rig_path_id);
-      l_s.contains(handle_) && handle_.all_of<additional_data>()) {
-    return std::make_tuple(l_s.get(handle_), handle_.get<additional_data>().rig_path_);
-  }
-  return std::make_tuple(uuid{}, FSys::path{});
-}
-
-void scan_data_t::solve_path(const FSys::path& in_path) {
-  BOOST_ASSERT(handle_);
-  auto l_uuid = FSys::software_flag_file(in_path);
-  if (handle_.any_of<additional_data>()) {
-    handle_.patch<additional_data>().solve_path_ = in_path;
-  } else {
-    handle_.emplace<additional_data>().solve_path_ = in_path;
-  }
-  if (auto& l_s = handle_.registry()->storage<uuid>(detail::solve_path_id); l_s.contains(handle_)) {
-    l_s.patch(handle_) = l_uuid;
-  } else {
-    l_s.emplace(handle_, l_uuid);
-  }
-}
-
-std::tuple<uuid, FSys::path> scan_data_t::solve_path() const {
-  BOOST_ASSERT(handle_);
-  if (auto& l_s = handle_.registry()->storage<uuid>(detail::solve_path_id);
-      l_s.contains(handle_) && handle_.all_of<additional_data>()) {
-    return std::make_tuple(l_s.get(handle_), handle_.get<additional_data>().solve_path_);
-  }
-  return std::make_tuple(uuid{}, FSys::path{});
-}
-
-void scan_data_t::destroy() {
-  BOOST_ASSERT(handle_);
-  if (auto& l_s = handle_.registry()->storage<entt::id_type>(detail::sql_id);
-      l_s.contains(handle_) && l_s.get(handle_) != 0)
-    g_ctx().get<sqlite_database>().destroy<scan_data_t>(l_s.get(handle_));
-  handle_.destroy();
-}
-void scan_data_t::seed_to_sql() {
-  if (handle_) {
+    entt::const_handle l_handle{in_registry, l_entity};
     database_t l_ret{};
-    if (auto& l_s = handle_.registry()->storage<uuid>(detail::ue_path_id); l_s.contains(handle_)) {
-      l_ret.ue_uuid_ = l_s.get(handle_);
-    }
-    if (auto& l_s = handle_.registry()->storage<uuid>(detail::rig_path_id); l_s.contains(handle_)) {
-      l_ret.rig_uuid_ = l_s.get(handle_);
-    }
-    if (auto& l_s = handle_.registry()->storage<uuid>(detail::solve_path_id); l_s.contains(handle_)) {
-      l_ret.solve_uuid_ = l_s.get(handle_);
-    }
-    if (auto& l_s = handle_.registry()->storage<entt::id_type>(detail::project_ref_id); l_s.contains(handle_)) {
-      entt::entity l_prj_ett{l_s.get(handle_)};
-      if (auto& l_prj_s = handle_.registry()->storage<uuid>(detail::project_id);
-          l_prj_ett != entt::null && l_prj_s.contains(l_prj_ett)) {
-        l_ret.project_ = l_prj_s.get(l_prj_ett);
-      }
-    }
-    auto& l_a         = handle_.get<additional_data>();
+    auto& l_id        = l_handle.get<path_uuid>();
+    l_ret.ue_uuid_    = l_id.ue_uuid_;
+    l_ret.rig_uuid_   = l_id.rig_uuid_;
+    l_ret.solve_uuid_ = l_id.solve_uuid_;
+
+    if (auto l_h = entt::to_entity(*in_registry.storage<project>(), *l_handle.get<project_ptr>().project_);
+        l_h != entt::null)
+      l_ret.project_ = in_registry.storage<uuid>(detail::project_id)->get(l_h);
+    auto& l_a         = l_handle.get<additional_data>();
     l_ret.ue_path_    = l_a.ue_path_.generic_string();
     l_ret.rig_path_   = l_a.rig_path_.generic_string();
     l_ret.solve_path_ = l_a.solve_path_.generic_string();
     l_ret.num_        = l_a.num_;
     l_ret.name_       = l_a.name_;
     l_ret.version_    = l_a.version_;
+    l_ret.id_         = to_entity(l_entity);
 
-    if (auto& l_s = handle_.registry()->storage<std::int32_t>(detail::sql_id); l_s.contains(handle_)) {
-      l_ret.id_ = l_s.get(handle_);
-    }
     g_ctx().get<sqlite_database>()(std::move(l_ret));
   }
+}
+
+void scan_data_t::dependent_uuid(entt::registry& in_reg, entt::entity in_entity) {
+  auto& l_data = in_reg.get<additional_data>(in_entity);
+
+  auto& l_uuid = in_reg.get_or_emplace<path_uuid>(in_entity);
+  try {
+    l_uuid.rig_uuid_   = FSys::software_flag_file(l_data.rig_path_);
+    l_uuid.ue_uuid_    = FSys::software_flag_file(l_data.ue_path_);
+    l_uuid.solve_uuid_ = FSys::software_flag_file(l_data.solve_path_);
+  } catch (...) {
+    default_logger_raw()->error(boost::current_exception_diagnostic_information());
+  }
+}
+void scan_data_t::on_destroy(entt::registry& in_reg, entt::entity in_entity) {
+  g_ctx().get<sqlite_database>().destroy<scan_data_t>(in_reg.storage<entt::id_type>(detail::sql_id).get(in_entity));
 }
 
 std::vector<entt::entity> scan_data_t::load_from_sql(
@@ -129,43 +59,37 @@ std::vector<entt::entity> scan_data_t::load_from_sql(
   if (in_data.empty()) return l_create;
 
   in_registry.create(l_create.begin(), l_create.end());
-
+  g_ctx().erase<scan_data_ctx_t>();
   {
-    auto& l_s = in_registry.storage<uuid>(detail::ue_path_id);
-    std::vector<uuid> l_vec =
-        in_data | ranges::views::transform([](const auto& in_db) { return in_db.ue_uuid_; }) | ranges::to_vector;
-    l_s.insert(l_create.begin(), l_create.end(), l_vec.begin());
+    std::vector<path_uuid> l_vec = in_data | ranges::views::transform([](const database_t& in_db) {
+                                     return path_uuid{in_db.ue_uuid_, in_db.rig_uuid_, in_db.solve_uuid_};
+                                   }) |
+                                   ranges::to_vector;
+    in_registry.insert<path_uuid>(l_create.begin(), l_create.end(), l_vec.begin());
   }
-  {
-    auto& l_s = in_registry.storage<uuid>(detail::rig_path_id);
-    std::vector<uuid> l_vec =
-        in_data | ranges::views::transform([](const auto& in_db) { return in_db.rig_uuid_; }) | ranges::to_vector;
-    l_s.insert(l_create.begin(), l_create.end(), l_vec.begin());
-  }
-  {
-    auto& l_s = in_registry.storage<uuid>(detail::solve_path_id);
-    std::vector<uuid> l_vec =
-        in_data | ranges::views::transform([](const auto& in_db) { return in_db.solve_uuid_; }) | ranges::to_vector;
-    l_s.insert(l_create.begin(), l_create.end(), l_vec.begin());
-  }
-
   std::map<uuid, entt::entity> l_prj_map{};
   for (auto&& [l_e, l_id] : entt::view<entt::get_t<uuid>>{in_registry.storage<uuid>(detail::project_id)}.each()) {
     l_prj_map.emplace(l_id, l_e);
   }
+  {
+    std::vector<project_ptr> l_vec = in_data | ranges::views::transform([&](const database_t& in_db) {
+                                       return project_ptr{in_registry.try_get<project>(l_prj_map[in_db.project_])};
+                                     }) |
+                                     ranges::to_vector;
+    in_registry.insert<project_ptr>(l_create.begin(), l_create.end(), l_vec.begin());
+  }
+
   std::vector<additional_data> l_vec = in_data | ranges::views::transform([&](const database_t& in_db) {
-                                         return additional_data{
-                                             in_db.ue_path_,
-                                             in_db.rig_path_,
-                                             in_db.solve_path_,
-                                             in_db.num_,
-                                             in_db.name_,
-                                             in_db.version_,
-                                             in_registry.try_get<project>(l_prj_map[in_db.project_])
-                                         };
+                                         return additional_data{in_db.ue_path_, in_db.rig_path_, in_db.solve_path_,
+                                                                in_db.num_,     in_db.name_,     in_db.version_};
                                        }) |
                                        ranges::to_vector;
   in_registry.insert<additional_data>(l_create.begin(), l_create.end(), l_vec.begin());
+  auto& l_ctx    = g_ctx().emplace<scan_data_ctx_t>();
+  l_ctx.conn_[0] = in_registry.on_construct<additional_data>().connect<&dependent_uuid>();
+  l_ctx.conn_[1] = in_registry.on_update<additional_data>().connect<&dependent_uuid>();
+  l_ctx.conn_[2] = in_registry.storage<entt::id_type>(detail::sql_id).on_destroy().connect<&on_destroy>();
+
   return l_create;
 }
 
--- src/doodle_core/metadata/scan_data_t.h
+++ src/doodle_core/metadata/scan_data_t.h
@@ -10,8 +10,21 @@
 namespace doodle {
 
 class scan_data_t {
+  static void dependent_uuid(entt::registry& in_reg, entt::entity in_entity);
+  static void on_destroy(entt::registry& in_reg, entt::entity in_entity);
+
  public:
   entt::handle handle_{};
+
+  struct scan_data_ctx_t {
+    std::array<entt::scoped_connection, 3> conn_;
+  };
+
+  struct path_uuid {
+    uuid ue_uuid_;
+    uuid rig_uuid_;
+    uuid solve_uuid_;
+  };
 
   struct additional_data {
     FSys::path ue_path_;
@@ -20,8 +33,6 @@ class scan_data_t {
     std::string name_;
     std::string version_;
     std::string num_;
-
-    project* project_;
   };
 
   struct database_t {
@@ -42,20 +53,8 @@ class scan_data_t {
 
   scan_data_t() = default;
   /// 获取函数
-  explicit scan_data_t(const entt::handle& in_h) : handle_(in_h) {}
-  explicit scan_data_t(entt::registry& in_registry) : handle_(in_registry, in_registry.create()) {
-    in_registry.storage<entt::id_type>(detail::sql_id).emplace(handle_);
-  }
 
-  void ue_path(const FSys::path& in_path);
-  std::tuple<uuid, FSys::path> ue_path() const;
-  void rig_path(const FSys::path& in_path);
-  std::tuple<uuid, FSys::path> rig_path() const;
-  void solve_path(const FSys::path& in_path);
-  std::tuple<uuid, FSys::path> solve_path() const;
-
-  void seed_to_sql();
-  void destroy();
+  static void seed_to_sql(const entt::registry& in_registry, const std::vector<entt::entity>& in_entity);
   static std::vector<entt::entity> load_from_sql(entt::registry& in_registry, const std::vector<database_t>& in_data);
 };
 
