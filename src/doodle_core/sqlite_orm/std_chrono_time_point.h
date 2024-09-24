99a2894c7510b06d1ec4fac28f76637c5e2ce9cf
uuiid <957714080@qq.com>
2024-09-24 16:47:22 +0800

    [自动灯光] 调整编译错误

--- src/doodle_core/sqlite_orm/std_chrono_time_point.h
+++ src/doodle_core/sqlite_orm/std_chrono_time_point.h
@@ -8,25 +8,25 @@
 #include <sqlite_orm/sqlite_orm.h>
 namespace sqlite_orm {
 
-template <typename Clock, typename Duration = typename Clock::duration>
+template <typename Clock, typename Duration>
 struct type_printer<std::chrono::time_point<Clock, Duration>>
     : std::conditional_t<std::is_floating_point_v<typename Duration::rep>, real_printer, integer_printer> {};
 
-template <typename Clock, typename Duration = typename Clock::duration>
+template <typename Clock, typename Duration>
 struct statement_binder<std::chrono::time_point<Clock, Duration>> : statement_binder<typename Duration::rep> {
   int bind(sqlite3_stmt* stmt, int index, const std::chrono::time_point<Clock, Duration>& value) const {
     return statement_binder<typename Duration::rep>::bind(stmt, index, value.count());
   }
 };
 
-template <typename Clock, typename Duration = typename Clock::duration>
+template <typename Clock, typename Duration>
 struct field_printer<std::chrono::time_point<Clock, Duration>> : field_printer<typename Duration::rep> {
   typename Duration::rep operator()(const std::chrono::time_point<Clock, Duration>& value) const {
     return field_printer<typename Duration::rep>::operator()(value.count());
   }
 };
 
-template <typename Clock, typename Duration = typename Clock::duration>
+template <typename Clock, typename Duration>
 struct row_extractor<std::chrono::time_point<Clock, Duration>> : row_extractor<typename Duration::rep> {
   std::chrono::time_point<Clock, Duration> extract(sqlite3_stmt* stmt, int columnIndex) const {
     // static std::locale g_utf_8_locale{"UTF-8"};
