b3ec43ebb8deb2bff2abb2ffa4f08d08bf2b1dc2
uuiid <957714080@qq.com>
2024-09-24 16:48:14 +0800

    [自动灯光] 调整数据库orm

--- src/doodle_core/metadata/work_xlsx_task_info.h
+++ src/doodle_core/metadata/work_xlsx_task_info.h
@@ -22,31 +22,29 @@ class work_xlsx_task_info {
 
 class work_xlsx_task_info_block {
  public:
-  // work_xlsx_task_info_block()  = default;
-  // ~work_xlsx_task_info_block() = default;
-
   std::vector<work_xlsx_task_info> task_info_;
   boost::uuids::uuid id_;
   chrono::year_month year_month_;
   entt::entity user_refs_;
   chrono::microseconds duration_;
-
-  // 传入user的uuid 和 实体对应数据
-  static std::vector<work_xlsx_task_info_block> select_all(
-      const sql_connection_ptr& in_comm, const std::map<boost::uuids::uuid, entt::entity>& in_map_id
-  );
-  static void create_table(const sql_connection_ptr& in_comm);
-
-  // 过滤已经存在的任务
-  static std::vector<bool> filter_exist(
-      const sql_connection_ptr& in_comm, const std::vector<work_xlsx_task_info_block>& in_task
-  );
-  static void insert(
-      const sql_connection_ptr& in_comm, const std::vector<work_xlsx_task_info_block>& in_task,
-      const std::map<entt::entity, boost::uuids::uuid>& in_map_id
-  );
-  static void update(const sql_connection_ptr& in_comm, const std::vector<work_xlsx_task_info_block>& in_task);
-  static void delete_by_ids(const sql_connection_ptr& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
 };
+
+namespace work_xlsx_task_info_helper {
+struct database_t{
+  using zoned_time = chrono::zoned_time<chrono::microseconds>;
+  std::int32_t id_{};
+  uuid uuid_id_{};
+  zoned_time start_time_;
+  zoned_time end_time_;
+  chrono::microseconds duration_;
+  std::optional<std::string> remark_;       // 程序标注
+  std::optional<std::string> user_remark_;  // 用户备注
+
+  chrono::days year_month_;
+  std::int32_t user_id_;
+
+  boost::uuids::uuid kitsu_task_ref_id_;
+};
+}  // namespace work_xlsx_task_info_helper
 
 }  // namespace doodle
