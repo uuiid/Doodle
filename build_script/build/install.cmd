5f3b5cbe5adcf256472fc3cecd544b452bae636d
uuiid <957714080@qq.com>
2024-09-25 15:30:59 +0800

    [自动灯光] 调整安装

--- build_script/build/install.cmd
+++ build_script/build/install.cmd
@@ -5,7 +5,7 @@ echo ----------------- build --------------------
 --workflow --preset build_release%Doodle_suffix%
 
 echo -----------------copy file--------------------
-robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.exe
-robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.7z
+robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.exe /np /nfl
+robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.7z /np /nfl
 
 
