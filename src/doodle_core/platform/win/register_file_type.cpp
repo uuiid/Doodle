1ec15e553a4edb66928f199f33f24eb8290126ab
uuiid <957714080@qq.com>
2024-09-25 13:43:23 +0800

    [自动灯光] 添加新的项目

--- src/doodle_core/platform/win/register_file_type.cpp
+++ src/doodle_core/platform/win/register_file_type.cpp
@@ -79,7 +79,11 @@ const std::vector<project>& register_file_type::get_project_list() {
       project{
           "我的师兄太强了", R"(\\192.168.10.242\public\WDSXTQL)", "WDSXTQL", "WD", R"(C:\sy\WDSXTQL)",
           R"(\\192.168.10.240\public\后期\WDSXTQL\)"
-      }
+      },
+      project{
+        "医妃", R"(\\192.168.10.242\public\WDSXTQL)", "WDSXTQL", "YF", R"(C:\sy\WDSXTQL)",
+        R"(\\192.168.10.240\public\后期\WDSXTQL\)"
+    }
 
   };
   return l_list;
