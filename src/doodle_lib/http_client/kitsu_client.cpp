5c541a015b959139e64a4b2450ab9024f4526f61
uuiid <957714080@qq.com>
2024-09-28 16:55:42 +0800

    [自动灯光] 添加项目查询和类别查询

--- src/doodle_lib/http_client/kitsu_client.cpp
+++ src/doodle_lib/http_client/kitsu_client.cpp
@@ -1,5 +1,6 @@
 #include "kitsu_client.h"
 
+#include <doodle_core/metadata/kitsu/task_type.h>
 namespace doodle::kitsu {
 boost::asio::awaitable<std::tuple<boost::system::error_code, kitsu_client::task>> kitsu_client::get_task(
     const boost::uuids::uuid& in_uuid
@@ -49,26 +50,57 @@ boost::asio::awaitable<std::tuple<boost::system::error_code, kitsu_client::user_
   co_return std::make_tuple(l_e, l_user);
 }
 
-boost::asio::awaitable<tl::expected<nlohmann::json, std::string>> kitsu_client::get_user_ctx() {
-  tl::expected<nlohmann::json, std::string> l_ret{};
-  auto&& [l_ec, l_res1] = co_await http::detail::read_and_write<boost::beast::http::string_body>(
-      http_client_core_ptr_, header_operator_req(boost::beast::http::request<boost::beast::http::empty_body>{
-                                 boost::beast::http::verb::get, "/api/auth/authenticated", 11
-                             })
-  );
-  if (l_ec) l_ret = tl::make_unexpected(l_ec.what());
-  header_operator_resp(l_res1);
-
-  boost::beast::http::request<boost::beast::http::empty_body> req{
-      boost::beast::http::verb::get, "/api/data/user/context", 11
-  };
+boost::asio::awaitable<tl::expected<std::vector<project_helper::database_t>, std::string>>
+kitsu_client::get_all_project() {
   auto&& [l_e, l_res] = co_await http::detail::read_and_write<boost::beast::http::string_body>(
-      http_client_core_ptr_, header_operator_req(std::move(req))
+      http_client_core_ptr_, header_operator_req(std::move(boost::beast::http::request<boost::beast::http::empty_body>{
+                                 boost::beast::http::verb::get, "/api/data/projects/all", 11
+                             }))
   );
+  tl::expected<std::vector<project_helper::database_t>, std::string> l_ret{};
   if (l_e)
     l_ret = tl::make_unexpected(l_e.what());
-  else
-    l_ret = nlohmann::json::parse(l_res.body());
+  else {
+    header_operator_resp(l_res);
+    std::vector<project_helper::database_t> l_list{};
+    for (auto&& l_prj : nlohmann::json::parse(l_res.body())) {
+      l_list
+          .emplace_back(project_helper::database_t{
+              .uuid_id_          = core_set::get_set().get_uuid(),
+              .name_             = l_prj["name"].get<std::string>(),
+              .path_             = "C:/sy",
+              .local_path_       = "C:/sy",
+              .auto_upload_path_ = "C:/sy",
+              .kitsu_uuid_       = l_prj["id"].get<uuid>(),
+          })
+          .generate_names();
+    }
+    l_ret = std::move(l_list);
+  }
+  co_return l_ret;
+}
+boost::asio::awaitable<tl::expected<std::vector<metadata::kitsu::task_type_t>, std::string>>
+kitsu_client::get_all_task_type() {
+  auto&& [l_e, l_res] = co_await http::detail::read_and_write<boost::beast::http::string_body>(
+      http_client_core_ptr_, header_operator_req(std::move(boost::beast::http::request<boost::beast::http::empty_body>{
+                                 boost::beast::http::verb::get, "/api/data/task-types", 11
+                             }))
+  );
+  tl::expected<std::vector<metadata::kitsu::task_type_t>, std::string> l_ret{};
+  if (l_e)
+    l_ret = tl::make_unexpected(l_e.what());
+  else {
+    header_operator_resp(l_res);
+    std::vector<metadata::kitsu::task_type_t> l_list{};
+    for (auto&& l_prj : nlohmann::json::parse(l_res.body())) {
+      l_list.emplace_back(metadata::kitsu::task_type_t{
+          .uuid_id_    = core_set::get_set().get_uuid(),
+          .name_       = l_prj["name"].get<std::string>(),
+          .kitsu_uuid_ = l_prj["id"].get<uuid>(),
+      });
+    }
+    l_ret = std::move(l_list);
+  }
   co_return l_ret;
 }
 
--- src/doodle_lib/http_client/kitsu_client.h
+++ src/doodle_lib/http_client/kitsu_client.h
@@ -8,6 +8,11 @@
 #include <boost/algorithm/string.hpp>
 
 #include <tl/expected.hpp>
+
+namespace doodle::metadata::kitsu {
+struct task_type_t;
+}
+
 namespace doodle::kitsu {
 class kitsu_client;
 
@@ -73,7 +78,8 @@ class kitsu_client {
   };
 
   boost::asio::awaitable<std::tuple<boost::system::error_code, user_t>> get_user(const boost::uuids::uuid& in_uuid);
-  boost::asio::awaitable<tl::expected<nlohmann::json, std::string>> get_user_ctx();
+  boost::asio::awaitable<tl::expected<std::vector<project_helper::database_t>, std::string>> get_all_project();
+  boost::asio::awaitable<tl::expected<std::vector<metadata::kitsu::task_type_t>, std::string>> get_all_task_type();
 };
 
 using kitsu_client_ptr = std::shared_ptr<kitsu_client>;
