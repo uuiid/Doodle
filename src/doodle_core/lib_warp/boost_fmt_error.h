d74296a316021fc08427d7c38d19f8c8176932b2
uuiid <957714080@qq.com>
2024-10-09 09:48:03 +0800

    [自动灯光] 调整错误格式化

--- src/doodle_core/lib_warp/boost_fmt_error.h
+++ src/doodle_core/lib_warp/boost_fmt_error.h
@@ -3,18 +3,18 @@
 //
 
 #pragma once
+#include <boost/system.hpp>
+
 #include <fmt/format.h>
 #include <fmt/ostream.h>
-
-#include <boost/system.hpp>
 
 namespace fmt {
 template <>
 struct formatter<::boost::system::error_code> : formatter<string_view> {
   template <typename FormatContext>
   auto format(const ::boost::system::error_code& in_, FormatContext& ctx) const -> decltype(ctx.out()) {
-    return formatter<string_view>::format(in_.what(), ctx
-    );
+    auto l_str = in_.message() + '[' + in_.to_string() + ']';
+    return formatter<string_view>::format(l_str, ctx);
   }
 };
-}
+}  // namespace fmt
