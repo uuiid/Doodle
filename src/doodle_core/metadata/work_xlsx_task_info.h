5d1096a22155290cfce5a49e1c77a669bdded51c
uuiid <957714080@qq.com>
2024-10-14 16:49:38 +0800

    [自动灯光] 调整格式化上下文

--- src/doodle_core/metadata/work_xlsx_task_info.h
+++ src/doodle_core/metadata/work_xlsx_task_info.h
@@ -20,12 +20,12 @@ struct database_t {
   boost::uuids::uuid kitsu_task_ref_id_;
   // to json
   friend void to_json(nlohmann::json& j, const database_t& p) {
-    j["id"]                = fmt::to_string(p.id_);
-    j["start_time"]        = fmt::format("{:%FT%T}", p.start_time_.get_local_time());
-    j["end_time"]          = fmt::format("{:%FT%T}", p.end_time_.get_local_time());
-    j["duration"]          = p.duration_.count();
-    j["remark"]            = p.remark_;
-    j["user_remark"]       = p.user_remark_;
+    j["id"]         = fmt::to_string(p.id_);
+    j["start_time"] = fmt::format("{:%FT%T}", p.start_time_.get_local_time());
+    j["end_time"]   = fmt::format("{:%FT%T}", p.end_time_.get_local_time());
+    j["duration"]   = p.duration_.count();
+    if (p.remark_) j["remark"] = *p.remark_;
+    if (p.user_remark_) j["user_remark"] = *p.user_remark_;
     j["kitsu_task_ref_id"] = fmt::to_string(p.kitsu_task_ref_id_);
   }
 };
