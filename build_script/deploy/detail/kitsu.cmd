ae78a7c6b90fc846dc23a28ce17e39493582dff0
uuiid <957714080@qq.com>
2024-10-15 13:30:52 +0800

    [自动灯光] 调整部署

--- build_script/deploy/detail/kitsu.cmd
+++ build_script/deploy/detail/kitsu.cmd
@@ -3,13 +3,10 @@ set DoodleSource=//192.168.20.89/Doodle2/build/Ninja_release/_CPack_Packages/win
 
 
 @REM 停止服务
-net stop doodle_file_association_http
-net stop doodle_file_exists
-
+net stop doodle_kitsu_supplement
 @REM 复制文件
 robocopy %DoodleSource% D:/doodle_exe/ /MIR
-
 @REM 启动服务
-net start doodle_file_association_http
-net start doodle_file_exists
-
+net start doodle_kitsu_supplement
+@REM 复制前端界面
+robocopy \\192.168.20.89\kitsu\dist D:/doodle_exe/ /MIR
