d22c7eb68100ad23c5ac0d0a370f496c0091eaeb
uuiid <957714080@qq.com>
2024-10-24 17:23:26 +0800

    [kitsu] 调整测试存在属性

--- src/doodle_lib/http_method/kitsu/task.cpp
+++ src/doodle_lib/http_method/kitsu/task.cpp
@@ -31,22 +31,22 @@ boost::asio::awaitable<boost::beast::http::message_generator> get_task_info_full
     auto l_task_type_name = l_json["task_type"]["name"];
     if (l_task_type_name == "角色" || l_task_type_name == "地编模型" || l_task_type_name == "绑定") {
       auto l_user_data = l_json["entity"]["data"];
-      scan::scan_key_t l_key{
-          .dep_     = conv_assets_type_enum(l_json["dep"]["name"]),
-          .season_  = season{l_user_data["gui_dang"].get<std::int32_t>()},
-          .project_ = find_project(l_json["project"]["name"]).uuid_id_,
-          .number_  = l_user_data.contains("bian_hao") ? l_user_data["bian_hao"].get<std::string>() : std::string{},
-          .name_    = l_user_data["pin_yin_ming_cheng"].get<std::string>(),
-          .version_name_ = l_user_data.contains("ban_ben") ? l_user_data["ban_ben"].get<std::string>() : std::string{},
-      };
-      FSys::path l_maya_path{};
-      FSys::path l_ue_path{};
-      auto& l_map = g_ctx().get<std::shared_ptr<scan_win_service_t>>()->get_scan_data_key();
-      if (l_map.contains(l_key)) {
-        l_maya_path = l_map.at(l_key)->rig_file_.path_;
-        l_ue_path   = l_map.at(l_key)->ue_file_.path_;
-      }
-      l_file_exist = FSys::exists(l_maya_path) && FSys::exists(l_ue_path);
+      if (l_user_data.contains("gui_dang") &&            //
+          l_user_data.contains("bian_hao") &&            //
+          l_user_data.contains("pin_yin_ming_cheng") &&  //
+          l_user_data.contains("ban_ben")) {
+        scan::scan_key_t l_key{
+            .dep_          = conv_assets_type_enum(l_json["dep"]["name"]),
+            .season_       = season{std::stoi(l_user_data["gui_dang"].get<std::string>())},
+            .project_      = find_project(l_json["project"]["name"]).uuid_id_,
+            .number_       = l_user_data["bian_hao"].get<std::string>(),
+            .name_         = l_user_data["pin_yin_ming_cheng"].get<std::string>(),
+            .version_name_ = l_user_data["ban_ben"].get<std::string>(),
+        };
+        auto& l_map  = g_ctx().get<std::shared_ptr<scan_win_service_t>>()->get_scan_data_key();
+        l_file_exist = l_map.contains(l_key);
+      } else
+        l_file_exist = false;
     } else {
       l_file_exist = true;
     }
@@ -80,28 +80,26 @@ boost::asio::awaitable<boost::beast::http::message_generator> get_task_with_task
                 l_json_task["task_type_id"].get<uuid>()
             );
             !l_p.empty() && l_p.front().use_chick_files) {
-          scan::scan_key_t l_key{
-              .dep_ = conv_assets_type_enum(l_asset_type_name),
-              .season_ =
-                  season{l_user_data.contains("gui_dang") ? std::stoi(l_user_data["gui_dang"].get<std::string>()) : 0},
-              .project_ = l_prj_id,
-              .number_  = l_user_data.contains("bian_hao") ? l_user_data["bian_hao"].get<std::string>() : std::string{},
-              .name_ = l_user_data.contains("pin_yin_ming_cheng") ? l_user_data["pin_yin_ming_cheng"].get<std::string>()
-                                                                  : std::string{},
-              .version_name_ =
-                  l_user_data.contains("ban_ben") ? l_user_data["ban_ben"].get<std::string>() : std::string{},
-          };
-          FSys::path l_maya_path{};
-          FSys::path l_ue_path{};
-          if (l_map.contains(l_key)) {
-            l_maya_path = l_map.at(l_key)->rig_file_.path_;
-            l_ue_path   = l_map.at(l_key)->ue_file_.path_;
-          }
-          // l_file_exist = FSys::exists(l_maya_path) && FSys::exists(l_ue_path);
-          l_file_exist = l_map.contains(l_key);
-        } else {
+          if (l_user_data.contains("gui_dang") &&            //
+              l_user_data.contains("bian_hao") &&            //
+              l_user_data.contains("pin_yin_ming_cheng") &&  //
+              l_user_data.contains("ban_ben") &&             //
+              !l_user_data["gui_dang"].get<std::string>().empty()
+          )
+            l_file_exist = l_map.contains(
+                scan::scan_key_t{
+                    .dep_          = conv_assets_type_enum(l_asset_type_name),
+                    .season_       = season{std::stoi(l_user_data["gui_dang"].get<std::string>())},
+                    .project_      = l_prj_id,
+                    .number_       = l_user_data["bian_hao"].get<std::string>(),
+                    .name_         = l_user_data["pin_yin_ming_cheng"].get<std::string>(),
+                    .version_name_ = l_user_data["ban_ben"].get<std::string>(),
+                }
+            );
+          else
+            l_file_exist = false;
+        } else
           l_file_exist = true;
-        }
         l_json_task["file_exist"] = l_file_exist;
       }
     }
@@ -116,12 +114,16 @@ boost::asio::awaitable<boost::beast::http::message_generator> get_task_with_task
 void task_reg(http_route& in_http_route) {
   in_http_route
 
-      .reg(std::make_shared<http_function>(
-          boost::beast::http::verb::get, "api/doodle/task/{task_id}/full", get_task_info_full
-      ))
-      .reg(std::make_shared<http_function>(
-          boost::beast::http::verb::get, "api/data/assets/with-tasks", get_task_with_tasks
-      ))
+      .reg(
+          std::make_shared<http_function>(
+              boost::beast::http::verb::get, "api/doodle/task/{task_id}/full", get_task_info_full
+          )
+      )
+      .reg(
+          std::make_shared<http_function>(
+              boost::beast::http::verb::get, "api/data/assets/with-tasks", get_task_with_tasks
+          )
+      )
 
       ;
 }
