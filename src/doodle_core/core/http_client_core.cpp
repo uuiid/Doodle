0f04a24d5f133fc17d541157b300f0cc60895c4a
uuiid <957714080@qq.com>
2024-11-05 17:59:06 +0800

    [kitsu] 调整关闭

--- src/doodle_core/core/http_client_core.cpp
+++ src/doodle_core/core/http_client_core.cpp
@@ -92,7 +92,7 @@ void http_client_data_base::do_close() {
       entt::overloaded{
           [this](socket_ptr& in_socket) {
             boost::system::error_code ec;
-            in_socket->socket().close();
+            in_socket->socket().close(ec);
             in_socket->socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
             if (ec && ec != boost::beast::errc::not_connected) {
               logger_->log(log_loc(), level::err, "do_close error: {}", ec.message());
