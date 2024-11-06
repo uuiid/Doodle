6c2d78a72af52e7b7a0549c0967af69706a976bb
uuiid <957714080@qq.com>
2024-11-06 11:32:08 +0800

    [kitsu] 调整初始化文件搜索服务

--- src/doodle_lib/launch/file_association_http.cpp
+++ src/doodle_lib/launch/file_association_http.cpp
@@ -7,6 +7,7 @@
 #include <doodle_core/core/app_service.h>
 #include <doodle_core/database_task/sqlite_client.h>
 #include <doodle_core/platform/win/register_file_type.h>
+#include <doodle_core/sqlite_orm/sqlite_database.h>
 
 #include <doodle_lib/core/http/http_listener.h>
 #include <doodle_lib/core/http/http_route.h>
@@ -19,17 +20,12 @@
 
 namespace doodle::launch {
 struct file_association_http_args_t {
-  std::string kitsu_ip_;
-  std::string kitsu_port_;
+  FSys::path db_path_{};
   std::uint16_t port_;
-
-  std::string kitsu_token_{};
 
   // form json
   friend void from_json(const nlohmann::json& in_json, file_association_http_args_t& out_obj) {
-    if (in_json.contains("kitsu_ip")) in_json.at("kitsu_ip").get_to(out_obj.kitsu_ip_);
-    if (in_json.contains("kitsu_port")) in_json.at("kitsu_port").get_to(out_obj.kitsu_port_);
-    if (in_json.contains("kitsu_token")) in_json.at("kitsu_token").get_to(out_obj.kitsu_token_);
+    if (in_json.contains("db_path")) in_json.at("db_path").get_to(out_obj.db_path_);
     in_json.at("port").get_to(out_obj.port_);
   }
 };
@@ -39,7 +35,7 @@ bool file_association_http_t::operator()(const argh::parser& in_arh, std::vector
   auto l_scan = g_ctx().emplace<std::shared_ptr<scan_win_service_t>>(std::make_shared<scan_win_service_t>());
   l_scan->start();
 
-  file_association_http_args_t l_args{.kitsu_ip_ = "192.168.40.182", .kitsu_port_ = "80", .port_ = 50026};
+  file_association_http_args_t l_args{.db_path_ = "D:/file.database", .port_ = 50026};
 
   if (auto l_file_path = in_arh({"config"}); l_file_path) {
     auto l_json = nlohmann::json::parse(FSys::ifstream{FSys::from_quotation_marks(l_file_path.str())});
@@ -52,6 +48,8 @@ bool file_association_http_t::operator()(const argh::parser& in_arh, std::vector
   }
   default_logger_raw()->log(log_loc(), level::warn, "开始服务器");
 
+  g_ctx().emplace<sqlite_database>().load(l_args.db_path_);
+
   auto l_rout_ptr = std::make_shared<http::http_route>();
   default_logger_raw()->log(log_loc(), level::warn, "开始路由");
   http::reg_file_association_http(*l_rout_ptr);
@@ -59,4 +57,4 @@ bool file_association_http_t::operator()(const argh::parser& in_arh, std::vector
   default_logger_raw()->log(log_loc(), level::warn, "启动侦听器");
   return false;
 }
-} // namespace doodle::launch
+}  // namespace doodle::launch
