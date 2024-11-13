eef2af8b1ba5547ea8c4eda3bfec6fc7f8f0f628
uuiid <957714080@qq.com>
2024-11-13 11:33:22 +0800

    [kitsu] 调整复制

--- src/doodle_core/sqlite_orm/detail/macro.h
+++ src/doodle_core/sqlite_orm/detail/macro.h
@@ -29,8 +29,6 @@
     }                                                                                                                \
   };
 
-
-
 #define DOODLE_GET_BY_PARENT_ID_SQL(class_name)                                                \
   template <>                                                                                  \
   std::vector<class_name> sqlite_database::get_by_parent_id<class_name>(const uuid& in_uuid) { \
@@ -58,7 +56,7 @@
 #define DOODLE_INSTALL_SQL(class_name)                                              \
   template <>                                                                       \
   boost::asio::awaitable<tl::expected<void, std::string>> sqlite_database::install( \
-      std::shared_ptr<class_name> in_data                                           \
+      const std::shared_ptr<class_name>& in_data                                    \
   ) {                                                                               \
     return impl_->install<class_name>(in_data);                                     \
   }
@@ -66,7 +64,7 @@
 #define DOODLE_INSTALL_RANGE(class_name)                                                  \
   template <>                                                                             \
   boost::asio::awaitable<tl::expected<void, std::string>> sqlite_database::install_range( \
-      std::shared_ptr<std::vector<class_name>> in_data                                    \
+      const std::shared_ptr<std::vector<class_name>>& in_data                             \
   ) {                                                                                     \
     return impl_->install_range<class_name>(in_data);                                     \
   }
@@ -74,7 +72,7 @@
 #define DOODLE_REMOVE_RANGE(class_name)                                                        \
   template <>                                                                                  \
   boost::asio::awaitable<tl::expected<void, std::string>> sqlite_database::remove<class_name>( \
-      std::shared_ptr<std::vector<std::int64_t>> in_data                                       \
+      const std::shared_ptr<std::vector<std::int64_t>>& in_data                                \
   ) {                                                                                          \
     return impl_->remove<class_name>(in_data);                                                 \
   }
@@ -82,7 +80,7 @@
 #define DOODLE_REMOVE_BY_UUID(class_name)                                                      \
   template <>                                                                                  \
   boost::asio::awaitable<tl::expected<void, std::string>> sqlite_database::remove<class_name>( \
-      std::shared_ptr<uuid> in_data                                                            \
+      const std::shared_ptr<uuid>& in_data                                                     \
   ) {                                                                                          \
     return impl_->remove<class_name>(in_data);                                                 \
   }
