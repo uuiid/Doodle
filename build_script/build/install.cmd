1c10f47cd7cd61457c9b0d689c5a58e5a1b716b5
uuiid <957714080@qq.com>
2024-11-18 17:19:27 +0800

    [kitsu] 调整打包格式

--- build_script/build/install.cmd
+++ build_script/build/install.cmd
@@ -6,6 +6,6 @@ echo ----------------- build --------------------
 
 echo -----------------copy file--------------------
 robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.exe /np /nfl
-robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.7z /np /nfl
+robocopy %my_pwd%\build\Ninja_release \\192.168.0.67\soft\TD软件\doodle *.zip /np /nfl
 
 
