944928c4161d93c893c3a54620e238254a51cbb8
uuiid <957714080@qq.com>
2024-11-21 17:41:17 +0800

    [kitsu]  去除无用的运行程序

--- src/exe_gui/auto_light_server.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by td_main on 2023/9/11.
-//
-#include <doodle_core/core/app_service.h>
-
-#include <doodle_lib/launch/auto_light_server_lau.h>
-
-#include "main_macro.h"
-DOODLE_MAIN_IMPL(doodle::launch::auto_light_server_lau);
