23b6421a5aa5f35d205f1444eeb12eb37968e854
uuiid <957714080@qq.com>
2024-11-27 16:26:12 +0800

    [kitsu] 去除sql依赖

--- src/doodle_core/metadata/kitsu_file_path.h
+++ src/doodle_core/metadata/kitsu_file_path.h
@@ -12,19 +12,5 @@ class kitsu_file_path {
   boost::uuids::uuid id_;
   FSys::path path_{};
 
- public:
-  static std::vector<kitsu_file_path> select_all(
-      const sql_connection_ptr& in_comm, const std::map<boost::uuids::uuid, entt::entity>& in_map_id
-  );
-  static void create_table(const sql_connection_ptr& in_comm);
-
-  // 过滤已经存在的任务
-  static std::vector<bool> filter_exist(const sql_connection_ptr& in_comm, const std::vector<kitsu_file_path>& in_task);
-  static void insert(
-      const sql_connection_ptr& in_comm, const std::vector<kitsu_file_path>& in_task,
-      const std::map<entt::entity, boost::uuids::uuid>& in_map_id
-  );
-  static void update(const sql_connection_ptr& in_comm, const std::vector<kitsu_file_path>& in_task);
-  static void delete_by_ids(const sql_connection_ptr& in_comm, const std::vector<boost::uuids::uuid>& in_ids);
 };
 }  // namespace doodle
--- src/doodle_core/metadata/work_xlsx_task_info.cpp
+++ src/doodle_core/metadata/work_xlsx_task_info.cpp
@@ -1,10 +1,6 @@
 #include "work_xlsx_task_info.h"
 
-#include <doodle_core/lib_warp/sqlite3/sqlite3.h>
 
-#include <sqlpp11/sqlite3/sqlite3.h>
-#include <sqlpp11/sqlpp11.h>
-//
 #include "doodle_core/metadata/user.h"
 namespace doodle {
 
