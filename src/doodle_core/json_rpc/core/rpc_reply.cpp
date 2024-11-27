cd7b26a2e8c62aed0b73e1e4091f361ef4266bc4
uuiid <957714080@qq.com>
2024-11-27 17:48:58 +0800

    [kitsu] 去除json rpc类

--- src/doodle_core/json_rpc/core/rpc_reply.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by TD on 2022/4/29.
-//
-#include "rpc_reply.h"
-namespace doodle::json_rpc {}
--- src/doodle_core/json_rpc/json_rpc_static_value.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by TD on 2022/5/16.
-//
-#include "json_rpc_static_value.h"
-namespace doodle::json_rpc::rpc_fun_name {
-
-}  // namespace doodle::json_rpc::rpc_fun_name
