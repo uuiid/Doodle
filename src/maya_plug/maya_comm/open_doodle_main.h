20a3aa70bd22b98f1945b38bba376e81a73dd69e
uuiid <957714080@qq.com>
2024-11-27 13:57:45 +0800

    [kitsu] 调整 gui打开

--- src/maya_plug/maya_comm/open_doodle_main.h
+++ src/maya_plug/maya_comm/open_doodle_main.h
@@ -4,15 +4,4 @@
 
 #pragma once
 
-#include <maya_plug/main/maya_plug_fwd.h>
-namespace doodle::maya_plug {
-namespace {
-constexpr char doodleCreate_name[] = "doodleCreate";
-}
-class open_doodle_main : public TemplateAction<open_doodle_main, doodleCreate_name> {
- public:
-  open_doodle_main();
-  ~open_doodle_main() override;
-  virtual MStatus doIt(const MArgList& list) override;
-};
-}  // namespace doodle::maya_plug
+
