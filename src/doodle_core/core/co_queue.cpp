11cd919ec65015c673271e3c6598854c6cf0d80f
uuiid <957714080@qq.com>
2024-12-03 11:58:57 +0800

    [kitsu] 调整启动

--- src/doodle_core/core/co_queue.cpp
+++ src/doodle_core/core/co_queue.cpp
@@ -13,19 +13,17 @@ void awaitable_queue_limitation::awaitable_queue_impl::await_suspend(std::functi
   }
 }
 
-
 void awaitable_queue_limitation::awaitable_queue_impl::next() {
   --await_task_count_;
   ++run_task_;
   const std::lock_guard l{lock_};
   next_list_.front()();
   next_list_.pop();
-
 }
 
 void awaitable_queue_limitation::awaitable_queue_impl::maybe_invoke() {
   if (await_task_count_ == 0) return;
   if (run_task_ >= limit_) return;
-  next();
+  while (await_task_count_ != 0 && run_task_ < limit_) next();
 }
-}
+}  // namespace doodle
