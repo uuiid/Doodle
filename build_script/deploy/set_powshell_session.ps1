ae6376623d1cd4abf47ba55e412783651773933c
uuiid <957714080@qq.com>
2024-12-06 14:21:54 +0800

    [kitsu] 添加远程

--- build_script/deploy/set_powshell_session.ps1
+++ build_script/deploy/set_powshell_session.ps1
@@ -3,6 +3,6 @@
 #Set-Item WSMan:\localhost\Service\Auth\Basic -Value True
 #Set-Item WSMan:\localhost\Client\AllowUnencrypted -Value True
 #Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True
-
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList auto_light,root
+$RootPassword = ConvertTo-SecureString "root" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList auto_light,$RootPassword
 Enter-PSSession -ComputerName 192.168.40.181 -Credential $Credential -Authentication Basic
