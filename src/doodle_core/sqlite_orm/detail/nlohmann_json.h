613565318df38c10fea3229d96ab332b05ef2e5d
uuiid <957714080@qq.com>
2024-12-25 16:48:27 +0800

    [kitsu] 调整自定义类型

---
+++ src/doodle_core/sqlite_orm/detail/nlohmann_json.h
@@ -0,0 +1,33 @@
+//
+// Created by TD on 24-12-25.
+//
+
+#pragma once
+#include <nlohmann/json.hpp>
+#include <sqlite_orm/sqlite_orm.h>
+namespace sqlite_orm {
+
+template <>
+struct type_printer<nlohmann::json> : public text_printer {};
+
+template <>
+struct statement_binder<nlohmann::json> : public statement_binder<std::string> {
+  int bind(sqlite3_stmt* stmt, int index, const nlohmann::json& value) const {
+    auto l_json_str = value.dump();
+    return statement_binder<std::string>::bind(stmt, index, l_json_str);
+  }
+};
+
+template <>
+struct field_printer<nlohmann::json> : field_printer<std::string> {
+  std::string operator()(const nlohmann::json& value) const { return '{' + value.dump() + '}'; }
+};
+
+template <>
+struct row_extractor<nlohmann::json> : row_extractor<std::string> {
+  nlohmann::json extract(sqlite3_stmt* stmt, int columnIndex) const {
+    const auto l_str = row_extractor<std::string>::extract(stmt, columnIndex);
+    return nlohmann::json::parse(l_str);
+  }
+};
+}  // namespace sqlite_orm
