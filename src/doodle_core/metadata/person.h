e2a2caf4001f8b5ee842d204e85aad2e06a2804b
uuiid <957714080@qq.com>
2024-12-26 15:42:17 +0800

    [kitsu] 添加人员类

--- src/doodle_core/metadata/person.h
+++ src/doodle_core/metadata/person.h
@@ -14,4 +14,77 @@ enum class person_role_type {
   client,
   vendor,
 };
-}
+
+// enum class contract_types {
+//   open_ended,
+//   fixed_term,
+//   short_term,
+//   freelance,
+//   apprentice,
+//   internship,
+// };
+
+enum class two_factor_authentication_types {
+  totp,
+  email_otp,
+  fido,
+};
+
+struct person_department_link {
+  std::int64_t id_;
+  uuid person_id_;
+  uuid department_id_;
+};
+
+struct DOODLE_CORE_API person {
+  DOODLE_BASE_FIELDS();
+
+  std::string first_name_;
+  std::string last_name_;
+  std::string email_;
+  std::string phone_;
+  std::string contract_type_;
+
+  bool active_;
+  bool archived_;
+  chrono::system_zoned_time last_presence_;
+
+  std::string password_;
+  std::string desktop_login_;
+  std::int32_t login_failed_attemps_;
+  chrono::system_zoned_time last_login_failed_;
+  bool totp_enabled_;
+  std::string totp_secret_;
+  bool email_otp_enabled_;
+  std::string email_otp_secret_;
+  bool fido_enabled_;
+  std::string fido_credentials_;
+  std::string otp_recovery_codes_;
+  two_factor_authentication_types preferred_two_factor_authentication_;
+
+  std::int32_t shotgun_id_;
+  std::string timezone_;
+  std::string locale_;
+  std::string data_;
+  person_role_type role_;
+  bool has_avatar_;
+
+  bool notifications_enabled_;
+  bool notifications_slack_enabled_;
+  std::string notifications_slack_userid_;
+  bool notifications_mattermost_enabled_;
+  std::string notifications_mattermost_userid_;
+  bool notifications_discord_enabled_;
+  std::string notifications_discord_userid_;
+
+  bool is_bot_;
+  std::string jti_;
+  chrono::system_zoned_time expiration_date_;
+
+  std::vector<uuid> departments_;
+  uuid studio_id_;
+
+  bool is_generated_from_ldap_;
+  std::string ldap_uid_;
+};
+}  // namespace doodle
