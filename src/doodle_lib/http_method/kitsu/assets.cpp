63b8bcd3fc5a6f574a73e3c94b10536e31d9edad
uuiid <957714080@qq.com>
2024-12-30 14:38:03 +0800

    [kitsu] 添加资产创建劫持

---
+++ src/doodle_lib/http_method/kitsu/assets.cpp
@@ -0,0 +1,23 @@
+//
+// Created by TD on 24-12-30.
+//
+
+#include "assets.h"
+
+#include <doodle_core/metadata/kitsu/task_type.h>
+#include <doodle_core/metadata/user.h>
+#include <doodle_core/sqlite_orm/sqlite_database.h>
+
+#include <doodle_lib/core/http/http_function.h>
+#include <doodle_lib/core/http/json_body.h>
+#include <doodle_lib/http_client/dingding_client.h>
+#include <doodle_lib/http_method/kitsu/kitsu.h>
+namespace doodle::http::kitsu {
+namespace {
+boost::asio::awaitable<boost::beast::http::message_generator> config(session_data_ptr in_handle){
+  }
+}
+void assets_reg(http_route& in_http_route){
+
+}
+}
