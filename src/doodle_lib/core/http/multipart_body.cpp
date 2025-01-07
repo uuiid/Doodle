77b9c31f91c46b2f496be97188a200365a2bc9ad
uuiid <957714080@qq.com>
2025-01-07 14:11:43 +0800

    [kitsu] 添加多部分解析器初步版本

--- src/doodle_lib/core/http/multipart_body.cpp
+++ src/doodle_lib/core/http/multipart_body.cpp
@@ -1,5 +1,356 @@
 //
 // Created by TD on 25-1-7.
 //
-
+#ifdef DOODLE_TEST_1
 #include "multipart_body.h"
+/* Based on node-formidable by Felix Geisendörfer
+ * Igor Afonov - afonov@gmail.com - 2012
+ * MIT License - http://www.opensource.org/licenses/mit-license.php
+ */
+#ifndef _multipart_parser_h
+#define _multipart_parser_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <ctype.h>
+#include <stdlib.h>
+
+typedef struct multipart_parser multipart_parser;
+typedef struct multipart_parser_settings multipart_parser_settings;
+typedef struct multipart_parser_state multipart_parser_state;
+
+typedef int (*multipart_data_cb)(multipart_parser*, const char* at, size_t length);
+typedef int (*multipart_notify_cb)(multipart_parser*);
+
+struct multipart_parser_settings {
+  multipart_data_cb on_header_field;
+  multipart_data_cb on_header_value;
+  multipart_data_cb on_part_data;
+
+  multipart_notify_cb on_part_data_begin;
+  multipart_notify_cb on_headers_complete;
+  multipart_notify_cb on_part_data_end;
+  multipart_notify_cb on_body_end;
+};
+
+multipart_parser* multipart_parser_init(const char* boundary, const multipart_parser_settings* settings);
+
+void multipart_parser_free(multipart_parser* p);
+
+size_t multipart_parser_execute(multipart_parser* p, const char* buf, size_t len);
+
+void multipart_parser_set_data(multipart_parser* p, void* data);
+void* multipart_parser_get_data(multipart_parser* p);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
+
+/* Based on node-formidable by Felix Geisendörfer
+ * Igor Afonov - afonov@gmail.com - 2012
+ * MIT License - http://www.opensource.org/licenses/mit-license.php
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+static void multipart_log(const char* format, ...) {
+#ifdef DEBUG_MULTIPART
+  va_list args;
+  va_start(args, format);
+
+  fprintf(stderr, "[HTTP_MULTIPART_PARSER] %s:%d: ", __FILE__, __LINE__);
+  vfprintf(stderr, format, args);
+  fprintf(stderr, "\n");
+#endif
+}
+
+#define NOTIFY_CB(FOR)                     \
+  do {                                     \
+    if (p->settings->on_##FOR) {           \
+      if (p->settings->on_##FOR(p) != 0) { \
+        return i;                          \
+      }                                    \
+    }                                      \
+  } while (0)
+
+#define EMIT_DATA_CB(FOR, ptr, len)                  \
+  do {                                               \
+    if (p->settings->on_##FOR) {                     \
+      if (p->settings->on_##FOR(p, ptr, len) != 0) { \
+        return i;                                    \
+      }                                              \
+    }                                                \
+  } while (0)
+
+#define LF 10
+#define CR 13
+
+struct multipart_parser {
+  void* data;
+
+  size_t index;
+  size_t boundary_length;
+
+  unsigned char state;
+
+  const multipart_parser_settings* settings;
+
+  char* lookbehind;
+  char multipart_boundary[1];
+};
+
+enum state {
+  s_uninitialized = 1,
+  s_start,
+  s_start_boundary,
+  s_header_field_start,
+  s_header_field,
+  s_headers_almost_done,
+  s_header_value_start,
+  s_header_value,
+  s_header_value_almost_done,
+  s_part_data_start,
+  s_part_data,
+  s_part_data_almost_boundary,
+  s_part_data_boundary,
+  s_part_data_almost_end,
+  s_part_data_end,
+  s_part_data_final_hyphen,
+  s_end
+};
+
+multipart_parser* multipart_parser_init(const char* boundary, const multipart_parser_settings* settings) {
+  multipart_parser* p = malloc(sizeof(multipart_parser) + strlen(boundary) + strlen(boundary) + 9);
+
+  strcpy(p->multipart_boundary, boundary);
+  p->boundary_length = strlen(boundary);
+
+  p->lookbehind      = (p->multipart_boundary + p->boundary_length + 1);
+
+  p->index           = 0;
+  p->state           = s_start;
+  p->settings        = settings;
+
+  return p;
+}
+
+void multipart_parser_free(multipart_parser* p) { free(p); }
+
+void multipart_parser_set_data(multipart_parser* p, void* data) { p->data = data; }
+
+void* multipart_parser_get_data(multipart_parser* p) { return p->data; }
+
+size_t multipart_parser_execute(multipart_parser* p, const char* buf, size_t len) {
+  size_t i    = 0;
+  size_t mark = 0;
+  char c, cl;
+  int is_last = 0;
+
+  while (i < len) {
+    c       = buf[i];
+    is_last = (i == (len - 1));
+    switch (p->state) {
+      case s_start:
+        multipart_log("s_start");
+        p->index = 0;
+        p->state = s_start_boundary;
+
+      /* fallthrough */
+      case s_start_boundary:
+        multipart_log("s_start_boundary");
+        if (p->index == p->boundary_length) {
+          if (c != CR) {
+            return i;
+          }
+          p->index++;
+          break;
+        } else if (p->index == (p->boundary_length + 1)) {
+          if (c != LF) {
+            return i;
+          }
+          p->index = 0;
+          NOTIFY_CB(part_data_begin);
+          p->state = s_header_field_start;
+          break;
+        }
+        if (i < 2) {
+          break;  // first '--'
+        }
+        if (c != p->multipart_boundary[p->index]) {
+          return i;
+        }
+        p->index++;
+        break;
+
+      case s_header_field_start:
+        multipart_log("s_header_field_start");
+        mark     = i;
+        p->state = s_header_field;
+
+      /* fallthrough */
+      case s_header_field:
+        multipart_log("s_header_field");
+        if (c == CR) {
+          p->state = s_headers_almost_done;
+          break;
+        }
+
+        if (c == ' ' || c == '\t') {
+          p->state = s_header_value_start;
+          break;
+        }
+
+        if (c == '-') {
+          break;
+        }
+
+        if (c == ':') {
+          EMIT_DATA_CB(header_field, buf + mark, i - mark);
+          p->state = s_header_value_start;
+          break;
+        }
+
+        cl = tolower(c);
+        if ((c != '-') && (cl < 'a' || cl > 'z')) {
+          multipart_log("invalid character in header name");
+          return i;
+        }
+        if (is_last) EMIT_DATA_CB(header_field, buf + mark, (i - mark) + 1);
+        break;
+
+      case s_headers_almost_done:
+        multipart_log("s_headers_almost_done");
+        if (c != LF) {
+          return i;
+        }
+
+        p->state = s_part_data_start;
+        break;
+
+      case s_header_value_start:
+        multipart_log("s_header_value_start");
+        if (c == ' ' || c == '\t') {
+          break;
+        }
+
+        mark     = i;
+        p->state = s_header_value;
+
+      /* fallthrough */
+      case s_header_value:
+        multipart_log("s_header_value");
+        if (c == CR) {
+          EMIT_DATA_CB(header_value, buf + mark, i - mark);
+          p->state = s_header_value_almost_done;
+          break;
+        }
+        if (is_last) EMIT_DATA_CB(header_value, buf + mark, (i - mark) + 1);
+        break;
+
+      case s_header_value_almost_done:
+        multipart_log("s_header_value_almost_done");
+        if (c != LF) {
+          return i;
+        }
+        p->state = s_header_field_start;
+        break;
+
+      case s_part_data_start:
+        multipart_log("s_part_data_start");
+        NOTIFY_CB(headers_complete);
+        mark     = i;
+        p->state = s_part_data;
+
+      /* fallthrough */
+      case s_part_data:
+        multipart_log("s_part_data");
+        if (c == CR && i >= len - p->boundary_length - 6) {
+          EMIT_DATA_CB(part_data, buf + mark, i - mark);
+          mark             = i;
+          p->state         = s_part_data_almost_boundary;
+          p->lookbehind[0] = CR;
+          break;
+        }
+        if (is_last) EMIT_DATA_CB(part_data, buf + mark, (i - mark) + 1);
+        break;
+
+      case s_part_data_almost_boundary:
+        multipart_log("s_part_data_almost_boundary");
+        if (c == LF) {
+          p->state = s_part_data_boundary;
+          i += 2;  // first '--'
+          p->lookbehind[1] = LF;
+          p->index         = 0;
+          break;
+        }
+        EMIT_DATA_CB(part_data, p->lookbehind, 1);
+        p->state = s_part_data;
+        mark     = i--;
+        break;
+
+      case s_part_data_boundary:
+        multipart_log("s_part_data_boundary");
+        if (p->multipart_boundary[p->index] != c) {
+          EMIT_DATA_CB(part_data, p->lookbehind, 2 + p->index);
+          p->state = s_part_data;
+          mark     = i--;
+          break;
+        }
+        p->lookbehind[2 + p->index] = c;
+        if ((++p->index) == p->boundary_length) {
+          NOTIFY_CB(part_data_end);
+          p->state = s_part_data_almost_end;
+        }
+        break;
+
+      case s_part_data_almost_end:
+        multipart_log("s_part_data_almost_end");
+        if (c == '-') {
+          p->state = s_part_data_final_hyphen;
+          break;
+        }
+        if (c == CR) {
+          p->state = s_part_data_end;
+          break;
+        }
+        return i;
+
+      case s_part_data_final_hyphen:
+        multipart_log("s_part_data_final_hyphen");
+        if (c == '-') {
+          NOTIFY_CB(body_end);
+          p->state = s_end;
+          break;
+        }
+        return i;
+
+      case s_part_data_end:
+        multipart_log("s_part_data_end");
+        if (c == LF) {
+          p->state = s_header_field_start;
+          NOTIFY_CB(part_data_begin);
+          break;
+        }
+        return i;
+
+      case s_end:
+        multipart_log("s_end: %02X", (int)c);
+        break;
+
+      default:
+        multipart_log("Multipart parser unrecoverable error");
+        return 0;
+    }
+    ++i;
+  }
+
+  return len;
+}
+
+#endif
--- src/doodle_lib/core/http/multipart_body.h
+++ src/doodle_lib/core/http/multipart_body.h
@@ -3,3 +3,184 @@
 //
 
 #pragma once
+#include <doodle_lib/core/http/http_session_data.h>
+
+#include <boost/asio/ssl/error.hpp>
+#include <boost/beast/http.hpp>
+
+#include <string>
+namespace doodle::http {
+struct multipart_body {
+  struct part_value_type {
+    std::string name{};
+    std::string file_name{};
+    detail::content_type content_type{};
+  };
+  enum class parser_state {
+    uninitialized = 1,
+    start,
+    start_boundary,
+    header_field_start,
+    header_field,
+    headers_almost_done,
+    header_value_start,
+    header_value,
+    header_value_almost_done,
+    part_data_start,
+    part_data,
+    part_data_almost_boundary,
+    part_data_boundary,
+    part_data_almost_end,
+    part_data_end,
+    part_data_final_hyphen,
+    end
+  };
+
+  struct value_type {
+    std::vector<part_value_type> parts_{};
+    std::string boundary_{};
+  };
+  // using value_type = std::vector<part_value_type>;
+
+  class reader {
+    value_type& body_;
+    part_value_type part_;
+    parser_state state_;
+    std::size_t index_{0};
+    std::string header_field_;
+    std::string header_value_;
+    std::optional<std::size_t> length_{0};
+    std::size_t pos_{};
+
+   public:
+    template <bool isRequest, class Fields>
+    explicit reader(boost::beast::http::header<isRequest, Fields>&, value_type& b) : body_(b), state_() {}
+    void init(boost::optional<std::uint64_t> const& length, boost::system::error_code& ec) {
+      state_ = parser_state::start;
+      index_ = 0;
+      ec     = {};
+      if (length) length_ = *length;
+    }
+    void parser_headers() {}
+    void add_data(const char& input) {}
+
+    template <class ConstBufferSequence>
+    std::size_t put(ConstBufferSequence const& buffers, boost::system::error_code& ec) {
+      auto const extra   = boost::beast::buffer_bytes(buffers);
+      ec                 = {};
+      const auto& l_buff = boost::beast::buffers_range_ref(buffers);
+      for (auto l_begin = l_buff.begin(), l_end = l_buff.end(); l_begin != l_end; ++l_begin) {
+        switch (state_) {
+          case parser_state::start:
+            index_ = 0;
+            state_ = parser_state::start_boundary;
+          case parser_state::start_boundary:
+            if (index_ == 0 && (*l_begin == '-' && *(l_begin + 1) == '-')) {
+              ++(++l_begin);  // 跳过 --
+            }
+
+            if (index_ == body_.boundary_.size()) {
+              if (*l_begin != '\r') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+              ++index_;
+              break;
+            }
+            if (index_ == body_.boundary_.size() + 1) {
+              if (*l_begin != '\n') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+              index_ = 0;
+              state_ = parser_state::header_field_start;
+            }
+
+            if (*l_begin != body_.boundary_[index_++])
+              return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            break;
+          case parser_state::header_field_start:
+            state_ = parser_state::header_field;
+          case parser_state::header_field:
+            if (*l_begin == '\r') {
+              state_ = parser_state::headers_almost_done;
+              break;
+            }
+            if (*l_begin == ' ' || *l_begin == '\t' || *l_begin == ':') {
+              state_ = parser_state::header_value_start;
+              break;
+            }
+            if (*l_begin == '-') break;
+            auto bl = std::tolower(*l_begin);
+            if (bl != '-' && (bl < 'a' || bl > 'z') && (bl < '0' || bl > '9'))
+              return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            header_field_ += *l_begin;
+            break;
+
+          case parser_state::headers_almost_done:
+            if (*l_begin == '\n') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            state_ = parser_state::part_data_start;
+            break;
+          case parser_state::header_value_start:
+            if (*l_begin == ' ' || *l_begin == '\t') break;
+            state_ = parser_state::header_value;
+          case parser_state::header_value:
+            if (*l_begin == '\r') {
+              state_ = parser_state::header_value_almost_done;
+              break;
+            }
+            header_value_ += *l_begin;
+            break;
+          case parser_state::header_value_almost_done:
+            if (*l_begin != '\n') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            state_ = parser_state::header_field_start;
+            break;
+          case parser_state::part_data_start:
+            state_ = parser_state::part_data;
+            parser_headers();
+          case parser_state::part_data:
+            if (*l_begin == '\r') {
+              state_ = parser_state::part_data_almost_boundary;
+            }
+            add_data(*l_begin);
+            break;
+          case parser_state::part_data_almost_boundary:
+            if (*l_begin != '\n') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            state_ = parser_state::part_data_boundary;
+            index_ = 0;
+            ++ ++l_begin;
+            break;
+          case parser_state::part_data_boundary:
+            if (*l_begin == body_.boundary_[index_]) {
+              state_ = parser_state::part_data;
+              break;
+            }
+            if (++index_ == body_.boundary_.size()) {
+              state_ = parser_state::part_data_almost_end;
+              break;
+            }
+
+          case parser_state::part_data_almost_end:
+            if (*l_begin == '-') {
+              state_ = parser_state::part_data_final_hyphen;
+              break;
+            }
+            if (*l_begin == '\r') {
+              state_ = parser_state::part_data_end;
+              break;
+            }
+          case parser_state::part_data_final_hyphen:
+            if (*l_begin != '-') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            state_ = parser_state::end;
+            break;
+          case parser_state::part_data_end:
+            if (*l_begin != '\n') return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+            state_ = parser_state::header_field_start;
+            break;
+          case parser_state::end:
+            break;
+
+          default:
+            return ec = make_error_code(boost::system::errc::invalid_argument), 0;
+        }
+        ++pos_;
+      }
+      return extra;
+    }
+  };
+};
+}  // namespace doodle::http
