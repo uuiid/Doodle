41a15470f4a3c3481a3470368468f5c3e415fbc4
uuiid <957714080@qq.com>
2025-01-10 11:32:02 +0800

    [kitsu] 调整外包打包

--- build_script/package_epiboly.ps1
+++ build_script/package_epiboly.ps1
@@ -6,4 +6,6 @@ $DoodleExePath = "E:\source\doodle\dist\doodle.exe"
 $DoodleOut = Convert-Path "$PSScriptRoot/../build"
 Initialize-Doodle -OutPath "$DoodleOut/epiboly"
 Copy-Item $DoodleExePath -Destination "$DoodleOut/epiboly"
-Compress-Archive -Path "$DoodleOut/epiboly/*" -DestinationPath "$DoodleOut/epiboly.zip" -Force
+#Compress-Archive -Path "$DoodleOut/epiboly/*" -DestinationPath "$DoodleOut/epiboly.zip" -Force
+$DoodleName = (Get-ChildItem "$DoodleOut/epiboly/dist/" -Filter "*.zip")[0].Name.Split("-")[1]
+Copy-Item "$DoodleOut/epiboly.zip" -Destination "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName-win64.zip"
