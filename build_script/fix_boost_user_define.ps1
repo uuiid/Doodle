a9d047e7478bcf394cd1dc971809310d3e7660eb
uuiid <957714080@qq.com>
2025-01-11 15:38:30 +0800

    [kitsu] 调整boost 定义位置

---
+++ build_script/fix_boost_user_define.ps1
@@ -0,0 +1,13 @@
+﻿$Root = Convert-Path "$PSScriptRoot/../"
+
+Copy-Item "$Root/vcpkg/ports/boost-config/vcpkg.json" -Destination "$Root/vcpkg_ports/boost-config/vcpkg.json"
+Copy-Item "$Root/vcpkg/ports/boost-config/portfile.cmake" -Destination "$Root/vcpkg_ports/boost-config/portfile.cmake"
+
+
+$RawData = Get-Content "$Root/vcpkg_ports/boost-config/portfile.cmake" -Raw
+
+$RawData = $RawData -replace "\n#undef BOOST_ALL_DYN_LINK\n", "\n#undef BOOST_ALL_DYN_LINK\n#define BOOST_PROCESS_USE_STD_FS\n#define BOOST_DLL_USE_STD_FS\n"
+
+Set-Content -LiteralPath "$Root/vcpkg_ports/boost-config/portfile.cmake" `
+    -Value $RawData `
+    -Encoding UTF8
