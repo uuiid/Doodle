87133dfe380dbb3e87f60645ea4e3eddc68dda41
uuiid <957714080@qq.com>
2025-01-13 17:50:44 +0800

    [kitsu] 调整指针

--- src/doodle_lib/http_method/task_info.cpp
+++ src/doodle_lib/http_method/task_info.cpp
@@ -214,6 +214,7 @@ class run_long_task_local : public std::enable_shared_from_this<run_long_task_lo
     } else if (in_json.contains("category")) {  // 检查文件任务
       auto l_arg_t = std::make_shared<maya_exe_ns::inspect_file_arg>();
       l_arg_t->config(in_json.get<maya_exe_ns::inspect_file_type>());
+      arg_ = l_arg_t;
     } else if (in_json.contains("image_to_move")) {
       auto l_image_to_move_args = std::make_shared<doodle::detail::image_to_move>();
       in_json.get_to(*l_image_to_move_args);
