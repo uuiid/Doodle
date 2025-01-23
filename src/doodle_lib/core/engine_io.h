7c385b80bf1feba431873cfacd52b6f4d7ed8f69
uuiid <957714080@qq.com>
2025-01-23 11:46:39 +0800

    [kitsu] 添加辅助函数

--- src/doodle_lib/core/engine_io.h
+++ src/doodle_lib/core/engine_io.h
@@ -8,6 +8,7 @@
 
 #include <boost/url/url.hpp>
 
+#include <cpp-base64/base64.h>
 namespace doodle::socket_io {
 enum class engine_io_packet_type : std::int8_t { open = 0, close, ping, pong, message, upgrade, noop };
 class DOODLE_CORE_API engine_io {
@@ -30,11 +31,12 @@ inline std::vector<std::string> split_multi_packet(const std::string& in_data) {
   std::vector<std::string> l_vec{};
   return boost::split(l_vec, in_data, boost::is_any_of("\x1e"));
 }
-// inline bool is_binary_packet(const std::string& in_data) { return in_data.front() == 'b'; }
-inline bool is_binary_packet(std::string& in_data) {
-  auto l_is = in_data.front() == 'b';
-  if (l_is) in_data.erase(0, 1);
-  return l_is;
+inline bool is_binary_packet(const std::string& in_data) { return in_data.front() == 'b'; }
+/// 解码二进制包
+inline std::string decode_binary_packet(const std::string& in_data) {
+  if (!is_binary_packet(in_data)) return in_data;
+  std::string_view l_str{in_data.data() + 1, in_data.size() - 1};
+  return base64_decode(l_str);
 }
 
 }  // namespace doodle::socket_io
