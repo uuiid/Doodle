637165ce6d275ceb48d5e60fce4685915d06364f
uuiid <957714080@qq.com>
2025-02-07 17:54:08 +0800

    [kitsu] 调整事件

--- src/doodle_lib/http_method/local/event.cpp
+++ src/doodle_lib/http_method/local/event.cpp
@@ -8,12 +8,10 @@
 #include <doodle_lib/core/http/http_function.h>
 
 namespace doodle::http::local {
-void local_event::event(const socket_io::socket_io_packet& in_packet) {}
 
-std::optional<socket_io::socket_io_packet> local_event::get_last_event() const { return std::nullopt; }
 void local_event_reg(http_route& in_route) {
   g_ctx().emplace<socket_io::sid_ctx>();
-  auto l_event = std::make_shared<local_event>();
+  auto l_event = std::make_shared<socket_io::event_t>();
   socket_io::create_socket_io(in_route, l_event);
 }
 }  // namespace doodle::http::local
--- src/doodle_lib/http_method/local/event.h
+++ src/doodle_lib/http_method/local/event.h
@@ -8,11 +8,5 @@
 
 namespace doodle::http::local {
 
-class local_event : public socket_io::event_base {
- public:
-  std::optional<socket_io::socket_io_packet> get_last_event() const override;
-  void event(const socket_io::socket_io_packet& in_packet) override;
-};
-
 void local_event_reg(http_route& in_route);
 }  // namespace doodle::http::local
