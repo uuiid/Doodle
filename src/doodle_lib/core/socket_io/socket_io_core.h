04e04e52d65a571b5f4a14e4f4d603e74bf24664
uuiid <957714080@qq.com>
2025-02-13 16:16:42 +0800

    [kitsu] 调整函数

--- src/doodle_lib/core/socket_io/socket_io_core.h
+++ src/doodle_lib/core/socket_io/socket_io_core.h
@@ -5,6 +5,8 @@
 #pragma once
 #include <doodle_core/doodle_core_fwd.h>
 
+#include <boost/signals2/signal.hpp>
+
 namespace doodle::socket_io {
 class sid_ctx;
 /// socket 连接
@@ -12,16 +14,29 @@ class socket_io_core : public std::enable_shared_from_this<socket_io_core> {
   uuid sid_;
   std::shared_ptr<sid_ctx> ctx_;
   std::string namespace_;
+  std::shared_ptr<boost::signals2::signal<void(const std::string&, const nlohmann::json&)>> on_message_;
 
  public:
   explicit socket_io_core(
       const std::shared_ptr<sid_ctx>& in_ctx, const std::string& in_namespace, const nlohmann::json& in_json
   );
+  // destructor
+  ~socket_io_core()                                      = default;
+  // copy constructor
+  socket_io_core(const socket_io_core& other)            = default;
+  socket_io_core& operator=(const socket_io_core& other) = default;
+  // move constructor
+  socket_io_core(socket_io_core&& other)                 = default;
+  socket_io_core& operator=(socket_io_core&& other)      = default;
 
   const uuid& get_sid() const { return sid_; }
   const std::string& get_namespace() const { return namespace_; }
   nlohmann::json auth_{};
 
   void emit(const std::string& in_event, const nlohmann::json& in_data);
+  template <typename Solt>
+  auto on_message(Solt&& in_solt) {
+    return on_message_->connect(in_solt);
+  }
 };
 }  // namespace doodle::socket_io
