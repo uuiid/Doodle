b2dd18a5c405232d2f185559c669c552e3aca575
uuiid <957714080@qq.com>
2025-02-14 15:51:23 +0800

    [kitsu] 添加接收消息函数

--- src/doodle_lib/http_method/local/event.cpp
+++ src/doodle_lib/http_method/local/event.cpp
@@ -14,6 +14,11 @@ void local_event_reg(http_route& in_route) {
   auto l_sid_ctx = std::make_shared<socket_io::sid_ctx>();
   l_sid_ctx->on_connect([](const std::shared_ptr<socket_io::socket_io_core>& in_core) {
     in_core->emit("auth", in_core->auth_);
+    in_core->on_message(
+        "message", socket_io::socket_io_core::slot_type{[l_s = std::weak_ptr{in_core}](const nlohmann::json& in_data) {
+                     l_s.lock()->emit("message-back", in_data);
+                   }}.track_foreign(in_core)
+    );
   });
   l_sid_ctx->on("/custom")->on_connect([](const std::shared_ptr<socket_io::socket_io_core>& in_core) {
     in_core->emit("auth", in_core->auth_);
