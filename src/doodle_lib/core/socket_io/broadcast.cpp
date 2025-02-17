e871a54c151ab046a19591b0697acd300fac8afd
uuiid <957714080@qq.com>
2025-02-17 16:51:18 +0800

    [kitsu] 添加广播函数

--- src/doodle_lib/core/socket_io/broadcast.cpp
+++ src/doodle_lib/core/socket_io/broadcast.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "broadcast.h"
+
+#include <doodle_lib/core/socket_io/socket_io_ctx.h>
+#include <doodle_lib/core/socket_io/socket_io_packet.h>
+namespace doodle::socket_io {
+void broadcast(
+    const std::string& in_event, const nlohmann::json& in_data, const std::string& in_namespace,
+    const std::shared_ptr<sid_ctx>& in_ctx
+) {
+  sid_ctx* l_sid = in_ctx.get();
+  if (!l_sid) l_sid = g_ctx().find<sid_ctx>();
+
+  if (!l_sid) return;
+
+  auto l_ptr        = std::make_shared<socket_io_packet>();
+  l_ptr->type_      = socket_io_packet_type::event;
+  l_ptr->namespace_ = in_namespace;
+  if (in_data.is_array()) {
+    l_ptr->json_data_ = in_data;
+    l_ptr->json_data_.insert(l_ptr->json_data_.begin(), in_event);
+  } else {
+    l_ptr->json_data_ = nlohmann::json::array();
+    l_ptr->json_data_.emplace_back(in_event);
+    l_ptr->json_data_.emplace_back(in_data);
+  }
+  l_sid->emit(l_ptr);
+}
+}  // namespace doodle::socket_io
--- src/doodle_lib/core/socket_io/broadcast.h
+++ src/doodle_lib/core/socket_io/broadcast.h
@@ -3,3 +3,14 @@
 //
 
 #pragma once
+
+#include <doodle_lib/doodle_lib_fwd.h>
+
+namespace doodle::socket_io {
+class sid_ctx;
+void broadcast(
+    const std::string& in_event, const nlohmann::json& in_data, const std::string& in_namespace = {},
+    const std::shared_ptr<sid_ctx>& in_ctx = nullptr
+);
+
+}  // namespace doodle::socket_io
