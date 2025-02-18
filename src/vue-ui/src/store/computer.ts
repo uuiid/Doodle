40f864083d7467b0b1b2d381106f83f09958b9bb
uuiid <957714080@qq.com>
2025-02-18 09:03:37 +0800

    [kitsu] 去除ui库

--- src/vue-ui/src/store/computer.ts
+++
@@ -1,7 +0,0 @@
-import superagent from 'superagent';
-
-export default {
-  get() {
-    return superagent.get('/v1/computer');
-  },
-};
--- src/vue-ui/src/store/task.ts
+++
@@ -1,7 +0,0 @@
-import superagent from 'superagent';
-
-export default {
-  get(page: number, page_size: number) {
-    return superagent.get('/v1/task').query({ page, page_size });
-  },
-};
