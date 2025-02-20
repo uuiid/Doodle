e67a4312458b8aa97ea823ba6cadcc5d8c8e5ecf
uuiid <957714080@qq.com>
2025-02-20 15:34:31 +0800

    [kitsu] 调整sql orm 库

--- vcpkg_ports/sqlite-orm/portfile.cmake
+++ vcpkg_ports/sqlite-orm/portfile.cmake
@@ -1,24 +1,24 @@
 # header-only library
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO uuiid/sqlite_orm
-    REF 886beac769f2c1970a96406ae77cfc09acaa83ff
-    SHA512 eb1f36cba13f6259c4a0adad96429c0d5f70a4a714ef214edca99d34f27e2543279ea3a0c2cee3db42f683e3582ee1a0cb768555251b6a97ed050831949ce50f
-    HEAD_REF dev
-    PATCHES 
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO fnc12/sqlite_orm
+        REF v1.9.1
+        SHA512 3e939ddb31e8f03a5f885e459b1ba8040b58e697a715148b829b075d612d1c8a5686ec889155ec9804929e11ec11285a39af3f1eb27a4edf0bcc56c4ee7530b1
+        HEAD_REF master
+        PATCHES
         fix-dependency.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
+        FEATURES
         test BUILD_TESTING
         example BUILD_EXAMPLES
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
         ${FEATURE_OPTIONS}
         -DSQLITE_ORM_ENABLE_CXX_17=ON
         -DSQLITE_ORM_ENABLE_CXX_20=ON
