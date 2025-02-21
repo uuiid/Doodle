121e39a0c65d39846d754489e49499d760049a2a
uuiid <957714080@qq.com>
2025-02-21 15:45:59 +0800

    [kitsu] 调整base64编码库

---
+++ src/doodle_lib/core/base64.cpp
@@ -0,0 +1,7 @@
+//
+// Created by TD on 25-2-21.
+//
+
+#include "base64.h"
+
+#include <cpp-base64/base64.cpp>
---
+++ src/doodle_lib/core/base64.h
@@ -0,0 +1,6 @@
+//
+// Created by TD on 25-2-21.
+//
+
+#pragma once
+#include <cpp-base64/base64.h>
