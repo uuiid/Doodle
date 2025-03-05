b8fff3c7fe5e1ed8aef20fd4eaeb2188218c31ab
uuiid <957714080@qq.com>
2025-03-05 14:10:29 +0800

    [kitsu] 调整测试位置

--- src/test_js/test/test-attendance.js
+++ src/test_js/test/test-attendance.js
@@ -2,17 +2,15 @@ import request from 'superagent';
 import { expect } from 'chai';
 import { v4 as uuidv4 } from 'uuid';
 
-import { io } from 'socket.io-client';
-// globalThis.chai = chai;
 
 import { URL } from './config.js';
 
 const user_id = '69a8d093-dcab-4890-8f9d-c51ef065d03b';
 const create_date = '2025-3-5';
-describe('考勤测试', function() {
-  describe('自定义考勤测试', function() {
+describe('考勤测试', function () {
+  describe('自定义考勤测试', function () {
     var l_id;
-    it('创建考勤', async function() {
+    it('创建考勤', async function () {
 
       const req = await request.post(`${URL}/api/doodle/attendance/${user_id}/custom`).send({
         create_date: create_date,
@@ -27,20 +25,23 @@ describe('考勤测试', function() {
       l_id = req.body.id;
     });
 
-    it('should 获取考勤', async function() {
-        const req = await request.get(`${URL}/api/doodle/attendance/${user_id}/${create_date}`);
-        expect(req.status).to.equal(200);
-        expect(req.body).to.is.an('array');
-        expect(req.body).to.have.length(1);
-        expect(req.body[0]).to.have.keys('id',
-          'start_time', 'end_time', 'remark', 'type', 'is_custom');
-      },
+    it('should 获取考勤', async function () {
+      const req = await request.get(`${URL}/api/doodle/attendance/${user_id}/${create_date}`);
+      expect(req.status).to.equal(200);
+      expect(req.body).to.is.an('array');
+      expect(req.body[0]).to.have.keys('id',
+        'start_time', 'end_time', 'remark', 'type', 'is_custom');
+    },
     );
 
     it('should 修改考勤', () => {
       const req = request.put(`${URL}/api/doodle/attendance/${l_id}`).send({
-        remark: '测试自定义考勤',
+        remark: '测试自定义考勤 modified',
       });
+      expect(req.status).to.equal(200);
+      expect(req.body).to.have.keys('id',
+        'start_time', 'end_time', 'remark', 'type', 'is_custom');
+      expect(req.body.remark).to.equal('测试自定义考勤 modified');
     });
   });
 });
