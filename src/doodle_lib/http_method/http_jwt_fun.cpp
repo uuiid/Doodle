72abbfac60a5334b6dd9fc90c632764b54707a5b
uuiid <957714080@qq.com>
2025-03-06 18:09:26 +0800

    [kitsu] 添加http jwt守护

--- src/doodle_lib/http_method/http_jwt_fun.cpp
+++ src/doodle_lib/http_method/http_jwt_fun.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "http_jwt_fun.h"
+
+#include <doodle_core/metadata/person.h>
+#include <doodle_core/sqlite_orm/sqlite_database.h>
+
+#include <jwt-cpp/jwt.h>
+namespace doodle::http {
+
+void http_jwt_fun::get_person(const session_data_ptr& in_data) {
+  auto l_jwt = in_data->req_header_[boost::beast::http::field::cookie];
+  if (l_jwt.empty()) l_jwt = in_data->req_header_[boost::beast::http::field::authorization];
+
+  if (auto l_it = l_jwt.find("access_token_cookie="); l_it != std::string::npos)
+    l_jwt = l_jwt.substr(l_it, l_jwt.find(';', l_it) - l_it);
+  else if (auto l_it_b = l_jwt.find("Bearer "); l_it_b != std::string::npos)
+    l_jwt = l_jwt.substr(l_it_b, l_jwt.find(' ', l_it_b) - l_it_b);
+
+  auto l_uuid = from_uuid_str(jwt::decode(l_jwt).get_payload_claim("id").as_string());
+  auto& l_sql = g_ctx().get<sqlite_database>();
+  if (l_sql.uuid_to_id<person>(l_uuid) == 0)
+    throw_exception(http_request_error{boost::beast::http::status::unauthorized, "请先登录"});
+  person_ = std::make_shared<person>(g_ctx().get<sqlite_database>().get_by_uuid<person>(l_uuid));
+}
+boost::asio::awaitable<boost::beast::http::message_generator> http_jwt_fun::callback(session_data_ptr in_handle) {
+  get_person(in_handle);
+  return http_function::callback(in_handle);
+}
+}  // namespace doodle::http
--- src/doodle_lib/http_method/http_jwt_fun.h
+++ src/doodle_lib/http_method/http_jwt_fun.h
@@ -3,3 +3,22 @@
 //
 
 #pragma once
+#include <doodle_lib/core/http/http_function.h>
+
+namespace doodle {
+struct person;
+}
+
+namespace doodle::http {
+class http_jwt_fun : public http_function {
+ protected:
+  std::shared_ptr<person> person_;
+
+  void get_person(const session_data_ptr& in_data);
+
+ public:
+  using http_function::http_function;
+
+  boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
+};
+}  // namespace doodle::http
