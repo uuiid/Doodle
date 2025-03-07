8bd0675cd41d30214a7e48ecad999b8498cceb8b
uuiid <957714080@qq.com>
2025-03-07 17:57:39 +0800

    [kitsu] 调整登录

--- src/doodle_lib/http_method/auth/login.cpp
+++ src/doodle_lib/http_method/auth/login.cpp
@@ -4,7 +4,9 @@
 
 #include "login.h"
 
+#include "doodle_core/metadata/organisation.h"
 #include <doodle_core/metadata/person.h>
+#include <doodle_core/sqlite_orm/sqlite_database.h>
 
 #include <doodle_lib/core/http/http_function.h>
 #include <doodle_lib/http_method/http_jwt_fun.h>
@@ -15,19 +17,20 @@ boost::asio::awaitable<boost::beast::http::message_generator> login(session_data
   co_return in_handle->make_msg("{}");
 }
 
-DOODLE_HTTP_FUN(authenticated, get, "auth/authenticated", http_jwt_fun)
+DOODLE_HTTP_FUN(authenticated, get, "api/auth/authenticated", http_jwt_fun)
 boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override {
   nlohmann::json l_r{};
   l_r["authenticated"] = true;
   l_r["user"]          = *person_;
-  l_r["organisation"]  = {};
-  co_return in_handle->make_msg("{}");
+  auto l_org           = g_ctx().get<sqlite_database>().get_all<organisation>();
+  l_r["organisation"]  = l_org.empty() ? organisation::get_default() : l_org.front();
+  co_return in_handle->make_msg(l_r.dump());
 }
 DOODLE_HTTP_FUN_END()
 
 }  // namespace
 void register_login(http_route& in_r) {
-  in_r.reg(std::make_shared<http_function>(boost::beast::http::verb::post, "auth/login", login))
+  in_r.reg(std::make_shared<http_function>(boost::beast::http::verb::post, "api/auth/login", login))
       .reg(std::make_shared<authenticated_get>());
 }
 }  // namespace doodle::http
