dd05d97a601ebb270b7feded5562bd6f426f4014
uuiid <957714080@qq.com>
2025-03-07 11:56:49 +0800

    [kitsu] 添加组织模型

--- src/doodle_core/metadata/organisation.h
+++ src/doodle_core/metadata/organisation.h
@@ -3,3 +3,21 @@
 //
 
 #pragma once
+#include <doodle_core/doodle_core_fwd.h>
+namespace doodle {
+struct DOODLE_CORE_API organisation {
+  std::string name_;
+  std::int32_t hours_by_day_;
+  bool has_avatar_;
+  bool use_original_file_name_;
+  bool timesheets_locked_;
+  bool format_duration_in_hours_;
+  bool hd_by_default_;
+  std::string chat_token_slack_;
+  std::string chat_webhook_mattermost_;
+  std::string chat_token_discord_;
+  std::string dark_theme_by_default_;
+
+
+};
+}  // namespace doodle
