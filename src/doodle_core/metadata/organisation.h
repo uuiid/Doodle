c9d6d71950a0b8234c7b8e4b5302d15620babb12
uuiid <957714080@qq.com>
2025-03-07 17:10:46 +0800

    [kitsu] 调整完成组织

--- src/doodle_core/metadata/organisation.h
+++ src/doodle_core/metadata/organisation.h
@@ -6,18 +6,20 @@
 #include <doodle_core/doodle_core_fwd.h>
 namespace doodle {
 struct DOODLE_CORE_API organisation {
+  DOODLE_BASE_FIELDS();
   std::string name_;
   std::int32_t hours_by_day_;
   bool has_avatar_;
   bool use_original_file_name_;
   bool timesheets_locked_;
-  bool format_duration_in_hours_;
+  std::optional<bool> format_duration_in_hours_;
   bool hd_by_default_;
   std::string chat_token_slack_;
   std::string chat_webhook_mattermost_;
   std::string chat_token_discord_;
   std::string dark_theme_by_default_;
 
+  inline static organisation get_default() { return {.name_ = "Default", .hours_by_day_ = 8}; }
   // from json
   template <typename BasicJsonType>
   friend void from_json(const BasicJsonType& j, organisation& p) {
