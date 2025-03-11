256afa3b90618334cccc08874c879b4f0b7084ea
uuiid <957714080@qq.com>
2025-03-11 16:48:12 +0800

    [kitsu] 添加task

--- src/doodle_core/metadata/task.h
+++ src/doodle_core/metadata/task.h
@@ -7,6 +7,42 @@
 #include <doodle_core/doodle_core_fwd.h>
 #include <doodle_core/metadata/base.h>
 namespace doodle {
+
+struct assignees_table {
+  std::int64_t id_;
+  uuid task_id_;
+  uuid person_id_;
+};
+
 struct DOODLE_CORE_API task {
-        DOODLE_BASE_FIELDS();
-};}
+  DOODLE_BASE_FIELDS();
+  std::string name_{};
+  std::string description_{};
+  std::int32_t priority_;
+  std::int32_t difficulty_;
+  std::float_t duration_;
+  std::float_t estimation_;
+  std::int32_t completion_rate_;
+  std::int32_t retake_count_;
+  std::int32_t sort_order_;
+  chrono::system_zoned_time start_date_;
+  chrono::system_zoned_time due_date_;
+  chrono::system_zoned_time real_start_date_;
+  chrono::system_zoned_time end_date_;
+  chrono::system_zoned_time done_date_;
+  chrono::system_zoned_time last_comment_date_;
+  std::int32_t nb_assets_ready_;
+  nlohmann::json data_;
+  std::int32_t shotgun_id_;
+  uuid last_preview_file_id_;
+
+  // 外键
+  uuid project_id;
+  uuid task_type_id;
+  uuid task_status_id;
+  uuid entity_id;
+  uuid assigner_id;
+  // 一对多
+  std::vector<uuid> assignees_{};
+};
+}  // namespace doodle
