1c0a6b7ce3e09779fd71b405778e18cf05a155ed
uuiid <957714080@qq.com>
2025-03-11 16:24:47 +0800

    [kitsu] 添加task结构

---
+++ src/doodle_core/metadata/task.h
@@ -0,0 +1,12 @@
+//
+// Created by TD on 25-3-11.
+//
+
+#pragma once
+
+#include <doodle_core/doodle_core_fwd.h>
+#include <doodle_core/metadata/base.h>
+namespace doodle {
+struct DOODLE_CORE_API task {
+        DOODLE_BASE_FIELDS();
+};}
