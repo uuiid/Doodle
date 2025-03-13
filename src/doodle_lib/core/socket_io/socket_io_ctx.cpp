20bf775103621a6dff05008d2acd969f7e56bb9f
uuiid <957714080@qq.com>
2025-03-13 13:10:46 +0800

    [kitsu] 调整上下文

--- src/doodle_lib/core/socket_io/socket_io_ctx.cpp
+++ src/doodle_lib/core/socket_io/socket_io_ctx.cpp
@@ -15,7 +15,7 @@ void sid_ctx::signal_type::message(const socket_io_packet_ptr& in_data) {
 }
 
 sid_ctx::sid_ctx()
-    : handshake_data_{.upgrades_ = {transport_type::websocket}, .ping_interval_ = chrono::milliseconds{2500}, .ping_timeout_ = chrono::milliseconds{200}, .max_payload_ = 1000000},
+    : handshake_data_{.upgrades_ = {transport_type::websocket}, .ping_interval_ = chrono::milliseconds{25000}, .ping_timeout_ = chrono::milliseconds{20000}, .max_payload_ = 1000000},
       /// 默认的命名空间
       signal_map_{{std::string{}, std::make_shared<signal_type>()}} {}
 
