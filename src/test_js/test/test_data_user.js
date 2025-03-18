e3c4f28f22056c6b9d58eb07a0a117d71fd6f7e6
uuiid <957714080@qq.com>
2025-03-18 15:35:31 +0800

    [kitsu] 调整测试

--- src/test_js/test/test_data_user.js
+++ src/test_js/test/test_data_user.js
@@ -7,14 +7,24 @@ import { io } from 'socket.io-client';
 // globalThis.chai = chai;
 
 import { URL } from './config.js';
+import jwt_encode from 'jwt-encode';
 
-
-describe('doodle user测试', function () {
-  it('测试鉴权', async function () {
+describe('doodle user测试', function() {
+  it('测试鉴权', async function() {
     try {
       const req = await request.get(`${URL}/api/data/user/context`);
     } catch (e) {
       expect(e.status).to.equal(401);
     }
+  });
+  it('测试登录用户的上下文', async function() {
+    const l_jwt = jwt_encode({
+      'id': 'b5a1383a-8bdc-4a79-ad33-f601fa0fc26d',
+    }, 'secret');
+    const req = await request.get(`${URL}/api/data/user/context`).set(
+      'Cookie', `access_token_cookie=${l_jwt}`,
+    );
+    expect(req.status).to.equal(200);
+    expect(req.body).to.have.keys('user_id', 'name');
   });
 });
