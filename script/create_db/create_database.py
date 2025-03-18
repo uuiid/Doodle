ca5127b7c2883124db7c3837e8830ba0bf908bbb
uuiid <957714080@qq.com>
2025-03-18 17:43:15 +0800

    [kitsu] 基本添加数据库转换

--- script/create_db/create_database.py
+++ script/create_db/create_database.py
@@ -1,13 +1,30 @@
+from os import getenv
+import zou.app
 import zou.app.models.project_status
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+import doodle.project_status
+PASS = getenv("KITSU_PASS")
 
 def main():
-    engine = create_engine("sqlite:///D:\\zou.db")
-    # zou.app.models.project_status.ProjectStatus.metadata.create_all(engine)
-    with Session(engine) as session:
-        session.add(zou.app.models.project_status.ProjectStatus(name="Open2", color="#00FF00"))
-        session.commit()
+    engine = create_engine("sqlite:///D:\\zou2.db", echo=True)
+
+    with zou.app.app.app_context():
+        prj : list[zou.app.models.project_status.ProjectStatus]= zou.app.models.project_status.ProjectStatus.query.all()
+        with Session(engine) as session:
+            doodle.project_status.ProjectStatus.metadata.create_all(engine)
+            for i in prj:
+                prj = doodle.project_status.ProjectStatus(
+                    uuid_id=i.id,
+                    name=i.name,
+                    color=i.color,
+                )
+                session.add(prj)
+            session.commit()
+        print("tset lev")
+        # print(prj)
+
+
 
 if __name__ == "__main__":
     main()
