ffbafbc6d2cb1edf66c596653ee3059ba641d393
uuiid <957714080@qq.com>
2025-03-19 16:06:31 +0800

    [kitsu] 调整基本的数据类型

--- script/create_db/doodle/working_file.py
+++ script/create_db/doodle/working_file.py
@@ -13,37 +13,39 @@ class WorkingFile(BaseMixin):
     Describes the file related to the work done on a given task. It is
     used as source of output files published for a given entity.
     """
-
-    shotgun_id = db.Column(db.Integer(), index=True)
-
-    name = db.Column(db.String(250))
-    description = db.Column(db.Text())
-    comment = db.Column(db.Text())
-    revision = db.Column(db.Integer())
-    size = db.Column(db.Integer())
-    checksum = db.Column(db.Integer())
-    path = db.Column(db.String(400))
-    data = db.Column(JSONB)
-
-    task_id = db.Column(
-        UUIDType(binary=True), db.ForeignKey("task.id"), index=True
+    __tablename__ = "working_file"
+    uuid_id : orm.Mapped[UUIDType] = orm.mapped_column(
+        UUIDType(binary=True), unique=True, nullable=False, index=True
     )
-    entity_id = db.Column(
-        UUIDType(binary=True), db.ForeignKey("entity.id"), index=True
+    shotgun_id = orm.mapped_column(sqlalchemy.Integer(), index=True)
+
+    name = orm.mapped_column(sqlalchemy.String(250))
+    description = orm.mapped_column(sqlalchemy.Text())
+    comment = orm.mapped_column(sqlalchemy.Text())
+    revision = orm.mapped_column(sqlalchemy.Integer())
+    size = orm.mapped_column(sqlalchemy.Integer())
+    checksum = orm.mapped_column(sqlalchemy.Integer())
+    path = orm.mapped_column(sqlalchemy.String(400))
+    data = orm.mapped_column(sqlalchemy.Text())
+
+    task_id = orm.mapped_column(
+        UUIDType(binary=True), sqlalchemy.ForeignKey("task.id"), index=True
     )
-    person_id = db.Column(
-        UUIDType(binary=True), db.ForeignKey("person.id"), index=True
+    entity_id = orm.mapped_column(
+        UUIDType(binary=True), sqlalchemy.ForeignKey("entity.id"), index=True
     )
-    software_id = db.Column(
-        UUIDType(binary=True), db.ForeignKey("software.id"), index=True
+    person_id = orm.mapped_column(
+        UUIDType(binary=True), sqlalchemy.ForeignKey("person.id"), index=True
+    )
+    software_id = orm.mapped_column(
+        UUIDType(binary=True), sqlalchemy.ForeignKey("software.id"), index=True
     )
     outputs = relationship("OutputFile", back_populates="source_file")
 
     __table_args__ = (
-        db.UniqueConstraint(
+        sqlalchemy.UniqueConstraint(
             "name", "task_id", "entity_id", "revision", name="working_file_uc"
         ),
     )
 
-    def __repr__(self):
-        return "<WorkingFile %s>" % self.id
+ 
