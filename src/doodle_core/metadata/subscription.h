c42c71a5503d57d8d2f92d4f1a184641a6ac9f67
uuiid <957714080@qq.com>
2025-03-26 15:03:22 +0800

    [kitsu] 添加订阅结构属性

--- src/doodle_core/metadata/subscription.h
+++ src/doodle_core/metadata/subscription.h
@@ -6,3 +6,13 @@
 
 #include <doodle_core/doodle_core_fwd.h>
 #include <doodle_core/metadata/base.h>
+
+namespace doodle {
+struct DOODLE_CORE_API subscription {
+  DOODLE_BASE_FIELDS();
+  uuid person_id;
+  uuid task_id;
+  uuid entity_id;
+  uuid task_type_id;
+};
+}  // namespace doodle
