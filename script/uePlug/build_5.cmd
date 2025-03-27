332b3beaf8c513616524876f0abe809e5d9461eb
uuiid <957714080@qq.com>
2025-03-27 09:29:29 +0800

    [kitsu] 添加第三方库构建

--- script/uePlug/build_5.cmd
+++ script/uePlug/build_5.cmd
@@ -6,6 +6,7 @@ set l_time=%l_time:.=_%
 set l_time=%l_time: =%
 call %UE_5% BuildPlugin -Plugin=%work_dir%Doodle\Doodle.uplugin -TargetPlatforms=Win64 -Package=%work_dir%%UE_VERSION%\Plugins\Doodle -Rocket
 robocopy %work_dir%%UE_VERSION%\Plugins\Doodle %work_dir%%UE_VERSION%\Plugins\Doodle_%l_time% /mir /ndl /np /njh /njs /ns /nc /nfl
+robocopy %work_dir%Doodle\ThirdParty %work_dir%%UE_VERSION%\Plugins\Doodle\ThirdParty /mir /ndl /np /njh /njs /ns /nc /nfl
 
 rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Intermediate
 rmdir /Q /S %work_dir%%UE_VERSION%\Plugins\Doodle\Source
