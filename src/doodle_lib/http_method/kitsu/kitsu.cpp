7b619e4fd6ce4a4242bdcfe19ea59fce5377f514
uuiid <957714080@qq.com>
2025-04-03 15:59:02 +0800

    [kitsu] 添加注册

--- src/doodle_lib/http_method/kitsu/kitsu.cpp
+++ src/doodle_lib/http_method/kitsu/kitsu.cpp
@@ -61,6 +61,8 @@ http_route_ptr create_kitsu_route(const FSys::path& in_root) {
       .reg(std::make_shared<data_user_done_tasks_get>())
       .reg(std::make_shared<data_user_time_spents_all_get>())
       .reg(std::make_shared<data_user_time_spents_get>())
+      .reg(std::make_shared<person_day_off_get>())
+      .reg(std::make_shared<person_day_off_all_get>())
       .reg(std::make_shared<user_context_get>());
 #endif
 
