82bbb1a0470c0f7cf2c24b3c868734d930b3d183
uuiid <957714080@qq.com>
2025-04-08 15:11:47 +0800

    [kitsu] 添加部门返回值

--- src/doodle_lib/http_method/kitsu/base_data.cpp
+++ src/doodle_lib/http_method/kitsu/base_data.cpp
@@ -1,3 +1,19 @@
 //
 // Created by TD on 25-4-8.
 //
+#include "doodle_core/metadata/person.h"
+#include "doodle_core/sqlite_orm/sqlite_database.h"
+#include <doodle_core/metadata/department.h>
+
+#include <doodle_lib/http_method/kitsu/kitsu_reg_url.h>
+
+namespace doodle::http {
+boost::asio::awaitable<boost::beast::http::message_generator> departments_get::callback(session_data_ptr in_handle) {
+  get_person(in_handle);
+  if (person_->role_ != person_role_type::admin)
+    throw_exception(http_request_error{boost::beast::http::status::unauthorized, "权限不足"});
+  auto l_list = g_ctx().get<sqlite_database>().get_all<department>();
+  co_return in_handle->make_msg((nlohmann::json{} = l_list).dump());
+}
+
+}  // namespace doodle::http
