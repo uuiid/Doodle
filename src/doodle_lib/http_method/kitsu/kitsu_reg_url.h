0aafbbfe510e89bf39c0ed68f20724a62587a0fd
uuiid <957714080@qq.com>
2025-04-28 15:32:27 +0800

    [kitsu] 添加序列查询头

--- src/doodle_lib/http_method/kitsu/kitsu_reg_url.h
+++ src/doodle_lib/http_method/kitsu/kitsu_reg_url.h
@@ -70,6 +70,10 @@ DOODLE_HTTP_FUN_END()
 DOODLE_HTTP_FUN(with_tasks, get, "api/data/assets/with-tasks", http_jwt_fun)
 boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
 DOODLE_HTTP_FUN_END()
+DOODLE_HTTP_FUN(sequences_with_tasks, get, "api/data/sequences/with-tasks", http_jwt_fun)
+boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
+DOODLE_HTTP_FUN_END()
+
 DOODLE_HTTP_FUN(asset_details, get, "api/data/assets/{asset_id}", http_jwt_fun)
 boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
 DOODLE_HTTP_FUN_END()
---
+++ src/doodle_lib/http_method/kitsu/sequences.cpp
@@ -0,0 +1,4 @@
+//
+// Created by TD on 25-4-28.
+//
+#include <doodle_lib/http_method/kitsu/kitsu_reg_url.h>
