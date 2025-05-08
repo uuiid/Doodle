3cb0ceaeac5b76cd0caf56d6566866dacd1406a2
uuiid <957714080@qq.com>
2025-05-08 15:05:25 +0800

    [kitsu] 添加lable 连接表映射

--- src/doodle_core/metadata/label.h
+++ src/doodle_core/metadata/label.h
@@ -5,14 +5,21 @@
 #pragma once
 #include <doodle_core/metadata/base.h>
 namespace doodle {
-struct Label {
+
+struct label_assets_link {
+  std::int32_t id_{};
+  uuid label_uuid_id_{};
+  uuid assets_uuid_id_{};
+};
+
+struct label {
   DOODLE_BASE_FIELDS();
   std::string label_;
   // to json
-  friend void to_json(nlohmann::json& j, const Label& p) {
+  friend void to_json(nlohmann::json& j, const label& p) {
     j["id"]    = p.uuid_id_;
     j["label"] = p.label_;
   }
-  friend void from_json(const nlohmann::json& j, Label& p) { j.at("label").get_to(p.label_); }
+  friend void from_json(const nlohmann::json& j, label& p) { j.at("label").get_to(p.label_); }
 };
 }  // namespace doodle
