ed414519c351d92cea90b6b452e245009549a164
uuiid <957714080@qq.com>
2025-05-13 15:10:06 +0800

    [kitsu] 添加图标获取

--- src/doodle_lib/http_method/kitsu/kitsu_reg_url.h
+++ src/doodle_lib/http_method/kitsu/kitsu_reg_url.h
@@ -134,4 +134,9 @@ DOODLE_HTTP_FUN(deepseek_key, get, "api/doodle/deepseek/key", http_jwt_fun)
 boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
 DOODLE_HTTP_FUN_END()
 // clang-format on
+
+DOODLE_HTTP_FUN(pictures_thumbnails_organisations, get, "/api/pictures/thumbnails/organisations/{id}", http_jwt_fun)
+boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
+DOODLE_HTTP_FUN_END()
+
 }  // namespace doodle::http
--- src/doodle_lib/http_method/kitsu/pictures.cpp
+++ src/doodle_lib/http_method/kitsu/pictures.cpp
@@ -1,4 +1,17 @@
 //
 // Created by TD on 25-5-12.
 //
-#include "kitsu_front_end.h"
+#include <doodle_lib/core/http/http_session_data.h>
+#include <doodle_lib/http_method/kitsu/kitsu.h>
+#include <doodle_lib/http_method/kitsu/kitsu_front_end.h>
+#include <doodle_lib/http_method/kitsu/kitsu_reg_url.h>
+namespace doodle::http {
+boost::asio::awaitable<boost::beast::http::message_generator> pictures_thumbnails_organisations_get::callback(
+    session_data_ptr in_handle
+) {
+  auto l_filename = in_handle->capture_->get("id");
+  auto l_path     = g_ctx().get<kitsu_ctx_t>().root_ / "pictures" / "thumbnails" / FSys::split_uuid_path(l_filename);
+  co_return in_handle->make_msg(l_path, kitsu::mime_type(l_path.extension()));
+}
+
+}  // namespace doodle::http
