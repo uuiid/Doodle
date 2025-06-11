2d6a0a01bfef89a127a9d83d4dbddac4121817b5
uuiid <957714080@qq.com>
2025-06-11 13:14:33 +0800

    [kitsu] ...

--- script/uePlug/Doodle/Source/Doodle/Doodle.Build.cs
+++ script/uePlug/Doodle/Source/Doodle/Doodle.Build.cs
@@ -1,98 +1,99 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+
 //using System.IO;
 
 public class doodle : ModuleRules
 {
-    public doodle(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-        DefaultBuildSettings = BuildSettingsVersion.Latest;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-        AddEngineThirdPartyPrivateStaticDependencies(Target,
-            "Eigen"
-        );
+	public doodle(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		AddEngineThirdPartyPrivateStaticDependencies(Target,
+			"Eigen"
+		);
 
-        PublicIncludePaths.AddRange(
-            new string[] {
-                // ... add public include paths required here ...
-            }
-            );
-
-
-        PrivateIncludePaths.AddRange(
-            new string[] {
-                // ... add other private include paths required here ...
-            }
-            );
+		PublicIncludePaths.AddRange(
+			new string[]
+			{
+				// ... add public include paths required here ...
+			}
+		);
 
 
-
-        PublicDependencyModuleNames.AddRange(
-            new string[]
-            {
-                "Core",
-                "Engine",
-                "Core",
-                "CoreUObject",
-                "Engine",
-                "Slate",
-                "SlateCore",
-                "InputCore",
-                "RenderCore",
-                "RHI",
-                //几何缓存依赖
-                "GeometryCache",
-                // ai模块
-                "AIModule",
-
-            }
-            );
+		PrivateIncludePaths.AddRange(
+			new string[]
+			{
+				// ... add other private include paths required here ...
+			}
+		);
 
 
-        PrivateDependencyModuleNames.AddRange(
-            new string[]
-            {
-                "Core",
-                "CoreUObject",
-                "Engine",
-                "Slate",
-                "SlateCore",
-                "InputCore",
-                "RenderCore",
-                "RHI",
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"Engine",
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				"RenderCore",
+				"RHI",
+				//几何缓存依赖
+				"GeometryCache",
+				// ai模块
+				"AIModule",
+			}
+		);
 
-                "AIModule",//我们需要AI模块
-                "MovieScene",//更新定序器场景
-                "LevelSequence",//创建定序器
-                // "Sequencer",//修改定序器(这个模块不可以被核心依赖)
-                "MovieScene",//更新定序器场景
-                "CinematicCamera",// 相机模块
-                // "SequencerScriptingEditor", // 编辑器
-                "MovieSceneTracks", // 骨骼物体轨道需要
-                "NavigationSystem",// 导航网格
-                "AnimGraphRuntime",// 动画评估模块
-                "Eigen"//数学
-                // "Projects",
-                // "InputCore",
-                // "CoreUObject",
-                // "Engine",
-                // "Slate",
-                // "SlateCore",
-                // "ContentBrowserAssetDataSource"
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				"RenderCore",
+				"RHI",
+
+				"AIModule", //我们需要AI模块
+				"MovieScene", //更新定序器场景
+				"LevelSequence", //创建定序器
+				// "Sequencer",//修改定序器(这个模块不可以被核心依赖)
+				"MovieScene", //更新定序器场景
+				"CinematicCamera", // 相机模块
+				// "SequencerScriptingEditor", // 编辑器
+				"MovieSceneTracks", // 骨骼物体轨道需要
+				"NavigationSystem", // 导航网格
+				"AnimGraphRuntime", // 动画评估模块
+				"Eigen" //数学
+				// "Projects",
+				// "InputCore",
+				// "CoreUObject",
+				// "Engine",
+				// "Slate",
+				// "SlateCore",
+				// "ContentBrowserAssetDataSource"
 
 
 				// ... add private dependencies that you statically link with here ...	
-            }
-            );
+			}
+		);
 
 
-        DynamicallyLoadedModuleNames.AddRange(
-            new string[]
-            {
-                // ... add any modules that your module loads dynamically here ...
-            }
-            );
-    }
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+		);
+	}
 }
