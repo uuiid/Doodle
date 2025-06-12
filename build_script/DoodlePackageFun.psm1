09bcd1fff6528754a98b39a564ac3ec3039c8e57
uuiid <957714080@qq.com>
2025-06-12 16:10:27 +0800

    [kitsu] 修正powshell 在数组只有两个并且剔除一个后, 会直接转换为字符串的bug

--- build_script/DoodlePackageFun.psm1
+++ build_script/DoodlePackageFun.psm1
@@ -86,8 +86,9 @@ function Initialize-Doodle
         $DoodleVersionList = Get-ChildItem -Path "$DoodleBuildRelease\*" -Include "*.zip" | ForEach-Object { $_.Name.Split("-")[1] }
         #         寻找版本号 3.6.678 并放在最后
         $DoodleVersionList = $DoodleVersionList | Where-Object { $_ -ne "3.6.678" }
-        $DoodleVersionList = $DoodleVersionList + "3.6.678"
-        Set-Content -Path "$OutPath\dist\version.txt" -Value ($DoodleVersionList -join "`n") -NoNewline
+        $DoodleVersionList = $DoodleVersionList -join "`n"
+        $DoodleVersionList += "`n3.6.678"
+        Set-Content -Path "$OutPath\dist\version.txt" -Value ($DoodleVersionList) -NoNewline
     }
 
     Copy-Item $DoodleExePath -Destination "$OutPath\dist"
