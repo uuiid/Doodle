c8633b8cd74234c6e0b2a7a02ef632d26232faa8
uuiid <957714080@qq.com>
2025-06-19 11:14:18 +0800

    [kitsu] 调整解析

--- src/doodle_core/sqlite_orm/detail/std_chrono_zoned_time.h
+++ src/doodle_core/sqlite_orm/detail/std_chrono_zoned_time.h
@@ -57,7 +57,10 @@ struct row_extractor<std::chrono::zoned_time<Duration>> : row_extractor<std::str
     const auto l_str = row_extractor<std::string>::extract(stmt, columnIndex);
     std::istringstream l_istr{l_str};
     std::chrono::time_point<std::chrono::system_clock, Duration> l_value{};
-    l_istr >> parse("%F %T", l_value);
+    if (l_istr >> parse("%F %T", l_value))
+      ;
+    else if (l_istr.clear(), l_istr.str(l_str), l_istr >> parse("%F", l_value))
+      ;
     return std::chrono::zoned_time{std::chrono::current_zone(), l_value};
   }
 };
