878595dd1040019c3d3d7002cb6c3b5200e9dfb2
uuiid <957714080@qq.com>
2025-07-02 13:04:47 +0800

    [kitsu] 调整email测试

---
+++ src/test/core/email.cpp
@@ -0,0 +1,22 @@
+//
+// Created by TD on 25-7-2.
+//
+#include "doodle_core/core/doodle_lib.h"
+#include "doodle_core/metadata/rules.h"
+#include "doodle_core/metadata/user.h"
+#include "doodle_core/time_tool/work_clock.h"
+#include <doodle_core/doodle_core.h>
+#include <doodle_core/metadata/time_point_wrap.h>
+
+#include <doodle_lib/http_method/seed_email.h>
+
+#include <boost/system/detail/error_code.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+
+BOOST_AUTO_TEST_SUITE(email)
+
+BOOST_AUTO_TEST_CASE(seed) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
