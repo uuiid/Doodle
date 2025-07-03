a831990ade326d53877c8e46292809988958d82b
uuiid <957714080@qq.com>
2025-07-03 13:15:17 +0800

    [kitsu] 完成查询

--- src/doodle_lib/http_method/kitsu/task.cpp
+++ src/doodle_lib/http_method/kitsu/task.cpp
@@ -124,7 +124,62 @@ boost::asio::awaitable<boost::beast::http::message_generator> tasks_comments_get
 }
 namespace {
 
-struct open_tasks_get_t {};
+struct open_tasks_get_t {
+  task task_;
+  decltype(project::name_) project_name_;
+  decltype(project::uuid_id_) project_id_;
+  decltype(project::has_avatar_) project_has_avatar_;
+  decltype(entity::uuid_id_) entity_id_;
+  decltype(entity::name_) entity_name_;
+  decltype(entity::description_) entity_description_;
+  decltype(entity::preview_file_id_) entity_preview_file_id_;
+  decltype(entity::parent_id_) entity_source_id_;
+  decltype(asset_type::name_) entity_type_name_;
+  decltype(entity::canceled_) entity_canceled_;
+  decltype(entity::name_) sequence_name_;
+  decltype(entity::uuid_id_) episode_id_;
+  decltype(entity::name_) episode_name_;
+  decltype(task::estimation_) estimation_;
+  decltype(task::duration_) duration_;
+  decltype(task::start_date_) start_date_;
+  decltype(task::due_date_) due_date_;
+  decltype(task::done_date_) done_date_;
+  decltype(task_type::name_) type_name_;
+  decltype(task_type::for_entity_) task_type_for_entity_;
+  decltype(task_status::name_) status_name_;
+  decltype(task_type::color_) type_color_;
+  decltype(task_status::color_) status_color_;
+  decltype(task_status::short_name_) status_short_name_;
+  // to json
+  friend void to_json(nlohmann::json& in_j, const open_tasks_get_t& in_p) {
+    to_json(in_j, in_p.task_);
+
+    in_j["project_name"]           = in_p.project_name_;
+    in_j["project_id"]             = in_p.project_id_;
+    in_j["project_has_avatar"]     = in_p.project_has_avatar_;
+    in_j["entity_id"]              = in_p.entity_id_;
+    in_j["entity_name"]            = in_p.entity_name_;
+    in_j["entity_description"]     = in_p.entity_description_;
+    in_j["entity_preview_file_id"] = in_p.entity_preview_file_id_;
+    in_j["entity_source_id"]       = in_p.entity_source_id_;
+    in_j["entity_type_name"]       = in_p.entity_type_name_;
+    in_j["entity_canceled"]        = in_p.entity_canceled_;
+    in_j["sequence_name"]          = in_p.sequence_name_;
+    in_j["episode_id"]             = in_p.episode_id_;
+    in_j["episode_name"]           = in_p.episode_name_;
+    in_j["estimation"]             = in_p.estimation_;
+    in_j["duration"]               = in_p.duration_;
+    in_j["start_date"]             = in_p.start_date_;
+    in_j["due_date"]               = in_p.due_date_;
+    in_j["done_date"]              = in_p.done_date_;
+    in_j["type_name"]              = in_p.type_name_;
+    in_j["task_type_for_entity"]   = in_p.task_type_for_entity_;
+    in_j["status_name"]            = in_p.status_name_;
+    in_j["type_color"]             = in_p.type_color_;
+    in_j["status_color"]           = in_p.status_color_;
+    in_j["status_short_name"]      = in_p.status_short_name_;
+  }
+};
 struct data_tasks_open_tasks_get_args {
   uuid person_id_{};
   chrono::system_zoned_time start_time_{};
@@ -136,12 +191,97 @@ struct data_tasks_open_tasks_get_args {
       if (key == "start_time" && has) start_time_ = from_chrono_time_zone_str(value);
       if (key == "end_time" && has) end_time_ = from_chrono_time_zone_str(value);
     }
+    if (person_id_.is_nil() || start_time_ == chrono::system_zoned_time{} || end_time_ == chrono::system_zoned_time{})
+      throw_exception(http_request_error{boost::beast::http::status::bad_request, "缺失查询参数"});
   }
 
-  void get() {
+  std::vector<open_tasks_get_t> get() {
+    std::vector<open_tasks_get_t> l_ret{};
     auto l_sql = g_ctx().get<sqlite_database>();
     using namespace sqlite_orm;
-    l_sql.impl_->storage_any_.select(columns(object<task>()));
+    constexpr auto sequence = "sequence"_alias.for_<entity>();
+    constexpr auto episode  = "episode"_alias.for_<entity>();
+    for (auto &&[task, project_name, project_has_avatar,
+
+      entity_uuid_id, entity_name, entity_description, entity_preview_file_id, entity_canceled,
+      entity_project_id, entity_source_id,
+
+      sequence_name,
+
+      episode_uuid_id, episode_name,
+
+      asset_type_name,
+
+      task_type_name, task_type_for_entity, task_type_color,
+
+      task_status_name, task_status_color, task_status_short_name
+    ] : l_sql.impl_->storage_any_.select(columns(
+
+             object<task>(), &project::name_, &project::has_avatar_,
+
+             &entity::uuid_id_, &entity::name_, &entity::description_, &entity::preview_file_id_, &entity::canceled_,
+             &entity::project_id_, &entity::source_id_,
+
+             sequence->*&entity::name_,
+
+             episode->*&entity::uuid_id_, episode->*&entity::name_,
+
+             &asset_type::name_,
+
+             &task_type::name_, &task_type::for_entity_, &task_type::color_,
+
+             &task_status::name_, &task_status::color_, &task_status::short_name_
+
+         ),
+        join<task_type>(on(c(&task_type::uuid_id_) == c(&task::task_type_id_))),
+        join<task_status>(on(c(&task_status::uuid_id_) == c(&task::task_status_id_))),
+        join<entity>(on(c(&entity::uuid_id_) == c(&task::entity_id_))),
+        join<asset_type>(on(c(&asset_type::uuid_id_) == c(&entity::entity_type_id_))),
+        join<project>(on(c(&project::uuid_id_) == c(&task::project_id_))),
+        join<assignees_table>(on(c(&assignees_table::task_id_) == c(&task::uuid_id_))),
+        join<project_status>(on(c(&project_status::uuid_id_) == c(&project::project_status_id_))),
+        left_outer_join<sequence>(on(c(sequence->*&entity::uuid_id_) == c(&entity::parent_id_))),
+        left_outer_join<episode>(on(c(episode->*&entity::uuid_id_) == c(sequence->*&entity::parent_id_))),
+        where(
+           c(&assignees_table::person_id_) == person_id_ &&
+           c(&task::start_date_) >= start_time_ ||
+           (c(&task::start_date_) >= start_time_ && c(&task::end_date_) <= end_time_)
+        ),
+        multi_order_by(order_by(&project::name_), order_by(episode->*&entity::name_), order_by(sequence->*&entity::name_),
+          order_by(&asset_type::name_),      order_by(&task_type::name_))
+
+         )) {
+      l_ret.emplace_back(
+          open_tasks_get_t{
+              .task_                   = task,
+              .project_name_           = project_name,
+              .project_id_             = person_id_,
+              .project_has_avatar_     = project_has_avatar,
+              .entity_id_              = entity_uuid_id,
+              .entity_name_            = entity_name,
+              .entity_description_     = entity_description,
+              .entity_preview_file_id_ = entity_preview_file_id,
+              .entity_source_id_       = entity_source_id,
+              .entity_type_name_       = asset_type_name,
+              .entity_canceled_        = entity_canceled,
+              .sequence_name_          = sequence_name,
+              .episode_id_             = episode_uuid_id,
+              .episode_name_           = episode_name,
+              .estimation_             = task.estimation_,
+              .duration_               = task.duration_,
+              .start_date_             = task.start_date_,
+              .due_date_               = task.due_date_,
+              .done_date_              = task.done_date_,
+              .type_name_              = task_type_name,
+              .task_type_for_entity_   = task_type_for_entity,
+              .status_name_            = task_status_name,
+              .type_color_             = task_type_color,
+              .status_color_           = task_status_color,
+              .status_short_name_      = task_status_short_name
+          }
+      );
+    }
+    return l_ret;
   }
 };
 
@@ -150,12 +290,10 @@ boost::asio::awaitable<boost::beast::http::message_generator> data_tasks_open_ta
     session_data_ptr in_handle
 ) {
   auto l_ptr = get_person(in_handle);
-  auto& sql  = g_ctx().get<sqlite_database>();
   data_tasks_open_tasks_get_args l_args{};
   l_args.parse_args(in_handle->url_.params());
-  l_args.get();
 
-  co_return in_handle->make_error_code_msg(boost::beast::http::status::not_implemented, "not implemented");
+  co_return in_handle->make_msg(nlohmann::json{} = l_args.get());
 }
 
 }  // namespace doodle::http
