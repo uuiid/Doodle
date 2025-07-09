1a314e4f69a249abee3bc53f3ca45cb84a8bd4c2
uuiid <957714080@qq.com>
2025-07-09 09:55:50 +0800

    [kitsu] 调整外包部分

--- src/doodle_lib/http_method/kitsu/epiboly.h
+++ src/doodle_lib/http_method/kitsu/epiboly.h
@@ -5,14 +5,17 @@
 #pragma once
 #include <doodle_lib/core/http/http_route.h>
 #include <doodle_lib/http_method/http_jwt_fun.h>
-namespace doodle::http  {
-DOODLE_HTTP_FUN(epiboly_config, get, "/api/config", http_jwt_fun)
-boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
+namespace doodle::http {
+// "/api/config"
+DOODLE_HTTP_FUN(epiboly_config, get, ucom_t{} / "api" / "config", http_jwt_fun_template<void>)
+boost::asio::awaitable<boost::beast::http::message_generator> callback_arg(session_data_ptr in_handle) override;
 DOODLE_HTTP_FUN_END()
-DOODLE_HTTP_FUN(epiboly_authenticated, get, "/api/auth/authenticated", http_jwt_fun)
-boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
+// "/api/auth/authenticated"
+DOODLE_HTTP_FUN(epiboly_authenticated, get, ucom_t{} / "api" / "auth" / "authenticated", http_jwt_fun_template<void>)
+boost::asio::awaitable<boost::beast::http::message_generator> callback_arg(session_data_ptr in_handle) override;
 DOODLE_HTTP_FUN_END()
-DOODLE_HTTP_FUN(epiboly_user_context, get, "/api/data/user/context", http_jwt_fun)
-boost::asio::awaitable<boost::beast::http::message_generator> callback(session_data_ptr in_handle) override;
+// "/api/data/user/context"
+DOODLE_HTTP_FUN(epiboly_user_context, get, ucom_t{} / "api" / "data" / "user" / "context", http_jwt_fun_template<void>)
+boost::asio::awaitable<boost::beast::http::message_generator> callback_arg(session_data_ptr in_handle) override;
 DOODLE_HTTP_FUN_END()
-}
+}  // namespace doodle::http
