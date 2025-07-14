f83d75c0c75581911a54b769ffed0c07c7e84572
uuiid <957714080@qq.com>
2025-07-14 17:13:01 +0800

    [kitsu] 继续去除无用的类

--- src/doodle_core/metadata/kitsu/assets_type.h
+++ src/doodle_core/metadata/kitsu/assets_type.h
@@ -5,15 +5,3 @@
 #pragma once
 #include <doodle_core/doodle_core_fwd.h>
 #include <doodle_core/metadata/assets.h>
-
-namespace doodle::metadata::kitsu {
-
-struct assets_type_t {
-  std::int32_t id_{};
-  uuid uuid_id_{};
-
-  std::string name_{};
-  details::assets_type_enum type_{details::assets_type_enum::other};
-};
-
-}  // namespace doodle::metadata::kitsu
