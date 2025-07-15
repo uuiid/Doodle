9c1521986081164f65d95cb5bc79856ca1107814
uuiid <957714080@qq.com>
2025-07-15 15:29:53 +0800

    [kitsu] 完成删除

--- src/doodle_lib/http_method/kitsu/task.cpp
+++ src/doodle_lib/http_method/kitsu/task.cpp
@@ -320,7 +320,11 @@ boost::asio::awaitable<boost::beast::http::message_generator> data_tasks_open_ta
 boost::asio::awaitable<boost::beast::http::message_generator> data_task_delete_::callback_arg(
     session_data_ptr in_handle, std::shared_ptr<capture_id_t> in_arg
 ) {
-  co_return in_handle->make_msg(nlohmann::json::array());
+  auto l_ptr = get_person(in_handle);
+  auto l_task = g_ctx().get<sqlite_database>().get_by_uuid<task>(in_arg->id_);
+  l_ptr->check_delete_access(l_task.project_id_);
+  co_await g_ctx().get<sqlite_database>().remove<task>(in_arg->id_);
+  co_return in_handle->make_msg({}, boost::beast::http::status::no_content);
 }
 
 }  // namespace doodle::http
