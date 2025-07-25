18f41bb5e9b8831e7b8d752f6b4f62a256edf2f5
uuiid <957714080@qq.com>
2025-07-25 17:03:14 +0800

    [kitsu] 调整路由

--- src/doodle_lib/http_method/kitsu/http_route_proxy.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by TD on 24-10-10.
-//
-
-#include "http_route_proxy.h"
-
-#include <doodle_lib/core/http/http_function.h>
-#include <doodle_lib/http_method/kitsu/kitsu.h>
-namespace doodle::http::kitsu {}  // namespace doodle::http::kitsu
