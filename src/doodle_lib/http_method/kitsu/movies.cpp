faaaee09ff64c2960eff844b7675de16e9d80573
uuiid <957714080@qq.com>
2025-07-31 13:48:48 +0800

    [kitsu] 调整检查

--- src/doodle_lib/http_method/kitsu/movies.cpp
+++ src/doodle_lib/http_method/kitsu/movies.cpp
@@ -16,7 +16,7 @@ boost::asio::awaitable<boost::beast::http::message_generator> movies_low_preview
   auto l_ptr  = get_person(in_handle);
   auto l_uuid = in_arg->id_;
   auto l_sql  = g_ctx().get<sqlite_database>();
-  auto l_entt = l_sql.get_by_uuid<entity>(l_uuid);
+  auto l_entt = l_sql.get_by_uuid<task>(l_sql.get_by_uuid<preview_file>(l_uuid).task_id_);
   l_ptr->is_project_access(l_entt.project_id_);
   auto l_path =
       g_ctx().get<kitsu_ctx_t>().root_ / "movies" / "lowdef" / FSys::split_uuid_path(fmt::format("{}.mp4", l_uuid));
