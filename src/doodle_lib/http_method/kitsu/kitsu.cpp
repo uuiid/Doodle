bcf28c1d5072893cd94f566219da2c14a9a5899c
uuiid <957714080@qq.com>
2025-08-05 13:24:10 +0800

    [kitsu] 调整url注册

--- src/doodle_lib/http_method/kitsu/kitsu.cpp
+++ src/doodle_lib/http_method/kitsu/kitsu.cpp
@@ -44,7 +44,7 @@ http_route_ptr create_kitsu_route_2(const FSys::path& in_root) {
   (*l_router)
       // clang-format off
       // 我们自己的后端
-      .reg_t<socket_io::socket_io_http>(R"("/socket\.io/")"_url, l_sid_ctx)
+      .reg_t<socket_io::socket_io_http>(R"(/socket\.io/)"_url, l_sid_ctx)
       .reg_t<doodle_data_asset_file_maya>("/api/doodle/data/asset/{}/file/maya"_url(&doodle_data_asset_file_image::id_))
       .reg_t<doodle_data_asset_file_ue>("/api/doodle/data/asset/{}/file/ue"_url(&doodle_data_asset_file_image::id_))
       .reg_t<doodle_data_asset_file_image>("/api/doodle/data/asset/{}/file/image"_url(&doodle_data_asset_file_image::id_))
@@ -188,7 +188,7 @@ http_route_ptr create_kitsu_local_route() {
         .reg_t<local::task_instance_restart>("/api/doodle/task/{}/restart"_url(&local::task_instance_restart::id_))
         .reg_t<local::task_instance_log>("/api/doodle/task/{}/restart"_url(&local::task_instance_log::id_))
 
-        .reg_t<socket_io::socket_io_http>(R"("/socket\.io/")"_url, l_sid_ctx)
+        .reg_t<socket_io::socket_io_http>(R"(/socket\.io/)"_url, l_sid_ctx)
 
         ;
   return l_rout_ptr;
