93706a4eb2e5ea6b03de35a68673c202363ae236
uuiid <957714080@qq.com>
2025-08-20 14:58:08 +0800

    优化类型转换逻辑，支持枚举类型的转换并更新相关模板特化

--- src/doodle_lib/core/http/http_function.h
+++ src/doodle_lib/core/http/http_function.h
@@ -15,6 +15,7 @@
 #include <boost/url.hpp>
 
 #include "http_route.h"
+#include <magic_enum/magic_enum_all.hpp>
 
 namespace doodle::http {
 
@@ -60,6 +61,7 @@ class url_route_component_t : public url_route_component_base_t {
   constexpr static auto g_year_month_day_regex = std::string_view{"([0-9]{4}-[0-9]{2}-[0-9]{2})"};
   constexpr static auto g_number               = std::string_view{"([0-9]+)"};
   constexpr static auto g_file_name            = std::string_view{R"((.+\.[A-Za-z]{3}$))"};
+  constexpr static auto g_enum                 = std::string_view{"([A-Za-z0-9_]+)"};
 
   struct component_base_t {
     virtual ~component_base_t()                                                            = default;
@@ -91,24 +93,38 @@ class url_route_component_t : public url_route_component_base_t {
 
     template <typename T1>
     T1 convert(const std::string& in_str) const;
-    template <>
-    uuid convert(const std::string& in_str) const {
+
+    template <typename Enum_T>
+      requires magic_enum::is_scoped_enum_v<Enum_T>
+    Enum_T convert(const std::string& in_str) const {
+      return magic_enum::enum_cast<Enum_T>(in_str).value();
+    }
+
+    template <typename Uuid_T>
+      requires std::is_same_v<Uuid_T, uuid>
+    Uuid_T convert(const std::string& in_str) const {
       return this->convert_uuid(in_str);
     }
-    template <>
-    chrono::year_month convert(const std::string& in_str) const {
+
+    template <typename Year_Month_T>
+      requires std::is_same_v<Year_Month_T, chrono::year_month>
+    Year_Month_T convert(const std::string& in_str) const {
       return this->convert_year_month(in_str);
     }
-    template <>
-    chrono::year_month_day convert(const std::string& in_str) const {
+    template <typename Year_Month_Day_T>
+      requires std::is_same_v<Year_Month_Day_T, chrono::year_month_day>
+    Year_Month_Day_T convert(const std::string& in_str) const {
       return this->convert_year_month_day(in_str);
     }
-    template <>
-    std::int32_t convert(const std::string& in_str) const {
+    template <typename Number_T>
+      requires std::is_same_v<Number_T, std::int32_t>
+    Number_T convert(const std::string& in_str) const {
       return this->convert_number(in_str);
     }
-    template <>
-    FSys::path convert(const std::string& in_str) const {
+
+    template <typename Path_T>
+      requires std::is_same_v<Path_T, FSys::path>
+    Path_T convert(const std::string& in_str) const {
       return this->convert_file_name(in_str);
     }
   };
@@ -137,7 +153,7 @@ class url_route_component_t : public url_route_component_base_t {
       static_assert(
           std::is_same_v<field_type, uuid> || std::is_same_v<field_type, chrono::year_month> ||
               std::is_same_v<field_type, chrono::year_month_day> || std::is_same_v<field_type, std::int32_t> ||
-              std::is_same_v<field_type, FSys::path>,
+              std::is_same_v<field_type, FSys::path> || magic_enum::is_scoped_enum_v<field_type>,
           "not support type"
       );
 
@@ -151,6 +167,10 @@ class url_route_component_t : public url_route_component_base_t {
         return g_number;
       else if constexpr (std::is_same_v<field_type, FSys::path>)
         return g_file_name;
+      else if constexpr (magic_enum::is_scoped_enum_v<field_type>) {
+        constexpr auto color_names = magic_enum::enum_names<field_type>();
+        return fmt::format("({})", fmt::join(color_names, "|"));
+      }
     }
     template <typename Member_Pointer>
       requires std::is_member_pointer_v<Member_Pointer>
@@ -159,7 +179,7 @@ class url_route_component_t : public url_route_component_base_t {
       static_assert(
           std::is_same_v<field_type, uuid> || std::is_same_v<field_type, chrono::year_month> ||
               std::is_same_v<field_type, chrono::year_month_day> || std::is_same_v<field_type, std::int32_t> ||
-              std::is_same_v<field_type, FSys::path>,
+              std::is_same_v<field_type, FSys::path> || magic_enum::is_scoped_enum_v<field_type>,
           "not support type"
       );
       return std::make_shared<component_t<Member_Pointer>>(in_target);
