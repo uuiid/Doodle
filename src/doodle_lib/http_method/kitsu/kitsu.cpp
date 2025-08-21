716a9f3260dc4c985676a7a6a1dafcbc1359214c
uuiid <957714080@qq.com>
2025-08-21 14:57:19 +0800

    添加data_project_sequences_casting结构及其JSON序列化逻辑，并注册新的API路由以获取项目序列数据

--- src/doodle_lib/http_method/kitsu/kitsu.cpp
+++ src/doodle_lib/http_method/kitsu/kitsu.cpp
@@ -174,7 +174,7 @@ http_route_ptr create_kitsu_route_2(const FSys::path& in_root) {
       .reg_t<actions_projects_task_types_shots_create_tasks>("/api/actions/projects/{}/task-types/{}/shots/create-tasks"_url(
                                                    &actions_projects_task_types_shots_create_tasks::project_id_,
                                                    &actions_projects_task_types_shots_create_tasks::task_type_id_))
-      .reg_t<data_project_sequences_casting>("/api/data/projects/{}/sequences/casting"_url(&data_project_sequences_casting::project_id_))
+      .reg_t<data_project_sequences_casting>("/api/data/projects/{}/sequences/all/casting"_url(&data_project_sequences_casting::project_id_))
        // 最后注册nodejs前端
       .reg_t<kitsu_front_end>(std::make_shared<kitsu_front_end_url_route_component>(), in_root)
       // clang-format on
