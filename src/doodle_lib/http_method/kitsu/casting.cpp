8f6d6ae3bbd836b41d1ac34b85748147c17aace4
uuiid <957714080@qq.com>
2025-08-21 13:26:10 +0800

    添加casting.cpp文件，包含相关头文件并更新kitsu_reg_url.h以支持新的API路由

---
+++ src/doodle_lib/http_method/kitsu/casting.cpp
@@ -0,0 +1,11 @@
+//
+// Created by TD on 25-8-21.
+//
+#include <doodle_core/metadata/entity_type.h>
+#include <doodle_core/sqlite_orm/detail/sqlite_database_impl.h>
+#include <doodle_core/sqlite_orm/sqlite_database.h>
+#include <doodle_core/sqlite_orm/sqlite_select_data.h>
+
+#include <doodle_lib/core/socket_io/broadcast.h>
+#include <doodle_lib/http_method/kitsu/kitsu_reg_url.h>
+#include <doodle_lib/http_method/kitsu/kitsu_result.h>
