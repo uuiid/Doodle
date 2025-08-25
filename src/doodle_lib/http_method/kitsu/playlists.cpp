dd6386c75a1b38e62f59c93a636baef44eb8cf7a
uuiid <957714080@qq.com>
2025-08-25 14:37:11 +0800

    重构并扩展base_playlist_t及相关结构，添加排序和JSON序列化逻辑

--- src/doodle_lib/http_method/kitsu/playlists.cpp
+++ src/doodle_lib/http_method/kitsu/playlists.cpp
@@ -121,6 +121,11 @@ struct base_playlist_t {
   uuid preview_file_task_id_;
   std::string parent_name_;
 
+  friend bool operator<(const base_playlist_t& lhs, const base_playlist_t& rhs) { return lhs.name_ < rhs.name_; }
+  friend bool operator<=(const base_playlist_t& lhs, const base_playlist_t& rhs) { return !(rhs < lhs); }
+  friend bool operator>(const base_playlist_t& lhs, const base_playlist_t& rhs) { return rhs < lhs; }
+  friend bool operator>=(const base_playlist_t& lhs, const base_playlist_t& rhs) { return !(lhs < rhs); }
+
   decltype(preview_file::uuid_id_) preview_file_id_;
   decltype(preview_file::extension_) preview_file_extension_;
   decltype(preview_file::width_) preview_file_width_;
@@ -151,28 +156,233 @@ struct base_playlist_t {
     j["preview_files"]            = p.preview_file_previews_;
   }
 };
-struct shot_for_playlist_t : base_playlist_t {};
+struct shot_for_playlist_t : base_playlist_t {
+  explicit shot_for_playlist_t(
+      const entity& in_seq_ent, const entity& in_shot, const uuid& in_task_id,
+      const std::map<uuid, std::vector<preview_files_for_entity_t>>& in_v, const uuid& in_task_type_id
+
+  )
+      : base_playlist_t(in_shot, in_task_id, in_v, in_task_type_id),
+        sequence_id_(in_seq_ent.uuid_id_),
+        sequence_name_(in_seq_ent.name_) {
+    parent_name_ = in_seq_ent.name_;
+  }
+  friend bool operator<(const shot_for_playlist_t& lhs, const shot_for_playlist_t& rhs) {
+    return lhs.sequence_name_ < rhs.sequence_name_;
+  }
+  friend bool operator<=(const shot_for_playlist_t& lhs, const shot_for_playlist_t& rhs) { return !(rhs < lhs); }
+  friend bool operator>(const shot_for_playlist_t& lhs, const shot_for_playlist_t& rhs) { return rhs < lhs; }
+  friend bool operator>=(const shot_for_playlist_t& lhs, const shot_for_playlist_t& rhs) { return !(lhs < rhs); }
+
+  uuid sequence_id_;
+  std::string sequence_name_;
+  // to_json
+  friend void to_json(nlohmann::json& j, const shot_for_playlist_t& p) {
+    to_json(j, static_cast<const base_playlist_t&>(p));
+    j["sequence_id"]   = p.sequence_id_;
+    j["sequence_name"] = p.sequence_name_;
+  }
+};
+struct asset_for_playlist_t : base_playlist_t {
+  explicit asset_for_playlist_t(
+      const asset_type& in_ass_type, const entity& in_entt, const uuid& in_task_id,
+      const std::map<uuid, std::vector<preview_files_for_entity_t>>& in_v, const uuid& in_task_type_id
+  )
+      : base_playlist_t(in_entt, in_task_id, in_v, in_task_type_id),
+        asset_type_id_(in_ass_type.uuid_id_),
+        asset_type_name_(in_ass_type.name_) {
+    parent_name_ = asset_type_name_;
+  }
+  uuid asset_type_id_;
+  std::string asset_type_name_;
+
+  friend bool operator<(const asset_for_playlist_t& lhs, const asset_for_playlist_t& rhs) {
+    return lhs.asset_type_name_ < rhs.asset_type_name_;
+  }
+  friend bool operator<=(const asset_for_playlist_t& lhs, const asset_for_playlist_t& rhs) { return !(rhs < lhs); }
+  friend bool operator>(const asset_for_playlist_t& lhs, const asset_for_playlist_t& rhs) { return rhs < lhs; }
+  friend bool operator>=(const asset_for_playlist_t& lhs, const asset_for_playlist_t& rhs) { return !(lhs < rhs); }
+
+  // to_json
+  friend void to_json(nlohmann::json& j, const asset_for_playlist_t& p) {
+    to_json(j, static_cast<const base_playlist_t&>(p));
+    j["asset_type_id"]   = p.asset_type_id_;
+    j["asset_type_name"] = p.asset_type_name_;
+  }
+};
+struct sequence_for_playlist_t : base_playlist_t {
+  explicit sequence_for_playlist_t(
+      const entity& in_seq_ent, const uuid& in_task_id,
+      const std::map<uuid, std::vector<preview_files_for_entity_t>>& in_v, const uuid& in_task_type_id
+  )
+      : base_playlist_t(in_seq_ent, in_task_id, in_v, in_task_type_id) {}
+
+  friend bool operator<(const sequence_for_playlist_t& lhs, const sequence_for_playlist_t& rhs) {
+    return static_cast<const base_playlist_t&>(lhs) < static_cast<const base_playlist_t&>(rhs);
+  }
+  friend bool operator<=(const sequence_for_playlist_t& lhs, const sequence_for_playlist_t& rhs) {
+    return !(rhs < lhs);
+  }
+  friend bool operator>(const sequence_for_playlist_t& lhs, const sequence_for_playlist_t& rhs) { return rhs < lhs; }
+  friend bool operator>=(const sequence_for_playlist_t& lhs, const sequence_for_playlist_t& rhs) {
+    return !(lhs < rhs);
+  }
+
+  // uuid sequence_id_;
+  // std::string sequence_name_;
+  // to_json
+  friend void to_json(nlohmann::json& j, const sequence_for_playlist_t& p) {
+    to_json(j, static_cast<const base_playlist_t&>(p));
+    j["sequence_id"]   = "";
+    j["sequence_name"] = "";
+  }
+};
+struct sequence_episode_for_playlist_t : base_playlist_t {
+  explicit sequence_episode_for_playlist_t(
+      const entity& in_episode_ent, const entity& in_seq_ent, const uuid& in_task_id,
+      const std::map<uuid, std::vector<preview_files_for_entity_t>>& in_v, const uuid& in_task_type_id
+  )
+      : base_playlist_t(in_seq_ent, in_task_id, in_v, in_task_type_id),
+        episode_id_(in_episode_ent.uuid_id_),
+        episode_name_(in_episode_ent.name_) {
+    parent_name_ = in_episode_ent.name_;
+  }
+  uuid episode_id_;
+  std::string episode_name_;
+
+  friend bool operator<(const sequence_episode_for_playlist_t& lhs, const sequence_episode_for_playlist_t& rhs) {
+    return lhs.episode_name_ < rhs.episode_name_;
+  }
+  friend bool operator<=(const sequence_episode_for_playlist_t& lhs, const sequence_episode_for_playlist_t& rhs) {
+    return !(rhs < lhs);
+  }
+  friend bool operator>(const sequence_episode_for_playlist_t& lhs, const sequence_episode_for_playlist_t& rhs) {
+    return rhs < lhs;
+  }
+  friend bool operator>=(const sequence_episode_for_playlist_t& lhs, const sequence_episode_for_playlist_t& rhs) {
+    return !(lhs < rhs);
+  }
+
+  // to_json
+  friend void to_json(nlohmann::json& j, const sequence_episode_for_playlist_t& p) {
+    to_json(j, static_cast<const base_playlist_t&>(p));
+    j["episode_id"]   = p.episode_id_;
+    j["episode_name"] = p.episode_name_;
+  }
+};
+struct episode_for_playlist_t : base_playlist_t {
+  explicit episode_for_playlist_t(
+      const entity& in_episode_ent, const uuid& in_task_id,
+      const std::map<uuid, std::vector<preview_files_for_entity_t>>& in_v, const uuid& in_task_type_id
+  )
+      : base_playlist_t(in_episode_ent, in_task_id, in_v, in_task_type_id) {}
+
+  friend bool operator<(const episode_for_playlist_t& lhs, const episode_for_playlist_t& rhs) {
+    return static_cast<const base_playlist_t&>(lhs) < static_cast<const base_playlist_t&>(rhs);
+  }
+  friend bool operator<=(const episode_for_playlist_t& lhs, const episode_for_playlist_t& rhs) { return !(rhs < lhs); }
+  friend bool operator>(const episode_for_playlist_t& lhs, const episode_for_playlist_t& rhs) { return rhs < lhs; }
+  friend bool operator>=(const episode_for_playlist_t& lhs, const episode_for_playlist_t& rhs) { return !(lhs < rhs); }
+
+  // uuid sequence_id_;
+  // std::string sequence_name_;
+  // to_json
+  friend void to_json(nlohmann::json& j, const episode_for_playlist_t& p) {
+    to_json(j, static_cast<const base_playlist_t&>(p));
+    j["sequence_id"]   = "";
+    j["sequence_name"] = "";
+  }
+};
+struct temp_playlist_t {
+  using playlist_variant_t = std::variant<
+      shot_for_playlist_t, asset_for_playlist_t, sequence_for_playlist_t, sequence_episode_for_playlist_t,
+      episode_for_playlist_t>;
+  std::vector<playlist_variant_t> playlist_;
+
+  // to_json
+  friend void to_json(nlohmann::json& j, const temp_playlist_t& p) {
+    j = nlohmann::json::array();
+    for (auto&& l_v : p.playlist_) {
+      std::visit([&](auto&& arg) { to_json(j.emplace_back(), arg); }, l_v);
+    }
+  }
+};
+
 }  // namespace
 boost::asio::awaitable<boost::beast::http::message_generator> data_project_playlists_temp::post(
     session_data_ptr in_handle
 ) {
   person_.check_project_access(project_id_);
+  auto l_sql      = g_ctx().get<sqlite_database>();
   auto l_task_ids = in_handle->get_json().get<std::vector<uuid>>();
   std::vector<task> l_tasks{};
   l_tasks.reserve(l_task_ids.size());
-  for (auto&& i : l_task_ids) l_tasks.emplace_back(g_ctx().get<sqlite_database>().get_by_uuid<task>(i));
+  for (auto&& i : l_task_ids) l_tasks.emplace_back(l_sql.get_by_uuid<task>(i));
   std::vector<entity> l_entts{};
   l_entts.reserve(l_tasks.size());
-  for (auto&& i : l_tasks) l_entts.emplace_back(g_ctx().get<sqlite_database>().get_by_uuid<entity>(i.entity_id_));
+  for (auto&& i : l_tasks) l_entts.emplace_back(l_sql.get_by_uuid<entity>(i.entity_id_));
+  bool l_is_sort{};
+  for (auto&& [key, value, has] : in_handle->url_.params())
+    if (key == "sort") l_is_sort = true;
+
+  temp_playlist_t l_playlist{};
   for (auto&& i : l_entts) {
     if (i.entity_type_id_ == asset_type::get_shot_id()) {
+      l_playlist.playlist_.emplace_back(
+          shot_for_playlist_t{
+              l_sql.get_by_uuid<entity>(i.parent_id_), i, l_tasks[0].uuid_id_,
+              l_sql.get_preview_files_for_entity(i.uuid_id_), l_tasks[0].task_type_id_
+          }
+      );
     } else if (i.entity_type_id_ == asset_type::get_sequence_id()) {
+      if (i.parent_id_.is_nil())
+        l_playlist.playlist_.emplace_back(
+            sequence_for_playlist_t{
+                i, l_tasks[0].uuid_id_, l_sql.get_preview_files_for_entity(i.uuid_id_), l_tasks[0].task_type_id_
+            }
+        );
+      else
+        l_playlist.playlist_.emplace_back(
+            sequence_episode_for_playlist_t{
+                l_sql.get_by_uuid<entity>(i.parent_id_), i, l_tasks[0].uuid_id_,
+                l_sql.get_preview_files_for_entity(i.uuid_id_), l_tasks[0].task_type_id_
+            }
+        );
+
     } else if (i.entity_type_id_ == asset_type::get_episode_id()) {
+      l_playlist.playlist_.emplace_back(
+          episode_for_playlist_t{
+              i, l_tasks[0].uuid_id_, l_sql.get_preview_files_for_entity(i.uuid_id_), l_tasks[0].task_type_id_
+          }
+      );
     } else {
+      l_playlist.playlist_.emplace_back(
+          asset_for_playlist_t{
+              l_sql.get_by_uuid<asset_type>(i.entity_type_id_), i, l_tasks[0].uuid_id_,
+              l_sql.get_preview_files_for_entity(i.uuid_id_), l_tasks[0].task_type_id_
+          }
+      );
     }
   }
+  if (l_is_sort) {
+    std::sort(
+        l_playlist.playlist_.begin(), l_playlist.playlist_.end(),
+        [](const temp_playlist_t::playlist_variant_t& l_l, const temp_playlist_t::playlist_variant_t& r_r) -> bool {
+          return std::visit(
+              [](auto&& l_l, auto&& r_r) {
+                if constexpr (std::is_same_v<std::decay_t<decltype(l_l)>, std::decay_t<decltype(r_r)>>) {
+                  return l_l < r_r;
+                } else {
+                  return false;
+                }
+              },
+              l_l, r_r
+          );
+        }
+    );
+  }
 
-  co_return in_handle->make_msg_204();
+  co_return in_handle->make_msg(nlohmann::json{} = l_playlist);
 }
 
 }  // namespace doodle::http
