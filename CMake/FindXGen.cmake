4fb06471a520e4932568f3460594da595001a206
uuiid <957714080@qq.com>
2025-09-03 15:26:22 +0800

    添加 XGen 支持到 CMake 配置
    - 在 CMakeLists.txt 中添加对 XGen 模块的查找支持。
    - 新增 FindXGen.cmake 文件，包含以下内容：
      - 根据操作系统设置默认的 Maya 安装路径。
      - 查找 XGen 的库和包含目录。
      - 检查并确认 XGen 是否找到。
      - 添加接口库 xgen，并创建别名 maya::xgen。

---
+++ CMake/FindXGen.cmake
@@ -0,0 +1,52 @@
+set(FIND_PACKAGE_INTERNAL_${CMAKE_FIND_PACKAGE_NAME} ${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION} CACHE STRING "内部缓存版本" FORCE)
+
+if (WIN32)
+    set(MAYA_DEFAULT_LOCATION "C:/Program Files/Autodesk/Maya${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION}")
+    set(ADSKXGEN_LIB AdskXGen.lib)
+elseif (APPLE)
+    set(MAYA_DEFAULT_LOCATION "/Applications/Autodesk/maya${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION}")
+    set(ADSKXGEN_LIB libAdskXGen.dylib)
+elseif (LINUX)
+    set(MAYA_DEFAULT_LOCATION "/usr/autodesk/maya${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION}")
+    set(ADSKXGEN_LIB libAdskXGen.so)
+endif ()
+
+set(XGEN_PLUGIN plug-ins/xgen)
+find_path(XGEN_LIB_DIR ${ADSKXGEN_LIB}
+        HINTS
+        ${MAYA_LOCATION}/${XGEN_PLUGIN}
+        $ENV{MAYA_LOCATION}/${XGEN_PLUGIN}
+        ${MAYA_DEFAULT_LOCATION}/${XGEN_PLUGIN}
+        "[HKLM\\SOFTWARE\\Autodesk\\Maya\\${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION}\\Setup\\InstallPath;MAYA_INSTALL_LOCATION]"
+        PATH_SUFFIXES
+        "lib/"
+        DOC
+        "XGen library path"
+)
+
+
+find_path(XGEN_INCLUDE_DIR
+        xgen/src/xgcore/XgConfig.h
+        HINTS
+        ${MAYA_LOCATION}/${XGEN_PLUGIN}
+        $ENV{MAYA_LOCATION}/${XGEN_PLUGIN}
+        ${MAYA_DEFAULT_LOCATION}/${XGEN_PLUGIN}
+        PATH_SUFFIXES
+        "include/"
+        DOC
+        "XGen include path"
+)
+
+if (XGEN_LIB_DIR AND XGEN_INCLUDE_DIR)
+    set(XGEN_FOUND TRUE)
+endif ()
+find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
+        REQUIRED_VARS
+        XGEN_LIB_DIR
+        XGEN_INCLUDE_DIR
+        REASON_FAILURE_MESSAGE "maya xgen 库中的组件没有找到"
+)
+
+add_library(xgen INTERFACE IMPORTED)
+target_include_directories(xgen INTERFACE ${XGEN_INCLUDE_DIR})
+add_library(maya::xgen ALIAS xgen)
