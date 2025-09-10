839990096002365407af05f759a815c7abe3852f
uuiid <957714080@qq.com>
2025-09-10 14:18:40 +0800

    更新打包脚本以处理版本号递增
    - 移除旧的压缩文件和插件压缩包以确保干净的打包环境。
    - 修改Doodle名称获取方式，确保获取最新的压缩文件名。
    - 将Doodle版本号的递增逻辑更新为更简洁的形式。
    - 更新最终文件拷贝的路径，以包含正确的版本号。

--- build_script/package_epiboly.ps1
+++ build_script/package_epiboly.ps1
@@ -4,17 +4,20 @@ Import-Module -Name $PSScriptRoot\DoodlePackageFun.psm1 -Force
 
 $DoodleExePath = "E:\source\doodle\dist\win-unpacked\*"
 $DoodleOut = Convert-Path "$PSScriptRoot/../build"
+Remove-Item -Path "$DoodleOut/epiboly" -Include "*.zip" -Recurse -Force
+
 Initialize-Doodle -OutPath "$DoodleOut/epiboly" -OnlyOne
 Copy-Item $DoodleExePath -Destination "$DoodleOut/epiboly" -Recurse -Force
+Remove-Item -Path "$DoodleOut/epiboly/dist/Plugins/*.zip" -Force
+
 Compress-Archive -Path "$DoodleOut/epiboly/*" -DestinationPath "$DoodleOut/epiboly.zip" -Force
-$DoodleName = (Get-ChildItem "$DoodleOut/epiboly/dist/" -Filter "*.zip")[0].Name.Split("-")[1] + ".0"
+$DoodleName = (Get-ChildItem "$DoodleOut/epiboly/dist/" -Filter "*.zip")[-1].Name.Split("-")[1] + ".0"
+$DoodleVersion = 0
 
-do
+while (Test-Path -Path "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName.$DoodleVersion-win64.zip")
 {
-    $Arr = $DoodleName.Split(".")
-    # 分解后最后一位递增 1
-    $DoodleName = ($Arr[0..($Arr.Length - 2)] | Join-String -Separator ".") + "." + ([Int32]$Arr[-1] + 1)
-    Write-Host $DoodleName;
-} while (Test-Path -Path "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName-win64.zip")
+    $DoodleVersion += 1
+    Write-Host "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName.$DoodleVersion-win64.zip";
+}
 
-Copy-Item "$DoodleOut/epiboly.zip" -Destination "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName-win64.zip" -Force
+Copy-Item "$DoodleOut/epiboly.zip" -Destination "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName.$DoodleVersion-win64.zip" -Force
