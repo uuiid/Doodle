3ecfb4ffcc3e688c351dcaa0118687378b4f6de2
uuiid <957714080@qq.com>
2025-09-16 14:42:24 +0800

    ✨ feat(core): add asynchronous task interface
    - add async_task.h for asynchronous task management
    - define abstract class async_task with virtual run method

---
+++ src/doodle_lib/core/asyn_task.h
@@ -0,0 +1,9 @@
+#include <doodle_lib/doodle_lib_fwd.h>
+
+namespace doodle {
+class async_task {
+ public:
+  virtual ~async_task()                            = default;
+  virtual boost::asio::awaitable<void> run() const = 0;
+};
+}  // namespace doodle
