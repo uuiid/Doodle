a9801cfea4435216cecaa3420eff12242739606e
uuiid <957714080@qq.com>
2025-10-27 12:02:52 +0800

    🐛 fix(gui): 修复异常处理宏定义
    - 修复【DOODLE_MAIN_IMPL_】宏中的异常处理，捕获所有异常
    - 添加【std::cout】输出异常信息，方便调试

--- src/exe_gui/main_macro.h
+++ src/exe_gui/main_macro.h
@@ -13,24 +13,22 @@
 
 //  int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR strCmdLine, int nCmdShow) try {
 
-#define DOODLE_MAIN_IMPL_(app_class)                                           \
-  try {                                                                        \
-    mi_version();                                                              \
-    using main_app = app_class;                                                \
-    main_app app{argc, argv};                                                  \
-    try {                                                                      \
-      return app.run();                                                        \
-    } catch (const std::exception& err) {                                      \
-      DOODLE_LOG_WARN(boost::diagnostic_information(err));                     \
-      return 1;                                                                \
-    } catch (...) {                                                            \
-      DOODLE_LOG_ERROR(boost::current_exception_diagnostic_information(true)); \
-      return 1;                                                                \
-    }                                                                          \
-    return 0;                                                                  \
-  } catch (...) {                                                              \
-    doodle::app_base::write_current_error_tmp_dir();                           \
-    return 1;                                                                  \
+#define DOODLE_MAIN_IMPL_(app_class)                                                   \
+  try {                                                                                \
+    mi_version();                                                                      \
+    using main_app = app_class;                                                        \
+    main_app app{argc, argv};                                                          \
+    try {                                                                              \
+      return app.run();                                                                \
+    } catch (...) {                                                                    \
+      DOODLE_LOG_ERROR(boost::current_exception_diagnostic_information(true));         \
+      std::cout << boost::current_exception_diagnostic_information(true) << std::endl; \
+      return 1;                                                                        \
+    }                                                                                  \
+    return 0;                                                                          \
+  } catch (...) {                                                                      \
+    doodle::app_base::write_current_error_tmp_dir();                                   \
+    return 1;                                                                          \
   }
 #define DOODLE_MAIN_IMPL(app_class) int main(int argc, const char* const argv[]) DOODLE_MAIN_IMPL_(app_class)
 #define DOODLE_WMAIN_IMPL(app_class) int _tmain(int argc, const TCHAR* const argv[]) DOODLE_MAIN_IMPL_(app_class)
