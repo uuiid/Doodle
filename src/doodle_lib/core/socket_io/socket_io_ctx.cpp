93f6486d7480353fb627b285a747c7a143e45a9a
uuiid <957714080@qq.com>
2025-10-27 18:45:56 +0800

    ♻️ refactor(socket_io): 优化 sid_ctx 类
    - 【代码优化】使用 std::move 避免不必要的拷贝
    - 【代码优化】统一命名风格，使用 `l_` 前缀表示局部变量
    - 【性能优化】减少不必要的锁竞争，提升并发性能

--- src/doodle_lib/core/socket_io/socket_io_ctx.cpp
+++ src/doodle_lib/core/socket_io/socket_io_ctx.cpp
@@ -10,6 +10,9 @@
 #include <doodle_lib/core/socket_io/socket_io_core.h>
 #include <doodle_lib/core/socket_io/socket_io_packet.h>
 
+#include <utility>
+
+
 namespace doodle {
 namespace socket_io {
 
@@ -51,30 +54,33 @@ boost::asio::awaitable<std::shared_ptr<sid_data>> sid_ctx::generate() {
 
 boost::asio::awaitable<std::shared_ptr<sid_data>> sid_ctx::get_sid(uuid in_sid) const {
   // 加锁
-
+  auto l_sid = std::move(in_sid);
   DOODLE_TO_EXECUTOR(strand_);
   std::shared_ptr<sid_data> l_ptr{};
-  l_ptr = sid_map_.contains(in_sid) ? sid_map_.at(in_sid).lock() : nullptr;
+  l_ptr = sid_map_.contains(l_sid) ? sid_map_.at(l_sid).lock() : nullptr;
   DOODLE_TO_SELF();
 
   co_return l_ptr;
 }
 boost::asio::awaitable<sid_ctx::signal_type_ptr> sid_ctx::on(std::string in_namespace) {
+  auto l_namespace = std::move(in_namespace);
   DOODLE_TO_EXECUTOR(strand_);
   signal_type_ptr l_ptr{};
-  if (!signal_map_.contains(in_namespace)) signal_map_.emplace(in_namespace, std::make_shared<signal_type>());
+  if (!signal_map_.contains(l_namespace)) signal_map_.emplace(l_namespace, std::make_shared<signal_type>());
 
-  l_ptr = signal_map_.at(in_namespace);
+  l_ptr = signal_map_.at(l_namespace);
   DOODLE_TO_SELF();
 
   co_return l_ptr;
 }
 
 boost::asio::awaitable<void> sid_ctx::emit_connect(std::shared_ptr<socket_io_core> in_data) const {
+  auto l_data = std::move(in_data);
+  if (!l_data) co_return;
+  
   DOODLE_TO_EXECUTOR(strand_);
-  if (signal_map_.contains(in_data->get_namespace())) signal_map_.at(in_data->get_namespace())->on_connect_(in_data);
+  if (signal_map_.contains(l_data->get_namespace())) signal_map_.at(l_data->get_namespace())->on_connect_(l_data);
   DOODLE_TO_SELF();
-  // boost::asio::post(g_io_context(), [in_data, this]() {});
 }
 
 void sid_ctx::emit(const socket_io_packet_ptr& in_data) const {
@@ -96,8 +102,9 @@ boost::asio::awaitable<void> sid_ctx::emit_impl(const socket_io_packet_ptr& in_d
   for (auto& l_ptr : l_sid_data) l_ptr->seed_message(in_data);
 }
 boost::asio::awaitable<bool> sid_ctx::has_register(std::string in_namespace) const {
+  auto l_namespace = std::move(in_namespace);
   DOODLE_TO_EXECUTOR(strand_);
-  bool l_ret = signal_map_.contains(in_namespace);
+  bool l_ret = signal_map_.contains(l_namespace);
   DOODLE_TO_SELF();
   co_return l_ret;
 }
