54dbe0d7313a98731726b66f77c43c87410b9c2f
uuiid <957714080@qq.com>
2025-11-04 14:21:24 +0800

    ✨ feat(deployment): 添加部署选项3以支持创建UE插件

--- build_script/deployment_server.ps1
+++ build_script/deployment_server.ps1
@@ -1,6 +1,7 @@
 ﻿param (
     [switch]$CopyServer,
-    [switch]$BuildKitsu
+    [switch]$BuildKitsu,
+    [switch]$CreateUEPlugins
 )
 
 $OutputEncoding = [System.Text.Encoding]::UTF8
@@ -8,12 +9,12 @@ $OutputEncoding = [System.Text.Encoding]::UTF8
 Import-Module -Name $PSScriptRoot\DoodlePackageFun.psm1 -Force
 
 $DoodleOut = Convert-Path "$PSScriptRoot/../build/pack"
-Initialize-Doodle -OutPath $DoodleOut -BuildKitsu:$BuildKitsu
+Initialize-Doodle -OutPath $DoodleOut -BuildKitsu:$BuildKitsu -CreateUEPlugins:$CreateUEPlugins
 &robocopy "$DoodleOut" "\\192.168.40.181\Dev\tmp" /s | Out-Null
 &robocopy "$DoodleOut\dist\assets" "\\192.168.40.181\Dev\tmp\dist\assets" /MIR | Out-Null
 
 $RootPassword = ConvertTo-SecureString "root" -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList administrator,$RootPassword
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList administrator, $RootPassword
 #Enter-PSSession -ComputerName 192.168.40.181 -Credential $Credential -Authentication Basic
 Invoke-Command -ComputerName 192.168.40.181 -Credential $Credential -Authentication Basic -ScriptBlock {
     #    Compare-Object -ReferenceObject (Get-Content -Path "D:\tmp\bin\file_association_http.exe") -DifferenceObject (Get-Content -Path "D:\kitsu\bin\file_association_http.exe")
@@ -21,8 +22,7 @@ Invoke-Command -ComputerName 192.168.40.181 -Credential $Credential -Authenticat
     $Tmp = "D:\tmp"
     $timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
     $LogPath = "$env:TEMP\build_$timestamp.log"
-    if ($Using:CopyServer -and ((Get-FileHash "$Target\bin\doodle_kitsu_supplement.exe").Hash -ne (Get-FileHash "$Tmp\bin\doodle_kitsu_supplement.exe").Hash))
-    {
+    if ($Using:CopyServer -and ((Get-FileHash "$Target\bin\doodle_kitsu_supplement.exe").Hash -ne (Get-FileHash "$Tmp\bin\doodle_kitsu_supplement.exe").Hash)) {
         Write-Host "更新服务"
         Stop-Service -Force -Name doodle_kitsu_supplement
         Start-Sleep -Milliseconds 50
