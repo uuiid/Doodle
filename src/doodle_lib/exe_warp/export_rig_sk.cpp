03ab7bfcbc2b17874481cae9966092f44bd54431
uuiid <957714080@qq.com>
2025-11-04 17:52:11 +0800

    ✨ feat(export_rig_sk): 重构导出逻辑，添加文件复制和JSON序列化支持

--- src/doodle_lib/exe_warp/export_rig_sk.cpp
+++ src/doodle_lib/exe_warp/export_rig_sk.cpp
@@ -17,89 +17,50 @@
 namespace doodle {
 
 boost::asio::awaitable<void> export_rig_sk_arg::run() {
-  if (asset_type_id_.is_nil()) throw_exception(doodle_error{"asset_type_id 不能为空"});
-
   auto l_arg = std::make_shared<maya_exe_ns::export_rig_arg>();
   l_arg->set_file_path(maya_file_);
   l_arg->set_logger(logger_ptr_);
   co_await l_arg->async_run_maya();
   auto l_maya_file = l_arg->get_out_arg();
-  FSys::path l_import_game_path{doodle_config::ue4_game};
-  if (asset_type_id_ == asset_type::get_character_id()) {
-    l_import_game_path /= "Character";
-    l_import_game_path /= pin_yin_ming_cheng_;
-    l_import_game_path /= "Meshs";
-  } else if (asset_type_id_ == asset_type::get_prop_id()) {
-    l_import_game_path /= "Prop";
-    l_import_game_path /= pin_yin_ming_cheng_;
-    l_import_game_path /= "Mesh";
-  } else if (asset_type_id_ == asset_type::get_ground_id()) {
-    l_import_game_path /= pin_yin_ming_cheng_;
-    l_import_game_path /= "SK";
-  }
 
-  else
-    throw_exception(doodle_error{"不支持的类型 {}", asset_type_id_});
+  for (auto&& p : impl_.ue_asset_copy_path_) {
+    logger_ptr_->info("复制UE资源文件 from {} to {}", p.from_, p.to_);
+    FSys::copy_diff(p.from_, p.to_, logger_ptr_);
+  }
 
   if (l_maya_file.out_file_list.empty()) throw_exception(doodle_error{"文件 {}, 未能输出骨架fbx", maya_file_});
   auto l_fbx = l_maya_file.out_file_list.front().out_file;
 
   if (!FSys::exists(l_fbx)) throw_exception(doodle_error{"文件 {} 不存在", l_fbx});
-  {
-    std::string l_new_fbx_name{};
-    if (asset_type_id_ == asset_type::get_character_id()) {
-      l_new_fbx_name = fmt::format("SK_Ch{}.fbx", bian_hao_, pin_yin_ming_cheng_);
-    } else if (asset_type_id_ == asset_type::get_prop_id()) {
-      l_new_fbx_name = fmt::format("SK_{}{}{}.fbx", pin_yin_ming_cheng_, ban_ben_.empty() ? "" : "_", ban_ben_);
-    } else if (asset_type_id_ == asset_type::get_ground_id()) {
-      l_new_fbx_name = fmt::format("SK_{}{}{}.fbx", pin_yin_ming_cheng_, ban_ben_.empty() ? "" : "_", ban_ben_);
-    }
-    logger_ptr_->info("重命名 {} 为 {}", l_fbx.filename(), l_new_fbx_name);
-    FSys::rename(l_fbx, l_fbx.parent_path() / l_new_fbx_name);
-    l_fbx = l_fbx.parent_path() / l_new_fbx_name;
-  }
+  logger_ptr_->info("重命名 {} 为 {}", l_fbx.filename(), impl_.import_game_path_.stem());
+  FSys::rename(l_fbx, l_fbx.parent_path() / impl_.import_game_path_.stem().replace_extension(l_fbx.extension()));
 
   nlohmann::json l_json{};
-  l_json            = import_and_render_ue_ns::import_skin_file{.fbx_file_ = l_fbx, .import_dir_ = l_import_game_path};
+  l_json = import_and_render_ue_ns::import_skin_file{
+      .fbx_file_ = l_fbx, .import_dir_ = impl_.import_game_path_.parent_path()
+  };
   auto l_tmp_path   = FSys::write_tmp_file("ue_import", l_json.dump(), ".json");
 
-  auto l_ue_project = ue_exe_ns::find_ue_project_file(ue_path_);
-  if (l_ue_project.empty()) throw doodle_error{"无法找到UE项目文件 {}", ue_path_};
-  auto l_local_ue_project =
-      core_set::get_set().get_cache_root(project_name_ / l_ue_project.stem()) / l_ue_project.filename();
-  auto l_local_ue_project_dir = l_local_ue_project.parent_path();
+  auto l_ue_project = ue_exe_ns::find_ue_project_file(impl_.ue_project_path_);
+  if (l_ue_project.empty()) throw doodle_error{"无法找到UE项目文件 {}", impl_.ue_project_path_};
 
-  FSys::copy_diff(
-      l_ue_project.parent_path() / doodle_config::ue4_content, l_local_ue_project_dir / doodle_config::ue4_content,
-      logger_ptr_
-  );
-  FSys::copy_diff(
-      l_ue_project.parent_path() / doodle_config::ue4_config, l_local_ue_project_dir / doodle_config::ue4_config,
-      logger_ptr_
-  );
-  FSys::copy_diff(l_ue_project, l_local_ue_project, logger_ptr_);
-
-  logger_ptr_->warn("排队导入skin文件 {} ", l_local_ue_project);
+  logger_ptr_->warn("排队导入skin文件 {} ", l_ue_project);
   auto l_time_info = std::make_shared<server_task_info::run_time_info_t>();
   co_await async_run_ue(
-      {l_local_ue_project.generic_string(), "-windowed", "-log", "-stdout", "-AllowStdOutLogVerbosity",
-       "-ForceLogFlush", "-Unattended", "-run=DoodleAutoAnimation", fmt::format("-ImportRig={}", l_tmp_path)},
+      {l_ue_project.generic_string(), "-windowed", "-log", "-stdout", "-AllowStdOutLogVerbosity", "-ForceLogFlush",
+       "-Unattended", "-run=DoodleAutoAnimation", fmt::format("-ImportRig={}", l_tmp_path)},
       logger_ptr_, false, l_time_info
   );
   l_time_info->info_ = fmt::format("导入skin文件 {}", l_fbx);
   on_run_time_info_(*l_time_info);
 
   // 上传文件
-  logger_ptr_->warn(
-      "上传文件 {} 到项目 {}", l_local_ue_project_dir / doodle_config::ue4_content,
-      l_ue_project.parent_path() / doodle_config::ue4_content
+  logger_ptr_->warn("上传文件 {} ", impl_.update_ue_path_);
+  co_await kitsu_client_->upload_asset_file_ue(
+      task_id_, std::make_shared<std::vector<FSys::path>>(std::vector<FSys::path>{impl_.update_ue_path_})
   );
-  FSys::copy_diff(
-      l_local_ue_project_dir / doodle_config::ue4_content, l_ue_project.parent_path() / doodle_config::ue4_content,
-      logger_ptr_
-  );
+
   co_return;
 }
 
-FSys::path export_rig_sk_arg::get_result() const { return result_path_; }
 }  // namespace doodle
--- src/doodle_lib/exe_warp/export_rig_sk.h
+++ src/doodle_lib/exe_warp/export_rig_sk.h
@@ -3,47 +3,63 @@
 #include <doodle_core/metadata/server_task_info.h>
 
 #include <doodle_lib/core/asyn_task.h>
+#include <doodle_lib/http_client/kitsu_client.h>
 
 #include <boost/signals2.hpp>
 
 #include <filesystem>
 #include <string>
 
+
 namespace doodle {
 class export_rig_sk_arg : public async_task {
  private:
-  mutable FSys::path result_path_;
-
  public:
+  struct file_copy_info {
+    FSys::path from_;
+    FSys::path to_;
+    // to json
+    friend void to_json(nlohmann::json& j, const file_copy_info& p) {
+      j["from"] = p.from_;
+      j["to"]   = p.to_;
+    }
+    // from json
+    friend void from_json(const nlohmann::json& j, file_copy_info& p) {
+      j.at("from").get_to(p.from_);
+      j.at("to").get_to(p.to_);
+    }
+  };
+  struct data_t {
+    FSys::path import_game_path_{};
+    FSys::path update_ue_path_{};
+    FSys::path ue_project_path_{};
+    std::vector<file_copy_info> ue_asset_copy_path_;  // 需要复制的UE路径
+                                                      // to json
+    friend void to_json(nlohmann::json& j, const data_t& p) {
+      j["import_game_path"]   = p.import_game_path_;
+      j["update_ue_path"]     = p.update_ue_path_;
+      j["ue_project_path"]    = p.ue_project_path_;
+      j["ue_asset_copy_path"] = p.ue_asset_copy_path_;
+    }
+    // from json
+    friend void from_json(const nlohmann::json& j, data_t& p) {
+      j.at("import_game_path").get_to(p.import_game_path_);
+      j.at("update_ue_path").get_to(p.update_ue_path_);
+      j.at("ue_project_path").get_to(p.ue_project_path_);
+      j.at("ue_asset_copy_path").get_to(p.ue_asset_copy_path_);
+    }
+  };
+  data_t impl_{};
   FSys::path maya_file_{};
-  FSys::path ue_path_{};
-  uuid asset_type_id_{};
-  std::string bian_hao_{};
-  std::string pin_yin_ming_cheng_{};
-  std::string ban_ben_{};
-  FSys::path project_name_{};
+  uuid task_id_{};
+  std::shared_ptr<kitsu::kitsu_client> kitsu_client_{};
+
   boost::signals2::signal<void(const server_task_info::run_time_info_t&)> on_run_time_info_;
-  // 获取结果
-  FSys::path get_result() const;
 
   // from json
-  friend void from_json(const nlohmann::json& in_json, export_rig_sk_arg& out_obj) {
-    if (in_json.contains("maya_file")) in_json.at("maya_file").get_to(out_obj.maya_file_);
-    if (in_json.contains("ue_path")) in_json.at("ue_path").get_to(out_obj.ue_path_);
-    if (in_json.contains("asset_type_id")) in_json.at("asset_type_id").get_to(out_obj.asset_type_id_);
-    if (in_json.contains("bian_hao")) in_json.at("bian_hao").get_to(out_obj.bian_hao_);
-    if (in_json.contains("pin_yin_ming_cheng")) in_json.at("pin_yin_ming_cheng").get_to(out_obj.pin_yin_ming_cheng_);
-    if (in_json.contains("ban_ben")) in_json.at("ban_ben").get_to(out_obj.ban_ben_);
-  }
+  friend void from_json(const nlohmann::json& in_json, export_rig_sk_arg& out_obj) { in_json.get_to(out_obj.impl_); }
   // to json
-  friend void to_json(nlohmann::json& in_json, const export_rig_sk_arg& out_obj) {
-    in_json["maya_file"]          = out_obj.maya_file_;
-    in_json["ue_path"]            = out_obj.ue_path_;
-    in_json["asset_type_id"]      = out_obj.asset_type_id_;
-    in_json["bian_hao"]           = out_obj.bian_hao_;
-    in_json["pin_yin_ming_cheng"] = out_obj.pin_yin_ming_cheng_;
-    in_json["ban_ben"]            = out_obj.ban_ben_;
-  }
+  friend void to_json(nlohmann::json& in_json, const export_rig_sk_arg& out_obj) { in_json = out_obj.impl_; }
 
   boost::asio::awaitable<void> run() override;
 };
