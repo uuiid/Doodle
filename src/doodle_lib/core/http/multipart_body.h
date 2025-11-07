468e8052064afb2ad2be96a927dd7d05e4a82189
uuiid <957714080@qq.com>
2025-11-07 14:24:14 +0800

    ✨ feat(multipart): 在multipart_body中添加文件输出支持

--- src/doodle_lib/core/http/multipart_body.h
+++ src/doodle_lib/core/http/multipart_body.h
@@ -201,6 +201,8 @@ struct multipart_body {
         }
       }
       if (l_reader->out_file_) {
+        auto l_str = boost::beast::make_printable(boost::asio::buffer(in_begin, in_length));
+        (*l_reader->out_file_) << l_str;
         // l_reader->out_file_->write(in_view.data(), in_view.length());
       } else {
         std::get<std::string>(l_reader->part_.body_).append(in_begin, in_begin + in_length);
