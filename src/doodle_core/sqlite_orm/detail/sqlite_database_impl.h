a25b48ab52073620f1f19de9ef509e4c6dcef0bf
uuiid <957714080@qq.com>
2025-11-11 16:51:38 +0800

    🐛 fix(entity): 移除entity_shot_extend表及其相关索引

--- src/doodle_core/sqlite_orm/detail/sqlite_database_impl.h
+++ src/doodle_core/sqlite_orm/detail/sqlite_database_impl.h
@@ -474,14 +474,6 @@ inline auto make_storage_doodle(const std::string& in_path) {
           foreign_key(&entity_concept_link::entity_id_).references(&entity::uuid_id_),     //
           foreign_key(&entity_concept_link::entity_out_id_).references(&entity::uuid_id_)  //
       ),
-      make_index("entity_shot_extend_entity_id_idx", &entity_shot_extend::entity_id_),
-      make_table<entity_shot_extend>(
-        "entity_shot_extend",
-        make_column("id", &entity_shot_extend::id_, primary_key().autoincrement()),
-        make_column("uuid", &entity_shot_extend::uuid_id_, unique(), not_null()),
-        make_column("entity_id", &entity_shot_extend::entity_id_),
-        foreign_key(&entity_shot_extend::entity_id_).references(&entity::uuid_id_).on_delete.cascade()
-      ),
       make_index("entity_asset_extend_entity_id_idx", &entity_asset_extend::entity_id_),
       make_table<entity_asset_extend>(
           "entity_asset_extend",                                                           //
