2847ec714d452fd9be2dfe4b544a4acc2ffb5fd3
uuiid <957714080@qq.com>
2025-11-11 13:27:33 +0800

    🐛 fix(kitsu): 添加新的API路由用于运行UE组装

--- src/doodle_lib/http_method/kitsu.cpp
+++ src/doodle_lib/http_method/kitsu.cpp
@@ -250,7 +250,12 @@ http_route_ptr create_kitsu_local_route() {
             "/api/doodle/task/{}/generate_uesk_file"_url(&local::task_instance_generate_uesk_file::id_)
         )
         .reg_t<ai_train_binding_weights>("/api/doodle/ai/train-binding-weights"_url)
-
+        .reg_t<local::actions_projects_shots_run_ue_assembly_local>(
+            "/api/actions/projects/{}/shots/{}/run-ue-assembly"_url(
+                &local::actions_projects_shots_run_ue_assembly_local::project_id_,
+                &local::actions_projects_shots_run_ue_assembly_local::id_
+            )
+        )
         .reg_t<socket_io::socket_io_http>(R"(/socket.io)"_url, l_sid_ctx)
 
         ;
