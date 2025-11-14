341c2a091d93bfcf0d353d4f2b7514965ba8747e
uuiid <957714080@qq.com>
2025-11-14 13:12:44 +0800

    🐛 fix(kitsu_auth.cpp): 更新up_file_ue和create_comment_task测试用例，确保正确上传文件和创建评论

--- src/test/http_client/kitsu_auth.cpp
+++ src/test/http_client/kitsu_auth.cpp
@@ -54,9 +54,24 @@ BOOST_AUTO_TEST_CASE(up_file_ue) {
         auto l_c = std::make_shared<::doodle::kitsu::kitsu_client>(core_set::get_set().server_ip);
         co_await l_c->upload_asset_file_ue(
             from_uuid_str("87ddbc98-61ef-4cf7-b23c-619c646685bc"),
-            std::make_shared<std::vector<FSys::path>>(
-                std::vector<FSys::path>{"E:/doodle_plug_dev_5/Content/test"}
-            )
+            std::make_shared<std::vector<FSys::path>>(std::vector<FSys::path>{"E:/doodle_plug_dev_5/Content/test"})
+        );
+        app_base::Get().on_cancel.emit();
+      }(),
+      boost::asio::use_future
+  );
+  l_app_base.run();
+  l_f.get();
+}
+BOOST_AUTO_TEST_CASE(create_comment_task) {
+  app_base l_app_base{};
+  auto l_f = boost::asio::co_spawn(
+      g_io_context(),
+      []() -> boost::asio::awaitable<void> {
+        auto l_c = std::make_shared<::doodle::kitsu::kitsu_client>(core_set::get_set().server_ip);
+        co_await l_c->comment_task(
+            from_uuid_str("d7210472-de23-4b76-8332-3470e0442190"), "测试通过客户端创建评论",
+            "D:/test_files/test_xgen/scenes/ce.ma"
         );
         app_base::Get().on_cancel.emit();
       }(),
