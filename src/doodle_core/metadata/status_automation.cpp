c6cf5d84c638986d540d0c45ba599eb6ef29c25c
uuiid <957714080@qq.com>
2025-11-20 18:43:47 +0800

    feat(status_automation): 修复任务状态更新逻辑并优化评论创建

--- src/doodle_core/metadata/status_automation.cpp
+++ src/doodle_core/metadata/status_automation.cpp
@@ -12,10 +12,11 @@
 #include "core/chrono_.h"
 #include "sqlite_orm/sqlite_database.h"
 #include <chrono>
+#include <memory>
 
 namespace doodle {
 boost::asio::awaitable<void> status_automation::run(const std::shared_ptr<task>& in_task, const uuid& in_person_id) {
-  if (!archived_ || in_task->task_type_id_ != in_task_type_id_ || in_task->task_status_id_ != in_task_status_id_)
+  if (archived_ || in_task->task_type_id_ != in_task_type_id_ || in_task->task_status_id_ != in_task_status_id_)
     co_return;
   auto l_sql = g_ctx().get<sqlite_database>();
   if (auto l_map = l_sql.get_task_type_priority_map(in_task->project_id_, entity_type_);
@@ -28,18 +29,22 @@ boost::asio::awaitable<void> status_automation::run(const std::shared_ptr<task>&
       if (auto l_task = l_sql.get_tasks_for_entity_and_task_type(in_task->entity_id_, out_task_type_id_); l_task) {
         auto l_task_type            = l_sql.get_by_uuid<task_type>(in_task_type_id_);
         auto l_task_status          = l_sql.get_by_uuid<task_status>(in_task_status_id_);
+        auto l_task_out_status      = l_sql.get_by_uuid<task_status>(out_task_status_id_);
         auto l_comment              = std::make_shared<comment>(comment{
                          .uuid_id_    = core_set::get_set().get_uuid(),
-                         .object_id_  = in_task->uuid_id_,
+                         .object_id_  = l_task->uuid_id_,
                          .text_       = fmt::format("自动化任务 {}更改触发, 设置状态{} ", l_task_type.name_, l_task_status.name_),
                          .created_at_ = chrono::system_zoned_time{chrono::current_zone(), std::chrono::system_clock::now()},
                          .updated_at_ = chrono::system_zoned_time{chrono::current_zone(), std::chrono::system_clock::now()},
                          .task_status_id_ = l_task_status.uuid_id_,
                          .person_id_      = in_person_id,
         });
+        l_task->task_status_id_     = l_task_out_status.uuid_id_;
         in_task->task_status_id_    = l_task_status.uuid_id_;
         in_task->last_comment_date_ = l_comment->created_at_;
         in_task->updated_at_        = l_comment->updated_at_;
+        auto l_tasl_tmp = std::make_shared<task>(*l_task);
+        co_await l_sql.install(l_tasl_tmp);
         co_await l_sql.install(l_comment);
         co_await l_sql.install(in_task);
       }
