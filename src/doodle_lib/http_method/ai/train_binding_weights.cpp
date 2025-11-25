d22724146dd27636abb2e2d86854a4006eeb5792
uuiid <957714080@qq.com>
2025-11-25 12:02:29 +0800

    feat(ai_train_binding_weights): 使用异步任务处理训练权重，提高性能

--- src/doodle_lib/http_method/ai/train_binding_weights.cpp
+++ src/doodle_lib/http_method/ai/train_binding_weights.cpp
@@ -1,8 +1,13 @@
+#include "doodle_core/core/global_function.h"
+
 #include <doodle_lib/ai/bone_weight_inference.h>
+
+#include <boost/asio/post.hpp>
 
 #include "ai_main.h"
 #include <filesystem>
 #include <vector>
+
 
 namespace doodle::http {
 
@@ -20,7 +25,9 @@ boost::asio::awaitable<boost::beast::http::message_generator> ai_train_binding_w
     session_data_ptr in_handle
 ) {
   auto l_args = in_handle->get_json().get<ai_train_binding_weights_args>();
-  ai::bone_weight_inference_model::train(l_args.input_path_, l_args.output_path_);
+  boost::asio::post(g_io_context(), [l_args]() {
+    doodle::ai::bone_weight_inference_model::train(l_args.input_path_, l_args.output_path_);
+  });
   co_return in_handle->make_msg(nlohmann::json{});
 }
 }  // namespace doodle::http
