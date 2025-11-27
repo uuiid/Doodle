93131d6542d4b1a583c02d44087126a4ebf47a96
uuiid <957714080@qq.com>
2025-11-27 18:28:08 +0800

    feat(open_sercer): 添加 Get-NssmSer 函数以获取服务状态并调用

--- build_script/open_sercer.ps1
+++ build_script/open_sercer.ps1
@@ -2,7 +2,6 @@ Import-Module -Name $PSScriptRoot\DoodlePackageFun.psm1 -Force
 $NewSession = New-ServerPSSession
 # (Get-Service "doodle_kitsu_*" | Sort-Object Status |  Format-List Name, Status, StartType)
 Invoke-Command -Session $NewSession -ScriptBlock {
-  Get-Service "doodle_kitsu_*" | Sort-Object Status |  Format-List Name, Status, StartType
   function Get-NssmLog {
     (Get-WinEvent -FilterHashtable @{
       Logname      = 'Application'
@@ -10,7 +9,13 @@ Invoke-Command -Session $NewSession -ScriptBlock {
       StartTime    = ((Get-Date).AddDays(-1))
     })[0..10] |  Format-List -Property TimeCreated, Message
   }
+
+  function Get-NssmSer {
+    Get-Service "doodle_kitsu_*" | Sort-Object Status |  Format-List Name, Status, StartType
+  }
+
   Get-NssmLog
+  Get-NssmSer
 }
 
 Enter-PSSession -Session $NewSession 
