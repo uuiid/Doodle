77c8db101b4c12db49d93163240491e995675cfe
uuiid <957714080@qq.com>
2025-11-28 10:28:24 +0800

    feat(ai_train_binding_weights): 添加 PUT 方法以支持模型推理和绑定权重

--- src/doodle_lib/http_method/ai/ai_main.h
+++ src/doodle_lib/http_method/ai/ai_main.h
@@ -9,5 +9,6 @@ namespace doodle::http {
 // /api/doodle/ai/train-binding-weights
 DOODLE_HTTP_FUN(ai_train_binding_weights)
 DOODLE_HTTP_FUN_OVERRIDE(post)
+DOODLE_HTTP_FUN_OVERRIDE(put)
 DOODLE_HTTP_FUN_END()
 }  // namespace doodle::http
--- src/doodle_lib/http_method/ai/train_binding_weights.cpp
+++ src/doodle_lib/http_method/ai/train_binding_weights.cpp
@@ -6,28 +6,54 @@
 
 #include "ai_main.h"
 #include <filesystem>
+#include <spdlog/spdlog.h>
 #include <vector>
 
-
 namespace doodle::http {
-
-struct ai_train_binding_weights_args {
+namespace {
+struct ai_train_binding_weights_post_args {
   std::vector<FSys::path> input_path_{};
   FSys::path output_path_{};
 
   // from json
-  friend void from_json(const nlohmann::json& in_json, ai_train_binding_weights_args& out) {
+  friend void from_json(const nlohmann::json& in_json, ai_train_binding_weights_post_args& out) {
     in_json.at("input_path").get_to(out.input_path_);
     in_json.at("output_path").get_to(out.output_path_);
   }
 };
+struct ai_train_binding_weights_put_args {
+  FSys::path model_path_{};
+  FSys::path fbx_path_{};
+
+  // from json
+  friend void from_json(const nlohmann::json& in_json, ai_train_binding_weights_put_args& out) {
+    in_json.at("model_path").get_to(out.model_path_);
+    in_json.at("fbx_path").get_to(out.fbx_path_);
+  }
+};
+}  // namespace
 boost::asio::awaitable<boost::beast::http::message_generator> ai_train_binding_weights::post(
     session_data_ptr in_handle
 ) {
-  auto l_args = in_handle->get_json().get<ai_train_binding_weights_args>();
+  auto l_args = in_handle->get_json().get<ai_train_binding_weights_post_args>();
   boost::asio::post(g_io_context(), [l_args]() {
     doodle::ai::bone_weight_inference_model::train(l_args.input_path_, l_args.output_path_);
   });
   co_return in_handle->make_msg(nlohmann::json{});
 }
+
+boost::asio::awaitable<boost::beast::http::message_generator> ai_train_binding_weights::put(
+    session_data_ptr in_handle
+) {
+  auto l_args     = in_handle->get_json().get<ai_train_binding_weights_put_args>();
+  auto l_out_path = l_args.model_path_.parent_path() / (l_args.model_path_.stem().string() + "_bound.fbx");
+  boost::asio::post(g_io_context(), [l_args, l_out_path]() {
+    doodle::ai::bone_weight_inference_model l_model{l_args.model_path_};
+    SPDLOG_WARN("开始推理模型 {} {} {}", l_args.model_path_, l_args.fbx_path_, l_out_path);
+    l_model.predict_by_fbx(l_args.fbx_path_, l_out_path);
+  });
+
+  co_return in_handle->make_msg(nlohmann::json{});
+}
+
 }  // namespace doodle::http
