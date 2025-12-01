733e94864a7c53670b9cf1edb5c213cbe32ca3c9
uuiid <957714080@qq.com>
2025-12-01 16:35:37 +0800

    fix(package_epiboly): 修复压缩包和版本命名逻辑，确保正确处理文件路径和版本后缀

--- build_script/package_epiboly.ps1
+++ build_script/package_epiboly.ps1
@@ -4,20 +4,33 @@ Import-Module -Name $PSScriptRoot\DoodlePackageFun.psm1 -Force
 
 $DoodleExePath = "E:\source\doodle\dist\win-unpacked\*"
 $DoodleOut = Convert-Path "$PSScriptRoot/../build"
-Remove-Item -Path "$DoodleOut/epiboly" -Include "*.zip" -Recurse -Force
 
 Initialize-Doodle -OutPath "$DoodleOut/epiboly" -OnlyOne
 Copy-Item $DoodleExePath -Destination "$DoodleOut/epiboly" -Recurse -Force
-Remove-Item -Path "$DoodleOut/epiboly/dist/Plugins/*.zip" -Force
+Remove-Item -Path "$DoodleOut/epiboly" -Include "*.zip" -Recurse -Force
+Remove-Item -Path "$DoodleOut/epiboly/dist" -Include "*.exe" -Recurse -Force
+
+$DataSource = "$DoodleOut/epiboly/epiboly.database"
+Copy-Item "\\192.168.40.181\Dev\kitsu_new.database" $DataSource -Force
+$Tabls = Invoke-SqliteQuery -DataSource $DataSource -Query "SELECT name FROM sqlite_master WHERE type='table';"
+foreach ($Tab in $Tabls) {
+    if ($Tab.name -eq "sqlite_sequence") { continue }
+    if ($Tab.name -eq "project") { continue }
+    if ($Tab.name -eq "project_status") { continue }
+    Invoke-SqliteQuery -DataSource $DataSource -Query "DROP TABLE $($Tab.name);"
+}
+Invoke-SqliteQuery -DataSource $DataSource -Query "VACUUM;"
 
 Compress-Archive -Path "$DoodleOut/epiboly/*" -DestinationPath "$DoodleOut/epiboly.zip" -Force
-$DoodleName = (Get-ChildItem "$DoodleOut/epiboly/dist/" -Filter "*.zip")[-1].Name.Split("-")[1] + ".0"
-$DoodleVersion = 0
+$Tags = git tag --sort=-v:refname;
+# 去除 前缀 v
+$Tags = $Tags | ForEach-Object { $_ -replace "v", "" }
+$DoodleVersion = $Tags[0]
+$DoodleVersionSuf = 0
 
-while (Test-Path -Path "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName.$DoodleVersion-win64.zip")
-{
-    $DoodleVersion += 1
-    Write-Host "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName.$DoodleVersion-win64.zip";
+while (Test-Path -Path "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleVersion.$DoodleVersionSuf-win64.zip") {
+    $DoodleVersionSuf += 1
+    Write-Host "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleVersion.$DoodleVersionSuf-win64.zip";
 }
 
-Copy-Item "$DoodleOut/epiboly.zip" -Destination "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleName.$DoodleVersion-win64.zip" -Force
+Copy-Item "$DoodleOut/epiboly.zip" -Destination "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleVersion.$DoodleVersionSuf-win64.zip" -Force
