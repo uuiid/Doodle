4e0ff0b70c224d496ad172980c74769c0d7ab452
uuiid <957714080@qq.com>
2025-12-03 14:52:08 +0800

    refactor(package_epiboly): 添加版本提取和打包输出信息

--- build_script/package_epiboly.ps1
+++ build_script/package_epiboly.ps1
@@ -1,6 +1,11 @@
 ﻿$OutputEncoding = [System.Text.Encoding]::UTF8
 
 Import-Module -Name $PSScriptRoot\DoodlePackageFun.psm1 -Force
+$Tags = git tag --sort=-v:refname;
+# 去除 前缀 v
+$Tags = $Tags | ForEach-Object { $_ -replace "v", "" }
+$DoodleVersion = $Tags[0]
+Write-Host "Packaging Doodle version $DoodleVersion for Epiboly"
 
 $DoodleExePath = "E:\source\doodle\dist\win-unpacked\*"
 $DoodleOut = Convert-Path "$PSScriptRoot/../build"
@@ -8,6 +13,9 @@ Remove-Item -Path "$DoodleOut/epiboly/*" -Recurse -Force
 Initialize-Doodle -OutPath "$DoodleOut/epiboly" -OnlyOne
 
 Copy-Item $DoodleExePath -Destination "$DoodleOut/epiboly" -Recurse -Force
+Expand-Archive -Path "$DoodleOut/Ninja_release/Doodle-$DoodleVersion-win64.zip" -DestinationPath "$DoodleOut/epiboly" -Force
+Copy-Item -Path "$DoodleOut/epiboly/Doodle-$DoodleVersion-win64/maya" -Destination "$DoodleOut/epiboly/maya" -Recurse -Force
+Remove-Item -Path "$DoodleOut/epiboly/Doodle-$DoodleVersion-win64" -Recurse -Force
 Remove-Item -Path "$DoodleOut/epiboly" -Include "*.zip" -Recurse -Force
 Remove-Item -Path "$DoodleOut/epiboly/dist" -Include "*.exe" -Recurse -Force
 
@@ -23,10 +31,7 @@ foreach ($Tab in $Tabls) {
 Invoke-SqliteQuery -DataSource $DataSource -Query "VACUUM;"
 
 Compress-Archive -Path "$DoodleOut/epiboly/*" -DestinationPath "$DoodleOut/epiboly.zip" -Force
-$Tags = git tag --sort=-v:refname;
-# 去除 前缀 v
-$Tags = $Tags | ForEach-Object { $_ -replace "v", "" }
-$DoodleVersion = $Tags[0]
+
 $DoodleVersionSuf = 0
 
 while (Test-Path -Path "\\192.168.0.67\soft\TD软件\doodle外包\epiboly-Doodle-$DoodleVersion.$DoodleVersionSuf-win64.zip") {
