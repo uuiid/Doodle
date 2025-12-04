7ca3a738b6144e8c7e7ff4c8f794e7f71b69e52b
uuiid <957714080@qq.com>
2025-12-04 16:11:40 +0800

    feat: 优化任务同步功能，增加文件列表去重和任务ID记录

--- src/doodle_lib/exe_warp/task_sync.cpp
+++ src/doodle_lib/exe_warp/task_sync.cpp
@@ -1,10 +1,58 @@
 #include "doodle_lib/exe_warp/task_sync.h"
 
+#include "doodle_core/core/file_sys.h"
+
+#include <filesystem>
+
 namespace doodle {
+
+task_sync::args& task_sync::args::operator+=(const args& in_other) {
+  update_file_list_.insert(
+      update_file_list_.end(), in_other.update_file_list_.begin(), in_other.update_file_list_.end()
+  );
+  download_file_list_.insert(
+      download_file_list_.end(), in_other.download_file_list_.begin(), in_other.download_file_list_.end()
+  );
+
+  // clear duplicate
+
+  if (auto l_update_end = std::unique(update_file_list_.begin(), update_file_list_.end());
+      l_update_end != update_file_list_.end())
+    update_file_list_.erase(l_update_end, update_file_list_.end());
+
+  if (auto l_download_end = std::unique(download_file_list_.begin(), download_file_list_.end());
+      l_download_end != download_file_list_.end())
+    download_file_list_.erase(l_download_end, download_file_list_.end());
+  return *this;
+}
+
 boost::asio::awaitable<void> task_sync::run() {
+  args l_total_args;
+  for (auto&& l_task_id : task_ids_) {
+    auto l_file_sync_info = co_await kitsu_client_->get_task_sync(l_task_id);
+    auto l_arg            = l_file_sync_info.get<args>();
+    for (auto&& l_info : l_arg.update_file_list_) l_info.task_id_ = l_task_id;
+    l_total_args += l_arg;
+  }
+  auto l_root = core_set::get_set().user_work_root_;
 
+  if (download_)
+    for (auto&& l_info : l_total_args.download_file_list_) FSys::copy_diff(l_info.from_path_, l_root / l_info.to_path_);
 
+  if (update_) {
+    for (auto&& l_info : l_total_args.update_file_list_) {
+      auto l_local_path = l_root / l_info.from_path_;
+      if (!FSys::exists(l_local_path)) continue;
+      for (auto&& p : FSys::recursive_directory_iterator(l_local_path)) {
+        if (p.is_directory()) continue;
+        auto l_relative_path = p.path().lexically_relative(l_local_path);
+        auto l_remote_path   = l_info.to_path_ / l_relative_path;
+        if (FSys::is_diff(p.path(), l_remote_path))
+          co_await kitsu_client_->upload_shot_animation_ue(l_info.task_id_, p.path(), l_relative_path.generic_string());
+      }
+    }
+  }
 
-  co_return ;
+  co_return;
 }
 }  // namespace doodle
--- src/doodle_lib/exe_warp/task_sync.h
+++ src/doodle_lib/exe_warp/task_sync.h
@@ -16,9 +16,17 @@ class DOODLELIB_API task_sync : public async_task {
 
   std::vector<uuid> task_ids_{};
   std::shared_ptr<kitsu::kitsu_client> kitsu_client_{};
+
+  bool update_{true};
+  bool download_{true};
+
   struct copy_info {
     FSys::path from_path_{};
     FSys::path to_path_{};
+
+    // 此处记录一下文件对对应的task id , 这个是上传时专用的, 下载时忽略
+    uuid task_id_{};
+
     // to json
     friend void to_json(nlohmann::json& out_json, const copy_info& in_obj) {
       out_json["from_path"] = in_obj.from_path_;
@@ -28,6 +36,10 @@ class DOODLELIB_API task_sync : public async_task {
     friend void from_json(const nlohmann::json& in_json, copy_info& out_obj) {
       in_json.at("from_path").get_to(out_obj.from_path_);
       in_json.at("to_path").get_to(out_obj.to_path_);
+    }
+    // equal
+    bool operator==(const copy_info& in_other) const {
+      return from_path_ == in_other.from_path_ && to_path_ == in_other.to_path_;
     }
   };
 
@@ -39,6 +51,8 @@ class DOODLELIB_API task_sync : public async_task {
       out_json["update_file_list"]   = in_obj.update_file_list_;
       out_json["download_file_list"] = in_obj.download_file_list_;
     }
+
+    args& operator+=(const args& in_other);
   };
 
   boost::asio::awaitable<void> run() override;
