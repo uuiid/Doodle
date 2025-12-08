ba2d00648e9e246440d5088f8759d6208a4d636b
uuiid <957714080@qq.com>
2025-12-08 13:15:02 +0800

    feat: 移除重复的sync_schema调用以优化数据库加载过程

--- src/doodle_core/sqlite_orm/sqlite_database.cpp
+++ src/doodle_core/sqlite_orm/sqlite_database.cpp
@@ -69,10 +69,10 @@ void sqlite_database::load(const FSys::path& in_path) {
 
   auto l_list = {details::upgrade_init(in_path), details::upgrade_1(in_path)};
   impl_       = std::make_shared<sqlite_database_impl>(in_path);
+  impl_->sync_schema();
   for (auto&& i : l_list) {
     i->upgrade(impl_);
   }
-  impl_->sync_schema();
   impl_->storage_any_.pragma.journal_mode(sqlite_orm::journal_mode::WAL);
 }
 boost::asio::awaitable<void> sqlite_database::backup(FSys::path in_path) {
