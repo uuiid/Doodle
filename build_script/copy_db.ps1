72463ac6354bee4390afc4f2edad3ed083fb5630
uuiid <957714080@qq.com>
2025-12-08 10:41:57 +0800

    feat: 更新copy_db.ps1以使用最新备份数据库并修正数据源路径

--- build_script/copy_db.ps1
+++ build_script/copy_db.ps1
@@ -1,12 +1,21 @@
-$DataSource = "$PSScriptRoot/../build/kitsu_new.db"
+$DataDestination = "$PSScriptRoot/../build/kitsu_new.db"
 # Install-Module PSSQLite
 Import-Module PSSQLite
-Copy-Item "\\192.168.0.181\Dev\kitsu_new.database" $DataSource -Force
-$prjs = Invoke-SqliteQuery -DataSource $DataSource -Query "select *from project;"
+
+$l_list = Get-ChildItem -Path "\\192.168.0.181\Dev\kitsu_data\backup" -Filter "kitsu_*.db" | Sort-Object LastWriteTime -Descending
+if ($l_list.Count -eq 0) {
+  Write-Host "没有找到备份数据库"
+  return;
+}
+$DataSource = $l_list[0].FullName
+Write-Host "使用最新的备份数据库: $DataSource"
+
+Copy-Item "$DataSource" $DataDestination -Force
+$prjs = Invoke-SqliteQuery -DataSource $DataDestination -Query "select *from project;"
 foreach ($p in $prjs) { 
   # //192.168.10.250/dd to D:/
   if ($p.path -eq '') { continue }  
   $path = 'D:/test_db' + $p.path.Substring(16)
   $q = "update project set path = '$path' where id=$($p.id);";
-  Invoke-SqliteQuery -DataSource $DataSource -Query $q;
+  Invoke-SqliteQuery -DataSource $DataDestination -Query $q;
 }
