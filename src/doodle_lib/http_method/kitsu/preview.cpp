2d8a05b1b08bcbc9a538358ab505ea275b4e07af
uuiid <957714080@qq.com>
2025-12-16 17:49:21 +0800

    feat: 优化预览文件安装逻辑，移除重复调用

--- src/doodle_lib/http_method/kitsu/preview.cpp
+++ src/doodle_lib/http_method/kitsu/preview.cpp
@@ -53,11 +53,11 @@ boost::asio::awaitable<boost::beast::http::message_generator> actions_tasks_comm
   l_preview_file->status_          = preview_file_statuses::processing;
   l_preview_file->source_          = "webgui";
   l_preview_file->extension_       = "mp4";
+  co_await l_sql.install(l_preview_file);
+
   auto l_preview_link              = std::make_shared<comment_preview_link>();
   l_preview_link->comment_id_      = comment_id_;
   l_preview_link->preview_file_id_ = l_preview_file->uuid_id_;
-
-  co_await l_sql.install(l_preview_file);
   co_await l_sql.install(l_preview_link);
   // 产生事件( "preview-file:new", "comment:update")
   socket_io::broadcast(
@@ -348,11 +348,11 @@ boost::asio::awaitable<boost::beast::http::message_generator> actions_tasks_comm
   l_preview_file->status_          = preview_file_statuses::processing;
   l_preview_file->source_          = "webgui";
   l_preview_file->extension_       = "mp4";
+  co_await l_sql.install(l_preview_file);
+
   auto l_preview_link              = std::make_shared<comment_preview_link>();
   l_preview_link->comment_id_      = comment_id_;
   l_preview_link->preview_file_id_ = l_preview_file->uuid_id_;
-
-  co_await l_sql.install(l_preview_file);
   co_await l_sql.install(l_preview_link);
   socket_io::broadcast(
       "preview-file:new",
