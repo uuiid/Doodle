087d483aa18f1f205393f6312c3ef0d13dda6406
uuiid <957714080@qq.com>
2025-12-16 09:50:43 +0800

    feat: 优化UE文件更新逻辑，添加路径处理以确保文件正确更新

--- src/doodle_lib/exe_warp/assets_update.cpp
+++ src/doodle_lib/exe_warp/assets_update.cpp
@@ -1,5 +1,7 @@
 #include "assets_update.h"
 
+#include <doodle_lib/exe_warp/ue_exe.h>
+
 #include "http_client/kitsu_client.h"
 #include <filesystem>
 #include <vector>
@@ -11,10 +13,16 @@ void from_json(const nlohmann::json& in_json, update_ue_files& out_obj) {
 }
 boost::asio::awaitable<void> update_ue_files::run() {
   std::vector<kitsu::kitsu_client::update_file_arg> l_files{};
-  auto l_json = co_await kitsu_client_->get_task_assets_update_ue_files(task_id_);
-  l_files     = kitsu::kitsu_client::update_file_arg::list_all_project_files(
-      ue_project_path_, l_json.get<std::vector<FSys::path>>()
-  );
+  auto l_json           = co_await kitsu_client_->get_task_assets_update_ue_files(task_id_);
+
+  auto l_ue_project_dir = ue_exe_ns::find_ue_project_file(ue_project_path_).parent_path();
+  std::vector<FSys::path> l_file_paths{};
+  for (auto&& l_path : l_json.get<std::vector<FSys::path>>()) {
+    SPDLOG_LOGGER_INFO(logger_ptr_, "需要更新的UE文件路径 {}", l_path.string());
+    l_file_paths.push_back(l_ue_project_dir / l_path);
+  }
+
+  l_files = kitsu::kitsu_client::update_file_arg::list_all_project_files(ue_project_path_, l_file_paths);
 
   if (l_files.empty()) co_return;
   kitsu_client_->set_logger(logger_ptr_);
