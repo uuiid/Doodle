14a576ec5cff4974afd473cab9a1029732ac11f6
uuiid <957714080@qq.com>
2025-12-25 15:07:53 +0800

    feat: 添加ffmpeg_video类及其头文件和源文件

---
+++ src/doodle_lib/core/ffmpeg_video.cpp
@@ -0,0 +1,1 @@
+#include "ffmpeg_video.h"
---
+++ src/doodle_lib/core/ffmpeg_video.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <doodle_core/doodle_core_fwd.h>
+
+namespace doodle {
+class ffmpeg_video {
+ public:
+  
+};
+}  // namespace doodle
