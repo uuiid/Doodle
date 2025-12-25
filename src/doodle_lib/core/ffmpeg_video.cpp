8080c8bb1fcf43144bc89ed40b35235c54e0e840
uuiid <957714080@qq.com>
2025-12-25 15:23:41 +0800

    feat: 添加ffmpeg_video类的成员变量和方法，增强视频处理功能

--- src/doodle_lib/core/ffmpeg_video.cpp
+++ src/doodle_lib/core/ffmpeg_video.cpp
@@ -1,1 +1,10 @@
 #include "ffmpeg_video.h"
+
+#include <avcpp/codeccontext.h>
+
+namespace doodle {
+void ffmpeg_video::process() {
+  //   ffmpeg处理视频
+}
+
+}  // namespace doodle
--- src/doodle_lib/core/ffmpeg_video.h
+++ src/doodle_lib/core/ffmpeg_video.h
@@ -2,9 +2,38 @@
 
 #include <doodle_core/doodle_core_fwd.h>
 
+#include <string>
+
 namespace doodle {
 class ffmpeg_video {
+  // 主要处理的视频
+  FSys::path video_path_;
+  // 输出文件
+  FSys::path out_path_;
+  // 字幕文件
+  FSys::path subtitle_path_;
+  // 混音文件
+  FSys::path audio_path_;
+  // 片头
+  FSys::path intro_path_;
+  // 片尾
+  FSys::path outro_path_;
+  // 水印
+  std::string watermark_text_;
+  // 时间码
+  bool time_code_{false};
+
  public:
-  
+  explicit ffmpeg_video(const FSys::path& in_video_path, const FSys::path& in_out_path)
+      : video_path_(in_video_path), out_path_(in_out_path) {}
+
+  void set_subtitle(const FSys::path& in_subtitle_path) { subtitle_path_ = in_subtitle_path; }
+  void set_audio(const FSys::path& in_audio_path) { audio_path_ = in_audio_path; }
+  void set_intro(const FSys::path& in_intro_path) { intro_path_ = in_intro_path; }
+  void set_outro(const FSys::path& in_outro_path) { outro_path_ = in_outro_path; }
+  void set_watermark(const std::string& in_watermark_text) { watermark_text_ = in_watermark_text; }
+  void set_time_code(bool in_time_code) { time_code_ = in_time_code; }
+  // 开始处理
+  void process();
 };
 }  // namespace doodle
