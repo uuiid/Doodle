5ccd9f733db5f462210945be4fb98af7b5c30068
uuiid <957714080@qq.com>
2025-12-28 10:37:32 +0800

    refactor: 移除对 boost::filesystem 的支持，统一使用 std::filesystem

--- src/doodle_core/core/file_sys.h
+++ src/doodle_core/core/file_sys.h
@@ -19,26 +19,18 @@ namespace doodle::chrono {
 using namespace std::chrono;
 }
 
-#define USE_STD_FSYS
-
 namespace doodle {
 using logger_ptr     = std::shared_ptr<spdlog::logger>;
 using logger_ptr_raw = spdlog::logger*;
 }  // namespace doodle
 
 namespace doodle::FSys {
-#ifdef USE_STD_FSYS
 using namespace std::filesystem;
 using fstream  = std::fstream;
 using istream  = std::istream;
 using ifstream = std::ifstream;
 using ofstream = std::ofstream;
 using ostream  = std::ostream;
-#else
-
-using namespace boost::filesystem;
-
-#endif
 
 DOODLE_CORE_API std::time_t last_write_time_t(const path& in_path);
 DOODLE_CORE_API chrono::sys_time_pos last_write_time_point(const path& in_path);
@@ -79,39 +71,3 @@ bool DOODLE_CORE_API is_diff(const FSys::path& in_path1, const FSys::path& in_pa
 bool DOODLE_CORE_API is_old_file(const FSys::path& in_path1, const FSys::path& in_path2);
 
 }  // namespace doodle::FSys
-
-#ifndef USE_STD_FSYS
-
-namespace fmt {
-/**
- * @brief 格式化资产类
- * 这个使用资产的路径属性格式化
- * @tparam 资产类
- */
-template <>
-struct formatter<::boost::filesystem::path> : formatter<string_view> {
-  /**
-   * @brief 格式化函数
-   *
-   * @tparam FormatContext fmt上下文类
-   * @param in_ 传入的资产类
-   * @param ctx 上下文
-   * @return decltype(ctx.out()) 基本上时 std::string
-   */
-  template <typename FormatContext>
-  auto format(const ::boost::filesystem::path& in_, FormatContext& ctx) const -> decltype(ctx.out()) {
-    return formatter<string_view>::format(in_.generic_string(), ctx);
-  }
-};
-}  // namespace fmt
-
-namespace nlohmann {
-template <>
-struct DOODLE_CORE_API adl_serializer<boost::filesystem::path> {
-  static void to_json(json& j, const boost::filesystem::path& in_path);
-
-  static void from_json(const json& j, boost::filesystem::path& in_path);
-};
-}  // namespace nlohmann
-
-#endif
