2f7e9c1a6116496229bdae09cf0345b231bf1925
uuiid <957714080@qq.com>
2026-01-16 10:44:20 +0800

    feat: 添加sparsemax头文件和源文件以支持新的稀疏最大化功能

---
+++ src/doodle_lib/ai/sparsemax.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <doodle_lib/doodle_lib_fwd.h>
+
+#include <filesystem>
+#include <memory>
+#include <torch/csrc/api/include/torch/torch.h>
+#include <vector>
+
+namespace doodle::ai {}
