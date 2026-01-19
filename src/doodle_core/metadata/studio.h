aecde1a2434dac1fae30e9bdd1a52619e2937f96
uuiid <957714080@qq.com>
2026-01-19 11:50:57 +0800

    feat: 添加外包工作室授权结构体及其序列化逻辑

--- src/doodle_core/metadata/studio.h
+++ src/doodle_core/metadata/studio.h
@@ -19,8 +19,21 @@ struct DOODLE_CORE_API studio {
     j["id"]       = p.uuid_id_;
     j["name"]     = p.name_;
     j["color"]    = p.color_;
-    
+
     j["archived"] = p.archived_;
+  }
+};
+
+// 外包按照工作室授权
+struct DOODLE_CORE_API outsource_studio_authorization {
+  DOODLE_BASE_FIELDS();
+  uuid studio_id_;  // 外包公司id
+  uuid enity_id_;   // 关联实体id, 资产,镜头等
+  // to json
+  friend void to_json(nlohmann::json& j, const outsource_studio_authorization& p) {
+    j["id"]        = p.uuid_id_;
+    j["studio_id"] = p.studio_id_;
+    j["entity_id"] = p.enity_id_;
   }
 };
 }  // namespace doodle
