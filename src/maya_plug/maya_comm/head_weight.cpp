68f65de539b4e52b5d684aabe3cab2fe12092b47
uuiid <957714080@qq.com>
2026-01-27 15:30:31 +0800

    feat: 添加head_weight类的实现和doIt方法的逻辑

--- src/maya_plug/maya_comm/head_weight.cpp
+++ src/maya_plug/maya_comm/head_weight.cpp
@@ -3,7 +3,19 @@
 //
 
 #include "head_weight.h"
+
+#include <maya/MArgDatabase.h>
+#include <maya/MDagPath.h>
+#include <maya/MGlobal.h>
+#include <maya/MSelectionList.h>
 namespace doodle::maya_plug {
+
+class head_weight::impl {
+ public:
+  impl()  = default;
+  ~impl() = default;
+  MDagPath head_mesh_path_;
+};
 
 MSyntax head_weight_syntax() {
   MSyntax l_syntax;
@@ -12,6 +24,25 @@ MSyntax head_weight_syntax() {
   return l_syntax;
 }
 
-MStatus head_weight::doIt(const MArgList& args) { return MStatus::kSuccess; }
+head_weight::head_weight() : p_impl(std::make_unique<impl>()) {}
+head_weight::~head_weight() = default;
+
+MStatus head_weight::doIt(const MArgList& args) {
+  MArgDatabase l_arg_db(syntax(), args);
+  MSelectionList l_sel_list{};
+  CHECK_MSTATUS_AND_RETURN_IT(l_arg_db.getObjects(l_sel_list));
+  if (l_sel_list.length() == 0) {
+    MGlobal::displayError("请至少选择一个头部模型");
+    return MStatus::kFailure;
+  }
+  CHECK_MSTATUS_AND_RETURN_IT(l_sel_list.getDagPath(0, p_impl->head_mesh_path_));
+  if (!p_impl->head_mesh_path_.hasFn(MFn::kMesh)) {
+    MGlobal::displayError("请选择一个头部网格模型");
+    return MStatus::kFailure;
+  }
+  
+
+  return MStatus::kSuccess;
+}
 
 }  // namespace doodle::maya_plug
--- src/maya_plug/maya_comm/head_weight.h
+++ src/maya_plug/maya_comm/head_weight.h
@@ -9,7 +9,12 @@ constexpr char name_doodle_head_weight[] = "doodle_head_weight";
 MSyntax head_weight_syntax();
 
 class head_weight : public TemplateAction<head_weight, name_doodle_head_weight, head_weight_syntax> {
+  class impl;
+  std::unique_ptr<impl> p_impl;
+
  public:
+  head_weight();
+  ~head_weight() override;
   MStatus doIt(const MArgList& args) override;
 };
 }  // namespace doodle::maya_plug
