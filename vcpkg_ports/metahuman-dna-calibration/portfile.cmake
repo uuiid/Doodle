795c692ef748f2f51d487cebc0ca729c5dbb6c13
uuiid <957714080@qq.com>
2026-01-30 18:26:22 +0800

    feat: 添加修复安装目录的补丁以优化库安装过程

--- vcpkg_ports/metahuman-dna-calibration/portfile.cmake
+++ vcpkg_ports/metahuman-dna-calibration/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
         HEAD_REF main
         PATCHES
         0001-python.patch
+        0002-fix-install-dir.patch
 )
 vcpkg_cmake_configure(
         SOURCE_PATH ${SOURCE_PATH}/dnacalib
