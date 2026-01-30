edcaf696f0e55808e283947936979e70bcbaa433
uuiid <957714080@qq.com>
2026-01-30 18:03:43 +0800

    feat: 更新 MetaHuman DNA 校准工具的 vcpkg 配置，修复 SHA512 校验和并调整源路径

--- vcpkg_ports/metahuman-dna-calibration/portfile.cmake
+++ vcpkg_ports/metahuman-dna-calibration/portfile.cmake
@@ -2,21 +2,18 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO EpicGames/MetaHuman-DNA-Calibration
         REF 8297ff1bd6f1692a6e221aa8395a66486e5a53c9
-        SHA512 0
+        SHA512 f32ad71cf81999497ea0fece394e82ecb59862952c925d2770526f824b0b7a8d6b8f6629df348d7e05af236b90bc3444fe140abe4d8426519789294cb4713b5a
         HEAD_REF main
         PATCHES
         0001-python.patch
 )
 vcpkg_cmake_configure(
-        SOURCE_PATH ${SOURCE_PATH}
+        SOURCE_PATH ${SOURCE_PATH}/dnacalib
         OPTIONS
-        -DBUILD_TEST=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
-vcpkg_cmake_config_fixup()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
