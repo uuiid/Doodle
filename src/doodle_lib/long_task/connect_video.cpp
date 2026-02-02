a612257157ae278211aa52bfded70e820795b6b6
uuiid <957714080@qq.com>
2026-02-02 14:25:04 +0800

    refactor: 统一参数格式，优化代码可读性

--- src/doodle_lib/long_task/connect_video.cpp
+++ src/doodle_lib/long_task/connect_video.cpp
@@ -15,8 +15,8 @@
 namespace doodle::detail {
 
 void connect_video(
-    const FSys::path &in_out_path, doodle::logger_ptr in_logger, const std::vector<FSys::path> &in_vector,
-    const image_size &in_size
+    const FSys::path& in_out_path, doodle::logger_ptr in_logger, const std::vector<FSys::path>& in_vector,
+    const image_size& in_size
 ) {
   in_logger->log(log_loc(), level::info, "开始创建视频 {}", in_out_path);
   in_logger->log(log_loc(), level::info, "获得视屏路径 {}", in_vector);
@@ -28,9 +28,8 @@ void connect_video(
   const cv::Size k_size{in_size.width, in_size.height};
   auto video = cv::VideoWriter{in_out_path.generic_string(), cv::VideoWriter::fourcc('a', 'v', 'c', '1'), 25, k_size};
   auto l_video_cap       = cv::VideoCapture{};
-  const auto &k_size_len = in_vector.size();
   cv::Mat l_image{};
-  for (auto &l_video : in_vector) {
+  for (auto& l_video : in_vector) {
     in_logger->log(log_loc(), level::info, "开始读取视屏 {}", l_video);
 
     l_video_cap.open(l_video.generic_string());
@@ -38,7 +37,6 @@ void connect_video(
       in_logger->log(log_loc(), level::warn, "视屏读取失败 跳过 {}", l_video);
       continue;
     }
-    auto l_video_count = l_video_cap.get(cv::CAP_PROP_FRAME_COUNT);
     in_logger->log(log_loc(), level::info, "开始将{}写入行视屏", l_video);
     while (l_video_cap.read(l_image)) {
       if (l_image.empty()) {
@@ -47,7 +45,6 @@ void connect_video(
       }
       if (l_image.cols != k_size.width || l_image.rows != k_size.height) cv::resize(l_image, l_image, k_size);
 
-      in_logger->log(log_loc(), level::info, "progress 1/{}", l_video_count * k_size_len + k_size_len / 10);
       video << l_image;
     }
   }
@@ -56,7 +53,7 @@ void connect_video(
 }
 boost::asio::awaitable<void> connect_video_t::run() {
   file_list_ |=
-      ranges::actions::sort([](const FSys::path &l_a, const FSys::path &l_b) { return l_a.stem() < l_b.stem(); });
+      ranges::actions::sort([](const FSys::path& l_a, const FSys::path& l_b) { return l_a.stem() < l_b.stem(); });
   connect_video(out_path_, logger_ptr_, file_list_, image_size_);
   co_return;
 }
