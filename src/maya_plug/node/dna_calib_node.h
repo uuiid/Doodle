42bba249987f4dd3acdbc2e020ab9433d2f89ed0
uuiid <957714080@qq.com>
2026-02-02 10:56:55 +0800

    feat: 添加 dna_calib_node 类及其成员变量以扩展 Maya 插件功能

--- src/maya_plug/node/dna_calib_node.h
+++ src/maya_plug/node/dna_calib_node.h
@@ -8,5 +8,31 @@
 
 #include <maya/MPxNode.h>
 namespace doodle::maya_plug {
+class dna_calib_node : public MPxNode {
+ public:
+  static MTypeId doodle_id;
+  const static constexpr auto node_type = MPxNode::Type::kDependNode;
+  static void* creator();
+  static MStatus initialize();
 
-}
+  const static constexpr std::string_view node_name{"doodle_dna_calib_node"};
+
+ private:
+  // DNA文件路径
+  static MObject dna_file_path;
+  // 校准结果输出路径
+  static MObject output_file_path;
+  // 校准结果输出路径
+  static MObject output_image_path;
+
+  // 起始帧
+  static MObject start_frame;
+  // 结束帧
+  static MObject end_frame;
+  // 步长
+  static MObject frame_step;
+
+  // 校准结果
+  static MObject calib_result;
+};
+}  // namespace doodle::maya_plug
