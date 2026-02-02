812e20e19d2a892159b1256a1f6ab7f03c9cbca9
uuiid <957714080@qq.com>
2026-02-02 10:36:08 +0800

    feat: 添加 dna_calib_node 头文件和源文件以扩展 Maya 插件功能

---
+++ src/maya_plug/node/dna_calib_node.cpp
@@ -0,0 +1,1 @@
+#include "dna_calib_node.h"
---
+++ src/maya_plug/node/dna_calib_node.h
@@ -0,0 +1,12 @@
+//
+// Created by TD on 2024/1/11.
+//
+
+#pragma once
+
+#include <maya_plug/maya_plug_fwd.h>
+
+#include <maya/MPxNode.h>
+namespace doodle::maya_plug {
+
+}
