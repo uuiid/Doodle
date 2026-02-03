0d2e5da490e23076ac826aa12f75f41d5c29a061
uuiid <957714080@qq.com>
2026-02-03 11:58:10 +0800

    feat: 添加 DNA 文件导入功能，增强文件读取和错误处理

--- src/maya_plug/maya_comm/dna_calib_import.cpp
+++ src/maya_plug/maya_comm/dna_calib_import.cpp
@@ -1,9 +1,12 @@
 #include "dna_calib_import.h"
 
+#include "doodle_core/exception/exception.h"
+
 #include <maya_plug/node/dna_calib_node.h>
 
 #include "data/maya_conv_str.h"
 #include "data/maya_tool.h"
+#include <dnacalib/DNACalib.h>
 #include <fmt/format.h>
 #include <maya/MArgDatabase.h>
 #include <maya/MFn.h>
@@ -12,6 +15,8 @@
 #include <maya/MSelectionList.h>
 #include <maya/MStatus.h>
 #include <maya/MSyntax.h>
+#include <pma/ScopedPtr.h>
+
 
 namespace doodle::maya_plug {
 MSyntax dna_calib_import_syntax() {
@@ -25,6 +30,17 @@ class dna_calib_import::impl {
  public:
   FSys::path file_path;
   MObject dna_node_obj{};
+
+  void import_dna_calib() {
+    auto l_dna_file = dnac::makeScoped<dnac::FileStream>(
+        file_path.generic_string().data(), dnac::FileStream::AccessMode::ReadWrite, dnac::FileStream::OpenMode::Binary
+    );
+    auto l_render = dnac::makeScoped<dnac::BinaryStreamReader>(l_dna_file.get());
+    l_render->read();
+    DOODLE_CHICK(dnac::Status::isOk(), "读取dna文件失败: {}", file_path);
+
+
+    }
 };
 dna_calib_import::dna_calib_import() : p_i(std::make_unique<impl>()) {}
 
