b8db7ee45646013ab1f69772d005ef400867b3f8
uuiid <957714080@qq.com>
2026-02-03 11:27:02 +0800

    feat: 优化 dna_calib_import 逻辑，增强节点选择和文件路径处理

--- src/maya_plug/maya_comm/dna_calib_import.cpp
+++ src/maya_plug/maya_comm/dna_calib_import.cpp
@@ -2,16 +2,21 @@
 
 #include <maya_plug/node/dna_calib_node.h>
 
+#include "data/maya_conv_str.h"
+#include "data/maya_tool.h"
+#include <fmt/format.h>
 #include <maya/MArgDatabase.h>
+#include <maya/MFn.h>
 #include <maya/MFnDependencyNode.h>
+#include <maya/MObject.h>
+#include <maya/MSelectionList.h>
 #include <maya/MStatus.h>
 #include <maya/MSyntax.h>
 
 namespace doodle::maya_plug {
 MSyntax dna_calib_import_syntax() {
   MSyntax l_syntax{};
-  l_syntax.addFlag("fp", "file_path", MSyntax::kString);
-  l_syntax.setObjectType(MSyntax::kSelectionList);
+  CHECK_MSTATUS(l_syntax.setObjectType(MSyntax::kSelectionList, 1, 1));
   l_syntax.useSelectionAsDefault(true);
   return l_syntax;
 }
@@ -19,6 +24,7 @@ MSyntax dna_calib_import_syntax() {
 class dna_calib_import::impl {
  public:
   FSys::path file_path;
+  MObject dna_node_obj{};
 };
 dna_calib_import::dna_calib_import() : p_i(std::make_unique<impl>()) {}
 
@@ -26,39 +32,41 @@ MStatus dna_calib_import::get_arg(const MArgList& in_arg) {
   MStatus l_status{};
   MArgDatabase const l_arg_data{syntax(), in_arg, &l_status};
   DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-  if (l_arg_data.isFlagSet("fp", &l_status)) {
+  MSelectionList l_list{};
+  DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_arg_data.getObjects(l_list));
+  for (unsigned int i = 0; i < l_list.length(); ++i) {
+    MObject l_node_obj{};
+    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_list.getDependNode(i, l_node_obj));
+    if (!l_node_obj.hasFn(MFn::kDependencyNode)) continue;
+    MFnDependencyNode l_fn_node{l_node_obj, &l_status};
     DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-    MString l_file_path{};
-    l_status = l_arg_data.getFlagArgument("fp", 0, l_file_path);
-    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-    p_i->file_path = conv::to_s(l_file_path);
+    if (l_fn_node.typeId() != dna_calib_node::doodle_id) continue;
+    p_i->dna_node_obj = l_node_obj;
+    break;
   }
   return MS::kSuccess;
 }
 
 MStatus dna_calib_import::doIt(const MArgList& in_list) {
-  get_arg(in_list);
-  MSelectionList l_list{};
+  DOODLE_CHECK_MSTATUS_AND_RETURN_IT(get_arg(in_list));
+
+  if (p_i->dna_node_obj.isNull())
+    return displayError(conv::to_ms(fmt::format("未选择dna_calib_node节点"))), MS::kFailure;
+
+  // 找到属性
   MStatus l_status{};
-  MArgDatabase const l_arg_data{syntax(), in_list, &l_status};
+  MFnDependencyNode l_fn_node{p_i->dna_node_obj, &l_status};
   DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-  l_status = l_arg_data.getObjects(l_list);
+  auto l_plug_file_path = l_fn_node.findPlug(dna_calib_node::dna_file_path, false, &l_status);
   DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-  for (unsigned int i = 0; i < l_list.length(); ++i) {
-    MDagPath l_dag_path{};
-    l_status = l_list.getDagPath(i, l_dag_path);
-    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-    MObject l_node_obj = l_dag_path.node(&l_status);
-    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-    if (!l_node_obj.hasFn(MFn::kDependencyNode)) continue;
-    MFnDependencyNode l_fn_node{l_node_obj, &l_status};
-    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-    if (l_fn_node.typeId() != dna_calib_node::doodle_id) continue;
-    MPlug l_plug = l_fn_node.findPlug(dna_calib_node::dna_file_path, true, &l_status);
-    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-    l_status = l_plug.setValue(conv::to_ms(p_i->file_path));
-    DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_status);
-  }
+  MString l_file_path_ms{};
+  DOODLE_CHECK_MSTATUS_AND_RETURN_IT(l_plug_file_path.getValue(l_file_path_ms));
+  p_i->file_path = conv::to_s(l_file_path_ms);
+  if (p_i->file_path.empty() || !FSys::exists(p_i->file_path))
+    return displayError(conv::to_ms(fmt::format("dna文件不存在: {}", p_i->file_path.generic_string()))), MS::kFailure;
+  // 读取文件
+
+  return MS::kSuccess;
 }
 
 }  // namespace doodle::maya_plug
