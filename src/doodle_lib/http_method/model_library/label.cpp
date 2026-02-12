d77a39a60d4591d22c680fba496e2a21393d9449
uuiid <957714080@qq.com>
2026-02-12 09:09:23 +0800

    feat: 添加日志记录以跟踪用户在资产库节点添加资产库文件的操作

--- src/doodle_lib/http_method/model_library/label.cpp
+++ src/doodle_lib/http_method/model_library/label.cpp
@@ -13,6 +13,10 @@ namespace doodle::http::model_library {
 boost::asio::awaitable<boost::beast::http::message_generator> assets_tree_link::post(http::session_data_ptr in_handle) {
   auto l_sql = g_ctx().get<sqlite_database>();
   if (l_sql.has_assets_tree_assets_link(id_, assets_id_)) co_return in_handle->make_msg(nlohmann::json{});
+  SPDLOG_LOGGER_WARN(
+      g_logger_ctrl().get_http(), "用户 {}({}) 在 资产库节点 {} 中添加 资产库文件 {} ", person_.person_.email_,
+      person_.person_.get_full_name(), id_, assets_id_
+  );
   auto l_link               = std::make_shared<assets_file_helper::link_parent_t>();
   l_link->assets_type_uuid_ = id_;
   l_link->assets_uuid_      = assets_id_;
