fc41dfac3f340336b8a78209ee2f227d24f36e0d
uuiid <957714080@qq.com>
2026-02-27 13:54:59 +0800

    feat: 添加 play_blast 类及其实现，支持图像排屏功能

---
+++ src/maya_plug/data/play_blast.cpp
@@ -0,0 +1,179 @@
+//
+// Created by TD on 2021/11/22.
+//
+
+#include "play_blast.h"
+
+#include "doodle_core/exception/exception.h"
+#include <doodle_core/core/app_base.h>
+#include <doodle_core/core/core_set.h>
+#include <doodle_core/metadata/move_create.h>
+#include <doodle_core/metadata/user.h>
+
+
+#include "boost/numeric/conversion/cast.hpp"
+
+#include "maya_plug/data/maya_conv_str.h"
+#include "maya_plug/exception/exception.h"
+#include <maya_plug/data/maya_camera.h>
+
+#include "maya/MRenderTargetManager.h"
+#include "maya/MString.h"
+#include "maya/MTextureManager.h"
+#include <cstdint>
+#include <filesystem>
+#include <fmt/chrono.h>
+#include <maya/M3dView.h>
+#include <maya/MAnimControl.h>
+#include <maya/MDrawContext.h>
+#include <maya/MFileIO.h>
+#include <maya/MGlobal.h>
+#include <wil/registry.h>
+namespace doodle::maya_plug {
+
+MString play_blast::p_post_render_notification_name{"doodle_maya_notification"};
+MString play_blast::k_play_blast_tex{"doodle_maya_play_blast_tex"};
+
+void play_blast::captureCallback(MHWRender::MDrawContext& context, void* clientData) {
+  using tex_ptr_t = std::shared_ptr<MTexture>;
+
+  MStatus k_s{};
+  auto* self     = static_cast<play_blast*>(clientData);
+  auto* k_render = MHWRender::MRenderer::theRenderer();
+  if (!self || !k_render) return;
+
+  auto k_p = self->get_file_path(self->p_current_time);
+  if (k_p.empty()) return;
+
+  MString const k_path{d_str{k_p.generic_string()}};
+
+  auto* l_tex_manager = k_render->getTextureManager();
+  tex_ptr_t const l_tex_ptr{context.copyCurrentColorRenderTargetToTexture(), [=](MTexture* in_texture) {
+                              if (in_texture) l_tex_manager->releaseTexture(in_texture);
+                            }};
+  if (l_tex_ptr) {
+    k_s = l_tex_manager->saveTexture(l_tex_ptr.get(), k_path);
+    if (!k_s) {
+      MGlobal::displayError(k_s.errorString());
+    }
+  }
+
+  DOODLE_LOG_INFO("save texture: {}", k_path);
+}
+
+play_blast::play_blast() : p_current_time(), p_uuid() {}
+FSys::path play_blast::get_file_dir() const {
+  auto k_cache_path = core_set::get_set().get_cache_root("maya_play_blast");
+  k_cache_path /= p_uuid;
+  if (!FSys::exists(k_cache_path)) {
+    FSys::create_directories(k_cache_path);
+  }
+  return k_cache_path;
+}
+
+FSys::path play_blast::get_file_path(const MTime& in_time) const {
+  auto k_cache_path = get_file_dir();
+  k_cache_path /= fmt::format("image_{:05d}.png", boost::numeric_cast<std::int32_t>(in_time.as(MTime::uiUnit())));
+  return k_cache_path;
+}
+
+FSys::path play_blast::play_blast_(const MTime& in_start, const MTime& in_end, const image_size& in_size) {
+  p_uuid = core_set::get_set().get_uuid_str();
+  MGlobal::executeCommand("hwRenderLoad;");
+  MGlobal::executeCommand(R"(setAttr "hardwareRenderingGlobals.floatingPointRTEnable" 0;)");
+  MGlobal::executeCommand(R"(setAttr "hardwareRenderingGlobals.multiSampleEnable" 0;)");
+  MGlobal::executeCommand(R"(setAttr "hardwareRenderingGlobals.ssaoEnable" 0;)");
+  MGlobal::executeCommand(R"(setAttr "hardwareRenderingGlobals.textureMaxResMode" 0;)");
+  MStatus k_s{};
+
+  auto k_cam = maya_camera::conjecture();
+  k_cam.set_render_cam();
+  k_cam.set_play_attr();
+  //  k_s = MGlobal::executeCommand(R"(colorManagementPrefs -e -outputTransformEnabled true -outputTarget "renderer";
+  //  colorManagementPrefs -e -outputUseViewTransform -outputTarget "renderer";)");
+  //  DOODLE_CHECK_MSTATUS_AND_RETURN_IT(k_s);
+  //
+  //  DOODLE_CHECK_MSTATUS_AND_RETURN_IT(k_s);
+  //  MGlobal::executeCommand(R"(colorManagementPrefs -e -outputTransformEnabled false -outputTarget "renderer";)");
+  std::vector<movie::image_attr> l_handle_list{};
+
+  {
+    MHWRender::MRenderer* renderer = MHWRender::MRenderer::theRenderer();
+    DOODLE_CHICK(renderer != nullptr, "无法获取渲染器");
+
+    //    renderer->addNotification(
+    //        &play_blast::captureCallback, p_post_render_notification_name,
+    //        MHWRender::MPassContext::kEndRenderSemantic, reinterpret_cast<void*>(this)
+    //    );
+
+    renderer->setOutputTargetOverrideSize(in_size.width, in_size.height);
+    renderer->setPresentOnScreen(false);
+    const auto l_cam_path  = k_cam.get_full_name();
+
+    using target_ptr_t     = std::shared_ptr<MHWRender::MRenderTarget>;
+    using tex_ptr_t        = std::shared_ptr<MTexture>;
+    using tex_data_ptr_t   = std::shared_ptr<void>;
+
+    auto* l_tex_manager    = renderer->getTextureManager();
+    auto* l_target_manager = renderer->getRenderTargetManager();
+    MHWRender::MRenderTarget* l_rt{l_target_manager->acquireRenderTarget(
+        MRenderTargetDescription{
+            k_play_blast_tex, boost::numeric_cast<std::uint32_t>(in_size.width),
+            boost::numeric_cast<std::uint32_t>(in_size.height), 1, ::MHWRender::MRasterFormat ::kB8G8R8A8, 1, false
+        }
+    )};
+    target_ptr_t const l_t{l_rt, [=](MHWRender::MRenderTarget* in_target) {
+                             if (in_target) l_target_manager->releaseRenderTarget(in_target);
+                           }};
+    //    MHWRender::MRenderTarget* l_rt{l_target_manager->acquireRenderTargetFromScreen(k_play_blast_tex)};
+
+    DOODLE_LOG_INFO("set output camera: {}", l_cam_path);
+    // 开始排屏, 并且开始将结果属性写入
+    /// \brief 当前帧和总帧数
+    auto k_len = in_end - in_start + 1;
+    for (MTime i{in_start}; i <= in_end; ++i) {
+      MAnimControl::setCurrentTime(i);
+
+      DOODLE_CHICK(renderer->render(conv::to_ms(fmt::format("batch:{}", l_cam_path)), &l_rt, 1), "渲染失败");
+
+      if (l_rt) {
+        int l_row_pitch      = 0;
+        size_t l_slice_pitch = 0;
+        MHWRender::MRenderTargetDescription l_target_desc;
+        tex_data_ptr_t const l_target_data{l_rt->rawData(l_row_pitch, l_slice_pitch), [](void* in_data) {
+                                             MHWRender::MRenderTarget::freeRawData(in_data);
+                                           }};
+
+        l_rt->targetDescription(l_target_desc);
+        MHWRender::MTextureDescription l_texture_desc;
+        l_texture_desc.fWidth         = l_target_desc.width();
+        l_texture_desc.fHeight        = l_target_desc.height();
+        l_texture_desc.fDepth         = 1;
+        l_texture_desc.fBytesPerRow   = l_row_pitch;
+        l_texture_desc.fBytesPerSlice = l_slice_pitch;
+        l_texture_desc.fMipmaps       = 1;
+        l_texture_desc.fArraySlices   = l_target_desc.arraySliceCount();
+        l_texture_desc.fFormat        = l_target_desc.rasterFormat();
+        l_texture_desc.fTextureType   = MHWRender::kImage2D;
+        l_texture_desc.fEnvMapType    = MHWRender::MEnvironmentMapType::kEnvNone;
+        tex_ptr_t const l_tex{
+            l_tex_manager->acquireTexture({}, l_texture_desc, l_target_data.get()), [=](MTexture* in_texture) {
+              if (in_texture) l_tex_manager->releaseTexture(in_texture);
+            }
+        };
+        auto k_p = get_file_path(i);
+        MString const k_path = conv::to_ms(k_p.generic_string());
+        maya_chick(l_tex_manager->saveTexture(l_tex.get(), k_path));
+      }
+    }
+    //    renderer->removeNotification(p_post_render_notification_name, MHWRender::MPassContext::kEndRenderSemantic);
+    // 恢复关闭屏幕上的更新
+    renderer->setPresentOnScreen(true);
+    // 禁用目标尺寸覆盖
+    renderer->unsetOutputTargetOverrideSize();
+  }
+
+  return get_file_dir();
+}
+
+}  // namespace doodle::maya_plug
--- src/maya_plug/maya_comm/doodle_batch_run.cpp
+++ src/maya_plug/maya_comm/doodle_batch_run.cpp
@@ -3,8 +3,22 @@
 #include "doodle_core/exception/exception.h"
 #include "doodle_core/lib_warp/maya_exe_out.h"
 
+#include <maya_plug/data/export_file_fbx.h>
+#include <maya_plug/data/maya_file_io.h>
+#include <maya_plug/data/ncloth_factory.h>
+#include <maya_plug/data/play_blast.h>
+#include <maya_plug/data/qcloth_factory.h>
+
+// #include <maya_plug/data/
+#include <maya_plug/data/reference_file.h>
+#include <maya_plug/data/sim_cover_attr.h>
+#include <maya_plug/fmt/fmt_dag_path.h>
+
 #include "data/maya_conv_str.h"
+#include "data/maya_tool.h"
+#include <maya/MAnimControl.h>
 #include <maya/MArgDatabase.h>
+
 namespace doodle::maya_plug {
 
 MSyntax doodle_batch_run_syntax() {
@@ -30,8 +44,102 @@ class cloth_sim_run {
   MTime anim_begin_time_{};
   MTime t_post_time_{};
   std::double_t film_aperture_{};
-  std::int32_t width{0};
-  std::int32_t height{0};
+  image_size size_{};
+
+  bool replace_ref_file_{};
+  bool sim_file_{};
+  bool export_file_{};  // 导出abc文件
+  bool touch_sim_{};
+  bool export_anim_file_{};
+  bool create_play_blast_{};
+
+  void config_cloth_sim(const nlohmann::json& in_json) {
+    anim_begin_time_ = MTime{boost::numeric_cast<std::double_t>(1001), MTime::uiUnit()};
+    t_post_time_     = MTime{boost::numeric_cast<std::double_t>(950), MTime::uiUnit()};
+    in_json.at("image_size").get_to(size_);
+    in_json.at("film_aperture").get_to(film_aperture_);
+    auto l_sim_path = in_json.at("sim_path").get<FSys::path>();
+    if (FSys::is_directory(l_sim_path)) {
+      for (auto&& l_p : FSys::directory_iterator(l_sim_path)) {
+        if (FSys::is_regular_file(l_p) && l_p.path().extension() == ".ma") {
+          sim_file_map_[l_p.path().filename().string()] = l_p.path();
+        }
+      }
+    }
+    replace_ref_file_  = in_json.at("replace_ref_file").get<bool>();
+    sim_file_          = in_json.at("sim_file").get<bool>();
+    export_file_       = in_json.at("export_file").get<bool>();
+    touch_sim_         = in_json.at("touch_sim").get<bool>();
+    export_anim_file_  = in_json.at("export_anim_file").get<bool>();
+    create_play_blast_ = in_json.at("create_play_blast").get<bool>();
+    display_info(
+        "配置布料解算完成 开始时间 {}(强制为950) 结束时间 {} 画幅 {}", anim_begin_time_.as(MTime::uiUnit()),
+        MAnimControl::maxTime().as(MTime::uiUnit()), size_
+    );
+    maya_chick(MAnimControl::setCurrentTime(MTime{boost::numeric_cast<std::double_t>(950), MTime::uiUnit()}));
+  }
+
+  void run() {
+    display_warning("开始扫瞄引用");
+    all_ref_files_ = reference_file_factory{}.create_ref();
+
+    for (auto&& l_ref : all_ref_files_) {
+      if (l_ref.export_group_attr() && l_ref.get_use_sim() && l_ref.has_sim_assets_file(sim_file_map_)) {
+        default_logger_raw()->log(log_loc(), level::info, "引用文件{}准备解算", l_ref.get_abs_path());
+        ref_files_.emplace_back(l_ref);
+      } else {
+        default_logger_raw()->log(log_loc(), level::info, "引用文件{}不解算", l_ref.get_abs_path());
+      }
+    }
+    if (replace_ref_file_) replace_ref_file();
+    DOODLE_LOG_INFO("开始解锁节点 initialShadingGroup");
+    maya_chick(MGlobal::executeCommand(d_str{R"(lockNode -l false -lu false ":initialShadingGroup";)"}));
+    DOODLE_LOG_INFO("开始创建布料");
+    cloth_factory_interface l_cf{};
+    if (qcloth_factory::has_cloth())
+      l_cf = std::make_shared<qcloth_factory>();
+    else if (ncloth_factory::has_cloth())
+      l_cf = std::make_shared<ncloth_factory>();
+
+    if (!l_cf) return;
+
+    auto l_cloth_list_ = l_cf->create_cloth();
+    std::map<std::string, reference_file> l_ref_map{};
+    for (auto&& in_handle : ref_files_) {
+      l_ref_map[in_handle.get_namespace()] = in_handle;
+    }
+
+    cloth_lists_.reserve(l_cloth_list_.size());
+    for (auto&& in_handle : l_cloth_list_) {
+      if (!l_ref_map.contains(in_handle->get_namespace())) {
+        default_logger_raw()->log(
+            log_loc(), level::info, "布料{}未找到对应的引用文件, 无法解算, 请查找对应的引用", in_handle->get_shape()
+        );
+      }
+      cloth_lists_.emplace_back(in_handle);
+    }
+    if (replace_ref_file_) set_cloth_attr();
+
+    if (sim_file_) {
+      DOODLE_LOG_INFO("安排解算布料");
+      sim();
+    } else if (touch_sim_) {
+      DOODLE_LOG_INFO("安排touch布料");
+      touch_sim();
+    }
+    if (create_play_blast_) {
+      DOODLE_LOG_INFO("安排排屏");
+      play_blast();
+    }
+    if (export_file_) {
+      DOODLE_LOG_INFO("安排导出abc和fbx");
+      export_abc();
+    }
+    if (export_anim_file_) {
+      DOODLE_LOG_INFO("安排导出动画文件");
+      export_anim_file();
+    }
+  }
 
  private:
   std::vector<reference_file> ref_files_{};
@@ -42,6 +150,146 @@ class cloth_sim_run {
   FSys::path camera_path_{};
   FSys::path out_path_file_;
   maya_out_arg out_arg_{};
+
+  void replace_ref_file() {
+    DOODLE_LOG_INFO("开始替换引用");
+    decltype(ref_files_) l_new_ref_files;
+    for (auto&& l_ref : ref_files_) {
+      if (l_ref.replace_sim_assets_file(sim_file_map_)) {
+        SPDLOG_LOGGER_WARN(default_logger_raw(), "引用文件 {} 替换引用成功", l_ref.get_abs_path());
+        l_new_ref_files.emplace_back(l_ref);
+      } else {
+        SPDLOG_LOGGER_WARN(default_logger_raw(), "引用文件 {} 替换引用失败, 不解算", l_ref.get_abs_path());
+      }
+    }
+    ref_files_ = std::move(l_new_ref_files);
+  }
+  void set_cloth_attr() {
+    std::map<std::string, reference_file> l_ref_map{};
+
+    for (auto&& in_handle : ref_files_) {
+      l_ref_map[in_handle.get_namespace()] = in_handle;
+    }
+
+    for (auto&& in_handle : cloth_lists_) {
+      auto l_ref_h = l_ref_map[in_handle->get_namespace()];
+      in_handle->add_collision(l_ref_h);     /// 添加碰撞
+      in_handle->rest();                     /// 添加rest
+      in_handle->cover_cloth_attr(l_ref_h);  /// 添加布料属性
+      in_handle->add_field(l_ref_h);         /// 添加场力
+    }
+  }
+  void sim() {
+    DOODLE_LOG_INFO("开始解算");
+
+    std::map<std::string, reference_file> l_ref_map{};
+
+    for (auto&& in_handle : ref_files_) {
+      l_ref_map[in_handle.get_namespace()] = in_handle;
+    }
+
+    ranges::for_each(cloth_lists_, [&](cloth_interface& in_handle) {
+      auto l_ref_h = l_ref_map[in_handle->get_namespace()];
+      in_handle->set_cache_folder(l_ref_h, true);  /// 设置缓存文件夹
+    });
+
+    /// \brief 在这里我们保存引用
+    auto k_save_file = maya_file_io::work_path("ma");
+    if (!FSys::exists(k_save_file)) {
+      FSys::create_directories(k_save_file);
+    }
+
+    k_save_file /= maya_file_io::get_current_path().filename();
+    try {
+      maya_file_io::save_file(k_save_file);
+      DOODLE_LOG_INFO("保存文件到 {}", k_save_file);
+    } catch (const std::runtime_error& error) {
+      DOODLE_LOG_WARN("无法保存文件 {} : {}", k_save_file, error.what());
+    }
+    const MTime k_end_time = MAnimControl::maxTime();
+    for (auto&& i = t_post_time_; i <= k_end_time; ++i) {
+      maya_chick(MAnimControl::setCurrentTime(i));
+      DOODLE_LOG_INFO("解算帧 {}", i);
+      ranges::for_each(cloth_lists_, [&](cloth_interface& in_handle) { in_handle->sim_cloth(); });
+    }
+  }
+  void touch_sim() {
+    DOODLE_LOG_INFO("开始触摸解算");
+
+    std::map<std::string, reference_file> l_ref_map{};
+
+    for (auto&& in_handle : ref_files_) {
+      l_ref_map[in_handle.get_namespace()] = in_handle;
+    }
+
+    ranges::for_each(cloth_lists_, [&](cloth_interface& in_handle) {
+      auto l_ref_h = l_ref_map[in_handle->get_namespace()];
+      in_handle->set_cache_folder_read_only();  // 设置缓存为只读
+    });
+
+    const MTime k_end_time = MAnimControl::maxTime();
+    for (auto&& i = t_post_time_; i <= k_end_time; ++i) {
+      maya_chick(MAnimControl::setCurrentTime(i));
+      DOODLE_LOG_INFO("测试解算帧 {}", i);
+      ranges::for_each(cloth_lists_, [&](cloth_interface& in_handle) { in_handle->sim_cloth(); });
+    }
+  }
+  void play_blast() {
+    DOODLE_LOG_INFO("开始排屏");
+    class play_blast l_p{};
+
+    const MTime k_end_time  = MAnimControl::maxTime();
+
+    out_arg_.movie_file_dir = l_p.play_blast_(anim_begin_time_, k_end_time, size_);
+  }
+  void export_abc() {
+    DOODLE_LOG_INFO("开始导出解算fbx");
+    auto l_gen             = std::make_shared<reference_file_ns::generate_abc_file_path>();
+    const MTime k_end_time = MAnimControl::maxTime();
+    l_gen->begin_end_time  = std::make_pair(anim_begin_time_, k_end_time);
+
+    export_file_fbx l_ex_fbx{};
+    ranges::for_each(ref_files_, [&](reference_file& in_handle) {
+      l_gen->set_fbx_path(true);
+      auto l_path = l_ex_fbx.export_sim(in_handle, l_gen);
+      for (auto& p : l_path) out_arg_.out_file_list.emplace_back(p);
+    });
+  }
+  void export_anim_file() {
+    DOODLE_LOG_INFO("开始导出动画文件");
+    export_file_fbx l_ex{};
+    auto l_gen             = std::make_shared<reference_file_ns::generate_abc_file_path>();
+    const MTime k_end_time = MAnimControl::maxTime();
+    l_gen->begin_end_time  = std::make_pair(anim_begin_time_, k_end_time);
+    l_gen->set_fbx_path(true);
+    ranges::for_each(
+        all_ref_files_ | ranges::views::filter([&](const reference_file& in_handle) -> bool {
+          return ranges::find(ref_files_, in_handle) == ref_files_.end();
+        }),
+        [&](reference_file& in_handle) {
+          if (!in_handle.is_loaded()) in_handle.load_file();
+          auto l_path = l_ex.export_anim(in_handle, l_gen);
+          out_arg_.out_file_list.emplace_back(l_path);
+        }
+    );
+
+    // 导出相机
+    camera_path_ = l_ex.export_cam(l_gen, film_aperture_);
+    out_arg_.out_file_list.emplace_back(camera_path_);  // 导出相机
+  }
+  void write_config() {
+    default_logger_raw()->log(log_loc(), level::info, "导出动画文件完成, 开始写出配置文件");
+    out_arg_.begin_time   = anim_begin_time_.value();
+    out_arg_.end_time     = MAnimControl::maxTime().value();
+
+    nlohmann::json l_json = out_arg_;
+    if (!out_path_file_.empty()) {
+      if (!FSys::exists(out_path_file_.parent_path())) FSys::create_directories(out_path_file_.parent_path());
+      default_logger_raw()->log(log_loc(), spdlog::level::info, "写出配置文件 {}", out_path_file_);
+      FSys::ofstream{out_path_file_} << l_json.dump(4);
+    } else
+      log_info(fmt::format("导出文件 {}", l_json.dump(4)));
+  }
 };
 
 class doodle_batch_run::impl {
@@ -59,11 +307,11 @@ MStatus doodle_batch_run::doIt(const MArgList& in_list) {
 
   } catch (const doodle_error& e) {
     setResult(e.error_code_);
-    displayError(conv::to_ms(e.what()));
+    display_error(e.what());
     return MStatus::kSuccess;
   } catch (...) {
     setResult(-1);
-    displayError(conv::to_ms(boost::current_exception_diagnostic_information()));
+    display_error(boost::current_exception_diagnostic_information());
     return MStatus::kSuccess;
   }
   return MStatus::kSuccess;
