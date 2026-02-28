1b44c40264c50f6582064ed1db497ddbb8cac84c
uuiid <957714080@qq.com>
2026-02-28 14:46:17 +0800

    refactor: 更新头文件引用，移除不必要的代码，简化项目结构

--- src/doodle_core/metadata/assets_file.cpp
+++ src/doodle_core/metadata/assets_file.cpp
@@ -1,18 +1,3 @@
 ﻿//
 // Created by TD on 2021/5/7.
 //
-
-#include <doodle_core/metadata/assets_file.h>
-#include <doodle_core/metadata/comment.h>
-#include <doodle_core/metadata/metadata.h>
-#include <doodle_core/metadata/project.h>
-#include <doodle_core/metadata/rules.h>
-#include <doodle_core/metadata/user.h>
-
-#include <boost/uuid/uuid.hpp>
-
-#include <core/core_set.h>
-#include <entt/entity/fwd.hpp>
-#include <functional>
-
-namespace doodle {}  // namespace doodle
