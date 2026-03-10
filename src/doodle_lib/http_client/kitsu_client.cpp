bb83cd995b302db1c1cd68182ccb969ed0e16530
uuiid <957714080@qq.com>
2026-03-10 15:48:32 +0800

    feat: 修改获取 UE 装配的 API 请求方法为 GET，优化请求头设置

--- src/doodle_lib/http_client/kitsu_client.cpp
+++ src/doodle_lib/http_client/kitsu_client.cpp
@@ -391,10 +391,10 @@ boost::asio::awaitable<void> kitsu_client::remove_shot_animation_export_file(con
 
 boost::asio::awaitable<nlohmann::json> kitsu_client::get_ue_assembly(uuid in_project_id, uuid in_shot_task_id) const {
   boost::beast::http::request<boost::beast::http::empty_body> l_req{
-      boost::beast::http::verb::post,
+      boost::beast::http::verb::get,
       fmt::format("/api/actions/projects/{}/shots/{}/run-ue-assembly", in_project_id, in_shot_task_id), 11
   };
-  set_req_headers(l_req, {});
+  set_req_headers(l_req);
 
   boost::beast::http::response<http::basic_json_body> l_res{};
   co_await http_client_ptr_->read_and_write(l_req, l_res, boost::asio::use_awaitable);
