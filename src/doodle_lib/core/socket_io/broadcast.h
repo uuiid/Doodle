94e5d029467057608637a5e7c3e52c77c8c0a5a2
uuiid <957714080@qq.com>
2026-03-20 10:18:31 +0800

    feat: 添加模板函数以支持广播任意可序列化数据类型

--- src/doodle_lib/core/socket_io/broadcast.h
+++ src/doodle_lib/core/socket_io/broadcast.h
@@ -13,4 +13,15 @@ void broadcast(
     const std::shared_ptr<sid_ctx>& in_ctx = nullptr
 );
 
+template <typename To_Jsonable>
+concept Jsonable = requires(const To_Jsonable& t) {
+  { nlohmann::json{} = t };
+};
+template <Jsonable To_Jsonable>
+void broadcast(
+    const std::string& in_event, const To_Jsonable& in_data, const std::string& in_namespace = {"/events"},
+    const std::shared_ptr<sid_ctx>& in_ctx = nullptr
+) {
+  broadcast(in_event, nlohmann::json{} = in_data, in_namespace, in_ctx);
+}
 }  // namespace doodle::socket_io
