d205a8f64d0aafc16ac701e3335c83592aa79354
uuiid <957714080@qq.com>
2026-04-27 14:36:20 +0800

    feat: 重构存储类，使用ORM结构体管理表信息和列信息

--- src/doodle_lib/sqlite_orm/orm/storage.h
+++ src/doodle_lib/sqlite_orm/orm/storage.h
@@ -1,33 +1,75 @@
 #pragma once
+#include <boost/pfr.hpp>
+#include <boost/pfr/core_name.hpp>
+
 #include <sqlite_orm/sqlite_orm.h>
 #include <typeindex>
+#include <utility>
 
 namespace doodle {
+
+namespace orm {
+// 使用 boost::pfr 来获取结构体成员变量的指针
+template <typename T, std::size_t Index>
+auto get_member_ptr() {
+
+  
+}
+
+enum class column_type {
+  null,
+  integer,
+  real,
+  text,
+  blob,
+};
+
+struct column_info {
+  std::string name_;
+  column_type type_;
+  bool not_null_{};
+  bool primary_key_{};
+  bool autoincrement_{};
+  void* ptr_{};  // 指向类成员变量的指针
+};
+
+struct table_info {
+  std::string name_;
+  std::vector<column_info> columns_;
+  std::type_index type_index_;
+};
+template <typename T>
+auto make_table_info() {
+  boost::pfr::for_each_field(std::declval<T>(), [](const auto& field, std::size_t index) {
+    using field_type  = std::decay_t<decltype(field)>;
+    // 获取成员变量的指针
+    auto ptr          = boost::pfr::get_relative_address<T, index, T>();
+    // 获取成员变量的类型
+    using member_type = std::remove_reference_t<decltype(std::declval<T>().*ptr)>;
+    // 获取成员变量的名称
+    std::string name  = boost::pfr::get_name<index, T>();
+    // 获取成员变量的类型
+    column_type type;
+    if constexpr (std::is_integral_v<member_type>) {
+      type = column_type::integer;
+    } else if constexpr (std::is_floating_point_v<member_type>) {
+      type = column_type::real;
+    } else if constexpr (std::is_same_v<member_type, std::string>) {
+      type = column_type::text;
+    } else {
+      type = column_type::blob;
+    }
+  });
+}
+}  // namespace orm
+
 class storage {
-  enum column_type { integer, real, text, blob, null };
-
-  struct column_info {
-    std::string name_;
-    std::string type_;
-    bool not_null_{};
-    bool primary_key_{};
-    bool autoincrement_{};
-  };
-
-  struct table_info {
-    std::string name_;
-    std::vector<column_info> columns_;
-    std::type_index type_index_;
-  };
-  std::vector<table_info> tables_;
+  std::vector<orm::table_info> tables_;
 
  public:
   virtual ~storage() = default;
-
-  template <typename T>
-  storage& reg(T&& in_table) {
-    // 解析 make_table 的返回值, 获取表名和列信息
-
+  storage& reg(orm::table_info&& in_table) {
+    tables_.push_back(std::move(in_table));
     return *this;
   }
 };
