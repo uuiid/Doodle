773e97b488644a1cdbf00915285fef88327b3dff
uuiid <957714080@qq.com>
2026-05-19 16:50:58 +0800

    refactor: 调整头文件包含顺序以优化代码结构

--- src/doodle_lib/sqlite_orm/orm/orm.h
+++ src/doodle_lib/sqlite_orm/orm/orm.h
@@ -1,25 +1,26 @@
-#include <doodle_lib/sqlite_orm/orm/all_macro.h>
+// clang-format off
+#include <doodle_lib/sqlite_orm/orm/fwd.h>
 #include <doodle_lib/sqlite_orm/orm/sqlite_statement.h>
 
-// clang-format off
+#include <doodle_lib/sqlite_orm/orm/alias.h>
+#include <doodle_lib/sqlite_orm/orm/all_macro.h>
+#include <doodle_lib/sqlite_orm/orm/bind_value.h>
 #include <doodle_lib/sqlite_orm/orm/column_operations.h>
 #include <doodle_lib/sqlite_orm/orm/column.h>
-#include <doodle_lib/sqlite_orm/orm/alias.h>
-#include <doodle_lib/sqlite_orm/orm/fwd.h>
-
-#include <doodle_lib/sqlite_orm/orm/storage.h>
-
-#include <doodle_lib/sqlite_orm/orm/select.h>
-#include <doodle_lib/sqlite_orm/orm/insert.h>
-#include <doodle_lib/sqlite_orm/orm/update.h>
-#include <doodle_lib/sqlite_orm/orm/delete.h>
 #include <doodle_lib/sqlite_orm/orm/create_trigger.h>
+#include <doodle_lib/sqlite_orm/orm/delete.h>
+#include <doodle_lib/sqlite_orm/orm/exception.h>
+#include <doodle_lib/sqlite_orm/orm/insert.h>
+#include <doodle_lib/sqlite_orm/orm/orm.h>
+#include <doodle_lib/sqlite_orm/orm/select.h>
+#include <doodle_lib/sqlite_orm/orm/storage.h>
+#include <doodle_lib/sqlite_orm/orm/update.h>
 
-
-#include <doodle_lib/sqlite_orm/orm/storage_impl.h>
-#include <doodle_lib/sqlite_orm/orm/select_impl.h>
-#include <doodle_lib/sqlite_orm/orm/column_impl.h>
 #include <doodle_lib/sqlite_orm/orm/alias_impl.h>
 #include <doodle_lib/sqlite_orm/orm/bind_value_impl.h>
+#include <doodle_lib/sqlite_orm/orm/column_impl.h>
 #include <doodle_lib/sqlite_orm/orm/column_operations_impl.h>
+#include <doodle_lib/sqlite_orm/orm/select_impl.h>
+#include <doodle_lib/sqlite_orm/orm/storage_impl.h>
+
 // clang-format on
