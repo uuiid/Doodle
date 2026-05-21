366c829b6d4464c621c72586bc5d64a68f429ded
uuiid <957714080@qq.com>
2026-05-21 11:35:25 +0800

    feat: 在 select_template_t 中的 order_by 方法中添加返回当前实例的功能

--- src/doodle_lib/sqlite_orm/orm/select_impl.h
+++ src/doodle_lib/sqlite_orm/orm/select_impl.h
@@ -237,6 +237,7 @@ template <typename T>
   requires is_alias_column_t_v<std::decay_t<T>>
 select_template_t<Columns...> select_template_t<Columns...>::order_by(T&& alias_column, bool ascending) {
   select_t::order_by(std::forward<T>(alias_column), ascending);
+  return *this;
 }
 
 template <typename... Columns>
