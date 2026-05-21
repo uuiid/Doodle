103e39301b06eb335e9ff25c66bd2ae479458826
uuiid <957714080@qq.com>
2026-05-21 13:43:16 +0800

    feat: 注释掉 sqlite_upgrade 中的部分升级逻辑，简化代码

--- src/doodle_lib/sqlite_orm/sqlite_upgrade.cpp
+++ src/doodle_lib/sqlite_orm/sqlite_upgrade.cpp
@@ -5,7 +5,6 @@
 #include "doodle_core/metadata/entity.h"
 #include <doodle_core/metadata/assets_file.h>
 
-#include <doodle_lib/sqlite_orm/detail/sqlite_database_impl.h>
 #include <doodle_lib/sqlite_orm/sqlite_database.h>
 #include <doodle_lib/sqlite_orm/sqlite_upgrade.h>
 
@@ -39,110 +38,109 @@ struct upgrade_init_t : sqlite_upgrade {
   explicit upgrade_init_t(const FSys::path& in_path) {}
 
   static void full_fts_sync(const std::shared_ptr<sqlite_database_impl>& in_data) {
-    using namespace sqlite_orm;
-    auto l_g                = in_data->storage_any_.transaction_guard();
-    using entity_fts_hidden = fts5::hidden_fields_of<entity_fts>;
-    in_data->storage_any_.insert(
-        into<entity_fts>(), columns(entity_fts_hidden::any_field), values(std::make_tuple("rebuild"))
-    );
-    l_g.commit();
+    // auto l_g                = in_data->storage_any_.transaction_guard();
+    // using entity_fts_hidden = fts5::hidden_fields_of<entity_fts>;
+    // in_data->storage_any_.insert(
+    //     into<entity_fts>(), columns(entity_fts_hidden::any_field), values(std::make_tuple("rebuild"))
+    // );
+    // l_g.commit();
   }
 
   void upgrade(const std::shared_ptr<sqlite_database_impl>& in_data) override {
-    auto l_exit_fts = in_data->storage_any_.table_exists("entity_fts");
-    if (auto l_exit_view = in_data->storage_any_.view_exists("entity_asset_view"); l_exit_view)
-      in_data->storage_any_.drop_view_if_exists("entity_asset_view");
-    in_data->sync_schema();
+//     auto l_exit_fts = in_data->storage_any_.table_exists("entity_fts");
+//     if (auto l_exit_view = in_data->storage_any_.view_exists("entity_asset_view"); l_exit_view)
+//       in_data->storage_any_.drop_view_if_exists("entity_asset_view");
+//     in_data->sync_schema();
 
-    // 不存在表, 并在同步后, 存在虚拟表, 说明是第一次安装, 需要将已有的 entity 数据同步到 entity_fts 虚拟表中
-    if (!l_exit_fts) {
-      full_fts_sync(in_data);
-    }
+//     // 不存在表, 并在同步后, 存在虚拟表, 说明是第一次安装, 需要将已有的 entity 数据同步到 entity_fts 虚拟表中
+//     if (!l_exit_fts) {
+//       full_fts_sync(in_data);
+//     }
 
-    if (in_data->storage_any_.pragma.user_version() == 0) {
-      in_data->storage_any_.pragma.user_version(g_current_version);
-    }
+//     if (in_data->storage_any_.pragma.user_version() == 0) {
+//       in_data->storage_any_.pragma.user_version(g_current_version);
+//     }
 
-    if (in_data->uuid_to_id<assets_helper::database_t>(g_lable_id) == 0) {
-      auto l_label      = std::make_shared<assets_helper::database_t>();
-      l_label->uuid_id_ = g_lable_id;
-      l_label->label_   = "标签";
-      in_data->install_unsafe<assets_helper::database_t>(l_label);
-    }
-    if (in_data->uuid_to_id<project_status>(g_open_id) == 0) {
-      auto l_s      = std::make_shared<project_status>();
-      l_s->uuid_id_ = g_open_id;
-      l_s->name_    = "Open";
-      l_s->color_   = "#000000";
-      in_data->install_unsafe<project_status>(l_s);
-    }
-    if (in_data->uuid_to_id<project_status>(g_closed_id) == 0) {
-      auto l_s      = std::make_shared<project_status>();
-      l_s->uuid_id_ = g_closed_id;
-      l_s->name_    = "Closed";
-      l_s->color_   = "#000000";
-      in_data->install_unsafe<project_status>(l_s);
-    }
-#define DOODLE_ASSET_TYPE(name, sql_name)                                    \
-  if (in_data->uuid_to_id<asset_type>(asset_type::get_##name##_id()) == 0) { \
-    auto l_s       = std::make_shared<asset_type>();                         \
-    l_s->uuid_id_  = asset_type::get_##name##_id();                          \
-    l_s->name_     = #sql_name;                                              \
-    l_s->archived_ = true;                                                   \
-    in_data->install_unsafe<asset_type>(l_s);                                \
-  }
-    DOODLE_ASSET_TYPE(scene, Scene)
-    DOODLE_ASSET_TYPE(sequence, Sequence)
-    DOODLE_ASSET_TYPE(shot, Shot)
-    DOODLE_ASSET_TYPE(edit, Edit)
-    DOODLE_ASSET_TYPE(concept, Concept)
-    DOODLE_ASSET_TYPE(episode, Episode)
-#undef DOODLE_ASSET_TYPE
-#define DOODLE_TASK_TYPE(name, sql_name)                                   \
-  if (in_data->uuid_to_id<task_type>(task_type::get_##name##_id()) == 0) { \
-    auto l_s       = std::make_shared<task_type>();                        \
-    l_s->uuid_id_  = task_type::get_##name##_id();                         \
-    l_s->name_     = #sql_name;                                            \
-    l_s->archived_ = true;                                                 \
-    l_s->color_    = "#999999";                                            \
-    in_data->install_unsafe<task_type>(l_s);                               \
-  }
-    DOODLE_TASK_TYPE(original_painting, 原画)
-    DOODLE_TASK_TYPE(character, 角色)
-    DOODLE_TASK_TYPE(ground_model, 地编模型)
-    DOODLE_TASK_TYPE(binding, 绑定)
-    DOODLE_TASK_TYPE(simulation, 解算资产)
-    DOODLE_TASK_TYPE(effect_asset, 特效资产)
-#undef DOODLE_TASK_TYPE
+//     if (in_data->uuid_to_id<assets_helper::database_t>(g_lable_id) == 0) {
+//       auto l_label      = std::make_shared<assets_helper::database_t>();
+//       l_label->uuid_id_ = g_lable_id;
+//       l_label->label_   = "标签";
+//       in_data->install_unsafe<assets_helper::database_t>(l_label);
+//     }
+//     if (in_data->uuid_to_id<project_status>(g_open_id) == 0) {
+//       auto l_s      = std::make_shared<project_status>();
+//       l_s->uuid_id_ = g_open_id;
+//       l_s->name_    = "Open";
+//       l_s->color_   = "#000000";
+//       in_data->install_unsafe<project_status>(l_s);
+//     }
+//     if (in_data->uuid_to_id<project_status>(g_closed_id) == 0) {
+//       auto l_s      = std::make_shared<project_status>();
+//       l_s->uuid_id_ = g_closed_id;
+//       l_s->name_    = "Closed";
+//       l_s->color_   = "#000000";
+//       in_data->install_unsafe<project_status>(l_s);
+//     }
+// #define DOODLE_ASSET_TYPE(name, sql_name)                                    \
+//   if (in_data->uuid_to_id<asset_type>(asset_type::get_##name##_id()) == 0) { \
+//     auto l_s       = std::make_shared<asset_type>();                         \
+//     l_s->uuid_id_  = asset_type::get_##name##_id();                          \
+//     l_s->name_     = #sql_name;                                              \
+//     l_s->archived_ = true;                                                   \
+//     in_data->install_unsafe<asset_type>(l_s);                                \
+//   }
+//     DOODLE_ASSET_TYPE(scene, Scene)
+//     DOODLE_ASSET_TYPE(sequence, Sequence)
+//     DOODLE_ASSET_TYPE(shot, Shot)
+//     DOODLE_ASSET_TYPE(edit, Edit)
+//     DOODLE_ASSET_TYPE(concept, Concept)
+//     DOODLE_ASSET_TYPE(episode, Episode)
+// #undef DOODLE_ASSET_TYPE
+// #define DOODLE_TASK_TYPE(name, sql_name)                                   \
+//   if (in_data->uuid_to_id<task_type>(task_type::get_##name##_id()) == 0) { \
+//     auto l_s       = std::make_shared<task_type>();                        \
+//     l_s->uuid_id_  = task_type::get_##name##_id();                         \
+//     l_s->name_     = #sql_name;                                            \
+//     l_s->archived_ = true;                                                 \
+//     l_s->color_    = "#999999";                                            \
+//     in_data->install_unsafe<task_type>(l_s);                               \
+//   }
+//     DOODLE_TASK_TYPE(original_painting, 原画)
+//     DOODLE_TASK_TYPE(character, 角色)
+//     DOODLE_TASK_TYPE(ground_model, 地编模型)
+//     DOODLE_TASK_TYPE(binding, 绑定)
+//     DOODLE_TASK_TYPE(simulation, 解算资产)
+//     DOODLE_TASK_TYPE(effect_asset, 特效资产)
+// #undef DOODLE_TASK_TYPE
   }
 };  // namespace doodle::details
 
 struct upgrade_2_t : sqlite_upgrade {
   explicit upgrade_2_t(const FSys::path& in_path) {}
   void upgrade(const std::shared_ptr<sqlite_database_impl>& in_data) override {
-    if (in_data->storage_any_.pragma.user_version() == 1) {
-      in_data->storage_any_.drop_trigger_if_exists("entity_fts_insert_trigger");
-      in_data->storage_any_.drop_trigger_if_exists("entity_fts_update_trigger");
-      in_data->storage_any_.drop_trigger_if_exists("entity_fts_delete_trigger");
-      in_data->storage_any_.drop_trigger_if_exists("entity_fts_insert_trigger2");
-      in_data->storage_any_.drop_trigger_if_exists("entity_fts_update_trigger2");
-      in_data->storage_any_.drop_trigger_if_exists("entity_fts_delete_trigger2");
-      in_data->storage_any_.drop_table_if_exists("entity_fts");
-      in_data->storage_any_.drop_view_if_exists("entity_asset_view");
+    // if (in_data->storage_any_.pragma.user_version() == 1) {
+    //   in_data->storage_any_.drop_trigger_if_exists("entity_fts_insert_trigger");
+    //   in_data->storage_any_.drop_trigger_if_exists("entity_fts_update_trigger");
+    //   in_data->storage_any_.drop_trigger_if_exists("entity_fts_delete_trigger");
+    //   in_data->storage_any_.drop_trigger_if_exists("entity_fts_insert_trigger2");
+    //   in_data->storage_any_.drop_trigger_if_exists("entity_fts_update_trigger2");
+    //   in_data->storage_any_.drop_trigger_if_exists("entity_fts_delete_trigger2");
+    //   in_data->storage_any_.drop_table_if_exists("entity_fts");
+    //   in_data->storage_any_.drop_view_if_exists("entity_asset_view");
 
-      in_data->sync_schema();
-      upgrade_init_t::full_fts_sync(in_data);
-      in_data->storage_any_.pragma.user_version(g_current_version);
-    }
+    //   in_data->sync_schema();
+    //   upgrade_init_t::full_fts_sync(in_data);
+    //   in_data->storage_any_.pragma.user_version(g_current_version);
+    // }
 
-    if (in_data->storage_any_.pragma.user_version() == 2) {
-      upgrade_init_t::full_fts_sync(in_data);
-      in_data->storage_any_.pragma.user_version(g_current_version);
-    }
+    // if (in_data->storage_any_.pragma.user_version() == 2) {
+    //   upgrade_init_t::full_fts_sync(in_data);
+    //   in_data->storage_any_.pragma.user_version(g_current_version);
+    // }
 
-    if (in_data->storage_any_.pragma.user_version() == 3) {
-      in_data->storage_any_.pragma.user_version(g_current_version);
-    }
+    // if (in_data->storage_any_.pragma.user_version() == 3) {
+    //   in_data->storage_any_.pragma.user_version(g_current_version);
+    // }
   }
   ~upgrade_2_t() override = default;
 };
