640e23141c19453117240204355588f141f8094d
uuiid <957714080@qq.com>
2026-05-25 11:09:42 +0800

    feat: 添加table_info和create_index头文件，扩展ORM功能

--- src/doodle_lib/sqlite_orm/orm/orm.h
+++ src/doodle_lib/sqlite_orm/orm/orm.h
@@ -16,6 +16,12 @@
 #include <doodle_lib/sqlite_orm/orm/select.h>
 #include <doodle_lib/sqlite_orm/orm/storage.h>
 #include <doodle_lib/sqlite_orm/orm/update.h>
+#include <doodle_lib/sqlite_orm/orm/table_info.h>
+#include <doodle_lib/sqlite_orm/orm/create_index.h>
+
+
+
+
 
 #include <doodle_lib/sqlite_orm/orm/alias_impl.h>
 #include <doodle_lib/sqlite_orm/orm/bind_value_impl.h>
