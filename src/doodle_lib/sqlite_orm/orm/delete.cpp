b1b0cd1352eb3a0ed5f97722159c2ebbf52dcb48
uuiid <957714080@qq.com>
2026-05-26 15:03:43 +0800

    feat: 重构 delete_t 类，使用 delete_state_t 结构体封装状态，优化 SQL 生成和绑定逻辑

--- src/doodle_lib/sqlite_orm/orm/delete.cpp
+++ src/doodle_lib/sqlite_orm/orm/delete.cpp
@@ -4,23 +4,24 @@
 
 namespace doodle::orm {
 std::string delete_t::to_sql(to_sql_ctx ctx) const {
-  if (!wheres_) throw std::runtime_error("WHERE condition is required for DELETE operation");
+  if (!state_->wheres_) throw std::runtime_error("WHERE condition is required for DELETE operation");
   auto l_ctx = ctx;
   l_ctx.ctx_ |= to_sql_ctx::delete_sql;  // 强制使用 delete_sql 上下文，以确保生成正确的 SQL 片段格式
-  auto l_sql = fmt::format("DELETE FROM {} WHERE {}", from_table_name_, wheres_->to_sql(*s_, l_ctx));
+  auto l_sql =
+      fmt::format("DELETE FROM {} WHERE {}", state_->from_table_name_, state_->wheres_->to_sql(*state_->s_, l_ctx));
   return l_sql;
 }
-delete_t& delete_t::operator()() {
-  if (!stmt_) {
+delete_t delete_t::operator()() {
+  if (!state_->stmt_) {
     to_sql_ctx l_ctx{.ctx_ = to_sql_ctx::delete_sql};
-    auto l_sql = to_sql(l_ctx);
-    stmt_      = std::make_shared<sqlite_stmt>();
-    stmt_->prepare(*s_, l_sql);
-    wheres_->collect_bind_variants(bind_variants_);
+    auto l_sql    = to_sql(l_ctx);
+    state_->stmt_ = std::make_shared<sqlite_stmt>();
+    state_->stmt_->prepare(*state_->s_, l_sql);
+    state_->wheres_->collect_bind_variants(state_->bind_variants_);
   }
-  stmt_->reset_bind();
-  for (const auto& val : bind_variants_.bind_values_) val.bind(*stmt_);
-  stmt_->step();
+  state_->stmt_->reset_bind();
+  for (const auto& val : state_->bind_variants_.bind_values_) val.bind(*state_->stmt_);
+  state_->stmt_->step();
   return *this;
 }
 }  // namespace doodle::orm
--- src/doodle_lib/sqlite_orm/orm/delete.h
+++ src/doodle_lib/sqlite_orm/orm/delete.h
@@ -7,37 +7,43 @@
 #include <doodle_lib/sqlite_orm/orm/storage.h>
 
 namespace doodle::orm {
+
 struct delete_t {
  private:
+  struct delete_state_t {
+    std::string from_table_name_;
+    std::shared_ptr<column_operations_base_t> wheres_;
+    storage* s_{nullptr};
+    std::shared_ptr<sqlite_stmt> stmt_;
+    bind_value_collector_t bind_variants_{};
+  };
   friend class storage;
   friend delete_t delete_from(storage& s);
 
-  std::string from_table_name_;
-  std::shared_ptr<column_operations_base_t> wheres_;
-  storage* s_{nullptr};
-  std::shared_ptr<sqlite_stmt> stmt_;
-  bind_value_collector_t bind_variants_{};
+  std::shared_ptr<delete_state_t> state_;
 
  public:
+  delete_t() : state_(std::make_shared<delete_state_t>()) {}
+
   template <typename T>
-  delete_t& where(T&& condition_fun) {
+  delete_t where(T&& condition_fun) {
     auto l_condition_fun_ptr = std::make_shared<T>(std::forward<T>(condition_fun));
-    wheres_                  = l_condition_fun_ptr;
+    state_->wheres_          = l_condition_fun_ptr;
     return *this;
   }
   template <typename FromTable>
-  delete_t& from() {
-    from_table_name_ = s_->get_table_name<FromTable>();
+  delete_t from() {
+    state_->from_table_name_ = state_->s_->get_table_name<FromTable>();
     return *this;
   }
 
-  delete_t& operator()();
+  delete_t operator()();
 
   std::string to_sql(to_sql_ctx ctx) const;
 };
 inline delete_t delete_from(storage& s) {
   delete_t l_delete{};
-  l_delete.s_ = &s;
+  l_delete.state_->s_ = &s;
   return l_delete;
 }
 }  // namespace doodle::orm
