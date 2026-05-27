3f93d371141d6d8dd5902a6e5ab9f284a6c59e7f
uuiid <957714080@qq.com>
2026-05-27 09:30:51 +0800

    feat: 修改 to_sql 方法中的字段名为小写，以确保一致性

--- src/doodle_lib/sqlite_orm/orm/storage.cpp
+++ src/doodle_lib/sqlite_orm/orm/storage.cpp
@@ -51,12 +51,12 @@ std::string table_fts_info::to_sql(storage& s, to_sql_ctx ctx) const {
     }
     l_column_sqls.push_back(std::move(l_sql));
   }
-  if (content_table_) l_column_sqls.push_back(fmt::format(R"(CONTENT='{}')", content_table_->get_table_name(s)));
+  if (content_table_) l_column_sqls.push_back(fmt::format(R"(content='{}')", content_table_->get_table_name(s)));
 
   if (content_rowid_)
-    l_column_sqls.push_back(fmt::format(R"(CONTENT_ROWID='{}')", content_rowid_->get_column_name(s, ctx)));
+    l_column_sqls.push_back(fmt::format(R"(content_rowid='{}')", content_rowid_->get_column_name(s, ctx)));
 
-  if (!tokenizer_.empty()) l_column_sqls.push_back(fmt::format(R"(TOKENIZER='{}')", tokenizer_));
+  if (!tokenizer_.empty()) l_column_sqls.push_back(fmt::format(R"(tokenize='{}')", tokenizer_));
   return fmt::format("CREATE VIRTUAL TABLE IF NOT EXISTS {} USING fts5 ({})", name_, fmt::join(l_column_sqls, ", "));
 }
 
