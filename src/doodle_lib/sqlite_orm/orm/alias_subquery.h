6b9f4fbcef66dac6ff860115ade2eb3470376ae3
uuiid <957714080@qq.com>
2026-06-01 17:07:06 +0800

    feat: 添加子查询别名操作符重载，增强subquery_alias_info_t功能

--- src/doodle_lib/sqlite_orm/orm/alias_subquery.h
+++ src/doodle_lib/sqlite_orm/orm/alias_subquery.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <doodle_core/doodle_core_fwd.h>
 
+#include <doodle_lib/sqlite_orm/orm/alias.h>
 #include <doodle_lib/sqlite_orm/orm/fwd.h>
 #include <doodle_lib/sqlite_orm/orm/select.h>
 #include <doodle_lib/sqlite_orm/orm/table_info.h>
@@ -15,6 +16,10 @@ struct subquery_alias_info_t : public table_info_base_t {
   // std::string get_table_name(const storage& s) const override;
   std::string to_sql(const storage& s, const to_sql_ctx& ctx) const override;
   void collect_bind_variants(bind_value_collector_t& bind_variants) const override;
+  template <typename Table, typename ValueType>
+  alias_column_info_t operator->*(ValueType Table::* in_column_alias) {
+    return alias_column_info_t{in_column_alias, alias_name_};
+  }
 };
 
 inline subquery_alias_info_t alias(const std::string& alias_name, const select_t& subquery) {
