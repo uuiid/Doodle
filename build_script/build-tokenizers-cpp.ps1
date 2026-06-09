94bea5c74fd1d5f391d3709b143cecbae7b409b8
uuiid <957714080@qq.com>
2026-06-09 17:31:30 +0800

    feat: 更新构建脚本以支持vcpkg工具链，修复构建目录检查逻辑

--- build_script/build-tokenizers-cpp.ps1
+++ build_script/build-tokenizers-cpp.ps1
@@ -1,13 +1,18 @@
 . $PSScriptRoot\build\set_env.ps1
 
+$vcpkg_root =  Convert-Path "$DoodleRoot\..\vcpkg"
+
 Push-Location $DoodleRoot/external/tokenizers-cpp
 
 # 进入 build 目录
-if (-Not (Test-Path -Path "build")) {
+if (-Not (Test-Path -Path "build" -ErrorAction SilentlyContinue)) {
   New-Item -ItemType Directory -Path "build" | Out-Null
 }
 
-Set-Location build
+Push-Location build
 # 构建并安装
-&cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install 
+&cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE="$vcpkg_root\scripts\buildsystems\vcpkg.cmake" -DMLC_ENABLE_SENTENCEPIECE_TOKENIZER=OFF
 &cmake --build . --target install
+
+Pop-Location
+Pop-Location
