cca3ff2cf467e654e4abecbecb96b7df7d7ce6f2
uuiid <957714080@qq.com>
2026-07-08 17:17:04 +0800

    refactor: 更新 to_string_fun_ 和 to_string 方法的参数类型为 const session&

--- src/doodle_lib/sqlite_orm/orm/bind_value.h
+++ src/doodle_lib/sqlite_orm/orm/bind_value.h
@@ -14,7 +14,7 @@ struct bind_value_t {
   std::any value_;
   // 这个类是可复制的, 因此需要确保 bind_fun_ 的复制行为正确, 因此, 不可以捕获 this指针, 直接在参数中传递需要的值
   std::function<void(const bind_value_t&, sqlite_stmt& stmt)> bind_fun_;
-  std::function<std::string(const bind_value_t&, session& in_session, const to_sql_ctx& ctx)> to_string_fun_;
+  std::function<std::string(const bind_value_t&, const session& in_session, const to_sql_ctx& ctx)> to_string_fun_;
 
   template <typename T>
     requires(!std::same_as<std::remove_cvref_t<T>, bind_value_t> && !char_pointer<T>)
@@ -34,7 +34,7 @@ struct bind_value_t {
   operator bool() const { return bind_fun_ != nullptr; }
 
   void bind(sqlite_stmt& stmt) const;
-  std::string to_string(session& in_session, const to_sql_ctx& ctx) const;
+  std::string to_string(const session& in_session, const to_sql_ctx& ctx) const;
 };
 struct bind_value_collector_t {
   std::vector<bind_value_t> bind_values_;
