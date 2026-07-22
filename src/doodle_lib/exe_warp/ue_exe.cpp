2b1d6acedc42d29cbd6569b41d6c5e3aec470377
uuiid <957714080@qq.com>
2026-07-22 18:48:13 +0800

    🐛 fix(ue_exe): 修复UE版本日志记录中使用的版本变量错误 - 将日志语句移到解包版本数据之后，并使用从kitsu获取的正确版本变量l_version代替get_ue_plug_version()函数

--- src/doodle_lib/exe_warp/ue_exe.cpp
+++ src/doodle_lib/exe_warp/ue_exe.cpp
@@ -87,9 +87,9 @@ boost::asio::awaitable<bool> installUePath(
   boost::scope::scope_exit l_scope{[] {}};
   auto l_version_data              = co_await in_kitsu_client->get_ue_plugins_version();
   auto l_ue_version                = core_set::get_set().ue4_version;
-  SPDLOG_LOGGER_WARN(in_logger, "UE 版本 {} 插件版本 {}", l_ue_version, get_ue_plug_version());
   
   auto [l_version, l_version_list] = l_version_data;
+  SPDLOG_LOGGER_WARN(in_logger, "UE 版本 {} 插件版本 {}", l_ue_version, l_version);
   DOODLE_CHICK(!l_version.empty(), "获取 UE 插件版本失败");
   if (l_version.starts_with(l_ue_version) && l_version == get_ue_plug_version()) co_return false;
 
