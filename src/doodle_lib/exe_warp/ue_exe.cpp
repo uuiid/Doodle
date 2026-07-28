27e9d998b5e0b2ba76942850c5c72c0c9935f1c0
uuiid <957714080@qq.com>
2026-07-28 10:27:40 +0800

    🐛 fix(ue_exe): 修复 UE 版本比较逻辑
    - 将版本精确匹配改为后缀匹配，以适应带后缀的版本字符串

--- src/doodle_lib/exe_warp/ue_exe.cpp
+++ src/doodle_lib/exe_warp/ue_exe.cpp
@@ -102,7 +102,7 @@ boost::asio::awaitable<bool> installUePath(
     l_version.starts_with(l_ue_version), "未能找到 UE 插件版本 {} 对应的 UE 版本 {}", l_version, l_ue_version
   );
   SPDLOG_LOGGER_WARN(in_logger, "UE 版本 {} 插件版本 {}", l_ue_version, l_version);
-  if (l_version == get_ue_plug_version()) co_return false;
+  if (l_version.ends_with(get_ue_plug_version())) co_return false;
 
   auto l_path     = co_await in_kitsu_client->get_ue_plugin(l_version);
   auto l_out_path = l_path.parent_path() / l_path.stem();
