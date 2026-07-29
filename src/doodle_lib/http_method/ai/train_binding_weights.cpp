64d260360b873c6de544d350305167864cb0977e
uuiid <957714080@qq.com>
2026-07-29 11:08:47 +0800

    feat: 将AI模型从LLM2Vec替换为Kimodo并添加帧数参数

--- src/doodle_lib/http_method/ai/train_binding_weights.cpp
+++ src/doodle_lib/http_method/ai/train_binding_weights.cpp
@@ -1,7 +1,6 @@
 #include "doodle_core/exception/exception.h"
 
-#include <doodle_lib/ai/hf_tokenizer.h>
-#include <doodle_lib/ai/llm2vec.h>
+#include <doodle_lib/ai/kimodo.h>
 #include <doodle_lib/core/global_function.h>
 #include <doodle_lib/http_method/ai/ai_main.h>
 
@@ -14,7 +13,6 @@
 #include <onnxruntime_cxx_api.h>
 #include <spdlog/spdlog.h>
 #include <string>
-
 
 namespace doodle::http {
 namespace {
@@ -35,31 +33,31 @@ void init_ort_env() {
 
 struct ai_train_binding_weights_post_args {
   std::string text_{};
+  std::int32_t num_frames_{120};
 
   // from json
   friend void from_json(const nlohmann::json& in_json, ai_train_binding_weights_post_args& out) {
     if (in_json.contains("text") && in_json.at("text").is_string()) in_json.at("text").get_to(out.text_);
+    if (in_json.contains("num_frames") && in_json.at("num_frames").is_number_integer())
+      in_json.at("num_frames").get_to(out.num_frames_);
   }
 };
 
 }  // namespace
 
 struct ai_train_animation::impl {
-  std::shared_ptr<doodle::ai::LLM2Vec> model_{};
+  std::shared_ptr<doodle::ai::kimodo> model_{};
   impl() = default;
   std::once_flag init_flag_;
   void init() {
-    model_ = std::make_shared<doodle::ai::LLM2Vec>(
-        R"(D:\ai_mod\onnx-McGill-NLP--LLM2Vec-Meta-Llama-3-8B-Instruct-mntp\model.onnx)",
-        R"(D:\ai_mod\onnx-McGill-NLP--LLM2Vec-Meta-Llama-3-8B-Instruct-mntp\tokenizer.json)"
-    );
+    model_        = std::make_shared<doodle::ai::kimodo>();
+    auto l_config = std::make_shared<doodle::ai::kimodo_model_config>();
+    l_config->load_from_json(R"(D:\ai_mod\onnx-models--nvidia--Kimodo-SOMA-RP-v1.1)");
+    model_->load(l_config);
   }
-  void run(const std::string& text) {
+  void run(const ai_train_binding_weights_post_args& in_args) {
     std::call_once(init_flag_, &impl::init, this);
-    auto embedding = (*model_)("", text);
-    SPDLOG_INFO(
-        "Generated embedding of size {} for input text '{}' [{}]", embedding.size(), text, fmt::join(embedding, ",")
-    );
+    auto output = model_->generate(in_args.text_, in_args.num_frames_, 50);
   }
 };
 ai_train_animation::ai_train_animation() : impl_ptr_(std::make_shared<impl>()) {}
@@ -67,7 +65,7 @@ DOODLE_HTTP_FUN_OVERRIDE_IMPLEMENT(ai_train_animation, post) {
 #ifndef NDEBUG
   init_ort_env();
   auto l_args = in_handle->get_json().get<ai_train_binding_weights_post_args>();
-  boost::asio::post(g_io_context(), [this, l_args]() { impl_ptr_->run(l_args.text_); });
+  boost::asio::post(g_io_context(), [this, l_args]() { impl_ptr_->run(l_args); });
 #endif
   co_return in_handle->make_msg(nlohmann::json{});
 }
