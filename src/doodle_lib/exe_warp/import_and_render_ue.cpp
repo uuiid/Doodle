e8c7682b0b37f8dbd54a5d4131106b819ed8981b
uuiid <957714080@qq.com>
2026-08-10 14:04:52 +0800

    🐛 fix: 注释Groom插件启用，避免AlembicHairImporter与Groom同时加载导致冲突

--- src/doodle_lib/exe_warp/import_and_render_ue.cpp
+++ src/doodle_lib/exe_warp/import_and_render_ue.cpp
@@ -67,7 +67,7 @@ void fix_project(const FSys::path& in_project_path) {
   l_enabled_plug(l_plugin, "MoviePipelineMaskRenderPass");
   l_enabled_plug(l_plugin, "MovieRenderPipeline");
   l_enabled_plug(l_plugin, "AlembicHairImporter");
-  l_enabled_plug(l_plugin, "Groom");
+  // l_enabled_plug(l_plugin, "Groom");
   l_enabled_plug(l_plugin, "UAssetBrowser", false);
   FSys::ofstream{in_project_path} << l_json.dump();
 }
