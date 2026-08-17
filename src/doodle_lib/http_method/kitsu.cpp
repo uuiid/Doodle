668db1e3a6398d7010a89b1dc1f63fe4b77e4f2d
uuiid <957714080@qq.com>
2026-08-17 16:22:14 +0800

    🐛 fix: 修正Seedance2子项目任务路由，添加entity_id路径参数

--- src/doodle_lib/http_method/kitsu.cpp
+++ src/doodle_lib/http_method/kitsu.cpp
@@ -377,7 +377,10 @@ http_route_ptr create_kitsu_route_2(const FSys::path& in_root) {
         &seedance2::seedance2_subproject_ai_generate_entity_instance::subproject_id_,
         &seedance2::seedance2_subproject_ai_generate_entity_instance::entity_id_
       ))
-      .reg_t<seedance2::seedance2_subproject_task>("/api/seedance2/subproject/{}/task"_url(&seedance2::seedance2_subproject_task::subproject_id_))
+      .reg_t<seedance2::seedance2_subproject_task>("/api/seedance2/subproject/{}/entity/{}/task"_url(
+        &seedance2::seedance2_subproject_task::subproject_id_,
+        &seedance2::seedance2_subproject_task::entity_id_
+      ))
       .reg_t<seedance2::seedance2_subproject_task_instance>("/api/seedance2/subproject/{}/task/{}"_url(
         &seedance2::seedance2_subproject_task_instance::subproject_id_,&seedance2::seedance2_subproject_task_instance::id_
       ))
