669b6baa9ede960abb0e578eabecf5122b3c6396
uuiid <957714080@qq.com>
2026-08-24 11:22:44 +0800

    ✨ feat: 备份脚本新增归档前一日Token消耗统计的POST请求

--- build_script/backup.ps1
+++ build_script/backup.ps1
@@ -8,3 +8,11 @@ $headers = @{
 $res = Invoke-WebRequest -Uri "http://$Kitsu_Ip/api/doodle/backup" -Method Post -Headers $headers 
 
 Copy-Item -Path $res.Content -Destination "\\192.168.0.67\bakk3\doodlebak\kitsu_backup" -Force
+
+# 归档前一日 token 消耗统计 (seedance2_tokens_person_date POST)
+$Yesterday = (Get-Date).AddDays(-1).ToString("yyyy-MM-dd")
+Write-Host "归档 $Yesterday 的 Token 消耗统计 http://$Kitsu_Ip/api/seedance2/tokens/person/date/$Yesterday"
+Invoke-WebRequest -Uri "http://$Kitsu_Ip/api/seedance2/tokens/person/date/$Yesterday" -Method Post -Headers @{
+  "Authorization" = "Bearer $KitsuCookies",
+  "Content-Type" = "application/json"
+} -Body "{}" | Out-Null
