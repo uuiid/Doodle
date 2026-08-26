138b526d3ff197a6096ec12b0d00378a5ecd7d05
uuiid <957714080@qq.com>
2026-08-26 18:03:10 +0800

    ✨ feat: 新增 nvidia-cutlass v4.5.2 的 vcpkg 移植及 CUDA/cuDNN 构建补丁

---
+++ vcpkg_ports/nvidia-cutlass/portfile.cmake
@@ -0,0 +1,58 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NVIDIA/cutlass
+    REF "v${VERSION}"
+    SHA512 435bd838b1c89a5fec45c95ac45bec78b3dd641f93665b97a2cea3d4a9a82bddb9d7cd4139afe9d749e5c6e7195521f09aff4871981eddfa546d5bf9b2dc23a3
+    HEAD_REF main
+    PATCHES
+        fix-cudnn-path.patch
+        fix-cuda128-atomic-load.patch
+)
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON_PATH "${PYTHON3}" PATH)
+vcpkg_add_to_path(PREPEND "${PYTHON_PATH}")
+
+vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT cuda_toolkit_root)
+list(APPEND FEATURE_OPTIONS
+    "-DCMAKE_CUDA_COMPILER=${NVCC}"
+    "-DCUDAToolkit_ROOT=${cuda_toolkit_root}"
+)
+
+list(APPEND CMAKE_MODULE_PATH "${CURRENT_INSTALLED_DIR}/share/cudnn")
+find_package(CUDNN REQUIRED)
+get_filename_component(CUDNN_LIBRARY_DIR "${CUDNN_LIBRARIES}" DIRECTORY)
+set(ENV{CUDNN_PATH} "${CUDNN_LIBRARY_DIR};${CUDNN_INCLUDE_DIRS}")
+
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DCMAKE_SUPPRESS_REGENERATION=ON # for some reason it keeps regenerating in Windows
+        "-DCUTLASS_REVISION:STRING=v${VERSION}"
+        -DCUTLASS_NATIVE_CUDA=OFF
+        -DCUTLASS_ENABLE_HEADERS_ONLY=ON
+        -DCUTLASS_ENABLE_TOOLS=ON
+        -DCUTLASS_ENABLE_LIBRARY=OFF
+        -DCUTLASS_ENABLE_PROFILER=OFF
+        -DCUTLASS_ENABLE_PERFORMANCE=OFF
+        -DCUTLASS_ENABLE_TESTS=OFF
+        -DCUTLASS_ENABLE_GTEST_UNIT_TESTS=OFF
+        -DCUTLASS_ENABLE_CUBLAS=ON
+        -DCUTLASS_ENABLE_CUDNN=ON
+        "-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}"
+        ${FEATURE_OPTIONS}
+    MAYBE_UNUSED_VARIABLES
+        CUTLASS_NATIVE_CUDA
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "NvidiaCutlass")
+
+
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/test"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
