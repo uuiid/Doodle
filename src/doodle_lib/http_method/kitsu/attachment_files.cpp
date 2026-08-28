b8a439b68760c3a93482634a0f1f8263fdf39db9
uuiid <957714080@qq.com>
2026-08-28 11:31:41 +0800

    ♻️ refactor: 移除附件文件获取接口中已注释的权限检查代码

--- src/doodle_lib/http_method/kitsu/attachment_files.cpp
+++ src/doodle_lib/http_method/kitsu/attachment_files.cpp
@@ -17,10 +17,6 @@ DOODLE_HTTP_FUN_OVERRIDE_IMPLEMENT(data_attachment_files_file, get) {
 
   if (l_attachment_file.comment_id_.is_nil() && l_attachment_file.chat_message_id_.is_nil())
     throw_exception(http_request_error{boost::beast::http::status::not_found, "未找到对应的文件"});
-  // if (!l_attachment_file.comment_id_.is_nil()) {
-  //   auto l_task = l_sql.get_by_uuid<task>(l_sql.get_by_uuid<comment>(l_attachment_file.comment_id_).object_id_);
-  //   person_.check_project_access(l_task.project_id_);
-  // }
   auto l_path = g_ctx().get<kitsu_ctx_t>().root_ / "files" / "attachments" /
                 FSys::split_uuid_path(fmt::to_string(l_attachment_file.uuid_id_));
   auto l_ext = l_attachment_file.extension_;
