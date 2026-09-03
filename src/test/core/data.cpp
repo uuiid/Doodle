76006756848235c990a04d5d5067a160f74117ab
uuiid <957714080@qq.com>
2026-09-03 11:58:05 +0800

    🔥 chore: 移除废弃的测试文件及其 CMake 配置

--- src/test/core/data.cpp
+++ src/test/core/data.cpp
@@ -25,23 +25,6 @@
 #include <vector>
 
 BOOST_AUTO_TEST_SUITE(data)
-BOOST_AUTO_TEST_CASE(http_client) {
-  using namespace doodle;
-  app_base l_app{};
-  kitsu::kitsu_client l_client{"http://127.0.0.1:50025"};
-
-  boost::asio::co_spawn(
-      g_io_context(),
-      [&]() -> boost::asio::awaitable<void> {
-        auto l_uuid = from_uuid_str("96a1f1d5-e37d-4f22-90e0-1817468c9c3e");
-        co_await l_client.upload_asset_file_maya(
-            l_uuid, "D:\\test_db\\public\\DYX\\6_moxing\\CFX\\Ch014B_rig_jxh_cloth.ma"
-        );
-      },
-      boost::asio::detached
-  );
-  l_app.run();
-}
 
 BOOST_AUTO_TEST_CASE(mu_sqlorm) {
   using namespace doodle;
